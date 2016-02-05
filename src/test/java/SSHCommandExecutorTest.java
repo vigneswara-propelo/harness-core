6f270c88ec9334d79d06c8e47d80840997e68453
Anubhaw Srivastava <anubhaw@harness.io>
2016-02-05 12:53:32 -0800

    Add test for SSHSudoCommandExecutor

--- src/test/java/SSHCommandExecutorTest.java
+++ src/test/java/SSHCommandExecutorTest.java
@@ -1,13 +1,14 @@
 import org.junit.Test;
 
+import software.wings.helpers.executors.SSHCommandExecutor;
 import software.wings.helpers.executors.callbacks.ConsoleExecutionCallback;
 import software.wings.helpers.executors.SSHSudoCommandExecutor;
 
 public class SSHCommandExecutorTest {
   @Test
   public void test() {
-    SSHSudoCommandExecutor executor = new SSHSudoCommandExecutor("localhost", 2222, "osboxes", "osboxes.org",
-        "sudo su - vagrant  -c 'ls && whoami'", new ConsoleExecutionCallback(null));
+    SSHCommandExecutor executor = new SSHCommandExecutor(
+        "localhost", 2222, "osboxes", "osboxes.org", "ls && whoami", new ConsoleExecutionCallback(null));
     executor.execute();
   }
 }
