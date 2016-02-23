da10f37f73132459f06caca714baa34cd5c04b6e
Anubhaw Srivastava <anubhaw@harness.io>
2016-02-23 14:16:38 -0800

    Remove callback classes as they no longer required

--- src/main/java/software/wings/core/ssh/executors/SSHJumpboxExecutor.java
+++ src/main/java/software/wings/core/ssh/executors/SSHJumpboxExecutor.java
@@ -1,14 +1,6 @@
 package software.wings.core.ssh.executors;
 
-import com.jcraft.jsch.*;
-import org.slf4j.LoggerFactory;
-import software.wings.core.ssh.executors.callbacks.SSHCommandExecutionCallback;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-
-import static software.wings.utils.Misc.quietSleep;
+import com.jcraft.jsch.Session;
 
 /**
  * Created by anubhaw on 2/5/16.
--- src/main/java/software/wings/core/ssh/executors/callbacks/SSHCommandExecutionCallback.java
+++
@@ -1,7 +0,0 @@
-package software.wings.core.ssh.executors.callbacks;
-
-public interface SSHCommandExecutionCallback {
-  public void log(String message);
-
-  public void updateStatus();
-}
