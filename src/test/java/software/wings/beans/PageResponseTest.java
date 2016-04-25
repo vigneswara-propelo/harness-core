78f297487811c16142786ed406b3f9a75c116ed9
Peeyush Aggarwal <peeyush@wings.software>
2016-04-25 13:04:12 -0700

    fix: change json format for PageResponse to be Object so it converts to json as expected.

---
+++ src/test/java/software/wings/beans/PageResponseTest.java
@@ -0,0 +1,24 @@
+package software.wings.beans;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.google.common.collect.Lists;
+
+import org.junit.Test;
+import software.wings.common.JsonUtils;
+
+/**
+ * Created by peeyushaggarwal on 4/25/16.
+ */
+public class PageResponseTest {
+  @Test
+  public void shouldReturnPageResponseAsAnObject() {
+    PageResponse pageResponse = new PageResponse();
+    pageResponse.setResponse(Lists.newArrayList());
+    pageResponse.setTotal(100);
+    assertThat(JsonUtils.asJson(pageResponse))
+        .isEqualTo("{\"offset\":null,\"start\":0,\"limit\":null,\"pageSize\":50,\"filters\":[],"
+            + "\"orders\":[],\"fieldsIncluded\":[],\"fieldsExcluded\":[],\"response\":[],"
+            + "\"total\":100,\"empty\":true,\"currentPage\":1,\"or\":false}");
+  }
+}
