33c504aac0f086ae128d9d9510318b708938bb26
Anubhaw Srivastava <anubhaw@harness.io>
2016-06-07 13:01:03 -0700

    Fix: set 401/403 status code on auth failures

--- src/main/java/software/wings/exception/WingsExceptionMapper.java
+++ src/main/java/software/wings/exception/WingsExceptionMapper.java
@@ -4,8 +4,12 @@ import static software.wings.beans.RestResponse.Builder.aRestResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import software.wings.beans.ErrorConstants;
+import software.wings.beans.ResponseMessage;
 
+import java.util.List;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.ExceptionMapper;
 
 // TODO: Auto-generated Javadoc
@@ -22,8 +26,16 @@ public class WingsExceptionMapper implements ExceptionMapper<WingsException> {
   @Override
   public Response toResponse(WingsException ex) {
     logger.error("Exception occured", ex);
-    return Response.status(Response.Status.BAD_REQUEST)
+    return Response.status(resolveHttpErrorCode(ex.getResponseMessageList()))
         .entity(aRestResponse().withResponseMessages(ex.getResponseMessageList()).build())
         .build();
+  }
+
+  private Status resolveHttpErrorCode(List<ResponseMessage> responseMessageList) {
+    String errorCode = null;
+    if (responseMessageList != null && responseMessageList.size() > 0) {
+      errorCode = responseMessageList.get(responseMessageList.size() - 1).getCode();
+    }
+    return ErrorConstants.httpErrorCodeMapper(errorCode);
   }
 }
