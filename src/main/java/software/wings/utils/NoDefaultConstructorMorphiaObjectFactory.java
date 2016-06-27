fc9029a0c1297d24c3bcc1252361c304f09098ce
Peeyush Aggarwal <peeyush@wings.software>
2016-06-27 16:09:16 -0700

    fix: using objensis instead of unsafe.

--- src/main/java/software/wings/utils/NoDefaultConstructorMorphiaObjectFactory.java
+++ src/main/java/software/wings/utils/NoDefaultConstructorMorphiaObjectFactory.java
@@ -1,17 +1,18 @@
 package software.wings.utils;
 
+import org.modelmapper.internal.objenesis.Objenesis;
+import org.modelmapper.internal.objenesis.ObjenesisStd;
 import org.mongodb.morphia.mapping.DefaultCreator;
 import org.mongodb.morphia.mapping.MappingException;
-import software.wings.exception.WingsException;
-import sun.misc.Unsafe;
 
 import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
 
 /**
  * Created by peeyushaggarwal on 6/23/16.
  */
 public class NoDefaultConstructorMorphiaObjectFactory extends DefaultCreator {
+  private static final Objenesis objenesis = new ObjenesisStd();
+
   @Override
   public Object createInstance(Class clazz) {
     try {
@@ -20,7 +21,8 @@ public class NoDefaultConstructorMorphiaObjectFactory extends DefaultCreator {
         return constructor.newInstance();
       }
       try {
-        return getUnsafe().allocateInstance(clazz);
+        return objenesis.newInstance(clazz);
+
       } catch (Exception e) {
         throw new MappingException("Failed to instantiate " + clazz.getName(), e);
       }
@@ -37,27 +39,5 @@ public class NoDefaultConstructorMorphiaObjectFactory extends DefaultCreator {
     } catch (NoSuchMethodException e) {
       return null;
     }
-  }
-
-  @SuppressWarnings("restriction")
-  private static Unsafe getUnsafe() {
-    try {
-      Field singleoneInstanceField = Unsafe.class.getDeclaredField("theUnsafe");
-      singleoneInstanceField.setAccessible(true);
-      return (Unsafe) singleoneInstanceField.get(null);
-
-    } catch (IllegalArgumentException e) {
-      throw createExceptionForObtainingUnsafe(e);
-    } catch (SecurityException e) {
-      throw createExceptionForObtainingUnsafe(e);
-    } catch (NoSuchFieldException e) {
-      throw createExceptionForObtainingUnsafe(e);
-    } catch (IllegalAccessException e) {
-      throw createExceptionForObtainingUnsafe(e);
-    }
-  }
-
-  private static WingsException createExceptionForObtainingUnsafe(Exception e) {
-    throw new WingsException(e);
   }
 }
