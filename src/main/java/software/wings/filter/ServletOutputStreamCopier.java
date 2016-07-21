c71df24b1862862b63f13768b3f08932828b819b
Peeyush Aggarwal <peeyush@wings.software>
2016-07-21 14:51:17 -0700

    misc: removed unecessary todos

--- src/main/java/software/wings/filter/ServletOutputStreamCopier.java
+++ src/main/java/software/wings/filter/ServletOutputStreamCopier.java
@@ -70,12 +70,9 @@ public class ServletOutputStreamCopier extends ServletOutputStream {
 
   @Override
   public boolean isReady() {
-    // TODO Auto-generated method stub
     return false;
   }
 
   @Override
-  public void setWriteListener(WriteListener arg0) {
-    // TODO Auto-generated method stub
-  }
+  public void setWriteListener(WriteListener arg0) {}
 }
