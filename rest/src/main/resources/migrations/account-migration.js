86da2e694f699b6842b22ed0acb701e7cf05b03f
Peeyush Aggarwal <peeyush@wings.software>
2016-11-09 17:24:21 -0800

    misc: fix migration and deploy

--- rest/src/main/resources/migrations/account-migration.js
+++ rest/src/main/resources/migrations/account-migration.js
@@ -2,8 +2,8 @@
 //changeset peeyushaggarwal:account-migration
 
 if(db.account.count() == 0) {
-  db.account.insert({ "_id" : "kmpySmUISimoRrJL6NL73w", "companyName" : "Wings Software",
-  "appId" : "__GLOBAL_APP_ID__", "createdAt" : 1476312420147, "lastUpdatedAt" : 1476312421284 });
+  db.accounts.insert({ "_id" : "kmpySmUISimoRrJL6NL73w", "companyName" : "Wings Software",
+  "appId" : "__GLOBAL_APP_ID__", "createdAt" : NumberLong("1476312420147"), "lastUpdatedAt" : NumberLong("1476312421284") });
   db.applications.updateMany({}, { "$set": { "accountId": "kmpySmUISimoRrJL6NL73w"}});
   db.settingAttributes.updateMany({}, { "$set": { "accountId": "kmpySmUISimoRrJL6NL73w"}});
   db.notifications.updateMany({}, { "$set": { "accountId": "kmpySmUISimoRrJL6NL73w"}});
