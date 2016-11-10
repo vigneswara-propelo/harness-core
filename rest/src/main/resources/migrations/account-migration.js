bb0cb7b8556d2c5bb3edcfd69eb35eac740b34d0
Peeyush Aggarwal <peeyush@wings.software>
2016-11-09 16:26:23 -0800

    fix: migration script

--- rest/src/main/resources/migrations/account-migration.js
+++ rest/src/main/resources/migrations/account-migration.js
@@ -4,11 +4,11 @@
 if(db.account.count() == 0) {
   db.account.insert({ "_id" : "kmpySmUISimoRrJL6NL73w", "companyName" : "Wings Software",
   "appId" : "__GLOBAL_APP_ID__", "createdAt" : 1476312420147, "lastUpdatedAt" : 1476312421284 });
-  db.applications.updateMany({}, { "accountId": "kmpySmUISimoRrJL6NL73w"});
-  db.settingAttributes.updateMany({}, { "accountId": "kmpySmUISimoRrJL6NL73w"});
-  db.notifications.updateMany({}, { "accountId": "kmpySmUISimoRrJL6NL73w"});
-  db.roles.updateMany({}, { "accountId": "kmpySmUISimoRrJL6NL73w"});
-  db.users.updateMany({}, { "accounts": ["kmpySmUISimoRrJL6NL73w"]});
-  db.appContainers.updateMany({}, { "accountId": "kmpySmUISimoRrJL6NL73w"});
+  db.applications.updateMany({}, { "$set": { "accountId": "kmpySmUISimoRrJL6NL73w"}});
+  db.settingAttributes.updateMany({}, { "$set": { "accountId": "kmpySmUISimoRrJL6NL73w"}});
+  db.notifications.updateMany({}, { "$set": { "accountId": "kmpySmUISimoRrJL6NL73w"}});
+  db.roles.updateMany({}, { "$set": { "accountId": "kmpySmUISimoRrJL6NL73w"}});
+  db.users.updateMany({}, { "$set": { "accountId": "kmpySmUISimoRrJL6NL73w"}});
+  db.appContainers.updateMany({}, { "$set": { "accountId": "kmpySmUISimoRrJL6NL73w"}});
   db.settingAttributes.updateMany({ name: { "$in": ["Wings Jenkins", "SMTP", "Splunk", "AppDynamics"] }}, { "$set": { "isPluginSetting": true }});
 }
