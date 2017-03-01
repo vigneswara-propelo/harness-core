e4b45d73e332aa6d7164b33bc0c599477b4dd8f0
Anubhaw Srivastava <anubhaw@harness.io>
2017-02-28 19:00:45 -0800

    Fix: compilation error

--- rest/src/main/java/software/wings/service/impl/AwsInfrastructureProvider.java
+++ rest/src/main/java/software/wings/service/impl/AwsInfrastructureProvider.java
@@ -64,6 +64,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
