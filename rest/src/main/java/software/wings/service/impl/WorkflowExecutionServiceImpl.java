29d013908c329fc9e59fcafb9dc12f3c4849c065
Rishi Singh <rishi@harness.io>
2017-03-20 16:52:00 -0700

    Instances from Kubernetes - rolled back

--- rest/src/main/java/software/wings/service/impl/WorkflowExecutionServiceImpl.java
+++ rest/src/main/java/software/wings/service/impl/WorkflowExecutionServiceImpl.java
@@ -1102,8 +1102,7 @@ public class WorkflowExecutionServiceImpl implements WorkflowExecutionService {
                                              .filter(e -> e.getInstanceStatusSummaries() != null)
                                              .flatMap(l -> l.getInstanceStatusSummaries().stream())
                                              .collect(Collectors.toList()));
-        } else if ((StateType.ECS_SERVICE_DEPLOY.name().equals(next.getStateType())
-                       || StateType.KUBERNETES_REPLICATION_CONTROLLER_DEPLOY.name().equals(next.getStateType()))
+        } else if (StateType.ECS_SERVICE_DEPLOY.name().equals(next.getStateType())
             && next.getStateExecutionData() instanceof CommandStateExecutionData) {
           CommandStateExecutionData commandStateExecutionData =
               (CommandStateExecutionData) next.getStateExecutionData();
