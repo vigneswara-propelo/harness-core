ba336792e7276c431c42bd36925469a0da4ce7ab
Peeyush Aggarwal <peeyush@wings.software>
2017-04-07 14:50:14 -0700

    fix: container info cloning issue.

--- rest/src/main/java/software/wings/cloudprovider/ContainerInfo.java
+++ rest/src/main/java/software/wings/cloudprovider/ContainerInfo.java
@@ -1,11 +1,15 @@
 package software.wings.cloudprovider;
 
+import com.google.common.base.MoreObjects;
+
 /**
  * Created by brett on 4/6/17
  */
 public class ContainerInfo {
   private String hostName;
   private String containerId;
+
+  public ContainerInfo() {}
 
   public ContainerInfo(String containerId) {
     this.hostName = containerId;
@@ -23,5 +27,28 @@ public class ContainerInfo {
 
   public String getContainerId() {
     return containerId;
+  }
+
+  /**
+   * Setter for property 'hostName'.
+   *
+   * @param hostName Value to set for property 'hostName'.
+   */
+  public void setHostName(String hostName) {
+    this.hostName = hostName;
+  }
+
+  /**
+   * Setter for property 'containerId'.
+   *
+   * @param containerId Value to set for property 'containerId'.
+   */
+  public void setContainerId(String containerId) {
+    this.containerId = containerId;
+  }
+
+  @Override
+  public String toString() {
+    return MoreObjects.toStringHelper(this).add("hostName", hostName).add("containerId", containerId).toString();
   }
 }
