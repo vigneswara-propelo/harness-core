abdc3cea0eb93ac95d01776c90805768f122e600
Raghvendra Singh <raghu@harness.io>
2017-04-17 15:58:33 -0700

    Api to get list of applications from appdynamics

---
+++ rest/src/main/java/software/wings/service/impl/appdynamics/AppdynamicsServiceImpl.java
@@ -0,0 +1,30 @@
+package software.wings.service.impl.appdynamics;
+
+import static software.wings.beans.DelegateTask.Context.Builder.aContext;
+
+import software.wings.beans.AppDynamicsConfig;
+import software.wings.beans.Base;
+import software.wings.beans.DelegateTask.Context;
+import software.wings.beans.SettingAttribute;
+import software.wings.delegatetasks.DelegateProxyFactory;
+import software.wings.service.intfc.appdynamics.AppdynamicsDeletegateService;
+import software.wings.service.intfc.appdynamics.AppdynamicsService;
+
+import java.io.IOException;
+import java.util.List;
+import javax.inject.Inject;
+
+/**
+ * Created by rsingh on 4/17/17.
+ */
+public class AppdynamicsServiceImpl implements AppdynamicsService {
+  @Inject private DelegateProxyFactory delegateProxyFactory;
+
+  @Override
+  public List<AppdynamicsApplicationResponse> getApplications(final SettingAttribute settingAttribute)
+      throws IOException {
+    Context context = aContext().withAccountId(settingAttribute.getAccountId()).withAppId(Base.GLOBAL_APP_ID).build();
+    return delegateProxyFactory.get(AppdynamicsDeletegateService.class, context)
+        .getAllApplications((AppDynamicsConfig) settingAttribute.getValue());
+  }
+}
