f0496222b81d383f74e93a9a7fcd9cc9b544a040
Mike Lewis <mike@harness.io>
2017-04-26 13:59:08 -0700

    Added encryption reference in Account, extended encryption libraries, added Encryptable interface Anything that contains fields to be encrypted must implement Encryptable, which just means it has a getAccountId method from which we can get the encryption details.

---
+++ rest/src/main/java/software/wings/security/encryption/EncryptionUtils.java
@@ -0,0 +1,27 @@
+package software.wings.security.encryption;
+
+import java.security.SecureRandom;
+
+/**
+ * Utility classes used for encryption-related work.
+ * Created by mike@ on 4/25/17.
+ */
+public class EncryptionUtils {
+  private static final int DEFAULT_SALT_SIZE = 32;
+
+  /**
+   * Generate a salt to use for encryption.
+   * @param bytes the length of the salt in bytes
+   * @return a byte array containing the salt to use
+   */
+  public static byte[] generateSalt(int bytes) {
+    SecureRandom random = new SecureRandom();
+    byte salt[] = new byte[bytes];
+    random.nextBytes(salt);
+    return salt;
+  }
+
+  public static byte[] generateSalt() {
+    return generateSalt(DEFAULT_SALT_SIZE);
+  }
+}
