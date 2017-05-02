67659467aaa8e7d51e9664437b3824e3228f13df
Peeyush Aggarwal <peeyush@wings.software>
2017-05-02 14:57:21 -0700

    fix: logdna key replacement for delegates.

--- delegate/setDelegateVersion.sh
+++ delegate/setDelegateVersion.sh
@@ -1,13 +1,14 @@
 if [ $# -ne 3 ]
 then
   echo "This script is used to set version on delegates."
-  echo "Usage: $0 <version> <buildname> <environment>"
+  echo "Usage: $0 <version> <buildname> <environment> <logdnakey>"
   echo "buildname: jenkins build name"
   echo "environment: demo, ci"
+  echo "logdnakey: key for logging against right logdna instance."
   exit 1
 fi
 
-echo "System-Properties: version=1.0.${1}" >> app.mf
+echo "System-Properties: version=1.0.${1} logdnakey=${4}" >> app.mf
 echo "Application-Version: version=1.0.${1}" >> app.mf
 jar ufm delegate-0.0.1-SNAPSHOT-capsule.jar app.mf
 rm -rf app.mf
