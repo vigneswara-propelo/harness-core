63e90cad9a05d39cbac5d784a9acb71752f263eb
Mike Lewis <mike@harness.io>
2017-05-05 13:19:18 -0700

    Utility to encrypt unencrypted passwords.

---
+++ rest/src/test/java/software/wings/service/impl/PasswordEncryptionUtil.java
@@ -0,0 +1,55 @@
+package software.wings.service.impl;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import software.wings.WingsBaseTest;
+import software.wings.beans.SettingAttribute;
+import software.wings.dl.WingsPersistence;
+import software.wings.rules.Integration;
+import software.wings.security.encryption.Encryptable;
+import software.wings.security.encryption.SimpleEncryption;
+
+import java.lang.reflect.Field;
+import java.util.List;
+import javax.inject.Inject;
+
+/**
+ * Created by mike@ on 5/4/17.
+ */
+@Integration
+@Ignore
+public class PasswordEncryptionUtil extends WingsBaseTest {
+  @Inject WingsPersistence wingsPersistence;
+
+  @Test
+  @Ignore
+  public void encryptUnencryptedPasswordsInSettingAttributes() {
+    List<SettingAttribute> settings = wingsPersistence.list(SettingAttribute.class);
+    settings.forEach(setting -> {
+      if (setting.getValue() instanceof Encryptable) {
+        boolean passwordNeedsFixing = false;
+        if (((Encryptable) setting.getValue()).getAccountId() == null) {
+          passwordNeedsFixing = true;
+        } else
+          try {
+            Field passwordField = setting.getValue().getClass().getDeclaredField("password");
+            passwordField.setAccessible(true);
+            if (null != passwordField) {
+              try {
+                SimpleEncryption encryption = new SimpleEncryption(setting.getAccountId());
+                char[] outputChars = encryption.decryptChars((char[]) passwordField.get(setting.getValue()));
+              } catch (Exception e) {
+                passwordNeedsFixing = true;
+              }
+            }
+          } catch (NoSuchFieldException nsfe) {
+            System.out.println("No password for this Encryptable, can't encrypt it: " + setting.getValue().toString());
+          }
+        if (passwordNeedsFixing) {
+          ((Encryptable) setting.getValue()).setAccountId(setting.getAccountId());
+          wingsPersistence.save(setting);
+        }
+      }
+    });
+  }
+}
