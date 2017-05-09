81439b9c9b4af2397fce1eac6124d36b4ab4d998
Brett Zane <brett@harness.io>
2017-05-08 18:05:53 -0700

    feature: Container specification show image name in dialog.

---
+++ rest/src/main/java/software/wings/beans/artifact/ArtifactEnumDataProvider.java
@@ -0,0 +1,41 @@
+package software.wings.beans.artifact;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.inject.Singleton;
+
+import software.wings.beans.SearchFilter;
+import software.wings.dl.PageRequest;
+import software.wings.service.intfc.ArtifactStreamService;
+import software.wings.stencils.DataProvider;
+
+import java.util.Map;
+import java.util.Optional;
+import javax.inject.Inject;
+
+/**
+ * Created by anubhaw on 2/2/17.
+ */
+@Singleton
+public class ArtifactEnumDataProvider implements DataProvider {
+  @Inject private ArtifactStreamService artifactStreamService;
+
+  @Override
+  public Map<String, String> getData(String appId, String... params) {
+    String serviceId = params[0];
+    Optional<ArtifactStream> artifactStream =
+        artifactStreamService
+            .list(PageRequest.Builder.aPageRequest()
+                      .addFilter(SearchFilter.Builder.aSearchFilter()
+                                     .withField("serviceId", SearchFilter.Operator.EQ, serviceId)
+                                     .build())
+                      .build())
+            .getResponse()
+            .stream()
+            .findFirst();
+    String artifactName = "";
+    if (artifactStream.isPresent()) {
+      artifactName = artifactStream.get().getSourceName();
+    }
+    return ImmutableMap.of(artifactName, artifactName);
+  }
+}
