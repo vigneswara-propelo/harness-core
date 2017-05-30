6334ec66fe210d71e20a99d4012b181bc4cf8303
Brett Zane <brett@harness.io>
2017-05-30 11:54:01 -0700

    fix: test cleanup.

--- rest/src/test/java/software/wings/beans/command/InitSshCommandUnitTest.java
+++ rest/src/test/java/software/wings/beans/command/InitSshCommandUnitTest.java
@@ -1,6 +1,7 @@
 package software.wings.beans.command;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static software.wings.beans.command.InitSshCommandUnit.escapifyString;
 
 import org.junit.Test;
 import software.wings.WingsBaseTest;
@@ -11,18 +12,18 @@ import software.wings.WingsBaseTest;
 public class InitSshCommandUnitTest extends WingsBaseTest {
   @Test
   public void testEscapifyString() {
-    assertThat(InitSshCommandUnit.escapifyString("a\\a")).isEqualTo("a\\\\a");
-    assertThat(InitSshCommandUnit.escapifyString("b\\\\b")).isEqualTo("b\\\\\\\\b");
-    assertThat(InitSshCommandUnit.escapifyString("a$b")).isEqualTo("a\\$b");
-    assertThat(InitSshCommandUnit.escapifyString("a&b")).isEqualTo("a\\&b");
-    assertThat(InitSshCommandUnit.escapifyString("a\"b")).isEqualTo("a\\\"b");
-    assertThat(InitSshCommandUnit.escapifyString("a'b")).isEqualTo("a\\'b");
-    assertThat(InitSshCommandUnit.escapifyString("a`b")).isEqualTo("a\\`b");
-    assertThat(InitSshCommandUnit.escapifyString("a(b")).isEqualTo("a\\(b");
-    assertThat(InitSshCommandUnit.escapifyString("a)b")).isEqualTo("a\\)b");
-    assertThat(InitSshCommandUnit.escapifyString("a|b")).isEqualTo("a\\|b");
-    assertThat(InitSshCommandUnit.escapifyString("a<b")).isEqualTo("a\\<b");
-    assertThat(InitSshCommandUnit.escapifyString("a>b")).isEqualTo("a\\>b");
-    assertThat(InitSshCommandUnit.escapifyString("a;b")).isEqualTo("a\\;b");
+    assertThat(escapifyString("a\\b")).isEqualTo("a\\\\b");
+    assertThat(escapifyString("a\\\\b")).isEqualTo("a\\\\\\\\b");
+    assertThat(escapifyString("a$b")).isEqualTo("a\\$b");
+    assertThat(escapifyString("a&b")).isEqualTo("a\\&b");
+    assertThat(escapifyString("a\"b")).isEqualTo("a\\\"b");
+    assertThat(escapifyString("a'b")).isEqualTo("a\\'b");
+    assertThat(escapifyString("a`b")).isEqualTo("a\\`b");
+    assertThat(escapifyString("a(b")).isEqualTo("a\\(b");
+    assertThat(escapifyString("a)b")).isEqualTo("a\\)b");
+    assertThat(escapifyString("a|b")).isEqualTo("a\\|b");
+    assertThat(escapifyString("a<b")).isEqualTo("a\\<b");
+    assertThat(escapifyString("a>b")).isEqualTo("a\\>b");
+    assertThat(escapifyString("a;b")).isEqualTo("a\\;b");
   }
 }
