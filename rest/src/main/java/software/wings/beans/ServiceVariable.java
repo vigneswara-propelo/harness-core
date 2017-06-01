bfd9c559a37ff8d674deaaf362307bdc77359373
Brett Zane <brett@harness.io>
2017-06-01 14:54:32 -0700

    fix: index length for service variable

--- rest/src/main/java/software/wings/beans/ServiceVariable.java
+++ rest/src/main/java/software/wings/beans/ServiceVariable.java
@@ -32,7 +32,7 @@ import javax.validation.constraints.NotNull;
       , @Field("templateId"), @Field("overrideType"), @Field("instances"), @Field("expression"), @Field("type"),
           @Field("name")
     },
-    options = @IndexOptions(unique = true)))
+    options = @IndexOptions(unique = true, name = "serviceVariableUniqueIdx")))
 public class ServiceVariable extends Base implements Encryptable {
   /**
    * The constant DEFAULT_TEMPLATE_ID.
