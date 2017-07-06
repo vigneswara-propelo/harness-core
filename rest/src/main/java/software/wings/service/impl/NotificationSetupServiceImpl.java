3925b6ec50c74ed5995658c07dfa00a19d93eb70
Brett Zane <brett@harness.io>
2017-07-06 16:49:34 -0700

    fix: Check for references before deleting Notification Group.

--- rest/src/main/java/software/wings/service/impl/NotificationSetupServiceImpl.java
+++ rest/src/main/java/software/wings/service/impl/NotificationSetupServiceImpl.java
@@ -6,23 +6,27 @@ import static software.wings.dl.PageRequest.Builder.aPageRequest;
 import com.google.inject.Singleton;
 
 import software.wings.beans.Base;
-import software.wings.beans.ErrorCode;
+import software.wings.beans.CanaryOrchestrationWorkflow;
 import software.wings.beans.NotificationChannelType;
 import software.wings.beans.NotificationGroup;
 import software.wings.beans.Role;
 import software.wings.beans.SearchFilter.Operator;
 import software.wings.beans.SettingAttribute;
+import software.wings.beans.Workflow;
+import software.wings.beans.WorkflowType;
 import software.wings.dl.PageRequest;
 import software.wings.dl.PageResponse;
 import software.wings.dl.WingsPersistence;
 import software.wings.exception.WingsException;
 import software.wings.service.intfc.NotificationSetupService;
 import software.wings.service.intfc.SettingsService;
+import software.wings.service.intfc.WorkflowService;
 import software.wings.utils.Validator;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import javax.inject.Inject;
 import javax.validation.executable.ValidateOnExecution;
 
@@ -35,6 +39,8 @@ public class NotificationSetupServiceImpl implements NotificationSetupService {
   @Inject private WingsPersistence wingsPersistence;
 
   @Inject private SettingsService settingsService;
+
+  @Inject private WorkflowService workflowService;
 
   /**
    * Gets supported channel type details.
@@ -105,6 +111,32 @@ public class NotificationSetupServiceImpl implements NotificationSetupService {
         wingsPersistence.get(NotificationGroup.class, Base.GLOBAL_APP_ID, notificationGroupId);
     if (!notificationGroup.isEditable()) {
       throw new WingsException(INVALID_REQUEST, "message", "Default Notification group can not be deleted");
+    }
+
+    List<Workflow> workflows =
+        workflowService
+            .listWorkflows(aPageRequest()
+                               .withLimit(PageRequest.UNLIMITED)
+                               .addFilter("workflowType", Operator.EQ, WorkflowType.ORCHESTRATION)
+                               .build())
+            .getResponse();
+    List<Workflow> inUse = workflows.stream()
+                               .filter(workflow
+                                   -> workflow.getOrchestrationWorkflow() instanceof CanaryOrchestrationWorkflow
+                                       && ((CanaryOrchestrationWorkflow) workflow.getOrchestrationWorkflow())
+                                              .getNotificationRules()
+                                              .stream()
+                                              .anyMatch(notificationRule
+                                                  -> notificationRule.getNotificationGroups().stream().anyMatch(
+                                                      ng -> ng.getUuid().equals(notificationGroupId))))
+                               .collect(Collectors.toList());
+    if (!inUse.isEmpty()) {
+      String message =
+          "'" + notificationGroup.getName() + "' is still in use by workflow '" + inUse.get(0).getName() + "'";
+      if (inUse.size() > 1) {
+        message += " and " + (inUse.size() - 1) + " other" + (inUse.size() > 2 ? "s" : "");
+      }
+      throw new WingsException(INVALID_REQUEST, "message", message);
     }
     return wingsPersistence.delete(NotificationGroup.class, Base.GLOBAL_APP_ID, notificationGroupId);
   }
