70b45cd1c16f8e791b70d02446728b32cf1cf8c6
Brett Zane <brett@harness.io>
2017-07-07 21:58:23 -0700

    fix: make "timed out. remove it from cache" warning instead of error.

--- rest/src/main/java/software/wings/service/impl/DelegateServiceImpl.java
+++ rest/src/main/java/software/wings/service/impl/DelegateServiceImpl.java
@@ -355,7 +355,7 @@ public class DelegateServiceImpl implements DelegateService {
         task.getTaskType(), task.isAsync());
     T responseData = topic.poll(task.getTimeout(), TimeUnit.MILLISECONDS);
     if (responseData == null) {
-      logger.error("Task [{}] timed out. remove it from cache", task.toString());
+      logger.warn("Task [{}] timed out. remove it from cache", task.toString());
       Caching.getCache("delegateSyncCache", String.class, DelegateTask.class).remove(taskId);
       throw new WingsException(ErrorCode.REQUEST_TIMEOUT, "name", Constants.DELEGATE_DIR);
     }
