6a3ef202080ffcb7adf6bc12d14d15ed4a5f5d1b
Raghvendra Singh <raghu@harness.io>
2017-07-12 15:43:21 -0700

    spunlk integration, integrate progress bar + summary message + bug fixes

--- python/splunk_intelligence/src/core/ThreeSigmaClassifier.py
+++ python/splunk_intelligence/src/core/ThreeSigmaClassifier.py
@@ -9,14 +9,22 @@ class ThreeSigmaClassifier(object):
         np_values = np.array(vals)
         self.klassifier[label] = [np.mean(np_values, axis=0), np.std(np_values, axis=0)]
 
-    def predict(self, label, values):
+    def predict(self, label, values, flag=0):
         mean = self.klassifier[label][0]
         std = self.klassifier[label][1]
         vals = map(int, values[:, 1])
         score = 0.0
         predictions = [1] * len(vals)
         for i, value in enumerate(vals):
-            if abs(value - mean) > 3 * std:
+            anomaly = 0
+            if flag == 0:
+                anomaly = (value - mean) / std > 3
+            elif flag == 1:
+                anomaly = (value - mean) / std < 3
+            else:
+                anomaly = abs(value - mean) / std > 3
+
+            if anomaly == True:
                 predictions[i] = -1
                 score = score + 1
         return predictions, ((len(values) - score) / len(values))
