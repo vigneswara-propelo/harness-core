1990b4c618fce82efe47e2beb23b08adcf4a094b
Brett Zane <brett@harness.io>
2017-07-19 14:33:20 -0700

    fix: remove throws from signature

--- rest/src/main/java/software/wings/delegatetasks/DelegateFileManager.java
+++ rest/src/main/java/software/wings/delegatetasks/DelegateFileManager.java
@@ -10,7 +10,7 @@ import java.io.InputStream;
  * Created by rishi on 12/19/16.
  */
 public interface DelegateFileManager {
-  DelegateFile upload(DelegateFile delegateFile, InputStream contentSource) throws IOException;
+  DelegateFile upload(DelegateFile delegateFile, InputStream contentSource);
   String getFileIdByVersion(FileBucket fileBucket, String entityId, int version, String accountId) throws IOException;
   InputStream downloadByFileId(FileService.FileBucket bucket, String fileId, String accountId) throws IOException;
   DelegateFile getMetaInfo(FileBucket fileBucket, String fileId, String accountId) throws IOException;
