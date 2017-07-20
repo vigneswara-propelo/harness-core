58e9f848fc758c98d52b1e5c64909a60fd058883
Raghvendra Singh <raghu@harness.io>
2017-07-20 15:44:40 -0700

    start manager and delegate using regular starting methods for integration tests

--- integration-tests-setup.sh
+++ integration-tests-setup.sh
@@ -4,11 +4,11 @@ mvn clean install -DskipTests=true
 mvn test-compile
 
 echo 'starting server'
-export MAVEN_OPTS="-Xbootclasspath/p:$HOME/.m2/repository/org/mortbay/jetty/alpn/alpn-boot/8.1.11.v20170118/alpn-boot-8.1.11.v20170118.jar"
-cd rest
-nohup mvn exec:java > portal.log 2>&1 &
-echo $! > manager.pid
-cd ../
+export HOSTNAME
+java -Xms1024m -Xmx4096m -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDetails -XX:+PrintGCDateStamps \
+     -Xloggc:portal-gc-logs.gc -XX:+UseParallelGC -XX:MaxGCPauseMillis=500 -Xbootclasspath/p:$HOME/.m2/repository/org/mortbay/jetty/alpn/alpn-boot/8.1.11.v20170118/alpn-boot-8.1.11.v20170118.jar \
+     -Dfile.encoding=UTF-8 -jar rest/target/rest-0.0.1-SNAPSHOT-capsule.jar rest/config.yml > portal.log 2>&1 &
+
 echo 'sleep for server to start'
 #wait for server to start
 output=$(curl -sS https://localhost:9090/api/version)
@@ -30,55 +30,44 @@ fi
 
 
 #run data gen to load test data
-cd rest
-mvn test -Dtest=software.wings.integration.DataGenUtil
+mvn test -pl rest -Dtest=software.wings.integration.DataGenUtil
 datagen_status=$?
 if [[ $datagen_status -ne 0 ]] ; then
   echo 'Datagen failed';
   exit $datagen_status
 fi
 
-mvn test -Dtest=software.wings.integration.JenkinsSettingOverWriteTest
+mvn test -pl rest -Dtest=software.wings.integration.JenkinsSettingOverWriteTest
 jenkins_overwrite_status=$?
 if [[ jenkins_overwrite_status -ne 0 ]] ; then
   echo 'jenkins overwrite failed';
   exit $jenkins_overwrite_status
 fi
 
-cd ../
-
 #Delegate integration test. Don't run with UI integration tests
 if [ "$TEST_SUITE" != "UI_INTEGRATION" ] ;
 then
-  cd rest
-  mvn test -Dtest=software.wings.integration.DelegateIntegrationTest
+  mvn test -pl rest -Dtest=software.wings.integration.DelegateIntegrationTest
   delegateIntegrationTestResult=$?
   if [[ $delegateIntegrationTestResult -ne 0 ]] ;
   then
     echo 'Delegate integration test failed';
     exit $delegateIntegrationTestResult
   fi
-  cd ../
 fi
 
-
 #run delegate
-export MAVEN_OPTS="-Xbootclasspath/p:$HOME/.m2/repository/org/mortbay/jetty/alpn/alpn-boot/8.1.11.v20170118/alpn-boot-8.1.11.v20170118.jar -Dversion=999.0.0"
-cd delegate
 sed -i -e 's/^doUpgrade.*/doUpgrade: false/' config-delegate.yml
-nohup mvn exec:java > delegate.out 2>&1 &
-echo $! > delegate.pid
-cd ../
+java -Xmx4096m -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:delegate-gc-logs.gc -XX:+UseParallelGC \
+     -XX:MaxGCPauseMillis=500 -jar delegate/target/delegate-0.0.1-SNAPSHOT-capsule.jar delegate/config-delegate.yml 2>&1 &
 
 #wait for delegate to start
 echo 'wait for delegate to start'
 
 #wait for delegate to start
-cd rest
-mvn test -Dtest=software.wings.integration.DelegateRegistrationIntegrationTest#shouldWaitForADelegateToRegister
+mvn test -pl rest -Dtest=software.wings.integration.DelegateRegistrationIntegrationTest#shouldWaitForADelegateToRegister
 foundRegisteredDelegate=$?
 if [[ $foundRegisteredDelegate -ne 0 ]] ; then
   echo 'Delegate registration failed';
   exit $foundRegisteredDelegate
 fi
-cd ../
