79e6482ed7c3703c73e7958e84eec3552c808329
Brett Zane <brett@harness.io>
2017-07-21 14:23:40 -0700

    Revert "fix: fix tests"
    This reverts commit be1cfe95b16b448cb8cbc3141049c654be1e48bb.

--- rest/src/main/java/software/wings/service/impl/DelegateServiceImpl.java
+++ rest/src/main/java/software/wings/service/impl/DelegateServiceImpl.java
@@ -419,6 +419,7 @@ public class DelegateServiceImpl implements DelegateService {
         UpdateOperations<DelegateTask> updateOperations =
             wingsPersistence.createUpdateOperations(DelegateTask.class).set("delegateId", delegateId);
         delegateTask = wingsPersistence.getDatastore().findAndModify(query, updateOperations);
+        Caching.getCache("delegateSyncCache", String.class, DelegateTask.class).put(taskId, delegateTask);
       }
     } else {
       // Sync
