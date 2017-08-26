fae5d460e0695acb2c1cf1b232499157ae123e70
Rishi Singh <rishi@harness.io>
2017-08-25 18:23:56 -0700

    Fix unit test

--- rest/src/test/java/software/wings/sm/states/CommandStateTest.java
+++ rest/src/test/java/software/wings/sm/states/CommandStateTest.java
@@ -275,7 +275,7 @@ public class CommandStateTest extends WingsBaseTest {
     verify(context, times(1)).getWorkflowType();
     verify(context, times(1)).getStateExecutionInstanceId();
     verify(context, times(1)).getStateExecutionInstanceName();
-    verify(context, times(2)).getServiceVariables();
+    verify(context, times(1)).getServiceVariables();
     verify(context).getStateExecutionData();
 
     verify(context, times(4)).renderExpression(anyString());
@@ -380,7 +380,7 @@ public class CommandStateTest extends WingsBaseTest {
     verify(context, times(1)).getWorkflowExecutionName();
     verify(context, times(1)).getStateExecutionInstanceId();
     verify(context, times(1)).getStateExecutionInstanceName();
-    verify(context, times(2)).getServiceVariables();
+    verify(context, times(1)).getServiceVariables();
     verify(context).getStateExecutionData();
 
     verify(activityService).updateStatus(ACTIVITY_ID, APP_ID, ExecutionStatus.SUCCESS);
@@ -426,7 +426,7 @@ public class CommandStateTest extends WingsBaseTest {
     verify(context, times(1)).getWorkflowType();
     verify(context, times(1)).getStateExecutionInstanceId();
     verify(context, times(1)).getStateExecutionInstanceName();
-    verify(context, times(2)).getServiceVariables();
+    verify(context, times(1)).getServiceVariables();
 
     verify(context, times(4)).renderExpression(anyString());
 
