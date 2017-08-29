a78309458cfcc562eed9671a44d8a70e0f2f9e13
Bob Sollish <bob@harness.io>
2017-08-29 14:38:17 -0700

    Remove (failing) unit tests against code that was removed (refactored).

--- rest/src/test/java/software/wings/resources/AppResourceTest.java
+++ rest/src/test/java/software/wings/resources/AppResourceTest.java
@@ -8,23 +8,17 @@ import static org.mockito.Mockito.when;
 import static software.wings.beans.Application.Builder.anApplication;
 import static software.wings.beans.Setup.SetupStatus.COMPLETE;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import org.junit.After;
 import org.junit.ClassRule;
 import org.junit.Test;
-import org.mockito.Mockito;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import software.wings.beans.Application;
 import software.wings.beans.RestResponse;
 import software.wings.service.intfc.AppService;
 import software.wings.utils.ResourceTestRule;
-import software.wings.yaml.YamlPayload;
 
-import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.GenericType;
-import javax.ws.rs.core.MediaType;
 
 /**
  * The Class AppResourceTest.
@@ -78,50 +72,4 @@ public class AppResourceTest {
     assertThat(actual.getResource()).isEqualTo(testApp);
     verify(appService).get(TEST_UUID, COMPLETE, true, 30);
   }
-
-  @Test
-  public void testGetYaml() {
-    when(appService.get(TEST_UUID2, COMPLETE, true, 30)).thenReturn(testApp2);
-    RestResponse<YamlPayload> actual = resources.client()
-                                           .target("/apps/yaml/" + TEST_UUID2)
-                                           .request()
-                                           .get(new GenericType<RestResponse<YamlPayload>>() {});
-
-    YamlPayload yp = actual.getResource();
-    String yaml = yp.getYaml();
-
-    ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-
-    try {
-      Application app = mapper.readValue(yaml, Application.class);
-
-      assertThat(app.getName()).isEqualTo(TEST_NAME);
-      assertThat(app.getDescription()).isEqualTo(TEST_DESCRIPTION);
-    } catch (Exception e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-  }
-
-  // This (POST) Unit Test doesn't really do anyting (useful) – moved to: YamlPayloadIntegrationTest
-  @Test
-  public void testSaveFromYaml() {
-    YamlPayload yp = new YamlPayload(TEST_YAML);
-
-    // when we save ANY instance of Application, we want to return testApp2
-    when(appService.save(Mockito.any(Application.class))).thenReturn(testApp2);
-    RestResponse<Application> actual =
-        resources.client()
-            .target("/apps/yaml?accountId=" + TEST_ACCOUNT_ID)
-            .request()
-            .post(Entity.entity(yp, MediaType.APPLICATION_JSON), new GenericType<RestResponse<Application>>() {});
-    Application app = actual.getResource();
-
-    assertThat(app.getName()).isEqualTo(TEST_NAME);
-    assertThat(app.getDescription()).isEqualTo(TEST_DESCRIPTION);
-  }
-
-  // A (PUT) Unit Test wonn't really do anyting (useful) – moved to: YamlPayloadIntegrationTest
-  @Test
-  public void testUpdateFromYaml() {}
 }
