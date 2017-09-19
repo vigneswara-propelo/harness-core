7f8d66a16602b037d0a2a50e02e323885cd9aed2
Rama Tummala <rama@harness.io>
2017-09-18 18:30:09 -0700

    Fix for NPE

--- rest/src/main/java/software/wings/service/impl/instance/sync/EcsContainerSyncImpl.java
+++ rest/src/main/java/software/wings/service/impl/instance/sync/EcsContainerSyncImpl.java
@@ -57,15 +57,16 @@ public class EcsContainerSyncImpl implements ContainerSync {
               awsHelperService.describeTasks(filter.getRegion(), awsConfig, describeTasksRequest);
           tasks = describeTasksResult.getTasks();
           for (Task task : tasks) {
-            EcsContainerInfo ecsContainerInfo = EcsContainerInfo.Builder.anEcsContainerInfo()
-                                                    .withClusterName(filter.getClusterName())
-                                                    .withTaskDefinitionArn(task.getTaskDefinitionArn())
-                                                    .withTaskArn(task.getTaskArn())
-                                                    .withVersion(task.getVersion())
-                                                    .withStartedAt(task.getStartedAt().getTime())
-                                                    .withStartedBy(task.getStartedBy())
-                                                    .withServiceName(serviceName)
-                                                    .build();
+            EcsContainerInfo ecsContainerInfo =
+                EcsContainerInfo.Builder.anEcsContainerInfo()
+                    .withClusterName(filter.getClusterName())
+                    .withTaskDefinitionArn(task.getTaskDefinitionArn())
+                    .withTaskArn(task.getTaskArn())
+                    .withVersion(task.getVersion())
+                    .withStartedAt(task.getStartedAt() == null ? null : task.getStartedAt().getTime())
+                    .withStartedBy(task.getStartedBy())
+                    .withServiceName(serviceName)
+                    .build();
             result.add(ecsContainerInfo);
           }
         }
