ba8e8d66c8c54e101f3252e24ef1b6c86a09cf48
Brett Zane <brett@harness.io>
2017-10-07 12:54:44 -0700

    Don't fail for removed feature flags, just mark them obsolete.

--- rest/src/main/java/software/wings/service/intfc/FeatureFlagService.java
+++ rest/src/main/java/software/wings/service/intfc/FeatureFlagService.java
@@ -5,5 +5,5 @@ import software.wings.beans.FeatureFlag.FeatureName;
 public interface FeatureFlagService {
   boolean isEnabled(FeatureName name, String accountId);
 
-  void initializeMissingFlags();
+  void initializeFeatureFlags();
 }
