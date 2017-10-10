2d441a9ccb9341d8b3206fa1173d035ae65617ab
Raghvendra Singh <raghu@harness.io>
2017-10-09 21:28:17 -0700

    more test for BuildSourceService

--- rest/src/test/java/software/wings/service/impl/BuildSourceServiceTest.java
+++ rest/src/test/java/software/wings/service/impl/BuildSourceServiceTest.java
@@ -1,5 +1,6 @@
 package software.wings.service.impl;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 import static org.mockito.internal.util.reflection.Whitebox.setInternalState;
@@ -13,17 +14,23 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import software.wings.WingsBaseTest;
 import software.wings.beans.DelegateTask.SyncTaskContext;
+import software.wings.beans.Service;
 import software.wings.beans.SettingAttribute;
 import software.wings.beans.SettingAttribute.Category;
 import software.wings.beans.artifact.ArtifactStreamType;
+import software.wings.beans.artifact.JenkinsArtifactStream;
 import software.wings.delegatetasks.DelegateProxyFactory;
 import software.wings.dl.WingsPersistence;
+import software.wings.helpers.ext.jenkins.BuildDetails;
 import software.wings.helpers.ext.jenkins.JobDetails;
 import software.wings.integration.BaseIntegrationTest;
 import software.wings.rules.RealMongo;
 import software.wings.service.intfc.BuildSourceService;
 import software.wings.service.intfc.JenkinsBuildService;
+import software.wings.utils.ArtifactType;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
@@ -85,5 +92,41 @@ public class BuildSourceServiceTest extends WingsBaseTest {
         appId, "todolist_war", jenkinsSettingAttribute.getUuid(), null, ArtifactStreamType.JENKINS.name());
     assertTrue(artifactPaths.size() > 0);
     assertTrue(artifactPaths.contains("target/todolist.war"));
+  }
+
+  @Test
+  @RealMongo
+  public void getBuilds() {
+    Service service =
+        Service.Builder.aService().withAppId(appId).withArtifactType(ArtifactType.WAR).withName("Some service").build();
+    wingsPersistence.save(service);
+    JenkinsArtifactStream artifactStream = new JenkinsArtifactStream();
+    artifactStream.setJobname("todolist_war");
+    artifactStream.setArtifactPaths(Collections.singletonList("target/todolist.war"));
+    artifactStream.setServiceId(service.getUuid());
+    artifactStream.setAppId(appId);
+    wingsPersistence.save(artifactStream);
+
+    List<BuildDetails> builds =
+        buildSourceService.getBuilds(appId, artifactStream.getUuid(), jenkinsSettingAttribute.getUuid());
+    assertTrue(builds.size() > 0);
+  }
+
+  @Test
+  @RealMongo
+  public void getLastSuccessfulBuild() {
+    Service service =
+        Service.Builder.aService().withAppId(appId).withArtifactType(ArtifactType.WAR).withName("Some service").build();
+    wingsPersistence.save(service);
+    JenkinsArtifactStream artifactStream = new JenkinsArtifactStream();
+    artifactStream.setJobname("todolist_war");
+    artifactStream.setArtifactPaths(Collections.singletonList("target/todolist.war"));
+    artifactStream.setServiceId(service.getUuid());
+    artifactStream.setAppId(appId);
+    wingsPersistence.save(artifactStream);
+
+    BuildDetails build =
+        buildSourceService.getLastSuccessfulBuild(appId, artifactStream.getUuid(), jenkinsSettingAttribute.getUuid());
+    assertNotNull(build);
   }
 }
