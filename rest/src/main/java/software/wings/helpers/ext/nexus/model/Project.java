8a60b29339f9e85702b70201dee5d239c26da9a8
Raghvendra Singh <raghu@harness.io>
2017-10-12 09:41:08 -0700

    delete unused code

--- rest/src/main/java/software/wings/helpers/ext/nexus/model/Project.java
+++ rest/src/main/java/software/wings/helpers/ext/nexus/model/Project.java
@@ -1,7 +1,5 @@
 package software.wings.helpers.ext.nexus.model;
 
-import com.google.common.base.MoreObjects;
-
 import java.io.Serializable;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
@@ -13,6 +11,7 @@ import javax.xml.bind.annotation.XmlType;
 
 @XmlType(name = "project")
 @XmlAccessorType(XmlAccessType.FIELD)
+@lombok.Data
 public class Project implements Serializable {
   private String modelVersion;
   private String groupId;
@@ -22,72 +21,4 @@ public class Project implements Serializable {
   private String description;
 
   private Parent parent;
-
-  public String getModelVersion() {
-    return modelVersion;
-  }
-
-  public void setModelVersion(String modelVersion) {
-    this.modelVersion = modelVersion;
-  }
-  public String getGroupId() {
-    return groupId;
-  }
-
-  public void setGroupId(String groupId) {
-    this.groupId = groupId;
-  }
-
-  public String getArtifactId() {
-    return artifactId;
-  }
-
-  public void setArtifactId(String artifactId) {
-    this.artifactId = artifactId;
-  }
-
-  public String getVersion() {
-    return version;
-  }
-
-  public void setVersion(String version) {
-    this.version = version;
-  }
-
-  public String getPackaging() {
-    return packaging;
-  }
-
-  public void setPackaging(String packaging) {
-    this.packaging = packaging;
-  }
-
-  public String getDescription() {
-    return description;
-  }
-
-  public void setDescription(String description) {
-    this.description = description;
-  }
-
-  public Parent getParent() {
-    return parent;
-  }
-
-  public void setParent(Parent parent) {
-    this.parent = parent;
-  }
-
-  @Override
-  public String toString() {
-    return MoreObjects.toStringHelper(this)
-        .add("modelVersion", modelVersion)
-        .add("groupId", groupId)
-        .add("artifactId", artifactId)
-        .add("version", version)
-        .add("packaging", packaging)
-        .add("description", description)
-        .add("parent", parent)
-        .toString();
-  }
 }
