609ad3c5900067b0e56a6fd92bd8e659b78836ab
Brett Zane <brett@harness.io>
2017-10-23 10:19:24 -0700

    Remove obsolete flag for manual intervention alerts.

--- rest/src/main/java/software/wings/beans/FeatureFlag.java
+++ rest/src/main/java/software/wings/beans/FeatureFlag.java
@@ -28,12 +28,5 @@ public class FeatureFlag extends Base {
 
   // Add your feature name here. When the feature is fully launched and no longer needs to be flagged, delete the
   // feature name.
-  public enum FeatureName {
-    GIT_SYNC,
-    ECS_CREATE_CLUSTER,
-    KUBERNETES_CREATE_CLUSTER,
-    TIME_SERIES_ML,
-    KMS,
-    MANUAL_INTERVENTION_ALERTS
-  }
+  public enum FeatureName { GIT_SYNC, ECS_CREATE_CLUSTER, KUBERNETES_CREATE_CLUSTER, TIME_SERIES_ML, KMS }
 }
