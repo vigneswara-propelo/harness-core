16e3794b2e4ceb3b0c01078a314c25ba858c1dac
Brett Zane <brett@harness.io>
2017-10-31 13:44:39 -0700

    code cleanup

--- rest/src/main/java/software/wings/resources/DelegateScopeResource.java
+++ rest/src/main/java/software/wings/resources/DelegateScopeResource.java
@@ -1,52 +1,30 @@
 package software.wings.resources;
 
-import com.google.common.collect.ImmutableMap;
-
 import com.codahale.metrics.annotation.ExceptionMetered;
 import com.codahale.metrics.annotation.Timed;
-import freemarker.template.TemplateException;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import org.hibernate.validator.constraints.NotEmpty;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import software.wings.beans.Base;
-import software.wings.beans.Delegate;
 import software.wings.beans.DelegateScope;
-import software.wings.beans.DelegateScripts;
-import software.wings.beans.DelegateTask;
-import software.wings.beans.DelegateTaskResponse;
 import software.wings.beans.RestResponse;
-import software.wings.common.Constants;
 import software.wings.dl.PageRequest;
 import software.wings.dl.PageResponse;
 import software.wings.security.PermissionAttribute.ResourceType;
 import software.wings.security.annotations.AuthRule;
 import software.wings.security.annotations.DelegateAuth;
-import software.wings.security.annotations.PublicApi;
 import software.wings.service.intfc.DelegateScopeService;
-import software.wings.service.intfc.DelegateService;
-import software.wings.service.intfc.DownloadTokenService;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Map;
 import javax.inject.Inject;
-import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.BeanParam;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.Response;
 
 /**
  * Created by brett on 8/4/17
@@ -57,7 +35,6 @@ import javax.ws.rs.core.Response;
 @AuthRule(ResourceType.DELEGATE_SCOPE)
 public class DelegateScopeResource {
   private DelegateScopeService delegateScopeService;
-  private final Logger logger = LoggerFactory.getLogger(getClass());
 
   @Inject
   public DelegateScopeResource(DelegateScopeService delegateService) {
