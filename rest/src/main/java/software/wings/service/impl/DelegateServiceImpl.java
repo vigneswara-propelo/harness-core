f9689c36fa70a9ed788e144e7c802b4dadbe5aa9
Brett Zane <brett@harness.io>
2017-11-04 08:08:15 -0700

    Revert "Another NPE"
    This reverts commit f7b0b75b9f1dc45b8bf5accd4a3ec3db64765fb0.

--- rest/src/main/java/software/wings/service/impl/DelegateServiceImpl.java
+++ rest/src/main/java/software/wings/service/impl/DelegateServiceImpl.java
@@ -585,10 +585,7 @@ public class DelegateServiceImpl implements DelegateService {
   public DelegateTask shouldProceedAnyway(String accountId, String delegateId, String taskId) {
     // Tell delegate whether to proceed anyway because all eligible delegates failed.
     if (!cacheHelper.getCache("delegateValidationCache", String.class, Set.class).containsKey(taskId)) {
-      DelegateTask delegateTask = getUnassignedDelegateTask(accountId, taskId);
-      if (delegateTask != null) {
-        return assignTask(delegateId, taskId, delegateTask);
-      }
+      return assignTask(delegateId, taskId, getUnassignedDelegateTask(accountId, taskId));
     }
     return null;
   }
