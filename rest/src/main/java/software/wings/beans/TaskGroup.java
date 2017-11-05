1dc5f46445f128130238554e5ea80236d3a44842
Brett Zane <brett@harness.io>
2017-11-05 13:59:15 -0800

    Added some missing task types to the catalog.

--- rest/src/main/java/software/wings/beans/TaskGroup.java
+++ rest/src/main/java/software/wings/beans/TaskGroup.java
@@ -5,6 +5,7 @@ package software.wings.beans;
  */
 
 public enum TaskGroup {
+  // *** NOTE: If you add an item to this list then also add an entry in catalogs.yml in the TASK_TYPES section. ***
   HTTP,
   SPLUNK,
   APPDYNAMICS,
