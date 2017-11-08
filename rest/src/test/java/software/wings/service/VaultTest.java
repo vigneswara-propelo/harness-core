3787889c9f56943766c6d409ea084426b726a959
Raghvendra Singh <raghu@harness.io>
2017-11-07 18:47:03 -0800

    fix vault tests

--- rest/src/test/java/software/wings/service/VaultTest.java
+++ rest/src/test/java/software/wings/service/VaultTest.java
@@ -16,7 +16,6 @@ import static software.wings.service.impl.security.KmsServiceImpl.SECRET_MASK;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -93,7 +92,6 @@ import javax.inject.Inject;
  * Created by rsingh on 11/3/17.
  */
 @RunWith(Parameterized.class)
-@Ignore
 public class VaultTest extends WingsBaseTest {
   private static String VAULT_TOKEN = System.getProperty("vault.token");
 
