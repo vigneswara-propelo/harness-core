879b36125f8388a6aac7fa90505e066e79837c02
Brett Zane <brett@harness.io>
2017-11-16 01:11:34 -0800

    shutdown upgrade check thread on watcher shutdown.

--- watcher/src/main/java/software/wings/watcher/app/WatcherApplication.java
+++ watcher/src/main/java/software/wings/watcher/app/WatcherApplication.java
@@ -93,6 +93,7 @@ public class WatcherApplication {
     // This should run in case of upgrade flow otherwise never called
     injector.getInstance(Key.get(ScheduledExecutorService.class, Names.named("inputExecutor"))).shutdownNow();
     injector.getInstance(Key.get(ScheduledExecutorService.class, Names.named("watchExecutor"))).shutdownNow();
+    injector.getInstance(Key.get(ScheduledExecutorService.class, Names.named("upgradeExecutor"))).shutdownNow();
     injector.getInstance(ExecutorService.class).shutdown();
     injector.getInstance(ExecutorService.class).awaitTermination(Integer.MAX_VALUE, TimeUnit.MILLISECONDS);
     logger.info("Flushing logs");
