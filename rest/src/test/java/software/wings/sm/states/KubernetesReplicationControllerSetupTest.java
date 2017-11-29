80e147df88bc2e103c3e1f2a009fad8296819cbe
Brett Zane <brett@harness.io>
2017-11-28 19:09:56 -0800

    Test for Kubernetes setup step.

---
+++ rest/src/test/java/software/wings/sm/states/KubernetesReplicationControllerSetupTest.java
@@ -0,0 +1,212 @@
+package software.wings.sm.states;
+
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
+import static org.joor.Reflect.on;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.mockito.internal.util.reflection.Whitebox.setInternalState;
+import static software.wings.api.ContainerServiceElement.ContainerServiceElementBuilder.aContainerServiceElement;
+import static software.wings.api.PhaseElement.PhaseElementBuilder.aPhaseElement;
+import static software.wings.api.ServiceElement.Builder.aServiceElement;
+import static software.wings.beans.Application.Builder.anApplication;
+import static software.wings.beans.Environment.Builder.anEnvironment;
+import static software.wings.beans.GcpKubernetesInfrastructureMapping.Builder.aGcpKubernetesInfrastructureMapping;
+import static software.wings.beans.ResizeStrategy.RESIZE_NEW_FIRST;
+import static software.wings.beans.Service.Builder.aService;
+import static software.wings.beans.ServiceTemplate.Builder.aServiceTemplate;
+import static software.wings.beans.SettingAttribute.Builder.aSettingAttribute;
+import static software.wings.beans.WorkflowExecution.WorkflowExecutionBuilder.aWorkflowExecution;
+import static software.wings.beans.artifact.Artifact.Builder.anArtifact;
+import static software.wings.beans.artifact.DockerArtifactStream.Builder.aDockerArtifactStream;
+import static software.wings.beans.command.Command.Builder.aCommand;
+import static software.wings.beans.command.ServiceCommand.Builder.aServiceCommand;
+import static software.wings.common.Constants.BUILD_NO;
+import static software.wings.common.UUIDGenerator.getUuid;
+import static software.wings.sm.StateExecutionInstance.Builder.aStateExecutionInstance;
+import static software.wings.sm.WorkflowStandardParams.Builder.aWorkflowStandardParams;
+import static software.wings.utils.WingsTestConstants.ACCOUNT_ID;
+import static software.wings.utils.WingsTestConstants.ACTIVITY_ID;
+import static software.wings.utils.WingsTestConstants.APP_ID;
+import static software.wings.utils.WingsTestConstants.APP_NAME;
+import static software.wings.utils.WingsTestConstants.ARTIFACT_ID;
+import static software.wings.utils.WingsTestConstants.CLUSTER_NAME;
+import static software.wings.utils.WingsTestConstants.COMPUTE_PROVIDER_ID;
+import static software.wings.utils.WingsTestConstants.ENV_ID;
+import static software.wings.utils.WingsTestConstants.ENV_NAME;
+import static software.wings.utils.WingsTestConstants.INFRA_MAPPING_ID;
+import static software.wings.utils.WingsTestConstants.SERVICE_ID;
+import static software.wings.utils.WingsTestConstants.SERVICE_NAME;
+import static software.wings.utils.WingsTestConstants.STATE_NAME;
+import static software.wings.utils.WingsTestConstants.TEMPLATE_ID;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mongodb.morphia.Key;
+import software.wings.WingsBaseTest;
+import software.wings.api.DeploymentType;
+import software.wings.api.PhaseElement;
+import software.wings.api.PhaseStepExecutionData;
+import software.wings.api.ServiceElement;
+import software.wings.beans.Activity;
+import software.wings.beans.Application;
+import software.wings.beans.DockerConfig;
+import software.wings.beans.Environment;
+import software.wings.beans.InfrastructureMapping;
+import software.wings.beans.Service;
+import software.wings.beans.ServiceTemplate;
+import software.wings.beans.SettingAttribute;
+import software.wings.beans.artifact.Artifact;
+import software.wings.beans.artifact.ArtifactStream;
+import software.wings.beans.command.CommandType;
+import software.wings.beans.command.ServiceCommand;
+import software.wings.service.intfc.ActivityService;
+import software.wings.service.intfc.AppService;
+import software.wings.service.intfc.ArtifactService;
+import software.wings.service.intfc.ArtifactStreamService;
+import software.wings.service.intfc.DelegateService;
+import software.wings.service.intfc.EnvironmentService;
+import software.wings.service.intfc.InfrastructureMappingService;
+import software.wings.service.intfc.ServiceResourceService;
+import software.wings.service.intfc.ServiceTemplateService;
+import software.wings.service.intfc.SettingsService;
+import software.wings.service.intfc.WorkflowExecutionService;
+import software.wings.service.intfc.security.EncryptionService;
+import software.wings.service.intfc.security.SecretManager;
+import software.wings.sm.ExecutionContextImpl;
+import software.wings.sm.StateExecutionInstance;
+import software.wings.sm.WorkflowStandardParams;
+
+import java.util.Collections;
+
+public class KubernetesReplicationControllerSetupTest extends WingsBaseTest {
+  private static final String KUBERNETES_REPLICATION_CONTROLLER_NAME = "kubernetes-rc-name.1";
+  private static final String KUBERNETES_REPLICATION_CONTROLLER_OLD_NAME = "kubernetes-rc-name.0";
+
+  @Mock private SettingsService settingsService;
+  @Mock private DelegateService delegateService;
+  @Mock private ServiceResourceService serviceResourceService;
+  @Mock private ActivityService activityService;
+  @Mock private InfrastructureMappingService infrastructureMappingService;
+  @Mock private AppService appService;
+  @Mock private EnvironmentService environmentService;
+  @Mock private ServiceTemplateService serviceTemplateService;
+  @Mock private SecretManager secretManager;
+  @Mock private WorkflowExecutionService workflowExecutionService;
+  @Mock private ArtifactService artifactService;
+  @Mock private ArtifactStreamService artifactStreamService;
+  @Mock private EncryptionService encryptionService;
+
+  @InjectMocks
+  private KubernetesReplicationControllerSetup kubernetesReplicationControllerSetup =
+      new KubernetesReplicationControllerSetup("name");
+
+  private ExecutionContextImpl context;
+
+  private WorkflowStandardParams workflowStandardParams = aWorkflowStandardParams()
+                                                              .withAppId(APP_ID)
+                                                              .withEnvId(ENV_ID)
+                                                              .withArtifactIds(Lists.newArrayList(ARTIFACT_ID))
+                                                              .build();
+  private ServiceElement serviceElement = aServiceElement().withUuid(SERVICE_ID).withName(SERVICE_NAME).build();
+  private PhaseElement phaseElement = aPhaseElement()
+                                          .withUuid(getUuid())
+                                          .withServiceElement(serviceElement)
+                                          .withInfraMappingId(INFRA_MAPPING_ID)
+                                          .withDeploymentType(DeploymentType.KUBERNETES.name())
+                                          .build();
+  private StateExecutionInstance stateExecutionInstance =
+      aStateExecutionInstance()
+          .withStateName(STATE_NAME)
+          .addContextElement(workflowStandardParams)
+          .addContextElement(phaseElement)
+          .addContextElement(aContainerServiceElement()
+                                 .withUuid(serviceElement.getUuid())
+                                 .withMaxInstances(10)
+                                 .withClusterName(CLUSTER_NAME)
+                                 .withNamespace("default")
+                                 .withName(KUBERNETES_REPLICATION_CONTROLLER_NAME)
+                                 .withResizeStrategy(RESIZE_NEW_FIRST)
+                                 .withInfraMappingId(INFRA_MAPPING_ID)
+                                 .withDeploymentType(DeploymentType.KUBERNETES)
+                                 .build())
+          .addStateExecutionData(new PhaseStepExecutionData())
+          .build();
+
+  private Application app = anApplication().withUuid(APP_ID).withName(APP_NAME).build();
+  private Environment env = anEnvironment().withAppId(APP_ID).withUuid(ENV_ID).withName(ENV_NAME).build();
+  private Service service = aService().withAppId(APP_ID).withUuid(SERVICE_ID).withName(SERVICE_NAME).build();
+  private Artifact artifact = anArtifact()
+                                  .withArtifactSourceName("source")
+                                  .withMetadata(ImmutableMap.of(BUILD_NO, "bn"))
+                                  .withServiceIds(singletonList(SERVICE_ID))
+                                  .build();
+  private ArtifactStream artifactStream = aDockerArtifactStream().withAppId(APP_ID).withImageName("imageName").build();
+
+  private SettingAttribute dockerConfig = aSettingAttribute()
+                                              .withValue(DockerConfig.builder()
+                                                             .dockerRegistryUrl("url")
+                                                             .password("pass".toCharArray())
+                                                             .username("user")
+                                                             .accountId(ACCOUNT_ID)
+                                                             .build())
+                                              .build();
+
+  @Before
+  public void setup() {
+    when(appService.get(APP_ID)).thenReturn(app);
+    when(serviceResourceService.get(APP_ID, SERVICE_ID)).thenReturn(service);
+    when(environmentService.get(APP_ID, ENV_ID, false)).thenReturn(env);
+
+    ServiceCommand serviceCommand =
+        aServiceCommand()
+            .withCommand(aCommand().withCommandType(CommandType.SETUP).withName("Setup Replication Controller").build())
+            .build();
+    when(serviceResourceService.getCommandByName(APP_ID, SERVICE_ID, ENV_ID, "Setup Replication Controller"))
+        .thenReturn(serviceCommand);
+    on(workflowStandardParams).set("appService", appService);
+    on(workflowStandardParams).set("environmentService", environmentService);
+    on(workflowStandardParams).set("artifactService", artifactService);
+
+    when(artifactService.get(any(), any())).thenReturn(artifact);
+    when(artifactStreamService.get(any(), any())).thenReturn(artifactStream);
+
+    InfrastructureMapping infrastructureMapping = aGcpKubernetesInfrastructureMapping()
+                                                      .withClusterName(CLUSTER_NAME)
+                                                      .withComputeProviderSettingId(COMPUTE_PROVIDER_ID)
+                                                      .build();
+    when(infrastructureMappingService.get(APP_ID, INFRA_MAPPING_ID)).thenReturn(infrastructureMapping);
+
+    Activity activity = Activity.builder().build();
+    activity.setUuid(ACTIVITY_ID);
+    when(activityService.save(any(Activity.class))).thenReturn(activity);
+
+    when(settingsService.get(any())).thenReturn(dockerConfig);
+
+    when(serviceTemplateService.getTemplateRefKeysByService(APP_ID, SERVICE_ID, ENV_ID))
+        .thenReturn(singletonList(new Key<>(ServiceTemplate.class, "serviceTemplate", TEMPLATE_ID)));
+
+    when(serviceTemplateService.get(APP_ID, TEMPLATE_ID)).thenReturn(aServiceTemplate().withUuid(TEMPLATE_ID).build());
+    when(serviceTemplateService.computeServiceVariables(APP_ID, ENV_ID, TEMPLATE_ID, null)).thenReturn(emptyList());
+    when(secretManager.getEncryptionDetails(anyObject(), anyString(), anyString())).thenReturn(Collections.emptyList());
+    setInternalState(kubernetesReplicationControllerSetup, "secretManager", secretManager);
+    //    when(encryptionService.decrypt(any(), any())).thenR
+    when(workflowExecutionService.getExecutionDetails(anyString(), anyString()))
+        .thenReturn(aWorkflowExecution().build());
+    context = new ExecutionContextImpl(stateExecutionInstance);
+  }
+
+  @Test
+  public void shouldExecute() {
+    on(context).set("serviceTemplateService", serviceTemplateService);
+
+    kubernetesReplicationControllerSetup.execute(context);
+  }
+}
