2b79f0b7a555050202479c3adc690b728a60d117
Brett Zane <brett@harness.io>
2017-12-03 00:17:07 -0800

    Remove unused class.

--- rest/src/main/java/software/wings/service/intfc/MaintenanceService.java
+++
@@ -1,6 +0,0 @@
-package software.wings.service.intfc;
-
-import io.dropwizard.lifecycle.Managed;
-import software.wings.core.maintenance.MaintenanceListener;
-
-public interface MaintenanceService extends Managed { void register(MaintenanceListener listener); }
