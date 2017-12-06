b5acb58e1a52f84703785a98eeb59abc0bd5dc32
George Georgiev <george@harness.io>
2017-12-05 16:15:41 -0800

    Make the jobs self pruning to simplify the object deletion implementation

---
+++ rest/src/test/java/software/wings/scheduler/StateMachineExecutionCleanupJobTest.java
@@ -0,0 +1,50 @@
+package software.wings.scheduler;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.SimpleScheduleBuilder;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import software.wings.WingsBaseTest;
+import software.wings.rules.Integration;
+import software.wings.rules.SetupScheduler;
+import software.wings.utils.Misc;
+
+@Integration
+@Ignore
+@SetupScheduler
+public class StateMachineExecutionCleanupJobTest extends WingsBaseTest {
+  @Inject @Named("JobScheduler") private QuartzScheduler jobScheduler;
+
+  private final static String appId = "Dummy App Id";
+
+  public void scheduleJob() {
+    JobDetail job = JobBuilder.newJob(StateMachineExecutionCleanupJob.class)
+                        .withIdentity(appId, StateMachineExecutionCleanupJob.GROUP)
+                        .usingJobData("appId", appId)
+                        .build();
+
+    Trigger trigger = TriggerBuilder.newTrigger()
+                          .withIdentity(appId, StateMachineExecutionCleanupJob.GROUP)
+                          .withSchedule(SimpleScheduleBuilder.simpleSchedule().withIntervalInSeconds(1).repeatForever())
+                          .build();
+
+    jobScheduler.scheduleJob(job, trigger);
+  }
+
+  @Test
+  public void selfPrune() {
+    scheduleJob();
+    // In 3 seconds we should have the scheduled job executed, that should discover that it is not needed and delete
+    // itself.
+    Misc.quietSleep(3000);
+    assertThat(jobScheduler.deleteJob(appId, StateMachineExecutionCleanupJob.GROUP)).isFalse();
+  }
+}
