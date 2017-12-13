7c6744a167cbb93ba0a0cb29483316656178f9ea
Brett Zane <brett@harness.io>
2017-12-13 09:23:09 -0800

    Fix build.

--- rest/src/test/java/software/wings/sm/states/EcsServiceDeployTest.java
+++ rest/src/test/java/software/wings/sm/states/EcsServiceDeployTest.java
@@ -76,7 +76,6 @@ import software.wings.beans.command.CommandType;
 import software.wings.beans.command.ServiceCommand;
 import software.wings.delegatetasks.DelegateProxyFactory;
 import software.wings.exception.WingsException;
-import software.wings.service.impl.ContainerServiceParams;
 import software.wings.service.intfc.ActivityService;
 import software.wings.service.intfc.AppService;
 import software.wings.service.intfc.ContainerService;
@@ -97,7 +96,6 @@ import software.wings.waitnotify.NotifyResponseData;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 
 /**
  * Created by rishi on 2/27/17.
@@ -199,7 +197,6 @@ public class EcsServiceDeployTest extends WingsBaseTest {
 
     when(delegateProxyFactory.get(eq(ContainerService.class), any(DelegateTask.SyncTaskContext.class)))
         .thenReturn(containerService);
-    when(containerService.getServiceDesiredCount(any(ContainerServiceParams.class))).thenReturn(Optional.of(0));
   }
 
   @Test
@@ -214,7 +211,6 @@ public class EcsServiceDeployTest extends WingsBaseTest {
 
   @Test
   public void shouldExecuteThrowInvalidRequest() {
-    when(containerService.getServiceDesiredCount(any(ContainerServiceParams.class))).thenReturn(Optional.empty());
     try {
       on(context).set("serviceTemplateService", serviceTemplateService);
       ecsServiceDeploy.execute(context);
