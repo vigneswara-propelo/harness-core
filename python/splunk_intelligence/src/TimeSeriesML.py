4540afe8a4f0bdfa700e82414fbe8b1d4b0ee98b
Parnian Zargham <parnian@harness.io>
2017-12-21 10:20:06 -0800

    after puling from master and removed the extra line

--- python/splunk_intelligence/src/TimeSeriesML.py
+++ python/splunk_intelligence/src/TimeSeriesML.py
@@ -691,18 +691,18 @@ def main(args):
     logger.info('test_events = ' + str(len(test_metrics)))
 
     # Uncomment when you want to save the files for local debugging
-<<<<<<< Updated upstream
+
     # write_to_file('/Users/parnianzargham/Desktop/wings/python/splunk_intelligence/time_series/test_live_new3.json', test_metrics)
     # write_to_file('/Users/parnianzargham/Desktop/wings/python/splunk_intelligence/time_series/control_live_new3.json',control_metrics)
-=======
-    write_to_file('/Users/parnianzargham/Desktop/wings/python/splunk_intelligence/time_series/test_live_new3.json', test_metrics)
 
-    write_to_file('/Users/parnianzargham/Desktop/wings/python/splunk_intelligence/time_series/control_live_new3.json',control_metrics)
->>>>>>> Stashed changes
+    # write_to_file('/Users/parnianzargham/Desktop/wings/python/splunk_intelligence/time_series/test_live_new3.json', test_metrics)
+    #
+    # write_to_file('/Users/parnianzargham/Desktop/wings/python/splunk_intelligence/time_series/control_live_new3.json',control_metrics)
+    #
 
     result = parallelize_processing(options, metric_template, control_metrics, test_metrics)
-    write_to_file('/Users/parnianzargham/Desktop/wings/python/splunk_intelligence/time_series/result_new_test3.json',
-                  result)
+    # write_to_file('/Users/parnianzargham/Desktop/wings/python/splunk_intelligence/time_series/result_new_test3.json',
+    #               result)
     post_to_wings_server(options, result)
 
 
