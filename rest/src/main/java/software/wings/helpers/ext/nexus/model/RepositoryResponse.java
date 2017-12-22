f59e463f549666f74866c9e7e3d25d5a6bfbcdd9
Srinivasa Gurubelli <srinivas@harness.io>
2017-12-21 19:50:13 -0800

    Added JsonIgnore fields annotation

--- rest/src/main/java/software/wings/helpers/ext/nexus/model/RepositoryResponse.java
+++ rest/src/main/java/software/wings/helpers/ext/nexus/model/RepositoryResponse.java
@@ -1,9 +1,12 @@
 package software.wings.helpers.ext.nexus.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 /**
  * Created by sgurubelli on 11/17/17.
  */
 @lombok.Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class RepositoryResponse {
   private int tid;
   private String action;
--- rest/src/main/java/software/wings/helpers/ext/nexus/model/Result.java
+++ rest/src/main/java/software/wings/helpers/ext/nexus/model/Result.java
@@ -1,5 +1,7 @@
 package software.wings.helpers.ext.nexus.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,6 +9,7 @@ import java.util.List;
  * Created by sgurubelli on 11/17/17.
  */
 @lombok.Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Result {
   private boolean success;
   private List<ResponseData> data = new ArrayList<>();
