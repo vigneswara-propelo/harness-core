b492c62b42f35888ab3b53273f4e932f9db98683
George Georgiev <george@harness.io>
2017-12-28 13:17:54 -0800

    Do not use Wings exception that specializes in user response for healthy API checks

--- rest/src/main/java/software/wings/resources/HealthResource.java
+++ rest/src/main/java/software/wings/resources/HealthResource.java
@@ -7,9 +7,7 @@ import com.codahale.metrics.annotation.Timed;
 import io.swagger.annotations.Api;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import software.wings.beans.ErrorCode;
 import software.wings.beans.RestResponse;
-import software.wings.exception.WingsException;
 import software.wings.security.annotations.PublicApi;
 
 import javax.ws.rs.Consumes;
@@ -35,7 +33,7 @@ public class HealthResource {
   public RestResponse<String> get() {
     if (isMaintenance()) {
       logger.info("In maintenance mode, responding RESOURCE_NOT_FOUND for load balancer health check.");
-      throw new WingsException(ErrorCode.RESOURCE_NOT_FOUND);
+      throw new RuntimeException("In maintenance");
     }
     return new RestResponse<>("healthy");
   }
