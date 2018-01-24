71f0c876a0443d15b16928d9bc995e201c32dc95
Parnian Zargham <parnian@harness.io>
2018-01-23 20:42:48 -0800

    lstm predictive analysis (#89)

---
+++ python/splunk_intelligence/src/DataHandler.py
@@ -0,0 +1,134 @@
+import logging
+import numpy as np
+from collections import OrderedDict
+from core.util.TimeSeriesUtils import MetricType
+
+
+log_format = "%(asctime)-15s %(levelname)s %(message)s"
+logging.basicConfig(level=logging.INFO, format=log_format)
+logger = logging.getLogger(__name__)
+
+
+def group_txns(analysis_minute, metric_names, transactions, metric_template, min_rpm, num_days):
+
+    result = {}
+    data_len = (analysis_minute + 1)
+    for transaction in transactions:
+        txn_name = transaction.get('name')
+        host = transaction.get('host')
+        data_collection_minute = transaction.get('dataCollectionMinute')
+        day = 0
+
+        if txn_name not in result:
+            result[txn_name] = OrderedDict({})
+            for metric_name in metric_names:
+                result[txn_name][metric_name] = OrderedDict({})
+
+        for metric_name in metric_names:
+            if host not in result[txn_name][metric_name]:
+                result[txn_name][metric_name][host] = OrderedDict({})
+                result[txn_name][metric_name][host]['data'] = np.array([([np.nan] * data_len)] * num_days)
+            if transaction.get(metric_name) is None or transaction.get(metric_name) == -1:
+                continue
+
+            result[txn_name][metric_name][host]['data'][day, data_collection_minute] = transaction.get(metric_name)
+
+
+    return sanitize_data(result, metric_template, min_rpm)
+
+
+def sanitize_data(txns_dict, metric_template, min_rpm_threshold):
+    """
+
+    Sanitize applies domain knowledge to embellish the collected data:
+
+    It does 2 things:
+
+    i) if the call count is 0, mark the average response time as unavailable, i.e set the data point
+    to np.nan
+
+    ii) if the requestsPerMinute is less than the min_rpm_threshold, mark the host as skipped. This flag will be used
+    when deciding if the transaction is an anomaly. All hosts that are skipped will be not be considered when
+    deciding if the transaction is anomalous later on.
+
+    :param txns_dict: the transaction dictionary
+    :param min_rpm_threshold: the minimum requests per minute threshold
+    :return: embellished transaction dictionary
+    """
+    for txn_data_dict in txns_dict.values():
+        for metric_name, metric_data_dict in txn_data_dict.items():
+            if metric_template.get_metric_type(metric_name) == MetricType.RESP_TIME\
+                     and metric_template.get_metric_name(MetricType.THROUGHPUT) is not None:
+                throughput_metric_name = metric_template.get_metric_name(MetricType.THROUGHPUT)
+                for host, metric_host_data_dict in metric_data_dict.items():
+                    metric_host_data_dict['data'][np.where(txn_data_dict[throughput_metric_name][host]['data'] == 0)[0]] \
+                        = np.nan
+            if metric_template.get_metric_type(metric_name) == MetricType.THROUGHPUT:
+                for host, metric_host_data_dict in metric_data_dict.items():
+                    if np.nansum(metric_host_data_dict['data']) < min_rpm_threshold:
+                        metric_host_data_dict['skip'] = True
+                    else:
+                        metric_host_data_dict['skip'] = False
+    return txns_dict
+
+
+def get_metrics_data(metric_name, metric_template, txn_data_dict):
+
+    """
+    extract data for the given metric_name from the transaction data dictionary
+    for processing.
+
+    {
+        host_names: [list of hosts from which the metric was extracted],
+
+        data: numpy 2d array of floats, which each subarray representing data collected from a node
+                    [ [ host1 data ], [ host2 data ], [ host3 data ].....]
+
+        data_type: histogram or count, useful when aggregating.
+                   Ex: average a histogram metric, whereas sum a count based metric
+
+        weights (optional): numpy 2d array of floats, that are the weights for the data points
+                    [ [ host1 weights ], [ host2 weights ], [ host3 weights ].....],
+
+        weights_type (optional): count
+    }
+
+    Weights are presently only used to scale the average response times by the call count. The intution is
+    that the response times are more significant if the call counts are higher.
+
+    :param metric_name: the name of the metric
+    :param metric_template containing the metric configurations
+    :param txn_data_dict: the transaction data dictionary
+    :return: the data dictionary for processing.
+    """
+    if txn_data_dict is None or len(txn_data_dict) == 0:
+        return {}
+
+    weights = []
+    data = []
+    host_names = []
+
+    for (host_name, host_data) in txn_data_dict[metric_name].items():
+        host_names.append(host_name)
+        data.append(host_data['data'].astype(np.float64))
+    return dict(host_names=host_names, data=np.stack(data, axis=2),
+                data_type=metric_template.get_metric_type(metric_name))
+
+
+def validate(txn_name, metric_name, dict, type = 'control'):
+    """
+    Skip the metric for a transaction if there is no valid data.
+    """
+    control_valid = 'data' in dict and len(np.where(np.isfinite(dict['data'].flatten()))[0]) > 0
+    # No useful control or test data.
+    if not control_valid :
+        logger.warn(
+            "Skipping Txn -  " + txn_name + " , metric - " + metric_name + " No valid " + type + " data found!")
+        return False
+    else:
+        return True
+
+
+
+## questions :
+## ask whether skip caused problems or not
---
+++ python/splunk_intelligence/src/LSTM.py
@@ -0,0 +1,175 @@
+import numpy as np
+import pandas as pd
+import DataHandler as dh
+import time
+import logging
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, TimeDistributed, Dropout
+from collections import OrderedDict
+from sources.MetricTemplate import MetricTemplate
+
+log_format = "%(asctime)-15s %(levelname)s %(message)s"
+logging.basicConfig(level=logging.INFO, format=log_format)
+logger = logging.getLogger(__name__)
+
+
+class LSTMAnomalyDetector(object):
+
+    def __init__(self, options, metric_template, control_txns, test_txns):
+        self._options = options
+        self.metric_template = MetricTemplate(metric_template)
+        self.metric_names = self.metric_template.get_metric_names()
+        self.raw_control_txns = control_txns
+        self.raw_test_txns = test_txns
+        self.min_rpm = options.min_rpm
+
+    def get_training_set(self, txn_name, control_txn_data_dict):
+        metrics_in_df = []
+        metrics_out_df = []
+        existing_metrics = []
+        for metric_ind, metric_name in enumerate(self.metric_names):
+            control_data_dict = dh.get_metrics_data(metric_name, self.metric_template, control_txn_data_dict)
+            if dh.validate(txn_name, metric_name, control_data_dict, type='control'):
+                metric_shifted_in_data = []
+                metric_shifted_out_data = []
+                ## number of days is first dimension, number of time point is second dimension, number of host is 3rd dimension of data
+                for host_ind in range(len(control_data_dict['host_names'])):
+                    host_data = control_data_dict['data'][:, :, host_ind]
+                    host_data = host_data[~np.isnan(host_data).all(axis=1)]
+                    # this for loop is for different days
+                    for data in host_data:
+                        data = data - np.nanmean(data)
+                        metric_host_df = pd.DataFrame(data, columns=[metric_name])
+                        shifted_in_df, shifted_out_df = format_data(metric_host_df, time_steps=self._options.time_steps,
+                                                                    dropnan=True)
+                        metric_shifted_in_data.append(shifted_in_df.values)
+                        metric_shifted_out_data.append(shifted_out_df.values)
+                ## appending shifted dataframes of all control hosts for each metric
+                metrics_in_df.append(np.concatenate(metric_shifted_in_data, axis=0))
+                metrics_out_df.append(np.concatenate(metric_shifted_out_data, axis=0))
+                existing_metrics.append(metric_name)
+        train_in_data = np.stack(metrics_in_df, axis=2) if metrics_in_df else np.array([])
+        train_out_data = np.stack(metrics_out_df, axis=2) if metrics_in_df else pd.DataFrame()
+        return dict(train_X=train_in_data, train_Y=train_out_data, metrics=existing_metrics)
+
+    def prediction(self, txn_ind, txn_name, control_txn_data_dict, test_txn_data_dict, result, txns_count):
+        response = {'results': {}, 'max_risk': -1, 'control_avg': -1, 'test_avg': -1}
+        training = self.get_training_set(txn_name, control_txn_data_dict)
+        model = train_lstm(training['train_X'], training['train_Y'], self._options.time_steps, self._options.epochs,
+                           self._options.batch_size, self._options.val_percent)
+        list_metrics_data = []
+        existing_metric = []
+        for metric_ind, metric_name in enumerate(self.metric_names):
+            test_data_dict = dh.get_metrics_data(metric_name, self.metric_template, test_txn_data_dict)
+            if metric_name in training['metrics']:
+                if dh.validate(txn_name, metric_name, test_data_dict, type='test'):
+                    # test data should be recorded just for last day
+                    day_test_data = np.squeeze(test_data_dict['data'], axis=0)
+                    list_metrics_data.append(np.transpose(day_test_data))
+                    existing_metric.append(metric_name)
+        test_metric_3d_data = np.stack(list_metrics_data, axis=2)
+        # test data after deployment
+        test_metric_3d_data[np.isnan(test_metric_3d_data)] = 0
+        new_predicted_data = test_metric_3d_data[:, :self._options.prediction_mins, :]
+        start = self._options.prediction_mins - self._options.time_steps
+        end = self._options.prediction_mins
+        while (end < self._options.total_time + 1):
+            test_x = test_metric_3d_data[:, start:end, :]
+            mean_x = np.mean(test_x, axis=1)[:, np.newaxis, :]
+            test_x = test_x - mean_x
+            y_hat = model.predict(test_x) + mean_x
+            new_predicted_data = np.concatenate([new_predicted_data, y_hat], axis=1)
+            start += self._options.time_steps
+            end += self._options.time_steps
+        result[txn_name] = OrderedDict({})
+        for metric_ind, metric in enumerate(existing_metric):
+            for host_ind, host_name in enumerate(test_data_dict['host_names']):
+                response['results'][host_name] = {}
+                response['results'][host_name]['control_data'] = new_predicted_data[host_ind,
+                                                                 :self._options.total_time + 1, metric_ind].tolist()
+                response['results'][host_name]['test_data'] = test_metric_3d_data[host_ind, :,
+                                                              metric_ind].tolist()
+            ''' Use numbers for dictionary keys to avoid a failure on the Harness manager side
+                when saving data to MongoDB. MongoDB does not allow 'dot' chars in the key names for
+                dictionaries, and transactions or metric names can contain the dot char.
+                So use numbers for keys and stick the name inside the dictionary. '''
+            if txn_ind not in result['transactions']:
+                result['transactions'][txn_ind] = dict(txn_name=txn_name, metrics={})
+
+            result['transactions'][txn_ind]['metrics'][metric_ind] = response
+            txns_count += 1
+        return 0
+
+    def analyze(self):
+        """
+         analyze all transaction / metric combinations
+        """
+        start_time = time.time()
+        result = {'transactions': {}}
+
+        control_txn_groups = dh.group_txns(self._options.total_time, self.metric_names, self.raw_control_txns,
+                                           self.metric_template, self._options.min_rpm, self._options.num_days)
+        test_txn_groups = dh.group_txns(self._options.total_time, self.metric_names, self.raw_test_txns,
+                                        self.metric_template, self._options.min_rpm, self._options.num_days)
+
+        txns_count = 0
+        if len(control_txn_groups) == 0 or len(test_txn_groups) == 0:
+            logger.warn(
+                "No control or test data given for minute " + str(
+                    self._options.analysis_minute) + ". Skipping analysis!!")
+        else:
+
+            for txn_ind, (txn_name, test_txn_data_dict) in enumerate(test_txn_groups.items()):
+
+                if txn_name in control_txn_groups:
+                    control_txn_data_dict = control_txn_groups[txn_name]
+                else:
+                    control_txn_data_dict = {}
+                #if txn_name =='WebTransaction':
+                self.prediction(txn_ind,  txn_name, control_txn_data_dict, test_txn_data_dict, result, txns_count)
+        logger.info('time taken ' + str(time.time() - start_time) + ' for # txns = ' + str(txns_count))
+        return result
+
+
+def train_lstm(features, target, time_steps, epochs, batch_size, val_percent):
+    num_metrics = features.shape[2]
+    val_dim = int(features.shape[0] * val_percent)
+    val_x = features[:val_dim, :, :]
+    train_x = features[val_dim:, :, :]
+    val_y = target[:val_dim, :, :]
+    train_y = target[val_dim:, :, :]
+    model = Sequential()
+    model.add(LSTM(features.shape[0], return_sequences=True, input_shape=(time_steps, num_metrics)))
+    model.add(Dropout(0.2))
+    model.add(LSTM(features.shape[0], return_sequences=True))  # returns (timesteps, batchsize)
+    model.add(TimeDistributed(Dense(num_metrics, activation='linear')))  # ouptput shape (none, timesteps, num_metric)
+    # model.add(TimeDistributed(Dense(num_metrics, activation='linear'), input_shape=(timesteps, num_metrics)))
+    model.compile(loss='mse', optimizer="rmsprop", metrics=['accuracy'])
+    model.fit(train_x, train_y,
+              batch_size=batch_size, epochs=epochs,
+              validation_data=(val_x, val_y))
+    return model
+
+
+def format_data(data, time_steps=1, dropnan=True):
+    df = data.fillna(0)
+    cols, names = list(), list()
+    var_name = df.columns[0]
+    # input sequence (t-n, ... t-1)
+    for i in range(time_steps, 0, -1):
+        cols.append(df.shift(i))
+        names += [(var_name + '-%d' % i)]
+    # forecast sequence (t, t+1, ... t+n)
+    for i in range(0, time_steps):
+        cols.append(df.shift(-i))
+        if i == 0:
+            names += [var_name]
+        else:
+            names += [(var_name + '+%d' % i)]
+    # put it all together
+    agg = pd.concat(cols, axis=1)
+    agg.columns = names
+    # drop rows with NaN values
+    if dropnan:
+        agg.dropna(inplace=True)
+    return agg.iloc[:, :time_steps], agg.iloc[:, time_steps:]
