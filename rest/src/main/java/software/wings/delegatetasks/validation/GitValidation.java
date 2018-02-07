bb1c413f407da3fd5337047df63d8cba147e5f8b
Brett Zane <brett@harness.io>
2018-02-07 13:17:32 -0800

    Minor cleanup (#592)

--- rest/src/main/java/software/wings/delegatetasks/validation/GitValidation.java
+++ rest/src/main/java/software/wings/delegatetasks/validation/GitValidation.java
@@ -1,18 +1,17 @@
 package software.wings.delegatetasks.validation;
 
 import static java.util.Collections.singletonList;
+import static org.apache.commons.lang3.StringUtils.startsWith;
 import static software.wings.beans.ErrorCode.UNREACHABLE_HOST;
 
 import com.google.inject.Inject;
 
-import org.apache.commons.lang3.StringUtils;
 import software.wings.beans.DelegateTask;
 import software.wings.beans.GitConfig;
 import software.wings.security.encryption.EncryptedDataDetail;
 import software.wings.service.intfc.security.EncryptionService;
 import software.wings.service.intfc.yaml.GitClient;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -29,25 +28,20 @@ public class GitValidation extends AbstractDelegateValidateTask {
   }
 
   @Override
+  @SuppressWarnings("unchecked")
   public List<DelegateConnectionResult> validate() {
-    Object[] parameters = getParameters();
-    GitConfig gitConfig = (GitConfig) parameters[1];
-    List<EncryptedDataDetail> encryptionDetails = (List<EncryptedDataDetail>) parameters[2];
+    GitConfig gitConfig = (GitConfig) getParameters()[1];
+    List<EncryptedDataDetail> encryptionDetails = (List<EncryptedDataDetail>) getParameters()[2];
     encryptionService.decrypt(gitConfig, encryptionDetails);
 
-    return singletonList(
-        DelegateConnectionResult.builder()
-            .criteria(gitConfig.getRepoUrl())
-            .validated(!StringUtils.startsWith(gitClient.validate(gitConfig), UNREACHABLE_HOST.getDescription()))
-            .build());
+    return singletonList(DelegateConnectionResult.builder()
+                             .criteria(gitConfig.getRepoUrl())
+                             .validated(!startsWith(gitClient.validate(gitConfig), UNREACHABLE_HOST.getDescription()))
+                             .build());
   }
 
   @Override
   public List<String> getCriteria() {
-    return singletonList(Arrays.stream(getParameters())
-                             .filter(o -> o instanceof GitConfig)
-                             .map(config -> ((GitConfig) config).getRepoUrl())
-                             .findFirst()
-                             .orElse(null));
+    return singletonList(((GitConfig) getParameters()[1]).getRepoUrl());
   }
 }
