a69a487a71279e300dfe0e2818ad7cf1dfa107db
George Georgiev <george@harness.io>
2018-03-03 16:53:46 -0800

    Fix the formatting for ManagerClientFactory (#857)

--- delegate/src/main/java/software/wings/managerclient/ManagerClientFactory.java
+++ delegate/src/main/java/software/wings/managerclient/ManagerClientFactory.java
@@ -24,66 +24,67 @@ import javax.net.ssl.X509TrustManager;
 /**
  * Created by peeyushaggarwal on 11/29/16.
  */
-public class ManagerClientFactory implements Provider<ManagerClient> {
-  public static final TrustManager[] TRUST_ALL_CERTS =
-      new X509TrustManager[] {new X509TrustManager(){public java.security.cert.X509Certificate[] getAcceptedIssuers(){
-          return new java.security.cert.X509Certificate[] {};
-}
 
-public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType) {}
-
-public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType) {}
-}
-}
-;
-
-private static final Logger logger = LoggerFactory.getLogger("http");
-private String baseUrl;
-private TokenGenerator tokenGenerator;
-
-public ManagerClientFactory(String baseUrl, TokenGenerator tokenGenerator) {
-  this.baseUrl = baseUrl;
-  this.tokenGenerator = tokenGenerator;
-}
-
-@Override
-public ManagerClient get() {
-  ObjectMapper objectMapper = new ObjectMapper();
-  objectMapper.registerModule(new Jdk8Module());
-  objectMapper.registerModule(new GuavaModule());
-  objectMapper.registerModule(new JavaTimeModule());
-  Retrofit retrofit = new Retrofit.Builder()
-                          .baseUrl(baseUrl)
-                          .client(getUnsafeOkHttpClient())
-                          .addConverterFactory(new KryoConverterFactory())
-                          .addConverterFactory(JacksonConverterFactory.create(objectMapper))
-                          .build();
-  return retrofit.create(ManagerClient.class);
-}
-
-private OkHttpClient getUnsafeOkHttpClient() {
-  try {
-    // Install the all-trusting trust manager
-    final SSLContext sslContext = SSLContext.getInstance("SSL");
-    sslContext.init(null, TRUST_ALL_CERTS, new java.security.SecureRandom());
-    // Create an ssl socket factory with our all-trusting manager
-    final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
-
-    return new Builder()
-        .connectionPool(new ConnectionPool())
-        .retryOnConnectionFailure(true)
-        .addInterceptor(new DelegateAuthInterceptor(tokenGenerator))
-        .sslSocketFactory(sslSocketFactory, (X509TrustManager) TRUST_ALL_CERTS[0])
-        .addInterceptor(chain
-            -> chain.proceed(chain.request()
-                                 .newBuilder()
-                                 .addHeader("User-Agent", "delegate/" + System.getProperty("version"))
-                                 .build()))
-        .addInterceptor(chain -> ExponentialBackOff.executeForEver(() -> chain.proceed(chain.request())))
-        .hostnameVerifier((hostname, session) -> true)
-        .build();
-  } catch (Exception e) {
-    throw new RuntimeException(e);
+class ManagerClientX509TrustManager implements X509TrustManager {
+  public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+    return new java.security.cert.X509Certificate[] {};
   }
+
+  public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType) {}
+
+  public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType) {}
 }
+
+public class ManagerClientFactory implements Provider<ManagerClient> {
+  public static final TrustManager[] TRUST_ALL_CERTS = new X509TrustManager[] {new ManagerClientX509TrustManager()};
+
+  private static final Logger logger = LoggerFactory.getLogger("http");
+  private String baseUrl;
+  private TokenGenerator tokenGenerator;
+
+  public ManagerClientFactory(String baseUrl, TokenGenerator tokenGenerator) {
+    this.baseUrl = baseUrl;
+    this.tokenGenerator = tokenGenerator;
+  }
+
+  @Override
+  public ManagerClient get() {
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.registerModule(new Jdk8Module());
+    objectMapper.registerModule(new GuavaModule());
+    objectMapper.registerModule(new JavaTimeModule());
+    Retrofit retrofit = new Retrofit.Builder()
+                            .baseUrl(baseUrl)
+                            .client(getUnsafeOkHttpClient())
+                            .addConverterFactory(new KryoConverterFactory())
+                            .addConverterFactory(JacksonConverterFactory.create(objectMapper))
+                            .build();
+    return retrofit.create(ManagerClient.class);
+  }
+
+  private OkHttpClient getUnsafeOkHttpClient() {
+    try {
+      // Install the all-trusting trust manager
+      final SSLContext sslContext = SSLContext.getInstance("SSL");
+      sslContext.init(null, TRUST_ALL_CERTS, new java.security.SecureRandom());
+      // Create an ssl socket factory with our all-trusting manager
+      final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+      return new Builder()
+          .connectionPool(new ConnectionPool())
+          .retryOnConnectionFailure(true)
+          .addInterceptor(new DelegateAuthInterceptor(tokenGenerator))
+          .sslSocketFactory(sslSocketFactory, (X509TrustManager) TRUST_ALL_CERTS[0])
+          .addInterceptor(chain
+              -> chain.proceed(chain.request()
+                                   .newBuilder()
+                                   .addHeader("User-Agent", "delegate/" + System.getProperty("version"))
+                                   .build()))
+          .addInterceptor(chain -> ExponentialBackOff.executeForEver(() -> chain.proceed(chain.request())))
+          .hostnameVerifier((hostname, session) -> true)
+          .build();
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
 }
