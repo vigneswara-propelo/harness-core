0e1f9504944cccdd501276d26053fd73b15a24b0
Rama Tummala <rama@harness.io>
2018-04-05 19:37:46 -0700

    Commenting the build pipeline restriction based on discussion with Rishi and Srinivas for serving NCR use case (#1290)

--- rest/src/main/java/software/wings/service/impl/PipelineServiceImpl.java
+++ rest/src/main/java/software/wings/service/impl/PipelineServiceImpl.java
@@ -541,14 +541,14 @@ public class PipelineServiceImpl implements PipelineService {
               .addParam("args", "Environment can not be null for non-build state");
         }
 
-        if (workflow.getOrchestrationWorkflow().getOrchestrationWorkflowType() == OrchestrationWorkflowType.BUILD
-            && i > 0) {
-          throw new WingsException(INVALID_ARGUMENT)
-              .addParam("args",
-                  "A pipeline can have only one build workflow and it has to be at the beginning of the pipeline. "
-                      + "If the pipeline needs more than one artifact use multiple steps in the build workflow "
-                      + "to build and collect all required artifacts.");
-        }
+//        if (workflow.getOrchestrationWorkflow().getOrchestrationWorkflowType() == OrchestrationWorkflowType.BUILD
+//            && i > 0) {
+//          throw new WingsException(INVALID_ARGUMENT)
+//              .addParam("args",
+//                  "A pipeline can have only one build workflow and it has to be at the beginning of the pipeline. "
+//                      + "If the pipeline needs more than one artifact use multiple steps in the build workflow "
+//                      + "to build and collect all required artifacts.");
+//        }
       }
     }
   }
