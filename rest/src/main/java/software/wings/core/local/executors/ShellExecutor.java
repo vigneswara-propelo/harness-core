f8423d764ba512a4148db0469432fb6ef7a658a9
Puneet Saraswat <puneet.saraswat@harness.io>
2018-04-17 09:51:03 -0700

    Creating temporary script file to execute local shell script (#1429)

--- rest/src/main/java/software/wings/core/local/executors/ShellExecutor.java
+++ rest/src/main/java/software/wings/core/local/executors/ShellExecutor.java
@@ -1,5 +1,6 @@
 package software.wings.core.local.executors;
 
+import static io.harness.data.structure.EmptyPredicate.isEmpty;
 import static io.harness.govern.Switch.unhandled;
 import static java.lang.String.format;
 import static org.apache.commons.codec.binary.Base64.encodeBase64String;
@@ -19,12 +20,18 @@ import software.wings.beans.command.CommandExecutionResult.CommandExecutionStatu
 import software.wings.delegatetasks.DelegateLogService;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class ShellExecutor {
   private DelegateLogService logService;
   private ShellExecutorConfig config;
   private ScriptType scriptType;
+
+  private static final File defaultWorkingDirectory = new File(System.getProperty("java.io.tmpdir"));
 
   ShellExecutor(DelegateLogService logService, ShellExecutorConfig config, ScriptType scriptType) {
     this.logService = logService;
@@ -41,25 +48,41 @@ public class ShellExecutor {
 
     saveExecutionLog(format("Executing command ..."), INFO);
 
-    String[] commandList = null;
-
     switch (this.scriptType) {
       case BASH:
-        commandList = new String[] {"/bin/bash", "-c", bashWrappedCommand(command)};
+        try {
+          commandExecutionStatus = executeBashScript(command);
+        } catch (Exception e) {
+          saveExecutionLog(format("Exception: %s", e), ERROR, commandExecutionStatus);
+        }
         break;
 
       case POWERSHELL:
-        commandList = new String[] {"Powershell", "-c", psWrappedCommand(command)};
+        commandExecutionStatus = executePowerShellScript(command);
         break;
 
       default:
         unhandled(this.scriptType);
     }
 
-    try {
+    return commandExecutionStatus;
+  }
+
+  private CommandExecutionStatus executeBashScript(String command) throws IOException {
+    CommandExecutionStatus commandExecutionStatus = FAILURE;
+    File workingDirectory =
+        isEmpty(config.getWorkingDirectory()) ? defaultWorkingDirectory : new File(config.getWorkingDirectory());
+
+    String scriptFilename = "harness" + this.config.getExecutionId() + ".sh";
+    File scriptFile = new File(workingDirectory, scriptFilename);
+
+    try (FileOutputStream outputStream = new FileOutputStream(scriptFile)) {
+      outputStream.write(command.getBytes());
+
+      String[] commandList = new String[] {"/bin/bash", scriptFilename};
       ProcessExecutor processExecutor = new ProcessExecutor()
                                             .command(commandList)
-                                            .directory(new File(config.getWorkingDirectory()))
+                                            .directory(workingDirectory)
                                             .environment(config.getEnvironment())
                                             .readOutput(true)
                                             .redirectOutput(new LogOutputStream() {
@@ -77,13 +100,51 @@ public class ShellExecutor {
 
       ProcessResult processResult = processExecutor.execute();
       commandExecutionStatus = processResult.getExitValue() == 0 ? SUCCESS : FAILURE;
-
       saveExecutionLog(
           format("Command completed with ExitCode (%d)", processResult.getExitValue()), INFO, commandExecutionStatus);
     } catch (Exception e) {
-      commandExecutionStatus = FAILURE;
-      saveExecutionLog(format("Exception: %s", e.getCause()), ERROR, commandExecutionStatus);
+      saveExecutionLog(format("Exception: %s", e), ERROR, commandExecutionStatus);
+    } finally {
+      Files.deleteIfExists(Paths.get(scriptFile.getAbsolutePath()));
     }
+
+    return commandExecutionStatus;
+  }
+
+  private CommandExecutionStatus executePowerShellScript(String command) {
+    CommandExecutionStatus commandExecutionStatus = FAILURE;
+
+    File workingDirectory =
+        isEmpty(config.getWorkingDirectory()) ? defaultWorkingDirectory : new File(config.getWorkingDirectory());
+
+    String[] commandList = new String[] {"Powershell", "-c", psWrappedCommand(command)};
+    try {
+      ProcessExecutor processExecutor = new ProcessExecutor()
+                                            .command(commandList)
+                                            .directory(workingDirectory)
+                                            .environment(config.getEnvironment())
+                                            .readOutput(true)
+                                            .redirectOutput(new LogOutputStream() {
+                                              @Override
+                                              protected void processLine(String line) {
+                                                saveExecutionLog(line.trim(), INFO);
+                                              }
+                                            })
+                                            .redirectError(new LogOutputStream() {
+                                              @Override
+                                              protected void processLine(String line) {
+                                                saveExecutionLog(line.trim(), ERROR);
+                                              }
+                                            });
+
+      ProcessResult processResult = processExecutor.execute();
+      commandExecutionStatus = processResult.getExitValue() == 0 ? SUCCESS : FAILURE;
+      saveExecutionLog(
+          format("Command completed with ExitCode (%d)", processResult.getExitValue()), INFO, commandExecutionStatus);
+    } catch (Exception e) {
+      saveExecutionLog(format("Exception: %s", e), ERROR, commandExecutionStatus);
+    }
+
     return commandExecutionStatus;
   }
 
@@ -93,11 +154,6 @@ public class ShellExecutor {
         "$decoded = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String(\\\"%s\\\")); Invoke-Expression $decoded",
         base64Command);
     return String.format("Invoke-Command -command {%s}", wrappedCommand);
-  }
-
-  private String bashWrappedCommand(String command) {
-    String base64Command = encodeBase64String(command.getBytes(StandardCharsets.UTF_8));
-    return String.format("\"eval $(echo %s|base64 -d)\"", base64Command);
   }
 
   private void saveExecutionLog(String line, LogLevel level) {
