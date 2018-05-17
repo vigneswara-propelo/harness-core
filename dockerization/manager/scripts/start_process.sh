5cd87dcb6a2157544a7ea95e621cee6cb6036691
Rushabh Shah <rushabh.shah@harness.io>
2018-05-17 16:49:19 -0700

    adding support for launching manager in kubernetes (#1875)

--- dockerization/manager/scripts/start_process.sh
+++ dockerization/manager/scripts/start_process.sh
@@ -20,5 +20,8 @@ fi
 
 echo "Using new relic env " $NEWRELIC_ENV
 
-java -Xms${MEMORY}m -Xmx${MEMORY}m -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:mygclogfilename.gc -XX:+UseParallelGC -XX:MaxGCPauseMillis=500 -Dnewrelic.environment=$NEWRELIC_ENV -Dfile.encoding=UTF-8 -jar $CAPSULE_JAR /opt/harness/config.yml > /opt/harness/logs/portal.log 2>&1
-
+if [[ "${DEPLOY_MODE}" == "KUBERNETES" ]]; then
+    java -Xms${MEMORY}m -Xmx${MEMORY}m -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:mygclogfilename.gc -XX:+UseParallelGC -XX:MaxGCPauseMillis=500 -Dnewrelic.environment=$NEWRELIC_ENV -Dfile.encoding=UTF-8 -jar $CAPSULE_JAR /opt/harness/config.yml
+else
+    java -Xms${MEMORY}m -Xmx${MEMORY}m -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:mygclogfilename.gc -XX:+UseParallelGC -XX:MaxGCPauseMillis=500 -Dnewrelic.environment=$NEWRELIC_ENV -Dfile.encoding=UTF-8 -jar $CAPSULE_JAR /opt/harness/config.yml > /opt/harness/logs/portal.log 2>&1
+fi
