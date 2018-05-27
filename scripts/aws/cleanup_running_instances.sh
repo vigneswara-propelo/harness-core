023e5ec3d3261ae0e8732908188ded4093b9bb27
George Georgiev <george@harness.io>
2018-05-27 13:31:37 -0700

    Add aws cleanup script (#1964)

---
+++ scripts/aws/cleanup_running_instances.sh
@@ -0,0 +1,14 @@
+REGIONS=`aws ec2 describe-regions | grep RegionName | awk '{ print $2 }' | sed 's/.$//' | sed 's/^.//'`
+
+for REGION in $REGIONS
+do
+    echo $REGION
+    aws ec2 describe-instances --filter "Name=instance-state-name,Values=running" --region $REGION |\
+      grep InstanceId | awk '{ print $2 }' | sed 's/.\{2\}$//' | sed 's/^.//' |\
+      while read INSTANCE
+      do
+        echo "  $INSTANCE"
+        # aws ec2 modify-instance-attribute --region $REGION --no-disable-api-termination --instance-id=$INSTANCE
+        aws ec2 terminate-instances --region $REGION --instance-id=$INSTANCE
+      done
+done
