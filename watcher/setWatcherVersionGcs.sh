aa4310a7cf4da63ea23bb3bee9ef1c3c17966427
Brett Zane <brett@harness.io>
2018-05-29 22:51:46 -0700

    Set watcher version script for GCS (#1994)

---
+++ watcher/setWatcherVersionGcs.sh
@@ -0,0 +1,16 @@
+if [ $# -ne 3 ]
+then
+  echo "This script is used to set version on watchers."
+  echo "Usage: $0 <version> <environment> <logdnakey>"
+  echo "version: portal build number"
+  echo "environment: ci, qa, prod"
+  echo "logdnakey: key for logging against right logdna instance."
+  exit 1
+fi
+
+echo "System-Properties: version=1.0.${1} logdnakey=${3}" >> app.mf
+echo "Application-Version: version=1.0.${1}" >> app.mf
+jar ufm watcher-0.0.1-SNAPSHOT-capsule.jar app.mf
+rm -rf app.mf
+echo "1.0.${1} latest/watcher.jar" >> watcher${2}.txt
+mv watcher-0.0.1-SNAPSHOT-capsule.jar watcher.jar
