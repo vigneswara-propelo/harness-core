45457c635504f22b653d34cdc3493fc5d561387b
Anshul Anshul <anshul@harness.io>
2018-06-07 22:48:12 -0700

    Added test to verify that all error codes has corresponding response message properties (#2083)
    * Added test to verify that all error codes has corresponding response message properties

---
+++ rest/src/test/java/software/wings/common/ErrorCodePropertiesTest.java
@@ -0,0 +1,37 @@
+package software.wings.common;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import software.wings.beans.ErrorCode;
+import software.wings.exception.WingsException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class ErrorCodePropertiesTest {
+  private static final String RESPONSE_MESSAGE_FILE = "/response_messages.properties";
+
+  @Test
+  public void testErrorCodesInProperties() {
+    Properties messages = new Properties();
+    try (InputStream in = getClass().getResourceAsStream(RESPONSE_MESSAGE_FILE)) {
+      messages.load(in);
+    } catch (IOException exception) {
+      throw new WingsException(exception);
+    }
+
+    Set<String> errorCodeSet =
+        Arrays.stream(ErrorCode.values()).map(error -> error.toString()).collect(Collectors.toSet());
+    Set<String> propertiesSet =
+        messages.keySet().stream().map(message -> message.toString()).collect(Collectors.toSet());
+
+    // Assert that all errorCodes are defined in properties
+    // and each property should have ErrorCode enum
+    assertEquals(errorCodeSet, propertiesSet);
+  }
+}
