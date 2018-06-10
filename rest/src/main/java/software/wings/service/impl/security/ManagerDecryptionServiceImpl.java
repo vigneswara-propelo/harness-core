9d1231f5288e0f28396098aec3b773c9e2fdaba8
Brett Zane <brett@harness.io>
2018-06-09 19:08:34 -0700

    Add some retries to decryption sync tasks that happen in workflow execution (#2097)

--- rest/src/main/java/software/wings/service/impl/security/ManagerDecryptionServiceImpl.java
+++ rest/src/main/java/software/wings/service/impl/security/ManagerDecryptionServiceImpl.java
@@ -4,8 +4,12 @@ import static io.harness.data.structure.EmptyPredicate.isEmpty;
 import static software.wings.beans.DelegateTask.SyncTaskContext.Builder.aContext;
 import static software.wings.utils.WingsReflectionUtils.getFieldByName;
 
+import com.google.common.util.concurrent.TimeLimiter;
+import com.google.common.util.concurrent.UncheckedTimeoutException;
 import com.google.inject.Inject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import software.wings.annotation.Encryptable;
 import software.wings.beans.Base;
 import software.wings.beans.DelegateTask.SyncTaskContext;
@@ -18,30 +22,51 @@ import software.wings.service.intfc.security.ManagerDecryptionService;
 
 import java.lang.reflect.Field;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by rsingh on 6/7/18.
  */
 public class ManagerDecryptionServiceImpl implements ManagerDecryptionService {
-  @Inject protected DelegateProxyFactory delegateProxyFactory;
+  private static final Logger logger = LoggerFactory.getLogger(ManagerDecryptionServiceImpl.class);
+
+  @Inject private DelegateProxyFactory delegateProxyFactory;
+  @Inject private TimeLimiter timeLimiter;
+
   @Override
   public void decrypt(Encryptable object, List<EncryptedDataDetail> encryptedDataDetails) {
     if (isEmpty(encryptedDataDetails)) {
       return;
     }
-    SyncTaskContext syncTaskContext =
-        aContext().withAccountId(object.getAccountId()).withAppId(Base.GLOBAL_APP_ID).build();
+    SyncTaskContext syncTaskContext = aContext()
+                                          .withAccountId(object.getAccountId())
+                                          .withAppId(Base.GLOBAL_APP_ID)
+                                          .withTimeout(TimeUnit.SECONDS.toMillis(10L))
+                                          .build();
     try {
-      Encryptable decrypted =
-          delegateProxyFactory.get(EncryptionService.class, syncTaskContext).decrypt(object, encryptedDataDetails);
+      Encryptable decrypted = timeLimiter.callWithTimeout(() -> {
+        while (true) {
+          try {
+            return delegateProxyFactory.get(EncryptionService.class, syncTaskContext)
+                .decrypt(object, encryptedDataDetails);
+          } catch (Exception e) {
+            logger.warn("Error decrypting value. Retrying.");
+          }
+        }
+      }, 65, TimeUnit.SECONDS, true);
       for (EncryptedDataDetail encryptedDataDetail : encryptedDataDetails) {
         Field f = getFieldByName(object.getClass(), encryptedDataDetail.getFieldName());
-        f.setAccessible(true);
-        f.set(object, f.get(decrypted));
+        if (f != null) {
+          f.setAccessible(true);
+          f.set(object, f.get(decrypted));
+        }
       }
       object.setDecrypted(true);
+    } catch (UncheckedTimeoutException ex) {
+      logger.warn("Timed out decrypting value", ex);
+      throw new WingsException(ErrorCode.KMS_OPERATION_ERROR).addParam("reason", "Timed out decrypting value");
     } catch (Exception e) {
-      throw new WingsException(ErrorCode.KMS_OPERATION_ERROR).addParam("reason", e.getMessage());
+      throw new WingsException(ErrorCode.KMS_OPERATION_ERROR, e).addParam("reason", e.getMessage());
     }
   }
 }
