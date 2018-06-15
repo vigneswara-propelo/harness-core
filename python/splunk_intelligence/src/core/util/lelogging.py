d328fb8040b664233d6bbe0a1be1563e3747beb2
Sriram Parthasarathy <sriram@harness.io>
2018-06-15 14:49:05 -0700

    Le onprem 2 (#2163)
    * le on prem
    
    * le onprem

--- python/splunk_intelligence/src/core/util/lelogging.py
+++ python/splunk_intelligence/src/core/util/lelogging.py
@@ -5,11 +5,11 @@ key = '4ac03b05674fc5c488e3b9b235078d5d'
 log_format = "%(asctime)-15s %(levelname)s %(message)s"
 logging.basicConfig(level=logging.INFO, format=log_format)
 log = logging.getLogger("le")
-if os.environ.get('learning_env') and os.environ.get('learning_env') != 'ON_PREM':
+if os.environ.get('learning_env') and os.environ.get('learning_env') != 'ON_PREM' and os.environ.get('logdna_key'):
     from logdna import LogDNAHandler
     from CustomLogger import CustomLogHandler
     options = {'index_meta': True, 'app': 'Learning_Engine_' + os.environ.get('learning_env')}
-    test = LogDNAHandler(key, options)
+    test = LogDNAHandler(os.environ.get('logdna_key'), options)
     clog_handler = CustomLogHandler(test)
     # clogHandler.setFormatter(log_format)
     # clogHandler.setLevel(logging.INFO)
