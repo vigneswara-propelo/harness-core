007e238d04ffa9fab96e2f1fac4a0dc395832683
Brett Zane <brett@harness.io>
2018-06-17 01:14:38 -0700

    Run migrations after startup (#2178)

--- rest/src/main/java/migrations/MigrationList.java
+++ rest/src/main/java/migrations/MigrationList.java
@@ -2,27 +2,17 @@ package migrations;
 
 import com.google.common.collect.ImmutableList;
 
-import migrations.all.AddUsersToAdminUserGroup;
 import migrations.all.AddValidUntilToActivity;
 import migrations.all.AddValidUntilToAlert;
+import migrations.all.AddValidUntilToCommandLog;
 import migrations.all.AddValidUntilToDelegateTask;
 import migrations.all.EntityNameValidationMigration_All_00;
-import migrations.all.FixInstanceData;
-import migrations.all.FixInstanceDataForAwsSSH;
-import migrations.all.GitSyncToAllAccounts;
 import migrations.all.LearningEngineTaskGroupNameMigration;
-import migrations.all.LogAnalysisExperimentalRecordsMigration;
 import migrations.all.MetricAnalysisRecordGroupNameMigration;
 import migrations.all.MetricDataRecordGroupNameMigration;
 import migrations.all.MetricMLAnalysisRecordGroupNameMigration;
-import migrations.all.NewRelicMetricAnalysisRecordsMigration;
 import migrations.all.NewRelicMetricDataGroupNameMigration;
-import migrations.all.NewRelicMetricDataRecordsMigration;
 import migrations.all.RemoveResizeFromStatefulSetWorkflows;
-import migrations.all.RenameProvisionNodeToInfrastructureNodeWorkflows;
-import migrations.all.SetRollbackFlagToWorkflows;
-import migrations.all.TimeSeriesAnalysisRecordsMigration;
-import migrations.all.TimeSeriesMLScoresMigration;
 import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.List;
@@ -35,17 +25,6 @@ public class MigrationList {
    */
   public static List<Pair<Integer, Class<? extends Migration>>> getMigrations() {
     return new ImmutableList.Builder<Pair<Integer, Class<? extends Migration>>>()
-        .add(Pair.of(130, FixInstanceData.class))
-        .add(Pair.of(131, LogAnalysisExperimentalRecordsMigration.class))
-        .add(Pair.of(132, FixInstanceDataForAwsSSH.class))
-        .add(Pair.of(133, NewRelicMetricAnalysisRecordsMigration.class))
-        .add(Pair.of(134, NewRelicMetricDataRecordsMigration.class))
-        .add(Pair.of(135, TimeSeriesAnalysisRecordsMigration.class))
-        .add(Pair.of(136, TimeSeriesMLScoresMigration.class))
-        .add(Pair.of(137, SetRollbackFlagToWorkflows.class))
-        .add(Pair.of(138, AddUsersToAdminUserGroup.class))
-        .add(Pair.of(139, RenameProvisionNodeToInfrastructureNodeWorkflows.class))
-        .add(Pair.of(140, GitSyncToAllAccounts.class))
         .add(Pair.of(141, RemoveResizeFromStatefulSetWorkflows.class))
         .add(Pair.of(142, MetricDataRecordGroupNameMigration.class))
         .add(Pair.of(143, MetricAnalysisRecordGroupNameMigration.class))
@@ -58,6 +37,7 @@ public class MigrationList {
         .add(Pair.of(150, AddValidUntilToDelegateTask.class))
         .add(Pair.of(151, AddValidUntilToActivity.class))
         .add(Pair.of(152, BaseMigration.class))
+        .add(Pair.of(153, AddValidUntilToCommandLog.class))
         .build();
   }
 }
