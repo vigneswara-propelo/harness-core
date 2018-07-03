eee38e59ee5c09808e951070d35210105f88a00c
Puneet Saraswat <puneet.saraswat@harness.io>
2018-07-03 11:00:54 -0700

    Refactoring DelegateQueueTask logic to not deserialize the full object (#2408)

--- rest/src/main/java/software/wings/app/DelegateQueueTask.java
+++ rest/src/main/java/software/wings/app/DelegateQueueTask.java
@@ -2,7 +2,7 @@ package software.wings.app;
 
 import static io.harness.data.structure.EmptyPredicate.isNotEmpty;
 import static java.util.stream.Collectors.toList;
-import static org.apache.commons.lang3.StringUtils.isBlank;
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
 import static software.wings.core.maintenance.MaintenanceController.isMaintenance;
 import static software.wings.dl.HQuery.excludeAuthority;
 import static software.wings.exception.WingsException.ExecutionContext.MANAGER;
@@ -10,11 +10,13 @@ import static software.wings.service.impl.DelegateServiceImpl.VALIDATION_TIMEOUT
 
 import com.google.inject.Inject;
 
+import io.harness.version.VersionInfoManager;
 import org.atmosphere.cpr.BroadcasterFactory;
 import org.mongodb.morphia.Key;
 import org.mongodb.morphia.mapping.Mapper;
 import org.mongodb.morphia.query.Query;
 import org.mongodb.morphia.query.UpdateOperations;
+import org.mongodb.morphia.query.WhereCriteria;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import software.wings.beans.DelegateTask;
@@ -28,7 +30,6 @@ import software.wings.waitnotify.WaitNotifyEngine;
 
 import java.time.Clock;
 import java.time.Duration;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -45,6 +46,7 @@ public class DelegateQueueTask implements Runnable {
   @Inject private BroadcasterFactory broadcasterFactory;
   @Inject private WaitNotifyEngine waitNotifyEngine;
   @Inject private Clock clock;
+  @Inject private VersionInfoManager versionInfoManager;
 
   /* (non-Javadoc)
    * @see java.lang.Runnable#run()
@@ -57,163 +59,119 @@ public class DelegateQueueTask implements Runnable {
 
     try (AcquiredLock ignore = persistentLocker.acquireLock(
              DelegateQueueTask.class, DelegateQueueTask.class.getName(), Duration.ofMinutes(1))) {
-      // Release async tasks acquired by delegate but not started execution. Introduce "ACQUIRED" status may be ?
-      Query<DelegateTask> releaseLongQueuedTasks = wingsPersistence.createQuery(DelegateTask.class, excludeAuthority)
-                                                       .filter("status", Status.QUEUED)
-                                                       .filter("async", true)
-                                                       .field("delegateId")
-                                                       .exists()
-                                                       .field("lastUpdatedAt")
-                                                       .lessThan(clock.millis() - TimeUnit.MINUTES.toMillis(2));
-
-      wingsPersistence.update(
-          releaseLongQueuedTasks, wingsPersistence.createUpdateOperations(DelegateTask.class).unset("delegateId"));
-
-      // Release sync tasks acquired by delegate but not started execution.
-      releaseLongQueuedTasks = wingsPersistence.createQuery(DelegateTask.class, excludeAuthority)
-                                   .filter("status", Status.QUEUED)
-                                   .filter("async", false)
-                                   .field("delegateId")
-                                   .exists()
-                                   .field("lastUpdatedAt")
-                                   .lessThan(clock.millis() - TimeUnit.SECONDS.toMillis(10));
-
-      wingsPersistence.update(
-          releaseLongQueuedTasks, wingsPersistence.createUpdateOperations(DelegateTask.class).unset("delegateId"));
-
-      // Find tasks which are timed out and update their status to FAILED.
-      List<DelegateTask> longRunningTimedOutTasks = new ArrayList<>();
-      try {
-        longRunningTimedOutTasks = wingsPersistence.createQuery(DelegateTask.class, excludeAuthority)
-                                       .filter("status", Status.STARTED)
-                                       .asList()
-                                       .stream()
-                                       .filter(DelegateTask::isTimedOut)
-                                       .collect(toList());
-      } catch (com.esotericsoftware.kryo.KryoException kryo) {
-        logger.warn("Delegate task schema backwards incompatibility", kryo);
-        for (Key<DelegateTask> key : wingsPersistence.createQuery(DelegateTask.class, excludeAuthority)
-                                         .filter("status", Status.STARTED)
-                                         .asKeyList()) {
-          try {
-            wingsPersistence.get(DelegateTask.class, key.getId().toString());
-          } catch (com.esotericsoftware.kryo.KryoException ex) {
-            wingsPersistence.delete(DelegateTask.class, key.getId().toString());
-          }
-        }
-      }
-
-      if (!longRunningTimedOutTasks.isEmpty()) {
-        logger.info("Found {} long running tasks, to be killed", longRunningTimedOutTasks.size());
-        longRunningTimedOutTasks.forEach(delegateTask -> {
-          Query<DelegateTask> updateQuery = wingsPersistence.createQuery(DelegateTask.class, excludeAuthority)
-                                                .filter("status", Status.STARTED)
-                                                .filter(Mapper.ID_KEY, delegateTask.getUuid());
-          UpdateOperations<DelegateTask> updateOperations =
-              wingsPersistence.createUpdateOperations(DelegateTask.class).set("status", Status.ERROR);
-
-          DelegateTask updatedDelegateTask =
-              wingsPersistence.getDatastore().findAndModify(updateQuery, updateOperations);
-
-          if (updatedDelegateTask == null) {
-            logger.info("Long running delegate task {} could not be updated to ERROR status", delegateTask.getUuid());
-          } else if (isBlank(updatedDelegateTask.getWaitId())) {
-            logger.info("Long running delegate task {} with type {} has no wait ID. Nothing to notify",
-                delegateTask.getUuid(), delegateTask.getTaskType());
-          } else {
-            logger.info("Long running delegate task {} is terminated", updatedDelegateTask.getUuid());
-            waitNotifyEngine.notify(updatedDelegateTask.getWaitId(),
-                ErrorNotifyResponseData.builder()
-                    .errorMessage("Delegate timeout. Delegate ID: " + updatedDelegateTask.getDelegateId())
-                    .build());
-          }
-        });
-      }
-
-      // Find tasks which have been queued for too long and update their status to ERROR.
-      List<DelegateTask> queuedTimedOutTasks = new ArrayList<>();
-      try {
-        queuedTimedOutTasks = wingsPersistence.createQuery(DelegateTask.class, excludeAuthority)
-                                  .filter("status", Status.QUEUED)
-                                  .field("lastUpdatedAt")
-                                  .lessThan(clock.millis() - TimeUnit.HOURS.toMillis(1))
-                                  .asList();
-      } catch (com.esotericsoftware.kryo.KryoException kryo) {
-        logger.warn("Delegate task schema backwards incompatibility", kryo);
-        for (Key<DelegateTask> key : wingsPersistence.createQuery(DelegateTask.class, excludeAuthority)
-                                         .filter("status", Status.QUEUED)
-                                         .asKeyList()) {
-          try {
-            wingsPersistence.get(DelegateTask.class, key.getId().toString());
-          } catch (com.esotericsoftware.kryo.KryoException ex) {
-            wingsPersistence.delete(DelegateTask.class, key.getId().toString());
-          }
-        }
-      }
-
-      if (!queuedTimedOutTasks.isEmpty()) {
-        logger.info("Found {} long queued tasks, to be killed", queuedTimedOutTasks.size());
-        queuedTimedOutTasks.forEach(delegateTask -> {
-          Query<DelegateTask> updateQuery = wingsPersistence.createQuery(DelegateTask.class, excludeAuthority)
-                                                .filter("status", Status.QUEUED)
-                                                .filter(Mapper.ID_KEY, delegateTask.getUuid());
-          UpdateOperations<DelegateTask> updateOperations =
-              wingsPersistence.createUpdateOperations(DelegateTask.class).set("status", Status.ERROR);
-
-          DelegateTask updatedDelegateTask =
-              wingsPersistence.getDatastore().findAndModify(updateQuery, updateOperations);
-
-          if (updatedDelegateTask == null) {
-            logger.error("Queued delegate task {} could not be updated to ERROR status", delegateTask.getUuid());
-          } else if (isBlank(updatedDelegateTask.getWaitId())) {
-            logger.error("Queued delegate task {} with type {} has no wait ID. Nothing to notify",
-                delegateTask.getUuid(), delegateTask.getTaskType());
-          } else {
-            logger.info("Queued delegate task {} is terminated", updatedDelegateTask.getUuid());
-            waitNotifyEngine.notify(updatedDelegateTask.getWaitId(),
-                ErrorNotifyResponseData.builder().errorMessage("Task queued too long").build());
-          }
-        });
-      }
-
-      // Re-broadcast queued tasks not picked up by any Delegate and not in process of validation
-      List<DelegateTask> unassignedTasks = null;
-      try {
-        unassignedTasks = wingsPersistence.createQuery(DelegateTask.class, excludeAuthority)
-                              .filter("status", Status.QUEUED)
-                              .field("delegateId")
-                              .doesNotExist()
-                              .asList();
-      } catch (com.esotericsoftware.kryo.KryoException kryo) {
-        logger.warn("Delegate task schema backwards incompatibility", kryo);
-        for (Key<DelegateTask> key : wingsPersistence.createQuery(DelegateTask.class, excludeAuthority)
-                                         .filter("status", Status.QUEUED)
-                                         .field("delegateId")
-                                         .doesNotExist()
-                                         .asKeyList()) {
-          try {
-            wingsPersistence.get(DelegateTask.class, key.getId().toString());
-          } catch (com.esotericsoftware.kryo.KryoException ex) {
-            wingsPersistence.delete(DelegateTask.class, key.getId().toString());
-          }
-        }
-      }
-
-      if (isNotEmpty(unassignedTasks)) {
-        long now = clock.millis();
-        unassignedTasks.forEach(delegateTask -> {
-          if (delegateTask.getValidationStartedAt() == null
-              || now - delegateTask.getValidationStartedAt() > VALIDATION_TIMEOUT) {
-            logger.info("Re-broadcast queued task [{}]", delegateTask.getUuid());
-            broadcasterFactory.lookup("/stream/delegate/" + delegateTask.getAccountId(), true).broadcast(delegateTask);
-          }
-        });
-      }
-
+      releaseLongQueuedTasks();
+      markTimedOutTasksAsFailed();
+      markLongQueuedTasksAsFailed();
+      rebroadcastUnassignedTasks();
     } catch (WingsException exception) {
       exception.logProcessedMessages(MANAGER, logger);
     } catch (Exception exception) {
       logger.error("Error seen in the Notifier call", exception);
+    }
+  }
+
+  private void releaseLongQueuedTasks() {
+    // Release async tasks acquired by delegate but not started execution. Introduce "ACQUIRED" status may be ?
+    Query<DelegateTask> releaseLongQueuedTasks = wingsPersistence.createQuery(DelegateTask.class, excludeAuthority)
+                                                     .filter("status", Status.QUEUED)
+                                                     .filter("async", true)
+                                                     .field("delegateId")
+                                                     .exists()
+                                                     .field("lastUpdatedAt")
+                                                     .lessThan(clock.millis() - TimeUnit.MINUTES.toMillis(2));
+
+    wingsPersistence.update(
+        releaseLongQueuedTasks, wingsPersistence.createUpdateOperations(DelegateTask.class).unset("delegateId"));
+
+    // Release sync tasks acquired by delegate but not started execution.
+    releaseLongQueuedTasks = wingsPersistence.createQuery(DelegateTask.class, excludeAuthority)
+                                 .filter("status", Status.QUEUED)
+                                 .filter("async", false)
+                                 .field("delegateId")
+                                 .exists()
+                                 .field("lastUpdatedAt")
+                                 .lessThan(clock.millis() - TimeUnit.SECONDS.toMillis(10));
+
+    wingsPersistence.update(
+        releaseLongQueuedTasks, wingsPersistence.createUpdateOperations(DelegateTask.class).unset("delegateId"));
+  }
+
+  private void markTimedOutTasksAsFailed() {
+    long currentTime = clock.millis();
+    Query<DelegateTask> longRunningTimedOutTasksQuery =
+        wingsPersistence.createQuery(DelegateTask.class, excludeAuthority).filter("status", Status.STARTED);
+    longRunningTimedOutTasksQuery.and(new WhereCriteria("this.lastUpdatedAt + this.timeout < " + currentTime));
+
+    List<Key<DelegateTask>> longRunningTimedOutTaskKeys = longRunningTimedOutTasksQuery.asKeyList();
+
+    if (!longRunningTimedOutTaskKeys.isEmpty()) {
+      List<String> keyList = longRunningTimedOutTaskKeys.stream().map(key -> key.getId().toString()).collect(toList());
+      logger.info("Marking following timed out tasks as failed [{}]", keyList);
+      markTasksAsFailed(keyList);
+    }
+  }
+
+  private void markLongQueuedTasksAsFailed() {
+    // Find tasks which have been queued for too long and update their status to ERROR.
+    Query<DelegateTask> longQueuedTasksQuery = wingsPersistence.createQuery(DelegateTask.class, excludeAuthority)
+                                                   .filter("status", Status.QUEUED)
+                                                   .field("lastUpdatedAt")
+                                                   .lessThan(clock.millis() - TimeUnit.HOURS.toMillis(1));
+
+    List<Key<DelegateTask>> longQueuedTaskKeys = longQueuedTasksQuery.asKeyList();
+
+    if (!longQueuedTaskKeys.isEmpty()) {
+      List<String> keyList = longQueuedTaskKeys.stream().map(key -> key.getId().toString()).collect(toList());
+      logger.info("Marking following long queued tasks as failed [{}]", keyList);
+      markTasksAsFailed(keyList);
+    }
+  }
+
+  private void markTasksAsFailed(List<String> taskIds) {
+    Query<DelegateTask> updateQuery =
+        wingsPersistence.createQuery(DelegateTask.class, excludeAuthority).field(Mapper.ID_KEY).in(taskIds);
+    UpdateOperations<DelegateTask> updateOperations =
+        wingsPersistence.createUpdateOperations(DelegateTask.class).set("status", Status.ERROR);
+    wingsPersistence.update(updateQuery, updateOperations);
+
+    List<DelegateTask> delegateTasks = wingsPersistence.createQuery(DelegateTask.class, excludeAuthority)
+                                           .field(Mapper.ID_KEY)
+                                           .in(taskIds)
+                                           .project(Mapper.ID_KEY, true)
+                                           .project("delegateId", true)
+                                           .project("waitId", true)
+                                           .asList();
+
+    delegateTasks.forEach(delegateTask -> {
+      if (isNotBlank(delegateTask.getWaitId())) {
+        logger.info("Delegate task {} is terminated", delegateTask.getUuid());
+        waitNotifyEngine.notify(delegateTask.getWaitId(),
+            ErrorNotifyResponseData.builder()
+                .errorMessage("Delegate timeout. Delegate ID: " + delegateTask.getDelegateId())
+                .build());
+      }
+    });
+  }
+
+  void rebroadcastUnassignedTasks() {
+    // Re-broadcast queued tasks not picked up by any Delegate and not in process of validation
+    List<DelegateTask> unassignedTasks = null;
+    unassignedTasks = wingsPersistence.createQuery(DelegateTask.class, excludeAuthority)
+                          .filter("status", Status.QUEUED)
+                          .filter("version", versionInfoManager.getVersionInfo().getVersion())
+                          .field("delegateId")
+                          .doesNotExist()
+                          .asList();
+
+    if (isNotEmpty(unassignedTasks)) {
+      long now = clock.millis();
+      unassignedTasks.forEach(delegateTask -> {
+        if (delegateTask.getValidationStartedAt() == null
+            || now - delegateTask.getValidationStartedAt() > VALIDATION_TIMEOUT) {
+          logger.info("Re-broadcast queued task [{}]", delegateTask.getUuid());
+          broadcasterFactory.lookup("/stream/delegate/" + delegateTask.getAccountId(), true).broadcast(delegateTask);
+        }
+      });
     }
   }
 }
