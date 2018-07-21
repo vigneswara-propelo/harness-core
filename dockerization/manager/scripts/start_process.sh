143e6de5467c3b7d5f9b23ad6e1ee9683d03a68a
Rushabh Shah <rushabh.shah@harness.io>
2018-07-20 22:03:51 -0700

    adding support for appdynamics in manager-docker image (#2664)

--- dockerization/manager/scripts/start_process.sh
+++ dockerization/manager/scripts/start_process.sh
@@ -18,10 +18,21 @@ if [[ -z "$NEWRELIC_ENV" ]]; then
    export NEWRELIC_ENV=dev
 fi
 
-echo "Using new relic env " $NEWRELIC_ENV
+export JAVA_OPTS="-Xms${MEMORY}m -Xmx${MEMORY}m -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:mygclogfilename.gc -XX:+UseParallelGC -XX:MaxGCPauseMillis=500 -Dfile.encoding=UTF-8"
+
+if [[ "${ENABLE_APPDYNAMICS}" == "true" ]] && [[ "${DISABLE_NEW_RELIC}" == "true" ]]; then
+    JAVA_OPTS=$JAVA_OPTS" -javaagent:/opt/harness/javaagent.jar"
+    echo "Using Appdynamics java agent"
+fi
+
+if [[ "${DISABLE_NEW_RELIC}" != "true" ]]; then
+    JAVA_OPTS=$JAVA_OPTS" -Dnewrelic.environment=$NEWRELIC_ENV"
+    echo "Using new relic env " $NEWRELIC_ENV
+fi
+
 
 if [[ "${DEPLOY_MODE}" == "KUBERNETES" ]]; then
-    java -Xms${MEMORY}m -Xmx${MEMORY}m -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:mygclogfilename.gc -XX:+UseParallelGC -XX:MaxGCPauseMillis=500 -Dnewrelic.environment=$NEWRELIC_ENV -Dfile.encoding=UTF-8 -jar $CAPSULE_JAR /opt/harness/config.yml
+    java $JAVA_OPTS -jar $CAPSULE_JAR /opt/harness/config.yml
 else
-    java -Xms${MEMORY}m -Xmx${MEMORY}m -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:mygclogfilename.gc -XX:+UseParallelGC -XX:MaxGCPauseMillis=500 -Dnewrelic.environment=$NEWRELIC_ENV -Dfile.encoding=UTF-8 -jar $CAPSULE_JAR /opt/harness/config.yml > /opt/harness/logs/portal.log 2>&1
+    java $JAVA_OPTS -jar $CAPSULE_JAR /opt/harness/config.yml > /opt/harness/logs/portal.log 2>&1
 fi
