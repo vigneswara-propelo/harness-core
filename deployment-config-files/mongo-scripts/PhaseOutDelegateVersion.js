040e7aa0e08851c8ece29c1d97fb72512ad60e72
Puneet Saraswat <puneet.saraswat@harness.io>
2018-07-30 17:29:44 -0700

    Updating Delegate publish scripts to always preserve PrimaryVersion (#2770)
    * Updating Delegate pulish scripts to always preserve PrimaryVersion

--- deployment-config-files/mongo-scripts/PhaseOutDelegateVersion.js
+++ deployment-config-files/mongo-scripts/PhaseOutDelegateVersion.js
@@ -1,10 +1,15 @@
+const managerConfigQuery = { _id: "__GLOBAL_CONFIG_ID__" };
+const managerConfig = db.managerConfiguration.find(managerConfigQuery);
+const primaryVersion = managerConfig.next().primaryVersion;
+print("Manager primary version is");
+printjson(primaryVersion);
+
 const accountQuery = { _id: _accountId_ };
 const newValues = {
-    $pull: {
-        "delegateConfiguration.delegateVersions": _version_
+    $set: {
+        "delegateConfiguration.delegateVersions": [primaryVersion]
     }
 };
-print("Phasing out delegate version: " + _version_ + " for Account: " + _accountId_);
 const res = db.accounts.findAndModify({ query: accountQuery, update: newValues });
 const account = db.accounts.find(accountQuery);
 print("Published Delegate Versions");
--- deployment-config-files/mongo-scripts/PublishDelegateVersion.js
+++ deployment-config-files/mongo-scripts/PublishDelegateVersion.js
@@ -1,10 +1,16 @@
+const managerConfigQuery = { _id: "__GLOBAL_CONFIG_ID__" };
+const managerConfig = db.managerConfiguration.find(managerConfigQuery);
+const primaryVersion = managerConfig.next().primaryVersion;
+print("Manager primary version is");
+printjson(primaryVersion);
+
 const accountQuery = { _id: _accountId_ };
 const newValues = {
-    $addToSet: {
-        "delegateConfiguration.delegateVersions": _version_
+    $set: {
+        "delegateConfiguration.delegateVersions": [primaryVersion, _version_]
     }
 };
-print("Publishing delegate version: " + _version_ + " for Account: " + _accountId_);
+print("Publishing new delegate version: " + _version_ + " for Account: " + _accountId_);
 const res = db.accounts.findAndModify({ query: accountQuery, update: newValues });
 const account = db.accounts.find(accountQuery);
 print("Published delegate Versions");
