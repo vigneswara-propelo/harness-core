0daf05a79e8be109f3b2ecace693ba63358e2d0c
Brett Zane <brett@harness.io>
2018-08-12 17:25:51 -0700

    Sleep 10 seconds between java process checks (#2936)

--- dockerization/delegate/scripts/run.sh
+++ dockerization/delegate/scripts/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 bash ./modify_scripts.sh
-bash ./start.sh && while [[ $(ps -ef|grep java|wc -l) -gt 1 ]]; do sleep 10000; done
+bash ./start.sh && while [[ $(ps -ef|grep java|wc -l) -gt 1 ]]; do sleep 10s; done
