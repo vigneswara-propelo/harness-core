2ff2ef4d8f9f306206d80da249a030ccc938b535
Parnian Zargham <parnian@harness.io>
2018-08-21 10:47:41 -0700

    saving le logs in a file for on prem (#3004)
    * saving le logs in a file for on prem
    
    * removed set level
    
    * fixed if condition
    
    * add log dir
    
    * set log size to 8MB

--- python/splunk_intelligence/src/core/util/lelogging.py
+++ python/splunk_intelligence/src/core/util/lelogging.py
@@ -1,11 +1,12 @@
 import logging
+from logging.handlers import RotatingFileHandler
 import os
 key = '4ac03b05674fc5c488e3b9b235078d5d'
 
 log_format = "%(asctime)-15s %(levelname)s %(message)s"
 logging.basicConfig(level=logging.INFO, format=log_format)
 log = logging.getLogger("le")
-if os.environ.get('learning_env') and os.environ.get('learning_env') != 'ON_PREM' and os.environ.get('logdna_key'):
+if os.environ.get('learning_env') and str(os.environ.get('learning_env')).lower() != 'on_prem' and os.environ.get('logdna_key'):
     from logdna import LogDNAHandler
     from CustomLogger import CustomLogHandler
     options = {'index_meta': True, 'app': 'Learning_Engine_' + os.environ.get('learning_env')}
@@ -14,6 +15,11 @@ if os.environ.get('learning_env') and os.environ.get('learning_env') != 'ON_PREM
     # clogHandler.setFormatter(log_format)
     # clogHandler.setLevel(logging.INFO)
     log.addHandler(clog_handler)
+elif str(os.environ.get('learning_env')).lower() == 'on_prem':
+    handler = RotatingFileHandler("logs/le_log", maxBytes=8388608, backupCount=10)
+    handler.setFormatter(logging.Formatter(log_format))
+    log.addHandler(handler)
+
 
 def get_log(name):
     return log
