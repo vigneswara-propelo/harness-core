73d143d97f8f253f89052effbe34eda8628bd5d0
Anubhaw Srivastava <anubhaw@harness.io>
2018-08-21 13:49:06 -0700

    Add migration for Template gallery seed templates (#3031)
    * Add migration for Template gallery seed templates
    
    * Add check for existing account with gallery
    
    * check style warning
    
    * Add root gallery if needed

--- rest/src/main/java/migrations/SeedDataMigrationList.java
+++ rest/src/main/java/migrations/SeedDataMigrationList.java
@@ -2,6 +2,7 @@ package migrations;
 
 import com.google.common.collect.ImmutableList;
 
+import migrations.seedata.TemplateGalleryDefaultTemplatesMigration;
 import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.List;
@@ -10,6 +11,7 @@ public class SeedDataMigrationList {
   public static List<Pair<Integer, Class<? extends SeedDataMigration>>> getMigrations() {
     return new ImmutableList.Builder<Pair<Integer, Class<? extends SeedDataMigration>>>()
         .add(Pair.of(1, BaseSeedDataMigration.class))
+        .add(Pair.of(2, TemplateGalleryDefaultTemplatesMigration.class))
         .build();
   }
 }
---
+++ rest/src/main/java/migrations/seedata/TemplateGalleryDefaultTemplatesMigration.java
@@ -0,0 +1,73 @@
+package migrations.seedata;
+
+import static software.wings.beans.Base.GLOBAL_ACCOUNT_ID;
+import static software.wings.common.TemplateConstants.HARNESS_GALLERY;
+
+import com.google.inject.Inject;
+
+import migrations.SeedDataMigration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import software.wings.beans.Account;
+import software.wings.beans.template.TemplateGallery;
+import software.wings.dl.HIterator;
+import software.wings.dl.WingsPersistence;
+import software.wings.service.intfc.template.TemplateGalleryService;
+
+/**
+ * Created by anubhaw on 8/20/18.
+ */
+public class TemplateGalleryDefaultTemplatesMigration implements SeedDataMigration {
+  private static final Logger logger = LoggerFactory.getLogger(TemplateGalleryDefaultTemplatesMigration.class);
+  @Inject private TemplateGalleryService templateGalleryService;
+  @Inject private WingsPersistence wingsPersistence;
+
+  @Override
+  public void migrate() {
+    try {
+      boolean rootTemplateGalleryDoesNotExist = wingsPersistence.createQuery(TemplateGallery.class)
+                                                    .field(TemplateGallery.NAME_KEY)
+                                                    .equal(HARNESS_GALLERY)
+                                                    .field("accountId")
+                                                    .equal(GLOBAL_ACCOUNT_ID)
+                                                    .getKey()
+          == null;
+      if (rootTemplateGalleryDoesNotExist) {
+        logger.error("TemplateGalleryDefaultTemplatesMigration root template gallery not found");
+        templateGalleryService.loadHarnessGallery(); // takes care of copying templates to individual accounts
+      } else {
+        // in case previous migration failed while copying Harness templates
+        copyHarnessTemplateToAccounts();
+      }
+    } catch (Exception ex) {
+      logger.error("TemplateGalleryDefaultTemplatesMigration failed", ex);
+    }
+  }
+
+  private void copyHarnessTemplateToAccounts() {
+    try (HIterator<Account> records = new HIterator<>(wingsPersistence.createQuery(Account.class).fetch())) {
+      while (records.hasNext()) {
+        Account account = records.next();
+        try {
+          logger.info("TemplateGalleryDefaultTemplatesMigration started for account [{}]", account.getUuid());
+          boolean templateGalleryDoesNotExist = wingsPersistence.createQuery(TemplateGallery.class)
+                                                    .field(TemplateGallery.NAME_KEY)
+                                                    .equal(account.getAccountName())
+                                                    .field("accountId")
+                                                    .equal(account.getUuid())
+                                                    .getKey()
+              == null;
+          if (templateGalleryDoesNotExist) {
+            templateGalleryService.copyHarnessTemplatesToAccount(account.getUuid(), account.getAccountName());
+          } else {
+            logger.info("TemplateGalleryDefaultTemplatesMigration gallery already exists for account [{}]. do nothing",
+                account.getUuid());
+          }
+          logger.info("TemplateGalleryDefaultTemplatesMigration finished for account [{}]", account.getUuid());
+        } catch (Exception ex) {
+          logger.error("TemplateGalleryDefaultTemplatesMigration failed for account [{}]", account.getUuid(), ex);
+        }
+      }
+    }
+  }
+}
