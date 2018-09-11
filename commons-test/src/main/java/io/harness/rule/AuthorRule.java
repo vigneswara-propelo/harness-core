5547d061a29a074cfdb593637e1f64b97ab0dde3
George Georgiev <george@harness.io>
2018-09-11 11:27:15 -0700

    Add email for author annotation (#3211)
    * Add email for author annotation

--- commons-test/src/main/java/io/harness/rule/AuthorRule.java
+++ commons-test/src/main/java/io/harness/rule/AuthorRule.java
@@ -3,6 +3,8 @@ package io.harness.rule;
 import io.harness.rule.RepeatRule.RepeatStatement.RepeatStatementBuilder;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -10,10 +12,13 @@ import java.lang.annotation.Target;
 import java.util.Objects;
 
 public class AuthorRule extends RepeatRule {
+  private static final Logger logger = LoggerFactory.getLogger(AuthorRule.class);
+
   @Retention(RetentionPolicy.RUNTIME)
   @Target({java.lang.annotation.ElementType.METHOD})
   public @interface Author {
-    String name();
+    String name() default "";
+    String email();
   }
 
   @Override
@@ -21,11 +26,28 @@ public class AuthorRule extends RepeatRule {
     final RepeatStatementBuilder builder = RepeatRule.RepeatStatement.builder().statement(statement).parentRule(this);
 
     Author author = description.getAnnotation(Author.class);
-    final String ghprbActualCommitAuthor = System.getenv("ghprbActualCommitAuthor");
-
-    if (author != null && Objects.equals(author.name(), ghprbActualCommitAuthor)) {
-      return builder.times(20).successes(20).timeoutOnly(true).build();
+    if (author == null) {
+      return statement;
     }
-    return statement;
+
+    // If there is email, it should match
+    if (!author.email().isEmpty()) {
+      final String ghprbActualCommitAuthorEmail = System.getenv("ghprbActualCommitAuthorEmail");
+      logger.info("ghprbActualCommitAuthorEmail = {}", ghprbActualCommitAuthorEmail);
+
+      if (!Objects.equals(author.email(), ghprbActualCommitAuthorEmail)) {
+        return statement;
+      }
+    } else if (!author.name().isEmpty()) {
+      final String ghprbActualCommitAuthor = System.getenv("ghprbActualCommitAuthor");
+      logger.info("ghprbActualCommitAuthor = {}", ghprbActualCommitAuthor);
+
+      if (!Objects.equals(author.name(), ghprbActualCommitAuthor)) {
+        return statement;
+      }
+    } else {
+      throw new RuntimeException("Either author email or name should be set");
+    }
+    return builder.times(20).successes(20).timeoutOnly(true).build();
   }
   }
