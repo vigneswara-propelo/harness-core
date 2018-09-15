f4b12cc8a5d283a834772fc18258d306fdf0c6e5
Puneet Saraswat <puneet.saraswat@harness.io>
2018-09-14 17:19:58 -0700

    Adding Delegate Connection Integration Test (#3277)

--- rest/src/test/java/software/wings/integration/DelegateRegistrationIntegrationTest.java
+++ rest/src/test/java/software/wings/integration/DelegateRegistrationIntegrationTest.java
@@ -7,6 +7,7 @@ import org.awaitility.Duration;
 import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 import software.wings.beans.Delegate;
+import software.wings.beans.DelegateConnection;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -21,6 +22,17 @@ public class DelegateRegistrationIntegrationTest extends BaseIntegrationTest {
       List<Delegate> delegates = wingsPersistence.createQuery(Delegate.class, excludeAuthority).asList();
       boolean connected = delegates.stream().anyMatch(Delegate::isConnected);
       logger.info("isDelegateConnected = {}", connected);
+      return connected;
+    }, CoreMatchers.is(true));
+  }
+
+  @Test
+  public void shouldWaitForADelegateConnectionsToAppear() {
+    await().with().pollInterval(Duration.ONE_SECOND).timeout(5, TimeUnit.MINUTES).until(() -> {
+      List<DelegateConnection> delegateConnections =
+          wingsPersistence.createQuery(DelegateConnection.class, excludeAuthority).asList();
+      boolean connected = !delegateConnections.isEmpty();
+      logger.info("Got {} delegate connections.", delegateConnections.size());
       return connected;
     }, CoreMatchers.is(true));
   }
