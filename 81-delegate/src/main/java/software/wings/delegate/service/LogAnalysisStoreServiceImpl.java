1aacbacbb31f1b52a9562f746fa046317873045c
Praveen Kambam Sugavanam <praveen.sugavanam@harness.io>
2018-10-04 00:46:09 -0700

    Null check and call with timer from delegate (#3535)
    * Null check and call with timer from delegate
    
    * unused import

--- 81-delegate/src/main/java/software/wings/delegate/service/LogAnalysisStoreServiceImpl.java
+++ 81-delegate/src/main/java/software/wings/delegate/service/LogAnalysisStoreServiceImpl.java
@@ -2,9 +2,13 @@ package software.wings.delegate.service;
 
 import static io.harness.network.SafeHttpCall.execute;
 
+import com.google.common.util.concurrent.TimeLimiter;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import software.wings.beans.RestResponse;
 import software.wings.delegatetasks.LogAnalysisStoreService;
 import software.wings.service.impl.analysis.LogElement;
 import software.wings.service.intfc.analysis.ClusterLevel;
@@ -13,13 +17,16 @@ import software.wings.verification.VerificationServiceClient;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by rsingh on 06/20/17.
  */
 @Singleton
 public class LogAnalysisStoreServiceImpl implements LogAnalysisStoreService {
+  private static final Logger logger = LoggerFactory.getLogger(LogAnalysisStoreServiceImpl.class);
   @Inject private VerificationServiceClient verificationServiceClient;
+  @Inject private TimeLimiter timeLimiter;
 
   @Override
   public boolean save(StateType stateType, String accountId, String appId, String stateExecutionId, String workflowId,
@@ -34,9 +41,20 @@ public class LogAnalysisStoreServiceImpl implements LogAnalysisStoreService {
       case LOGZ:
       case LOG_VERIFICATION:
       case BUG_SNAG:
-        return execute(verificationServiceClient.saveLogs(accountId, appId, stateExecutionId, workflowId,
-                           workflowExecutionId, serviceId, ClusterLevel.L0, delegateTaskId, stateType, logs))
-            .getResource();
+        try {
+          RestResponse<Boolean> response = timeLimiter.callWithTimeout(
+              ()
+                  -> execute(verificationServiceClient.saveLogs(accountId, appId, stateExecutionId, workflowId,
+                      workflowExecutionId, serviceId, ClusterLevel.L0, delegateTaskId, stateType, logs)),
+              15, TimeUnit.SECONDS, true);
+          if (response == null) {
+            return false;
+          }
+          return response.getResource();
+        } catch (Exception ex) {
+          logger.error("Exception while saving log data for stateExecutionId: {}", stateExecutionId);
+          return false;
+        }
       default:
         throw new IllegalStateException("Invalid state: " + stateType);
     }
