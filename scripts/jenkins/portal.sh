3b8a3b6384ac0aba195c4e34104e190e9da18b96
George Georgiev <george@harness.io>
2018-10-05 14:00:19 -0700

    Move jenkins portal script in the repo (#3565)
    * Move jenkins portal script in the repo

---
+++ scripts/jenkins/portal.sh
@@ -0,0 +1,65 @@
+### Dockerization of Manager ####### Doc
+
+mkdir -p dist/manager ;
+
+#mkdir -p dist/temp ;
+#cd dist/temp
+#tar -xvf ../../tools/apm/appdynamics/AppServerAgent-4.5.0.23604.tar.gz
+#cd ../..
+
+cd dist/manager
+
+cp ../../71-rest/target/rest-capsule.jar .
+cp ../../71-rest/src/main/resources/hazelcast.xml .
+cp ../../keystore.jks .
+cp ../../71-rest/newrelic.yml .
+cp ../../71-rest/config.yml .
+cp ../../tools/apm/appdynamics/AppServerAgent-4.5.0.23604.tar.gz .
+
+cp ../../dockerization/manager/Dockerfile-manager-jenkins-k8 ./Dockerfile
+cp ../../dockerization/manager/Dockerfile-manager-jenkins-k8-gcr ./Dockerfile-gcr
+cp -r ../../dockerization/manager/scripts/ .
+cp -r ../../dockerization/common-resources/ .
+echo ${BUILD_NUMBER} > build_number.txt
+cd ../..
+
+mkdir -p dist/verification ;
+
+cd dist/verification
+
+cp ../../79-verification/target/verification-capsule.jar .
+cp ../../79-verification/keystore.jks .
+cp ../../79-verification/verification-config.yml .
+
+cp ../../dockerization/verification/Dockerfile-verification-jenkins-k8 ./Dockerfile
+cp ../../dockerization/verification/Dockerfile-verification-jenkins-k8-gcr ./Dockerfile-gcr
+cp -R ../../dockerization/verification/scripts/ .
+cp -r ../../dockerization/common-resources/ .
+echo ${BUILD_NUMBER} > build_number.txt
+cd ../..
+
+
+mkdir -p dist/delegate
+cd dist/delegate
+cp ../../81-delegate/target/delegate-capsule.jar .
+cd ../..
+cp 81-delegate/target/delegate-capsule.jar delegate-${BUILD_NUMBER}.jar
+
+mkdir -p dist/watcher
+cd dist/watcher
+cp ../../82-watcher/target/watcher-capsule.jar .
+cd ../..
+cp 82-watcher/target/watcher-capsule.jar watcher-${BUILD_NUMBER}.jar
+
+mkdir -p dist/disconnected_on_prem_pov
+cd dist/disconnected_on_prem_pov
+cp -r ../../on-prem/harness_disconnected_on_prem_pov_final .
+cp -r ../../on-prem/disconnected_on_prem_pov_installer .
+tar -zcvf disconnected_on_prem_pov_template.tar.gz *
+cd ../..
+cp dist/disconnected_on_prem_pov/disconnected_on_prem_pov_template.tar.gz disconnected_on_prem_pov_template.tar.gz
+
+mkdir -p dist/test
+cd dist/test
+cp ../../71-rest/target/rest-data-gen.jar .
+cd ../..
