65f2fe1d8d682caa116129a96fbd7994eb9c740f
Pranjal Kumar <pranjal@harness.io>
2018-10-08 23:31:27 +0530

    HAR-5277 : Enable Verification Service to report to AppD (#3578)

--- dockerization/verification/scripts/start_process.sh
+++ dockerization/verification/scripts/start_process.sh
@@ -19,6 +19,13 @@ fi
 
 export JAVA_OPTS="-Xms${MEMORY}m -Xmx${MEMORY}m -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:mygclogfilename.gc -XX:+UseParallelGC -XX:MaxGCPauseMillis=500 -Dfile.encoding=UTF-8"
 
+if [[ "${ENABLE_APPDYNAMICS}" == "true" ]]; then
+    tar -xvzf AppServerAgent-4.5.0.23604.tar.gz
+    JAVA_OPTS=$JAVA_OPTS" -javaagent:/opt/harness/AppServerAgent-4.5.0.23604/javaagent.jar -Dappdynamics.jvm.shutdown.mark.node.as.historical=true -Dappdynamics.agent.nodeName="$(hostname)
+    echo "Using Appdynamics java agent"
+fi
+
+
 if [[ "${DEPLOY_MODE}" == "KUBERNETES" ]] || [[ "${DEPLOY_MODE}" == "KUBERNETES_ONPREM" ]]; then
     java $JAVA_OPTS -jar $CAPSULE_JAR /opt/harness/verification-config.yml
 else
