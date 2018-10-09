8e247028fa0014f5c1506e3f0da0dbc75714ae7e
George Georgiev <george@harness.io>
2018-10-09 12:53:31 -0700

    Release beanching (#3608)

---
+++ scripts/jenkins/release-branch.sh
@@ -0,0 +1,22 @@
+export VERSION_FILE=pom.xml
+
+export VERSION=`cat ${VERSION_FILE} |\
+    grep '<build.number>' |\
+    sed -e 's: *<build.number>::g' | sed -e 's:</build.number>::g'`
+export VERSION=${VERSION%??}
+export NEW_VERSION=$(( ${VERSION}+1 ))
+
+export BRANCH=`echo "${GIT_BRANCH}" | sed -e "s/origin\///g"`
+export SHA=`git rev-parse HEAD`
+
+git checkout -b release/${VERSION}
+git checkout ${BRANCH}
+
+
+sed -i "s:<build.number>${VERSION}00</build.number>:<build.number>${NEW_VERSION}00</build.number>:g" ${VERSION_FILE}
+git add ${VERSION_FILE}
+git commit -m "Branching to release/${VERSION}. New version ${NEW_VERSION}"
+
+echo VERSION=${VERSION} > build.properties
+echo BRANCH=${BRANCH} >> build.properties
+echo SHA=${SHA} >> build.properties
--- scripts/jenkins/verification-dockerizer.sh
+++ scripts/jenkins/verification-dockerizer.sh
@@ -2,10 +2,13 @@ export VERSION=`cat destination/dist/manager/version.txt`
 
 docker login -u _json_key --password-stdin https://us.gcr.io < $GCR_CREDENTIALS
 
-docker build -t us.gcr.io/platform-205701/harness/verification-service:${VERSION} destination/dist/verification -f destination/dist/verification/Dockerfile-gcr
-docker push us.gcr.io/platform-205701/harness/verification-service:${VERSION}
+docker build -t us.gcr.io/platform-205701/harness/${SNAPSHOT_PREFIX}verification-service:${VERSION} destination/dist/verification -f destination/dist/verification/Dockerfile-gcr
+docker push us.gcr.io/platform-205701/harness/${SNAPSHOT_PREFIX}verification-service:${VERSION}
 
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+if [ -z "${SNAPSHOT_PREFIX}" ]
+then
+    docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
-docker build -t harness/verification-service:${VERSION} destination/dist/verification
-docker push harness/verification-service:${VERSION}
+    docker build -t harness/verification-service:${VERSION} destination/dist/verification
+    docker push harness/verification-service:${VERSION}
+fi
