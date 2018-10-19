e059ff9f3f881204c241c1dd144a8241603bc73b
Rushabh Shah <rushabh.shah@harness.io>
2018-10-19 11:45:53 -0700

    [HAR-6020] adding on prem proxy settings support for verification service (#3793)

--- dockerization/proxy/scripts/replace_parameters.sh
+++ dockerization/proxy/scripts/replace_parameters.sh
@@ -7,3 +7,8 @@ sed -i "s|MANAGER3|${MANAGER3}|" /etc/nginx/conf.d/harness-on-prem-proxy.conf
 sed -i "s|UI1|${UI1}|" /etc/nginx/conf.d/harness-on-prem-proxy.conf
 sed -i "s|UI2|${UI2}|" /etc/nginx/conf.d/harness-on-prem-proxy.conf
 sed -i "s|UI3|${UI3}|" /etc/nginx/conf.d/harness-on-prem-proxy.conf
+
+
+sed -i "s|VERIFICATION1|${VERIFICATION1}|" /etc/nginx/conf.d/harness-on-prem-proxy.conf
+sed -i "s|VERIFICATION2|${VERIFICATION2}|" /etc/nginx/conf.d/harness-on-prem-proxy.conf
+sed -i "s|VERIFICATION3|${VERIFICATION3}|" /etc/nginx/conf.d/harness-on-prem-proxy.conf
