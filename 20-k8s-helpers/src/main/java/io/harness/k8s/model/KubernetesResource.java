24cc2de681c4105caf28e565784aecdf90209cb1
Puneet Saraswat <puneet.saraswat@harness.io>
2018-10-24 08:50:09 -0700

    [HAR-6060]: Adding helpers for manifest annotations/labels/fields (#3854)

--- 20-k8s-helpers/src/main/java/io/harness/k8s/model/KubernetesResource.java
+++ 20-k8s-helpers/src/main/java/io/harness/k8s/model/KubernetesResource.java
@@ -1,11 +1,82 @@
 package io.harness.k8s.model;
 
 import lombok.Builder;
-import lombok.Value;
+import lombok.Data;
 
-@Value
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Data
 @Builder
 public class KubernetesResource {
   private KubernetesResourceId resourceId;
   private Object value;
+
+  private static final String dotMatch = "\\.";
+
+  public Object getField(String key) {
+    Object object = this.getValue();
+
+    List<String> keyList = Arrays.asList(key.split(dotMatch));
+
+    for (int i = 0; i < keyList.size() - 1; i++) {
+      String item = keyList.get(i);
+      if (object instanceof Map && ((Map) object).containsKey(item)) {
+        object = ((Map) object).get(item);
+      } else {
+        return null;
+      }
+    }
+
+    if (object instanceof Map) {
+      return ((Map) object).get(keyList.get(keyList.size() - 1));
+    }
+
+    return null;
+  }
+
+  public KubernetesResource setField(String key, Object newValue) {
+    if (this.getValue() == null) {
+      this.setValue(new HashMap<>());
+    }
+
+    Map objectMap = (Map) this.getValue();
+
+    List<String> keyList = Arrays.asList(key.split(dotMatch));
+
+    for (int i = 0; i < keyList.size() - 1; i++) {
+      String item = keyList.get(i);
+      if (objectMap.containsKey(item)) {
+        objectMap = (Map) objectMap.get(item);
+      } else {
+        objectMap.put(item, new HashMap<>());
+      }
+    }
+
+    objectMap.put(keyList.get(keyList.size() - 1), newValue);
+
+    return this;
+  }
+
+  public KubernetesResource addAnnotations(Map newAnnotations) {
+    Map annotations = (Map) this.getField("metadata.annotations");
+    if (annotations == null) {
+      annotations = new HashMap();
+    }
+
+    annotations.putAll(newAnnotations);
+    return this.setField("metadata.annotations", annotations);
+  }
+
+  public KubernetesResource addLabels(Map newLabels) {
+    Map labels = (Map) this.getField("metadata.labels");
+    if (labels == null) {
+      labels = new HashMap();
+    }
+
+    labels.putAll(newLabels);
+    return this.setField("metadata.labels", labels);
+  }
 }
---
+++ 20-k8s-helpers/src/test/java/io/harness/k8s/model/KubernetesResourceTest.java
@@ -0,0 +1,75 @@
+package io.harness.k8s.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.google.common.base.Charsets;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.io.Resources;
+
+import io.harness.k8s.manifest.ManifestHelper;
+import org.junit.Test;
+
+import java.net.URL;
+import java.util.Map;
+
+public class KubernetesResourceTest {
+  @Test
+  public void setAndGetTest() throws Exception {
+    URL url = this.getClass().getResource("/deploy.yaml");
+    String fileContents = Resources.toString(url, Charsets.UTF_8);
+    KubernetesResource resource = ManifestHelper.processYaml(fileContents).get(0);
+
+    assertThat(resource.getField("random")).isEqualTo(null);
+    assertThat(resource.getField("random.random")).isEqualTo(null);
+
+    resource.setField("kind", "myKind");
+    String kind = (String) resource.getField("kind");
+    assertThat(kind).isEqualTo("myKind");
+
+    resource.setField("metadata.name", "myName");
+    String name = (String) resource.getField("metadata.name");
+    assertThat(name).isEqualTo("myName");
+
+    resource.setField("metadata.labels.key1", "value1");
+    String key = (String) resource.getField("metadata.labels.key1");
+    assertThat(key).isEqualTo("value1");
+
+    resource.setField("metadata.labels.key2", "value2");
+    Map labels = (Map) resource.getField("metadata.labels");
+    assertThat(labels).hasSize(3);
+    assertThat(labels.get("app")).isEqualTo("nginx");
+    assertThat(labels.get("key1")).isEqualTo("value1");
+    assertThat(labels.get("key2")).isEqualTo("value2");
+  }
+
+  @Test
+  public void addAnnotationTest() throws Exception {
+    URL url = this.getClass().getResource("/deploy.yaml");
+    String fileContents = Resources.toString(url, Charsets.UTF_8);
+    KubernetesResource resource = ManifestHelper.processYaml(fileContents).get(0);
+
+    resource.addAnnotations(ImmutableMap.of("key1", "value1", "key2", "value2"));
+
+    Map annotations = (Map) resource.getField("metadata.annotations");
+
+    assertThat(annotations).hasSize(2);
+    assertThat(annotations.get("key1")).isEqualTo("value1");
+    assertThat(annotations.get("key2")).isEqualTo("value2");
+  }
+
+  @Test
+  public void addLabelsTest() throws Exception {
+    URL url = this.getClass().getResource("/deploy.yaml");
+    String fileContents = Resources.toString(url, Charsets.UTF_8);
+    KubernetesResource resource = ManifestHelper.processYaml(fileContents).get(0);
+
+    resource.addLabels(ImmutableMap.of("key1", "value1", "key2", "value2"));
+
+    Map labels = (Map) resource.getField("metadata.labels");
+
+    assertThat(labels).hasSize(3);
+    assertThat(labels.get("app")).isEqualTo("nginx");
+    assertThat(labels.get("key1")).isEqualTo("value1");
+    assertThat(labels.get("key2")).isEqualTo("value2");
+  }
+}
