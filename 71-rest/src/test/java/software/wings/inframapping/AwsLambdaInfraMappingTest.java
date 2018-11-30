e616233516933a76010e7f8ef085c0de432cefdd
Vaibhav Singhal <vaibhav.si@harness.io>
2018-11-30 13:10:10 +0530

    [HAR-6838]: added test for aws lambda infra mapping updation via terraform (#4354)
    * added test for aws lambda infra mapping updation via terraform
    
    * added static import for assertEquals to remove redundancy
    
    * corrected formatting
    
    * corrected formatting

---
+++ 71-rest/src/test/java/software/wings/inframapping/AwsLambdaInfraMappingTest.java
@@ -0,0 +1,53 @@
+package software.wings.inframapping;
+
+import static org.junit.Assert.assertEquals;
+
+import io.harness.exception.InvalidRequestException;
+import org.junit.Before;
+import org.junit.Test;
+import software.wings.WingsBaseTest;
+import software.wings.beans.AwsLambdaInfraStructureMapping;
+import software.wings.beans.InfrastructureMappingBlueprint.NodeFilteringType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AwsLambdaInfraMappingTest extends WingsBaseTest {
+  private AwsLambdaInfraStructureMapping awsLambdaInfraStructureMapping;
+
+  @Before
+  public void setUp() throws Exception {
+    awsLambdaInfraStructureMapping = new AwsLambdaInfraStructureMapping();
+  }
+
+  @Test
+  public void testApplyProvisionerVariables() {
+    Map<String, Object> outputVariablesMap = new HashMap<>();
+
+    outputVariablesMap.put("region", "testRegion");
+    outputVariablesMap.put("role", "testRole");
+    outputVariablesMap.put("vpcId", "testVpc");
+
+    awsLambdaInfraStructureMapping.applyProvisionerVariables(
+        outputVariablesMap, NodeFilteringType.AWS_AUTOSCALING_GROUP);
+    assertEquals("testRegion", awsLambdaInfraStructureMapping.getRegion());
+    assertEquals("testRole", awsLambdaInfraStructureMapping.getRole());
+    assertEquals("testVpc", awsLambdaInfraStructureMapping.getVpcId());
+  }
+
+  @Test(expected = InvalidRequestException.class)
+  public void testInvalidKey() {
+    Map<String, Object> outputVariablesMap = new HashMap<>();
+
+    outputVariablesMap.put("region1", "testRegion");
+    awsLambdaInfraStructureMapping.applyProvisionerVariables(
+        outputVariablesMap, NodeFilteringType.AWS_AUTOSCALING_GROUP);
+  }
+
+  @Test(expected = InvalidRequestException.class)
+  public void testMandatoryFields() {
+    Map<String, Object> outputVariablesMap = new HashMap<>();
+    awsLambdaInfraStructureMapping.applyProvisionerVariables(
+        outputVariablesMap, NodeFilteringType.AWS_AUTOSCALING_GROUP);
+  }
+}
