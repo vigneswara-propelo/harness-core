7d548f1520003ec83e0a9ca27e947be42d2275a5
Mark Lu <mark.lu@harness.io>
2019-01-03 19:23:14 -0800

    [HAR-7102]: Revert the time limiter executor thread pool change. (#4900)

--- 12-commons/src/main/java/io/harness/time/TimeModule.java
+++ 12-commons/src/main/java/io/harness/time/TimeModule.java
@@ -6,10 +6,6 @@ import com.google.common.util.concurrent.TimeLimiter;
 import io.harness.govern.DependencyModule;
 
 import java.util.Set;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 public class TimeModule extends DependencyModule {
   private static volatile TimeModule instance;
@@ -23,11 +19,7 @@ public class TimeModule extends DependencyModule {
 
   @Override
   protected void configure() {
-    // Create a dedicated executor service for TimeLimiter: it has 2 core threads anc can scale up to 32 threads.
-    // Its task queue can grow up to 100k, then all subsequent tasks submission will be blocked if queue is full.
-    ExecutorService timeLimiterExecutorService =
-        new ThreadPoolExecutor(2, 32, 60L, TimeUnit.SECONDS, new LinkedBlockingQueue<>(100000));
-    bind(TimeLimiter.class).toInstance(new SimpleTimeLimiter(timeLimiterExecutorService));
+    bind(TimeLimiter.class).toInstance(new SimpleTimeLimiter());
   }
 
   @Override
