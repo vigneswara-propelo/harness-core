f464220b528e6ad8d330a2a1715c8d3e18614414
Aman <aman.singh@harness.io>
2019-01-08 10:53:39 +0530

    [HAR-7753]: SP initiated login for SAML azure (#4856)
    * Saml Changes

--- 71-rest/src/main/java/software/wings/security/saml/SamlClientService.java
+++ 71-rest/src/main/java/software/wings/security/saml/SamlClientService.java
@@ -2,6 +2,7 @@ package software.wings.security.saml;
 
 import static com.google.common.base.Charsets.UTF_8;
 import static io.harness.exception.WingsException.USER;
+import static org.opensaml.xml.util.Base64.encodeBytes;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -10,6 +11,7 @@ import com.coveo.saml.SamlClient;
 import com.coveo.saml.SamlException;
 import io.harness.eraro.ErrorCode;
 import io.harness.exception.WingsException;
+import org.apache.http.client.utils.URIBuilder;
 import org.hibernate.validator.constraints.NotBlank;
 import software.wings.beans.Account;
 import software.wings.beans.User;
@@ -19,17 +21,29 @@ import software.wings.security.authentication.AuthenticationUtil;
 import software.wings.service.intfc.SSOSettingService;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Base64;
 import java.util.Iterator;
+import java.util.zip.Deflater;
+import java.util.zip.DeflaterOutputStream;
 
 @Singleton
 public class SamlClientService {
+  public static final String SAML_REQUEST_URI_KEY = "SAMLRequest";
   @Inject AuthenticationUtil authenticationUtil;
   @Inject SSOSettingService ssoSettingService;
   private static final String GOOGLE_HOST = "accounts.google.com";
   private static final String AZURE_HOST = "login.microsoftonline.com";
+
+  /**
+   * This field is identifier of SAML application and is used to point to the
+   * specific application for Saml redirection in case of azure and gcp.
+   */
+  public static final String RELYING_PARTY_IDENTIFIER = "app.harness.io";
 
   public String getHost(@NotBlank String url) throws URISyntaxException {
     return new URI(url).getHost();
@@ -53,8 +67,8 @@ public class SamlClientService {
   }
 
   public SamlClient getSamlClient(String samlData) throws SamlException {
-    return SamlClient.fromMetadata(
-        "Harness", null, new InputStreamReader(new ByteArrayInputStream(samlData.getBytes(UTF_8)), UTF_8));
+    return SamlClient.fromMetadata(RELYING_PARTY_IDENTIFIER, null,
+        new InputStreamReader(new ByteArrayInputStream(samlData.getBytes(UTF_8)), UTF_8));
   }
 
   public SamlRequest generateSamlRequest(User user) {
@@ -63,15 +77,26 @@ public class SamlClientService {
       SamlRequest samlRequest = new SamlRequest();
       try {
         SamlClient samlClient = getSamlClientFromAccount(primaryAccount);
-        samlRequest.setIdpRedirectUrl(samlClient.getIdentityProviderUrl());
+        URIBuilder redirectionUri = new URIBuilder(samlClient.getIdentityProviderUrl());
+        redirectionUri.addParameter(SAML_REQUEST_URI_KEY, encodeParamaeters(samlClient.getSamlRequest()));
+        samlRequest.setIdpRedirectUrl(redirectionUri.toString());
         return samlRequest;
-
-      } catch (SamlException e) {
-        throw new WingsException(e);
+      } catch (SamlException | URISyntaxException | IOException e) {
+        throw new WingsException("Generating Saml request failed for user: [%s]", e);
       }
     }
 
     throw new WingsException(ErrorCode.INVALID_AUTHENTICATION_MECHANISM, USER);
+  }
+
+  public String encodeParamaeters(String encodedXmlParam) throws IOException {
+    String decodedXmlParam = new String(Base64.getMimeDecoder().decode(encodedXmlParam), UTF_8);
+    ByteArrayOutputStream bytesOut = new ByteArrayOutputStream();
+    Deflater deflater = new Deflater(8, true);
+    DeflaterOutputStream deflaterStream = new DeflaterOutputStream(bytesOut, deflater);
+    deflaterStream.write(decodedXmlParam.getBytes(UTF_8));
+    deflaterStream.finish();
+    return encodeBytes(bytesOut.toByteArray(), 8);
   }
 
   public SamlClient getSamlClientFromOrigin(String origin) throws SamlException {
