c0c836753948a24f8ddb29e65ac7e17a0d33d4cb
Anshul Anshul <anshul@harness.io>
2019-01-11 13:37:34 -0800

    [HAR-7798]: Don't generate alert in logs if the the yamlPath is invalid (#5042)
    * [HAR-7798]: Don't generate alert in logs if the the yamlPath is invalid

--- 71-rest/src/main/java/software/wings/service/impl/yaml/YamlGitServiceImpl.java
+++ 71-rest/src/main/java/software/wings/service/impl/yaml/YamlGitServiceImpl.java
@@ -602,7 +602,8 @@ public class YamlGitServiceImpl implements YamlGitService {
       if (!folderYamlTypes.stream().anyMatch(
               yamlType -> Pattern.compile(yamlType.getPathExpression()).matcher(filePath).matches())) {
         throw new WingsException(
-            "Invalid entity name, entity can not contain / in the name. Caused invalid file path: " + filePath);
+            "Invalid entity name, entity can not contain / in the name. Caused invalid file path: " + filePath,
+            WingsException.USER);
       }
     }
   }
