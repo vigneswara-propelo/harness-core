6cc87e59d238a326a073425a11a06a7067fb0b38
Anshul Anshul <anshul@harness.io>
2019-01-24 12:13:09 -0800

    [HAR-8448]: Remove extra logging added for debugging longer response … (#5296)
    * [HAR-8448]: Remove extra logging added for debugging longer response times

--- 71-rest/src/main/java/software/wings/service/impl/yaml/service/YamlServiceImpl.java
+++ 71-rest/src/main/java/software/wings/service/impl/yaml/service/YamlServiceImpl.java
@@ -181,22 +181,17 @@ public class YamlServiceImpl<Y extends BaseYaml, B extends Base> implements Yaml
     List<GitFileChange> gitFileChangeList = asList(change);
 
     try {
-      logger.info("YAML-GIT-SYNC Processing changeset from yaml");
       List<ChangeContext> changeContextList = processChangeSet(asList(change));
-      logger.info("YAML-GIT-SYNC Done processing changeset from yaml");
       notNullCheck("Change Context List is null", changeContextList);
       boolean empty = isEmpty(changeContextList);
       if (!empty) {
         // We only sent one
-        logger.info("YAML-GIT-SYNC Setting resource");
+
         ChangeContext changeContext = changeContextList.get(0);
         Object base = changeContext.getYamlSyncHandler().get(
             changeContext.getChange().getAccountId(), changeContext.getChange().getFilePath());
-        logger.info("YAML-GIT-SYNC Setting resource");
         rr.setResource(base);
-        logger.info("YAML-GIT-SYNC Removing git sync errors");
         yamlGitService.removeGitSyncErrors(accountId, gitFileChangeList, false);
-        logger.info("YAML-GIT-SYNC Done removing git sync errors");
       } else {
         throw new WingsException(ErrorCode.GENERAL_YAML_ERROR, USER)
             .addParam("message", "Update failed. Reason: " + yamlPayload.getName());
@@ -447,7 +442,6 @@ public class YamlServiceImpl<Y extends BaseYaml, B extends Base> implements Yaml
     YamlType previousYamlType = null;
     int numOfParallelChanges = 0;
 
-    logger.info("YAML-GIT-SYNC Processing changeContext");
     for (ChangeContext changeContext : changeContextList) {
       String yamlFilePath = changeContext.getChange().getFilePath();
       YamlType yamlType = changeContext.getYamlType();
@@ -480,9 +474,7 @@ public class YamlServiceImpl<Y extends BaseYaml, B extends Base> implements Yaml
       }));
     }
 
-    logger.info("YAML-GIT-SYNC Checking futures and evicting cache");
     checkFuturesAndEvictCache(futures, previousYamlType, accountId);
-    logger.info("YAML-GIT-SYNC Done checking futures and evicting cache");
 
     if (failedYamlFileChangeMap.size() > 0) {
       logAllErrorsWhileYamlInjestion(failedYamlFileChangeMap);
@@ -518,7 +510,6 @@ public class YamlServiceImpl<Y extends BaseYaml, B extends Base> implements Yaml
       }
       quietSleep(ofMillis(10));
     }
-    logger.info("YAML-GIT-SYNC Evicting cache");
 
     // Evict only in case of below six entity types
     if (YamlType.APPLICATION.equals(yamlType) || YamlType.SERVICE.equals(yamlType)
