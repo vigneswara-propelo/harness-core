7a50ba509d5a19f20478ff7baf7f92b89041f79f
George Georgiev <george@harness.io>
2019-02-04 21:10:32 -0800

    [HAR-5978]: cleanup AwsElbListener (#5482)
    * [HAR-5978]: cleanup AwsElbListener

--- 20-delegate-tasks/src/main/java/io/harness/serializer/kryo/DelegateTasksKryoRegister.java
+++ 20-delegate-tasks/src/main/java/io/harness/serializer/kryo/DelegateTasksKryoRegister.java
@@ -1,6 +1,7 @@
 package io.harness.serializer.kryo;
 
 import com.esotericsoftware.kryo.Kryo;
+import io.harness.delegate.task.protocol.AwsElbListener;
 import io.harness.delegates.beans.ScriptType;
 import io.harness.delegates.beans.ShellScriptApprovalTaskParameters;
 import io.harness.serializer.KryoRegistrar;
@@ -10,5 +11,6 @@ public class DelegateTasksKryoRegister implements KryoRegistrar {
   public void register(Kryo kryo) throws Exception {
     kryo.register(ShellScriptApprovalTaskParameters.class, 20001);
     kryo.register(ScriptType.class, 5253);
+    kryo.register(AwsElbListener.class, 5600);
   }
 }
