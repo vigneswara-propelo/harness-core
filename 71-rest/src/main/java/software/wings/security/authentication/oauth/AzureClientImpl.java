e619383358b291249f08fc7af5fa379166f36069
Aman <aman.singh@harness.io>
2019-02-24 19:08:21 -0800

    [HAR-8828]: Added base64encoding and urlencoding in Azure secret (#5789)
    * Changed the sso redirection url
    
    * Added base64encoding
    
    * Added url encoding

--- 71-rest/src/main/java/software/wings/security/authentication/oauth/AzureClientImpl.java
+++ 71-rest/src/main/java/software/wings/security/authentication/oauth/AzureClientImpl.java
@@ -14,6 +14,7 @@ import lombok.experimental.FieldDefaults;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Request.Builder;
+import org.apache.commons.codec.binary.Base64;
 import org.apache.http.client.utils.URIBuilder;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -23,7 +24,9 @@ import software.wings.app.MainConfiguration;
 import software.wings.security.SecretManager;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
+import java.net.URLEncoder;
 import java.util.concurrent.ExecutionException;
 
 @Singleton
@@ -38,14 +41,19 @@ public class AzureClientImpl extends BaseOauthClient implements OauthClient {
   static final Logger logger = LoggerFactory.getLogger(AzureClientImpl.class);
 
   @Inject
-  public AzureClientImpl(MainConfiguration mainConfiguration, SecretManager secretManager) {
+  public AzureClientImpl(MainConfiguration mainConfiguration, SecretManager secretManager)
+      throws UnsupportedEncodingException {
     super(secretManager);
     AzureConfig azureConfig = mainConfiguration.getAzureConfig();
-    service = new ServiceBuilder(azureConfig.getClientId())
-                  .apiSecret(azureConfig.getClientSecret())
+    String clientId = new String(Base64.decodeBase64(azureConfig.getClientId().getBytes()));
+    String clientSecret = new String(Base64.decodeBase64(azureConfig.getClientSecret().getBytes()));
+    clientSecret = URLEncoder.encode(clientSecret, "UTF-8");
+    service = new ServiceBuilder(clientId)
+                  .apiSecret(clientSecret)
                   .scope("User.Read") // replace with desired scope
                   .callback(azureConfig.getCallbackUrl())
                   .build(new MicrosoftAzureActiveDirectory20ApiV2());
+    logger.info("Azure client settings are: {}", azureConfig.toString());
   }
 
   @Override
