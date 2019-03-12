94ae5c392010f6eca594939c547e10c605cf1ac5
Puneet Saraswat <puneet.saraswat@harness.io>
2019-03-11 17:48:23 -0700

    [HAR-10099]: Log execption in DockerConfigFunctor (#6072)
    * [HAR-10099]: Log execption in DockerConfigFunctor

--- 71-rest/src/main/java/software/wings/expression/DockerConfigFunctor.java
+++ 71-rest/src/main/java/software/wings/expression/DockerConfigFunctor.java
@@ -3,10 +3,14 @@ package software.wings.expression;
 import io.harness.exception.InvalidRequestException;
 import io.harness.expression.ExpressionFunctor;
 import lombok.Builder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import software.wings.service.impl.artifact.ArtifactCollectionUtil;
 
 @Builder
 public class DockerConfigFunctor implements ExpressionFunctor {
+  private static final Logger logger = LoggerFactory.getLogger(DockerConfigFunctor.class);
+
   private String appId;
   private ArtifactCollectionUtil artifactCollectionUtil;
 
@@ -21,6 +25,7 @@ public class DockerConfigFunctor implements ExpressionFunctor {
     try {
       return artifactCollectionUtil.getDockerConfig(appId, artifactStreamId);
     } catch (InvalidRequestException e) {
+      logger.error("Error in getDockerConfig", e);
       return "";
     }
   }
