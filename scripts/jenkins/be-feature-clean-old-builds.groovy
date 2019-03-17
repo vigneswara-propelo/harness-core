a63531b65b160585afcec75a6542487fa6c44255
George Georgiev <george@harness.io>
2019-03-17 09:02:44 -0700

    [HAR-6304]: add jenkins groovy script (#6166)
    * [HAR-6304]: add jenkins groovy script

---
+++ scripts/jenkins/be-feature-clean-old-builds.groovy
@@ -0,0 +1,57 @@
+import jenkins.model.Jenkins
+
+def jobs = [
+        'portal-feature',
+]
+
+def jobId(name) {
+    splits = name.split()
+    if (splits.length != 2) {
+        return null
+    }
+    return splits[0]
+}
+
+jobs.each {
+    def jobName = it
+
+    def map = [:]
+    Jenkins.instance.getItemByFullName(jobName).builds.each {
+        def id = jobId(it.getDisplayName())
+
+        if (id == null) {
+            if (it.getExecutor() == null) {
+                println("Delete failed job #" + it.getNumber())
+                it.delete()
+            }
+            return;
+        }
+
+        def current = map.get(id)
+        if (current == null || current.getNumber() < it.getNumber()) {
+            map.put(id, it)
+        }
+    }
+
+    Jenkins.instance.getItemByFullName(jobName).builds.each {
+        def current = map.get(jobId(it.getDisplayName()))
+        if (current == null) {
+            return;
+        }
+
+        if (current.getNumber() == it.getNumber()) {
+            return;
+        }
+
+        def executor = it.getExecutor()
+        if (executor != null) {
+            println("Interrupt not needed job " + jobName +" - " + it.getDisplayName() + " #" + it.getNumber())
+            executor.interrupt()
+        }
+
+        if (!current.isBuilding()) {
+            println("Delete old job " + jobName +" - " + it.getDisplayName() + " #" + it.getNumber())
+            it.delete()
+        }
+    }
+}
---
+++ scripts/jenkins/pr-script-update-warnings.groovy
@@ -0,0 +1,25 @@
+import jenkins.model.Jenkins
+import javax.xml.transform.stream.StreamSource
+import java.util.regex.Pattern
+import java.util.regex.Matcher
+
+def updateJob(name, amount) {
+    job = Jenkins.instance.getItemByFullName(name)
+    config = job.getConfigFile().file.text
+
+    def Pattern pattern = Pattern.compile("<threshold>(\\d+)</threshold>")
+    Matcher matcher = pattern.matcher(config)
+    matcher.find()
+
+    current = Integer.parseInt(matcher.group(1))
+    next = current - amount
+
+    config = config.replace("<threshold>" + current + "</threshold>", "<threshold>" + next + "</threshold>")
+
+    InputStream is = new ByteArrayInputStream(config.getBytes());
+    job.updateByXml(new StreamSource(is));
+    job.save();
+}
+
+updateJob("pr-portal-warnings", 2)
+updateJob("pr-portal-checkstyle-to-be", 5)
