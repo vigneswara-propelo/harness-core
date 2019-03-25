6b63abdd5d2dd40bb14d86887a3177f92f197773
Swamy Sambamurthy <swamy@harness.io>
2019-03-26 04:56:11 +0530

    [PL-1160]: Adding CRUD and list test for Encrypted Files (#6309)
    * [PL-1160]: Adding CRUD and list test for Encrypted Files

--- 76-functional-test/src/test/java/io/harness/RestUtils/SecretsRestUtils.java
+++ 76-functional-test/src/test/java/io/harness/RestUtils/SecretsRestUtils.java
@@ -1,5 +1,7 @@
 package io.harness.RestUtils;
 
+import static javax.ws.rs.core.MediaType.MULTIPART_FORM_DATA;
+
 import io.harness.beans.PageResponse;
 import io.harness.framework.Setup;
 import io.harness.rest.RestResponse;
@@ -9,6 +11,7 @@ import software.wings.security.encryption.EncryptedData;
 import software.wings.service.impl.security.SecretText;
 import software.wings.settings.SettingValue.SettingVariableTypes;
 
+import java.io.File;
 import java.util.List;
 import javax.ws.rs.core.GenericType;
 
@@ -68,6 +71,60 @@ public class SecretsRestUtils {
             .oauth2(bearerToken)
             .queryParam("accountId", accountId)
             .queryParam("type", SettingVariableTypes.SECRET_TEXT)
+            .get("/secrets/list-secrets-page")
+            .as(new GenericType<RestResponse<PageResponse<EncryptedData>>>() {}.getType());
+    return encryptedDataList.getResource().getResponse();
+  }
+
+  public String addSecretFile(String accountId, String bearerToken, String name, String fileName) {
+    File file = new File(fileName);
+    RestResponse<String> secretsResponse = Setup.portal()
+                                               .auth()
+                                               .oauth2(bearerToken)
+                                               .contentType(MULTIPART_FORM_DATA)
+                                               .queryParam("accountId", accountId)
+                                               .formParam("name", name)
+                                               .multiPart("file", file, "text/html")
+                                               .formParam("usageRestrictions", "{\"appEnvRestrictions\":[]}")
+                                               .post("/secrets/add-file")
+                                               .as(new GenericType<RestResponse<String>>() {}.getType());
+    return secretsResponse.getResource();
+  }
+
+  public boolean updateSecretFile(String accountId, String bearerToken, String name, String fileName, String uuid) {
+    File file = new File(fileName);
+    RestResponse<Boolean> secretsResponse = Setup.portal()
+                                                .auth()
+                                                .oauth2(bearerToken)
+                                                .contentType(MULTIPART_FORM_DATA)
+                                                .queryParam("accountId", accountId)
+                                                .formParam("name", name)
+                                                .multiPart("file", file, "text/html")
+                                                .formParam("usageRestrictions", "{\"appEnvRestrictions\":[]}")
+                                                .formParam("uuid", uuid)
+                                                .post("/secrets/update-file")
+                                                .as(new GenericType<RestResponse<Boolean>>() {}.getType());
+    return secretsResponse.getResource();
+  }
+
+  public boolean deleteSecretFile(String accountId, String bearerToken, String uuid) {
+    RestResponse<Boolean> secretsResponse = Setup.portal()
+                                                .auth()
+                                                .oauth2(bearerToken)
+                                                .queryParam("accountId", accountId)
+                                                .queryParam("uuid", uuid)
+                                                .delete("/secrets/delete-file")
+                                                .as(new GenericType<RestResponse<Boolean>>() {}.getType());
+    return secretsResponse.getResource();
+  }
+
+  public List<EncryptedData> listSecretsFile(String accountId, String bearerToken) {
+    RestResponse<PageResponse<EncryptedData>> encryptedDataList =
+        Setup.portal()
+            .auth()
+            .oauth2(bearerToken)
+            .queryParam("accountId", accountId)
+            .queryParam("type", SettingVariableTypes.CONFIG_FILE)
             .get("/secrets/list-secrets-page")
             .as(new GenericType<RestResponse<PageResponse<EncryptedData>>>() {}.getType());
     return encryptedDataList.getResource().getResponse();
--- 76-functional-test/src/test/java/io/harness/functional/secrets/SecretsLocalFunctionalTest.java
+++ 76-functional-test/src/test/java/io/harness/functional/secrets/SecretsLocalFunctionalTest.java
@@ -30,8 +30,8 @@ public class SecretsLocalFunctionalTest extends AbstractFunctionalTest {
   @Test
   @Owner(emails = "swamy@harness.io", resent = false)
   @Category(FunctionalTests.class)
-  public void secretsCRUDTests() {
-    logger.info("Local secrets test starts");
+  public void secretsTextCRUDTests() {
+    logger.info("Local secrets text test starts");
     String secretsName = "Secret-" + System.currentTimeMillis();
     String secretsNewName = "newName-" + System.currentTimeMillis();
     String secretValue = "value";
@@ -63,6 +63,43 @@ public class SecretsLocalFunctionalTest extends AbstractFunctionalTest {
     boolean isDeletionDone = srUtils.deleteSecret(getAccount().getUuid(), bearerToken, secretsId);
     assertTrue(isDeletionDone);
     encryptedDataList = srUtils.listSecrets(getAccount().getUuid(), bearerToken);
-    assertTrue(encryptedDataList.size() == 0);
+    isSecretPresent = SecretsUtils.isSecretAvailable(encryptedDataList, secretsNewName);
+    assertFalse(isSecretPresent);
+  }
+
+  @Test
+  @Owner(emails = "swamy@harness.io", resent = false)
+  @Category(FunctionalTests.class)
+  public void secretsFileCRUDTests() {
+    logger.info("Local secrets file test starts");
+    String secretsName = "Secret-" + System.currentTimeMillis();
+    String secretsNewName = "newName-" + System.currentTimeMillis();
+    String filePath = System.getProperty("user.dir");
+    filePath = filePath + "/"
+        + "src/test/resources/secrets/"
+        + "testFile.txt";
+
+    List<EncryptedData> encryptedDataList = srUtils.listSecretsFile(getAccount().getUuid(), bearerToken);
+    boolean isSecretPresent = SecretsUtils.isSecretAvailable(encryptedDataList, secretsName);
+    assertFalse(isSecretPresent);
+
+    String secretId = srUtils.addSecretFile(getAccount().getUuid(), bearerToken, secretsName, filePath);
+    assertTrue(StringUtils.isNotBlank(secretId));
+    encryptedDataList = srUtils.listSecretsFile(getAccount().getUuid(), bearerToken);
+    isSecretPresent = SecretsUtils.isSecretAvailable(encryptedDataList, secretsName);
+    assertTrue(isSecretPresent);
+
+    boolean isUpdated =
+        srUtils.updateSecretFile(getAccount().getUuid(), bearerToken, secretsNewName, filePath, secretId);
+    assertTrue(isUpdated);
+    encryptedDataList = srUtils.listSecretsFile(getAccount().getUuid(), bearerToken);
+    isSecretPresent = SecretsUtils.isSecretAvailable(encryptedDataList, secretsNewName);
+    assertTrue(isSecretPresent);
+
+    boolean isDeleted = srUtils.deleteSecretFile(getAccount().getUuid(), bearerToken, secretId);
+    assertTrue(isDeleted);
+    encryptedDataList = srUtils.listSecretsFile(getAccount().getUuid(), bearerToken);
+    isSecretPresent = SecretsUtils.isSecretAvailable(encryptedDataList, secretsNewName);
+    assertFalse(isSecretPresent);
   }
 }
