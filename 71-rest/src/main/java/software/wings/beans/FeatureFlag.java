4e0d8985c9241646f5bb972a4f61a209217aac6a
George Georgiev <george@harness.io>
2019-04-10 13:56:56 -0700

    [HAR-5978]: refactor feature flag (#6422)
    * [HAR-5978]: refactor feature flag

--- 71-rest/src/main/java/software/wings/beans/FeatureFlag.java
+++ 71-rest/src/main/java/software/wings/beans/FeatureFlag.java
@@ -1,30 +1,24 @@
 package software.wings.beans;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.AllArgsConstructor;
+import io.harness.persistence.PersistentEntity;
+import io.harness.persistence.UpdatedAtAware;
 import lombok.Builder;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 import org.mongodb.morphia.annotations.Entity;
-import org.mongodb.morphia.annotations.Field;
-import org.mongodb.morphia.annotations.Index;
-import org.mongodb.morphia.annotations.IndexOptions;
-import org.mongodb.morphia.annotations.Indexes;
+import org.mongodb.morphia.annotations.Id;
 
 import java.util.Set;
 
 @Data
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
-@EqualsAndHashCode(callSuper = true)
 @Entity(value = "featureFlag", noClassnameStored = true)
-@Indexes(@Index(fields = { @Field("name") }, options = @IndexOptions(name = "featureFlagIdx", unique = true)))
 @JsonIgnoreProperties({"obsolete", "accountIds"})
-public class FeatureFlag extends Base {
-  private String name;
+public class FeatureFlag implements PersistentEntity, UpdatedAtAware {
+  @Id private String name;
   private boolean enabled;
   private boolean obsolete;
   private Set<String> accountIds;
+
+  private long lastUpdatedAt;
 }
