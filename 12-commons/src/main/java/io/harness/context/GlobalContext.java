e6eef4abc04a83f61ab00d1b3c943527213bdd11
George Georgiev <george@harness.io>
2019-04-11 12:15:41 -0700

    [HAR-5978]: add setGlobalContextRecord (#6585)
    * [HAR-5978]: add setGlobalContextRecord

--- 12-commons/src/main/java/io/harness/context/GlobalContext.java
+++ 12-commons/src/main/java/io/harness/context/GlobalContext.java
@@ -1,9 +1,16 @@
 package io.harness.context;
 
+import static java.lang.String.format;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class GlobalContext {
+  private static final Logger logger = LoggerFactory.getLogger(GlobalContext.class);
+
   private Map<String, GlobalContextData> map = new ConcurrentHashMap<>();
 
   public GlobalContextData get(String key) {
@@ -13,6 +20,13 @@ public class GlobalContext {
     }
 
     return globalContextData;
+  }
+
+  public void setGlobalContextRecord(GlobalContextData data) {
+    if (map.containsKey(data.getKey())) {
+      logger.error(format("Global data %s is already set. Something is wrong!!!", data.getKey()), new Exception());
+    }
+    map.put(data.getKey(), data);
   }
 
   public void upsertGlobalContextRecord(GlobalContextData data) {
