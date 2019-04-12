d1866d56a59a517a8c35e8c9423981dfde2bb052
George Georgiev <george@harness.io>
2019-04-12 15:31:59 -0700

    [HAR-5978]: add string literal check (#6620)
    * [HAR-5978]: add string literal check

---
+++ tools/checkstyle/src/test/java/io.harness.checks/StringLiteralsCheckTest.java
@@ -0,0 +1,30 @@
+package io.harness.checks;
+
+import com.puppycrawl.tools.checkstyle.AbstractModuleTestSupport;
+import com.puppycrawl.tools.checkstyle.DefaultConfiguration;
+import com.puppycrawl.tools.checkstyle.TreeWalker;
+import org.junit.Test;
+
+public class StringLiteralsCheckTest extends AbstractModuleTestSupport {
+  @Override
+  protected String getPackageLocation() {
+    return "io.harness.checks";
+  }
+
+  public DefaultConfiguration config() {
+    DefaultConfiguration config = createModuleConfig(StringLiteralsCheck.class);
+
+    DefaultConfiguration twConf = createModuleConfig(TreeWalker.class);
+    twConf.addChild(config);
+    twConf.addAttribute("fileExtensions", "jv");
+
+    return twConf;
+  }
+
+  @Test
+  public void testIssues() throws Exception {
+    final String[] expected = {"5:54: Unallowed string literal as a parameter."};
+
+    verify(config(), getPath("StringLiteralsCheckIssues.jv"), expected);
+  }
+}
