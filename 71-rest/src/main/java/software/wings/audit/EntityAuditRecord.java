a03d750ca289df5518225f9a3974adb4f33d010d
Satyam Shanker <satyam.shanker@harness.io>
2019-04-12 17:52:01 -0700

    [CD-2938]: Update Audit Data model based on latest mocks (#6621)

--- 71-rest/src/main/java/software/wings/audit/EntityAuditRecord.java
+++ 71-rest/src/main/java/software/wings/audit/EntityAuditRecord.java
@@ -10,10 +10,27 @@ public class EntityAuditRecord {
   public static final String OPERATION_TYPE_UPDATE = "UPDATE";
   public static final String OPERATION_TYPE_DELETE = "DELETE";
 
+  // Details of the entity being modified
   private String entityId;
   private String entityType;
   private String entityName;
   private String operationType;
   private String entityOldYamlRecordId;
   private String entityNewYamlRecordId;
+
+  // Details of the affected application.
+  // May be NULL for account level entities.
+  // The application column is always there.
+  // Hence maintained separately.
+  private String appId;
+  private String appName;
+
+  // Details of the affected resource.
+  // Mostly, this could be Service / Environment.
+  // Added separately to make indexing and
+  // UI aggregation easier.
+  private String affectedResourceId;
+  private String affectedResourceName;
+  private String affectedResourceType;
+  private String affectedResourceOperation;
 }
