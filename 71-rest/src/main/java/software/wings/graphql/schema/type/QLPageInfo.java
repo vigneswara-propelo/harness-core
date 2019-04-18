578c1c2c9ad3edf2935ca51090be85d9acbc0929
Rushabh Shah <rushabh.shah@harness.io>
2019-04-17 17:18:12 -0700

    [PL-1457]: add hasMore to the pageInfo datastructure as per recommendation by George (#6706)

--- 71-rest/src/main/java/software/wings/graphql/schema/type/QLPageInfo.java
+++ 71-rest/src/main/java/software/wings/graphql/schema/type/QLPageInfo.java
@@ -9,6 +9,7 @@ import lombok.experimental.FieldDefaults;
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class QLPageInfo {
+  Boolean hasMore;
   Integer total;
   Integer limit;
   Integer offset;
