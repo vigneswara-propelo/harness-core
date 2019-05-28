664bc48095e4598a97f2c53d510869e0791d28f5
Ankit Singhal <ankit.singhal@harness.io>
2019-05-28 16:40:49 +0530

    [PL-2030]: Incorporated Pager Duty Usage as a Community violation (#7440)

--- 71-rest/src/main/java/software/wings/licensing/violations/checkers/LiveNotificationViolationChecker.java
+++ 71-rest/src/main/java/software/wings/licensing/violations/checkers/LiveNotificationViolationChecker.java
@@ -26,11 +26,11 @@ import software.wings.service.intfc.UserGroupService;
 import software.wings.service.intfc.WorkflowService;
 import software.wings.sm.StateType;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import javax.validation.constraints.NotNull;
 
 @Singleton
@@ -57,6 +57,7 @@ public class LiveNotificationViolationChecker implements FeatureViolationChecker
                              .addAll(getSlackUsages(accountId))
                              .addAll(getJiraUsages(accountId))
                              .addAll(getSnowUsages(accountId))
+                             .addAll(getPagerDutyUsages(accountId))
                              .build();
     if (isNotEmpty(usages)) {
       featureViolationList =
@@ -66,23 +67,34 @@ public class LiveNotificationViolationChecker implements FeatureViolationChecker
     return CollectionUtils.emptyIfNull(featureViolationList);
   }
 
-  // get usages of Slack under user groups
+  // Get usages of PagerDuty under user groups
+  private Iterable<Usage> getPagerDutyUsages(String accountId) {
+    return getUserGroups(accountId)
+        .stream()
+        .filter(LiveNotificationViolationChecker::hasPagerDuty)
+        .map(LiveNotificationViolationChecker::asUsage)
+        .collect(Collectors.toList());
+  }
+
+  // Get usages of Slack under user groups
   private List<Usage> getSlackUsages(String accountId) {
-    List<Usage> usages = new ArrayList<>();
-    PageRequest<UserGroup> req = aPageRequest().addFilter(ACCOUNT_ID_KEY, Operator.EQ, accountId).build();
+    return getUserGroups(accountId)
+        .stream()
+        .filter(LiveNotificationViolationChecker::hasSlack)
+        .map(LiveNotificationViolationChecker::asUsage)
+        .collect(Collectors.toList());
+  }
 
-    PageResponse<UserGroup> response = userGroupService.list(accountId, req, false);
-    for (UserGroup userGroup : response) {
-      if (hasSlack(userGroup)) {
-        usages.add(Usage.builder()
-                       .entityId(userGroup.getUuid())
-                       .entityType(EntityType.USER_GROUP.toString())
-                       .entityName(userGroup.getName())
-                       .build());
-      }
-    }
+  private static Usage asUsage(UserGroup userGroup) {
+    return Usage.builder()
+        .entityId(userGroup.getUuid())
+        .entityType(EntityType.USER_GROUP.toString())
+        .entityName(userGroup.getName())
+        .build();
+  }
 
-    return usages;
+  private PageResponse<UserGroup> getUserGroups(String accountId) {
+    return userGroupService.list(accountId, new PageRequest<>(), false);
   }
 
   // gets usages of JIRA state under workflows
@@ -98,6 +110,11 @@ public class LiveNotificationViolationChecker implements FeatureViolationChecker
   private static boolean hasSlack(UserGroup userGroup) {
     return userGroup.getNotificationSettings() != null && userGroup.getNotificationSettings().getSlackConfig() != null
         && !EmptyPredicate.isEmpty(userGroup.getNotificationSettings().getSlackConfig().getOutgoingWebhookUrl());
+  }
+
+  private static boolean hasPagerDuty(UserGroup userGroup) {
+    return userGroup.getNotificationSettings() != null
+        && !EmptyPredicate.isEmpty(userGroup.getNotificationSettings().getPagerDutyIntegrationKey());
   }
 
   private List<Workflow> getAllWorkflowsByAccountId(@NotNull String accountId) {
