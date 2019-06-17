cc0c44d640f1c2ca16b79de653244521ff933dec
Praveen Kambam Sugavanam <praveen.sugavanam@harness.io>
2019-06-17 16:47:04 -0700

    [LE-1862]: Add new index with lastUpdatedAt (#7790)

--- 71-rest/src/main/java/software/wings/service/impl/analysis/LogMLAnalysisRecord.java
+++ 71-rest/src/main/java/software/wings/service/impl/analysis/LogMLAnalysisRecord.java
@@ -64,7 +64,11 @@ import java.util.Map;
         @Field("stateExecutionId"), @Field(value = "logCollectionMinute", type = IndexType.DESC)
       }, options = @IndexOptions(name = "stateIdx")), @Index(fields = {
         @Field("cvConfigId"), @Field(value = "logCollectionMinute", type = IndexType.DESC), @Field("deprecated")
-      }, options = @IndexOptions(name = "cvConfigLogCollectionMinIdx"))
+      }, options = @IndexOptions(name = "cvConfigLogCollectionMinIdx")), @Index(fields = {
+        @Field("cvConfigId")
+        , @Field(value = "logCollectionMinute", type = IndexType.DESC), @Field("deprecated"),
+            @Field(value = "lastUpdatedAt", type = IndexType.DESC),
+      }, options = @IndexOptions(name = "cvConfigLogCollectionMinLastUpdatedIdx"))
 })
 @Data
 @NoArgsConstructor
