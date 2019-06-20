286ca5adf78a9108d2f7b75d86aa73ddd4aa9b63
George Georgiev <george@harness.io>
2019-06-20 16:10:46 -0700

    [HAR-6304]: use provider module for test mongo (#7868)
    * [HAR-6304]: use provider module for test mongo

--- 20-persistence-test/src/main/java/io/harness/module/TestMongoModule.java
+++ 20-persistence-test/src/main/java/io/harness/module/TestMongoModule.java
@@ -1,14 +1,16 @@
 package io.harness.module;
 
-import com.google.inject.AbstractModule;
-import com.google.inject.name.Names;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import com.google.inject.name.Named;
 
 import com.deftlabs.lock.mongo.DistributedLockSvc;
+import io.harness.govern.ProviderModule;
 import lombok.extern.slf4j.Slf4j;
 import org.mongodb.morphia.AdvancedDatastore;
 
 @Slf4j
-public class TestMongoModule extends AbstractModule {
+public class TestMongoModule extends ProviderModule {
   private AdvancedDatastore primaryDatastore;
   private DistributedLockSvc distributedLockSvc;
 
@@ -17,9 +19,15 @@ public class TestMongoModule extends AbstractModule {
     this.distributedLockSvc = distributedLockSvc;
   }
 
-  @Override
-  protected void configure() {
-    bind(AdvancedDatastore.class).annotatedWith(Names.named("primaryDatastore")).toInstance(primaryDatastore);
-    bind(DistributedLockSvc.class).toInstance(distributedLockSvc);
+  @Provides
+  @Named("primaryDatastore")
+  public AdvancedDatastore primaryDatastore() {
+    return primaryDatastore;
+  }
+
+  @Provides
+  @Singleton
+  public DistributedLockSvc distributedLockSvc() {
+    return distributedLockSvc;
   }
 }
