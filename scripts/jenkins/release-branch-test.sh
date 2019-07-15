c8fabe4376828acdd4c39355ed65bab7ea97199d
George Georgiev <george@harness.io>
2019-07-15 15:03:49 -0700

    [HAR-6304]: add new team (#8241)
    * [HAR-6304]: add new team

---
+++ scripts/jenkins/release-branch-test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+git log --remotes=origin/release/* --pretty=oneline --abbrev-commit | grep -iE '\[(HAR|CD|PL|LE|CCM)-[0-9]+]:' -o | sort | uniq > release.txt
+git log --remotes=origin/[m]aster --pretty=oneline --abbrev-commit | grep -iE '\[(HAR|CD|PL|LE|CCM)-[0-9]+]:' -o | sort | uniq > master.txt
+
+NOT_MERGED=`comm -23 release.txt master.txt`
+
+if [ ! -z "$NOT_MERGED" ]
+then
+    echo
+    echo
+    echo "Jira issues in release branches that are not reflected in master:"
+    echo ${NOT_MERGED}
+    echo
+    echo
+
+    exit 1
+fi
