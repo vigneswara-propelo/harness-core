42dede1c8651ab352c8554bf27be2aa8c1e11967
George Georgiev <george@harness.io>
2019-07-30 15:17:56 -0700

    [HAR-5978]: remove ArtifactCollectionJob (#8499)
    * [HAR-5978]: remove ArtifactCollectionJob

--- 71-rest/src/test/java/software/wings/scheduler/JobSchedulerTest.java
+++ 71-rest/src/test/java/software/wings/scheduler/JobSchedulerTest.java
@@ -12,20 +12,12 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.quartz.Job;
-import org.quartz.JobBuilder;
-import org.quartz.JobDetail;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.quartz.SchedulerException;
-import org.quartz.SimpleScheduleBuilder;
-import org.quartz.Trigger;
-import org.quartz.TriggerBuilder;
 import org.quartz.impl.matchers.GroupMatcher;
 import software.wings.WingsBaseTest;
 
-/**
- * Created by anubhaw on 10/21/16.
- */
 @Slf4j
 public class JobSchedulerTest extends WingsBaseTest {
   @Inject private BackgroundJobScheduler jobScheduler;
@@ -52,34 +44,6 @@ public class JobSchedulerTest extends WingsBaseTest {
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
       logger.info("Job a execution");
     }
-  }
-
-  @Test
-  @Owner(emails = ANUBHAW)
-  @Category(UnitTests.class)
-  @Ignore("TODO: please provide clear motivation why this test is ignored")
-  public void shouldCreateScheduler() throws SchedulerException, InterruptedException {
-    // define the job and tie it to our HelloJob class
-    JobDetail job = JobBuilder.newJob(ArtifactCollectionJob.class)
-                        .withIdentity("myJob")
-                        .usingJobData("artifactStreamId", "AnKfDNKdReaQoDVLJbeWpQ")
-                        .usingJobData("appId", "vwWDnKjhSzml8GuBPQq-6Q")
-                        .build();
-
-    // Trigger the job to run now, and then every 40 seconds
-    Trigger trigger =
-        TriggerBuilder.newTrigger()
-            .withIdentity("myTrigger", "group1")
-            .startNow()
-            .withSchedule(SimpleScheduleBuilder.simpleSchedule().withIntervalInSeconds(20000).withRepeatCount(1))
-            .build();
-
-    Trigger trigger2 = TriggerBuilder.newTrigger().withIdentity("a", "v").startNow().build();
-
-    // Tell quartz to schedule the job using our trigger
-    jobScheduler.getScheduler().scheduleJob(job, trigger2);
-    Thread.sleep(1000000);
-    logger.info("Completed");
   }
 
   @Test
