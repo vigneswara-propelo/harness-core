e83d5b44d34973db2f47c38dc802ce076c0440c9
Sowmya K <sowmya.k@harness.io>
2019-08-05 22:24:39 +0530

    [LE-2102]: Migrate metric templates when migration account is migrated from freemium to prod (#8415)

--- 71-rest/src/main/java/software/wings/service/impl/analysis/TimeSeriesMetricTemplates.java
+++ 71-rest/src/main/java/software/wings/service/impl/analysis/TimeSeriesMetricTemplates.java
@@ -4,6 +4,7 @@ import static io.harness.data.structure.EmptyPredicate.isEmpty;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.github.reinert.jjschema.SchemaIgnore;
+import io.harness.annotation.HarnessExportableEntity;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -32,6 +33,7 @@ import java.util.Map;
     @Field("stateExecutionId"), @Field("cvConfigId")
   }, options = @IndexOptions(unique = true, name = "unique_Idx"))
 })
+@HarnessExportableEntity
 @Data
 @EqualsAndHashCode(callSuper = false)
 @FieldNameConstants(innerTypeName = "TimeSeriesMetricTemplatesKeys")
