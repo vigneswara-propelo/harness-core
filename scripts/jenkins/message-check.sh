955dc41630f9c7c0b8c137bc53ac9da98762b56e
George Georgiev <george@harness.io>
2019-08-26 11:45:10 -0700

    [CE-3]: Add ce to the accepted project prefixes (#8923)
    * [CE-3]: Add ce to the accepted project prefixes

---
+++ scripts/jenkins/message-check.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set +e
+
+PR_MESSAGE=`echo "${ghprbPullTitle}" | grep -iE '\[(HAR|CD|PL|LE|CCM|CE|SWAT|ER|DOC)-[0-9]+]:'`
+
+if [ -z "$PR_MESSAGE" ]
+then
+    echo The PR title \"${ghprbPullTitle}\"
+    echo "does not match the expectations"
+    echo 'Make sure that your message starts with [HAR|CD|PL|LE|CCM|CE|SWAT|ER|DOC-<number>]: <description>'
+    exit 1
+fi
+
+KEY=`echo "${ghprbPullTitle}" | grep -o -iqE '(HAR|CD|PL|LE|CCM|CE|SWAT|ER|DOC)-[0-9]+'`
+
+
+#TODO: enable priorities check
+
+#AUTHOR=`echo ${ghprbPullAuthorEmail} | sed 's/\(.*\)@harness.io$/\1/g'`
+
+#curl -v https://harness.atlassian.net/rest/api/3/search?jql=filter=12111%20AND%20assignee=$AUTHOR\&fields=key --user $JIRA_USERNAME:$JIRA_PASSWORD -o result.txt > /dev/null 2>&1
+
+#PRIORITY_ISSUES=`cat result.txt | tr "," "\n" | grep -o -e "HAR-[0-9]+"`
+
+#if [ ! -z "$PRIORITY_ISSUES" ]
+#then
+#    if ! echo "${PRIORITY_ISSUES}" | grep ${KEY}
+#    then
+#        echo The issue ${KEY} is not your imidiate priority.
+#        echo Please first address the folowing issues: ${PRIORITY_ISSUES}
+#        #exit 1
+#    fi
+#fi
--- scripts/jenkins/release-branch-test.sh
+++ scripts/jenkins/release-branch-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git log --remotes=origin/release/* --pretty=oneline --abbrev-commit | grep -iE '\[(HAR|CD|PL|LE|CCM)-[0-9]+]:' -o | sort | uniq > release.txt
-git log --remotes=origin/[m]aster --pretty=oneline --abbrev-commit | grep -iE '\[(HAR|CD|PL|LE|CCM)-[0-9]+]:' -o | sort | uniq > master.txt
+git log --remotes=origin/release/* --pretty=oneline --abbrev-commit | grep -iE '\[(HAR|CD|PL|LE|CCM|CE|SWAT|ER|DOC)-[0-9]+]:' -o | sort | uniq > release.txt
+git log --remotes=origin/[m]aster --pretty=oneline --abbrev-commit | grep -iE '\[(HAR|CD|PL|LE|CCM|CE|SWAT|ER|DOC)-[0-9]+]:' -o | sort | uniq > master.txt
 
 NOT_MERGED=`comm -23 release.txt master.txt`
 
