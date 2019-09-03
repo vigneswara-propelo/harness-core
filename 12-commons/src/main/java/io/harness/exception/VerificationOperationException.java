f580e8a37e0f2551a94760e9821d51ad94a81581
Raghvendra Singh <raghu@harness.io>
2019-09-03 15:41:21 -0700

    [LE-2455]: Create verification specific exception (#9045)
    * [LE-2455]: Create verification specific exception
    
    * [LE-2455]: Create verification specific exception

---
+++ 12-commons/src/main/java/io/harness/exception/VerificationOperationException.java
@@ -0,0 +1,31 @@
+package io.harness.exception;
+
+import io.harness.eraro.ErrorCode;
+import io.harness.eraro.Level;
+
+import java.util.EnumSet;
+
+public class VerificationOperationException extends WingsException {
+  private static final String REASON_KEY = "reason";
+
+  public VerificationOperationException(ErrorCode errorCode, String reason) {
+    super(null, null, errorCode, Level.ERROR, null, null);
+    super.param(REASON_KEY, reason);
+  }
+
+  public VerificationOperationException(ErrorCode errorCode, String reason, Throwable cause) {
+    super(null, cause, errorCode, Level.ERROR, null, null);
+    super.param(REASON_KEY, reason);
+  }
+
+  public VerificationOperationException(ErrorCode errorCode, String reason, EnumSet<ReportTarget> reportTargets) {
+    super(null, null, errorCode, Level.ERROR, reportTargets, null);
+    super.param(REASON_KEY, reason);
+  }
+
+  public VerificationOperationException(
+      ErrorCode errorCode, String reason, Throwable cause, EnumSet<ReportTarget> reportTargets) {
+    super(null, cause, errorCode, Level.ERROR, reportTargets, null);
+    super.param(REASON_KEY, reason);
+  }
+}
