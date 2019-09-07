9d7d48aa516f4e85a1476d7a20b2c9ba5c4bcce8
Yogesh Chauhan <yogesh.chauhan@harness.io>
2019-09-07 10:37:46 +0530

    [CD-5513]: Migration to remove service Infra yaml folder (#8941)
    * [CD-5513]: Initial Commit
    
    * [CD-5513]: Add logging

---
+++ 71-rest/src/main/java/migrations/all/RemoveServiceInfraFolder.java
@@ -0,0 +1,69 @@
+package migrations.all;
+
+import static software.wings.beans.yaml.YamlConstants.APPLICATIONS_FOLDER;
+import static software.wings.beans.yaml.YamlConstants.ENVIRONMENTS_FOLDER;
+import static software.wings.beans.yaml.YamlConstants.INFRA_MAPPING_FOLDER;
+import static software.wings.beans.yaml.YamlConstants.SETUP_FOLDER;
+
+import com.google.common.base.Joiner;
+import com.google.inject.Inject;
+
+import io.harness.exception.ExceptionUtils;
+import lombok.extern.slf4j.Slf4j;
+import migrations.Migration;
+import org.apache.commons.lang3.StringUtils;
+import software.wings.beans.Application;
+import software.wings.beans.Environment;
+import software.wings.beans.yaml.Change.ChangeType;
+import software.wings.beans.yaml.GitFileChange;
+import software.wings.beans.yaml.GitFileChange.Builder;
+import software.wings.service.intfc.AppService;
+import software.wings.service.intfc.EnvironmentService;
+import software.wings.service.intfc.yaml.YamlChangeSetService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+public class RemoveServiceInfraFolder implements Migration {
+  @Inject private AppService appService;
+  @Inject private EnvironmentService environmentService;
+  @Inject private YamlChangeSetService yamlChangeSetService;
+  private final String accountId = "kmpySmUISimoRrJL6NL73w";
+  private final String DEBUG_LINE = "SERVICE_INFRA_YAML:";
+
+  public void migrate() {
+    final List<Application> applications = appService.getAppsByAccountId(accountId);
+    List<GitFileChange> gitFileChanges = new ArrayList<>();
+    for (Application app : applications) {
+      logger.info(Joiner.on(StringUtils.SPACE).join(DEBUG_LINE, "Starting migration for app", app.getUuid()));
+      try {
+        List<Environment> environments = environmentService.getEnvByApp(app.getUuid());
+
+        gitFileChanges.clear();
+
+        environments.forEach(environment -> {
+          GitFileChange gitFileChange =
+              generateGitFileChangeForInfraMappingDelete(accountId, app.getName(), environment.getName());
+          gitFileChanges.add(gitFileChange);
+          logger.info(Joiner.on(StringUtils.SPACE)
+                          .join(DEBUG_LINE, "Adding to git file changeSet", gitFileChange.getFilePath()));
+        });
+        yamlChangeSetService.saveChangeSet(accountId, gitFileChanges, app);
+      } catch (Exception ex) {
+        logger.error(
+            Joiner.on(StringUtils.SPACE).join(DEBUG_LINE, ExceptionUtils.getMessage(ex), "app", app.getUuid()));
+      }
+      logger.info(Joiner.on(StringUtils.SPACE).join(DEBUG_LINE, "Finished migration for app", app.getUuid()));
+    }
+  }
+
+  private GitFileChange generateGitFileChangeForInfraMappingDelete(String accountId, String appName, String envName) {
+    return Builder.aGitFileChange()
+        .withAccountId(accountId)
+        .withChangeType(ChangeType.DELETE)
+        .withFilePath(Joiner.on("/").join(
+            SETUP_FOLDER, APPLICATIONS_FOLDER, appName, ENVIRONMENTS_FOLDER, envName, INFRA_MAPPING_FOLDER))
+        .build();
+  }
+}
