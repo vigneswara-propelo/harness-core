18d3ce7846e901ef57c1f985d8039b0fc3c04c01
Satyam Shanker <satyam.shanker@harness.io>
2019-09-17 17:32:16 -0700

    [CD-6114]: Resolve infra definition id expression correctly (#9310)

--- 71-rest/src/main/java/software/wings/sm/states/PhaseSubWorkflow.java
+++ 71-rest/src/main/java/software/wings/sm/states/PhaseSubWorkflow.java
@@ -139,7 +139,7 @@ public class PhaseSubWorkflow extends SubWorkflowState {
         Validator.notNullCheck("Service might have been deleted", service, USER);
       }
 
-      if (infraDefinitionId != null) {
+      if (infraDefinitionIdExpression == null && infraDefinitionId != null) {
         infrastructureDefinition = infrastructureDefinitionService.get(app.getAppId(), infraDefinitionId);
         Validator.notNullCheck("Infra Definition not found, check if it is deleted", infrastructureDefinition, USER);
       }
