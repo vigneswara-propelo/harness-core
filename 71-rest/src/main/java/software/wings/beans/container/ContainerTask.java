0b8911bea871476feb4ab3fc093e98d7533a7311
Mark Lu <mark.lu@harness.io>
2019-09-26 14:08:25 -0700

    [PL-3712]: Mark ContainerTask entity as HarnessExportableEntity (#9484)

--- 71-rest/src/main/java/software/wings/beans/container/ContainerTask.java
+++ 71-rest/src/main/java/software/wings/beans/container/ContainerTask.java
@@ -5,6 +5,7 @@ import static software.wings.yaml.YamlHelper.trimYaml;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.github.reinert.jjschema.SchemaIgnore;
+import io.harness.annotation.HarnessExportableEntity;
 import io.harness.beans.EmbeddedUser;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -30,6 +31,7 @@ import java.util.regex.Pattern;
     fields = { @Field("serviceId")
                , @Field("deploymentType") }))
 @Entity("containerTasks")
+@HarnessExportableEntity
 @FieldNameConstants(innerTypeName = "ContainerTaskKeys")
 public abstract class ContainerTask extends DeploymentSpecification {
   static final String DOCKER_IMAGE_NAME_PLACEHOLDER_REGEX = "\\$\\{DOCKER_IMAGE_NAME}";
