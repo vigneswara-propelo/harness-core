63827f9bd8807b7c0752231552276ec03fc356db
Rushabh Shah <rushabh.shah@harness.io>
2019-10-01 16:04:30 -0700

    [PL-3585]: Unit test for UsageMetricsEventPublisher and DeploymentEvent (#9555)
    * [PL-3585]: initial checkin
    
    * [PL-3585]: remove unwanted module
    
    * [PL-3585]: remove unwanted file
    
    * [PL-3585]: fix checkstyle warning
    
    * [PL-3585]: fix warning

---
+++ 71-rest/src/test/java/io/harness/event/usagemetrics/UsageMetricsEventPublisherUnitTest.java
@@ -0,0 +1,26 @@
+package io.harness.event.usagemetrics;
+
+import com.google.inject.Inject;
+
+import io.harness.category.element.UnitTests;
+import io.harness.event.usagemetrics.UsageMetricsTestUtils.UsageMetricsTestKeys;
+import io.harness.rule.OwnerRule.Owner;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import software.wings.WingsBaseTest;
+import software.wings.api.DeploymentTimeSeriesEvent;
+import software.wings.beans.WorkflowExecution;
+
+public class UsageMetricsEventPublisherUnitTest extends WingsBaseTest {
+  @Inject UsageMetricsEventPublisher usageMetricsEventPublisher;
+
+  @Test
+  @Owner(emails = "rushabh@harness.io")
+  @Category(UnitTests.class)
+  public void testConstructDeploymentEvent() {
+    WorkflowExecution workflowExecution = UsageMetricsTestUtils.generateWorkflowExecution(0);
+    DeploymentTimeSeriesEvent timeSeriesEvent = usageMetricsEventPublisher.constructDeploymentTimeSeriesEvent(
+        UsageMetricsTestKeys.ACCOUNTID, workflowExecution);
+    UsageMetricsTestUtils.validateTimeSeriesEventInfo(timeSeriesEvent, 0);
+  }
+}
---
+++ 71-rest/src/test/java/io/harness/event/usagemetrics/UsageMetricsTestUtils.java
@@ -0,0 +1,87 @@
+package io.harness.event.usagemetrics;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
+import io.harness.beans.EmbeddedUser;
+import io.harness.beans.ExecutionStatus;
+import io.harness.event.timeseries.processor.EventProcessor;
+import lombok.experimental.FieldNameConstants;
+import org.assertj.core.api.Assertions;
+import software.wings.api.DeploymentTimeSeriesEvent;
+import software.wings.beans.EnvSummary;
+import software.wings.beans.Environment.EnvironmentType;
+import software.wings.beans.PipelineExecution.Builder;
+import software.wings.beans.WorkflowExecution;
+import software.wings.beans.artifact.Artifact;
+import software.wings.beans.artifact.Artifact.ArtifactKeys;
+import software.wings.service.impl.event.timeseries.TimeSeriesEventInfo;
+
+import java.util.Arrays;
+import java.util.Map;
+
+@FieldNameConstants(innerTypeName = "UsageMetricsTestKeys")
+public class UsageMetricsTestUtils {
+  private String ACCOUNTID;
+  private String APPID;
+  private String WORKFLOWID;
+  private String CLOUDPROVIDER1, CLOUDPROVIDER2;
+  private String PIPELINEID;
+  private String PIPELINEEXECUTIONID;
+  private String SERVICE1;
+  private String ENV1;
+  private String TRIGGER1;
+  private String USER1;
+  private String EXECUTIONID;
+
+  public static void validateTimeSeriesEventInfo(DeploymentTimeSeriesEvent timeSeriesEvent, int number) {
+    final TimeSeriesEventInfo timeSeriesEventInfo = timeSeriesEvent.getTimeSeriesEventInfo();
+    Assertions.assertThat(timeSeriesEventInfo).isNotNull();
+    Assertions.assertThat(timeSeriesEventInfo.getAccountId()).isEqualTo(UsageMetricsTestKeys.ACCOUNTID);
+    Assertions.assertThat(timeSeriesEventInfo.getStringData().get(EventProcessor.EXECUTIONID))
+        .isEqualTo(UsageMetricsTestKeys.EXECUTIONID + number);
+    Assertions.assertThat(timeSeriesEventInfo.getLongData().get(EventProcessor.STARTTIME)).isEqualTo(100L);
+    Assertions.assertThat(timeSeriesEventInfo.getLongData().get(EventProcessor.ENDTIME)).isEqualTo(200L);
+    Assertions.assertThat(timeSeriesEventInfo.getLongData().get(EventProcessor.DURATION)).isEqualTo(100L);
+    Assertions.assertThat(timeSeriesEventInfo.getLongData().get(EventProcessor.ROLLBACK_DURATION)).isEqualTo(100L);
+    Assertions.assertThat(timeSeriesEventInfo.getStringData().get(EventProcessor.APPID))
+        .isEqualTo(UsageMetricsTestKeys.APPID);
+    Assertions.assertThat(timeSeriesEventInfo.getStringData().get(EventProcessor.STATUS))
+        .isEqualTo(ExecutionStatus.SUCCESS.name());
+    Assertions.assertThat(timeSeriesEventInfo.getStringData().get(EventProcessor.PARENT_EXECUTION))
+        .contains(UsageMetricsTestKeys.PIPELINEEXECUTIONID);
+    Assertions.assertThat(timeSeriesEventInfo.getStringData().get(EventProcessor.PIPELINE))
+        .contains(UsageMetricsTestKeys.PIPELINEID);
+    Assertions.assertThat(timeSeriesEventInfo.getListData().get(EventProcessor.CLOUD_PROVIDER_LIST))
+        .contains(UsageMetricsTestKeys.CLOUDPROVIDER1, UsageMetricsTestKeys.CLOUDPROVIDER2);
+    Assertions.assertThat(timeSeriesEventInfo.getStringData().get(EventProcessor.TRIGGER_ID))
+        .contains(UsageMetricsTestKeys.TRIGGER1);
+    Assertions.assertThat(timeSeriesEventInfo.getStringData().get(EventProcessor.TRIGGERED_BY))
+        .contains(UsageMetricsTestKeys.USER1);
+  }
+
+  public static WorkflowExecution generateWorkflowExecution(int number) {
+    final Map<String, String> artifactBuildNumber = Maps.newHashMap();
+    artifactBuildNumber.put(ArtifactKeys.metadata_buildNo, "123");
+    return WorkflowExecution.builder()
+        .uuid(UsageMetricsTestKeys.EXECUTIONID + number)
+        .accountId(UsageMetricsTestKeys.ACCOUNTID)
+        .appId(UsageMetricsTestKeys.APPID)
+        .status(ExecutionStatus.SUCCESS)
+        .workflowId(UsageMetricsTestKeys.WORKFLOWID)
+        .startTs(100L)
+        .endTs(200L)
+        .cloudProviderIds(Lists.newArrayList(UsageMetricsTestKeys.CLOUDPROVIDER1, UsageMetricsTestKeys.CLOUDPROVIDER2))
+        .environments(Lists.newArrayList(EnvSummary.builder().environmentType(EnvironmentType.PROD).build()))
+        .pipelineExecution(Builder.aPipelineExecution().withPipelineId(UsageMetricsTestKeys.PIPELINEID).build())
+        .pipelineExecutionId(UsageMetricsTestKeys.PIPELINEEXECUTIONID)
+        .artifacts(Lists.newArrayList(Artifact.Builder.anArtifact().withMetadata(artifactBuildNumber).build()))
+        .serviceIds(Arrays.asList(UsageMetricsTestKeys.SERVICE1))
+        .envIds(Arrays.asList(UsageMetricsTestKeys.ENV1))
+        .triggeredBy(EmbeddedUser.builder().uuid(UsageMetricsTestKeys.USER1).build())
+        .deploymentTriggerId(UsageMetricsTestKeys.TRIGGER1)
+        .duration(100L)
+        .rollbackDuration(100L)
+        .build();
+  }
+}
