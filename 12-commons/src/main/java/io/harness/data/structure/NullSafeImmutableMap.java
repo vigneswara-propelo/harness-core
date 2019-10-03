418ff50c8c1b3ed66cf7f4ea2af8453c1f35875b
George Georgiev <george@harness.io>
2019-10-03 14:57:44 -0700

    [CE-1]: fix NullSafeImmutableMap sonar issues (#9606)
    * [CE-1]: fix NullSafeImmutableMap sonar issues

--- 12-commons/src/main/java/io/harness/data/structure/NullSafeImmutableMap.java
+++ 12-commons/src/main/java/io/harness/data/structure/NullSafeImmutableMap.java
@@ -1,13 +1,21 @@
 package io.harness.data.structure;
 
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+// Note that the name of the class is chosen to allow for a simple replacement of the original ImmutableMap
+// ImmutableMap.builder()... simply becomes NullSafeImmutableMap.builder()...
+// Technically it can inherit the ImmutableMap and became a an object class, but this will serve no purpose
+// since it does not extend the functionality of the class.
 public class NullSafeImmutableMap {
-  public static class Builder<K, V> extends com.google.common.collect.ImmutableMap.Builder<K, V> {
-    public Builder<K, V> put(K key, V value) {
+  public static class NullSafeBuilder<K, V> extends com.google.common.collect.ImmutableMap.Builder<K, V> {
+    @Override
+    public NullSafeBuilder<K, V> put(K key, V value) {
       super.put(key, value);
       return this;
     }
 
-    public Builder<K, V> putIfNotNull(K key, V value) {
+    public NullSafeBuilder<K, V> putIfNotNull(K key, V value) {
       if (value != null) {
         put(key, value);
       }
@@ -15,7 +23,7 @@ public class NullSafeImmutableMap {
     }
   }
 
-  public static <K, V> Builder builder() {
-    return new Builder<K, V>();
+  public static <K, V> NullSafeBuilder builder() {
+    return new NullSafeBuilder<K, V>();
   }
 }
