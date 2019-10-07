3fdc4fe3cf44e030c3277d4c84fbe8df7d1f9ade
Vaibhav Singhal <vaibhav.si@harness.io>
2019-10-07 11:14:53 +0530

    [CD-6558]: added more tests (#9630)
    * refactored method to fix sonar check
    
    * rearranged imports
    
    * added more tests

--- 71-rest/src/test/java/software/wings/service/impl/StateExecutionServiceImplTest.java
+++ 71-rest/src/test/java/software/wings/service/impl/StateExecutionServiceImplTest.java
@@ -4,6 +4,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.spy;
+import static software.wings.utils.WingsTestConstants.ACCOUNT_ID;
+import static software.wings.utils.WingsTestConstants.APP_ID;
+import static software.wings.utils.WingsTestConstants.INFRA_DEFINITION_ID;
+import static software.wings.utils.WingsTestConstants.INFRA_MAPPING_ID;
 
 import io.harness.category.element.UnitTests;
 import org.joor.Reflect;
@@ -23,13 +27,11 @@ import software.wings.sm.PhaseExecutionSummary;
 import software.wings.sm.PhaseStepExecutionSummary;
 import software.wings.sm.StateExecutionInstance;
 import software.wings.sm.StateExecutionInstance.Builder;
-import software.wings.utils.WingsTestConstants;
 
 import java.util.Collections;
 import java.util.List;
 
 public class StateExecutionServiceImplTest extends WingsBaseTest {
-  private static final String INFRA_DEFINITION_ID = "INFRA_DEFINITION_ID";
   private static final String RANDOM = "RANDOM";
 
   @Mock private FeatureFlagService featureFlagService;
@@ -60,17 +62,61 @@ public class StateExecutionServiceImplTest extends WingsBaseTest {
                                                 .withInfraDefinitionId(INFRA_DEFINITION_ID)
                                                 .withPhaseExecutionSummary(phaseExecutionSummary)
                                                 .build();
-    StateExecutionInstance stateExecutionInstance =
-        Builder.aStateExecutionInstance().appId(WingsTestConstants.APP_ID).build();
+    StateExecutionInstance stateExecutionInstance = Builder.aStateExecutionInstance().appId(APP_ID).build();
     doReturn(Collections.singletonList(phaseExecutionData))
         .when(stateExecutionService)
         .fetchPhaseExecutionData(any(), any(), any(), any());
-    doReturn(WingsTestConstants.ACCOUNT_ID).when(appService).getAccountIdByAppId(any());
+    doReturn(ACCOUNT_ID).when(appService).getAccountIdByAppId(any());
     doReturn(true).when(featureFlagService).isEnabled(any(), any());
 
     List<ServiceInstance> hostExclusionList =
         stateExecutionService.getHostExclusionList(stateExecutionInstance, phaseElement, null);
 
-    assertThat(serviceInstanceList).isEqualTo(hostExclusionList);
+    assertThat(hostExclusionList).isEqualTo(serviceInstanceList);
+  }
+
+  @Test
+  @Category(UnitTests.class)
+  public void shouldReturnEmptyWhenNoPreviousPhases() {
+    PhaseElement phaseElement = PhaseElement.builder().infraDefinitionId(INFRA_DEFINITION_ID).build();
+    StateExecutionInstance stateExecutionInstance = Builder.aStateExecutionInstance().appId(APP_ID).build();
+    doReturn(Collections.emptyList()).when(stateExecutionService).fetchPhaseExecutionData(any(), any(), any(), any());
+    doReturn(ACCOUNT_ID).when(appService).getAccountIdByAppId(any());
+    doReturn(true).when(featureFlagService).isEnabled(any(), any());
+
+    List<ServiceInstance> hostExclusionList =
+        stateExecutionService.getHostExclusionList(stateExecutionInstance, phaseElement, null);
+
+    assertThat(hostExclusionList).isEqualTo(Collections.emptyList());
+  }
+
+  @Test
+  @Category(UnitTests.class)
+  public void shouldReturnHostsForMultiplePhasesForFeatureFlagOff() {
+    PhaseElement phaseElement = PhaseElement.builder().infraMappingId(INFRA_MAPPING_ID).build();
+    List<ServiceInstance> serviceInstanceList =
+        Collections.singletonList(ServiceInstance.Builder.aServiceInstance().build());
+    SelectNodeStepExecutionSummary selectNodeStepExecutionSummary = new SelectNodeStepExecutionSummary();
+    selectNodeStepExecutionSummary.setExcludeSelectedHostsFromFuturePhases(true);
+    selectNodeStepExecutionSummary.setServiceInstanceList(serviceInstanceList);
+    PhaseStepExecutionSummary phaseStepExecutionSummary = new PhaseStepExecutionSummary();
+    phaseStepExecutionSummary.setStepExecutionSummaryList(Collections.singletonList(selectNodeStepExecutionSummary));
+    PhaseExecutionSummary phaseExecutionSummary = new PhaseExecutionSummary();
+    phaseExecutionSummary.setPhaseStepExecutionSummaryMap(Collections.singletonMap(RANDOM, phaseStepExecutionSummary));
+    PhaseExecutionData phaseExecutionData = PhaseExecutionDataBuilder.aPhaseExecutionData()
+                                                .withInfraMappingId(INFRA_MAPPING_ID)
+                                                .withPhaseExecutionSummary(phaseExecutionSummary)
+                                                .build();
+    StateExecutionInstance stateExecutionInstance = Builder.aStateExecutionInstance().appId(APP_ID).build();
+    doReturn(Collections.singletonList(phaseExecutionData))
+        .when(stateExecutionService)
+        .fetchPhaseExecutionData(any(), any(), any(), any());
+    doReturn(ACCOUNT_ID).when(appService).getAccountIdByAppId(any());
+    doReturn(false).when(featureFlagService).isEnabled(any(), any());
+
+    List<ServiceInstance> hostExclusionList =
+        stateExecutionService.getHostExclusionList(stateExecutionInstance, phaseElement, null);
+
+    assertThat(hostExclusionList).isEqualTo(serviceInstanceList);
   }
 }
