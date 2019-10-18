8856e18c2edd9f3bc630cae41162a610a431e973
Jatin Shridhar <jatin@harness.io>
2019-10-19 00:34:42 +0530

    [PL-4021]: add post-install checks script (#9915)

--- on-prem/harness_disconnected_on_prem_pov_final/post_install_scripts/post_install_commands.sh
+++ on-prem/harness_disconnected_on_prem_pov_final/post_install_scripts/post_install_commands.sh
@@ -2,17 +2,87 @@
 
 echo "Checking whether docker containers are up and running..."
 
-container_names=("mongoContainer" "harness-proxy", "harnessManager", "verificationService", "harness_ui", "learningEngine")
+container_names=("mongoContainer" "harness-proxy" "harnessManager" "verificationService" "harness_ui" "learningEngine")
 
 for container_name in "${container_names[@]}"
 do
-   if [ $(docker inspect -f '{{.State.Running}}' $container_name) = "true" ]; then
-      echo "docker container $container_name is running successfully"
+   if [[ $(docker inspect -f '{{.State.Running}}' ${container_name}) = "true" ]]; then
+      echo "✓ docker container $container_name is running successfully"
    else
-      echo "docker container $container_name failed to start"
+      echo "✗ docker container $container_name failed to start"
       exit -1
    fi
 done
+
+INFRA_PROPERTY_FILE=../inframapping.properties
+
+source ../utils.sh
+
+function checkUIisUp() {
+   host1=$(getProperty "$INFRA_PROPERTY_FILE" "HOST1_IP_ADDRESS")
+
+   url="${host1}:7143"
+
+   curl -sI -m 5 ${url} | grep -q "200 OK"
+   if [[ $? -eq 0 ]]; then
+       echo "✓ UI is up"
+   else
+       echo "✗ UI is NOT up"
+   fi
+}
+
+function checkManagerIsHealthy() {
+   host1=$(getProperty "$INFRA_PROPERTY_FILE" "HOST1_IP_ADDRESS")
+
+   url="${host1}:7143/api/health"
+   curl -s -m 5 ${url} | grep -q "healthy"
+
+   if [[ $? -eq 0 ]]; then
+       echo "✓ Manager is healthy"
+   else
+       echo "✗ Manager is not healthy"
+   fi
+}
+
+function checkVerificationServiceIsHealthy() {
+   host1=$(getProperty "$INFRA_PROPERTY_FILE" "HOST1_IP_ADDRESS")
+
+   url="${host1}:7143/verification/health"
+   curl -s -m 5 ${url} | grep -q "healthy"
+
+   if [[ $? -eq 0 ]]; then
+       echo "✓ Verification Service is healthy"
+   else
+       echo "✗ Verification Service is not healthy"
+   fi
+}
+
+function checkDelegateAndApiVersionMatch() {
+   host1=$(getProperty "$INFRA_PROPERTY_FILE" "HOST1_IP_ADDRESS")
+
+   url="${host1}:7143/storage/wingsdelegates/delegateprod.txt"
+   curl -sI -m 5 ${url} | grep -q "200 OK"
+
+   if [[ $? -eq 0 ]]; then
+       echo "✓ Delegate version API is up"
+   else
+       echo "✗ Delegate version API failed."
+   fi
+   delegateVersion=$(curl -s -m 5 ${url} | awk '{print $1}')
+   apiVersionEndpoint="${host1}:7143/api/version"
+   curl -s -m 5 ${apiVersionEndpoint} | grep -q "${delegateVersion}"
+   if [[ $? -eq 0 ]]; then
+       echo "✓ Delegate version and API version match."
+   else
+       echo "✗ Delegate version did not match API version."
+   fi
+}
+
+echo "Checking service API reachability."
+checkUIisUp
+checkManagerIsHealthy
+checkVerificationServiceIsHealthy
+checkDelegateAndApiVersionMatch
 
 
 
