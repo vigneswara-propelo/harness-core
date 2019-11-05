16e22520d0c49baea84d37f35c3950713b88eb2b
Vikas Naiyar <vikas.naiyar@harness.io>
2019-11-05 13:55:47 +0530

    [CCM-179]: Deploy spring batch in QA env (#10192)

--- dockerization/batch-processing/scripts/replace_configs.sh
+++ dockerization/batch-processing/scripts/replace_configs.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
+
 CONFIG_FILE=/opt/harness/batch-processing-config.yml
+
 if [[ "" != "$MONGO_URI" ]]; then
   yq write -i $CONFIG_FILE mongo.uri "$MONGO_URI"
+fi
+
+if [[ "" != "$TIMESCALEDB_URI" ]]; then
+  yq write -i $CONFIG_FILE timescaledb.timescaledbUrl "$TIMESCALEDB_URI"
+fi
+
+if [[ "" != "$TIMESCALEDB_USERNAME" ]]; then
+  yq write -i $CONFIG_FILE timescaledb.timescaledbUsername "$TIMESCALEDB_USERNAME"
+fi
+
+if [[ "" != "$TIMESCALEDB_PASSWORD" ]]; then
+  yq write -i $CONFIG_FILE timescaledb.timescaledbPassword "$TIMESCALEDB_PASSWORD"
 fi
