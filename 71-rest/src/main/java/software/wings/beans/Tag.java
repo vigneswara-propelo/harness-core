c3ad83b4fb3f806ea05996fbbfc22d6107caed0f
Satyam Shanker <satyam.shanker@harness.io>
2019-11-12 10:33:52 -0800

    [CD-7261]: Fix NPE in Lambda state when Tags are used (#10275)

--- 71-rest/src/main/java/software/wings/beans/Tag.java
+++ 71-rest/src/main/java/software/wings/beans/Tag.java
@@ -1,11 +1,15 @@
 package software.wings.beans;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode
 public class Tag {
   private String key;
