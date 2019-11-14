65c915a9942532077b1bade5b9eb99661cbc13d8
George Georgiev <george@harness.io>
2019-11-14 08:51:50 -0800

    [CE-8]: envvars the not merged (#10373)

--- scripts/jenkins/release-branch-test.sh
+++ scripts/jenkins/release-branch-test.sh
@@ -3,18 +3,5 @@
 git log --remotes=origin/release/* --pretty=oneline --abbrev-commit | grep -iE '\[(CCM|CD|CE|DOC|ER|HAR|LE|PL|SEC|SWAT)-[0-9]+]:' -o | sort | uniq > release.txt
 git log --remotes=origin/[m]aster --pretty=oneline --abbrev-commit | grep -iE '\[(CCM|CD|CE|DOC|ER|HAR|LE|PL|SEC|SWAT)-[0-9]+]:' -o | sort | uniq > master.txt
 
-NOT_MERGED=`comm -23 release.txt master.txt`
-
-if [ ! -z "$NOT_MERGED" ]
-then
-    echo
-    echo
-    echo "Jira issues in release branches that are not reflected in master:"
-    echo ${NOT_MERGED}
-    echo
-    echo
-
-    echo NOT_MERGED="${NOT_MERGED}" > envvars
-
-    exit 1
-fi
+NOT_MERGED=`comm -23 release.txt master.txt | tr '\n' ' '`
+echo NOT_MERGED="${NOT_MERGED}" > envvars
