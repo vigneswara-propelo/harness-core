b106d6e8398200353740b223b73562b6511465f0
K Rohit Reddy <rohit.reddy@harness.io>
2019-11-25 14:44:23 +0530

    [CCM-196]: Added Query Filters for InstanceData Mongo Reader (#10548)
    * [CCM-196]: Added Query Filters for InstanceData Mongo Reader

--- 78-batch-processing/src/main/java/io/harness/batch/processing/billing/reader/InstanceDataMongoEventReader.java
+++ 78-batch-processing/src/main/java/io/harness/batch/processing/billing/reader/InstanceDataMongoEventReader.java
@@ -10,7 +10,10 @@ import org.springframework.batch.item.data.MongoItemReader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.Date;
 
 @Slf4j
 @Singleton
@@ -25,6 +28,21 @@ public class InstanceDataMongoEventReader implements InstanceDataEventReader {
     reader.setTemplate(mongoTemplate);
     reader.setTargetType(InstanceData.class);
     Query query = new Query();
+    query.addCriteria(
+        Criteria.where(InstanceDataKeys.usageStartTime)
+            .exists(true)
+            .orOperator(
+                Criteria.where(InstanceDataKeys.usageStartTime)
+                    .exists(true)
+                    .andOperator(Criteria.where(InstanceDataKeys.usageStartTime)
+                                     .lt(new Date(startDate))
+                                     .orOperator(Criteria.where(InstanceDataKeys.usageStopTime).exists(false),
+                                         Criteria.where(InstanceDataKeys.usageStopTime).gt(new Date(startDate)))),
+                Criteria.where(InstanceDataKeys.usageStartTime)
+                    .exists(true)
+                    .andOperator(Criteria.where(InstanceDataKeys.usageStartTime).gte(new Date(startDate)),
+                        Criteria.where(InstanceDataKeys.usageStartTime).lt(new Date(endDate)))));
+
     query.with(new Sort(Sort.Direction.ASC, InstanceDataKeys.usageStartTime));
     reader.setQuery(query);
     reader.setPageSize(READER_BATCH_SIZE);
---
+++ 78-batch-processing/src/test/java/io/harness/batch/processing/billing/reader/InstanceDataMongoEventReaderTest.java
@@ -0,0 +1,32 @@
+package io.harness.batch.processing.billing.reader;
+
+import static io.harness.rule.OwnerRule.ROHIT;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.google.inject.Inject;
+
+import io.harness.batch.processing.entities.InstanceData;
+import io.harness.category.element.UnitTests;
+import io.harness.rule.OwnerRule.Owner;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.springframework.batch.item.ItemReader;
+import software.wings.WingsBaseTest;
+
+import java.time.Instant;
+
+public class InstanceDataMongoEventReaderTest extends WingsBaseTest {
+  @Inject private InstanceDataMongoEventReader instanceDataMongoEventReader;
+
+  private final Instant NOW = Instant.now();
+  private final long START_TIME = NOW.getEpochSecond();
+  private final long END_TIME = NOW.getEpochSecond();
+
+  @Test
+  @Owner(developers = ROHIT)
+  @Category(UnitTests.class)
+  public void testInstanceDataMongoEventReader() {
+    ItemReader<InstanceData> reader = instanceDataMongoEventReader.getEventReader(START_TIME, END_TIME);
+    assertThat(reader).isNotNull();
+  }
+}
