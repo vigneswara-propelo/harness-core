910418031327954406a526a5efc3077e738da95a
Yogesh Chauhan <yogesh.chauhan@harness.io>
2019-11-25 08:40:20 +0530

    [CD-7465]: Add migration for full sync for account (#10545)
    * [CD-7465]: Add migration for full sync for account
    
    * [CD-7465]: handle NPE

---
+++ 71-rest/src/main/java/migrations/all/TriggerFullAppGitSync.java
@@ -0,0 +1,35 @@
+package migrations.all;
+
+import static io.harness.data.structure.EmptyPredicate.isNotEmpty;
+import static software.wings.beans.FeatureName.INFRA_MAPPING_REFACTOR;
+
+import com.google.inject.Inject;
+
+import migrations.Migration;
+import software.wings.beans.FeatureFlag;
+import software.wings.beans.FeatureFlag.FeatureFlagKeys;
+import software.wings.dl.WingsPersistence;
+import software.wings.service.intfc.yaml.YamlGitService;
+
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class TriggerFullAppGitSync implements Migration {
+  @Inject private WingsPersistence wingsPersistence;
+  @Inject private YamlGitService yamlGitService;
+
+  @Override
+  public void migrate() {
+    final FeatureFlag featureFlag = wingsPersistence.createQuery(FeatureFlag.class)
+                                        .filter(FeatureFlagKeys.name, INFRA_MAPPING_REFACTOR.name())
+                                        .get();
+    Set<String> accounts = featureFlag.getAccountIds();
+    if (isNotEmpty(accounts)) {
+      ExecutorService executorService = Executors.newFixedThreadPool(10);
+      for (String accountId : accounts) {
+        executorService.submit(() -> yamlGitService.fullSyncForEntireAccount(accountId));
+      }
+    }
+  }
+}
