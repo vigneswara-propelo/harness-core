14af9b8330b76c60795ee2d08052e3236c27ec09
George Georgiev <george@harness.io>
2019-12-05 08:32:09 -0800

    [CE-3]: add check for pr fix (#10705)

--- toolset/git-hooks/pre-push.sh
+++ toolset/git-hooks/pre-push.sh
@@ -38,9 +38,10 @@ then
     echo '\033[0;31m' checking behind commits is disabled - to enable: '\033[0;37m'git config --add $CHECK_BEHIND_COMMITS 3 \# or any other number \>= 0 '\033[0m'
 else
     echo '\033[0;34m' checking behind commits  ... to disable: '\033[0;37m'git config --unset $CHECK_BEHIND_COMMITS '\033[0m'
-
     CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
     TARGET_BRANCH=`echo $CURRENT_BRANCH | sed -e "s/^\([^@]*\)$/\1@master/" | sed -e "s/^.*@//"`
+
+    git fetch origin ${TARGET_BRANCH}:${TARGET_BRANCH}
 
     BEHIND=`git rev-list --left-right --count ${TARGET_BRANCH}...${CURRENT_BRANCH} | awk '{ print $1}'`
 
@@ -51,3 +52,24 @@ else
     fi
 fi
 
+CHECK_PR_FIX=hook.pre-push.prfix
+if [ "`git config $CHECK_PR_FIX`" == "false" ]
+then
+    echo '\033[0;31m' checking for PR fix in target branch is disabled - to enable: '\033[0;37m'git config --unset $CHECK_PR_FIX '\033[0m'
+else
+    echo '\033[0;34m' checking for PR fix target branch  ... to disable: '\033[0;37m'git config --add $CHECK_PR_FIX false '\033[0m'
+
+    CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+    TARGET_BRANCH=`echo $CURRENT_BRANCH | sed -e "s/^\([^@]*\)$/\1@master/" | sed -e "s/^.*@//"`
+
+    git fetch origin ${TARGET_BRANCH}:${TARGET_BRANCH}
+
+    FIXES=`git rev-list --left-right --pretty ${CURRENT_BRANCH}..${TARGET_BRANCH} | grep "[PR_FIX]"`
+
+    if [ ! -z "${FIXES}" ]
+    then
+        echo "There are unmerged PR fixes in ${TARGET_BRANCH}."
+        echo ${FIXES}
+        exit 1
+    fi
+fi
