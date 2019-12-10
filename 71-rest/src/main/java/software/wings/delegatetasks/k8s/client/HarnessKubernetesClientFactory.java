69d30774128f6620861b5bc7baabeec0a9bf4602
K Rohit Reddy <rohit.reddy@harness.io>
2019-12-10 17:43:39 +0530

    [CCM-245]: Add port 443 to K8s Master URL (#10721)
    * [CCM-245]: Add port 443 to K8s Master URL
    
    * [CCM-242]: Review comments addressed

--- 71-rest/src/main/java/software/wings/delegatetasks/k8s/client/HarnessKubernetesClientFactory.java
+++ 71-rest/src/main/java/software/wings/delegatetasks/k8s/client/HarnessKubernetesClientFactory.java
@@ -9,11 +9,15 @@ import io.fabric8.kubernetes.client.Adapters;
 import io.fabric8.kubernetes.client.Client;
 import io.fabric8.kubernetes.client.ExtensionAdapter;
 import io.fabric8.kubernetes.client.KubernetesClient;
+import io.harness.exception.InvalidRequestException;
 import software.wings.beans.KubernetesConfig;
 import software.wings.delegatetasks.k8s.exception.K8sClusterException;
 import software.wings.helpers.ext.container.ContainerDeploymentDelegateHelper;
 import software.wings.helpers.ext.k8s.request.K8sClusterConfig;
 import software.wings.service.impl.KubernetesHelperService;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 @Singleton
 public class HarnessKubernetesClientFactory implements KubernetesClientFactory {
@@ -23,6 +27,7 @@ public class HarnessKubernetesClientFactory implements KubernetesClientFactory {
   @Override
   public KubernetesClient newKubernetesClient(K8sClusterConfig k8sClusterConfig) {
     KubernetesConfig kubernetesConfig = containerDeploymentDelegateHelper.getKubernetesConfig(k8sClusterConfig);
+    kubernetesConfig.setMasterUrl(modifyMasterUrl(kubernetesConfig.getMasterUrl()));
     return kubernetesHelperService.getKubernetesClient(kubernetesConfig, emptyList());
   }
 
@@ -34,5 +39,25 @@ public class HarnessKubernetesClientFactory implements KubernetesClientFactory {
       return adapter.adapt(client);
     }
     throw new K8sClusterException("Cluster doesn't support metrics-server");
+  }
+
+  String modifyMasterUrl(String masterURL) {
+    URL url;
+    try {
+      url = new URL(masterURL);
+    } catch (MalformedURLException e) {
+      throw new InvalidRequestException("URL is incorrect, the method will return the URL as is", e);
+    }
+    if (url.getPort() == -1) {
+      if (masterURL.endsWith("/")) {
+        masterURL = masterURL.substring(0, masterURL.length() - 1);
+      }
+      if (url.getProtocol().equals("https")) {
+        return masterURL.concat(":443");
+      } else {
+        return masterURL.concat(":80");
+      }
+    }
+    return masterURL;
   }
 }
---
+++ 71-rest/src/test/java/software/wings/delegatetasks/k8s/client/HarnessKubernetesClientFactoryTest.java
@@ -0,0 +1,44 @@
+package software.wings.delegatetasks.k8s.client;
+
+import static io.harness.rule.OwnerRule.ROHIT;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import com.google.inject.Inject;
+
+import io.harness.category.element.UnitTests;
+import io.harness.exception.InvalidRequestException;
+import io.harness.rule.OwnerRule.Owner;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import software.wings.WingsBaseTest;
+
+public class HarnessKubernetesClientFactoryTest extends WingsBaseTest {
+  @Inject private HarnessKubernetesClientFactory harnessKubernetesClientFactory;
+
+  @Test
+  @Owner(developers = ROHIT)
+  @Category(UnitTests.class)
+  public void testMasterURLModifier() {
+    String masterURL = "https://35.197.55.65";
+    String modifiedURL = harnessKubernetesClientFactory.modifyMasterUrl(masterURL + "/");
+    assertThat(modifiedURL).isEqualTo(masterURL + ":443");
+
+    masterURL = "https://35.197.55.65:90";
+    modifiedURL = harnessKubernetesClientFactory.modifyMasterUrl(masterURL);
+    assertThat(modifiedURL).isEqualTo(masterURL);
+
+    masterURL = "http://35.197.55.65";
+    modifiedURL = harnessKubernetesClientFactory.modifyMasterUrl(masterURL);
+    assertThat(modifiedURL).isEqualTo(masterURL + ":80");
+  }
+
+  @Test
+  @Owner(developers = ROHIT)
+  @Category(UnitTests.class)
+  public void testMasterURLModifierException() {
+    String masterURL = "invalidURL";
+    assertThatExceptionOfType(InvalidRequestException.class)
+        .isThrownBy(() -> harnessKubernetesClientFactory.modifyMasterUrl(masterURL));
+  }
+}
