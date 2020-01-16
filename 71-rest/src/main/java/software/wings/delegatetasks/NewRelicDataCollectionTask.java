a342692d96b0d8710d55e87b20f08edadd44c58d
Sowmya K <sowmya.k@harness.io>
2020-01-16 21:27:08 +0530

    [CV-1416]: Added precondition for non empty instances (#11114)
    * [CV-1416]: Added preconditional check for non empty instances
    
    * Spell check
    
    * Used check state instead of argument

--- 71-rest/src/main/java/software/wings/delegatetasks/NewRelicDataCollectionTask.java
+++ 71-rest/src/main/java/software/wings/delegatetasks/NewRelicDataCollectionTask.java
@@ -1,6 +1,7 @@
 package software.wings.delegatetasks;
 
 import static io.harness.data.structure.EmptyPredicate.isEmpty;
+import static io.harness.data.structure.EmptyPredicate.isNotEmpty;
 import static io.harness.threading.Morpheus.sleep;
 import static software.wings.common.VerificationConstants.DATA_COLLECTION_RETRY_SLEEP;
 import static software.wings.service.impl.newrelic.NewRelicDelgateServiceImpl.METRIC_NAME_NON_SPECIAL_CHARS;
@@ -15,6 +16,7 @@ import static software.wings.service.impl.newrelic.NewRelicMetricValueDefinition
 import static software.wings.service.impl.newrelic.NewRelicMetricValueDefinition.ERROR;
 import static software.wings.service.impl.newrelic.NewRelicMetricValueDefinition.REQUSET_PER_MINUTE;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.Table.Cell;
 import com.google.common.collect.TreeBasedTable;
 import com.google.inject.Inject;
@@ -405,6 +407,8 @@ public class NewRelicDataCollectionTask extends AbstractDelegateDataCollectionTa
                 ? windowStartTimeManager + TimeUnit.MINUTES.toMillis(dataCollectionInfo.getCollectionTime())
                 : windowEndTimeManager;
 
+            Preconditions.checkState(
+                isNotEmpty(instances), "No instances found for application %s" + dataCollectionInfo.getNewRelicAppId());
             logger.info("AnalysisType is Predictive. So we're collecting metrics by application instead of host/node");
             callables.add(() -> fetchAndSaveMetricsForNode(instances.get(0), metricBatches, endTimeForCollection));
           } else {
