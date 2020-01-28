8e2a273c68903ffb68db934c5eb9abef7ead80a2
Vardan Bansal <vardan.bansal@harness.io>
2020-01-29 00:45:16 +0530

    [DX-235]: Added unit test (#11408)
    * [DX-235]: Added account filter
    
    * [DX-235]: Added unit tests

--- 76-graphql-test/src/test/java/io/harness/UserTest.java
+++ 76-graphql-test/src/test/java/io/harness/UserTest.java
@@ -17,6 +17,7 @@ import org.junit.experimental.categories.Category;
 import software.wings.beans.Account;
 import software.wings.beans.AccountType;
 import software.wings.beans.User;
+import software.wings.graphql.schema.type.QLPageInfo;
 import software.wings.graphql.schema.type.QLUser;
 import software.wings.graphql.schema.type.QLUser.QLUserKeys;
 import software.wings.graphql.schema.type.user.QLUserConnection;
@@ -103,5 +104,61 @@ public class UserTest extends GraphQLTest {
     assertThat(qlUser.getIsUserLocked()).isEqualTo(user.isUserLocked());
     assertThat(qlUser.getIsPasswordExpired()).isEqualTo(user.isPasswordExpired());
     assertThat(qlUser.getIsImportedFromIdentityProvider()).isEqualTo(user.isImported());
+  }
+
+  @Test
+  @Owner(developers = VARDAN_BANSAL)
+  @Category({GraphQLTests.class, UnitTests.class})
+  public void test_OnlySeeUsersOfAccountItGotAddedTo() {
+    String userQueryPattern = MultilineStringMixin.$.GQL(/*
+        {
+      users(limit:%d){
+        nodes{
+          id
+          name
+          email
+          isEmailVerified
+          isTwoFactorAuthenticationEnabled
+          isUserLocked
+          isPasswordExpired
+          isImportedFromIdentityProvider
+        }
+        pageInfo{
+        offset
+        limit
+        total
+        hasMore
+      }
+    }
+}
+*/ UserTest.class);
+    String query1 = String.format(userQueryPattern, 2);
+    final Account account1 =
+        accountGenerator.ensureAccount(random(String.class), random(String.class), AccountType.TRIAL);
+    final Account account2 =
+        accountGenerator.ensureAccount(random(String.class), random(String.class), AccountType.TRIAL);
+    accountGenerator.ensureUser(
+        "userId1", random(String.class), random(String.class), random(String.class).toCharArray(), account1);
+    accountGenerator.ensureUser(
+        "userId2", random(String.class), random(String.class), random(String.class).toCharArray(), account2);
+    accountGenerator.ensureUser(
+        "userId3", random(String.class), random(String.class), random(String.class).toCharArray(), account1);
+
+    QLUserConnection userConnection = qlExecute(QLUserConnection.class, query1, account1.getUuid());
+    assertThat(userConnection.getNodes().size()).isEqualTo(2);
+    QLPageInfo pageInfo = userConnection.getPageInfo();
+    assertThat(pageInfo.getOffset()).isEqualTo(0);
+    assertThat(pageInfo.getLimit()).isEqualTo(2);
+    assertThat(pageInfo.getTotal()).isEqualTo(2);
+    assertThat(pageInfo.getHasMore()).isEqualTo(false);
+
+    String query2 = String.format(userQueryPattern, 1);
+    userConnection = qlExecute(QLUserConnection.class, query2, account2.getUuid());
+    assertThat(userConnection.getNodes().size()).isEqualTo(1);
+    pageInfo = userConnection.getPageInfo();
+    assertThat(pageInfo.getOffset()).isEqualTo(0);
+    assertThat(pageInfo.getLimit()).isEqualTo(1);
+    assertThat(pageInfo.getTotal()).isEqualTo(1);
+    assertThat(pageInfo.getHasMore()).isEqualTo(false);
   }
 }
