c66e9c81366e08625663b2e70f10caf9d5e547a1
Srinivasa Gurubelli <srinivas@harness.io>
2020-01-30 22:01:41 -0800

    [CDC-3307]: Update the scripts to include CDP and CDC (#11461)

--- scripts/jenkins/release-branch-test.sh
+++ scripts/jenkins/release-branch-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git log --remotes=origin/release/* --pretty=oneline --abbrev-commit | grep -iE '\[(CCM|CCE|CD|CE|CV|DOC|ER|HAR|LE|PL|SEC|SWAT|DX)-[0-9]+]:' -o | sort | uniq > release.txt
-git log --remotes=origin/[m]aster --pretty=oneline --abbrev-commit | grep -iE '\[(CCM|CCE|CD|CE|CV|DOC|ER|HAR|LE|PL|SEC|SWAT|DX)-[0-9]+]:' -o | sort | uniq > master.txt
+git log --remotes=origin/release/* --pretty=oneline --abbrev-commit | grep -iE '\[(CCM|CCE|CD|CDP|CDC|CE|CV|DOC|ER|HAR|LE|PL|SEC|SWAT|DX)-[0-9]+]:' -o | sort | uniq > release.txt
+git log --remotes=origin/[m]aster --pretty=oneline --abbrev-commit | grep -iE '\[(CCM|CCE|CD|CDP|CDC|CE|CV|DOC|ER|HAR|LE|PL|SEC|SWAT|DX)-[0-9]+]:' -o | sort | uniq > master.txt
 
 NOT_MERGED=`comm -23 release.txt master.txt | tr '\n' ' '`
 echo NOT_MERGED="${NOT_MERGED}" > envvars
