fc81b514e26dbb6f92a5d5711aecd404a11ec51e
Mehul Kasliwal <Mehul.Kasliwal@harness.io>
2020-02-29 22:52:28 +0530

    [PL-7617]: Added Jenkins scripts for openJDK232 (#11863)
    * [PL-7617]: Added dockerfiles
    
    * [PL-7617]: Added jenkins scripts
    
    * [PL-7617]: Added jenkins scripts
    
    * [PL-7617]: Renamed delegate jars
    
    * [PL-7617]: Renamed delegateTags
    
    * [PL-7617]: Manifest changes
    
    * [PL-7617]: Base image changes
    
    * [PL-7617]: Base image changes
    
    * [PL-7617]: Listing repo
    
    * [PL-7617]: Changed base images

---
+++ scripts/jenkins/portal-openjdk232.sh
@@ -0,0 +1,139 @@
+### Dockerization of Manager ####### Doc
+
+mkdir -p dist ;
+cd dist
+cp -R ../scripts/jenkins/ .
+cd ..
+
+mkdir -p dist/migrator ;
+
+cd dist/migrator ;
+
+cp ../../51-migrator/target/migrator-capsule.jar .
+cp ../../keystore.jks .
+
+cp ../../dockerization/migrator/Dockerfile-migrator-jenkins-k8-gcr-openJDK232 ./Dockerfile-gcr
+
+echo ${VERSION} > version.txt
+if [ ! -z ${PURPOSE} ]
+then
+    echo ${PURPOSE} > purpose.txt
+fi
+
+cd ../..
+
+mkdir -p dist/manager ;
+
+cd dist/manager
+
+cp ../../71-rest/target/rest-capsule.jar .
+cp ../../71-rest/src/main/resources/hazelcast.xml .
+cp ../../keystore.jks .
+cp ../../71-rest/key.pem .
+cp ../../71-rest/cert.pem .
+cp ../../71-rest/newrelic.yml .
+cp ../../71-rest/config.yml .
+cp ../../tools/apm/appdynamics/AppServerAgent-4.5.0.23604.tar.gz .
+cp ../../tools/monitoring/datadog/dd-java-agent.jar .
+
+cp ../../dockerization/manager/Dockerfile-manager-jenkins-k8-openJDK232 ./Dockerfile
+cp ../../dockerization/manager/Dockerfile-manager-jenkins-k8-gcr-openJDK232 ./Dockerfile-gcr
+cp -r ../../dockerization/manager/scripts/ .
+cp -r ../../dockerization/common-resources/ .
+echo ${VERSION} > version.txt
+if [ ! -z ${PURPOSE} ]
+then
+    echo ${PURPOSE} > purpose.txt
+fi
+
+cd ../..
+
+mkdir -p dist/verification-service ;
+
+cd dist/verification-service
+
+cp ../../79-verification/target/verification-capsule.jar .
+cp ../../79-verification/keystore.jks .
+cp ../../79-verification/verification-config.yml .
+cp ../../tools/apm/appdynamics/AppServerAgent-4.5.0.23604.tar.gz .
+
+cp ../../dockerization/verification/Dockerfile-verification-jenkins-k8-openJDK232 ./Dockerfile
+cp ../../dockerization/verification/Dockerfile-verification-jenkins-k8-gcr-openJDK232 ./Dockerfile-gcr
+cp -R ../../dockerization/verification/scripts/ .
+cp -r ../../dockerization/common-resources/ .
+echo ${VERSION} > version.txt
+if [ ! -z ${PURPOSE} ]
+then
+    echo ${PURPOSE} > purpose.txt
+fi
+
+cd ../..
+
+mkdir -p dist/event-server ;
+cd dist/event-server
+cp ../../72-event-server/target/event-server-capsule.jar .
+cp ../../72-event-server/key.pem .
+cp ../../72-event-server/cert.pem .
+cp ../../72-event-server/event-service-config.yml .
+cp ../../dockerization/event-server/Dockerfile-event-server-jenkins-k8-openJDK232 Dockerfile
+cp ../../dockerization/event-server/Dockerfile-event-server-jenkins-k8-gcr-openJDK232 Dockerfile-gcr
+cp -r ../../dockerization/event-server/scripts/ .
+cp -r ../../dockerization/common-resources/ .
+echo ${VERSION} > version.txt
+if [ ! -z ${PURPOSE} ]
+then
+    echo ${PURPOSE} > purpose.txt
+fi
+cd ../..
+
+mkdir -p dist/batch-processing ;
+cd dist/batch-processing
+cp ../../78-batch-processing/target/batch-processing-capsule.jar .
+cp ../../78-batch-processing/batch-processing-config.yml .
+cp ../../dockerization/batch-processing/Dockerfile-batch-processing-jenkins-k8-openJDK232 Dockerfile
+cp ../../dockerization/batch-processing/Dockerfile-batch-processing-jenkins-k8-gcr-openJDK232 Dockerfile-gcr
+cp -r ../../dockerization/batch-processing/scripts/ .
+cp -r ../../dockerization/common-resources/ .
+echo ${VERSION} > version.txt
+if [ ! -z ${PURPOSE} ]
+then
+    echo ${PURPOSE} > purpose.txt
+fi
+cd ../..
+
+echo "System-Properties: version=1.0.23200${VERSION} logdnakey=${LOGDNA_KEY}" >> app.mf
+echo "Application-Version: version=1.0.23200${VERSION}" >> app.mf
+
+mkdir -p dist/delegate
+cp 81-delegate/target/delegate-capsule.jar dist/delegate/delegate-capsule.jar
+cp 81-delegate/config-delegate.yml dist/delegate/config-delegate.yml
+jar ufm dist/delegate/delegate-capsule.jar app.mf
+cp dist/delegate/delegate-capsule.jar delegate-23200${VERSION}.jar
+
+mkdir -p dist/watcher
+cp 82-watcher/target/watcher-capsule.jar dist/watcher/watcher-capsule.jar
+jar ufm dist/watcher/watcher-capsule.jar app.mf
+cp dist/watcher/watcher-capsule.jar watcher-23200${VERSION}.jar
+
+rm -rf app.mf
+
+mkdir -p dist/disconnected_on_prem_pov
+cd dist/disconnected_on_prem_pov
+cp -r ../../on-prem/harness_disconnected_on_prem_pov_final .
+cp -r ../../on-prem/disconnected_on_prem_pov_installer .
+tar -zcvf disconnected_on_prem_pov_template.tar.gz *
+cd ../..
+cp dist/disconnected_on_prem_pov/disconnected_on_prem_pov_template.tar.gz disconnected_on_prem_pov_template.tar.gz
+
+mkdir -p dist/disconnected_on_prem_k8s
+cd dist/disconnected_on_prem_k8s
+cp -r ../../on-prem/kubernetes_installer .
+tar -zcvf disconnected_on_prem_k8s_installer_builder.tar.gz *
+cd ../..
+cp dist/disconnected_on_prem_k8s/disconnected_on_prem_k8s_installer_builder.tar.gz disconnected_on_prem_k8s_installer_builder.tar.gz
+
+mkdir -p dist/test
+cd dist/test
+cp ../../91-model-gen-tool/target/model-gen-tool-capsule.jar .
+cp ../../91-model-gen-tool/config-datagen.yml .
+cd ../..
---
+++ scripts/jenkins/service-dockerizer-openjdk232.sh
@@ -0,0 +1,24 @@
+set -x
+
+export VERSION=`cat destination/dist/${SERVICE}/version.txt`
+export PURPOSE=`cat destination/dist/${SERVICE}/purpose.txt 2>/dev/null`
+
+if [ ! -z "${PURPOSE}" ]
+then
+    export PURPOSE=/${PURPOSE}
+fi
+
+
+docker login -u _json_key --password-stdin https://us.gcr.io < $GCR_CREDENTIALS
+
+export IMAGE_TAG="us.gcr.io/platform-205701/harness${PURPOSE}/${SNAPSHOT_PREFIX}${SERVICE}:23200${VERSION}"
+docker build -t ${IMAGE_TAG} destination/dist/${SERVICE} -f destination/dist/${SERVICE}/Dockerfile-gcr
+docker push ${IMAGE_TAG}
+
+if [ "${PURPOSE}" = /on-prem ]
+then
+    docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+
+    docker build -t harness/${SERVICE}:${VERSION} destination/dist/${SERVICE}
+    docker push harness/${SERVICE}:${VERSION}
+fi
