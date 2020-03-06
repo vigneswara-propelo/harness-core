2ca6de076d7f2bb190f054e90d8207118557b2b2
Kamal Joshi <kamal.joshi@harness.io>
2020-03-06 11:11:20 -0800

    Revert "[CV-3599]: Used exponential backoff to handle retries (#11834)" (#11976)
    This reverts commit b09a5ee86e76115bcf167fbefd917be6c8b9bc00.

--- 79-verification/src/main/java/io/harness/managerclient/VerificationManagerClientHelper.java
+++ 79-verification/src/main/java/io/harness/managerclient/VerificationManagerClientHelper.java
@@ -1,37 +1,28 @@
 package io.harness.managerclient;
 
-import com.google.common.annotations.VisibleForTesting;
+import static io.harness.threading.Morpheus.sleep;
+import static java.time.Duration.ofMillis;
+
 import com.google.inject.Inject;
 
 import io.harness.beans.ExecutionStatus;
-import io.harness.eraro.ErrorCode;
-import io.harness.exception.VerificationOperationException;
+import io.harness.exception.WingsException;
 import io.harness.network.SafeHttpCall;
 import lombok.extern.slf4j.Slf4j;
-import net.jodah.failsafe.Failsafe;
-import net.jodah.failsafe.RetryPolicy;
-import org.springframework.web.client.HttpServerErrorException.InternalServerError;
 import retrofit2.Call;
 import software.wings.service.impl.analysis.AnalysisContext;
 import software.wings.service.intfc.verification.CVActivityLogService;
 import software.wings.verification.VerificationDataAnalysisResponse;
 
-import java.time.Duration;
-import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Slf4j
 public class VerificationManagerClientHelper {
+  private static final int MAX_RETRY = 3;
   @Inject private VerificationManagerClient managerClient;
-  @Inject private CVActivityLogService cvActivityLogService;
-
-  @VisibleForTesting static long INITIAL_DELAY_MS = 1000;
-  private static final long MAX_DELAY_MS = 60000;
-  private static final double DELAY_FACTOR = 1.57;
-  @VisibleForTesting static Duration JITTER = Duration.ofSeconds(1);
-  static final int MAX_ATTEMPTS = 10;
+  @Inject CVActivityLogService cvActivityLogService;
 
   public Map<String, Object> getManagerHeader(String accountId, String analysisVersion) {
     try {
@@ -67,29 +58,21 @@ public class VerificationManagerClientHelper {
         managerClient.sendNotifyForVerificationState(getManagerHeader(accountId, null), correlationId, response));
   }
 
-  public <T> T callManagerWithRetry(final Call<T> call) {
-    final int[] retryCount = {0};
-
-    RetryPolicy<Object> retryPolicy =
-        new RetryPolicy<>()
-            .handle(Exception.class)
-            .withBackoff(INITIAL_DELAY_MS, MAX_DELAY_MS, ChronoUnit.MILLIS, DELAY_FACTOR)
-            .withMaxAttempts(MAX_ATTEMPTS)
-            .withJitter(JITTER)
-            .abortOn(InternalServerError.class)
-            .onFailedAttempt(event -> {
-              retryCount[0]++;
-              logger.warn("[Retrying] Error while calling manager for call {}, retryCount: {}",
-                  call.request().toString(), retryCount[0], event.getLastFailure());
-            })
-            .onFailure(event
-                -> logger.error("Error while calling manager for call {} after all retries", call.request().toString(),
-                    event.getFailure()));
-    try {
-      return Failsafe.with(retryPolicy).get(() -> SafeHttpCall.execute(call.clone()));
-    } catch (Exception e) {
-      throw new VerificationOperationException(ErrorCode.RETRY_FAILED,
-          "Exception occurred while calling manager from verification service. Exception: " + e.getMessage());
+  public <T> T callManagerWithRetry(Call<T> call) {
+    int retryCount = 0;
+    while (retryCount < MAX_RETRY) {
+      try {
+        return SafeHttpCall.execute(call);
+      } catch (Exception ex) {
+        retryCount++;
+        call = call.clone();
+        logger.info(
+            "Error while calling manager for call {}, retryCount: {}", call.request().toString(), retryCount, ex);
+        sleep(ofMillis(1000));
+      }
     }
+    logger.error("Error while calling manager for call {} after all retries", call.request().toString());
+    throw new WingsException(
+        "Exception occurred while calling manager from verification service. Call: " + call.request().toString());
   }
 }
