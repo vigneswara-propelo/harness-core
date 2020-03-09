08aba1231d90a6686b0d174548332e0755aeabfc
Prabu Rajendran <prabu.rajendran@harness.io>
2020-03-09 10:50:05 +0530

    [CDC-3628]: Resolved a warning. Added automation around shell script … (#11928)
    * [CDC-3628]: Resolved a warning. Added automation around shell script approval
    
    * [CDC-3628]: Made changes suggested in review
    
    * Revert "[CDC-3628]: Made changes suggested in review"
    
    This reverts commit 293882017c9323948e65768e4ece53ea12fbd3b5.
    
    * [CDC-3628]: Added a check to verify if workflow successfully executed

---
+++ 85-functional-test/src/test/java/io/harness/functional/workflow/ShellScriptApprovalFunctionalTest.java
@@ -0,0 +1,89 @@
+package io.harness.functional.workflow;
+
+import static io.harness.generator.EnvironmentGenerator.Environments.GENERIC_TEST;
+import static io.harness.rule.OwnerRule.PRABU;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.inject.Inject;
+
+import io.harness.beans.ExecutionStatus;
+import io.harness.category.element.FunctionalTests;
+import io.harness.functional.AbstractFunctionalTest;
+import io.harness.functional.WorkflowUtils;
+import io.harness.generator.ApplicationGenerator;
+import io.harness.generator.EnvironmentGenerator;
+import io.harness.generator.OwnerManager;
+import io.harness.generator.Randomizer;
+import io.harness.rule.Owner;
+import io.harness.testframework.restutils.WorkflowRestUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import software.wings.beans.Application;
+import software.wings.beans.Environment;
+import software.wings.beans.GraphNode;
+import software.wings.beans.Workflow;
+import software.wings.beans.WorkflowExecution;
+import software.wings.beans.approval.ApprovalStateParams;
+import software.wings.beans.approval.ShellScriptApprovalParams;
+import software.wings.beans.artifact.Artifact;
+import software.wings.sm.StateType;
+import software.wings.sm.states.ApprovalState.ApprovalStateType;
+
+import java.util.Collections;
+
+public class ShellScriptApprovalFunctionalTest extends AbstractFunctionalTest {
+  @Inject private OwnerManager ownerManager;
+  @Inject private WorkflowUtils workflowUtils;
+
+  @Inject private ApplicationGenerator applicationGenerator;
+  @Inject private EnvironmentGenerator environmentGenerator;
+  private final Randomizer.Seed seed = new Randomizer.Seed(0);
+
+  private OwnerManager.Owners owners;
+
+  Application application;
+
+  @Before
+  public void setUp() {
+    owners = ownerManager.create();
+    application = applicationGenerator.ensurePredefined(seed, owners, ApplicationGenerator.Applications.GENERIC_TEST);
+    assertThat(application).isNotNull();
+  }
+
+  @Test
+  @Owner(developers = PRABU)
+  @Category(FunctionalTests.class)
+  public void shouldCreateShellScriptApprovalStepAndRun() throws Exception {
+    Environment environment = environmentGenerator.ensurePredefined(seed, owners, GENERIC_TEST);
+    assertThat(environment).isNotNull();
+
+    ShellScriptApprovalParams shellparams = new ShellScriptApprovalParams();
+    shellparams.setRetryInterval(30);
+    shellparams.setScriptString("HARNESS_APPROVAL_STATUS=\"APPROVED\"");
+
+    ApprovalStateParams params = new ApprovalStateParams();
+    params.setShellScriptApprovalParams(shellparams);
+
+    Workflow workflow = workflowUtils.buildCanaryWorkflowPostDeploymentStep(
+        "Approval Workflow " + System.currentTimeMillis(), environment.getUuid(),
+        GraphNode.builder()
+            .name("SS-Approval-workflow-" + System.currentTimeMillis())
+            .type(StateType.APPROVAL.toString())
+            .properties(ImmutableMap.<String, Object>builder()
+                            .put("approvalStateType", ApprovalStateType.SHELL_SCRIPT)
+                            .put("approvalStateParams", params)
+                            .build())
+            .build());
+    // Test  creating a workflow
+    Workflow savedWorkflow =
+        WorkflowRestUtils.createWorkflow(bearerToken, application.getAccountId(), application.getUuid(), workflow);
+    assertThat(savedWorkflow).isNotNull();
+
+    WorkflowExecution workflowExecution = runWorkflow(bearerToken, application.getUuid(), environment.getUuid(),
+        savedWorkflow.getUuid(), Collections.<Artifact>emptyList());
+    assertThat(workflowExecution).isNotNull();
+    assertThat(workflowExecution.getStatus()).isEqualTo(ExecutionStatus.SUCCESS);
+  }
+}
