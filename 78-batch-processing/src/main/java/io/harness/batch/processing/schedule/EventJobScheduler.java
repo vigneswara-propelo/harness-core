8b5013db9be68e00d7f4b548e2405298ee10c71b
Hitesh Aringa <hitesh.aringa@harness.io>
2020-03-17 08:29:03 +0530

    [CE-1042]: disable batch job for harness account (#12141)

--- 78-batch-processing/src/main/java/io/harness/batch/processing/schedule/EventJobScheduler.java
+++ 78-batch-processing/src/main/java/io/harness/batch/processing/schedule/EventJobScheduler.java
@@ -40,7 +40,8 @@ public class EventJobScheduler {
 
   private void runJob(String accountId, Job job) {
     try {
-      if (ImmutableSet.of(DEPLOYMENT_EVENT, K8S_WATCH_EVENT).contains(BatchJobType.fromJob(job))) {
+      if (ImmutableSet.of(DEPLOYMENT_EVENT, K8S_WATCH_EVENT).contains(BatchJobType.fromJob(job))
+          || accountId.equals("wFHXHD0RRQWoO8tIZT5YVw")) {
         return;
       }
       batchJobRunner.runJob(accountId, job);
