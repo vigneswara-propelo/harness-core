91f7ed6a8c0cfc7f184d34db6037dd28254ab0e4
George Georgiev <george@harness.io>
2020-03-30 13:33:40 -0700

    [PL-10009]: add some of the delegate metrics (#12404)
    * [PL-10009]: add some of the delegate metrics

--- scripts/terraform/stackdriver/delegate.tf
+++ scripts/terraform/stackdriver/delegate.tf
@@ -80,14 +80,13 @@ resource "google_logging_metric" "delegate_tasks_creation" {
   }
   label_extractors = {
     "syncAsync": "REGEXP_EXTRACT(jsonPayload.message, \"(sync|async)\")",
-    "taskType": "REGEXP_EXTRACT(jsonPayload.message, \"type: (.+),\")",
+    "taskType": "EXTRACT(jsonPayload.harness.taskType)",
     "taskGroup": "EXTRACT(jsonPayload.harness.taskGroup)"
   }
 }
 
-
-resource "google_logging_metric" "delegate_tasks_acquire" {
-  name = join("_", [local.name_prefix, "delegate_tasks_acquire"])
+resource "google_logging_metric" "delegate_tasks_acquire_by_owner" {
+  name = join("_", [local.name_prefix, "delegate_tasks_acquire_by_owner"])
   description = "Owner: Platform delegate"
   filter = join("\n", [local.filter_prefix,
     "\"Acquiring delegate task\""])
@@ -99,14 +98,102 @@ resource "google_logging_metric" "delegate_tasks_acquire" {
       value_type = "STRING"
       description = "The accountId"
     }
+  }
+  label_extractors = {
+    "accountId": "EXTRACT(jsonPayload.harness.accountId)"
+  }
+}
+
+resource "google_logging_metric" "delegate_tasks_acquire_by_type" {
+  name = join("_", [local.name_prefix, "delegate_tasks_acquire_by_type"])
+  description = "Owner: Platform delegate"
+  filter = join("\n", [local.filter_prefix,
+    "\"eligible to execute task\""])
+  metric_descriptor {
+    metric_kind = "DELTA"
+    value_type = "INT64"
     labels {
-      key = "delegateId"
+      key = "taskType"
       value_type = "STRING"
-      description = "The delegateId that acquiring the task"
+      description = "The type of the task"
+    }
+    labels {
+      key = "taskGroup"
+      value_type = "STRING"
+      description = "The group of the task"
+    }
+  }
+  label_extractors = {
+    "taskType": "EXTRACT(jsonPayload.harness.taskType)",
+    "taskGroup": "EXTRACT(jsonPayload.harness.taskGroup)"
+  }
+}
+
+resource "google_logging_metric" "delegate_tasks_validate" {
+  name = join("_", [local.name_prefix, "delegate_tasks_validate"])
+  description = "Owner: Platform delegate"
+  filter = join("\n", [local.filter_prefix,
+    "\"Delegate to validate\""])
+  metric_descriptor {
+    metric_kind = "DELTA"
+    value_type = "INT64"
+    labels {
+      key = "taskType"
+      value_type = "STRING"
+      description = "The type of the task"
+    }
+    labels {
+      key = "taskGroup"
+      value_type = "STRING"
+      description = "The group of the task"
+    }
+  }
+  label_extractors = {
+    "taskType": "EXTRACT(jsonPayload.harness.taskType)",
+    "taskGroup": "EXTRACT(jsonPayload.harness.taskGroup)"
+  }
+}
+
+resource "google_logging_metric" "delegate_tasks_assigning" {
+  name = join("_", [local.name_prefix, "delegate_tasks_assigning"])
+  description = "Owner: Platform delegate"
+  filter = join("\n", [local.filter_prefix,
+    "\"Assigning sync task to delegate\" OR \"Assigning async task to delegate\""])
+  metric_descriptor {
+    metric_kind = "DELTA"
+    value_type = "INT64"
+    labels {
+      key = "taskType"
+      value_type = "STRING"
+      description = "The type of the task"
+    }
+    labels {
+      key = "taskGroup"
+      value_type = "STRING"
+      description = "The group of the task"
+    }
+  }
+  label_extractors = {
+    "taskType": "EXTRACT(jsonPayload.harness.taskType)",
+    "taskGroup": "EXTRACT(jsonPayload.harness.taskGroup)"
+  }
+}
+
+resource "google_logging_metric" "delegate_no_eligible" {
+  name = join("_", [local.name_prefix, "delegate_no_eligible"])
+  description = "Owner: Platform delegate"
+  filter = join("\n", [local.filter_prefix,
+    "\"delegates active but no delegates are eligible to execute task\""])
+  metric_descriptor {
+    metric_kind = "DELTA"
+    value_type = "INT64"
+    labels {
+      key = "accountId"
+      value_type = "STRING"
+      description = "The accountId"
     }
   }
   label_extractors = {
     "accountId": "EXTRACT(jsonPayload.harness.accountId)"
-    "delegateId": "EXTRACT(jsonPayload.harness.delegateId)"
   }
 }
