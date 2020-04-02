8729a215886bc3d31b706de2a1deb315f6e760df
Mehul Kasliwal <Mehul.Kasliwal@harness.io>
2020-04-02 11:41:56 +0530

    [PL-7619]: Support openjdk for Dev and PR env (#12444)
    * [PL-7619]: Support Dev and PR
    
    * [PL-7619]: Support Dev and PR
    
    * [PL-7619]: Support Dev and PR
    
    * [PL-7619]: Support Dev and PR

--- scripts/jenkins/service-dockerizer-openjdk.sh
+++ scripts/jenkins/service-dockerizer-openjdk.sh
@@ -7,12 +7,15 @@ export JDK=`cat destination/dist/jdk.txt 2>/dev/null`
 if [ ! -z "${PURPOSE}" ]
 then
     export PURPOSE=/${PURPOSE}-${JDK}
+    export SERVICEDIR=${SERVICE}
+else
+    export SERVICEDIR=${SERVICE}-${JDK}
 fi
 
 
 docker login -u _json_key --password-stdin https://us.gcr.io < $GCR_CREDENTIALS
 
-export IMAGE_TAG="us.gcr.io/platform-205701/harness${PURPOSE}/${SNAPSHOT_PREFIX}${SERVICE}:${VERSION}"
+export IMAGE_TAG="us.gcr.io/platform-205701/harness${PURPOSE}/${SNAPSHOT_PREFIX}${SERVICEDIR}:${VERSION}"
 docker build -t ${IMAGE_TAG} destination/dist/${SERVICE} -f destination/dist/${SERVICE}/Dockerfile-gcr
 docker push ${IMAGE_TAG}
 
