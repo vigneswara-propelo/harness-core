be96db621e894e30265005df579102b780b41481
Marko Barjaktarovic <marko.barjaktarovic@harness.io>
2020-04-03 16:47:34 +0200

    [PL-0]: added myself to owner rule and author check (#12500)
    * [PL-0]: added myself to owner rule and author check

--- 11-commons-test/src/main/java/io/harness/rule/OwnerRule.java
+++ 11-commons-test/src/main/java/io/harness/rule/OwnerRule.java
@@ -110,6 +110,7 @@ public class OwnerRule implements TestRule {
   public static final String VAIBHAV_SI = "vaibhav.si";
   public static final String VENKATESH = "venkatesh.kotrike";
   public static final String VIKAS = "vikas.naiyar";
+  public static final String VUK = "vuk.skobalj";
   public static final String YOGESH = "yogesh.chauhan";
   public static final String VARDAN_BANSAL = "vardan.bansal";
   public static final String NANDAN = "nandan.chandrashekar";
@@ -171,6 +172,7 @@ public class OwnerRule implements TestRule {
           .put(VAIBHAV_SI, defaultDevInfo(VAIBHAV_SI).slack("UCK76T36U").team(CONTINUOUS_DEPLOYMENT_PLATFORM).build())
           .put(VENKATESH, DevInfo.builder().email("venkatesh.kotrike@harness.io").slack("UGF55UEHF").build())
           .put(VIKAS, defaultDevInfo(VIKAS).slack("UE7M4CNMA").team(PLATFORM).build())
+          .put(VUK, defaultDevInfo(VUK).slack("U0115RT3EQL").team(PLATFORM).build())
           .put(YOGESH, defaultDevInfo(YOGESH).slack("UJVLUUXAT").team(CONTINUOUS_DEPLOYMENT_PLATFORM).build())
           .put(VARDAN_BANSAL, defaultDevInfo(VARDAN_BANSAL).slack("UH8NYAAUU").team(DEVELOPER_EXPERIENCE).build())
           .put(NANDAN, defaultDevInfo(NANDAN).slack("UKMS5KCBS").team(CONTINUOUS_VERIFICATION).build())
