e4593b047bacad35a4f5265f8404b671c201b88b
Mehul Kasliwal <Mehul.Kasliwal@harness.io>
2020-04-21 00:18:31 +0530

    [PL-10307]: Revert docker file (#12885)

--- scripts/jenkins/service-dockerizer.sh
+++ scripts/jenkins/service-dockerizer.sh
@@ -14,3 +14,11 @@ docker login -u _json_key --password-stdin https://us.gcr.io < $GCR_CREDENTIALS
 export IMAGE_TAG="us.gcr.io/platform-205701/harness${PURPOSE}/${SNAPSHOT_PREFIX}${SERVICE}:${VERSION}"
 docker build -t ${IMAGE_TAG} destination/dist/${SERVICE} -f destination/dist/${SERVICE}/Dockerfile-gcr
 docker push ${IMAGE_TAG}
+
+if [ "${PURPOSE}" = /on-prem ]
+then
+    docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+
+    docker build -t harness/${SERVICE}:${VERSION} destination/dist/${SERVICE}
+    docker push harness/${SERVICE}:${VERSION}
+fi
