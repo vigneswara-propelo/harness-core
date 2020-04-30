5c559e819909fbb4e93b13a82d47d0f4ce553585
Prashant Pal <prashant.pal@harness.io>
2020-04-30 11:38:52 +0530

    [CDC-5854]: Changed RegistryListener to not scan wings (#13145)

--- 25-orchestration-beans/src/main/java/io/harness/RegistryListener.java
+++ 25-orchestration-beans/src/main/java/io/harness/RegistryListener.java
@@ -8,11 +8,13 @@ import io.harness.annotations.Produces;
 import io.harness.annotations.Redesign;
 import io.harness.registries.RegistrableEntity;
 import io.harness.registries.Registry;
+import lombok.extern.slf4j.Slf4j;
 import org.reflections.Reflections;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Redesign
 public class RegistryListener implements ProvisionListener {
   @Override
@@ -21,8 +23,10 @@ public class RegistryListener implements ProvisionListener {
     if (!(provision instanceof Registry)) {
       return;
     }
-    Reflections reflections = new Reflections(OrchestrationBeansModule.class.getPackage().getName(), "software.wings");
     Registry registry = (Registry) provision;
+    long startTime = System.currentTimeMillis();
+    logger.info("Registry Loading Starting got registry type {} at {}", registry.getType(), startTime);
+    Reflections reflections = new Reflections(OrchestrationBeansModule.class.getPackage().getName());
     List<Class<?>> allClasses = reflections.getTypesAnnotatedWith(Produces.class)
                                     .stream()
                                     .filter(clazz -> registry.getRegistrableEntityClass().isAssignableFrom(clazz))
@@ -31,5 +35,7 @@ public class RegistryListener implements ProvisionListener {
       RegistrableEntity entity = on(clazz).create().get();
       registry.register(entity.getType(), entity);
     }
+    logger.info("Registry Loading Ended for registry type {} took {}ms", registry.getType(),
+        System.currentTimeMillis() - startTime);
   }
 }
