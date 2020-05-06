ad6baa004ecab37c8d2b47f84bac6d3b4510c2da
Harsh Jain <harsh.jain@harness.io>
2020-05-06 12:57:29 +0530

    [CI-27]: fix execution rule (#13264)

--- 73-ci-execution/src/test/java/io/harness/executionplan/rule/CIExecutionRule.java
+++ 73-ci-execution/src/test/java/io/harness/executionplan/rule/CIExecutionRule.java
@@ -6,22 +6,20 @@ import com.google.inject.Module;
 
 import io.harness.CIExecutionServiceModule;
 import io.harness.factory.ClosingFactory;
+import io.harness.factory.ClosingFactoryModule;
 import io.harness.govern.ServersModule;
-import io.harness.module.TestMongoModule;
-import io.harness.mongo.HObjectFactory;
 import io.harness.mongo.MongoPersistence;
-import io.harness.mongo.QueryFactory;
 import io.harness.persistence.HPersistence;
 import io.harness.queue.QueueController;
 import io.harness.rule.InjectorRuleMixin;
-import io.harness.rule.MongoRuleMixin;
+import io.harness.testlib.module.MongoRuleMixin;
+import io.harness.testlib.module.TestMongoModule;
 import io.harness.threading.CurrentThreadExecutor;
 import io.harness.threading.ExecutorModule;
 import org.junit.rules.MethodRule;
 import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.Statement;
 import org.mongodb.morphia.AdvancedDatastore;
-import org.mongodb.morphia.Morphia;
 
 import java.io.Closeable;
 import java.lang.annotation.Annotation;
@@ -44,15 +42,9 @@ public class CIExecutionRule implements MethodRule, InjectorRuleMixin, MongoRule
   public List<Module> modules(List<Annotation> annotations) throws Exception {
     ExecutorModule.getInstance().setExecutorService(new CurrentThreadExecutor());
 
-    String databaseName = databaseName();
-    MongoInfo mongoInfo = testMongo(annotations, closingFactory);
-
-    Morphia morphia = new Morphia();
-    morphia.getMapper().getOptions().setObjectFactory(new HObjectFactory());
-    AdvancedDatastore datastore = (AdvancedDatastore) morphia.createDatastore(mongoInfo.getClient(), databaseName);
-    datastore.setQueryFactory(new QueryFactory());
-
     List<Module> modules = new ArrayList();
+    modules.add(new ClosingFactoryModule(closingFactory));
+    modules.add(mongoTypeModule(annotations));
     modules.add(new AbstractModule() {
       @Override
       protected void configure() {
@@ -76,7 +68,8 @@ public class CIExecutionRule implements MethodRule, InjectorRuleMixin, MongoRule
         });
       }
     });
-    modules.addAll(new TestMongoModule(datastore).cumulativeDependencies());
+    modules.addAll(new TestMongoModule().cumulativeDependencies());
+
     modules.addAll(new CIExecutionServiceModule().cumulativeDependencies());
     return modules;
   }
