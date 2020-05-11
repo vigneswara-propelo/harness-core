802727ae74a3e4a8cc231641d94952887da09d6f
George Georgiev <george@harness.io>
2020-05-10 20:56:40 -0700

    [PL-4456]: add branch to the pr builds (#13362)
    * [PL-4456]: add branch to the pr builds

--- scripts/jenkins/service-dockerizer-openjdk.sh
+++ scripts/jenkins/service-dockerizer-openjdk.sh
@@ -1,9 +1,10 @@
 set -x
 
 export VERSION=`cat destination/dist/${SERVICE}/version.txt`
-export PURPOSE=`cat destination/dist/${SERVICE}/purpose.txt 2>/dev/null`
 export JDK=`cat destination/dist/jdk.txt 2>/dev/null`
 
+
+export PURPOSE=`cat destination/dist/${SERVICE}/purpose.txt 2>/dev/null`
 if [ ! -z "${PURPOSE}" ]
 then
     export PURPOSE=/${PURPOSE}-${JDK}
@@ -12,17 +13,22 @@ else
     export SERVICEDIR=${SERVICE}-${JDK}
 fi
 
+export IMAGE_TAG=`cat destination/dist/${SERVICE}/image_tag.txt 2>/dev/null`
+if [ -z "${IMAGE_TAG}" ]
+then
+    export IMAGE_TAG=${VERSION}
+fi
 
 docker login -u _json_key --password-stdin https://us.gcr.io < $GCR_CREDENTIALS
 
-export IMAGE_TAG="us.gcr.io/platform-205701/harness${PURPOSE}/${SNAPSHOT_PREFIX}${SERVICEDIR}:${VERSION}"
-docker build -t ${IMAGE_TAG} destination/dist/${SERVICE} -f destination/dist/${SERVICE}/Dockerfile-gcr
-docker push ${IMAGE_TAG}
+export IMAGE_REPO="us.gcr.io/platform-205701/harness${PURPOSE}/${SNAPSHOT_PREFIX}${SERVICEDIR}:${IMAGE_TAG}"
+docker build -t ${IMAGE_REPO} destination/dist/${SERVICE} -f destination/dist/${SERVICE}/Dockerfile-gcr
+docker push ${IMAGE_REPO}
 
 if [ "${PURPOSE}" = "/on-prem-${JDK}" ]
 then
     docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
-    docker build -t harness/${SERVICE}:${VERSION} destination/dist/${SERVICE}
-    docker push harness/${SERVICE}:${VERSION}
+    docker build -t harness/${SERVICE}:${IMAGE_TAG} destination/dist/${SERVICE}
+    docker push harness/${SERVICE}:${IMAGE_TAG}
 fi
--- scripts/jenkins/service-dockerizer.sh
+++ scripts/jenkins/service-dockerizer.sh
@@ -1,16 +1,22 @@
 set -e
 
 export VERSION=`cat destination/dist/${SERVICE}/version.txt`
-export PURPOSE=`cat destination/dist/${SERVICE}/purpose.txt 2>/dev/null`
 
+export PURPOSE=`cat destination/dist/${SERVICE}/purpose.txt 2>/dev/null`
 if [ ! -z "${PURPOSE}" ]
 then
     export PURPOSE=/${PURPOSE}
 fi
 
 
+export IMAGE_TAG=`cat destination/dist/${SERVICE}/image_tag.txt 2>/dev/null`
+if [ -z "${IMAGE_TAG}" ]
+then
+    export IMAGE_TAG=${VERSION}
+fi
+
 docker login -u _json_key --password-stdin https://us.gcr.io < $GCR_CREDENTIALS
 
-export IMAGE_TAG="us.gcr.io/platform-205701/harness${PURPOSE}/${SNAPSHOT_PREFIX}${SERVICE}:${VERSION}"
-docker build -t ${IMAGE_TAG} destination/dist/${SERVICE} -f destination/dist/${SERVICE}/Dockerfile-gcr
-docker push ${IMAGE_TAG}
+export IMAGE_REPO="us.gcr.io/platform-205701/harness${PURPOSE}/${SNAPSHOT_PREFIX}${SERVICE}:${IMAGE_TAG}"
+docker build -t ${IMAGE_REPO} destination/dist/${SERVICE} -f destination/dist/${SERVICE}/Dockerfile-gcr
+docker push ${IMAGE_REPO}
