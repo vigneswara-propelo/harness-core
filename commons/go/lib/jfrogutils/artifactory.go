d124810a393706b6a8f687f2c2a3bc17a011c935
Shubham Agrawal <shubham.agrawal@harness.io>
2020-06-03 22:07:56 +0530

    [CI-45]: Golang util to upload files to Jfrog artifactory (#13855)
    * [CI-45] Golang util to upload files to Jfrog artifactory
    
    * Fixed formatting
    
    * Added number of retries
    
    * Added mocks
    
    * Added jfrog cli to docker image
    
    * Added UTs for Jfrog artifactory upload
    
    * Added timeout for upload
    
    * [CI-49]: Add kaniko docker image to CI addon (#13964)
    
    * [CI-49]: Add kaniko docker image to CI addon

---
+++ commons/go/lib/jfrogutils/artifactory.go
@@ -0,0 +1,205 @@
+package jfrogutils
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"os/exec"
+	"strings"
+	"time"
+
+	"github.com/wings-software/portal/commons/go/lib/utils"
+	"go.uber.org/zap"
+)
+
+//go:generate mockgen -source artifactory.go -package=jfrogutils -destination mocks/artifactory_mock.go Artifactory
+
+const (
+	defaultNumRetries   int64  = 3       // default number of retries
+	defaultTimeoutSecs  int64  = 300     // 5 minutes
+	jfrogPath           string = "jfrog" // jfrog cli path
+	failureUploadStatus string = "failure"
+	successUploadStatus string = "success"
+)
+
+var execCommandWithContext = exec.CommandContext
+
+// Artifactory represents an interface to upload files to Jfrog artifactory.
+type Artifactory interface {
+	Upload(ctx context.Context, srcFilePattern, targetRepositoryPath, artifactoryURL string) error
+}
+
+type artifactory struct {
+	userName    string
+	password    string
+	apiKey      string
+	accessToken string
+	numRetries  int64
+	log         *zap.SugaredLogger
+	timeoutSecs int64
+}
+
+type uploadStatus struct {
+	Status string
+	Totals fileUploadStatus
+}
+
+type fileUploadStatus struct {
+	Success int64
+	Failure int64
+}
+
+// ArtifactoryClientOption is a type for providing arguments to NewArtifactoryClient in variadic format
+type ArtifactoryClientOption func(*artifactory)
+
+// WithArtifactoryClientRetries sets the number of retries for any operation performed using the Artifactory client.
+// Number of retries provided should be a positive value.
+func WithArtifactoryClientRetries(numRetries int64) ArtifactoryClientOption {
+	return func(a *artifactory) {
+		a.numRetries = numRetries
+	}
+}
+
+// WithArtifactoryClientTimeout sets the timeout of any operation performed using the Artifactory client.
+// Timeout provide should be a positive value.
+func WithArtifactoryClientTimeout(timeoutSecs int64) ArtifactoryClientOption {
+	return func(a *artifactory) {
+		a.timeoutSecs = timeoutSecs
+	}
+}
+
+// WithArtifactoryClientBasicAuth sets the username and password for interacting with jfrog repository.
+func WithArtifactoryClientBasicAuth(userName, password string) ArtifactoryClientOption {
+	return func(a *artifactory) {
+		a.userName = userName
+		a.password = password
+	}
+}
+
+// WithArtifactoryClientAPIKeyAuth sets the api key for interacting with jfrog repository.
+func WithArtifactoryClientAPIKeyAuth(apiKey string) ArtifactoryClientOption {
+	return func(a *artifactory) {
+		a.apiKey = apiKey
+	}
+}
+
+// WithArtifactoryClientAccessTokenAuth sets the access token for interacting with jfrog repository.
+func WithArtifactoryClientAccessTokenAuth(accessToken string) ArtifactoryClientOption {
+	return func(a *artifactory) {
+		a.accessToken = accessToken
+	}
+}
+
+// NewArtifactoryClient creates a new Jfrog artifactory client and returns it.
+func NewArtifactoryClient(log *zap.SugaredLogger, opts ...ArtifactoryClientOption) (Artifactory, error) {
+	a := &artifactory{
+		log:         log,
+		numRetries:  defaultNumRetries,
+		timeoutSecs: defaultTimeoutSecs,
+	}
+	for _, opt := range opts {
+		opt(a)
+	}
+
+	// Ensure that one of basic auth, api key or access token is provided.
+	if a.userName == "" && a.apiKey == "" && a.accessToken == "" {
+		return nil, fmt.Errorf("Provide one of basic auth, api key or access token")
+	}
+
+	return a, nil
+}
+
+// Uploads files to Jfrog artifactory using jfrog-cli
+// Requires jfrog-cli to be installed on the system
+func (a *artifactory) Upload(ctx context.Context, srcFilePattern, targetRepositoryPath, artifactoryURL string) error {
+	// upload command format: jfrog rt u --url=<> --retries=<> --user=<> --password=<> srcFilePattern dstFilePattern
+	start := time.Now()
+	ctx, cancel := context.WithTimeout(ctx, time.Second*time.Duration(a.timeoutSecs))
+	defer cancel()
+
+	args := []string{"rt", "u", fmt.Sprintf("--url=%s", artifactoryURL), fmt.Sprintf("--retries=%d", a.numRetries)}
+	printableArgs := strings.Join(args[:], " ")
+	if a.userName != "" {
+		args = append(args, fmt.Sprintf("--user=%s", a.userName))
+		args = append(args, fmt.Sprintf("--password=%s", a.password))
+	} else if a.apiKey != "" {
+		args = append(args, fmt.Sprintf("--apikey=%s", a.apiKey))
+	} else {
+		args = append(args, fmt.Sprintf("--access-token=%s", a.accessToken))
+	}
+	args = append(args, srcFilePattern)
+	args = append(args, targetRepositoryPath)
+	printableArgs = fmt.Sprintf("%s <auth> %s %s", printableArgs, srcFilePattern, targetRepositoryPath)
+
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	cmd := execCommandWithContext(ctx, jfrogPath, args...)
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	if ctx.Err() == context.DeadlineExceeded {
+		logCommandExecWarning(a.log, "time out while executing jfrog cli for upload", stderr.String(), printableArgs, start, err)
+		return err
+	}
+
+	if err != nil {
+		logCommandExecWarning(a.log, "error encountered while executing jfrog cli for upload", stderr.String(), printableArgs, start, err)
+		return err
+	}
+
+	// Parse JSON output
+	uploadStatus := &uploadStatus{}
+	err = json.Unmarshal(out.Bytes(), uploadStatus)
+	if err != nil {
+		logJSONParseWarning(a.log, "error encountered while parsing jfrog upload status", out.String(), printableArgs, start, err)
+		return err
+	}
+
+	if uploadStatus.Status == failureUploadStatus {
+		err = fmt.Errorf("failed to upload files to jfrog")
+		warnMsg := "failed to upload all the files to jfrog"
+		logUploadFailureWarning(a.log, warnMsg, printableArgs, uploadStatus.Totals.Failure, uploadStatus.Totals.Success, start, err)
+		return err
+	}
+
+	a.log.Infow(
+		"Successfully uploaded files to jfrog",
+		"failed_uploads", uploadStatus.Totals.Failure,
+		"successful_uploads", uploadStatus.Totals.Success,
+		"arguments", printableArgs,
+		"elapsed_time_ms", utils.TimeSince(start),
+	)
+	return nil
+}
+
+func logCommandExecWarning(log *zap.SugaredLogger, warnMsg, stderr, args string, startTime time.Time, err error) {
+	log.Warnw(
+		warnMsg,
+		"standard_error", stderr,
+		"arguments", args,
+		"elapsed_time_ms", utils.TimeSince(startTime),
+		zap.Error(err),
+	)
+}
+
+func logJSONParseWarning(log *zap.SugaredLogger, warnMsg, uploadStatus, args string, startTime time.Time, err error) {
+	log.Warnw(
+		warnMsg,
+		"upload_status", uploadStatus,
+		"arguments", args,
+		"elapsed_time_ms", utils.TimeSince(startTime),
+		zap.Error(err),
+	)
+}
+
+func logUploadFailureWarning(log *zap.SugaredLogger, warnMsg, args string, failedUploads, successfulUploads int64, startTime time.Time, err error) {
+	log.Warnw(
+		warnMsg,
+		"failed_uploads", failedUploads,
+		"successful_uploads", successfulUploads,
+		"arguments", args,
+		"elapsed_time_ms", utils.TimeSince(startTime),
+		zap.Error(err),
+	)
+}
---
+++ commons/go/lib/jfrogutils/artifactory_test.go
@@ -0,0 +1,214 @@
+package jfrogutils
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/exec"
+	"strconv"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"github.com/wings-software/portal/commons/go/lib/logs"
+	"go.uber.org/zap"
+)
+
+var mockedExitStatus = 0
+var mockedStdout string
+var mockedStderr string
+
+func TestExecCommandHelper(t *testing.T) {
+	if os.Getenv("GO_WANT_HELPER_PROCESS") != "1" {
+		return
+	}
+
+	fmt.Fprintf(os.Stdout, os.Getenv("STDOUT"))
+	fmt.Fprintf(os.Stderr, os.Getenv("STDERR"))
+	i, _ := strconv.Atoi(os.Getenv("EXIT_STATUS"))
+	os.Exit(i)
+}
+
+func fakeExecCommandWithContext(ctx context.Context, command string, args ...string) *exec.Cmd {
+	cs := []string{"-test.run=TestExecCommandHelper", "--", command}
+	cs = append(cs, args...)
+	cmd := exec.Command(os.Args[0], cs...)
+	es := strconv.Itoa(mockedExitStatus)
+	cmd.Env = []string{"GO_WANT_HELPER_PROCESS=1",
+		"STDOUT=" + mockedStdout,
+		"STDERR=" + mockedStderr,
+		"EXIT_STATUS=" + es}
+	return cmd
+}
+
+func TestNewArtifactoryClientWithNoAuth(t *testing.T) {
+	ctrl, _ := gomock.WithContext(context.Background(), t)
+	defer ctrl.Finish()
+	_, err := NewArtifactoryClient(nil)
+	assert.NotEqual(t, err, nil)
+}
+
+func TestNewArtifactoryClientWithBasicAuth(t *testing.T) {
+	ctrl, _ := gomock.WithContext(context.Background(), t)
+	defer ctrl.Finish()
+
+	usrName := "admin"
+	password := "password"
+	_, err := NewArtifactoryClient(nil, WithArtifactoryClientBasicAuth(usrName, password), WithArtifactoryClientRetries(3))
+	assert.Equal(t, err, nil)
+}
+
+func TestNewArtifactoryClientWithAccessToken(t *testing.T) {
+	ctrl, _ := gomock.WithContext(context.Background(), t)
+	defer ctrl.Finish()
+
+	accessToken := "token"
+	_, err := NewArtifactoryClient(nil, WithArtifactoryClientAccessTokenAuth(accessToken), WithArtifactoryClientTimeout(1))
+	assert.Equal(t, err, nil)
+}
+
+func TestNewArtifactoryClientWithAPIKey(t *testing.T) {
+	ctrl, _ := gomock.WithContext(context.Background(), t)
+	defer ctrl.Finish()
+
+	apiKey := "apiKey"
+	_, err := NewArtifactoryClient(nil, WithArtifactoryClientAPIKeyAuth(apiKey))
+	assert.Equal(t, err, nil)
+}
+
+func Test_artifactory_Upload_BasicAuthRunErr(t *testing.T) {
+	originalExecCommandCtx := execCommandWithContext
+	defer func() { execCommandWithContext = originalExecCommandCtx }()
+	execCommandWithContext = fakeExecCommandWithContext
+	mockedExitStatus = 1
+	mockedStdout = ""
+	mockedStderr = "Invalid number of arguments"
+
+	ctrl, ctx := gomock.WithContext(context.Background(), t)
+	defer ctrl.Finish()
+
+	usrName := "admin"
+	password := "password"
+	srcFilePattern := "/tmp/mock.jar"
+	targetRepoPath := "tmp"
+	artifactoryURL := "mock-url"
+	log, _ := logs.GetObservedLogger(zap.ErrorLevel)
+	rt := &artifactory{usrName, password, "", "", defaultNumRetries, log.Sugar(), defaultTimeoutSecs}
+
+	err := rt.Upload(ctx, srcFilePattern, targetRepoPath, artifactoryURL)
+	assert.Equal(t, err, err.(*exec.ExitError))
+}
+
+func Test_artifactory_Upload_BasicAuthJsonParseErr(t *testing.T) {
+	originalExecCommandCtx := execCommandWithContext
+	defer func() { execCommandWithContext = originalExecCommandCtx }()
+	execCommandWithContext = fakeExecCommandWithContext
+	mockedExitStatus = 0
+	mockedStdout = ""
+	mockedStderr = ""
+
+	ctrl, ctx := gomock.WithContext(context.Background(), t)
+	defer ctrl.Finish()
+
+	usrName := "admin"
+	password := "password"
+	srcFilePattern := "/tmp/mock.jar"
+	targetRepoPath := "tmp"
+	artifactoryURL := "mock-url"
+	log, _ := logs.GetObservedLogger(zap.ErrorLevel)
+	rt := &artifactory{usrName, password, "", "", defaultNumRetries, log.Sugar(), defaultTimeoutSecs}
+
+	err := rt.Upload(ctx, srcFilePattern, targetRepoPath, artifactoryURL)
+	assert.Equal(t, err, err.(*json.SyntaxError))
+}
+
+func Test_artifactory_Upload_BasicAuthUploadFailErr(t *testing.T) {
+	originalExecCommandCtx := execCommandWithContext
+	defer func() { execCommandWithContext = originalExecCommandCtx }()
+	execCommandWithContext = fakeExecCommandWithContext
+	mockedExitStatus = 0
+	mockedStdout = "{\"status\": \"failure\", \"totals\": {\"success\": 0, \"failure\": 1}}"
+	mockedStderr = ""
+
+	ctrl, ctx := gomock.WithContext(context.Background(), t)
+	defer ctrl.Finish()
+
+	usrName := "admin"
+	password := "password"
+	srcFilePattern := "/tmp/mock.jar"
+	targetRepoPath := "tmp"
+	artifactoryURL := "mock-url"
+	log, _ := logs.GetObservedLogger(zap.ErrorLevel)
+	rt := &artifactory{usrName, password, "", "", defaultNumRetries, log.Sugar(), defaultTimeoutSecs}
+
+	err := rt.Upload(ctx, srcFilePattern, targetRepoPath, artifactoryURL)
+	assert.Equal(t, err.Error(), "failed to upload files to jfrog")
+}
+
+func Test_artifactory_Upload_BasicAuthUploadSuccess(t *testing.T) {
+	originalExecCommandCtx := execCommandWithContext
+	defer func() { execCommandWithContext = originalExecCommandCtx }()
+	execCommandWithContext = fakeExecCommandWithContext
+	mockedExitStatus = 0
+	mockedStdout = "{\"status\": \"success\", \"totals\": {\"success\": 1, \"failure\": 0}}"
+	mockedStderr = ""
+
+	ctrl, ctx := gomock.WithContext(context.Background(), t)
+	defer ctrl.Finish()
+
+	usrName := "admin"
+	password := "password"
+	srcFilePattern := "/tmp/mock.jar"
+	targetRepoPath := "tmp"
+	artifactoryURL := "mock-url"
+	log, _ := logs.GetObservedLogger(zap.ErrorLevel)
+	rt := &artifactory{usrName, password, "", "", defaultNumRetries, log.Sugar(), defaultTimeoutSecs}
+
+	err := rt.Upload(ctx, srcFilePattern, targetRepoPath, artifactoryURL)
+	assert.Equal(t, err, nil)
+}
+
+func Test_artifactory_Upload_ApiKeyUploadSuccess(t *testing.T) {
+	originalExecCommandCtx := execCommandWithContext
+	defer func() { execCommandWithContext = originalExecCommandCtx }()
+	execCommandWithContext = fakeExecCommandWithContext
+	mockedExitStatus = 0
+	mockedStdout = "{\"status\": \"success\", \"totals\": {\"success\": 1, \"failure\": 0}}"
+	mockedStderr = ""
+
+	ctrl, ctx := gomock.WithContext(context.Background(), t)
+	defer ctrl.Finish()
+
+	apiKey := "test-key"
+	srcFilePattern := "/tmp/mock.jar"
+	targetRepoPath := "tmp"
+	artifactoryURL := "mock-url"
+	log, _ := logs.GetObservedLogger(zap.ErrorLevel)
+	rt := &artifactory{"", "", apiKey, "", defaultNumRetries, log.Sugar(), defaultTimeoutSecs}
+
+	err := rt.Upload(ctx, srcFilePattern, targetRepoPath, artifactoryURL)
+	assert.Equal(t, err, nil)
+}
+
+func Test_artifactory_Upload_AccessToeknUploadSuccess(t *testing.T) {
+	originalExecCommandCtx := execCommandWithContext
+	defer func() { execCommandWithContext = originalExecCommandCtx }()
+	execCommandWithContext = fakeExecCommandWithContext
+	mockedExitStatus = 0
+	mockedStdout = "{\"status\": \"success\", \"totals\": {\"success\": 1, \"failure\": 0}}"
+	mockedStderr = ""
+
+	ctrl, ctx := gomock.WithContext(context.Background(), t)
+	defer ctrl.Finish()
+
+	accessToken := "test-token"
+	srcFilePattern := "/tmp/mock.jar"
+	targetRepoPath := "tmp"
+	artifactoryURL := "mock-url"
+	log, _ := logs.GetObservedLogger(zap.ErrorLevel)
+	rt := &artifactory{"", "", "", accessToken, defaultNumRetries, log.Sugar(), defaultTimeoutSecs}
+
+	err := rt.Upload(ctx, srcFilePattern, targetRepoPath, artifactoryURL)
+	assert.Equal(t, err, nil)
+}
