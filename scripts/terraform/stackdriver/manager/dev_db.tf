80aa18e2a84b4a5e150cee67ce76648da81b86af
George Georgiev <george@harness.io>
2020-06-02 18:55:57 -0700

    [PL-4453]: add developers db dashboard (#13950)
    * [PL-4453]: add developers db dashboard

--- scripts/terraform/stackdriver/manager/dev_db.tf
+++ scripts/terraform/stackdriver/manager/dev_db.tf
@@ -2,7 +2,20 @@ resource "google_logging_metric" "dev_db_index_issues" {
   name = join("_", [local.name_prefix, "dev_db_index_issues"])
   description = "Owner: Platform commons"
   filter = join("\n", [local.filter_prefix,
-    "(\"IndexManager\" OR \"HObjectFactory\")",
+    "jsonPayload.logger:\"io.harness.mongo.IndexManager\"",
+    "severity=\"ERROR\""
+  ])
+  metric_descriptor {
+    metric_kind = "DELTA"
+    value_type = "INT64"
+  }
+}
+
+resource "google_logging_metric" "dev_db_factory_issues" {
+  name = join("_", [local.name_prefix, "dev_db_factory_issues"])
+  description = "Owner: Platform commons"
+  filter = join("\n", [local.filter_prefix,
+    "jsonPayload.logger=\"io.harness.mongo.HObjectFactory\"",
     "severity=\"ERROR\""
   ])
   metric_descriptor {
@@ -21,4 +34,80 @@ resource "google_logging_metric" "dev_db_native_iterator_issues" {
     metric_kind = "DELTA"
     value_type = "INT64"
   }
+}
+
+resource "google_monitoring_dashboard" "dev_db_dashboard" {
+  dashboard_json = <<EOF
+
+{
+  "displayName": "Database Issues - ${var.deployment}",
+  "gridLayout": {
+    "columns": "2",
+    "widgets": [
+      {
+        "title": "Issues with collection indexes",
+        "xyChart": {
+          "dataSets": [
+            {
+              "timeSeriesQuery": {
+                "timeSeriesFilter": {
+                  "filter": "metric.type=\"logging.googleapis.com/user/x_${var.deployment}_dev_db_index_issues\" resource.type=\"gke_container\"",
+                  "aggregation": {
+                    "perSeriesAligner": "ALIGN_RATE",
+                    "crossSeriesReducer": "REDUCE_SUM"
+                  },
+                  "secondaryAggregation": {}
+                },
+                "unitOverride": "1"
+              },
+              "plotType": "STACKED_BAR",
+              "minAlignmentPeriod": "60s"
+            }
+          ],
+          "timeshiftDuration": "0s",
+          "yAxis": {
+            "label": "y1Axis",
+            "scale": "LINEAR"
+          },
+          "chartOptions": {
+            "mode": "COLOR"
+          }
+        }
+      },
+      {
+        "title": "Issues with morphia object factory",
+        "xyChart": {
+          "dataSets": [
+            {
+              "timeSeriesQuery": {
+                "timeSeriesFilter": {
+                  "filter": "metric.type=\"logging.googleapis.com/user/x_${var.deployment}_dev_db_factory_issues\" resource.type=\"gke_container\"",
+                  "aggregation": {
+                    "perSeriesAligner": "ALIGN_RATE",
+                    "crossSeriesReducer": "REDUCE_SUM"
+                  },
+                  "secondaryAggregation": {}
+                },
+                "unitOverride": "1"
+              },
+              "plotType": "STACKED_BAR",
+              "minAlignmentPeriod": "60s"
+            }
+          ],
+          "timeshiftDuration": "0s",
+          "yAxis": {
+            "label": "y1Axis",
+            "scale": "LINEAR"
+          },
+          "chartOptions": {
+            "mode": "COLOR"
+          }
+        }
+      }
+    ]
+  }
+}
+
+
+EOF
 }
