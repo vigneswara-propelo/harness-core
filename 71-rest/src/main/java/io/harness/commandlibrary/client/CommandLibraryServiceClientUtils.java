05b2ee42c7e0caba7b9e35b7222c16b07874e9df
Rohit Kumar <rohit.kumar@harness.io>
2020-06-16 13:10:31 +0530

    [DX-1730]:  handle auth error for manager to command library service calls (#14295)

--- 71-rest/src/main/java/io/harness/commandlibrary/client/CommandLibraryServiceClientUtils.java
+++ 71-rest/src/main/java/io/harness/commandlibrary/client/CommandLibraryServiceClientUtils.java
@@ -36,12 +36,29 @@ public class CommandLibraryServiceClientUtils {
   }
 
   private static <T> javax.ws.rs.core.Response createFailureResponse(Response<T> response) throws IOException {
-    final ResponseBody errorBody = response.errorBody();
-    final ResponseBuilder responeBuilder = status(response.code());
-    if (errorBody != null) {
-      responeBuilder.type(errorBody.contentType().toString()).entity(errorBody.string());
+    if (response.code() == 401) {
+      return handleAuthError(response);
+    } else {
+      return handleDefaultError(response);
     }
-    return responeBuilder.build();
+  }
+
+  private static <T> javax.ws.rs.core.Response handleDefaultError(Response<T> response) throws IOException {
+    final ResponseBuilder responseBuilder = status(response.code());
+
+    final ResponseBody errorBody = response.errorBody();
+    if (errorBody != null) {
+      responseBuilder.type(errorBody.contentType().toString()).entity(errorBody.string());
+    }
+    return responseBuilder.build();
+  }
+
+  private static <T> javax.ws.rs.core.Response handleAuthError(Response<T> response) throws IOException {
+    final ResponseBody errorBody = response.errorBody();
+
+    final String errorMessage = errorBody != null ? errorBody.string() : "";
+    throw new GeneralException(
+        String.format("auth error while connecting command library service with message : [%s] ", errorMessage));
   }
 
   private static <T> javax.ws.rs.core.Response createSuccessResponse(Response<T> response) {
