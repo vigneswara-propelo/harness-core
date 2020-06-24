af2c533c3607be7b5f256731e8ea32e0b08b3ab4
Bojana Milovanovic <bojana.milovanovic@harness.io>
2020-06-24 19:16:11 +0200

    [CDP-13021]: Increasing code coverage on k8s delegatetasks (#14565)
    * [CDP-13021]: Increasing code coverage on k8s delegatetasks
    
    * [CDP-13021]: Use constants from WingsTestConstants class
    
    * [CDP-13021]: Resolve conflict with master

---
+++ 71-rest/src/test/java/software/wings/delegatetasks/k8s/K8sTaskTest.java
@@ -0,0 +1,113 @@
+package software.wings.delegatetasks.k8s;
+
+import static io.harness.rule.OwnerRule.BOJANA;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static software.wings.utils.WingsTestConstants.ACCOUNT_ID;
+import static software.wings.utils.WingsTestConstants.ACTIVITY_ID;
+import static software.wings.utils.WingsTestConstants.APP_ID;
+import static software.wings.utils.WingsTestConstants.COMMAND_NAME;
+import static software.wings.utils.WingsTestConstants.RELEASE_NAME;
+import static software.wings.utils.WingsTestConstants.TIMEOUT_INTERVAL;
+import static software.wings.utils.WingsTestConstants.WORKFLOW_EXECUTION_ID;
+
+import io.harness.beans.DelegateTask;
+import io.harness.category.element.UnitTests;
+import io.harness.delegate.beans.TaskData;
+import io.harness.delegate.command.CommandExecutionResult;
+import io.harness.rule.Owner;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import software.wings.WingsBaseTest;
+import software.wings.beans.DelegateTaskPackage;
+import software.wings.delegatetasks.k8s.taskhandler.K8sTaskHandler;
+import software.wings.helpers.ext.container.ContainerDeploymentDelegateHelper;
+import software.wings.helpers.ext.helm.HelmConstants;
+import software.wings.helpers.ext.k8s.request.K8sClusterConfig;
+import software.wings.helpers.ext.k8s.request.K8sTaskParameters;
+import software.wings.helpers.ext.k8s.response.K8sTaskExecutionResponse;
+import software.wings.service.intfc.k8s.delegate.K8sGlobalConfigService;
+
+import java.util.Map;
+import java.util.function.BooleanSupplier;
+import java.util.function.Consumer;
+
+public class K8sTaskTest extends WingsBaseTest {
+  @Mock private ContainerDeploymentDelegateHelper containerDeploymentDelegateHelper;
+  @Mock private K8sGlobalConfigService k8sGlobalConfigService;
+  @Mock private K8sTaskHandler k8sTaskHandler;
+  @Mock private Map<String, K8sTaskHandler> k8sCommandTaskTypeToTaskHandlerMap;
+
+  private K8sClusterConfig k8sClusterConfig;
+
+  @InjectMocks
+  private K8sTask k8sTask =
+      new K8sTask(DelegateTaskPackage.builder()
+                      .delegateTask(DelegateTask.builder().data(TaskData.builder().build()).build())
+                      .build(),
+          mock(Consumer.class), mock(BooleanSupplier.class));
+
+  private K8sTaskParameters k8sTaskParameters;
+
+  @Before
+  public void setup() {
+    k8sClusterConfig = K8sClusterConfig.builder().build();
+    k8sTaskParameters =
+        new K8sTaskParameters(ACCOUNT_ID, APP_ID, COMMAND_NAME, ACTIVITY_ID, k8sClusterConfig, WORKFLOW_EXECUTION_ID,
+            RELEASE_NAME, TIMEOUT_INTERVAL, K8sTaskParameters.K8sTaskType.INSTANCE_SYNC, HelmConstants.HelmVersion.V2);
+  }
+
+  @Test
+  @Owner(developers = BOJANA)
+  @Category(UnitTests.class)
+  public void testRunInstanceSync() {
+    when(k8sCommandTaskTypeToTaskHandlerMap.get(K8sTaskParameters.K8sTaskType.INSTANCE_SYNC.name()))
+        .thenReturn(k8sTaskHandler);
+    k8sTask.run(k8sTaskParameters);
+    verify(k8sTaskHandler, times(1)).executeTask(k8sTaskParameters, null);
+    verify(k8sTaskHandler, times(1)).executeTask(k8sTaskParameters, null);
+  }
+
+  @Test
+  @Owner(developers = BOJANA)
+  @Category(UnitTests.class)
+  public void testRunInstanceSyncException() {
+    when(k8sCommandTaskTypeToTaskHandlerMap.get(K8sTaskParameters.K8sTaskType.INSTANCE_SYNC.name())).thenReturn(null);
+    K8sTaskExecutionResponse k8sTaskExecutionResponse = k8sTask.run(k8sTaskParameters);
+    assertThat(k8sTaskExecutionResponse.getCommandExecutionStatus())
+        .isEqualTo(CommandExecutionResult.CommandExecutionStatus.FAILURE);
+  }
+
+  @Test
+  @Owner(developers = BOJANA)
+  @Category(UnitTests.class)
+  public void testRun() {
+    k8sTaskParameters.setCommandType(K8sTaskParameters.K8sTaskType.APPLY);
+    String kubeconfigFileContent = "kubeconfigFileContent";
+    when(containerDeploymentDelegateHelper.getKubeconfigFileContent(any(K8sClusterConfig.class)))
+        .thenReturn(kubeconfigFileContent);
+    when(k8sCommandTaskTypeToTaskHandlerMap.get(K8sTaskParameters.K8sTaskType.APPLY.name())).thenReturn(k8sTaskHandler);
+    k8sTask.run(k8sTaskParameters);
+    verify(containerDeploymentDelegateHelper, times(1)).getKubeconfigFileContent(k8sClusterConfig);
+    verify(k8sCommandTaskTypeToTaskHandlerMap, times(1)).get(K8sTaskParameters.K8sTaskType.APPLY.name());
+    verify(k8sTaskHandler, times(1)).executeTask(any(K8sTaskParameters.class), any(K8sDelegateTaskParams.class));
+  }
+
+  @Test
+  @Owner(developers = BOJANA)
+  @Category(UnitTests.class)
+  public void testRunException() {
+    k8sTaskParameters.setCommandType(K8sTaskParameters.K8sTaskType.APPLY);
+    k8sGlobalConfigService = null;
+    K8sTaskExecutionResponse response = k8sTask.run(k8sTaskParameters);
+    verify(containerDeploymentDelegateHelper, times(1)).getKubeconfigFileContent(k8sClusterConfig);
+    assertThat(response.getCommandExecutionStatus()).isEqualTo(CommandExecutionResult.CommandExecutionStatus.FAILURE);
+  }
+}
--- 71-rest/src/test/java/software/wings/delegatetasks/k8s/client/HarnessKubernetesClientFactoryTest.java
+++ 71-rest/src/test/java/software/wings/delegatetasks/k8s/client/HarnessKubernetesClientFactoryTest.java
@@ -1,9 +1,14 @@
 package software.wings.delegatetasks.k8s.client;
 
 import static io.harness.rule.OwnerRule.AVMOHAN;
+import static io.harness.rule.OwnerRule.BOJANA;
 import static io.harness.rule.OwnerRule.ROHIT;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.powermock.api.mockito.PowerMockito.when;
 
 import com.google.inject.Inject;
 
@@ -12,10 +17,20 @@ import io.harness.exception.InvalidRequestException;
 import io.harness.rule.Owner;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import software.wings.WingsBaseTest;
+import software.wings.beans.KubernetesConfig;
+import software.wings.helpers.ext.container.ContainerDeploymentDelegateHelper;
+import software.wings.helpers.ext.k8s.request.K8sClusterConfig;
+import software.wings.service.impl.KubernetesHelperService;
+
+import java.util.Collections;
 
 public class HarnessKubernetesClientFactoryTest extends WingsBaseTest {
-  @Inject private HarnessKubernetesClientFactory harnessKubernetesClientFactory;
+  @InjectMocks @Inject private HarnessKubernetesClientFactory harnessKubernetesClientFactory;
+  @Mock private ContainerDeploymentDelegateHelper containerDeploymentDelegateHelper;
+  @Mock private KubernetesHelperService kubernetesHelperService;
 
   @Test
   @Owner(developers = ROHIT)
@@ -60,5 +75,21 @@ public class HarnessKubernetesClientFactoryTest extends WingsBaseTest {
     String masterUrl = "https://int-capi-rancher.cncpl.us/k8s/clusters/c-pv9p9";
     assertThat(harnessKubernetesClientFactory.modifyMasterUrl(masterUrl))
         .isEqualTo("https://int-capi-rancher.cncpl.us:443/k8s/clusters/c-pv9p9");
+  }
+
+  @Test
+  @Owner(developers = BOJANA)
+  @Category(UnitTests.class)
+  public void testNewKubernetesClient() {
+    String masterUrl = "https://int-capi-rancher.cncpl.us/k8s/clusters/c-pv9p9";
+    KubernetesConfig kubernetesConfig = KubernetesConfig.builder().masterUrl(masterUrl).build();
+    K8sClusterConfig k8sClusterConfig = K8sClusterConfig.builder().build();
+    when(containerDeploymentDelegateHelper.getKubernetesConfig(any(K8sClusterConfig.class)))
+        .thenReturn(kubernetesConfig);
+
+    harnessKubernetesClientFactory.newKubernetesClient(k8sClusterConfig);
+    verify(kubernetesHelperService, times(1)).getKubernetesClient(kubernetesConfig, Collections.emptyList());
+    verify(containerDeploymentDelegateHelper, times(1)).getKubernetesConfig(k8sClusterConfig);
+    assertThat(kubernetesConfig.getMasterUrl()).isEqualTo("https://int-capi-rancher.cncpl.us:443/k8s/clusters/c-pv9p9");
   }
 }
