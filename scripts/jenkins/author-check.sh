6513c4eb0f0c1742eeae5a5c572beb72558ba0c1
Arvind Choudhary <arvind.choudhary@harness.io>
2020-06-25 12:06:27 +0530

    [CDP-13027]: updated mail mapping (#14587)
    * [CDP-13027]: updated mail mapping
    
    * [CDP-13027]: updated EXCEPTION_COMMITS comparator count to 222 from 221

--- scripts/jenkins/author-check.sh
+++ scripts/jenkins/author-check.sh
@@ -127,7 +127,7 @@ mv .mailmap.tmp .mailmap
 
 echo $EXECPTION_COMMITS exceptions
 
-if [ $EXECPTION_COMMITS -gt 221 ]
+if [ $EXECPTION_COMMITS -gt 222 ]
 then
     echo "You bringing commit with excepted author that is no longer allowed"
     git log --oneline --format='%aN <%aE>' | grep  "$EXCEPTIONS"
