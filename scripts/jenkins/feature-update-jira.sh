5a13668c60c695b048a5bc19a31e72720227a2e0
George Georgiev <george@harness.io>
2020-06-28 13:22:37 -0700

    [DEL-700]: update jira field about last feature build (#14660)
    * [DEL-700]: update jira field about last feature build

---
+++ scripts/jenkins/feature-update-jira.sh
@@ -0,0 +1,16 @@
+PROJECTS="CCE|CCM|CDC|CDNG|CDP|CE|CI|CV|CVNG|DEL|DOC|DX|ER|OPS|PL|SEC|SWAT"
+
+KEY=`git log --pretty=oneline --abbrev-commit -1 |\
+  grep -o -iE "\[(${PROJECTS})-[0-9]+]:" | grep -o -iE "(${PROJECTS})-[0-9]+"`
+FIELD_ID="customfield_10769"
+
+if [ ! -z "$KEY" ]
+then
+  echo $KEY
+  curl \
+    -X PUT \
+    --data "{ \"fields\" : { \"${FIELD_ID}\" : \"${BUILD_ID}\" }}" \
+    -H "Content-Type: application/json" \
+    https://harness.atlassian.net/rest/api/2/issue/${KEY} \
+    --user $JIRA_USERNAME:$JIRA_PASSWORD
+fi
