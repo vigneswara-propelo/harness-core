0661439db8fc3145ce07c58cdbe5061de3080275
Prashant Pal <prashant.pal@harness.io>
2020-07-06 15:07:02 +0530

    [CDNG-388]: Renamed the module to 24-manager-delegate-service-beans (#14864)
    * [CDNG-210]: Moving NG Delegate grpc server to a separate module 23-ng-delegate-grpc-server
    
    This reverts commit f9c833cd
    
    * [CDNG-387]: Enhanced SendTaskDefinition and client signature
    
    * [CDNG-387]: Successful sync Task Execution
    
    * fixed test and sonar warnings
    
    * [CDNG-388]: Renamed the module to 25-manager-delegate-service-beans
    
    * removed iml files
    
    * minor change

--- scripts/jenkins/build-protos.sh
+++ scripts/jenkins/build-protos.sh
@@ -13,7 +13,7 @@ bazel ${OUTPUT_BASE} build \
   //20-delegate-beans/src/main/proto/... \
   //21-delegate-agent-beans/src/main/proto/... \
   //22-delegate-service-beans/src/main/proto/... \
-  //24-ng-delegate-service-beans/src/main/proto/... \
+  //24-manager-delegate-service-beans/src/main/proto/... \
   //product/ci/engine/proto/...
 
 cleanup() {
@@ -48,7 +48,7 @@ cleanup 13-grpc-api/src/generated/java \
         20-delegate-beans/src/generated/java \
         21-delegate-agent-beans/src/generated/java \
         22-delegate-service-beans/src/generated/java \
-        24-ng-delegate-service-beans/src/generated/java \
+        24-manager-delegate-service-beans/src/generated/java \
         72-ci-beans/src/generated/java
 
 compile_proto_module 13-grpc-api 13-grpc-api/src/main 13-grpc-api/src/generated/java
@@ -57,7 +57,7 @@ compile_proto_module 19-delegate-tasks-beans 19-delegate-tasks-beans/src/main 19
 compile_proto_module 20-delegate-beans 20-delegate-beans/src/main 20-delegate-beans/src/generated/java
 compile_proto_module 21-delegate-agent-beans 21-delegate-agent-beans/src/main 21-delegate-agent-beans/src/generated/java
 compile_proto_module 22-delegate-service-beans 22-delegate-service-beans/src/main 22-delegate-service-beans/src/generated/java
-compile_proto_module 24-ng-delegate-service-beans 24-ng-delegate-service-beans/src/main 24-ng-delegate-service-beans/src/generated/java
+compile_proto_module 24-manager-delegate-service-beans 24-manager-delegate-service-beans/src/main 24-manager-delegate-service-beans/src/generated/java
 
 compile_proto_module cienginepb product/ci/engine 72-ci-beans/src/generated/java
 compile_proto_module ciaddonpb product/ci/addon 72-ci-beans/src/generated/java
