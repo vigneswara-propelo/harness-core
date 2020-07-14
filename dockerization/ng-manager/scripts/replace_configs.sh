4fe79425a611c325c12bd28777d8248def734e84
Vikas Naiyar <vikas.naiyar@harness.io>
2020-07-14 14:31:07 +0530

    [CDNG-463]: Changing name of the grpcServer attribute (#15115)
    * [CDNG-463]: Making config and Ingress changes for grpc calls from ng-manager
    
    * [CDNG-463]: Changing name of the grpcServer attribute

--- dockerization/ng-manager/scripts/replace_configs.sh
+++ dockerization/ng-manager/scripts/replace_configs.sh
@@ -5,7 +5,7 @@ CONFIG_FILE=/opt/harness/config.yml
 yq delete -i $CONFIG_FILE server.applicationConnectors[0]
 yq write -i $CONFIG_FILE server.adminConnectors "[]"
 
-yq delete -i $CONFIG_FILE grpcServerConfig.connectors[0]
+yq delete -i $CONFIG_FILE grpcServer.connectors[0]
 
 if [[ "" != "$LOGGING_LEVEL" ]]; then
     yq write -i $CONFIG_FILE logging.level "$LOGGING_LEVEL"
@@ -69,6 +69,6 @@ if [[ "" != "$MANAGER_AUTHORITY" ]]; then
 fi
 
 if [[ "" != "$GRPC_SERVER_PORT" ]]; then
-  yq write -i $CONFIG_FILE grpcServerConfig.connectors[0].port "$GRPC_SERVER_PORT"
+  yq write -i $CONFIG_FILE grpcServer.connectors[0].port "$GRPC_SERVER_PORT"
 fi
 
