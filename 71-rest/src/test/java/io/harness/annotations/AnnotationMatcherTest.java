6eb7b19997dcc68e31d138181deca2dceaf711c2
Prashant Pal <prashant.pal@harness.io>
2020-07-16 04:04:12 +0530

    [CDC-8154]: Added test for maintaining the file coverage (#15176)

--- 71-rest/src/test/java/io/harness/annotations/AnnotationMatcherTest.java
+++ 71-rest/src/test/java/io/harness/annotations/AnnotationMatcherTest.java
@@ -3,8 +3,13 @@ package io.harness.annotations;
 import static io.harness.rule.OwnerRule.PRASHANT;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.google.common.base.Preconditions;
+
 import io.harness.annotations.dev.ExcludeRedesign;
+import io.harness.annotations.dev.HarnessTeam;
+import io.harness.annotations.dev.OwnedBy;
 import io.harness.category.element.UnitTests;
+import io.harness.reflection.CodeUtils;
 import io.harness.rule.Owner;
 import org.apache.commons.io.IOUtils;
 import org.junit.Test;
@@ -14,6 +19,7 @@ import software.wings.WingsBaseTest;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -40,5 +46,38 @@ public class AnnotationMatcherTest extends WingsBaseTest {
       expectedList = IOUtils.readLines(in, "UTF-8");
     }
     assertThat(expectedList).containsAll(nameList);
+  }
+
+  @Test
+  @Owner(developers = PRASHANT)
+  @Category(UnitTests.class)
+  public void testAllCoverageClasses() throws IOException {
+    Set<Class<? extends Object>> redesignClasses = new HashSet<>();
+    Reflections reflectionsHarness = new Reflections("io.harness");
+    Reflections reflectionsWings = new Reflections("software.wings");
+    redesignClasses.addAll(reflectionsHarness.getTypesAnnotatedWith(OwnedBy.class));
+    redesignClasses.addAll(reflectionsWings.getTypesAnnotatedWith(OwnedBy.class));
+    List<String> nameList =
+        redesignClasses.stream()
+            .distinct()
+            .filter(clazz -> !clazz.isAnonymousClass() && !clazz.isMemberClass() && !clazz.isInterface())
+            .filter(clazz
+                -> clazz.getAnnotation(OwnedBy.class) != null
+                    && clazz.getAnnotation(OwnedBy.class).value().equals(HarnessTeam.CDC))
+            .map(this ::extractClassLocation)
+            .sorted()
+            .collect(Collectors.toList());
+    List<String> expectedList;
+    try (InputStream in = getClass().getResourceAsStream("/annotations/coverage-class-list.txt")) {
+      expectedList = IOUtils.readLines(in, "UTF-8");
+    }
+    assertThat(expectedList).isEqualTo(nameList);
+  }
+
+  private String extractClassLocation(Class<?> clazz) {
+    List<String> locationList = Arrays.asList(Preconditions.checkNotNull(CodeUtils.location(clazz)).split("/"));
+    String moduleBasePath = locationList.get(locationList.indexOf("target") - 1) + "/src/main/java/";
+    String packagePath = clazz.getPackage().getName().replace(".", "/") + "/";
+    return moduleBasePath + packagePath + clazz.getSimpleName() + ".java";
   }
 }
