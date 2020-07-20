f4d9fcb0260098a7c7cbcfaf7852d3bddd6650b4
Vikas Naiyar <vikas.naiyar@harness.io>
2020-07-20 12:02:13 +0530

    [CDNG-531]: Added integration test for CD NG app startup (#15273)
    * [CDNG-531]: Added test for CD NG app statup
    
    * [CDNG-531]: Reverting the changes to DelegateSyncTaskResponse.java file

---
+++ 120-ng-manager/src/test/java/io/harness/NGAppStartupTest.java
@@ -0,0 +1,35 @@
+package io.harness;
+
+import static io.harness.rule.OwnerRule.VIKAS;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.dropwizard.testing.ResourceHelpers;
+import io.dropwizard.testing.junit.DropwizardAppRule;
+import io.harness.category.element.IntegrationTests;
+import io.harness.ng.NextGenApplication;
+import io.harness.ng.NextGenConfiguration;
+import io.harness.rule.Owner;
+import org.glassfish.jersey.client.JerseyClientBuilder;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.core.Response;
+
+public class NGAppStartupTest {
+  @ClassRule
+  public static final DropwizardAppRule<NextGenConfiguration> RULE =
+      new DropwizardAppRule<>(NextGenApplication.class, ResourceHelpers.resourceFilePath("test-config.yml"));
+
+  @Test
+  @Owner(developers = VIKAS)
+  @Category(IntegrationTests.class)
+  public void testAppStartup() {
+    final Client client = new JerseyClientBuilder().build();
+    final Response response =
+        client.target(String.format("http://localhost:%d/swagger.json", RULE.getLocalPort())).request().get();
+    assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
+    response.close();
+  }
+}
