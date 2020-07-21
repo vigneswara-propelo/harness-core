81726f2cc8c8e0372993820258828f587e053c87
Vikas Naiyar <vikas.naiyar@harness.io>
2020-07-21 08:45:36 +0530

    [CDNG-531]: Updating test to start a local mongo server and then test app startup (#15305)
    * [CDNG-531]: Ignoring the NGAppStartupTest to avoid Jenkins build failure
    
    * [CDNG-531]: Updating test to start a local mongo server and then test app startup

--- 120-ng-manager/src/test/java/io/harness/NGAppStartupTest.java
+++ 120-ng-manager/src/test/java/io/harness/NGAppStartupTest.java
@@ -3,34 +3,70 @@ package io.harness;
 import static io.harness.rule.OwnerRule.VIKAS;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.mongodb.ServerAddress;
+import de.bwaldvogel.mongo.MongoServer;
+import de.bwaldvogel.mongo.backend.memory.MemoryBackend;
+import io.dropwizard.testing.ConfigOverride;
+import io.dropwizard.testing.DropwizardTestSupport;
 import io.dropwizard.testing.ResourceHelpers;
-import io.dropwizard.testing.junit.DropwizardAppRule;
 import io.harness.category.element.UnitTests;
 import io.harness.ng.NextGenApplication;
 import io.harness.ng.NextGenConfiguration;
 import io.harness.rule.Owner;
 import org.glassfish.jersey.client.JerseyClientBuilder;
-import org.junit.ClassRule;
-import org.junit.Ignore;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
+import java.net.InetSocketAddress;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.core.Response;
 
 public class NGAppStartupTest {
-  @ClassRule
-  public static final DropwizardAppRule<NextGenConfiguration> RULE =
-      new DropwizardAppRule<>(NextGenApplication.class, ResourceHelpers.resourceFilePath("test-config.yml"));
+  public static MongoServer MONGO_SERVER;
+  public static DropwizardTestSupport<NextGenConfiguration> SUPPORT;
+
+  private static MongoServer startMongoServer() {
+    final MongoServer mongoServer = new MongoServer(new MemoryBackend());
+    mongoServer.bind("localhost", 0);
+    return mongoServer;
+  }
+
+  private static void stopMongoServer() {
+    if (MONGO_SERVER != null) {
+      MONGO_SERVER.shutdownNow();
+    }
+  }
+
+  private static String getMongoUri() {
+    InetSocketAddress serverAddress = MONGO_SERVER.getLocalAddress();
+    final ServerAddress addr = new ServerAddress(serverAddress);
+    return String.format("mongodb://%s:%s/ng-harness", addr.getHost(), addr.getPort());
+  }
+
+  @BeforeClass
+  public static void beforeClass() {
+    MONGO_SERVER = startMongoServer();
+    SUPPORT = new DropwizardTestSupport<NextGenConfiguration>(NextGenApplication.class,
+        ResourceHelpers.resourceFilePath("test-config.yml"), ConfigOverride.config("mongo.uri", getMongoUri()));
+    SUPPORT.before();
+  }
+
+  @AfterClass
+  public static void afterClass() {
+    stopMongoServer();
+    SUPPORT.after();
+  }
 
   @Test
   @Owner(developers = VIKAS)
   @Category(UnitTests.class)
-  @Ignore("Working locally, need to find a way to get it working on Jenkins")
   public void testAppStartup() {
     final Client client = new JerseyClientBuilder().build();
     final Response response =
-        client.target(String.format("http://localhost:%d/swagger.json", RULE.getLocalPort())).request().get();
+        client.target(String.format("http://localhost:%d/swagger.json", SUPPORT.getLocalPort())).request().get();
+
     assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
     response.close();
   }
