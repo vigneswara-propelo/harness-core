cc81e0fc304b9f1f45ff55a099a86dae9b8c9db1
Rohit Kumar <rohit.kumar@harness.io>
2020-07-22 10:17:52 +0530

    [CDNG-572]: refactored wings persistence config (#15356)

--- 71-rest/src/main/java/software/wings/app/WingsPersistenceConfig.java
+++ 71-rest/src/main/java/software/wings/app/WingsPersistenceConfig.java
@@ -3,13 +3,9 @@ package software.wings.app;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
 
-import io.harness.annotation.HarnessRepo;
 import io.harness.ng.SpringPersistenceConfig;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@EnableMongoRepositories(basePackages = {"io.harness.cdng"}, includeFilters = @ComponentScan.Filter(HarnessRepo.class))
 @Configuration
 public class WingsPersistenceConfig extends SpringPersistenceConfig {
   @Inject
