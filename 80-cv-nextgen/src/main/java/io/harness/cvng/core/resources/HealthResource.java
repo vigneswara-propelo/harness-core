7f89f915e73e91b6098e0657f73537cbec098221
Sowmya K <sowmya.k@harness.io>
2020-07-29 15:01:27 +0530

    [CVNG-644]: Add health check for cvng nextgen (#15539)
    * [CVNG-644]: Add health check for cvng nextgen
    
    * Fix checkstyle

---
+++ 80-cv-nextgen/src/main/java/io/harness/cvng/core/resources/HealthResource.java
@@ -0,0 +1,60 @@
+package io.harness.cvng.core.resources;
+
+import static io.harness.exception.WingsException.USER;
+import static io.harness.maintenance.MaintenanceController.getMaintenanceFlag;
+
+import com.google.inject.Inject;
+
+import com.codahale.metrics.annotation.ExceptionMetered;
+import com.codahale.metrics.annotation.Timed;
+import com.codahale.metrics.health.HealthCheck;
+import io.harness.eraro.ErrorCode;
+import io.harness.exception.NoResultFoundException;
+import io.harness.health.HealthException;
+import io.harness.health.HealthService;
+import io.harness.rest.RestResponse;
+import io.harness.security.annotations.PublicApi;
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Api("health")
+@Path("/health")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+@PublicApi
+@Slf4j
+public class HealthResource {
+  private HealthService healthService;
+
+  @Inject
+  public HealthResource(HealthService healthService) {
+    this.healthService = healthService;
+  }
+
+  @GET
+  @Timed
+  @ExceptionMetered
+  public RestResponse<String> get() throws Exception {
+    if (getMaintenanceFlag()) {
+      logger.info("In maintenance mode. Throwing exception to prevent traffic.");
+      throw NoResultFoundException.newBuilder()
+          .code(ErrorCode.RESOURCE_NOT_FOUND)
+          .message("in maintenance mode")
+          .reportTargets(USER)
+          .build();
+    }
+
+    final HealthCheck.Result check = healthService.check();
+    if (check.isHealthy()) {
+      return new RestResponse<>("healthy");
+    }
+
+    throw new HealthException(check.getMessage(), check.getError());
+  }
+}
