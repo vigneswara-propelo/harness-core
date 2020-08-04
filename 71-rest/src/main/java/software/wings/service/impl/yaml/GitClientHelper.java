b214fd9a1471b1714fae998f687de99ea50c0cc9
Abhinav Singh <abhinav.singh@harness.io>
2020-08-04 21:17:15 +0530

    [CDP-13951]: Store repos in new path in delegates (#15353)

--- 71-rest/src/main/java/software/wings/service/impl/yaml/GitClientHelper.java
+++ 71-rest/src/main/java/software/wings/service/impl/yaml/GitClientHelper.java
@@ -3,9 +3,11 @@ package software.wings.service.impl.yaml;
 import static io.harness.eraro.ErrorCode.GENERAL_ERROR;
 import static io.harness.eraro.ErrorCode.GENERAL_YAML_ERROR;
 import static io.harness.eraro.ErrorCode.GIT_CONNECTION_ERROR;
+import static io.harness.exception.WingsException.ADMIN_SRE;
 import static io.harness.exception.WingsException.SRE;
 import static io.harness.exception.WingsException.USER_ADMIN;
 import static io.harness.govern.Switch.unhandled;
+import static org.apache.commons.codec.binary.Hex.encodeHexString;
 import static software.wings.beans.yaml.Change.ChangeType.ADD;
 import static software.wings.beans.yaml.Change.ChangeType.DELETE;
 import static software.wings.beans.yaml.Change.ChangeType.MODIFY;
@@ -18,6 +20,7 @@ import com.google.inject.Singleton;
 
 import io.harness.eraro.ErrorCode;
 import io.harness.exception.WingsException;
+import io.harness.exception.YamlException;
 import io.harness.filesystem.FileIo;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.jgit.api.errors.GitAPIException;
@@ -25,6 +28,7 @@ import org.eclipse.jgit.api.errors.JGitInternalException;
 import org.eclipse.jgit.diff.DiffEntry;
 import org.eclipse.jgit.errors.MissingObjectException;
 import org.eclipse.jgit.errors.TransportException;
+import org.jetbrains.annotations.NotNull;
 import software.wings.beans.GitConfig;
 import software.wings.beans.GitConfig.GitRepositoryType;
 import software.wings.beans.GitOperationContext;
@@ -37,6 +41,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.MessageDigest;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
@@ -46,13 +51,15 @@ import java.util.stream.Stream;
 public class GitClientHelper {
   public static final String REPOSITORY = "./repository";
   private static final String GIT_REPO_BASE_DIR =
-      "./repository/${REPO_TYPE}/${ACCOUNT_ID}/${CONNECTOR_ID}/${REPO_NAME}";
+      "./repository/${REPO_TYPE}/${ACCOUNT_ID}/${CONNECTOR_ID}/${REPO_NAME}/${REPO_URL_HASH}";
   public static final String REPOSITORY_GIT_FILE_DOWNLOADS = "./repository/gitFileDownloads";
   public static final String REPOSITORY_GIT_FILE_DOWNLOADS_ACCOUNT = "./repository/gitFileDownloads/{ACCOUNT_ID}";
   public static final String REPOSITORY_GIT_FILE_DOWNLOADS_BASE =
       "./repository/gitFileDownloads/{ACCOUNT_ID}/{CONNECTOR_ID}";
-  public static final String REPOSITORY_GIT_FILE_DOWNLOADS_REPO_DIR =
+  public static final String REPOSITORY_GIT_FILE_DOWNLOADS_REPO_BASE_DIR =
       REPOSITORY_GIT_FILE_DOWNLOADS_BASE + "/{REPO_NAME}";
+  public static final String REPOSITORY_GIT_FILE_DOWNLOADS_REPO_DIR =
+      REPOSITORY_GIT_FILE_DOWNLOADS_REPO_BASE_DIR + "/{REPO_URL_HASH}";
 
   private LoadingCache<String, Object> cache = CacheBuilder.newBuilder()
                                                    .maximumSize(2000)
@@ -74,16 +81,30 @@ public class GitClientHelper {
   }
 
   public String getFileDownloadRepoDirectory(GitConfig gitConfig, String connectorId) {
-    String repoName = gitConfig.getRepoUrl()
-                          .substring(gitConfig.getRepoUrl().lastIndexOf('/') + 1)
-                          .split("\\.")[0]; // TODO:: support more url types and validation
+    String repoName = getRepoName(gitConfig);
+    String repoUrlHash = getRepoUrlHash(gitConfig);
     if (gitConfig.getGitRepoType() == null) {
       logger.error("gitRepoType can not be null. defaulting it to YAML");
       gitConfig.setGitRepoType(GitRepositoryType.YAML);
     }
-    return REPOSITORY_GIT_FILE_DOWNLOADS_REPO_DIR.replace("{ACCOUNT_ID}", gitConfig.getAccountId())
+    return buildGitFileDownloadsRepoDir(gitConfig.getAccountId(), connectorId, repoName, repoUrlHash);
+  }
+
+  @NotNull
+  private String buildGitFileDownloadsRepoDir(
+      String accountId, String connectorId, String repoName, String repoUrlHash) {
+    return REPOSITORY_GIT_FILE_DOWNLOADS_REPO_DIR.replace("{ACCOUNT_ID}", accountId)
         .replace("{CONNECTOR_ID}", connectorId)
-        .replace("{REPO_NAME}", repoName);
+        .replace("{REPO_NAME}", repoName)
+        .replace("{REPO_URL_HASH}", repoUrlHash);
+  }
+
+  @NotNull
+  private String buildGitRepoBaseDir(String accountId, String connectorId, String repoName, String repoUrlHash) {
+    return GIT_REPO_BASE_DIR.replace("${ACCOUNT_ID}", accountId)
+        .replace("${CONNECTOR_ID}", connectorId)
+        .replace("${REPO_NAME}", repoName)
+        .replace("${REPO_URL_HASH}", repoUrlHash);
   }
 
   public void createDirStructureForFileDownload(GitConfig gitConfig, String connectorId) {
@@ -97,6 +118,18 @@ public class GitClientHelper {
       FileIo.createDirectoryIfDoesNotExist(
           REPOSITORY_GIT_FILE_DOWNLOADS_BASE.replace("{ACCOUNT_ID}", gitConfig.getAccountId())
               .replace("{CONNECTOR_ID}", connectorId));
+
+      FileIo.createDirectoryIfDoesNotExist(
+          REPOSITORY_GIT_FILE_DOWNLOADS_REPO_BASE_DIR.replace("{ACCOUNT_ID}", gitConfig.getAccountId())
+              .replace("{CONNECTOR_ID}", connectorId)
+              .replace("{REPO_NAME}", getRepoName(gitConfig)));
+
+      FileIo.createDirectoryIfDoesNotExist(
+          REPOSITORY_GIT_FILE_DOWNLOADS_BASE.replace("{ACCOUNT_ID}", gitConfig.getAccountId())
+              .replace("{CONNECTOR_ID}", connectorId)
+              .replace("{REPO_NAME}", getRepoName(gitConfig))
+              .replace("{REPO_URL_HASH}", getRepoUrlHash(gitConfig)));
+
     } catch (IOException e) {
       logger.error("Failed to created required dir structure for gitFileDownloads", e);
       throw new WingsException(
@@ -129,13 +162,19 @@ public class GitClientHelper {
   }
 
   public String getRepoPathForFileDownload(GitConfig gitConfig, String gitConnectorId) {
-    String repoName = gitConfig.getRepoUrl()
-                          .substring(gitConfig.getRepoUrl().lastIndexOf('/') + 1)
-                          .split("\\.")[0]; // TODO:: support more url types and validation);
+    String repoName = getRepoName(gitConfig);
+    String repoUrlHash = getRepoUrlHash(gitConfig);
+    return buildGitFileDownloadsRepoDir(gitConfig.getAccountId(), gitConnectorId, repoName, repoUrlHash);
+  }
 
-    return REPOSITORY_GIT_FILE_DOWNLOADS_REPO_DIR.replace("{ACCOUNT_ID}", gitConfig.getAccountId())
-        .replace("{CONNECTOR_ID}", gitConnectorId)
-        .replace("{REPO_NAME}", repoName);
+  private String getRepoName(GitConfig gitConfig) {
+    return gitConfig.getRepoUrl()
+        .substring(gitConfig.getRepoUrl().lastIndexOf('/') + 1) // TODO:: support more url types and validation);
+        .split("\\.")[0];
+  }
+
+  private String getRepoUrlHash(GitConfig gitConfig) {
+    return calculateHash(gitConfig.getRepoUrl());
   }
 
   public synchronized void releaseLock(GitConfig gitConfig, String repoPath) {
@@ -189,17 +228,24 @@ public class GitClientHelper {
     GitConfig gitConfig = gitOperationContext.getGitConfig();
     String gitConnectorId = gitOperationContext.getGitConnectorId();
 
-    String repoName = gitConfig.getRepoUrl()
-                          .substring(gitConfig.getRepoUrl().lastIndexOf('/') + 1)
-                          .split("\\.")[0]; // TODO:: support more url types and validation
+    String repoName = getRepoName(gitConfig);
+    String repoUrlHash = getRepoUrlHash(gitConfig);
+
     if (gitConfig.getGitRepoType() == null) {
       logger.error("gitRepoType can not be null. defaulting it to YAML");
       gitConfig.setGitRepoType(GitRepositoryType.YAML);
     }
-    return GIT_REPO_BASE_DIR.replace("${REPO_TYPE}", gitConfig.getGitRepoType().name().toLowerCase())
-        .replace("${ACCOUNT_ID}", gitConfig.getAccountId())
-        .replace("${CONNECTOR_ID}", gitConnectorId)
-        .replace("${REPO_NAME}", repoName);
+    return buildGitRepoBaseDir(gitConfig.getAccountId(), gitConnectorId, repoName, repoUrlHash);
+  }
+
+  private String calculateHash(String input) {
+    try {
+      MessageDigest md = MessageDigest.getInstance("SHA-1");
+      byte[] messageDigest = md.digest(input.getBytes());
+      return encodeHexString(messageDigest);
+    } catch (Exception e) {
+      throw new YamlException(String.format("Error while calculating hash for input [%s].", input), e, ADMIN_SRE);
+    }
   }
 
   public String fetchCompleteUrl(GitConfig gitConfig, String repoName) {
