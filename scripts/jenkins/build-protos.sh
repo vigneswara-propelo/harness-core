0ff102c2fd5ac76a6d34a06ad715200dbf89d13b
George Georgiev <george@harness.io>
2020-08-18 13:23:42 -0700

    [PL-0]: cleanup protobuf generation script (#16073)
    * [PL-0]: cleanup protobuf generation script

--- scripts/jenkins/build-protos.sh
+++ scripts/jenkins/build-protos.sh
@@ -8,11 +8,6 @@ then
 fi
 
 bazel ${OUTPUT_BASE} build -s \
-  //13-grpc-api/src/main/proto/... \
-  //19-delegate-tasks-beans/src/... \
-  //20-delegate-beans/src/main/proto/... \
-  //21-delegate-agent-beans/src/main/proto/... \
-  //22-delegate-service-beans/src/main/proto/... \
   //22-ng-delegate-service-beans/src/main/proto/... \
   //24-manager-delegate-service-beans/src/main/proto/... \
   //24-ng-manager-service-beans/src/main/proto/... \
@@ -46,22 +41,11 @@ compile_proto_module() {
   find ${generatedModulePath} -name '*.pb.meta' -delete
 }
 
-cleanup 13-grpc-api/src/generated/java \
-        19-delegate-tasks-beans/src/generated/java \
-        20-delegate-beans/src/generated/java \
-        21-delegate-agent-beans/src/generated/java \
-        22-delegate-service-beans/src/generated/java \
-        22-ng-delegate-service-beans/src/generated/java \
+cleanup 22-ng-delegate-service-beans/src/generated/java \
         24-manager-delegate-service-beans/src/generated/java \
         24-ng-manager-service-beans/src/generated/java \
         72-ci-beans/src/generated/java
 
-compile_proto_module 13-grpc-api 13-grpc-api/src/main 13-grpc-api/src/generated/java
-
-compile_proto_module 19-delegate-tasks-beans 19-delegate-tasks-beans/src/main 19-delegate-tasks-beans/src/generated/java
-compile_proto_module 20-delegate-beans 20-delegate-beans/src/main 20-delegate-beans/src/generated/java
-compile_proto_module 21-delegate-agent-beans 21-delegate-agent-beans/src/main 21-delegate-agent-beans/src/generated/java
-compile_proto_module 22-delegate-service-beans 22-delegate-service-beans/src/main 22-delegate-service-beans/src/generated/java
 compile_proto_module 22-ng-delegate-service-beans 22-ng-delegate-service-beans/src/main 22-ng-delegate-service-beans/src/generated/java
 compile_proto_module 24-manager-delegate-service-beans 24-manager-delegate-service-beans/src/main 24-manager-delegate-service-beans/src/generated/java
 compile_proto_module 24-ng-manager-service-beans 24-ng-manager-service-beans/src/main 24-ng-manager-service-beans/src/generated/java
