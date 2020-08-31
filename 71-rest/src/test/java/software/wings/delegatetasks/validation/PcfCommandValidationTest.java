8541c1084df53069c7d965e320123e64efe05ec8
Anil Chowdhury <anil.chowdhury@harness.io>
2020-08-31 16:02:21 +0530

    [CDP-14546]: Fixing Junit test (#16325)

--- 71-rest/src/test/java/software/wings/delegatetasks/validation/PcfCommandValidationTest.java
+++ 71-rest/src/test/java/software/wings/delegatetasks/validation/PcfCommandValidationTest.java
@@ -1,20 +1,14 @@
 package software.wings.delegatetasks.validation;
 
-import static io.harness.rule.OwnerRule.ABOSII;
 import static io.harness.rule.OwnerRule.ADWAIT;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.joor.Reflect.on;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 
 import io.harness.CategoryTest;
 import io.harness.category.element.UnitTests;
 import io.harness.delegate.beans.DelegateTaskPackage;
 import io.harness.delegate.beans.TaskData;
 import io.harness.rule.Owner;
-import io.harness.security.encryption.EncryptedDataDetail;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import software.wings.beans.PcfConfig;
@@ -24,10 +18,7 @@ import software.wings.helpers.ext.pcf.request.PcfCommandRollbackRequest;
 import software.wings.helpers.ext.pcf.request.PcfCommandRouteUpdateRequest;
 import software.wings.helpers.ext.pcf.request.PcfCommandSetupRequest;
 import software.wings.helpers.ext.pcf.request.PcfInstanceSyncRequest;
-import software.wings.service.intfc.security.EncryptionService;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.function.Consumer;
 
 public class PcfCommandValidationTest extends CategoryTest {
@@ -116,24 +107,5 @@ public class PcfCommandValidationTest extends CategoryTest {
     deployRequest.setUseAppAutoscalar(false);
     deployRequest.setUseCfCLI(true);
     assertThat(pcfCommandValidation.pcfCliValidationRequired(deployRequest)).isTrue();
-  }
-
-  @Test
-  @Owner(developers = ABOSII)
-  @Category(UnitTests.class)
-  @Ignore("Marked failing test intermittent")
-  public void testGetCriteriaWithEncryptionDetails() {
-    Consumer consumer = mock(Consumer.class);
-    EncryptionService encryptionService = mock(EncryptionService.class);
-    List<EncryptedDataDetail> encryptedDetails = Collections.emptyList();
-    PCFCommandValidation pcfCommandValidation =
-        new PCFCommandValidation("", DelegateTaskPackage.builder().data(TaskData.builder().build()).build(), consumer);
-    on(pcfCommandValidation).set("encryptionService", encryptionService);
-
-    PcfConfig pcfConfig = PcfConfig.builder().endpointUrl("url").username("user".toCharArray()).build();
-    PcfCommandRequest request = PcfCommandSetupRequest.builder().pcfConfig(pcfConfig).build();
-    String criteria = pcfCommandValidation.getCriteria(request, encryptedDetails);
-    verify(encryptionService, times(1)).decrypt(pcfConfig, encryptedDetails);
-    assertThat(criteria).isEqualTo("Pcf:url");
   }
 }
