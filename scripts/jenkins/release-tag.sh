29d855e47e4e96dcc9870c20c3cffd41ec7aacdc
Guna Chandrasekaran <guna.chandrasekaran@harness.io>
2020-09-17 12:46:40 -0700

    [BT-70]: Add release tagging script (#16848)
    * [BT-70]: Add release tagging script
    
    * [BT-70]: Add release tagging script
    
    * [BT-70]: Add release tagging script

---
+++ scripts/jenkins/release-tag.sh
@@ -0,0 +1,40 @@
+
+if [ -z "$RELEASE_COMMIT" ]
+then
+  echo "Please Provide Valid git commit sha to RELEASE_COMMIT variable"
+  exit 1
+fi
+
+UNIT_TEST_SIGN_OFF=`git tag -l --contains $RELEASE_COMMIT | grep signoff-unittest`
+
+if [ -z "$UNIT_TEST_SIGN_OFF" ]
+then
+  echo "The specified sha $RELEASE_COMMIT was not signed off from unit tests"
+  exit 1
+else
+  echo "The specified sha $RELEASE_COMMIT was  signed off from unit tests"
+fi
+
+FUNCTIONAL_TEST_SIGN_OFF=`git tag -l --contains $RELEASE_COMMIT | grep signoff-functional-test`
+
+if [ -z "$FUNCTIONAL_TEST_SIGN_OFF" ]
+then
+  echo "The specified sha $RELEASE_COMMIT was not signed off from functional tests"
+  exit 1
+else
+  echo "The specified sha $RELEASE_COMMIT was signed off from functional tests"
+fi
+
+
+git checkout "$RELEASE_COMMIT"
+
+# initialize variables
+export VERSION_FILE=build.properties
+
+export VERSION=`cat ${VERSION_FILE} |\
+    grep 'build.number=' |\
+    sed -e 's: *build.number=::g'`
+export VERSION=${VERSION%??}
+
+# Export variables
+echo VERSION=${VERSION} > jenkins.properties
