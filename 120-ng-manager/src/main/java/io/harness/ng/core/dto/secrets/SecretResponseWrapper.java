708ec92ccbd7b38589de3cd55b61a4307bccff67
Nikhil Ranjan <nikhil.ranjan@harness.io>
2020-09-23 14:08:50 +0530

    [CDNG-589]: Add notnull to secret in response (#16981)

--- 120-ng-manager/src/main/java/io/harness/ng/core/dto/secrets/SecretResponseWrapper.java
+++ 120-ng-manager/src/main/java/io/harness/ng/core/dto/secrets/SecretResponseWrapper.java
@@ -3,10 +3,12 @@ package io.harness.ng.core.dto.secrets;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 @Builder
 public class SecretResponseWrapper {
-  private SecretDTOV2 secret;
+  @NotNull private SecretDTOV2 secret;
   private Long createdAt;
   private Long updatedAt;
 }
