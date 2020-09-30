a1ad40552eb5ed34fdda15bed94a533d51685023
prashantsharma04 <prashant.sharma@harness.io>
2020-09-30 14:24:49 +0530

    [BT-124]: 24-ng-manager-service-beans to bazel (#17211)
    * [BT-124]: 24-ng-manager-service-beans to bazel
    
    * [BT-124]: 24-ng-manager-service-beans to bazel
    
    * format

--- scripts/bazel/bazel_script.sh
+++ scripts/bazel/bazel_script.sh
@@ -100,11 +100,14 @@ build_bazel_module 21-delegate-agent-beans
 build_bazel_module 21-persistence
 build_bazel_tests 21-persistence
 build_bazel_module 22-delegate-service-beans
+build_bazel_module 22-ng-delegate-service-beans
 build_bazel_module 22-ng-project-n-orgs
 build_bazel_module 22-secret-manager-client
 build_bazel_module 22-timeout-engine
 build_bazel_module 22-wait-engine
 build_bazel_module 23-delegate-service-driver
+build_bazel_module 24-common-entities
+build_bazel_module 24-ng-manager-service-beans
 build_java_proto_module 13-grpc-api
 build_java_proto_module 19-delegate-tasks-beans
 build_java_proto_module 20-delegate-beans
