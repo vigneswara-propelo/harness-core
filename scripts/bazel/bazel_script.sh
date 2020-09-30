2bb0b3da745bdce98955dce86953c6e877f7e91a
prashantsharma04 <prashant.sharma@harness.io>
2020-09-30 10:28:18 +0530

    [BT-97]: 22-secret-manager-client to bazel (#17195)
    * [BT-97]: 22-secret-manager-client to bazel
    
    * added in pom
    
    * added dep for supporter-test
    
    Co-authored-by: Brijesh Dhakar <brijesh.dhakar@harness.io>

--- scripts/bazel/bazel_script.sh
+++ scripts/bazel/bazel_script.sh
@@ -101,6 +101,7 @@ build_bazel_module 21-persistence
 build_bazel_tests 21-persistence
 build_bazel_module 22-delegate-service-beans
 build_bazel_module 22-ng-project-n-orgs
+build_bazel_module 22-secret-manager-client
 build_bazel_module 22-timeout-engine
 build_bazel_module 22-wait-engine
 build_java_proto_module 13-grpc-api
