10da2f0528a2e77ead3771d63041c06791fb17d6
prashantsharma04 <prashant.sharma@harness.io>
2020-10-05 17:30:30 +0530

    [BT-82]: 22-ng-core-clients in bazel (#17315)
    * adding deos in 29-orchestration
    
    * delomboked 3 classes
    
    * add build in 29
    
    * Revert "delomboked 3 classes"
    
    This reverts commit e766549cbb552210f09ab29f121ccf9c399f86e3.
    
    * rebase without 29
    
    * rebase
    
    * [BT-69]: Port 22-ng-core to bazel build
    
    * [BT-82]: 22-ng-core-clients in bazel
    
    Co-authored-by: Brijesh Dhakar <brijesh.dhakar@harness.io>
    Co-authored-by: Prashant Pal <prashant.pal@harness.io>

--- scripts/bazel/bazel_script.sh
+++ scripts/bazel/bazel_script.sh
@@ -101,6 +101,7 @@ build_bazel_module 21-persistence
 build_bazel_tests 21-persistence
 build_bazel_module 22-delegate-service-beans
 build_bazel_module 22-ng-core
+build_bazel_module 22-ng-core-clients
 build_bazel_module 22-ng-delegate-service-beans
 build_bazel_module 22-ng-project-n-orgs
 build_bazel_module 22-secret-manager-client
