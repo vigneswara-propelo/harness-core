ce63fea4f9468bc01d38a3f2897b3607e9b365a5
Deepak Patankar <deepak.patankar@harness.io>
2020-10-15 09:31:59 +0530

    [CDNG-1481]: Move the Identifier Ref from 12 commons to 13 ng commons (#17571)
    * [CDNG-1481]: Move the Identifier Ref from 12 to 13
    
    * [CDNG-1481]: Move the identifier ref to 13 commons
    
    * [CDNG-1481]: Move the secret concept to 13 commons
    
    * [CDNG-1481]: Add dependency on ng commons for 19 and 20 delegate tasks

--- 12-commons/src/main/java/io/harness/serializer/kryo/CommonsKryoRegistrar.java
+++ 12-commons/src/main/java/io/harness/serializer/kryo/CommonsKryoRegistrar.java
@@ -2,7 +2,6 @@ package io.harness.serializer.kryo;
 
 import com.esotericsoftware.kryo.Kryo;
 import io.harness.encryption.Scope;
-import io.harness.encryption.SecretRefData;
 import io.harness.exception.ArtifactServerException;
 import io.harness.exception.GeneralException;
 import io.harness.exception.InvalidArtifactServerException;
@@ -16,7 +15,6 @@ public class CommonsKryoRegistrar implements KryoRegistrar {
   public void register(Kryo kryo) {
     kryo.register(VerificationOperationException.class, 3001);
     kryo.register(ServiceNowException.class, 3002);
-    kryo.register(SecretRefData.class, 3003);
     kryo.register(Scope.class, 3004);
     kryo.register(GeneralException.class, 3005);
     kryo.register(ArtifactServerException.class, 7244);
--- 13-ng-commons/src/main/java/io/serializer/kryo/NGCommonsKryoRegistrar.java
+++ 13-ng-commons/src/main/java/io/serializer/kryo/NGCommonsKryoRegistrar.java
@@ -1,6 +1,7 @@
 package io.serializer.kryo;
 
 import com.esotericsoftware.kryo.Kryo;
+import io.harness.encryption.SecretRefData;
 import io.harness.ng.core.BaseNGAccess;
 import io.harness.serializer.KryoRegistrar;
 
@@ -8,5 +9,6 @@ public class NGCommonsKryoRegistrar implements KryoRegistrar {
   @Override
   public void register(Kryo kryo) {
     kryo.register(BaseNGAccess.class, 54324);
+    kryo.register(SecretRefData.class, 3003);
   }
 }
