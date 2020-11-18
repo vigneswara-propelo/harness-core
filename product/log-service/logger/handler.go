0aa1b0ce742e75bdeec937d1b42734b46e173760
Vistaar Juneja <vistaar.juneja@harness.io>
2020-11-18 15:58:07 +0530

    [CI-430]: Add op type, account ID, time taken for each operation in log service (#18338)

--- product/log-service/logger/handler.go
+++ product/log-service/logger/handler.go
@@ -18,16 +18,21 @@ func Middleware(next http.Handler) http.Handler {
 		}
 		ctx := r.Context()
 		log := FromContext(ctx).WithField("request-id", id)
+		accountID := r.FormValue("accountID")
+		log = log.WithFields(logrus.Fields{
+			"accountID": accountID,
+			"method":    r.Method,
+			"request":   r.RequestURI,
+			"remote":    r.RemoteAddr,
+		})
 		ctx = WithContext(ctx, log)
 		start := time.Now()
 		next.ServeHTTP(w, r.WithContext(ctx))
 		end := time.Now()
-		log.WithFields(logrus.Fields{
-			"method":  r.Method,
-			"request": r.RequestURI,
-			"remote":  r.RemoteAddr,
+		log = log.WithFields(logrus.Fields{
 			"latency": end.Sub(start),
 			"time":    end.Format(time.RFC3339),
-		}).Trace()
+		})
+		log.Infof("Completed request")
 	})
 }
