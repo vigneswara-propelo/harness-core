dfb81c88ce3b28f394588f3e3e951722bd84eeb5
Sahil Hindwani <sahil.hindwani@harness.io>
2020-12-03 20:41:50 +0530

    [CDNG-2777]: Fully qualified name till given root and till given field (#18802)
    * [CDNG-2777]: Fully qualified name till given root and till given field
    
    * [CDNG-2777]: Add one more function to return the QN between two fieldNames

--- 884-pms-commons/src/main/java/io/harness/pms/yaml/YamlUtils.java
+++ 884-pms-commons/src/main/java/io/harness/pms/yaml/YamlUtils.java
@@ -2,6 +2,8 @@ package io.harness.pms.yaml;
 
 import static io.harness.data.structure.UUIDGenerator.generateUuid;
 
+import io.harness.data.structure.EmptyPredicate;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -14,12 +16,15 @@ import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.google.common.base.Preconditions;
 import java.io.IOException;
-import java.util.Iterator;
+import java.util.*;
 import java.util.Map.Entry;
+import java.util.stream.Collectors;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class YamlUtils {
+  private static List<String> ignorableStringForQualifiedName = Arrays.asList("step", "parallel", "spec");
+
   private final ObjectMapper mapper;
 
   static {
@@ -76,6 +81,103 @@ public class YamlUtils {
     }
   }
 
+  /**
+   * Get Qualified Name till the pipeline level
+   * @param yamlNode
+   * @return
+   */
+  public String getFullyQualifiedName(YamlNode yamlNode) {
+    return getQualifiedNameList(yamlNode, "pipeline").stream().collect(Collectors.joining("."));
+  }
+
+  /**
+   *
+   * Gives the qualified Name till the given field name.
+   *
+   * @param yamlNode
+   * @param fieldName - the field till which we want the qualified name
+   * @return
+   */
+  public String getQualifiedNameTillGivenField(YamlNode yamlNode, String fieldName) {
+    return getQualifiedNameList(yamlNode, fieldName).stream().collect(Collectors.joining("."));
+  }
+
+  /**
+   * Gets qualified Name between from and to. Starting from the given yamlNode
+   * @param yamlNode
+   * @param from
+   * @param to
+   * @return
+   */
+  public String getQNBetweenTwoFields(YamlNode yamlNode, String from, String to) {
+    List<String> qualifiedNames = getQualifiedNameList(yamlNode, "pipeline");
+    String response = "";
+    for (String qualifiedName : qualifiedNames) {
+      if (qualifiedName.equals(from)) {
+        response += qualifiedName + ".";
+      }
+      if (qualifiedName.equals(to)) {
+        response += qualifiedName;
+        break;
+      }
+    }
+    return response;
+  }
+
+  private List<String> getQualifiedNameList(YamlNode yamlNode, String fieldName) {
+    if (yamlNode.getParentNode() == null) {
+      return Collections.singletonList(getQNForNode(yamlNode, null));
+    }
+    String qualifiedName = getQNForNode(yamlNode, yamlNode.getParentNode());
+    if (EmptyPredicate.isEmpty(qualifiedName)) {
+      return getQualifiedNameList(yamlNode.getParentNode(), fieldName);
+    }
+    if (qualifiedName.equals(fieldName)) {
+      List<String> qualifiedNameList = new ArrayList<>();
+      qualifiedNameList.add(qualifiedName);
+      return qualifiedNameList;
+    }
+    List<String> qualifiedNameList = getQualifiedNameList(yamlNode.getParentNode(), fieldName);
+    qualifiedNameList.add(qualifiedName);
+    return qualifiedNameList;
+  }
+
+  private String getQNForNode(YamlNode yamlNode, YamlNode parentNode) {
+    if (parentNode == null) {
+      return "";
+    }
+    if (parentNode.getParentNode() != null && parentNode.getParentNode().isArray()) {
+      if (yamlNode.getIdentifier() != null) {
+        return yamlNode.getIdentifier();
+      } else {
+        return "";
+      }
+    }
+    YamlField field = null;
+    // Because UUID won't be there in leaf objects
+    if (yamlNode.getUuid() != null) {
+      field = getMatchingFieldNameFromParentUsingUUID(parentNode, yamlNode.getUuid());
+    } else {
+      field = getMatchingFieldNameFromParentUsingValueAsText(parentNode, yamlNode.getCurrJsonNode().asText());
+    }
+
+    if (field == null || shouldNotIncludeInQualifiedName(field.getName())) {
+      return "";
+    }
+
+    return field.getName();
+  }
+
+  private boolean shouldNotIncludeInQualifiedName(String fieldName) {
+    if (ignorableStringForQualifiedName.contains(fieldName)) {
+      return true;
+    }
+    if (fieldName.contains("Definition")) {
+      return true;
+    }
+    return false;
+  }
+
   private void injectUuidInObject(JsonNode node) {
     ObjectNode objectNode = (ObjectNode) node;
     objectNode.put(YamlNode.UUID_FIELD_NAME, generateUuid());
@@ -90,5 +192,23 @@ public class YamlUtils {
     for (Iterator<JsonNode> it = arrayNode.elements(); it.hasNext();) {
       injectUuid(it.next());
     }
+  }
+
+  private YamlField getMatchingFieldNameFromParentUsingUUID(YamlNode parent, String uuid) {
+    for (YamlField field : parent.fields()) {
+      if (uuid.equals(parent.getField(field.getName()).getNode().getUuid())) {
+        return field;
+      }
+    }
+    return null;
+  }
+
+  public YamlField getMatchingFieldNameFromParentUsingValueAsText(YamlNode parent, String value) {
+    for (YamlField field : parent.fields()) {
+      if (value.equals(parent.getField(field.getName()).getNode().getCurrJsonNode().asText())) {
+        return field;
+      }
+    }
+    return null;
   }
 }
--- 884-pms-commons/src/test/java/io/harness/pms/yaml/YamlUtilsTest.java
+++ 884-pms-commons/src/test/java/io/harness/pms/yaml/YamlUtilsTest.java
@@ -1,6 +1,7 @@
 package io.harness.pms.yaml;
 
 import static io.harness.rule.OwnerRule.ARCHIT;
+import static io.harness.rule.OwnerRule.SAHIL;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -59,5 +60,146 @@ public class YamlUtilsTest extends CategoryTest {
     YamlField siblingOfStage2 = stage2Node.nextSiblingFromParentArray("stage", Arrays.asList("stage", "parallel"));
     assertThat(siblingOfStage2.getNode().asArray().get(0).getField("stage").getNode().getIdentifier())
         .isEqualTo(parallel1Node.asArray().get(0).getField("stage").getNode().getIdentifier());
+  }
+
+  @Test
+  @Owner(developers = SAHIL)
+  @Category(UnitTests.class)
+  public void testgetFullyQualifiedName() throws IOException {
+    ClassLoader classLoader = this.getClass().getClassLoader();
+    final URL testFile = classLoader.getResource("pipeline.yaml");
+    String yamlContent = Resources.toString(testFile, Charsets.UTF_8);
+    YamlField yamlField = YamlUtils.readTree(YamlUtils.injectUuid(yamlContent));
+    // Pipeline Node
+    YamlNode pipelineNode = yamlField.getNode().getField("pipeline").getNode();
+
+    // Stages Node
+    YamlField stagesNode = pipelineNode.getField("stages");
+    // Stage1 Node
+    YamlNode stage1Node = stagesNode.getNode().asArray().get(0).getField("stage").getNode();
+
+    String stageFQN = YamlUtils.getFullyQualifiedName(stage1Node);
+    assertThat(stageFQN).isEqualTo("pipeline.stages.qaStage");
+    // Stage1 Service Node
+    YamlNode serviceNode = stage1Node.getField("spec").getNode().getField("service").getNode();
+    assertThat(YamlUtils.getFullyQualifiedName(serviceNode)).isEqualTo("pipeline.stages.qaStage.service");
+
+    // image Path qualified Name
+    YamlNode imagePath = serviceNode.getField("serviceDefinition")
+                             .getNode()
+                             .getField("spec")
+                             .getNode()
+                             .getField("artifacts")
+                             .getNode()
+                             .getField("primary")
+                             .getNode()
+                             .getField("spec")
+                             .getNode()
+                             .getField("imagePath")
+                             .getNode();
+    assertThat(YamlUtils.getFullyQualifiedName(imagePath))
+        .isEqualTo("pipeline.stages.qaStage.service.artifacts.primary.imagePath");
+
+    // infrastructure qualified name
+    YamlNode infraNode = stage1Node.getField("spec").getNode().getField("infrastructure").getNode();
+    assertThat(YamlUtils.getFullyQualifiedName(infraNode)).isEqualTo("pipeline.stages.qaStage.infrastructure");
+
+    // step qualified name
+    YamlNode stepsNode =
+        stage1Node.getField("spec").getNode().getField("execution").getNode().getField("steps").getNode();
+    YamlNode step1Node = stepsNode.asArray().get(0).getField("step").getNode();
+    assertThat(YamlUtils.getFullyQualifiedName(step1Node))
+        .isEqualTo("pipeline.stages.qaStage.execution.steps.rolloutDeployment");
+  }
+
+  @Test
+  @Owner(developers = SAHIL)
+  @Category(UnitTests.class)
+  public void testgetQNBetweenTwoFields() throws IOException {
+    ClassLoader classLoader = this.getClass().getClassLoader();
+    final URL testFile = classLoader.getResource("pipeline.yaml");
+    String yamlContent = Resources.toString(testFile, Charsets.UTF_8);
+    YamlField yamlField = YamlUtils.readTree(YamlUtils.injectUuid(yamlContent));
+    // Pipeline Node
+    YamlNode pipelineNode = yamlField.getNode().getField("pipeline").getNode();
+
+    // Stages Node
+    YamlField stagesNode = pipelineNode.getField("stages");
+    // Stage1 Node
+    YamlNode stage1Node = stagesNode.getNode().asArray().get(0).getField("stage").getNode();
+
+    // Stage1 Service Node
+    YamlNode serviceNode = stage1Node.getField("spec").getNode().getField("service").getNode();
+
+    // image Path qualified Name
+    YamlNode imagePath = serviceNode.getField("serviceDefinition")
+                             .getNode()
+                             .getField("spec")
+                             .getNode()
+                             .getField("artifacts")
+                             .getNode()
+                             .getField("primary")
+                             .getNode()
+                             .getField("spec")
+                             .getNode()
+                             .getField("imagePath")
+                             .getNode();
+    assertThat(YamlUtils.getQNBetweenTwoFields(imagePath, "stages", "service")).isEqualTo("stages.service");
+
+    // step qualified name
+    YamlNode stepsNode =
+        stage1Node.getField("spec").getNode().getField("execution").getNode().getField("steps").getNode();
+    YamlNode step1Node = stepsNode.asArray().get(0).getField("step").getNode();
+    assertThat(YamlUtils.getQNBetweenTwoFields(step1Node, "execution", "steps")).isEqualTo("execution.steps");
+  }
+
+  @Test
+  @Owner(developers = SAHIL)
+  @Category(UnitTests.class)
+  public void testQNTillGivenFieldName() throws IOException {
+    ClassLoader classLoader = this.getClass().getClassLoader();
+    final URL testFile = classLoader.getResource("pipeline.yaml");
+    String yamlContent = Resources.toString(testFile, Charsets.UTF_8);
+    YamlField yamlField = YamlUtils.readTree(YamlUtils.injectUuid(yamlContent));
+    // Pipeline Node
+    YamlNode pipelineNode = yamlField.getNode().getField("pipeline").getNode();
+
+    // Stages Node
+    YamlField stagesNode = pipelineNode.getField("stages");
+    // Stage1 Node
+    YamlNode stage1Node = stagesNode.getNode().asArray().get(0).getField("stage").getNode();
+
+    String stageFQN = YamlUtils.getFullyQualifiedName(stage1Node);
+    assertThat(stageFQN).isEqualTo("pipeline.stages.qaStage");
+    // Stage1 Service Node
+    YamlNode serviceNode = stage1Node.getField("spec").getNode().getField("service").getNode();
+    assertThat(YamlUtils.getQualifiedNameTillGivenField(serviceNode, "stages")).isEqualTo("stages.qaStage.service");
+
+    // image Path qualified Name
+    YamlNode imagePath = serviceNode.getField("serviceDefinition")
+                             .getNode()
+                             .getField("spec")
+                             .getNode()
+                             .getField("artifacts")
+                             .getNode()
+                             .getField("primary")
+                             .getNode()
+                             .getField("spec")
+                             .getNode()
+                             .getField("imagePath")
+                             .getNode();
+    assertThat(YamlUtils.getQualifiedNameTillGivenField(imagePath, "service"))
+        .isEqualTo("service.artifacts.primary.imagePath");
+
+    // infrastructure qualified name
+    YamlNode infraNode = stage1Node.getField("spec").getNode().getField("infrastructure").getNode();
+    assertThat(YamlUtils.getQualifiedNameTillGivenField(infraNode, "infrastructure")).isEqualTo("infrastructure");
+
+    // step qualified name
+    YamlNode stepsNode =
+        stage1Node.getField("spec").getNode().getField("execution").getNode().getField("steps").getNode();
+    YamlNode step1Node = stepsNode.asArray().get(0).getField("step").getNode();
+    assertThat(YamlUtils.getQualifiedNameTillGivenField(step1Node, "execution"))
+        .isEqualTo("execution.steps.rolloutDeployment");
   }
 }
