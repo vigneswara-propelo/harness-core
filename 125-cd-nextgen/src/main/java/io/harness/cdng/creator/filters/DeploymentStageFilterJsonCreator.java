42aa45d4de1e8e6c8660b34e52acb920357328ba
prashantsharma04 <prashant.sharma@harness.io>
2020-12-08 11:12:38 +0530

    [CDNG-2965]: Filter Json in 125 (#18900)
    * first change in deployment stage filter json
    
    * [CDNG-2965]: Filter Json in 125
    
    * added code in function
    
    * clang and singular
    
    * checkstyle
    
    * removed to string

--- 125-cd-nextgen/src/main/java/io/harness/cdng/creator/filters/DeploymentStageFilterJsonCreator.java
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/creator/filters/DeploymentStageFilterJsonCreator.java
@@ -1,7 +1,15 @@
 package io.harness.cdng.creator.filters;
 
+import static io.harness.data.structure.EmptyPredicate.isNotEmpty;
+
 import io.harness.cdng.creator.plan.stage.DeploymentStageConfig;
+import io.harness.cdng.pipeline.PipelineInfrastructure;
+import io.harness.cdng.service.beans.ServiceConfig;
+import io.harness.cdng.service.beans.ServiceDefinition;
+import io.harness.pms.cdng.sample.cd.creator.filters.CdFilter;
+import io.harness.pms.cdng.sample.cd.creator.filters.CdFilter.CdFilterBuilder;
 import io.harness.pms.filter.creation.FilterCreationResponse;
+import io.harness.pms.filter.creation.FilterCreationResponse.FilterCreationResponseBuilder;
 import io.harness.pms.sdk.core.filter.creation.beans.FilterCreationContext;
 import io.harness.pms.sdk.core.pipeline.filters.FilterJsonCreator;
 
@@ -23,6 +31,31 @@ public class DeploymentStageFilterJsonCreator implements FilterJsonCreator<Deplo
   @Override
   public FilterCreationResponse handleNode(
       FilterCreationContext filterCreationContext, DeploymentStageConfig yamlField) {
-    return null;
+    FilterCreationResponseBuilder creationResponse = FilterCreationResponse.builder();
+
+    CdFilterBuilder cdFilter = CdFilter.builder();
+    if (yamlField.getExecution() == null) {
+      return creationResponse.build();
+    }
+
+    ServiceConfig service = yamlField.getService();
+    if (service != null && isNotEmpty(service.getName().getValue())) {
+      cdFilter.serviceName(service.getName().getValue());
+    }
+
+    ServiceDefinition serviceDefinition = service.getServiceDefinition();
+    if (serviceDefinition != null && serviceDefinition.getType() != null) {
+      cdFilter.deploymentType(serviceDefinition.getType());
+    }
+
+    PipelineInfrastructure infrastructure = yamlField.getInfrastructure();
+    if (infrastructure != null && infrastructure.getEnvironment() != null
+        && isNotEmpty(infrastructure.getEnvironment().getName().getValue())) {
+      cdFilter.environmentName(infrastructure.getEnvironment().getName().getValue());
+    }
+
+    creationResponse.pipelineFilter(cdFilter.build());
+    creationResponse.stageCount(1);
+    return creationResponse.build();
   }
 }
