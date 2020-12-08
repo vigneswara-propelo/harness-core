52bb2ad033979bd71177a73056ad04c84630f9ba
Harsh Jain <harsh.jain@harness.io>
2020-12-08 12:19:53 +0530

    [CI-594]: fix outcome serialization (#18928)

--- 950-delegate-tasks-beans/src/main/java/io/harness/delegate/task/stepstatus/StepMapOutput.java
+++ 950-delegate-tasks-beans/src/main/java/io/harness/delegate/task/stepstatus/StepMapOutput.java
@@ -1,5 +1,6 @@
 package io.harness.delegate.task.stepstatus;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import java.util.Map;
 import lombok.Builder;
 import lombok.Data;
@@ -7,6 +8,7 @@ import lombok.Singular;
 
 @Data
 @Builder
+@JsonTypeName("stepMapOutput")
 public class StepMapOutput implements StepOutput {
   @Singular("output") Map<String, String> map;
 }
--- 950-delegate-tasks-beans/src/main/java/io/harness/delegate/task/stepstatus/StepOutput.java
+++ 950-delegate-tasks-beans/src/main/java/io/harness/delegate/task/stepstatus/StepOutput.java
@@ -1,3 +1,8 @@
 package io.harness.delegate.task.stepstatus;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type", defaultImpl = StepMapOutput.class)
+@JsonSubTypes({ @JsonSubTypes.Type(value = StepMapOutput.class, name = "stepMapOutput") })
 public interface StepOutput {}
