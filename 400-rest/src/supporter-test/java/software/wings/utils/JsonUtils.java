209cb2255ce344757f3d926ca701eb12d1b76902
George Georgiev <george@harness.io>
2020-12-08 18:03:07 -0800

    [BT-0]: fix graphql tests (#18966)
    * [BT-0]: fix graphql tests

--- 400-rest/src/supporter-test/java/software/wings/utils/JsonUtils.java
+++ 400-rest/src/supporter-test/java/software/wings/utils/JsonUtils.java
@@ -5,41 +5,42 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class JsonUtils {
-  private static final ObjectMapper OBJECT_MAPPER;
+  private static final ObjectMapper objectMapper;
 
   static {
-    OBJECT_MAPPER = new ObjectMapper();
-    OBJECT_MAPPER.enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING);
-    OBJECT_MAPPER.enable(DeserializationFeature.READ_ENUMS_USING_TO_STRING);
-    OBJECT_MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    objectMapper = new ObjectMapper();
+    objectMapper.enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING);
+    objectMapper.enable(DeserializationFeature.READ_ENUMS_USING_TO_STRING);
+    objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
   }
 
   public static <T> T readResourceFile(String filPath, Class<T> tClass) {
-    File file = new File(JsonUtils.class.getClassLoader().getResource(filPath).getFile());
+    InputStream stream = JsonUtils.class.getClassLoader().getResourceAsStream(filPath);
     try {
-      return OBJECT_MAPPER.readValue(file, tClass);
+      return objectMapper.readValue(stream, tClass);
     } catch (IOException e) {
       throw new IllegalStateException(e);
     }
   }
+
   public static <T> T readResourceFile(String filPath, TypeReference<T> tClass) {
-    File file = new File(JsonUtils.class.getClassLoader().getResource(filPath).getFile());
+    InputStream stream = JsonUtils.class.getClassLoader().getResourceAsStream(filPath);
     try {
-      return OBJECT_MAPPER.readValue(file, tClass);
+      return objectMapper.readValue(stream, tClass);
     } catch (IOException e) {
       throw new IllegalStateException(e);
     }
   }
 
   public static <T> T convertJsonNodeToObj(JsonNode jsonNode, TypeReference<T> tClass) {
-    return OBJECT_MAPPER.convertValue(jsonNode, tClass);
+    return objectMapper.convertValue(jsonNode, tClass);
   }
 
   public static JsonNode toJsonNode(Object obj) {
-    return OBJECT_MAPPER.convertValue(obj, JsonNode.class);
+    return objectMapper.convertValue(obj, JsonNode.class);
   }
 }
