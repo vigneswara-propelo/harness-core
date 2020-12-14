9b2705221a074656a0ff2fc6f5c7e18b696f3d4a
prashantsharma04 <prashant.sharma@harness.io>
2020-12-14 18:23:42 +0530

    [CDNG-2978]: Code for CDNGFilterCreationResponseMerger in 125-cd-nextgen (#18930)
    * [CDNG-2978]: Code for CDNGFilterCreationResponseMerger in 125-cd-nextgen
    
    * added a test
    
    * checkstyle
    
    * format

--- 125-cd-nextgen/src/main/java/io/harness/cdng/creator/filters/CDNGFilterCreationResponseMerger.java
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/creator/filters/CDNGFilterCreationResponseMerger.java
@@ -1,9 +1,35 @@
 package io.harness.cdng.creator.filters;
 
+import static io.harness.data.structure.EmptyPredicate.isNotEmpty;
+
+import io.harness.pms.cdng.sample.cd.creator.filters.CdFilter;
 import io.harness.pms.filter.creation.FilterCreationResponse;
 import io.harness.pms.sdk.core.pipeline.filters.FilterCreationResponseMerger;
 
 public class CDNGFilterCreationResponseMerger implements FilterCreationResponseMerger {
   @Override
-  public void mergeFilterCreationResponse(FilterCreationResponse finalResponse, FilterCreationResponse current) {}
+  public void mergeFilterCreationResponse(FilterCreationResponse finalResponse, FilterCreationResponse current) {
+    if (current == null || current.getPipelineFilter() == null) {
+      return;
+    }
+
+    if (finalResponse.getPipelineFilter() == null) {
+      finalResponse.setPipelineFilter(CdFilter.builder().build());
+    }
+
+    CdFilter finalCdFilter = (CdFilter) finalResponse.getPipelineFilter();
+    CdFilter currentCdFilter = (CdFilter) current.getPipelineFilter();
+
+    if (isNotEmpty(currentCdFilter.getDeploymentTypes())) {
+      finalCdFilter.addDeploymentTypes(currentCdFilter.getDeploymentTypes());
+    }
+
+    if (isNotEmpty(currentCdFilter.getEnvironmentNames())) {
+      finalCdFilter.addEnvironmentNames(currentCdFilter.getEnvironmentNames());
+    }
+
+    if (isNotEmpty(currentCdFilter.getServiceNames())) {
+      finalCdFilter.addServiceNames(currentCdFilter.getServiceNames());
+    }
+  }
 }
---
+++ 125-cd-nextgen/src/test/java/io/harness/cdng/creator/filters/CDNGFilterCreationResponseMergerTest.java
@@ -0,0 +1,29 @@
+package io.harness.cdng.creator.filters;
+
+import static io.harness.rule.OwnerRule.PRASHANTSHARMA;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.harness.CategoryTest;
+import io.harness.category.element.UnitTests;
+import io.harness.pms.cdng.sample.cd.creator.filters.CdFilter;
+import io.harness.pms.filter.creation.FilterCreationResponse;
+import io.harness.rule.Owner;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+public class CDNGFilterCreationResponseMergerTest extends CategoryTest {
+  CDNGFilterCreationResponseMerger cdngFilterCreationResponseMerger = new CDNGFilterCreationResponseMerger();
+  FilterCreationResponse finalResponse = FilterCreationResponse.builder().build();
+  FilterCreationResponse currentResponse = FilterCreationResponse.builder().build();
+
+  @Test
+  @Owner(developers = PRASHANTSHARMA)
+  @Category(UnitTests.class)
+  public void cdngFilter() {
+    currentResponse.setPipelineFilter(CdFilter.builder().build());
+    cdngFilterCreationResponseMerger.mergeFilterCreationResponse(finalResponse, currentResponse);
+    assertThat(finalResponse.getPipelineFilter()).isNotNull();
+  }
+}
