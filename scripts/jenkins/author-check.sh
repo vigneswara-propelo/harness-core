39467f954c2cbb8df689715d0a29b830302eb7ea
boopesh007 <68562380+boopesh007@users.noreply.github.com>
2021-01-12 11:26:10 +0530

    [BT-205]: Adding a Enhancement to make case insensitive while hot-fixing and improving messaging  (#19721)
    * [BT-205]: Adding case insensitiveness and improving messaging

--- scripts/jenkins/author-check.sh
+++ scripts/jenkins/author-check.sh
@@ -138,6 +138,7 @@ git log -1000 --oneline --format='%aN <%aE>' | sort -u |\
     grep -iv "^diptiman.adak <diptiman.adak@harness.io>$" |\
     grep -iv "^Piyush Patel <piyush.patel@harness.io>$" |\
     grep -iv "^Tudor Macari <tudor.macari@harness.io>$" |\
+    grep -iv "^Boopesh Shanmugam <boopesh.shanmugam@harness.io>$" |\
     grep -iv "^Sainath Batthala <sainath.batthala@harness.io>$" ` || :
 
 if [ ! -z "$UNKNOWN_USERS" ]
--- scripts/jenkins/release-branch-test.sh
+++ scripts/jenkins/release-branch-test.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-
 PROJECTS="BT|CCE|CCM|CDC|CDNG|CDP|CE|CI|CV|CVNG|DEL|DOC|DX|ER|OPS|PL|SEC|SWAT|GTM"
-
-git log --remotes=origin/release/* --pretty=oneline --abbrev-commit | grep -iE "\[(${PROJECTS})-[0-9]+]:" -o | sort | uniq > release.txt
-git log --remotes=origin/[m]aster --pretty=oneline --abbrev-commit | grep -iE "\[(${PROJECTS})-[0-9]+]:" -o | sort | uniq > master.txt
-
+git log --remotes=origin/release/* --pretty=oneline --abbrev-commit | grep -iE "\[(${PROJECTS})-[0-9]+]:" -o | sort | uniq | tr '[:lower:]' '[:upper:]' > release.txt
+git log --remotes=origin/[m]aster --pretty=oneline --abbrev-commit | grep -iE "\[(${PROJECTS})-[0-9]+]:" -o | sort | uniq | tr '[:lower:]' '[:upper:]' > master.txt
 NOT_MERGED=`comm -23 release.txt master.txt | tr '\n' ' '`
-echo NOT_MERGED="${NOT_MERGED}" > envvars
+if [ -z "$NOT_MERGED" ]
+then
+      echo "Hotfix changes are reflected in Master as well" > envvars
+else
+      echo NOT_MERGED="${NOT_MERGED}" > envvars
+fi
