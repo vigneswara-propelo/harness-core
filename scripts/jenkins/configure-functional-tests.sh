8c97d2fbd05cd743fa2d4932251b057592a91f45
Raj Patel <raj.patel@harness.io>
2021-01-13 10:19:15 +0530

    [CDNG-0]: Fix bash script sed regex - functional tests (#19731)
    * [CDNG-0]: Fix bash script sed regex
    
    * [CDNG-0]: remove mac specific flag
    
    * [CDNG-0]: Fix the ci config.yml
    
    * [CDNG-0]: Add a validation for mongo urls after replace
    
    * [CDNG-0]: fix validations script for unreplaced urls
    
    * [CDNG-0]: fix grep flag for jenkins pod
    
    * [CDNG-0]: Fix the ci mongo db name

--- scripts/jenkins/configure-functional-tests.sh
+++ scripts/jenkins/configure-functional-tests.sh
@@ -1,19 +1,39 @@
-sed -i "s|uri: mongodb://localhost:27017/harness|uri: ${MONGO_URI}|" 400-rest/config.yml
-sed -i 's|disabledCache: \[\]|disabledCache: \["userPermissionCache"\]|' 400-rest/config.yml
-sed -i 's|disabledCaches: \[\]|disabledCaches: \["userPermissionCache"\]|' 400-rest/config.yml
-sed -i "s|uri: http://localhost:9200|uri: ${ELASTICSEARCH_URI}|" 400-rest/config.yml
-sed -i "s|indexSuffix: _default|indexSuffix: ${ELASTICSEARCH_INDEX_SUFFIX}|" 400-rest/config.yml
-sed -i "s|searchEnabled: false|searchEnabled: true|" 400-rest/config.yml
-sed -i "s|sentinel: false|sentinel: true|" 400-rest/config.yml
-sed -i "s|masterName: \"test\"|masterName: ${REDIS_MASTER_NAME}|" 400-rest/config.yml
-sed -i "s|envNamespace: \"\"|envNamespace: ${TIMESTAMP}|" 400-rest/config.yml
-sed -i "s|redis://redis1:26379|${REDIS_SENTINEL1}|" 400-rest/config.yml
-sed -i "s|redis://redis2:26379|${REDIS_SENTINEL2}|" 400-rest/config.yml
-sed -i "s|redis://redis3:26379|${REDIS_SENTINEL3}|" 400-rest/config.yml
-sed -i "s|distributedLockImplementation: MONGO|distributedLockImplementation: REDIS|" 400-rest/config.yml
+function replace_text() {
+  search_string=$1
+  replace_string=$2
+  file_path=$3
+  sed -i "s|$search_string|$replace_string|" $file_path
+}
 
-sed -i "s|uri: mongodb://localhost:27017/harness|uri: ${MONGO_URI}|" 350-event-server/event-service-config.yml
-sed -i "s|uri: mongodb://localhost:27017/harness|uri: ${MONGO_URI}|" 280-batch-processing/batch-processing-config.yml
-sed -i "s|uri: mongodb://localhost:27017/events|uri: ${MONGO_URI}_events|" 280-batch-processing/batch-processing-config.yml
-sed -i "s|uri: mongodb://localhost:27017/harness|uri: ${MONGO_URI}|" 210-command-library-server/command-library-server-config.yml
-sed -i "s|uri: mongodb://localhost:27017/harness|uri: ${MONGO_URI}|" 310-ci-manager/ci-manager-config.yml
+replace_text "uri: mongodb://localhost:27017/harness" "uri: ${MONGO_URI}" 400-rest/config.yml
+replace_text "disabledCache: \[\]" "disabledCache: \["userPermissionCache"\]" 400-rest/config.yml
+replace_text "disabledCaches: \[\]" "disabledCaches: \["userPermissionCache"\]" 400-rest/config.yml
+replace_text "uri: http://localhost:9200" "uri: ${ELASTICSEARCH_URI}" 400-rest/config.yml
+replace_text "indexSuffix: _default" "indexSuffix: ${ELASTICSEARCH_INDEX_SUFFIX}" 400-rest/config.yml
+replace_text "searchEnabled: false" "searchEnabled: true" 400-rest/config.yml
+replace_text "sentinel: false" "sentinel: true" 400-rest/config.yml
+replace_text "masterName: \"test\"" "masterName: \"${REDIS_MASTER_NAME}\"" 400-rest/config.yml
+replace_text "envNamespace: \"\"" "envNamespace: ${TIMESTAMP}" 400-rest/config.yml
+replace_text "\"redis://redis1:26379\"" "\"${REDIS_SENTINEL1}\"" 400-rest/config.yml
+replace_text "\"redis://redis2:26379\"" "\"${REDIS_SENTINEL2}\"" 400-rest/config.yml
+replace_text "\"redis://redis3:26379\"" "\"${REDIS_SENTINEL3}\"" 400-rest/config.yml
+replace_text "distributedLockImplementation: MONGO" "distributedLockImplementation: REDIS" 400-rest/config.yml
+
+replace_text "uri: mongodb://localhost:27017/harness" "uri: ${MONGO_URI}" 350-event-server/event-service-config.yml
+replace_text "uri: mongodb://localhost:27017/harness" "uri: ${MONGO_URI}" 280-batch-processing/batch-processing-config.yml
+replace_text "uri: mongodb://localhost:27017/events" "uri: ${MONGO_URI}_events" 280-batch-processing/batch-processing-config.yml
+replace_text "uri: mongodb://localhost:27017/harness" "uri: ${MONGO_URI}" 210-command-library-server/command-library-server-config.yml
+replace_text "uri: mongodb://localhost:27017/harnessci" "uri: ${MONGO_URI}_ci" 310-ci-manager/ci-manager-config.yml
+replace_text "uri: mongodb://localhost:27017/harness" "uri: ${MONGO_URI}" 310-ci-manager/ci-manager-config.yml
+
+CONFIG_FILES=(400-rest/config.yml 350-event-server/event-service-config.yml 280-batch-processing/batch-processing-config.yml 210-command-library-server/command-library-server-config.yml 310-ci-manager/ci-manager-config.yml)
+
+for config_file in ${CONFIG_FILES[@]}; do
+  mongo_uri_count=$(grep -P "^\s*uri: mongodb://.*:27017" $config_file | grep -v localhost:27017 | wc -l | awk '{print $1}')
+  good_mongo_uri_count=$(grep -P "^\s*uri: mongodb://.*:27017/functional_test_\d{10}(_[a-zA-Z]+)?$" $config_file | grep -v localhost:27017 | wc -l | awk '{print $1}')
+  echo "$config_file - Mongo URI count: $mongo_uri_count | Good Mongo URI count: $good_mongo_uri_count"
+  if [[ $mongo_uri_count != $good_mongo_uri_count ]]; then
+    echo "Invalid Mongo URIs found in $config_file, please follow the format - functional_test_<epoch> or functional_test_<epoch>_sometext"
+    exit 1
+  fi
+done
