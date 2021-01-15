01741bc46a2ddb3c1664078830b968ad47259554
Harsh Jain <harsh.jain@harness.io>
2021-01-15 22:55:09 +0530

    [CI-895]: add capability (#19889)

--- 950-delegate-tasks-beans/src/main/java/io/harness/delegate/beans/ci/CIK8BuildTaskParams.java
+++ 950-delegate-tasks-beans/src/main/java/io/harness/delegate/beans/ci/CIK8BuildTaskParams.java
@@ -4,6 +4,10 @@ import io.harness.delegate.beans.ci.pod.CIK8ContainerParams;
 import io.harness.delegate.beans.ci.pod.CIK8PodParams;
 import io.harness.delegate.beans.ci.pod.CIK8ServicePodParams;
 import io.harness.delegate.beans.ci.pod.ConnectorDetails;
+import io.harness.delegate.beans.connector.k8Connector.KubernetesClusterConfigDTO;
+import io.harness.delegate.beans.executioncapability.ExecutionCapability;
+import io.harness.delegate.beans.executioncapability.ExecutionCapabilityDemander;
+import io.harness.expression.ExpressionEvaluator;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
@@ -14,7 +18,7 @@ import lombok.Data;
 @Data
 @Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class CIK8BuildTaskParams implements CIBuildSetupTaskParams {
+public class CIK8BuildTaskParams implements CIBuildSetupTaskParams, ExecutionCapabilityDemander {
   @NotNull private ConnectorDetails k8sConnector;
   @NotNull private CIK8PodParams<CIK8ContainerParams> cik8PodParams;
   @NotNull private List<CIK8ServicePodParams> servicePodParams;
@@ -23,5 +27,12 @@ public class CIK8BuildTaskParams implements CIBuildSetupTaskParams {
   @Override
   public Type getType() {
     return type;
+  }
+
+  @Override
+  public List<ExecutionCapability> fetchRequiredExecutionCapabilities(ExpressionEvaluator maskingEvaluator) {
+    KubernetesClusterConfigDTO kubernetesClusterConfigDTO =
+        (KubernetesClusterConfigDTO) k8sConnector.getConnectorConfig();
+    return kubernetesClusterConfigDTO.fetchRequiredExecutionCapabilities(maskingEvaluator);
   }
 }
