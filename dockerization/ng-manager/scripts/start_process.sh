09ab58420b72950bcdec36f1913af57db9be3878
Ankush Shaw <ankush.shaw@harness.io>
2021-01-15 17:59:59 +0530

    [CDNG-3615]: configurations to add AppDynamics agent in ng-manager (#19841)
    * [CDNG-3615]: configurations to add AppDynamics agent in ng-manager
    
    * [CDNG-3615]: appD integration with notification service

--- dockerization/ng-manager/scripts/start_process.sh
+++ dockerization/ng-manager/scripts/start_process.sh
@@ -26,6 +26,13 @@ export GC_PARAMS=" -XX:+UseG1GC -XX:InitiatingHeapOccupancyPercent=40 -XX:MaxGCP
 
 export JAVA_OPTS="-Xms${MAX_MEMORY} -Xmx${MIN_MEMORY} -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:mygclogfilename.gc $GC_PARAMS"
 
+if [[ "${ENABLE_APPDYNAMICS}" == "true" ]]; then
+    mkdir /opt/harness/AppServerAgent-20.8.0.30686 && unzip AppServerAgent-20.8.0.30686.zip -d /opt/harness/AppServerAgent-20.8.0.30686
+    node_name="-Dappdynamics.agent.nodeName=$(hostname)"
+    JAVA_OPTS=$JAVA_OPTS" -Dcapsule.jvm.args=-javaagent:/opt/harness/AppServerAgent-20.8.0.30686/javaagent.jar -Dappdynamics.jvm.shutdown.mark.node.as.historical=true"
+    JAVA_OPTS="$JAVA_OPTS $node_name"
+    echo "Using Appdynamics java agent"
+fi
 
 if [[ "${DEPLOY_MODE}" == "KUBERNETES" ]] || [[ "${DEPLOY_MODE}" == "KUBERNETES_ONPREM" ]]; then
     java $JAVA_OPTS -jar $CAPSULE_JAR $COMMAND /opt/harness/config.yml
