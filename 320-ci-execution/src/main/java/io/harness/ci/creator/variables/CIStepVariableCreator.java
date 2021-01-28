f8630d0cf40943c503e723e391650b1242a42bd5
Aleksandar Radisavljevic <aleksandar.radisavljevic@harness.io>
2021-01-28 09:53:08 +0100

    [CI-1094]: variable creator for ci (#20204)
    Co-authored-by: aman <aman.singh@harness.io>

---
+++ 320-ci-execution/src/main/java/io/harness/ci/creator/variables/CIStepVariableCreator.java
@@ -0,0 +1,14 @@
+package io.harness.ci.creator.variables;
+
+import io.harness.pms.sdk.core.pipeline.variables.GenericStepVariableCreator;
+
+import com.google.common.collect.Sets;
+import java.util.Set;
+
+public class CIStepVariableCreator extends GenericStepVariableCreator {
+  @Override
+  public Set<String> getSupportedStepTypes() {
+    return Sets.newHashSet("SaveCacheS3", "BuildAndPushGCR", "BuildAndPushECR", "BuildAndPushDockerHub", "Plugin",
+        "RestoreCacheGCS", "RestoreCacheS3", "SaveCacheGCS", "Run", "S3Upload", "GCSUpload", "ArtifactoryUpload");
+  }
+}
--- 320-ci-execution/src/main/java/io/harness/ci/plan/creator/CIPipelineServiceInfoProvider.java
+++ 320-ci-execution/src/main/java/io/harness/ci/plan/creator/CIPipelineServiceInfoProvider.java
@@ -1,11 +1,14 @@
 package io.harness.ci.plan.creator;
 
+import io.harness.ci.creator.variables.CIStageVariableCreator;
+import io.harness.ci.creator.variables.CIStepVariableCreator;
 import io.harness.ci.plan.creator.execution.CIExecutionPmsPlanCreator;
 import io.harness.ci.plan.creator.filter.CIStageFilterJsonCreator;
 import io.harness.ci.plan.creator.stage.IntegrationStagePMSPlanCreator;
 import io.harness.ci.plan.creator.step.CIPMSStepPlanCreator;
 import io.harness.pms.contracts.steps.StepInfo;
 import io.harness.pms.sdk.core.pipeline.filters.FilterJsonCreator;
+import io.harness.pms.sdk.core.pipeline.variables.ExecutionVariableCreator;
 import io.harness.pms.sdk.core.plan.creation.creators.PartialPlanCreator;
 import io.harness.pms.sdk.core.plan.creation.creators.PipelineServiceInfoProvider;
 import io.harness.pms.sdk.core.variables.VariableCreator;
@@ -42,7 +45,11 @@ public class CIPipelineServiceInfoProvider implements PipelineServiceInfoProvide
 
   @Override
   public List<VariableCreator> getVariableCreators() {
-    return null;
+    List<VariableCreator> variableCreators = new ArrayList<>();
+    variableCreators.add(new CIStageVariableCreator());
+    variableCreators.add(new ExecutionVariableCreator());
+    variableCreators.add(new CIStepVariableCreator());
+    return variableCreators;
   }
 
   @Override
