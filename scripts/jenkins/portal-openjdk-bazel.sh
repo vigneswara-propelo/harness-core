e37910d1cab307ad162f87d6c3b05ed648d0a934
Ankush Shaw <ankush.shaw@harness.io>
2021-02-08 15:30:42 +0530

    [CDNG-0]: portal-openjdk-baze.sh updated (#20539)

--- scripts/jenkins/portal-openjdk-bazel.sh
+++ scripts/jenkins/portal-openjdk-bazel.sh
@@ -212,3 +212,59 @@ then
 fi
 
 cd ../..
+
+mkdir -p dist/notification-service
+cd dist/notification-service
+
+cp ../../830-notification-service/target/notification-service-capsule.jar .
+cp ../../830-notification-service/config.yml .
+cp ../../830-notification-service/keystore.jks .
+cp ../../830-notification-service/key.pem .
+cp ../../830-notification-service/cert.pem .
+cp ../../dockerization/notification-service/Dockerfile-notification-service-jenkins-k8-openjdk ./Dockerfile
+cp ../../dockerization/notification-service/Dockerfile-notification-service-jenkins-k8-gcr-openjdk ./Dockerfile-gcr
+cp -r ../../dockerization/notification-service/scripts/ .
+
+echo ${JDK} > jdk.txt
+echo ${VERSION} > version.txt
+if [ ! -z ${PURPOSE} ]
+then
+    echo ${PURPOSE} > purpose.txt
+fi
+
+cd ../..
+
+mkdir -p dist/pipeline-service
+cd dist/pipeline-service
+
+cp ${HOME}/.bazel-dirs/bin/800-pipeline-service/module_deploy.jar pipeline-service-capsule.jar
+cp ../../800-pipeline-service/config.yml .
+cp ../../800-pipeline-service/keystore.jks .
+cp ../../800-pipeline-service/key.pem .
+cp ../../800-pipeline-service/cert.pem .
+cp ../../dockerization/pipeline-service/Dockerfile-pipeline-service-jenkins-k8-openjdk ./Dockerfile
+cp ../../dockerization/pipeline-service/Dockerfile-pipeline-service-jenkins-k8-gcr-openjdk ./Dockerfile-gcr
+cp -r ../../dockerization/pipeline-service/scripts/ .
+
+echo ${JDK} > jdk.txt
+echo ${VERSION} > version.txt
+if [ ! -z ${PURPOSE} ]
+then
+    echo ${PURPOSE} > purpose.txt
+fi
+
+cd ../..
+
+mkdir -p dist/eventsapi-monitor
+cd dist/eventsapi-monitor
+
+cp -r ../../950-events-api/src/monitoring/* .
+
+echo ${JDK} > jdk.txt
+echo ${VERSION} > version.txt
+if [ ! -z ${PURPOSE} ]
+then
+    echo ${PURPOSE} > purpose.txt
+fi
+
+cd ../..
