c5a6d09c274fef5df6a51b81c7067e51701d43c2
Ankush Shaw <ankush.shaw@harness.io>
2021-02-15 15:58:50 +0530

    [CDNG-4504]: grpc config override for manager in notification (#20710)

--- dockerization/notification-service/scripts/replace_configs.sh
+++ dockerization/notification-service/scripts/replace_configs.sh
@@ -104,3 +104,11 @@ fi
 if [[ "" != "$JWT_IDENTITY_SERVICE_SECRET" ]]; then
   yq write -i $CONFIG_FILE secrets.jwtIdentityServiceSecret "$JWT_IDENTITY_SERVICE_SECRET"
 fi
+
+if [[ "" != "$GRPC_MANAGER_TARGET" ]]; then
+  yq write -i $CONFIG_FILE grpcClient.target $GRPC_MANAGER_TARGET
+fi
+
+if [[ "" != "$GRPC_MANAGER_AUTHORITY" ]]; then
+  yq write -i $CONFIG_FILE grpcClient.authority $GRPC_MANAGER_AUTHORITY
+fi
