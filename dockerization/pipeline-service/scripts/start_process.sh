aa9d86b846d8357265d3d15959d13ccbad4e8783
prashantsharma04 <prashant.sharma@harness.io>
2021-03-10 13:08:04 +0530

    [CDNG-5089]: Pipeline Service With AppD (#21406)

--- dockerization/pipeline-service/scripts/start_process.sh
+++ dockerization/pipeline-service/scripts/start_process.sh
@@ -26,6 +26,14 @@ export GC_PARAMS=" -XX:+UseG1GC -XX:InitiatingHeapOccupancyPercent=40 -XX:MaxGCP
 
 export JAVA_OPTS="-Xms${MAX_MEMORY} -Xmx${MIN_MEMORY} -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:mygclogfilename.gc $GC_PARAMS"
 
+if [[ "${ENABLE_APPDYNAMICS}" == "true" ]]; then
+    mkdir /opt/harness/AppServerAgent-20.8.0.30686 && unzip AppServerAgent-20.8.0.30686.zip -d /opt/harness/AppServerAgent-20.8.0.30686
+    node_name="-Dappdynamics.agent.nodeName=$(hostname)"
+    JAVA_OPTS=$JAVA_OPTS" -javaagent:/opt/harness/AppServerAgent-20.8.0.30686/javaagent.jar -Dappdynamics.jvm.shutdown.mark.node.as.historical=true"
+    JAVA_OPTS="$JAVA_OPTS $node_name"
+    echo "Using Appdynamics java agent"
+fi
+
 curl https://storage.googleapis.com/harness-prod-public/public/shared/tools/alpn/release/8.1.13.v20181017/alpn-boot-8.1.13.v20181017.jar  --output alpn-boot-8.1.13.v20181017.jar
 JAVA_OPTS=$JAVA_OPTS" -Xbootclasspath/p:alpn-boot-8.1.13.v20181017.jar"
 
