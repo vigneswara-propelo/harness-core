0a9d8a8c4f6f49e015168255c875d11b34fdd4e4
Adarsh Agarwal <adarsh.agarwal@harness.io>
2021-03-16 19:06:25 +0530

    [CE-4150]: Filter aggregations passed from UI (#21580)
    * [CE-4150]: Filter aggregations passed from UI
    
    * [CE-4150]: Added use of Pre Aggregated table for formTotalCountQuery

--- 400-rest/src/main/java/software/wings/graphql/datafetcher/billing/BillingDataQueryBuilder.java
+++ 400-rest/src/main/java/software/wings/graphql/datafetcher/billing/BillingDataQueryBuilder.java
@@ -158,6 +158,7 @@ public class BillingDataQueryBuilder {
           && isValidGroupByForPreAggregation(groupBy) && areFiltersValidForPreAggregation(filters)
           && areAggregationsValidForPreAggregation(aggregateFunction)) {
         selectQuery.addCustomFromTable(BILLING_DATA_HOURLY_PRE_AGGREGATED_TABLE);
+        aggregateFunction = getSupportedAggregations(aggregateFunction);
       } else {
         selectQuery.addCustomFromTable(BILLING_DATA_HOURLY_TABLE);
       }
@@ -408,7 +409,12 @@ public class BillingDataQueryBuilder {
         selectQuery.addCustomFromTable(schema.getBillingDataTable());
       }
     } else {
-      selectQuery.addCustomFromTable(BILLING_DATA_HOURLY_TABLE);
+      if (featureFlagService.isEnabled(CE_BILLING_DATA_HOURLY_PRE_AGGREGATION, accountId)
+          && isValidGroupByForPreAggregation(groupBy) && areFiltersValidForPreAggregation(filters)) {
+        selectQuery.addCustomFromTable(BILLING_DATA_HOURLY_PRE_AGGREGATED_TABLE);
+      } else {
+        selectQuery.addCustomFromTable(BILLING_DATA_HOURLY_TABLE);
+      }
     }
 
     if (isValidGroupByForFilterValues(groupBy)) {
