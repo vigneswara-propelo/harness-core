dae5ea6cc4a62c15260956ee9d5d9e92573a0a10
Mohit Garg <mohit.garg@harness.io>
2021-03-22 02:57:43 +0530

    [CDNG-5126]: fix error code (#21647)
    * [CDNG-5126]: new exceptions added for delegate exception framework
    
    * [CDNG-5126]: response messages updated for new exceptions
    
    * [CDNG-5126]: exception code name fix

--- 980-commons/src/main/java/io/harness/exception/DelegateErrorHandlerException.java
+++ 980-commons/src/main/java/io/harness/exception/DelegateErrorHandlerException.java
@@ -1,6 +1,6 @@
 package io.harness.exception;
 
-import static io.harness.eraro.ErrorCode.DELEGATE_ERROR_HANDLER_ERROR;
+import static io.harness.eraro.ErrorCode.DELEGATE_ERROR_HANDLER_EXCEPTION;
 
 import io.harness.eraro.Level;
 
@@ -8,12 +8,12 @@ public class DelegateErrorHandlerException extends WingsException {
   private static final String MESSAGE_ARG = "message";
 
   public DelegateErrorHandlerException(String message) {
-    super(message, null, DELEGATE_ERROR_HANDLER_ERROR, Level.ERROR, null, null);
+    super(message, null, DELEGATE_ERROR_HANDLER_EXCEPTION, Level.ERROR, null, null);
     super.param(MESSAGE_ARG, message);
   }
 
   public DelegateErrorHandlerException(String message, Throwable cause) {
-    super(message, cause, DELEGATE_ERROR_HANDLER_ERROR, Level.ERROR, null, null);
+    super(message, cause, DELEGATE_ERROR_HANDLER_EXCEPTION, Level.ERROR, null, null);
     super.param(MESSAGE_ARG, message);
   }
 }
