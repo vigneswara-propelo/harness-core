ca80fc978e08e5ff76a73db58e704613afb82654
George Georgiev <george@harness.io>
2021-03-25 17:31:43 -0700

    [BT-0]: fix the recency check (#22011)

--- scripts/jenkins/recency-check.sh
+++ scripts/jenkins/recency-check.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
-LAST_UNMERGED_SHA=`git rev-list --left-right origin/${ghprbTargetBranch}...HEAD | tail -n 1 | cut -c 2-`
+if [ -z "${ghprbTargetBranch}" ]
+then
+  ghprbTargetBranch=master
+fi
 
+BREAKING_COMMITS=`git log --pretty=oneline --no-merges HEAD..origin/${ghprbTargetBranch} | grep '\[PR_FIX\]\|\[REFACTORING\]'`
+
+if [ ! -z "${BREAKING_COMMITS}" ]
+then
+  echo There are breaking commits that you need to merge.
+  echo "${BREAKING_COMMITS}"
+  exit 1
+fi
+
+LAST_UNMERGED_SHA=`git log --format=format:%H --no-merges HEAD..origin/${ghprbTargetBranch} | tail -n 1`
 if [ -z "${LAST_UNMERGED_SHA}" ]
 then
   exit 0
 fi
+
+echo last unmerged commit
+git log -n 1 ${LAST_UNMERGED_SHA}
 
 UNMERGED_SINCE=`git show -s --format=%ct ${LAST_UNMERGED_SHA}`
 NOW=`date +'%s'`
 
 if [[ $UNMERGED_SINCE -lt $(($NOW - 86400)) ]]
 then
-  echo There are too old commit that are not merged to your branch.
+  echo It is too old, please merge your branch with ${ghprbTargetBranch}
   exit 1
 fi
