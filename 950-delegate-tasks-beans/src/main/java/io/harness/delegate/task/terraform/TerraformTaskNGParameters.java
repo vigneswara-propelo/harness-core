41a8ef3761351461fa16474ddf37dde4f8a84243
Satyam Shanker <satyam.shanker@harness.io>
2021-04-02 20:06:17 -0700

    [CDNG-5754]: Tf Delegate Task beans params (#22254)

--- 950-delegate-tasks-beans/src/main/java/io/harness/delegate/task/terraform/TerraformTaskNGParameters.java
+++ 950-delegate-tasks-beans/src/main/java/io/harness/delegate/task/terraform/TerraformTaskNGParameters.java
@@ -1,12 +1,56 @@
 package io.harness.delegate.task.terraform;
 
-import io.harness.delegate.task.TaskParameters;
+import static io.harness.annotations.dev.HarnessTeam.CDP;
+import static io.harness.expression.Expression.ALLOW_SECRETS;
+import static io.harness.expression.Expression.DISALLOW_SECRETS;
 
+import static java.util.Collections.emptyList;
+
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.delegate.beans.executioncapability.ExecutionCapability;
+import io.harness.delegate.beans.executioncapability.ExecutionCapabilityDemander;
+import io.harness.delegate.task.TaskParameters;
+import io.harness.delegate.task.git.GitFetchFilesConfig;
+import io.harness.expression.Expression;
+import io.harness.expression.ExpressionEvaluator;
+import io.harness.security.encryption.EncryptedRecordData;
+import io.harness.security.encryption.EncryptionConfig;
+
+import java.util.List;
+import java.util.Map;
 import lombok.Builder;
+import lombok.NonNull;
 import lombok.Value;
 
 @Value
 @Builder
-public class TerraformTaskNGParameters implements TaskParameters {
-  TFTaskType taskType;
+@OwnedBy(CDP)
+public class TerraformTaskNGParameters implements TaskParameters, ExecutionCapabilityDemander {
+  @NonNull String accountId;
+  String currentStateFileId;
+
+  @NonNull TFTaskType taskType;
+  @NonNull String provisionerIdentifier;
+  String workspace;
+  @NonNull GitFetchFilesConfig configFiles;
+  List<GitFetchFilesConfig> remoteVarfiles;
+  @Expression(ALLOW_SECRETS) List<String> inlineVarFiles;
+  @Expression(ALLOW_SECRETS) String backendConfig;
+  @Expression(DISALLOW_SECRETS) List<String> targets;
+  @Expression(ALLOW_SECRETS) Map<String, Object> environmentVariables;
+  boolean saveTerraformStateJson;
+
+  // For plan
+  // ToDo: Add Tf Plan Command from 950 delegate task beans after promotion
+
+  // For Apply when inheriting from plan
+  EncryptionConfig encryptionConfig;
+  EncryptedRecordData encryptedTfPlan;
+  String planName;
+
+  @Override
+  public List<ExecutionCapability> fetchRequiredExecutionCapabilities(ExpressionEvaluator maskingEvaluator) {
+    // ToDo: Implement me
+    return emptyList();
+  }
 }
