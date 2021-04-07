9c43729b459b270fe68a6bc795cb383af10485d9
Naman Verma <naman.verma@harness.io>
2021-04-07 15:06:21 +0530

    [CDNG-5878]: Add path param validation test (#22383)
    * [CDNG-5878]: Add path param validation test
    
    * [CDNG-5878]: Add team

--- 800-pipeline-service/src/test/java/io/harness/pms/core/ResourceFileAnnotationsTest.java
+++ 800-pipeline-service/src/test/java/io/harness/pms/core/ResourceFileAnnotationsTest.java
@@ -6,6 +6,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import io.harness.CategoryTest;
 import io.harness.PipelineServiceConfiguration;
+import io.harness.annotations.dev.HarnessTeam;
+import io.harness.annotations.dev.OwnedBy;
 import io.harness.category.element.UnitTests;
 import io.harness.rule.Owner;
 
@@ -13,18 +15,23 @@ import com.google.common.collect.Sets;
 import io.swagger.annotations.ApiOperation;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
-public class SwaggerApiOperationTest extends CategoryTest {
+@OwnedBy(HarnessTeam.PIPELINE)
+public class ResourceFileAnnotationsTest extends CategoryTest {
   @Test
   @Owner(developers = NAMAN)
   @Category(UnitTests.class)
@@ -47,6 +54,42 @@ public class SwaggerApiOperationTest extends CategoryTest {
           assertThat(uniqueOperationName.contains(apiOperationAnnotation.nickname())).isFalse();
           uniqueOperationName.add(apiOperationAnnotation.nickname());
         });
+      }
+    }
+  }
+
+  @Test
+  @Owner(developers = NAMAN)
+  @Category(UnitTests.class)
+  public void testPathParams() {
+    Collection<Class<?>> resourceClasses = PipelineServiceConfiguration.getResourceClasses();
+    for (Class<?> clazz : resourceClasses) {
+      for (final Method method : clazz.getDeclaredMethods()) {
+        List<String> pathParams = new ArrayList<>();
+        Annotation[][] parametersAnnotationsList = method.getParameterAnnotations();
+        for (Annotation[] annotations : parametersAnnotationsList) {
+          for (Annotation parameterAnnotation : annotations) {
+            if (parameterAnnotation.annotationType() == PathParam.class) {
+              pathParams.add(((PathParam) parameterAnnotation).value());
+            }
+          }
+        }
+        Path pathAnnotation = method.getAnnotation(Path.class);
+        List<String> foundPathParams = new ArrayList<>();
+        if (pathAnnotation == null) {
+          assertThat(pathParams.size()).isEqualTo(0);
+          continue;
+        }
+        String path = pathAnnotation.value();
+        String[] pathComponents = path.split("/");
+        for (String component : pathComponents) {
+          if (component.matches("\\{.*}")) {
+            String value = component.split("\\{")[1].split("}")[0];
+            assertThat(pathParams.contains(value)).isTrue();
+            foundPathParams.add(value);
+          }
+        }
+        pathParams.forEach(pathParam -> assertThat(foundPathParams.contains(pathParam)).isTrue());
       }
     }
   }
