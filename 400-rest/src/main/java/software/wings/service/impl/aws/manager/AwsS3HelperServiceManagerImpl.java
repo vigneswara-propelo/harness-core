2a68d2a37a19dbe3a77176ebf3963fb003c9e9ae
Deepak Puthraya <deepak.puthraya@harness.io>
2021-04-15 18:04:20 +0530

    [CDC-12384]: Update AWSS3helper to add wildcard scope (#22452)

--- 400-rest/src/main/java/software/wings/service/impl/aws/manager/AwsS3HelperServiceManagerImpl.java
+++ 400-rest/src/main/java/software/wings/service/impl/aws/manager/AwsS3HelperServiceManagerImpl.java
@@ -1,13 +1,15 @@
 package software.wings.service.impl.aws.manager;
 
+import static io.harness.annotations.dev.HarnessModule._410_CG_REST;
 import static io.harness.annotations.dev.HarnessTeam.CDP;
 import static io.harness.data.structure.EmptyPredicate.isNotEmpty;
 
-import static software.wings.beans.Application.GLOBAL_APP_ID;
+import static software.wings.service.impl.AssignDelegateServiceImpl.SCOPE_WILDCARD;
 
 import static java.util.Collections.singletonList;
 
 import io.harness.annotations.dev.OwnedBy;
+import io.harness.annotations.dev.TargetModule;
 import io.harness.beans.Cd1SetupFields;
 import io.harness.beans.DelegateTask;
 import io.harness.delegate.beans.DelegateResponseData;
@@ -34,6 +36,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Singleton
 @OwnedBy(CDP)
+@TargetModule(_410_CG_REST)
 public class AwsS3HelperServiceManagerImpl implements AwsS3HelperServiceManager {
   private static final long TIME_OUT_IN_MINUTES = 2;
   @Inject private DelegateService delegateService;
@@ -50,7 +53,7 @@ public class AwsS3HelperServiceManagerImpl implements AwsS3HelperServiceManager 
     DelegateTask delegateTask =
         DelegateTask.builder()
             .accountId(accountId)
-            .setupAbstraction(Cd1SetupFields.APP_ID_FIELD, GLOBAL_APP_ID)
+            .setupAbstraction(Cd1SetupFields.APP_ID_FIELD, SCOPE_WILDCARD)
             .tags(isNotEmpty(request.getAwsConfig().getTag()) ? singletonList(request.getAwsConfig().getTag()) : null)
             .data(TaskData.builder()
                       .async(false)
--- 400-rest/src/main/java/software/wings/service/intfc/aws/manager/AwsS3HelperServiceManager.java
+++ 400-rest/src/main/java/software/wings/service/intfc/aws/manager/AwsS3HelperServiceManager.java
@@ -1,8 +1,10 @@
 package software.wings.service.intfc.aws.manager;
 
+import static io.harness.annotations.dev.HarnessModule._410_CG_REST;
 import static io.harness.annotations.dev.HarnessTeam.CDP;
 
 import io.harness.annotations.dev.OwnedBy;
+import io.harness.annotations.dev.TargetModule;
 import io.harness.security.encryption.EncryptedDataDetail;
 
 import software.wings.beans.AwsConfig;
@@ -10,6 +12,7 @@ import software.wings.beans.AwsConfig;
 import java.util.List;
 
 @OwnedBy(CDP)
+@TargetModule(_410_CG_REST)
 public interface AwsS3HelperServiceManager {
   List<String> listBucketNames(AwsConfig awsConfig, List<EncryptedDataDetail> encryptionDetails);
 }
