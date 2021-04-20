84db184f7d4cfe60af0bfad21f5647dd61436d09
George Georgiev <george@harness.io>
2021-04-20 16:19:02 -0700

    [DEL-1769]: add needed indexes to progress update (#22795)
    * [DEL-1769]: add needed indexes to progress update

--- 950-wait-engine/src/main/java/io/harness/waiter/ProgressUpdate.java
+++ 950-wait-engine/src/main/java/io/harness/waiter/ProgressUpdate.java
@@ -3,14 +3,19 @@ package io.harness.waiter;
 import static java.time.Duration.ofDays;
 
 import io.harness.annotation.HarnessEntity;
+import io.harness.mongo.index.FdIndex;
 import io.harness.mongo.index.FdTtlIndex;
+import io.harness.mongo.index.MongoIndex;
+import io.harness.mongo.index.SortCompoundMongoIndex;
 import io.harness.persistence.CreatedAtAccess;
 import io.harness.persistence.PersistentEntity;
 import io.harness.persistence.UuidAccess;
 
+import com.google.common.collect.ImmutableList;
 import java.time.Duration;
 import java.time.OffsetDateTime;
 import java.util.Date;
+import java.util.List;
 import lombok.Builder;
 import lombok.Value;
 import lombok.experimental.FieldNameConstants;
@@ -26,11 +31,21 @@ import org.mongodb.morphia.annotations.Id;
 @Entity(value = "progressUpdate", noClassnameStored = true)
 @HarnessEntity(exportable = false)
 public class ProgressUpdate implements PersistentEntity, UuidAccess, CreatedAtAccess {
+  public static List<MongoIndex> mongoIndexes() {
+    return ImmutableList.<MongoIndex>builder()
+        .add(SortCompoundMongoIndex.builder()
+                 .name("correlation")
+                 .field(ProgressUpdateKeys.correlationId)
+                 .descSortField(ProgressUpdateKeys.createdAt)
+                 .build())
+        .build();
+  }
+
   public static final Duration TTL = ofDays(21);
 
   @Id private String uuid;
   private String correlationId;
-  private long createdAt;
+  @FdIndex private long createdAt;
   private byte[] progressData;
   private long expireProcessing;
 
