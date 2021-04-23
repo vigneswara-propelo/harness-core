0c150e56e7a9076e693ab317ab6d90b4be38d24e
Mohit Garg <mohit.garg@harness.io>
2021-04-23 18:14:56 +0530

    [CDNG-5126]: made DataException abstract and added comments (#22919)
    * [CDNG-5126]: new exceptions added for delegate exception framework
    
    * [CDNG-5126]: response messages updated for new exceptions
    
    * [CDNG-5126]: exception code name fix
    
    * [CDNG-5126]: added new exception constructors with error messages
    
    * [CDNG-5126]: added new exception builder to construct nested exceptions
    
    * [CDNG-5126]: added new exception
    
    * [CDNG-5126]: exception name change
    
    * [CDNG-5126]: exception registered to kryo
    
    * [CDNG-5126]: added response message for new exception
    
    * [CDNG-5126]: added owner details to exception
    
    * [CDNG-5126]: added owner details to classes
    
    * [CDNG-5126]: add new DataException for handling metadata for tasks
    
    * [CDNG-5126]: add new DataException for handling metadata for tasks
    
    * [CDNG-5126]: add new DataException for handling metadata for tasks
    
    * [CDNG-5126]: added response message for new data exception
    
    * [CDNG-5126]: made DataException abstract and added comments

--- 980-commons/src/main/java/io/harness/exception/DataException.java
+++ 980-commons/src/main/java/io/harness/exception/DataException.java
@@ -3,7 +3,11 @@ package io.harness.exception;
 import static io.harness.eraro.ErrorCode.DATA;
 import static io.harness.eraro.Level.INFO;
 
-public class DataException extends WingsException {
+/**
+ * This exception serves as super class for all exceptions to be used to store metadata for error
+ * handling framework
+ */
+public abstract class DataException extends WingsException {
   public DataException(Throwable cause) {
     super(null, cause, DATA, INFO, null, null);
   }
