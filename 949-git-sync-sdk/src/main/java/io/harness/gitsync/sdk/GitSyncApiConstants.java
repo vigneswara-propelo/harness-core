65d1067bef9a401b0abbb22fc730f075c7d98ebe
Deepak Patankar <deepak.patankar@harness.io>
2021-04-26 19:03:41 +0530

    [CDNG-7319]: Change folder path name (#22987)

--- 949-git-sync-sdk/src/main/java/io/harness/gitsync/sdk/GitSyncApiConstants.java
+++ 949-git-sync-sdk/src/main/java/io/harness/gitsync/sdk/GitSyncApiConstants.java
@@ -12,7 +12,7 @@ public class GitSyncApiConstants {
   public static final String COMMIT_MSG_KEY = "commitMsg";
   public static final String CREATE_PR_KEY = "createPr";
   public static final String LAST_OBJECT_ID_KEY = "lastObjectId";
-  public static final String FOLDER_PATH = "folderPath";
+  public static final String FOLDER_PATH = "rootFolder";
   public static final String NEW_BRANCH = "isNewBranch";
   public static final String TARGET_BRANCH_FOR_PR = "targetBranchForPr";
 }
