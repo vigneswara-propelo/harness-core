87372d11499cdb6b41e4a4e5247ace9846193076
Udham Singh <udham.singh@harness.io>
2021-04-27 15:23:07 +0530

    [BT-336]: Adding script to generate coverage report locally (#23024)
    * [BT-336]: Adding script to generate coverage report locally.
    
    * [BT-336]: Adding exit to helper function
    
    * [BT-336]: Corrections in helper function

---
+++ scripts/bazel/generate_coverage.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env zsh
+
+#Helper Function
+function help(){
+  echo -e "USAGE: generate_coverage.sh <Module Name> \n\
+  Example generate_coverage.sh 990-commons-test \n\
+  NOTE: Provide Only Module Name without Slashes and Dots"
+  exit 0
+}
+
+#Function to check if package is installed or not
+#args: $1: Name of the Package
+function check_package_installed() {
+  LOCAL_PACKAGE_NAME=$1
+  echo "Checking if $LOCAL_PACKAGE_NAME is installed or not..."
+  brew list $LOCAL_PACKAGE_NAME
+  if [ "$?" -eq 1 ];then
+    echo "Installing $LOCAL_PACKAGE_NAME package..."
+    brew install $LOCAL_PACKAGE_NAME
+  fi
+}
+
+#Function to remove and create empty directory.
+#args: $1: Path of the Directory
+function clear_dir(){
+  LOCAL_DIR_NAME=$1
+  rm -rf ${LOCAL_DIR_NAME}
+  mkdir -p ${LOCAL_DIR_NAME}
+}
+
+MODULE_NAME=$1
+MODULE_COVERAGE_DIR="$HOME/combined_coverage_report/${MODULE_NAME}"
+
+#Main Code Starts here
+
+if [ "$1" = '--help' ];then
+  help
+fi
+
+check_package_installed lcov
+clear_dir ${MODULE_COVERAGE_DIR}
+
+echo "Bazel Command: bazel coverage -s -k --java_toolchain=@bazel_tools//tools/jdk:toolchain_hostjdk8 --collect_code_coverage --combined_report=lcov --coverage_report_generator=@bazel_tools//tools/test:coverage_report_generator //${MODULE_NAME}/..."
+bazel coverage -s -k --java_toolchain=@bazel_tools//tools/jdk:toolchain_hostjdk8 \
+--collect_code_coverage --combined_report=lcov \
+--coverage_report_generator=@bazel_tools//tools/test:coverage_report_generator \
+//${MODULE_NAME}/...
+
+echo "Generating HTML Coverage Report..."
+genhtml bazel-out/_coverage/_coverage_report.dat -o ${MODULE_COVERAGE_DIR}
+if [ "$?" -eq 0 ];then
+  echo -e "Combined HTML Report Generated: ${MODULE_COVERAGE_DIR}/index.html \n \
+          Run: \"open ${MODULE_COVERAGE_DIR}/index.html\" to see report in browser."
+else
+  echo "There is an issue in generating combined coverage report"
+  exit 1
+fi
