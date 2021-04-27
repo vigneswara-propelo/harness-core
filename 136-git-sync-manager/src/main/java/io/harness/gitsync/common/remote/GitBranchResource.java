3b687c832924a182f650912e8000994da8e842d2
Deepak Patankar <deepak.patankar@harness.io>
2021-04-27 19:25:54 +0530

    [CDNG-7319]: Add the api response (#23042)

--- 136-git-sync-manager/src/main/java/io/harness/gitsync/common/remote/GitBranchResource.java
+++ 136-git-sync-manager/src/main/java/io/harness/gitsync/common/remote/GitBranchResource.java
@@ -12,12 +12,17 @@ import io.harness.ng.beans.PageRequest;
 import io.harness.ng.beans.PageResponse;
 import io.harness.ng.core.OrgIdentifier;
 import io.harness.ng.core.ProjectIdentifier;
+import io.harness.ng.core.dto.ErrorDTO;
+import io.harness.ng.core.dto.FailureDTO;
 import io.harness.ng.core.dto.ResponseDTO;
 import io.harness.ng.core.utils.URLDecoderUtility;
+import io.harness.security.annotations.NextGenManagerAuth;
 
 import com.google.inject.Inject;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DefaultValue;
@@ -34,6 +39,12 @@ import org.hibernate.validator.constraints.NotEmpty;
 @Path("/git-sync-branch")
 @Produces({"application/json", "text/yaml", "text/html"})
 @Consumes({"application/json", "text/yaml", "text/html"})
+@ApiResponses(value =
+    {
+      @ApiResponse(code = 400, response = FailureDTO.class, message = "Bad Request")
+      , @ApiResponse(code = 500, response = ErrorDTO.class, message = "Internal server error")
+    })
+@NextGenManagerAuth
 @AllArgsConstructor(onConstructor = @__({ @Inject }))
 @OwnedBy(DX)
 public class GitBranchResource {
