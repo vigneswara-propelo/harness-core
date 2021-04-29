cba6733e69c811c6fcfe62f710f89866bc779615
George Georgiev <george@harness.io>
2021-04-29 15:06:03 -0700

    [BT-322]: fix the remote cache config to upload to local disk (#23145)

--- scripts/bazel/generate_credentials.sh
+++ scripts/bazel/generate_credentials.sh
@@ -17,6 +17,7 @@ cat <<EOT > bazelrc.cache
 build --remote_cache=${REMOTE_CACHE}
 build --google_credentials=${GOOGLE_CREDENTIALS_FILE}
 build --remote_upload_local_results=false
+build --incompatible_remote_results_ignore_disk=true
 build --experimental_guard_against_concurrent_changes
 EOT
 
