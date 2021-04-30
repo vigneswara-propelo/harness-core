3a1a4e9d026ff6468576aaf4948a711ff60a59ed
Rushabh Shah <rushabh.shah@harness.io>
2021-04-30 15:20:43 -0700

    [FFM-800]: change distributedLockImplementation for batchProcessing to NOOP (#23069)
    * [FFM-800]: change distributedLockImplementation for batchProcessing to NOOP
    
    * [FFM-800]: remove unwanted dependency
    
    * [FFM-800]: change the mechanism for binding for persistentLocker
    
    * [FFM-800]: change the mechanism for binding for persistentLocker

--- 280-batch-processing/src/main/java/io/harness/batch/processing/config/BatchProcessingModule.java
+++ 280-batch-processing/src/main/java/io/harness/batch/processing/config/BatchProcessingModule.java
@@ -1,7 +1,5 @@
 package io.harness.batch.processing.config;
 
-import static io.harness.lock.DistributedLockImplementation.MONGO;
-
 import io.harness.batch.processing.metrics.CeCloudMetricsService;
 import io.harness.batch.processing.metrics.CeCloudMetricsServiceImpl;
 import io.harness.batch.processing.metrics.ProductMetricsService;
@@ -18,11 +16,12 @@ import io.harness.ccm.views.service.ViewsBillingService;
 import io.harness.ccm.views.service.impl.CEViewServiceImpl;
 import io.harness.ccm.views.service.impl.ViewCustomFieldServiceImpl;
 import io.harness.ccm.views.service.impl.ViewsBillingServiceImpl;
-import io.harness.ff.FeatureFlagModule;
-import io.harness.lock.DistributedLockImplementation;
+import io.harness.ff.FeatureFlagService;
+import io.harness.ff.FeatureFlagServiceImpl;
+import io.harness.lock.PersistentLocker;
+import io.harness.lock.noop.PersistentNoopLocker;
 import io.harness.mongo.MongoConfig;
 import io.harness.persistence.HPersistence;
-import io.harness.redis.RedisConfig;
 
 import software.wings.dl.WingsMongoPersistence;
 import software.wings.dl.WingsPersistence;
@@ -39,8 +38,8 @@ import com.google.common.util.concurrent.SimpleTimeLimiter;
 import com.google.common.util.concurrent.TimeLimiter;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
+import com.google.inject.Scopes;
 import com.google.inject.Singleton;
-import com.google.inject.name.Named;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -61,7 +60,14 @@ public class BatchProcessingModule extends AbstractModule {
     bind(ViewCustomFieldService.class).to(ViewCustomFieldServiceImpl.class);
     bind(CeAccountExpirationChecker.class).to(CeAccountExpirationCheckerImpl.class);
     bind(AnomalyService.class).to(AnomalyServiceImpl.class);
-    install(FeatureFlagModule.getInstance());
+    /**
+     * This dependency only exists for the CFMigrationService which BatchProcessing will never use. However,
+     * since it is sharing the same module, we have to provide an implementation for the same. Hence we are using
+     * NOOP over here
+     * @return
+     */
+    bind(PersistentLocker.class).to(PersistentNoopLocker.class).in(Scopes.SINGLETON);
+    bind(FeatureFlagService.class).to(FeatureFlagServiceImpl.class);
     bind(TimeLimiter.class).toInstance(new SimpleTimeLimiter());
   }
 
@@ -69,18 +75,5 @@ public class BatchProcessingModule extends AbstractModule {
   @Singleton
   MongoConfig mongoConfig(BatchMainConfig batchMainConfig) {
     return batchMainConfig.getHarnessMongo();
-  }
-
-  @Provides
-  @Singleton
-  DistributedLockImplementation distributedLockImplementation() {
-    return MONGO;
-  }
-
-  @Provides
-  @Named("lock")
-  @Singleton
-  RedisConfig redisConfig() {
-    return RedisConfig.builder().build();
   }
 }
