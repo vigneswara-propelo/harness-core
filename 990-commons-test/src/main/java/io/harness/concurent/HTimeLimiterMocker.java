cfeb2f4844b2e6c7c6d477f44e8cdc76755831cf
George Georgiev <george@harness.io>
2021-04-30 13:29:49 -0700

    [BT-340]: fix code coverage builds (#23173)
    * [BT-340]: fix code coverage builds

--- 990-commons-test/src/main/java/io/harness/concurent/HTimeLimiterMocker.java
+++ 990-commons-test/src/main/java/io/harness/concurent/HTimeLimiterMocker.java
@@ -14,12 +14,12 @@ import org.mockito.stubbing.OngoingStubbing;
 @UtilityClass
 public class HTimeLimiterMocker {
   public <T> OngoingStubbing<T> mockCallInterruptible(TimeLimiter timeLimiter, Duration duration) throws Exception {
-    return when(
+    return (OngoingStubbing<T>) when(
         timeLimiter.callWithTimeout(any(Callable.class), eq(duration.toMillis()), eq(TimeUnit.MILLISECONDS), eq(true)));
   }
 
   public <T> OngoingStubbing<T> mockCallUninterruptible(TimeLimiter timeLimiter, Duration duration) throws Exception {
-    return when(timeLimiter.callWithTimeout(
+    return (OngoingStubbing<T>) when(timeLimiter.callWithTimeout(
         any(Callable.class), eq(duration.toMillis()), eq(TimeUnit.MILLISECONDS), eq(false)));
   }
 }
