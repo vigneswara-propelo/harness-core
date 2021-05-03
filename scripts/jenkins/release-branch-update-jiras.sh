73015c70b8d9593833f720b9358a43067c673e36
Boopesh Shanmugam <boopesh.shanmugam@harness.io>
2021-05-03 10:27:06 +0530

    [BT-353]: Tracking Dummy Commits with JIRA. (#23161)

--- scripts/jenkins/release-branch-update-jiras.sh
+++ scripts/jenkins/release-branch-update-jiras.sh
@@ -1,7 +1,49 @@
-KEYS=`git log --pretty=oneline --abbrev-commit |\
+#!/bin/bash
+PROJECTS="BT|CCE|CCM|CDC|CDNG|CDP|CE|CI|CV|CVNG|DEL|DOC|DX|ER|FFM|OPS|PL|SEC|SWAT|GTM|ONP"
+KEYS=$(git log --pretty=oneline --abbrev-commit |\
       awk "/${PREVIOUS_CUT_COMMIT_MESSAGE}/ {exit} {print}" |\
-      grep -o -iE '(BT|CCE|CCM|CDC|CDNG|CDP|CE|CI|CV|CVNG|DEL|DOC|DX|ER|FFM|OPS|PL|SEC|SWAT|GTM|ONP)-[0-9]+' | sort | uniq`
+      grep -o -iE '('$PROJECTS')-[0-9]+' | sort | uniq)
+#Getting the dummy commits that are made to the branch and creating a ticket out of it
+git log --pretty=oneline --abbrev-commit |\
+      awk "/${PREVIOUS_CUT_COMMIT_MESSAGE}/ {exit} {print}" |\
+      grep -iE "\[(${PROJECTS})-0]:.*" -o | sort | uniq  | tr '\n' ',' > dummyJiraList.txt
+dummyJiraList=$(sed 's/,/\\n/g' dummyJiraList.txt)
 
+#Creating a ticket for such tickets
+if [ -s dummyJiraList.txt ]
+then
+  response=$(curl -X POST \
+  https://harness.atlassian.net/rest/api/2/issue/ \
+  --user $JIRA_USERNAME:$JIRA_PASSWORD \
+  -H 'content-type: application/json' \
+  -d '{
+    "fields": {
+       "project":
+       {
+          "key": "ART"
+       },
+       "summary": "Contains the details of untracked changes going all the release build:'$VERSION'00",
+       "description": "'{code}"$dummyJiraList"{code}'",
+       "issuetype": {
+          "name": "Story"
+       }
+   }
+}')
+  ticketId=$(echo $response | grep -o -iE '(ART)-[0-9]+')
+else
+  echo "No Dummy Commits"
+fi
+
+
+#Assigning the Release BE Number to this ticket
+if [ -z "$ticketId" ]
+then
+  echo "No Ticket is Created as there are no dummy commits"
+else
+  KEYS="${KEYS} ${ticketId}"
+fi
+
+#Updating all the Jira tickets with the Release Build Number
 if [ "${PURPOSE}" = "saas" ]
 then
     FIELD_ID="customfield_10644"
@@ -23,3 +65,4 @@ do
        https://harness.atlassian.net/rest/api/2/issue/${KEY} \
        --user $JIRA_USERNAME:$JIRA_PASSWORD
 done
+
