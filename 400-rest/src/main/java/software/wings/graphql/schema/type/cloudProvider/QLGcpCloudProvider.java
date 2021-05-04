ede2ba6434a0a017d7ec3be546a7a6388a1106bb
Sri Pardha Chidella <sripardha.chidella@harness.io>
2021-05-04 14:10:22 +0530

    [CDP-17218]: GCP graphQL changes (#22784)
    * [CDP-16771]: gcp graphql changes
    
    * [CDP-17218]: changes
    
    * [CDP-17218]: aeriform check

--- 400-rest/src/main/java/software/wings/graphql/schema/type/cloudProvider/QLGcpCloudProvider.java
+++ 400-rest/src/main/java/software/wings/graphql/schema/type/cloudProvider/QLGcpCloudProvider.java
@@ -1,6 +1,9 @@
 package software.wings.graphql.schema.type.cloudProvider;
 
+import static io.harness.annotations.dev.HarnessTeam.CDP;
+
 import io.harness.annotations.dev.HarnessModule;
+import io.harness.annotations.dev.OwnedBy;
 import io.harness.annotations.dev.TargetModule;
 import io.harness.ccm.health.CEHealthStatus;
 
@@ -8,6 +11,7 @@ import software.wings.graphql.schema.type.QLUser;
 import software.wings.security.PermissionAttribute.ResourceType;
 import software.wings.security.annotations.Scope;
 
+import java.util.List;
 import lombok.Builder;
 import lombok.Value;
 import lombok.experimental.FieldNameConstants;
@@ -17,6 +21,7 @@ import lombok.experimental.FieldNameConstants;
 @FieldNameConstants(innerTypeName = "QLGcpConfigKeys")
 @Scope(ResourceType.SETTING)
 @TargetModule(HarnessModule._380_CG_GRAPHQL)
+@OwnedBy(CDP)
 public class QLGcpCloudProvider implements QLCloudProvider {
   private String id;
   private String name;
@@ -26,6 +31,7 @@ public class QLGcpCloudProvider implements QLCloudProvider {
   private String type;
   private boolean isContinuousEfficiencyEnabled;
   private CEHealthStatus ceHealthStatus;
+  private List<String> delegateSelectors;
 
   public static class QLGcpCloudProviderBuilder implements QLCloudProviderBuilder {}
 }
