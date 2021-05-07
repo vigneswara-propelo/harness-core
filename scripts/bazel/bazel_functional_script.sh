81e433e499c7628a149e5a082ea42e4f96b3283b
Abhinav Singh <abhinav.singh@harness.io>
2021-05-07 10:21:35 +0530

    [BT-359]: Update bazel_functional_script (#23393)
    * Update bazel_functional_script.sh

--- scripts/bazel/bazel_functional_script.sh
+++ scripts/bazel/bazel_functional_script.sh
@@ -36,8 +36,7 @@ if [ "${RUN_BAZEL_FUNCTIONAL_TESTS}" == "true" ]; then
   MANAGER_PID=$!
   java -Xbootclasspath/p:/root/.m2/repository/org/mortbay/jetty/alpn/alpn-boot/8.1.13.v20181017/alpn-boot-8.1.13.v20181017.jar -Xmx4096m -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:mygclogfilename.gc -XX:+UseParallelGC -XX:MaxGCPauseMillis=500 -jar /home/jenkins/.bazel-dirs/bin/260-delegate/module_deploy.jar /home/jenkins/workspace/pr-portal-funtional-tests/260-delegate/config-delegate.yml &
   DELEGATE_PID=$!
-  sleep 300
-  bazel ${bazelrc} test --keep_going ${GCP} ${BAZEL_ARGUMENTS} --jobs=3 -- //200-functional-test/... //190-deployment-functional-tests:software.wings.functional.terraform.TerraformFunctionalTest //190-deployment-functional-tests:software.wings.functional.customDeployment.CustomDeploymentFunctionalTest || true
+  bazel ${bazelrc} test --keep_going ${GCP} ${BAZEL_ARGUMENTS} --jobs=3 -- //200-functional-test/... || true
   kill -9 $MANAGER_PID
   kill -9 $DELEGATE_PID
 fi
