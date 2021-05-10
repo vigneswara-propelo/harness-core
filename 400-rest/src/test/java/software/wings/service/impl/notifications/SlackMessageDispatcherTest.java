52ba889d9c11cf48f16d37a96b553941dc143559
Prabu Rajendran <prabu.rajendran@harness.io>
2021-05-10 16:05:04 +0530

    [CDC-13182]: adding username and icon for approval required messages (#23237)
    * [CDC-13182]: adding username and icon for approval required messages
    
    * [CDC-13182]: removed wrong import

--- 400-rest/src/test/java/software/wings/service/impl/notifications/SlackMessageDispatcherTest.java
+++ 400-rest/src/test/java/software/wings/service/impl/notifications/SlackMessageDispatcherTest.java
@@ -1,9 +1,11 @@
 package software.wings.service.impl.notifications;
 
+import static io.harness.rule.OwnerRule.PRABU;
 import static io.harness.rule.OwnerRule.UJJAWAL;
 
 import static software.wings.beans.EntityType.ORCHESTRATED_DEPLOYMENT;
 import static software.wings.common.Constants.HARNESS_NAME;
+import static software.wings.common.NotificationMessageResolver.NotificationMessageType.APPROVAL_NEEDED_NOTIFICATION;
 import static software.wings.common.NotificationMessageResolver.NotificationMessageType.ENTITY_CREATE_NOTIFICATION;
 import static software.wings.utils.WingsTestConstants.ACCOUNT_ID;
 import static software.wings.utils.WingsTestConstants.APP_ID;
@@ -11,9 +13,13 @@ import static software.wings.utils.WingsTestConstants.ENV_NAME;
 import static software.wings.utils.WingsTestConstants.WORKFLOW_EXECUTION_ID;
 import static software.wings.utils.WingsTestConstants.WORKFLOW_NAME;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.anyList;
 import static org.mockito.Mockito.when;
 
+import io.harness.beans.FeatureName;
 import io.harness.category.element.UnitTests;
+import io.harness.ff.FeatureFlagService;
 import io.harness.rule.Owner;
 
 import software.wings.WingsBaseTest;
@@ -29,6 +35,7 @@ import java.util.Collections;
 import java.util.List;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -37,6 +44,7 @@ public class SlackMessageDispatcherTest extends WingsBaseTest {
   @Inject @InjectMocks private SlackMessageDispatcher slackMessageDispatcher;
   @Mock private SlackNotificationService slackNotificationService;
   @Mock private NotificationMessageResolver notificationMessageResolver;
+  @Mock private FeatureFlagService featureFlagService;
 
   @Test
   @Owner(developers = UJJAWAL)
@@ -62,5 +70,65 @@ public class SlackMessageDispatcherTest extends WingsBaseTest {
     Mockito.verify(slackNotificationService)
         .sendMessage(Mockito.eq(setting), Mockito.anyString(), Mockito.eq(HARNESS_NAME), Mockito.anyString(),
             Mockito.anyString());
+  }
+
+  @Test
+  @Owner(developers = PRABU)
+  @Category(UnitTests.class)
+  public void shouldSendApprovalRequiredMessageWithIconOverride() {
+    when(featureFlagService.isEnabled(FeatureName.SLACK_APPROVALS, ACCOUNT_ID)).thenReturn(false);
+    when(featureFlagService.isEnabled(FeatureName.SEND_SLACK_NOTIFICATION_FROM_DELEGATE, ACCOUNT_ID)).thenReturn(false);
+    InformationNotification notification =
+        InformationNotification.builder()
+            .accountId(ACCOUNT_ID)
+            .appId(APP_ID)
+            .entityId(WORKFLOW_EXECUTION_ID)
+            .entityType(ORCHESTRATED_DEPLOYMENT)
+            .notificationTemplateId(APPROVAL_NEEDED_NOTIFICATION.name())
+            .notificationTemplateVariables(ImmutableMap.of("WORKFLOW_NAME", WORKFLOW_NAME, "ENV_NAME", ENV_NAME, "DATE",
+                "DATE", SlackApprovalMessageKeys.MESSAGE_IDENTIFIER, "suppressTraditionalNotificationOnSlack"))
+            .build();
+
+    List<Notification> notifications = Collections.singletonList(notification);
+
+    SlackNotificationSetting setting = SlackNotificationSetting.emptyConfig();
+
+    slackMessageDispatcher.dispatch(notifications, setting);
+    ArgumentCaptor<String> stringArgumentCaptor = ArgumentCaptor.forClass(String.class);
+    Mockito.verify(slackNotificationService).sendJSONMessage(stringArgumentCaptor.capture(), anyList());
+    assertThat(stringArgumentCaptor.getValue())
+        .isNotEmpty()
+        .contains("\"username\" : \"Harness\"")
+        .contains("\"icon_url\" : \"https://s3.amazonaws.com/wings-assets/slackicons/logo-slack.png\"");
+  }
+
+  @Test
+  @Owner(developers = PRABU)
+  @Category(UnitTests.class)
+  public void shouldSendSlackApprovalMessageWithIconOverride() {
+    when(featureFlagService.isEnabled(FeatureName.SLACK_APPROVALS, ACCOUNT_ID)).thenReturn(true);
+    when(featureFlagService.isEnabled(FeatureName.SEND_SLACK_NOTIFICATION_FROM_DELEGATE, ACCOUNT_ID)).thenReturn(false);
+    InformationNotification notification =
+        InformationNotification.builder()
+            .accountId(ACCOUNT_ID)
+            .appId(APP_ID)
+            .entityId(WORKFLOW_EXECUTION_ID)
+            .entityType(ORCHESTRATED_DEPLOYMENT)
+            .notificationTemplateId(APPROVAL_NEEDED_NOTIFICATION.name())
+            .notificationTemplateVariables(ImmutableMap.of("WORKFLOW_NAME", WORKFLOW_NAME, "ENV_NAME", ENV_NAME, "DATE",
+                "DATE", SlackApprovalMessageKeys.MESSAGE_IDENTIFIER, "suppressTraditionalNotificationOnSlack"))
+            .build();
+
+    List<Notification> notifications = Collections.singletonList(notification);
+
+    SlackNotificationSetting setting = SlackNotificationSetting.emptyConfig();
+
+    slackMessageDispatcher.dispatch(notifications, setting);
+    ArgumentCaptor<String> stringArgumentCaptor = ArgumentCaptor.forClass(String.class);
+    Mockito.verify(slackNotificationService).sendJSONMessage(stringArgumentCaptor.capture(), anyList());
+    assertThat(stringArgumentCaptor.getValue())
+        .isNotEmpty()
+        .contains("\"username\" : \"Harness\"")
+        .contains("\"icon_url\" : \"https://s3.amazonaws.com/wings-assets/slackicons/logo-slack.png\"");
   }
 }
