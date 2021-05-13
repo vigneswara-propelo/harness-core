8db0be7b288afe7bd01cc2a05d37ad8e5caeec02
Udham Singh <udham.singh@harness.io>
2021-05-13 19:20:51 +0530

    Bt 31 (#23609)
    * [BT-31]: Commenting out 340-ce-nextgen from sonar and adding test flags
    
    * [BT-31]: Rename 935-access-control-decision to 946-access-control-decision
    
    * [BT-31]: Commenting out coverage flags
    
    * [BT-31]: Adding new modules for Sonar Coverage
    
    * [BT-31]: Commenting Modules 220-graphql-test and 190-deployment-functional-tests because of no module target
    
    * [BT-367]: Handling correct exit status for Bazel tests

--- scripts/bazel/bazel_script.sh
+++ scripts/bazel/bazel_script.sh
@@ -28,7 +28,7 @@ fi
 if [ "${RUN_BAZEL_TESTS}" == "true" ]; then
   bazel ${bazelrc} build ${BAZEL_ARGUMENTS} -- //... -//product/... -//commons/...
   bazel ${bazelrc} test --cache_test_results=${CACHE_TEST_RESULTS} --define=HARNESS_ARGS=${HARNESS_ARGS} --keep_going ${BAZEL_ARGUMENTS} -- //... -//product/... -//commons/... -//200-functional-test/... -//190-deployment-functional-tests/...
-  exit 0
+  exit $?
 fi
 
 if [ "${RUN_CHECKS}" == "true" ]; then
