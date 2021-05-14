b76a99955135843a650f91660a48372bc42bf439
Harsh Jain <harsh.jain@harness.io>
2021-05-14 13:04:53 +0530

    [CI-1601]: add retry in clean up (#23617)
    * [CI-1601]: add retry in clean up
    
    * [CI-1601]: add retry in clean up

--- 320-ci-execution/src/main/java/ci/pipeline/execution/PipelineExecutionUpdateEventHandler.java
+++ 320-ci-execution/src/main/java/ci/pipeline/execution/PipelineExecutionUpdateEventHandler.java
@@ -2,19 +2,20 @@ package ci.pipeline.execution;
 
 import static io.harness.pms.execution.utils.StatusUtils.isFinalStatus;
 
+import static java.lang.String.format;
+
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.beans.DelegateTaskRequest;
+import io.harness.delegate.beans.ci.CIK8CleanupTaskParams;
 import io.harness.engine.executions.node.NodeExecutionService;
 import io.harness.execution.NodeExecution;
 import io.harness.execution.NodeExecutionMapper;
 import io.harness.ngpipeline.common.AmbianceHelper;
-import io.harness.plancreator.steps.common.StageElementParameters;
 import io.harness.pms.contracts.ambiance.Ambiance;
 import io.harness.pms.contracts.execution.NodeExecutionProto;
 import io.harness.pms.sdk.core.events.AsyncOrchestrationEventHandler;
 import io.harness.pms.sdk.core.events.OrchestrationEvent;
-import io.harness.pms.serializer.recaster.RecastOrchestrationUtils;
 import io.harness.service.DelegateGrpcClientWrapper;
 import io.harness.steps.StepOutcomeGroup;
 
@@ -22,6 +23,8 @@ import com.google.inject.Inject;
 import java.time.Duration;
 import java.util.Objects;
 import lombok.extern.slf4j.Slf4j;
+import net.jodah.failsafe.Failsafe;
+import net.jodah.failsafe.RetryPolicy;
 
 @Slf4j
 @OwnedBy(HarnessTeam.CI)
@@ -29,6 +32,9 @@ public class PipelineExecutionUpdateEventHandler implements AsyncOrchestrationEv
   @Inject private NodeExecutionService nodeExecutionService;
   @Inject private GitBuildStatusUtility gitBuildStatusUtility;
   @Inject private PodCleanupUtility podCleanupUtility;
+
+  private final Duration RETRY_SLEEP_DURATION = Duration.ofSeconds(2);
+  private final int MAX_ATTEMPTS = 3;
 
   @Inject private DelegateGrpcClientWrapper delegateGrpcClientWrapper;
   @Override
@@ -49,29 +55,43 @@ public class PipelineExecutionUpdateEventHandler implements AsyncOrchestrationEv
     }
 
     try {
-      if (Objects.equals(nodeExecution.getNode().getGroup(), StepOutcomeGroup.STAGE.name())
-          && isFinalStatus(nodeExecution.getStatus())) {
-        StageElementParameters integrationStageStepParameters = RecastOrchestrationUtils.fromDocument(
-            nodeExecution.getResolvedStepParameters(), StageElementParameters.class);
+      RetryPolicy<Object> retryPolicy = getRetryPolicy(format("[Retrying failed call to clean pod attempt: {}"),
+          format("Failed to clean pod after retrying {} times"));
 
-        log.info("Received event with status {} to clean stage {}, planExecutionId {}", nodeExecution.getStatus(),
-            integrationStageStepParameters.getIdentifier(), ambiance.getPlanExecutionId());
-        DelegateTaskRequest delegateTaskRequest =
-            DelegateTaskRequest.builder()
-                .accountId(accountId)
-                .taskSetupAbstractions(ambiance.getSetupAbstractions())
-                .executionTimeout(java.time.Duration.ofSeconds(60))
-                .taskType("CI_CLEANUP")
-                .taskParameters(podCleanupUtility.buildAndfetchCleanUpParameters(ambiance))
-                .taskDescription("CI cleanup pod task")
-                .build();
+      Failsafe.with(retryPolicy).run(() -> {
+        if (Objects.equals(nodeExecution.getNode().getGroup(), StepOutcomeGroup.STAGE.name())
+            && isFinalStatus(nodeExecution.getStatus())) {
+          CIK8CleanupTaskParams cik8CleanupTaskParams = podCleanupUtility.buildAndfetchCleanUpParameters(ambiance);
 
-        String taskId = delegateGrpcClientWrapper.submitAsyncTask(delegateTaskRequest, Duration.ZERO);
-        log.info("Submitted cleanup request  with taskId {} for Integration stage  {}, planExecutionId {}", taskId,
-            integrationStageStepParameters.getIdentifier(), ambiance.getPlanExecutionId());
-      }
+          log.info("Received event with status {} to clean podName {}, planExecutionId {}, stage {}",
+              nodeExecution.getStatus(), cik8CleanupTaskParams.getPodNameList(), ambiance.getPlanExecutionId(),
+              nodeExecution.getNode().getIdentifier());
+
+          DelegateTaskRequest delegateTaskRequest = DelegateTaskRequest.builder()
+                                                        .accountId(accountId)
+                                                        .taskSetupAbstractions(ambiance.getSetupAbstractions())
+                                                        .executionTimeout(java.time.Duration.ofSeconds(120))
+                                                        .taskType("CI_CLEANUP")
+                                                        .taskParameters(cik8CleanupTaskParams)
+                                                        .taskDescription("CI cleanup pod task")
+                                                        .build();
+          String taskId = delegateGrpcClientWrapper.submitAsyncTask(delegateTaskRequest, Duration.ZERO);
+          log.info("Submitted cleanup request with taskId {} for podName {}, planExecutionId {}, stage {}", taskId,
+              cik8CleanupTaskParams.getPodNameList(), ambiance.getPlanExecutionId(),
+              nodeExecution.getNode().getIdentifier());
+        }
+      });
     } catch (Exception ex) {
       log.error("Failed to send cleanup call for node {}", nodeExecution.getUuid(), ex);
     }
+  }
+
+  private RetryPolicy<Object> getRetryPolicy(String failedAttemptMessage, String failureMessage) {
+    return new RetryPolicy<>()
+        .handle(Exception.class)
+        .withDelay(RETRY_SLEEP_DURATION)
+        .withMaxAttempts(MAX_ATTEMPTS)
+        .onFailedAttempt(event -> log.info(failedAttemptMessage, event.getAttemptCount(), event.getLastFailure()))
+        .onFailure(event -> log.error(failureMessage, event.getAttemptCount(), event.getFailure()));
   }
 }
