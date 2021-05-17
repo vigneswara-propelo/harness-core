e40d250b8ff59f073897ad0c982ba26b38ca282c
Garvit Pahal <garvit.pahal@harness.io>
2021-05-17 22:04:48 +0530

    [CDNG-7874]: Improve expression grpc service error message (#23683)

--- 800-pipeline-service/src/main/java/io/harness/pms/plan/execution/data/service/expressions/EngineExpressionGrpcServiceImpl.java
+++ 800-pipeline-service/src/main/java/io/harness/pms/plan/execution/data/service/expressions/EngineExpressionGrpcServiceImpl.java
@@ -2,6 +2,7 @@ package io.harness.pms.plan.execution.data.service.expressions;
 
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
+import io.harness.exception.ExceptionUtils;
 import io.harness.pms.contracts.service.EngineExpressionProtoServiceGrpc.EngineExpressionProtoServiceImplBase;
 import io.harness.pms.contracts.service.ExpressionEvaluateBlobRequest;
 import io.harness.pms.contracts.service.ExpressionEvaluateBlobResponse;
@@ -11,8 +12,10 @@ import io.harness.pms.expression.PmsEngineExpressionService;
 
 import com.google.inject.Inject;
 import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
 
 @OwnedBy(HarnessTeam.PIPELINE)
+@Slf4j
 public class EngineExpressionGrpcServiceImpl extends EngineExpressionProtoServiceImplBase {
   private final PmsEngineExpressionService pmsEngineExpressionService;
 
@@ -24,17 +27,29 @@ public class EngineExpressionGrpcServiceImpl extends EngineExpressionProtoServic
   @Override
   public void renderExpression(
       ExpressionRenderBlobRequest request, StreamObserver<ExpressionRenderBlobResponse> responseObserver) {
-    String value = pmsEngineExpressionService.renderExpression(
-        request.getAmbiance(), request.getExpression(), request.getSkipUnresolvedExpressionsCheck());
-    responseObserver.onNext(ExpressionRenderBlobResponse.newBuilder().setValue(value).build());
-    responseObserver.onCompleted();
+    try {
+      String value = pmsEngineExpressionService.renderExpression(
+          request.getAmbiance(), request.getExpression(), request.getSkipUnresolvedExpressionsCheck());
+      responseObserver.onNext(ExpressionRenderBlobResponse.newBuilder().setValue(value).build());
+      responseObserver.onCompleted();
+    } catch (Exception ex) {
+      log.error("Error rendering expression for sdk", ex);
+      responseObserver.onError(
+          io.grpc.Status.INTERNAL.withDescription(ExceptionUtils.getMessage(ex)).asRuntimeException());
+    }
   }
 
   @Override
   public void evaluateExpression(
       ExpressionEvaluateBlobRequest request, StreamObserver<ExpressionEvaluateBlobResponse> responseObserver) {
-    String value = pmsEngineExpressionService.evaluateExpression(request.getAmbiance(), request.getExpression());
-    responseObserver.onNext(ExpressionEvaluateBlobResponse.newBuilder().setValue(value).build());
-    responseObserver.onCompleted();
+    try {
+      String value = pmsEngineExpressionService.evaluateExpression(request.getAmbiance(), request.getExpression());
+      responseObserver.onNext(ExpressionEvaluateBlobResponse.newBuilder().setValue(value).build());
+      responseObserver.onCompleted();
+    } catch (Exception ex) {
+      log.error("Error evaluating expression for sdk", ex);
+      responseObserver.onError(
+          io.grpc.Status.INTERNAL.withDescription(ExceptionUtils.getMessage(ex)).asRuntimeException());
+    }
   }
 }
--- 882-pms-sdk-core/src/main/java/io/harness/pms/sdk/core/resolver/expressions/EngineGrpcExpressionService.java
+++ 882-pms-sdk-core/src/main/java/io/harness/pms/sdk/core/resolver/expressions/EngineGrpcExpressionService.java
@@ -2,6 +2,7 @@ package io.harness.pms.sdk.core.resolver.expressions;
 
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
+import io.harness.exception.GeneralException;
 import io.harness.pms.contracts.ambiance.Ambiance;
 import io.harness.pms.contracts.service.EngineExpressionProtoServiceGrpc.EngineExpressionProtoServiceBlockingStub;
 import io.harness.pms.contracts.service.ExpressionEvaluateBlobRequest;
@@ -13,6 +14,7 @@ import io.harness.pms.serializer.recaster.RecastOrchestrationUtils;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import io.grpc.StatusRuntimeException;
 
 @OwnedBy(HarnessTeam.PIPELINE)
 @Singleton
@@ -27,21 +29,29 @@ public class EngineGrpcExpressionService implements EngineExpressionService {
 
   @Override
   public String renderExpression(Ambiance ambiance, String expression, boolean skipUnresolvedExpressionsCheck) {
-    ExpressionRenderBlobResponse expressionRenderBlobResponse =
-        engineExpressionProtoServiceBlockingStub.renderExpression(
-            ExpressionRenderBlobRequest.newBuilder()
-                .setAmbiance(ambiance)
-                .setExpression(expression)
-                .setSkipUnresolvedExpressionsCheck(skipUnresolvedExpressionsCheck)
-                .build());
-    return expressionRenderBlobResponse.getValue();
+    try {
+      ExpressionRenderBlobResponse expressionRenderBlobResponse =
+          engineExpressionProtoServiceBlockingStub.renderExpression(
+              ExpressionRenderBlobRequest.newBuilder()
+                  .setAmbiance(ambiance)
+                  .setExpression(expression)
+                  .setSkipUnresolvedExpressionsCheck(skipUnresolvedExpressionsCheck)
+                  .build());
+      return expressionRenderBlobResponse.getValue();
+    } catch (StatusRuntimeException ex) {
+      throw new GeneralException(ex.getStatus().getDescription());
+    }
   }
 
   @Override
   public Object evaluateExpression(Ambiance ambiance, String expression) {
-    ExpressionEvaluateBlobResponse expressionEvaluateBlobResponse =
-        engineExpressionProtoServiceBlockingStub.evaluateExpression(
-            ExpressionEvaluateBlobRequest.newBuilder().setAmbiance(ambiance).setExpression(expression).build());
-    return RecastOrchestrationUtils.fromDocumentJson(expressionEvaluateBlobResponse.getValue(), Object.class);
+    try {
+      ExpressionEvaluateBlobResponse expressionEvaluateBlobResponse =
+          engineExpressionProtoServiceBlockingStub.evaluateExpression(
+              ExpressionEvaluateBlobRequest.newBuilder().setAmbiance(ambiance).setExpression(expression).build());
+      return RecastOrchestrationUtils.fromDocumentJson(expressionEvaluateBlobResponse.getValue(), Object.class);
+    } catch (StatusRuntimeException ex) {
+      throw new GeneralException(ex.getStatus().getDescription());
+    }
   }
 }
