46bf9e531ea939b474aeba542e57cea52936d794
Archit Singla <archit.singla@harness.io>
2021-05-17 12:36:17 +0530

    [CDNG-5765]: Removing unwanted yaml beans (#23651)
    * [CDNG-5765]: Removing unwanted yaml beans
    
    * [CDNG-5765]: Removing unwanted yaml beans
    
    * [CDNG-5765]: Removing file

--- 125-cd-nextgen/src/main/java/io/harness/pms/cdng/infra/yaml/InfrastructurePms.java
+++
@@ -1,9 +0,0 @@
-package io.harness.pms.cdng.infra.yaml;
-
-import static com.fasterxml.jackson.annotation.JsonTypeInfo.As.EXTERNAL_PROPERTY;
-import static com.fasterxml.jackson.annotation.JsonTypeInfo.Id.NAME;
-
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-
-@JsonTypeInfo(use = NAME, property = "type", include = EXTERNAL_PROPERTY, visible = true)
-public interface InfrastructurePms {}
