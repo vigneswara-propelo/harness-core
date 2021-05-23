f8ca9c3e8140a388ff8f375bbd973da66379c581
George Georgiev <george@harness.io>
2021-05-22 20:05:08 -0700

    [BT-322]: fix function tests (#23883)

--- scripts/bazel/bazel_functional_script.sh
+++ scripts/bazel/bazel_functional_script.sh
@@ -31,6 +31,6 @@ if [ "${RUN_BAZEL_FUNCTIONAL_TESTS}" == "true" ]; then
   java -Xbootclasspath/p:alpn-boot-8.1.13.v20181017.jar -Xmx4096m -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:mygclogfilename.gc -XX:+UseParallelGC -XX:MaxGCPauseMillis=500 -jar /home/jenkins/.bazel-dirs/bin/260-delegate/module_deploy.jar /home/jenkins/workspace/pr-portal-funtional-tests/260-delegate/config-delegate.yml &
   DELEGATE_PID=$!
   bazel test --keep_going ${BAZEL_ARGUMENTS} --jobs=3 -- //200-functional-test/... || true
-  kill -9 $MANAGER_PID
-  kill -9 $DELEGATE_PID
+  kill -9 $MANAGER_PID || true
+  kill -9 $DELEGATE_PID || true
 fi
