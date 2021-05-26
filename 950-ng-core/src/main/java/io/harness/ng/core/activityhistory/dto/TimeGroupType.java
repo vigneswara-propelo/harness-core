1efc6862b418f61d43c395ed18f3287d90e7a19c
Mohit Garg <mohit.garg@harness.io>
2021-05-26 11:50:42 +0530

    [CDNG-8154]: Add utility to produce growth trend for given entities required as part of dashboard APIs (#23927)
    * [CDNG-5126]: add new DataException for handling metadata for tasks
    
    * [CDNG-8154]: growth trend evaluator added and corresponding dependencies

--- 950-ng-core/src/main/java/io/harness/ng/core/activityhistory/dto/TimeGroupType.java
+++ 950-ng-core/src/main/java/io/harness/ng/core/activityhistory/dto/TimeGroupType.java
@@ -1,3 +1,20 @@
 package io.harness.ng.core.activityhistory.dto;
 
-public enum TimeGroupType { DAY, HOUR }
+import io.harness.annotations.dev.HarnessTeam;
+import io.harness.annotations.dev.OwnedBy;
+
+import lombok.Getter;
+
+@Getter
+@OwnedBy(HarnessTeam.DX)
+public enum TimeGroupType {
+  HOUR(60 * 60 * 1000),
+  DAY(24 * 60 * 60 * 1000),
+  WEEK(7 * 24 * 60 * 60 * 1000);
+
+  private long durationInMs;
+
+  TimeGroupType(long durationInMs) {
+    this.durationInMs = durationInMs;
+  }
+}
