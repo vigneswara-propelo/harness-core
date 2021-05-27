7fde4abaaa6591d61a3c8e51394f209dbd225c9c
Raj Patel <raj.patel@harness.io>
2021-05-27 11:31:05 +0530

    [PL-14258]: redis-cli v6 for monitoring pod (#24013)
    * [PL-14258]: add redis-cli v6 for monitoring pod
    
    * [PL-14258]: add install_redis script
    
    * [PL-14258]: fix file not found error
    
    * [PL-14258]: fix dockerfile

---
+++ 953-events-api/src/monitoring/install_redis.sh
@@ -0,0 +1,7 @@
+# Installing Redis from source as alpine doesn't have Redis 6 yet
+# We need Redis 6 for connecting to SSL enabled instance
+wget https://download.redis.io/releases/redis-6.2.3.tar.gz
+tar xvzf redis-6.2.3.tar.gz
+cd redis-6.2.3
+make
+cp src/redis-server src/redis-cli /usr/bin/
