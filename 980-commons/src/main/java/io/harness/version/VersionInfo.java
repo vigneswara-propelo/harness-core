6e825e3379261028b5d2c5bb72c94e32f755ba34
Rohit Karelia <rohit.karelia@harness.io>
2021-05-26 17:09:57 -0700

    [PL-14181]: Update build with path details (#23841)
    * [PL-14181]: Update build with path details
    
    * [PL-14181]: Update version in logback
    
    * [PL-14181]: Just update build.properties
    
    * [PL-14181]: Fix Aeriform
    
    * [PL-14181]: Merge branch 'master' into PL-14181_update_version

--- 980-commons/src/main/java/io/harness/version/VersionInfo.java
+++ 980-commons/src/main/java/io/harness/version/VersionInfo.java
@@ -1,14 +1,19 @@
 package io.harness.version;
 
+import io.harness.annotations.dev.HarnessTeam;
+import io.harness.annotations.dev.OwnedBy;
+
 import lombok.Builder;
 import lombok.Value;
 
 @Value
 @Builder
+@OwnedBy(HarnessTeam.PL)
 public class VersionInfo {
   private String version;
   private String buildNo;
   private String gitCommit;
   private String gitBranch;
   private String timestamp;
+  private String fullVersionWithPatch;
 }
