e290a6db79f202f12370509b0eddb16a4d230b50
Shivakumar Ningappa <shivakumar.ningappa@harness.io>
2021-06-01 08:12:31 -0700

    [CI-1725]: move java agent out of portal into a separate repository (#24222)

--- 980-java-agent/java-agent-boot/src/main/java/harness/callgraph/boot/TraceDispatcher.java
+++
@@ -1,9 +0,0 @@
-package harness.callgraph.boot;
-
-public abstract class TraceDispatcher {
-  public static volatile TraceDispatcher INSTANCE;
-
-  public abstract Object enter(String type, String method, String signature, boolean testMethod);
-
-  public abstract void exit(Object node);
-}
