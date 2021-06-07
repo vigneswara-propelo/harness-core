a838b2d77527b694492796ba8c3609b7303ac679
Abhinav Singh <abhinav.singh@harness.io>
2021-06-07 22:02:05 +0530

    [CDNG-9205]: Redirect stdout of scm to delegate (#24441)

--- 930-delegate-tasks/src/main/java/io/harness/delegate/task/scm/ScmUnixManager.java
+++ 930-delegate-tasks/src/main/java/io/harness/delegate/task/scm/ScmUnixManager.java
@@ -9,34 +9,39 @@ import io.grpc.ManagedChannel;
 import java.io.File;
 import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
+import org.zeroturnaround.exec.ProcessExecutor;
+import org.zeroturnaround.exec.StartedProcess;
 
 @Slf4j
 @OwnedBy(HarnessTeam.DX)
 public abstract class ScmUnixManager implements AutoCloseable {
   abstract ManagedChannel getChannel();
+
   private final String PATH_TO_SCM_BUILD = InstallUtils.getScmFolderPath();
 
   protected String socketAddress;
-  protected ProcessBuilder processBuilder;
-  protected Process server;
+  protected ProcessExecutor processBuilder;
+  protected StartedProcess server;
 
   public ScmUnixManager() throws IOException {
     socketAddress = "/tmp/" + UUIDGenerator.generateUuid();
-    processBuilder = new ProcessBuilder();
+    processBuilder = new ProcessExecutor();
     runServer();
   }
 
   public void close() throws IOException {
-    server.destroy();
+    server.getProcess().destroy();
     processBuilder.command("rm", "-rf", socketAddress);
-    Process process = processBuilder.start();
-    process.destroy();
+    final StartedProcess process = processBuilder.start();
+    process.getProcess().destroy();
   }
 
   private void runServer() throws IOException {
     processBuilder.directory(new File(PATH_TO_SCM_BUILD));
     processBuilder.command("./" + InstallUtils.getScmBinary(), "--unix", socketAddress);
     log.info("Running SCM server at path: {} on port: {}", PATH_TO_SCM_BUILD, socketAddress);
+    processBuilder.redirectOutput(System.out);
+    processBuilder.redirectError(System.err);
     server = processBuilder.start();
   }
 }
