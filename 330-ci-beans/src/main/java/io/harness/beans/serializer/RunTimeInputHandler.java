02384e480a0562082507d4cec2a14ce8a03599c5
Shubham Agrawal <shubham.agrawal@harness.io>
2021-06-07 12:33:58 +0530

    [CI-1733]: Fix archive format parsing for s3 save/restore cache (#24262)

--- 330-ci-beans/src/main/java/io/harness/beans/serializer/RunTimeInputHandler.java
+++ 330-ci-beans/src/main/java/io/harness/beans/serializer/RunTimeInputHandler.java
@@ -42,11 +42,11 @@ public class RunTimeInputHandler {
     }
   }
 
-  public ArchiveFormat resolveArchiveFormat(ParameterField<ArchiveFormat> archiveFromat) {
-    if (archiveFromat == null || archiveFromat.isExpression() || archiveFromat.getValue() == null) {
+  public ArchiveFormat resolveArchiveFormat(ParameterField<ArchiveFormat> archiveFormat) {
+    if (archiveFormat == null || archiveFormat.isExpression() || archiveFormat.getValue() == null) {
       return ArchiveFormat.TAR;
     } else {
-      return archiveFromat.getValue();
+      return ArchiveFormat.fromString(archiveFormat.fetchFinalValue().toString());
     }
   }
 
--- 330-ci-beans/src/main/java/io/harness/beans/yaml/extended/ArchiveFormat.java
+++ 330-ci-beans/src/main/java/io/harness/beans/yaml/extended/ArchiveFormat.java
@@ -1,13 +1,23 @@
 package io.harness.beans.yaml.extended;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum ArchiveFormat {
   @JsonProperty("tar") TAR("tar"),
   @JsonProperty("gzip") GZIP("gzip");
-
   private final String displayName;
+
+  @JsonCreator
+  public static ArchiveFormat getArchiveFormat(@JsonProperty("archiveFormat") String displayName) {
+    for (ArchiveFormat archiveFormat : ArchiveFormat.values()) {
+      if (archiveFormat.displayName.equalsIgnoreCase(displayName)) {
+        return archiveFormat;
+      }
+    }
+    throw new IllegalArgumentException("Invalid value: " + displayName);
+  }
 
   ArchiveFormat(String displayName) {
     this.displayName = displayName;
@@ -21,5 +31,9 @@ public enum ArchiveFormat {
   @Override
   public String toString() {
     return displayName;
+  }
+
+  public static ArchiveFormat fromString(final String s) {
+    return ArchiveFormat.getArchiveFormat(s);
   }
 }
