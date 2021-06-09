cf57514429a2890a3dddb49ff8d0b377215cbfd6
Harsh Jain <harsh.jain@harness.io>
2021-06-09 14:46:47 +0530

    [CI-1773]: fix capabilty check (#24520)

--- 950-delegate-tasks-beans/src/main/java/io/harness/delegate/beans/ci/CIK8CleanupTaskParams.java
+++ 950-delegate-tasks-beans/src/main/java/io/harness/delegate/beans/ci/CIK8CleanupTaskParams.java
@@ -3,7 +3,12 @@ package io.harness.delegate.beans.ci;
 import static io.harness.expression.Expression.ALLOW_SECRETS;
 
 import io.harness.delegate.beans.ci.pod.ConnectorDetails;
+import io.harness.delegate.beans.connector.k8Connector.K8sTaskCapabilityHelper;
+import io.harness.delegate.beans.connector.k8Connector.KubernetesClusterConfigDTO;
+import io.harness.delegate.beans.executioncapability.ExecutionCapability;
+import io.harness.delegate.beans.executioncapability.ExecutionCapabilityDemander;
 import io.harness.expression.Expression;
+import io.harness.expression.ExpressionEvaluator;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
@@ -14,7 +19,7 @@ import lombok.Data;
 @Data
 @Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class CIK8CleanupTaskParams implements CICleanupTaskParams {
+public class CIK8CleanupTaskParams implements CICleanupTaskParams, ExecutionCapabilityDemander {
   @NotNull private ConnectorDetails k8sConnector;
   @NotNull private List<String> podNameList; // Currently only single pod deletion is supported for each stage
   @NotNull private List<String> serviceNameList;
@@ -25,5 +30,12 @@ public class CIK8CleanupTaskParams implements CICleanupTaskParams {
   @Override
   public Type getType() {
     return type;
+  }
+
+  @Override
+  public List<ExecutionCapability> fetchRequiredExecutionCapabilities(ExpressionEvaluator maskingEvaluator) {
+    KubernetesClusterConfigDTO kubernetesClusterConfigDTO =
+        (KubernetesClusterConfigDTO) k8sConnector.getConnectorConfig();
+    return K8sTaskCapabilityHelper.fetchRequiredExecutionCapabilities(kubernetesClusterConfigDTO, maskingEvaluator);
   }
 }
