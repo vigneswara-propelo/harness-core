76bbc29479acfbec31ff32d79f97329362546342
Naman Verma <naman.verma@harness.io>
2021-06-18 16:16:52 +0530

    [CDNG-9727]: While trying to introduce git diff for pipeline using Github Enterprise, pipeline update itself is failing (#24876)
    * [CDNG-9727]: While trying to introduce git diff for pipeline using Github Enterprise, pipeline update itself is failing
    
    * [CDNG-9727]: team annotation

--- 910-delegate-service-driver/src/main/java/io/harness/service/DelegateGrpcClientWrapper.java
+++ 910-delegate-service-driver/src/main/java/io/harness/service/DelegateGrpcClientWrapper.java
@@ -1,8 +1,13 @@
 package io.harness.service;
 
+import static io.harness.annotations.dev.HarnessTeam.DEL;
+
+import io.harness.annotations.dev.OwnedBy;
 import io.harness.beans.DelegateTaskRequest;
 import io.harness.callback.DelegateCallbackToken;
 import io.harness.delegate.beans.DelegateResponseData;
+import io.harness.delegate.beans.ErrorNotifyResponseData;
+import io.harness.exception.WingsException;
 import io.harness.grpc.DelegateServiceGrpcClient;
 import io.harness.serializer.KryoSerializer;
 import io.harness.tasks.BinaryResponseData;
@@ -14,6 +19,7 @@ import com.google.inject.name.Named;
 import java.time.Duration;
 import java.util.function.Supplier;
 
+@OwnedBy(DEL)
 @Singleton
 public class DelegateGrpcClientWrapper {
   @Inject private DelegateServiceGrpcClient delegateServiceGrpcClient;
@@ -28,6 +34,13 @@ public class DelegateGrpcClientWrapper {
     if (disableDeserialization) {
       delegateResponseData =
           (DelegateResponseData) kryoSerializer.asInflatedObject(((BinaryResponseData) responseData).getData());
+      if (delegateResponseData instanceof ErrorNotifyResponseData) {
+        WingsException exception = ((ErrorNotifyResponseData) delegateResponseData).getException();
+        // if task registered to error handling framework on delegate, then exception won't be null
+        if (exception != null) {
+          throw exception;
+        }
+      }
     } else {
       delegateResponseData = (DelegateResponseData) responseData;
     }
