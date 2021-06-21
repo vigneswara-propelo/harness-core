4c8fc7e040134494c59556bde0d38f44f520d780
Tathagat Chaurasiya <tathagat.chaurasiya@harness.io>
2021-06-21 15:02:46 +0530

    [BT-420]: upadate cd functional test script (#24320)
    * [BT-420]: upadate cd functional test script
    
    * [BT-420]: use same config yaml as of ft
    
    * [BT-420]: update script for ci pipeline
    
    * [BT-420]: update script for ci pipeline

--- scripts/bazel/bazel_cd_functional_script.sh
+++ scripts/bazel/bazel_cd_functional_script.sh
@@ -2,14 +2,10 @@
 
 set -ex
 
-ps auxwwwe
-echo end off ps-report
-
 local_repo=${HOME}/.m2/repository
 BAZEL_ARGUMENTS=
 if [ "${PLATFORM}" == "jenkins" ]; then
   GCP="--google_credentials=${GCP_KEY}"
-  bazelrc=--bazelrc=bazelrc.remote
   local_repo=/root/.m2/repository
   if [ ! -z "${DISTRIBUTE_TESTING_WORKER}" ]; then
     bash scripts/bazel/testDistribute.sh
@@ -25,13 +21,26 @@ if [[ ! -z "${OVERRIDE_LOCAL_M2}" ]]; then
   local_repo=${OVERRIDE_LOCAL_M2}
 fi
 
-if [ "${STEP}" == "dockerization" ]; then
-  GCP=""
-fi
 if [ "${RUN_BAZEL_FUNCTIONAL_TESTS}" == "true" ]; then
-  java -Xmx4096m -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:mygclogfilename.gc -XX:+UseParallelGC -XX:MaxGCPauseMillis=500 -jar /home/jenkins/workspace/cd-deployment-functional-tests/260-delegate/target/delegate-capsule.jar /home/jenkins/workspace/cd-deployment-functional-tests/260-delegate/config-delegate.yml &
-  bazel ${bazelrc} test --keep_going ${GCP} ${BAZEL_ARGUMENTS} --jobs=2 -- //190-deployment-functional-tests/... || true
-fi
 
-ps auxwwwe
-echo end off ps-report
+  bazel build ${GCP} ${BAZEL_ARGUMENTS} -- //190-deployment-functional-tests/...
+  curl https://storage.googleapis.com/harness-prod-public/public/shared/tools/alpn/release/8.1.13.v20181017/alpn-boot-8.1.13.v20181017.jar --output alpn-boot-8.1.13.v20181017.jar
+
+  bazel run ${GCP} ${BAZEL_ARGUMENTS} 230-model-test:app &
+  MANAGER_PID=$!
+
+  bazel test --keep_going ${GCP} ${BAZEL_ARGUMENTS} --jobs=3 -- //190-deployment-functional-tests:io.harness.functional.DummyFirstCdFunctionalTest || true
+
+  java -Xbootclasspath/p:alpn-boot-8.1.13.v20181017.jar -Xmx4096m -XX:+HeapDumpOnOutOfMemoryError \
+    -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:mygclogfilename.gc -XX:+UseParallelGC \
+    -XX:MaxGCPauseMillis=500 -jar /harness/bazel-out/k8-fastbuild/bin/260-delegate/module_deploy.jar /harness/260-delegate/config-delegate.yml &
+  DELEGATE_PID=$!
+
+  bazel test --keep_going ${GCP} ${BAZEL_ARGUMENTS} --jobs=3 -- //190-deployment-functional-tests/... || true
+
+  echo "INFO: MANAGER_PID = $MANAGER_PID"
+  echo "INFO: DELEGATE_PID = $DELEGATE_PID"
+
+  kill -9 $MANAGER_PID || true
+  kill -9 $DELEGATE_PID || true
+fi
