10395ededd9999cabd763150971a5610d98ca8ad
Xin Shao <xin.shao@harness.io>
2021-06-23 19:51:50 -0400

    [DEL-1817]: Delegate service classic grpc connection in 400 rest (#24772)
    * [DEL-1817]: Implement delegate service classic grpc client in 400 rest
    
    * [DEL-1817]: Introduce the switch in delegateserviceimpl and modify the protobuf to return bytes response
    
    * [DEL-1817]: Kryo deserialize executeTaskResponse
    
    * [DEL-1817]: Remove implementation of delegateserviceimpl
    
    * [DEL-1817]: Modify delegateserviceclassicgrpcimpl
    
    * [DEL-1817]: Aeriform and pmd checks

---
+++ 400-rest/src/main/java/io/harness/grpc/DelegateExecuteTaskResponseData.java
@@ -0,0 +1,11 @@
+package io.harness.grpc;
+
+import io.harness.annotations.dev.HarnessTeam;
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.delegate.beans.DelegateResponseData;
+
+import lombok.NoArgsConstructor;
+
+@OwnedBy(HarnessTeam.DEL)
+@NoArgsConstructor
+public class DelegateExecuteTaskResponseData implements DelegateResponseData {}
---
+++ 400-rest/src/main/java/io/harness/grpc/DelegateServiceClassicGrpcClient.java
@@ -0,0 +1,51 @@
+package io.harness.grpc;
+
+import io.harness.annotations.dev.HarnessTeam;
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.beans.DelegateTask;
+import io.harness.delegate.DelegateClassicTaskRequest;
+import io.harness.delegate.DelegateTaskGrpc;
+import io.harness.delegate.ExecuteTaskResponse;
+import io.harness.delegate.QueueTaskResponse;
+import io.harness.delegate.beans.DelegateResponseData;
+import io.harness.serializer.KryoSerializer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.protobuf.ByteString;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@OwnedBy(HarnessTeam.DEL)
+public class DelegateServiceClassicGrpcClient {
+  private final DelegateTaskGrpc.DelegateTaskBlockingStub delegateTaskBlockingStub;
+  private final KryoSerializer kryoSerializer;
+
+  public DelegateServiceClassicGrpcClient(
+      DelegateTaskGrpc.DelegateTaskBlockingStub blockingStub, KryoSerializer kryoSerializer) {
+    this.delegateTaskBlockingStub = blockingStub;
+    this.kryoSerializer = kryoSerializer;
+  }
+
+  public String queueTask(DelegateTask task) {
+    ByteString.copyFrom(kryoSerializer.asDeflatedBytes(task));
+    DelegateClassicTaskRequest delegateClassicTaskRequest =
+        DelegateClassicTaskRequest.newBuilder()
+            .setDelegateTaskKryo(ByteString.copyFrom(kryoSerializer.asDeflatedBytes(task)))
+            .build();
+    final QueueTaskResponse queueTaskResponse = delegateTaskBlockingStub.queueTask(delegateClassicTaskRequest);
+    return queueTaskResponse.getUuid();
+  }
+
+  public <T extends DelegateResponseData> T executeTask(DelegateTask task) {
+    DelegateClassicTaskRequest delegateClassicTaskRequest =
+        DelegateClassicTaskRequest.newBuilder()
+            .setDelegateTaskKryo(ByteString.copyFrom(kryoSerializer.asDeflatedBytes(task)))
+            .build();
+
+    final ExecuteTaskResponse executeTaskResponse = delegateTaskBlockingStub.executeTask(delegateClassicTaskRequest);
+    ObjectMapper mapper = new ObjectMapper();
+    return (T) mapper.convertValue(
+        kryoSerializer.asInflatedObject(executeTaskResponse.getDelegateTaskResponseKryo().toByteArray()),
+        DelegateExecuteTaskResponseData.class);
+  }
+}
