8b49cfa03a18c2b6ea60008384d7813337518b64
Karan Siwach <karan.siwach@harness.io>
2021-06-24 18:30:16 +0530

    [PL-14871]: outbox collection indexes (#24991)
    * [PL-14871]: outbox index update
    
    * [PL-14871]: check

--- 955-outbox-sdk/src/main/java/io/harness/outbox/OutboxEvent.java
+++ 955-outbox-sdk/src/main/java/io/harness/outbox/OutboxEvent.java
@@ -4,12 +4,9 @@ import static io.harness.annotations.dev.HarnessTeam.PL;
 
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.context.GlobalContext;
-import io.harness.iterator.PersistentIterable;
-import io.harness.iterator.PersistentRegularIterable;
 import io.harness.mongo.index.CompoundMongoIndex;
 import io.harness.mongo.index.MongoIndex;
 import io.harness.ng.core.Resource;
-import io.harness.ng.core.Resource.ResourceKeys;
 import io.harness.ng.core.ResourceScope;
 
 import com.google.common.collect.ImmutableList;
@@ -21,7 +18,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.FieldNameConstants;
-import lombok.experimental.UtilityClass;
 import org.mongodb.morphia.annotations.Entity;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
@@ -35,7 +31,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Entity(value = "outboxEvents", noClassnameStored = true)
 @Document("outboxEvents")
 @TypeAlias("outboxEvents")
-public class OutboxEvent implements PersistentIterable, PersistentRegularIterable {
+public class OutboxEvent {
   @Id @org.mongodb.morphia.annotations.Id String id;
 
   @NotNull ResourceScope resourceScope;
@@ -50,51 +46,14 @@ public class OutboxEvent implements PersistentIterable, PersistentRegularIterabl
 
   GlobalContext globalContext;
 
-  Long nextIteration;
-
   public static List<MongoIndex> mongoIndexes() {
     return ImmutableList.<MongoIndex>builder()
         .add(CompoundMongoIndex.builder()
-                 .name("createdAt_blocked_outbox_Idx")
-                 .field(OutboxEventKeys.createdAt)
+                 .name("blocked_createdAt_nextUnblockAttemptAt_outbox_Idx")
                  .field(OutboxEventKeys.blocked)
-                 .build())
-        .add(CompoundMongoIndex.builder()
-                 .name("resourceType_createdAt_outbox_Idx")
-                 .field(OutboxEventKeys.RESOURCE_TYPE_KEY)
                  .field(OutboxEventKeys.createdAt)
-                 .build())
-        .add(CompoundMongoIndex.builder()
-                 .name("eventType_createdAt_outbox_Idx")
-                 .field(OutboxEventKeys.eventType)
-                 .field(OutboxEventKeys.createdAt)
-                 .build())
-        .add(CompoundMongoIndex.builder()
-                 .name("blocked_nextIteration_createdAt_outbox_Idx")
-                 .field(OutboxEventKeys.blocked)
-                 .field(OutboxEventKeys.nextIteration)
-                 .field(OutboxEventKeys.createdAt)
+                 .field(OutboxEventKeys.nextUnblockAttemptAt)
                  .build())
         .build();
-  }
-
-  @Override
-  public void updateNextIteration(String fieldName, long nextIteration) {
-    this.nextIteration = nextIteration;
-  }
-
-  @Override
-  public Long obtainNextIteration(String fieldName) {
-    return this.nextIteration;
-  }
-
-  @Override
-  public String getUuid() {
-    return this.id;
-  }
-
-  @UtilityClass
-  public static final class OutboxEventKeys {
-    public static final String RESOURCE_TYPE_KEY = OutboxEventKeys.resource + "." + ResourceKeys.type;
   }
 }
