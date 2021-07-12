0b3025b30f00f859ee4dd67081a29d909763cd86
Mohit Garg <mohit.garg@harness.io>
2021-07-12 15:34:22 +0530

    [CDNG-10691]: replaced perpetual task service with its wrapper service for NG (#25682)
    * [CDNG-10691]: replaced perpetual task service with its wrapper service for NG
    
    * [CDNG-10691]: check style fix

--- 121-instance/src/main/java/io/harness/service/instancesyncperpetualtask/InstanceSyncPerpetualTaskServiceImpl.java
+++ 121-instance/src/main/java/io/harness/service/instancesyncperpetualtask/InstanceSyncPerpetualTaskServiceImpl.java
@@ -3,11 +3,14 @@ package io.harness.service.instancesyncperpetualtask;
 import static io.harness.annotations.dev.HarnessTeam.DX;
 
 import io.harness.annotations.dev.OwnedBy;
+import io.harness.delegate.AccountId;
 import io.harness.dtos.infrastructuremapping.InfrastructureMappingDTO;
+import io.harness.grpc.DelegateServiceGrpcClient;
 import io.harness.models.constants.InstanceSyncConstants;
-import io.harness.perpetualtask.PerpetualTaskClientContext;
+import io.harness.perpetualtask.PerpetualTaskClientContextDetails;
+import io.harness.perpetualtask.PerpetualTaskId;
 import io.harness.perpetualtask.PerpetualTaskSchedule;
-import io.harness.perpetualtask.PerpetualTaskService;
+import io.harness.perpetualtask.TaskClientParams;
 import io.harness.service.instancesynchandler.AbstractInstanceSyncHandler;
 
 import com.google.inject.Inject;
@@ -21,31 +24,36 @@ import lombok.AllArgsConstructor;
 @OwnedBy(DX)
 @AllArgsConstructor(onConstructor = @__({ @Inject }))
 public class InstanceSyncPerpetualTaskServiceImpl implements InstanceSyncPerpetualTaskService {
-  private PerpetualTaskService perpetualTaskService;
+  private DelegateServiceGrpcClient delegateServiceGrpcClient;
 
   @Override
   public String createPerpetualTask(
       InfrastructureMappingDTO infrastructureMappingDTO, AbstractInstanceSyncHandler abstractInstanceSyncHandler) {
-    return perpetualTaskService.createTask(abstractInstanceSyncHandler.getPerpetualTaskType(),
-        infrastructureMappingDTO.getAccountIdentifier(), preparePerpetualTaskClientContext(infrastructureMappingDTO),
-        preparePerpetualTaskSchedule(), false,
+    PerpetualTaskId perpetualTaskId = delegateServiceGrpcClient.createPerpetualTask(
+        AccountId.newBuilder().setId(infrastructureMappingDTO.getAccountIdentifier()).build(),
+        abstractInstanceSyncHandler.getPerpetualTaskType(), preparePerpetualTaskSchedule(),
+        preparePerpetualTaskClientContext(infrastructureMappingDTO), false,
         abstractInstanceSyncHandler.getPerpetualTaskDescription(infrastructureMappingDTO));
+    return perpetualTaskId.getId();
   }
 
   @Override
   public void resetPerpetualTask(String accountIdentifier, String perpetualTaskId) {
-    perpetualTaskService.resetTask(accountIdentifier, perpetualTaskId, null);
+    delegateServiceGrpcClient.resetPerpetualTask(AccountId.newBuilder().setId(accountIdentifier).build(),
+        PerpetualTaskId.newBuilder().setId(perpetualTaskId).build(), null);
   }
 
   // --------------------------- PRIVATE METHODS -------------------------------
 
-  private PerpetualTaskClientContext preparePerpetualTaskClientContext(
+  private PerpetualTaskClientContextDetails preparePerpetualTaskClientContext(
       InfrastructureMappingDTO infrastructureMappingDTO) {
     Map<String, String> clientContextMap = new HashMap<>();
     // TODO check if more fields required to be set in perpetual task
     //  ideally infrastructure mapping id should be enough
     clientContextMap.put(InstanceSyncConstants.INFRASTRUCTURE_MAPPING_ID, infrastructureMappingDTO.getId());
-    return PerpetualTaskClientContext.builder().clientParams(clientContextMap).build();
+    return PerpetualTaskClientContextDetails.newBuilder()
+        .setTaskClientParams(TaskClientParams.newBuilder().putAllParams(clientContextMap).build())
+        .build();
   }
 
   private PerpetualTaskSchedule preparePerpetualTaskSchedule() {
