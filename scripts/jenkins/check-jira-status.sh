0ce6bac91cda704a98dcb7230f471bef8ecacc98
Nataraja Maruthi <nataraja@harness.io>
2021-07-18 18:49:24 +0530

    [ART-286]: Added script to rever the JIRA number (#25836)
    * [ART-286]: added version update to hotfix sctipt
    
    * [ART-286]: Jira status check script addition
    
    * revert: [ART-286]: Testing revert commit
    
    * [ART-286]: Added script to rever the JIRA number
    
    * [ART-286]: Added script to rever the JIRA number

---
+++ scripts/jenkins/check-jira-status.sh
@@ -0,0 +1,23 @@
+PROJECTS="BT|CCE|CCM|CDC|CDNG|CDP|CE|CI|CV|CVNG|DEL|DOC|DX|ER|FFM|OPS|PL|SEC|SWAT|GTM|ONP|ART"
+
+KEY=`git log --pretty=oneline --abbrev-commit -1 |\
+  grep -o -iE "\[(${PROJECTS})-[0-9]+]:" | grep -o -iE "(${PROJECTS})-[0-9]+"`
+
+if [ -z "$KEY" ]; then
+  KEY=`git rev-parse --abbrev-ref HEAD | grep -o -iE "(${PROJECTS})-[0-9]+"`
+fi
+
+echo $KEY
+
+status=`curl -X GET -H "Content-Type: application/json" https://harness.atlassian.net/rest/api/2/issue/${KEY}?fields=status --user $JIRA_USERNAME:$JIRA_PASSWORD | jq ".fields.status.name" | tr -d '"'`
+
+echo $status
+
+if [[ "${status}" = "QA Test" || "${status}" = "Done" ]]
+        then
+           echo "status is in done or qa test status"
+        else
+           echo "jira not in done or qa test status, Hence failing, current status is : ${status} "
+	         exit 1
+fi
+
---
+++ scripts/jenkins/release-branch-revert-jiras.sh
@@ -0,0 +1,34 @@
+KEYS=`git log --pretty=oneline --abbrev-commit |\
+      awk "/${PREVIOUS_CUT_COMMIT_MESSAGE}/ {exit} {print}" | grep -iE "revert: \["  |\
+      grep -o -iE '(ART|BT|CCE|CCM|CDC|CDNG|CDP|CE|CI|CV|CVNG|DEL|DOC|DX|ER|FFM|OPS|PL|SEC|SWAT|GTM|ONP)-[0-9]+' | sort | uniq`
+
+LABLE="HOTFIX"
+
+if [ "${PURPOSE}" = "saas" ]
+then
+    LABLE="SAAS_HOTFIX"
+    FIELD_ID="customfield_10644"
+elif [ "${PURPOSE}" = "on-prem" ]
+then
+    LABLE="ONPREM_HOTFIX"
+    FIELD_ID="customfield_10646"
+else
+   echo "Unknown purpose ${PURPOSE}"
+   exit 1
+fi
+
+echo $KEYS
+
+echo $LABLE
+
+
+for KEY in ${KEYS}
+do
+    echo $KEY
+    curl \
+       -X PUT \
+       --data "{ \"fields\" : { \"${FIELD_ID}\" : \"\" }}" \
+       -H "Content-Type: application/json" \
+       https://harness.atlassian.net/rest/api/2/issue/${KEY} \
+       --user $JIRA_USERNAME:$JIRA_PASSWORD
+done
