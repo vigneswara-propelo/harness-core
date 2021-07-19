99086badfa4edbf9ed3f9e7845444f0c80e6c50e
Garvit Pahal <garvit.pahal@harness.io>
2021-07-19 16:41:06 +0530

    [CDNG-10811]: Remove ng approval feature flag (#25854)

--- 800-pipeline-service/src/main/java/io/harness/pms/pipeline/CommonStepInfo.java
+++ 800-pipeline-service/src/main/java/io/harness/pms/pipeline/CommonStepInfo.java
@@ -3,7 +3,6 @@ package io.harness.pms.pipeline;
 import static io.harness.annotations.dev.HarnessTeam.PIPELINE;
 
 import io.harness.annotations.dev.OwnedBy;
-import io.harness.beans.FeatureName;
 import io.harness.pms.contracts.steps.StepInfo;
 import io.harness.pms.contracts.steps.StepMetaData;
 import io.harness.pms.helpers.PmsFeatureFlagHelper;
@@ -43,7 +42,6 @@ public class CommonStepInfo {
                                                               .addCategory("Approval")
                                                               .setFolderPath("Approval")
                                                               .build())
-                                         .setFeatureFlag(FeatureName.NG_HARNESS_APPROVAL.name())
                                          .build();
   StepInfo jiraApprovalStepInfo = StepInfo.newBuilder()
                                       .setName("Jira Approval")
@@ -53,21 +51,18 @@ public class CommonStepInfo {
                                                            .addCategory("Approval")
                                                            .setFolderPath("Approval")
                                                            .build())
-                                      .setFeatureFlag(FeatureName.NG_HARNESS_APPROVAL.name())
                                       .build();
   StepInfo jiraCreateStepInfo =
       StepInfo.newBuilder()
           .setName("Jira Create")
           .setType(StepSpecTypeConstants.JIRA_CREATE)
           .setStepMetaData(StepMetaData.newBuilder().addCategory("Jira").setFolderPath("Jira").build())
-          .setFeatureFlag(FeatureName.NG_HARNESS_APPROVAL.name())
           .build();
   StepInfo jiraUpdateStepInfo =
       StepInfo.newBuilder()
           .setName("Jira Update")
           .setType(StepSpecTypeConstants.JIRA_UPDATE)
           .setStepMetaData(StepMetaData.newBuilder().addCategory("Jira").setFolderPath("Jira").build())
-          .setFeatureFlag(FeatureName.NG_HARNESS_APPROVAL.name())
           .build();
   StepInfo barrierStepInfo =
       StepInfo.newBuilder()
