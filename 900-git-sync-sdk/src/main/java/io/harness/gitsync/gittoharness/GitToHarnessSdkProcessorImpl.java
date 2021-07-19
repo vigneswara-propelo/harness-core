77549a342a7cc084d16ac24110b52d7ba53f56a8
Abhinav Singh <abhinav.singh@harness.io>
2021-07-20 01:06:50 +0530

    [CDNG-10820]: better error msg in case of g2h processing failure (#25865)
    * [CDNG-10820]: better error msg in case of g2h processing failure

--- 900-git-sync-sdk/src/main/java/io/harness/gitsync/gittoharness/GitToHarnessSdkProcessorImpl.java
+++ 900-git-sync-sdk/src/main/java/io/harness/gitsync/gittoharness/GitToHarnessSdkProcessorImpl.java
@@ -7,6 +7,7 @@ import static io.harness.logging.AutoLogContext.OverrideBehavior.OVERRIDE_ERROR;
 
 import io.harness.EntityType;
 import io.harness.annotations.dev.OwnedBy;
+import io.harness.exception.ExceptionUtils;
 import io.harness.exception.InvalidRequestException;
 import io.harness.gitsync.ChangeSet;
 import io.harness.gitsync.ChangeSets;
@@ -137,8 +138,8 @@ public class GitToHarnessSdkProcessorImpl implements GitToHarnessSdkProcessor {
           updateFileProcessingResponse(
               FileProcessingStatus.SUCCESS, null, processingResponseMap, changeSet.getFilePath(), commitId, accountId);
         } catch (Exception e) {
-          log.error("Exception {}", changeSet.getFilePath(), e);
-          updateFileProcessingResponse(FileProcessingStatus.FAILURE, e.getMessage(), processingResponseMap,
+          log.error("Exception in processing [{}]", changeSet.getFilePath(), e);
+          updateFileProcessingResponse(FileProcessingStatus.FAILURE, getErrorMessage(e), processingResponseMap,
               changeSet.getFilePath(), commitId, accountId);
         }
       }
@@ -235,5 +236,9 @@ public class GitToHarnessSdkProcessorImpl implements GitToHarnessSdkProcessor {
 
   private List<ChangeSet> sortChangeSets(Supplier<List<EntityType>> sortOrder, List<ChangeSet> changeSetList) {
     return changeSetList.stream().sorted(new ChangeSetSortComparator(sortOrder.get())).collect(Collectors.toList());
+  }
+
+  private String getErrorMessage(Exception ex) {
+    return ExceptionUtils.getMessage(ex);
   }
 }
