b093bd043ca0e8eb1c2fe35b1d13d276f242996f
Shubhanshu Verma <shubhanshu.verma@harness.io>
2021-07-22 18:02:36 +0530

    [CCM-4798]: Fixed binding issue (#25927)

--- 280-batch-processing/src/main/java/io/harness/batch/processing/config/BatchProcessingModule.java
+++ 280-batch-processing/src/main/java/io/harness/batch/processing/config/BatchProcessingModule.java
@@ -13,6 +13,8 @@ import io.harness.ccm.anomaly.service.impl.AnomalyServiceImpl;
 import io.harness.ccm.anomaly.service.itfc.AnomalyService;
 import io.harness.ccm.billing.bigquery.BigQueryService;
 import io.harness.ccm.billing.bigquery.BigQueryServiceImpl;
+import io.harness.ccm.commons.service.impl.InstanceDataServiceImpl;
+import io.harness.ccm.commons.service.intf.InstanceDataService;
 import io.harness.ccm.communication.CESlackWebhookService;
 import io.harness.ccm.communication.CESlackWebhookServiceImpl;
 import io.harness.ccm.views.service.CEViewService;
@@ -77,6 +79,7 @@ public class BatchProcessingModule extends AbstractModule {
     bind(AnomalyService.class).to(AnomalyServiceImpl.class);
     install(new ConnectorResourceClientModule(batchMainConfig.getNgManagerServiceHttpClientConfig(),
         batchMainConfig.getNgManagerServiceSecret(), BATCH_PROCESSING.getServiceId()));
+    bind(InstanceDataService.class).to(InstanceDataServiceImpl.class);
 
     bindCFServices();
 
