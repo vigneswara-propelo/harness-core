eaf6090eef647caf29384421a3cfd1a666b107b3
K Rohit Reddy <rohit.reddy@harness.io>
2021-07-22 11:17:06 +0530

    [CCM-4777]: On-Premise Dockerfile, Script changes for CDC (#25891)
    * [CCM-4777]: Move to Safe Alpine Base images
    
    * [CCM-4777]: Non Root User, Exporting Command

--- dockerization/change-data-capture/scripts/start_process.sh
+++ dockerization/change-data-capture/scripts/start_process.sh
@@ -13,6 +13,10 @@ fi
 
 echo "Using memory " $MEMORY
 
+if [[ -z "$COMMAND" ]]; then
+   export COMMAND=server
+fi
+
 if [[ -z "$CAPSULE_JAR" ]]; then
    export CAPSULE_JAR=/opt/harness/change-data-capture.jar
 fi
@@ -21,7 +25,7 @@ export JAVA_OPTS="-Xms${MEMORY}m -Xmx${MEMORY}m -XX:+HeapDumpOnOutOfMemoryError 
 
 
 if [[ "${DEPLOY_MODE}" == "KUBERNETES" ]] || [[ "${DEPLOY_MODE}" == "KUBERNETES_ONPREM" ]]; then
-    java $JAVA_OPTS -jar $CAPSULE_JAR server /opt/harness/config.yml
+    java $JAVA_OPTS -jar $CAPSULE_JAR $COMMAND /opt/harness/config.yml
 else
-    java $JAVA_OPTS -jar $CAPSULE_JAR server /opt/harness/config.yml > /opt/harness/logs/change-data-capture.log 2>&1
+    java $JAVA_OPTS -jar $CAPSULE_JAR $COMMAND /opt/harness/config.yml > /opt/harness/logs/change-data-capture.log 2>&1
 fi
