a2e0fe7d6d02f00d0b2f795fd4672f3796ab1415
Nataraja Maruthi <nataraja@harness.io>
2021-07-27 17:01:14 +0530

    [ART-296]: Removed the CI credentials in Script (#26099)

--- scripts/jenkins/release-branch-test.sh
+++ scripts/jenkins/release-branch-test.sh
@@ -4,16 +4,6 @@ set -e
 
 PROJECTS="ART|BT|CCE|CCM|CDC|CDNG|CDP|CE|CI|CV|CVNG|DEL|DOC|DX|ER|FFM|OPS|PIP|PL|SEC|SWAT|GTM|ONP"
 
-cat <<EOF > ${HOME}/.netrc
-machine ${DRONE_NETRC_MACHINE}
-login ${DRONE_NETRC_USERNAME}
-password ${DRONE_NETRC_PASSWORD}
-EOF
-
-git fetch --unshallow
-
-git fetch --all
-
 git log --remotes=origin/release/* --pretty=oneline --abbrev-commit | grep -iE "\[(${PROJECTS})-[0-9]+]:" -o | sort | uniq | tr '[:lower:]' '[:upper:]' > release.txt
 git log --remotes=origin/[m]aster --pretty=oneline --abbrev-commit | grep -iE "\[(${PROJECTS})-[0-9]+]:" -o | sort | uniq | tr '[:lower:]' '[:upper:]' > master.txt
 NOT_MERGED=`comm -23 release.txt master.txt | tr '\n' ' '`
