335ac40575f7c95ea99153f71be8d584c458397c
Rohit Karelia <rohit.karelia@harness.io>
2021-08-04 17:04:23 -0700

    [CDNG-11429]: Update unsubscribe api (#26354)

--- 125-cd-nextgen/src/main/java/io/harness/cdng/polling/PollingFrameworkService.java
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/polling/PollingFrameworkService.java
@@ -5,6 +5,7 @@ import io.harness.annotations.dev.OwnedBy;
 import io.harness.polling.contracts.PollingItem;
 import io.harness.polling.contracts.service.PollingDocument;
 import io.harness.polling.contracts.service.PollingFrameworkServiceGrpc;
+import io.harness.polling.contracts.service.PollingResponse;
 
 import io.grpc.stub.StreamObserver;
 
@@ -13,6 +14,12 @@ public class PollingFrameworkService extends PollingFrameworkServiceGrpc.Polling
   @Override
   public void subscribe(PollingItem request, StreamObserver<PollingDocument> responseObserver) {
     responseObserver.onNext(PollingDocument.newBuilder().build());
+    responseObserver.onCompleted();
+  }
+
+  @Override
+  public void unsubscribe(PollingItem request, StreamObserver<PollingResponse> responseObserver) {
+    responseObserver.onNext(PollingResponse.newBuilder().build());
     responseObserver.onCompleted();
   }
 }
