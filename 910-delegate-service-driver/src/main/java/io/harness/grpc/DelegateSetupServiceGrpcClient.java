7dbdd2520be422112b123f92fa6e17689d01c73a
Xin Shao <xin.shao@harness.io>
2021-08-09 16:10:22 -0400

    [DEL-2300]: Use harness library instead of google commons string library (#26470)

--- 910-delegate-service-driver/src/main/java/io/harness/grpc/DelegateSetupServiceGrpcClient.java
+++ 910-delegate-service-driver/src/main/java/io/harness/grpc/DelegateSetupServiceGrpcClient.java
@@ -1,6 +1,7 @@
 package io.harness.grpc;
 
 import static io.harness.annotations.dev.HarnessTeam.DEL;
+import static io.harness.data.structure.EmptyPredicate.isNotEmpty;
 
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.delegate.AccountId;
@@ -11,7 +12,6 @@ import io.harness.delegatesetup.DelegateSetupServiceGrpc.DelegateSetupServiceBlo
 import io.harness.owner.OrgIdentifier;
 import io.harness.owner.ProjectIdentifier;
 
-import com.google.common.base.Strings;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import javax.annotation.Nullable;
@@ -28,10 +28,10 @@ public class DelegateSetupServiceGrpcClient {
     final AccountId accountIdentifier = AccountId.newBuilder().setId(accountId).build();
     final Builder builder = DelegateGroupsRequest.newBuilder().setAccountId(accountIdentifier);
 
-    if (Strings.isNullOrEmpty(orgId)) {
+    if (isNotEmpty(orgId)) {
       builder.setOrgId(OrgIdentifier.newBuilder().setId(orgId).build());
     }
-    if (Strings.isNullOrEmpty(projectId)) {
+    if (isNotEmpty(projectId)) {
       builder.setProjectId(ProjectIdentifier.newBuilder().setId(projectId).build());
     }
     final DelegateGroupsCountResponse response = blockingStub.getDelegateGroupsCount(builder.build());
