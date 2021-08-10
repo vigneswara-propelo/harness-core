35231c5e9b4a11f8a6d259edb801c58a693d2e2a
Naman Verma <naman.verma@harness.io>
2021-08-10 15:59:54 +0530

    [PIE-352]: Add tests for generic stage and approval stage plan creators (#26506)
    * [PIE-352]: Add tests for generic stage and approval stage plan creators
    
    * Remove unused imports

---
+++ 860-orchestration-steps/src/test/java/io/harness/plancreator/approval/ApprovalStagePlanCreatorTest.java
@@ -0,0 +1,83 @@
+package io.harness.plancreator.approval;
+
+import static io.harness.annotations.dev.HarnessTeam.PIPELINE;
+import static io.harness.rule.OwnerRule.NAMAN;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.category.element.UnitTests;
+import io.harness.plancreator.stages.stage.StageElementConfig;
+import io.harness.pms.sdk.core.plan.creation.beans.PlanCreationContext;
+import io.harness.pms.sdk.core.plan.creation.beans.PlanCreationResponse;
+import io.harness.pms.yaml.YAMLFieldNameConstants;
+import io.harness.pms.yaml.YamlField;
+import io.harness.pms.yaml.YamlNode;
+import io.harness.pms.yaml.YamlUtils;
+import io.harness.rule.Owner;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+import java.io.IOException;
+import java.net.URL;
+import java.util.LinkedHashMap;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mockito.MockitoAnnotations;
+
+@OwnedBy(PIPELINE)
+public class ApprovalStagePlanCreatorTest {
+  YamlField approvalStageYamlField;
+  PlanCreationContext approvalStageContext;
+  StageElementConfig approvalStageConfig;
+
+  @Before
+  public void setUp() throws IOException {
+    MockitoAnnotations.initMocks(this);
+    ClassLoader classLoader = this.getClass().getClassLoader();
+    final URL testFile = classLoader.getResource("complex_pipeline.yaml");
+    assertThat(testFile).isNotNull();
+    String pipelineYaml = Resources.toString(testFile, Charsets.UTF_8);
+    String pipelineYamlWithUuid = YamlUtils.injectUuid(pipelineYaml);
+
+    YamlField pipelineYamlField = YamlUtils.readTree(pipelineYamlWithUuid).getNode().getField("pipeline");
+    assertThat(pipelineYamlField).isNotNull();
+    YamlField stagesYamlField = pipelineYamlField.getNode().getField("stages");
+    assertThat(stagesYamlField).isNotNull();
+    List<YamlNode> stageYamlNodes = stagesYamlField.getNode().asArray();
+
+    approvalStageYamlField = stageYamlNodes.get(0).getField("stage");
+    approvalStageContext = PlanCreationContext.builder().currentField(approvalStageYamlField).build();
+    approvalStageConfig = YamlUtils.read(approvalStageYamlField.getNode().toString(), StageElementConfig.class);
+  }
+
+  @Test
+  @Owner(developers = NAMAN)
+  @Category(UnitTests.class)
+  public void testCreatePlanForChildrenNodes() {
+    YamlField specField = approvalStageYamlField.getNode().getField(YAMLFieldNameConstants.SPEC);
+    assertThat(specField).isNotNull();
+    String specUuid = specField.getNode().getUuid();
+    YamlField executionField = specField.getNode().getField(YAMLFieldNameConstants.EXECUTION);
+    assertThat(executionField).isNotNull();
+    String executionUuid = executionField.getNode().getUuid();
+
+    ApprovalStagePlanCreator approvalStagePlanCreator = new ApprovalStagePlanCreator();
+    LinkedHashMap<String, PlanCreationResponse> planForChildrenNodes =
+        approvalStagePlanCreator.createPlanForChildrenNodes(approvalStageContext, approvalStageConfig);
+    assertThat(planForChildrenNodes).isNotEmpty();
+    assertThat(planForChildrenNodes).hasSize(2);
+    assertThat(planForChildrenNodes.containsKey(specUuid)).isTrue();
+    assertThat(planForChildrenNodes.containsKey(executionUuid)).isTrue();
+
+    PlanCreationResponse specPlanCreationResponse = planForChildrenNodes.get(specUuid);
+    assertThat(specPlanCreationResponse.getNodes()).hasSize(1);
+
+    PlanCreationResponse executionPlanCreationResponse = planForChildrenNodes.get(executionUuid);
+    assertThat(executionPlanCreationResponse.getDependencies()).hasSize(1);
+    assertThat(executionPlanCreationResponse.getDependencies().containsKey(executionUuid)).isTrue();
+    assertThat(executionPlanCreationResponse.getDependencies().get(executionUuid)).isEqualTo(executionField);
+  }
+}
---
+++ 860-orchestration-steps/src/test/java/io/harness/plancreator/stages/GenericStagePlanCreatorTest.java
@@ -0,0 +1,110 @@
+package io.harness.plancreator.stages;
+
+import static io.harness.annotations.dev.HarnessTeam.PIPELINE;
+import static io.harness.rule.OwnerRule.NAMAN;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
+
+import io.harness.OrchestrationStepsTestBase;
+import io.harness.advisers.nextstep.NextStepAdviserParameters;
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.category.element.UnitTests;
+import io.harness.plancreator.approval.ApprovalStagePlanCreator;
+import io.harness.plancreator.stages.stage.StageElementConfig;
+import io.harness.plancreator.steps.common.StageElementParameters;
+import io.harness.pms.contracts.steps.StepCategory;
+import io.harness.pms.contracts.steps.StepType;
+import io.harness.pms.sdk.core.plan.PlanNode;
+import io.harness.pms.sdk.core.plan.creation.beans.PlanCreationContext;
+import io.harness.pms.sdk.core.steps.io.StepParameters;
+import io.harness.pms.yaml.YAMLFieldNameConstants;
+import io.harness.pms.yaml.YamlField;
+import io.harness.pms.yaml.YamlNode;
+import io.harness.pms.yaml.YamlUtils;
+import io.harness.rule.Owner;
+import io.harness.serializer.KryoSerializer;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+import com.google.inject.Inject;
+import com.google.protobuf.ByteString;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+@OwnedBy(PIPELINE)
+public class GenericStagePlanCreatorTest extends OrchestrationStepsTestBase {
+  YamlField approvalStageYamlField;
+  PlanCreationContext approvalStageContext;
+  StageElementConfig approvalStageConfig;
+
+  @InjectMocks ApprovalStagePlanCreator approvalStagePlanCreator;
+  @Mock KryoSerializer kryoSerializer;
+  @Inject KryoSerializer kryoSerializerUnMocked;
+
+  @Before
+  public void setUp() throws IOException {
+    MockitoAnnotations.initMocks(this);
+    ClassLoader classLoader = this.getClass().getClassLoader();
+    final URL testFile = classLoader.getResource("complex_pipeline.yaml");
+    assertThat(testFile).isNotNull();
+    String pipelineYaml = Resources.toString(testFile, Charsets.UTF_8);
+    String pipelineYamlWithUuid = YamlUtils.injectUuid(pipelineYaml);
+
+    YamlField pipelineYamlField = YamlUtils.readTree(pipelineYamlWithUuid).getNode().getField("pipeline");
+    assertThat(pipelineYamlField).isNotNull();
+    YamlField stagesYamlField = pipelineYamlField.getNode().getField("stages");
+    assertThat(stagesYamlField).isNotNull();
+    List<YamlNode> stageYamlNodes = stagesYamlField.getNode().asArray();
+
+    approvalStageYamlField = stageYamlNodes.get(0).getField("stage");
+    approvalStageContext = PlanCreationContext.builder().currentField(approvalStageYamlField).build();
+    approvalStageConfig = YamlUtils.read(approvalStageYamlField.getNode().toString(), StageElementConfig.class);
+  }
+
+  @Test
+  @Owner(developers = NAMAN)
+  @Category(UnitTests.class)
+  public void testCreatePlanForParentNode() {
+    YamlField sibling = approvalStageYamlField.getNode().nextSiblingFromParentArray(
+        approvalStageYamlField.getName(), Arrays.asList(YAMLFieldNameConstants.STAGE, YAMLFieldNameConstants.PARALLEL));
+    assertThat(sibling).isNotNull();
+    NextStepAdviserParameters nextStepAdviserParameters =
+        NextStepAdviserParameters.builder().nextNodeId(sibling.getNode().getUuid()).build();
+    byte[] siblingAsBytes = kryoSerializerUnMocked.asBytes(nextStepAdviserParameters);
+    doReturn(siblingAsBytes).when(kryoSerializer).asBytes(nextStepAdviserParameters);
+
+    PlanNode approvalStagePlanNode =
+        approvalStagePlanCreator.createPlanForParentNode(approvalStageContext, approvalStageConfig, null);
+    assertThat(approvalStagePlanNode).isNotNull();
+    assertThat(approvalStagePlanNode.getUuid()).isEqualTo(approvalStageYamlField.getNode().getUuid());
+    assertThat(approvalStagePlanNode.getName()).isEqualTo("a1-1");
+    assertThat(approvalStagePlanNode.getIdentifier()).isEqualTo("a11");
+    assertThat(approvalStagePlanNode.getGroup()).isEqualTo("STAGE");
+    assertThat(approvalStagePlanNode.getStepType())
+        .isEqualTo(StepType.newBuilder().setType("APPROVAL_STAGE").setStepCategory(StepCategory.STAGE).build());
+    assertThat(approvalStagePlanNode.getGroup()).isEqualTo("STAGE");
+    assertThat(approvalStagePlanNode.getSkipCondition()).isNull();
+    assertThat(approvalStagePlanNode.getWhenCondition()).isEqualTo("<+OnPipelineSuccess>");
+
+    assertThat(approvalStagePlanNode.getFacilitatorObtainments()).hasSize(1);
+    assertThat(approvalStagePlanNode.getFacilitatorObtainments().get(0).getType().getType()).isEqualTo("CHILD");
+
+    assertThat(approvalStagePlanNode.getAdviserObtainments()).hasSize(1);
+    assertThat(approvalStagePlanNode.getAdviserObtainments().get(0).getType().getType()).isEqualTo("NEXT_STEP");
+    assertThat(approvalStagePlanNode.getAdviserObtainments().get(0).getParameters())
+        .isEqualTo(ByteString.copyFrom(siblingAsBytes));
+
+    StepParameters stepParameters = approvalStagePlanNode.getStepParameters();
+    assertThat(stepParameters).isNotNull();
+    assertThat(stepParameters instanceof StageElementParameters).isTrue();
+  }
+}
