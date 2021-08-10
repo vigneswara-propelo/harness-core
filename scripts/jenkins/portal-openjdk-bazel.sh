b201c872119fed4f537ac5a659a31ee6ac6005c1
Xin Shao <xin.shao@harness.io>
2021-08-10 15:47:01 -0400

    [DEL-2302]:  Add build steps for DMS (#26527)

--- scripts/jenkins/portal-openjdk-bazel.sh
+++ scripts/jenkins/portal-openjdk-bazel.sh
@@ -39,6 +39,34 @@ fi
 
 cd ../..
 
+mkdir -p dist/delegate-service-app ;
+
+cd dist/delegate-service-app
+
+cp ${HOME}/.bazel-dirs/bin/270-delegate-service-app/module_deploy.jar delegate-service-capsule.jar
+cp ../../270-delegate-service-app/main/resources/hazelcast.xml .
+cp ../../270-delegate-service-app/keystore.jks .
+cp ../../270-delegate-service-app/key.pem .
+cp ../../270-delegate-service-app/cert.pem .
+cp ../../270-delegate-service-app/delegate-service-config.yml .
+cp ../../270-delegate-service-app/src/main/resources/redisson-jcache.yaml .
+cp ../../alpn-boot-8.1.13.v20181017.jar .
+
+cp ../../dockerization/delegate-service-app/Dockerfile-delegate-service-app-jenkins-k8-openjdk ./Dockerfile
+cp ../../dockerization/delegate-service-app/Dockerfile-delegate-service-app-jenkins-k8-gcr-openjdk ./Dockerfile-gcr
+cp -r ../../dockerization/delegate-service-app/scripts/ .
+mv scripts/start_process_bazel.sh scripts/start_process.sh
+
+cp ../../protocol.info .
+echo ${JDK} > jdk.txt
+echo ${VERSION} > version.txt
+if [ ! -z ${PURPOSE} ]
+then
+    echo ${PURPOSE} > purpose.txt
+fi
+
+cd ../..
+
 mkdir -p dist/cv-nextgen ;
 
 cd dist/cv-nextgen
