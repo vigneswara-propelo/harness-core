6101c76ba7718f4852f416618de38920c8c5934c
Harsh Jain <harsh.jain@harness.io>
2021-08-11 11:09:04 +0530

    [CI-2353]: fix ci module (#26542)

--- 310-ci-manager/src/main/java/io/harness/app/CIPersistenceModule.java
+++ 310-ci-manager/src/main/java/io/harness/app/CIPersistenceModule.java
@@ -1,11 +1,25 @@
 package io.harness.app;
 
+import io.harness.mongo.MongoConfig;
+import io.harness.springdata.HTransactionTemplate;
 import io.harness.springdata.SpringPersistenceConfig;
 import io.harness.springdata.SpringPersistenceModule;
+
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
 
 public class CIPersistenceModule extends SpringPersistenceModule {
   @Override
   protected Class<?>[] getConfigClasses() {
     return new Class<?>[] {SpringPersistenceConfig.class};
+  }
+
+  @Provides
+  @Singleton
+  protected TransactionTemplate getTransactionTemplate(
+      MongoTransactionManager mongoTransactionManager, MongoConfig mongoConfig) {
+    return new HTransactionTemplate(mongoTransactionManager, mongoConfig.isTransactionsEnabled());
   }
 }
