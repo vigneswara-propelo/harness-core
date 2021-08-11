657a0794741e6f34ca20ef45ea580cbc777a2a79
Bojan Micovic <bojan.micovic@harness.io>
2021-08-11 17:56:33 +0200

    [DEL-0]: Revert del 2275 (#26525) (#26565)
    * [DEL-0] Disable calling DelegateSetupGrpcService since it doesn't work
    
    * [DEL-0]: update hotix build number
    
    Co-authored-by: Marko Milic <marko.milic@harness.io>

--- 120-ng-manager/src/main/java/io/harness/ng/core/api/impl/DelegateDetailsServiceImpl.java
+++ 120-ng-manager/src/main/java/io/harness/ng/core/api/impl/DelegateDetailsServiceImpl.java
@@ -3,7 +3,6 @@ package io.harness.ng.core.api.impl;
 import static io.harness.annotations.dev.HarnessTeam.DEL;
 
 import io.harness.annotations.dev.OwnedBy;
-import io.harness.grpc.DelegateSetupServiceGrpcClient;
 import io.harness.ng.core.api.DelegateDetailsService;
 
 import com.google.inject.Inject;
@@ -12,10 +11,8 @@ import lombok.RequiredArgsConstructor;
 @OwnedBy(DEL)
 @RequiredArgsConstructor(onConstructor = @__({ @Inject }))
 public class DelegateDetailsServiceImpl implements DelegateDetailsService {
-  private final DelegateSetupServiceGrpcClient setupServiceGrpcClient;
-
   @Override
   public long getDelegateGroupCount(final String accountId, final String orgId, final String projectId) {
-    return setupServiceGrpcClient.getDelegateGroupsCount(accountId, orgId, projectId);
+    return 0;
   }
 }
