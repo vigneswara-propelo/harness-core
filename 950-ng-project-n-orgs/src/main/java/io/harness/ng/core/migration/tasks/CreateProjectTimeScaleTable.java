4fa799bc8baac3f548c91eb99c21b45a1cdd98e7
Abhinav Mittal <abhinav.mittal@harness.io>
2021-08-11 13:44:02 +0530

    [CDC-14212]: Migrations extend NGTimescaleAbstract class (#26422)

--- 950-ng-project-n-orgs/src/main/java/io/harness/ng/core/migration/tasks/CreateProjectTimeScaleTable.java
+++ 950-ng-project-n-orgs/src/main/java/io/harness/ng/core/migration/tasks/CreateProjectTimeScaleTable.java
@@ -1,46 +1,15 @@
 package io.harness.ng.core.migration.tasks;
 
-import io.harness.migration.NGMigration;
-import io.harness.timescaledb.TimeScaleDBService;
+import io.harness.annotations.dev.HarnessTeam;
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.migration.timescale.NGAbstractTimeScaleMigration;
 
-import com.google.inject.Inject;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.sql.Connection;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.ibatis.jdbc.ScriptRunner;
 
 @Slf4j
-public class CreateProjectTimeScaleTable implements NGMigration {
-  @Inject TimeScaleDBService timeScaleDBService;
-
-  @Override
-  public void migrate() {
-    if (timeScaleDBService.isValid()) {
-      try (Connection connection = timeScaleDBService.getDBConnection()) {
-        runMigration(connection, getFileName());
-      } catch (Exception e) {
-        log.error("Failed to run instance rename migration on db", e);
-      }
-    } else {
-      log.info("TIMESCALEDBSERVICE NOT AVAILABLE");
-    }
-  }
-
-  private String getFileName() {
+@OwnedBy(HarnessTeam.PIPELINE)
+public class CreateProjectTimeScaleTable extends NGAbstractTimeScaleMigration {
+  public String getFileName() {
     return "timescale/create_project_table.sql";
-  }
-
-  private void runMigration(Connection connection, String name) {
-    InputStream inputstream = getClass().getClassLoader().getResourceAsStream(name);
-    if (inputstream == null) {
-      log.warn("Skipping migration {} as script not found", name);
-      return;
-    }
-    InputStreamReader inputStreamReader = new InputStreamReader(inputstream, StandardCharsets.UTF_8);
-    ScriptRunner scriptRunner = new ScriptRunner(connection);
-    scriptRunner.setStopOnError(true);
-    scriptRunner.runScript(inputStreamReader);
   }
 }
