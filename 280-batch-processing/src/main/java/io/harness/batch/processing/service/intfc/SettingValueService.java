d93802c908ecae4c55073ed8c45c9cb55bd8f9e5
Amit Jambure <amit.jambure@harness.io>
2021-08-23 16:49:12 +0530

    [CCM-4936]: Batch: Delete unused classes. (#26804)

--- 280-batch-processing/src/main/java/io/harness/batch/processing/service/intfc/SettingValueService.java
+++
@@ -1,7 +0,0 @@
-package io.harness.batch.processing.service.intfc;
-
-import software.wings.settings.SettingValue;
-
-public interface SettingValueService {
-  SettingValue getSettingValueService(String settingId);
-}
