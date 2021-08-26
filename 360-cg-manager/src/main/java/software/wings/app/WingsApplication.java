6dbda762ac3c838d70ea82b196c6b507a9e76d88
Jenny James <jenny.james@harness.io>
2021-08-25 19:57:40 -0700

    [DEL-2374]:  Make waitNotifyEngine common for manager and dms (#26912)
    * dummy
    
    * [DEL-2374]: Make waitNotifyEngine common for manager and dms

--- 360-cg-manager/src/main/java/software/wings/app/WingsApplication.java
+++ 360-cg-manager/src/main/java/software/wings/app/WingsApplication.java
@@ -447,6 +447,7 @@ public class WingsApplication extends Application<MainConfiguration> {
       registerManagedBeansManager(configuration, environment, injector);
     }
 
+    registerWaitEnginePublishers(injector);
     if (isManager()) {
       registerQueueListeners(injector);
     }
@@ -939,8 +940,6 @@ public class WingsApplication extends Application<MainConfiguration> {
 
   private void registerQueueListeners(Injector injector) {
     log.info("Initializing queue listeners...");
-
-    registerWaitEnginePublishers(injector);
 
     QueueListenerController queueListenerController = injector.getInstance(QueueListenerController.class);
     EventListener genericEventListener =
