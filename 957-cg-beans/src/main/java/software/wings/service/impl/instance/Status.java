cc3820c90047573319aaa368636c387f7a927f54
George Georgiev <george@harness.io>
2021-09-03 11:28:01 -0700

    [BT-322]: move the coverage properties to a separate file (#27110)
    * [BT-322]: move the coverage properties to a separate file

--- 957-cg-beans/src/main/java/software/wings/service/impl/instance/Status.java
+++ 957-cg-beans/src/main/java/software/wings/service/impl/instance/Status.java
@@ -2,9 +2,7 @@ package software.wings.service.impl.instance;
 
 import static io.harness.annotations.dev.HarnessTeam.PL;
 
-import io.harness.annotations.dev.HarnessModule;
 import io.harness.annotations.dev.OwnedBy;
-import io.harness.annotations.dev.TargetModule;
 
 import lombok.Builder;
 import lombok.Value;
@@ -12,7 +10,6 @@ import lombok.Value;
 @Value
 @Builder
 @OwnedBy(PL)
-@TargetModule(HarnessModule._957_CG_BEANS)
 public class Status {
   boolean success;
   String errorMessage;
