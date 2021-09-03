9543d6a61b1d2214aa823e6ff0ea5b7349b189c7
Brijesh Dhakar <brijesh.dhakar@harness.io>
2021-09-03 15:45:56 +0530

    [PIE-1202]: adding notification template for ms teams (#27096)
    * [PIE-1202]: adding sample template for pipeline events for msteams
    
    * [PIE-1202]: adding notification template for ms teams
    
    * [PIE-1202]: updating template
    
    * [PIE-1202]: fixing aeriform check

--- 884-pms-commons/src/main/java/io/harness/plancreator/steps/TaskSelectorYaml.java
+++ 884-pms-commons/src/main/java/io/harness/plancreator/steps/TaskSelectorYaml.java
@@ -1,9 +1,7 @@
 package io.harness.plancreator.steps;
 
-import io.harness.annotations.dev.HarnessModule;
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
-import io.harness.annotations.dev.TargetModule;
 import io.harness.delegate.TaskSelector;
 
 import java.util.List;
@@ -11,8 +9,6 @@ import java.util.stream.Collectors;
 import lombok.Data;
 
 @Data
-// TODO this should go to yaml commons
-@TargetModule(HarnessModule._884_PMS_COMMONS)
 @OwnedBy(HarnessTeam.PIPELINE)
 public class TaskSelectorYaml {
   String delegateSelectors;
