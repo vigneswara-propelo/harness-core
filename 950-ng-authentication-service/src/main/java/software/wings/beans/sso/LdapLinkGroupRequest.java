079512e653cd3eff23fa458ba1835826c8c890c3
George Georgiev <george@harness.io>
2021-09-03 21:16:21 -0700

    [BT-322]: cleanup dependencies (#27091)
    * [BT-322]: cleanup dependencies

--- 950-ng-authentication-service/src/main/java/software/wings/beans/sso/LdapLinkGroupRequest.java
+++ 950-ng-authentication-service/src/main/java/software/wings/beans/sso/LdapLinkGroupRequest.java
@@ -1,20 +1,14 @@
 package software.wings.beans.sso;
 
-import static io.harness.annotations.dev.HarnessModule._950_NG_AUTHENTICATION_SERVICE;
 import static io.harness.annotations.dev.HarnessTeam.PL;
 
 import io.harness.annotations.dev.OwnedBy;
-import io.harness.annotations.dev.TargetModule;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.NotBlank;
 
-/**
- * @author Swapnil on 28/08/18
- */
 @OwnedBy(PL)
 @Data
-@TargetModule(_950_NG_AUTHENTICATION_SERVICE)
 public class LdapLinkGroupRequest {
   @NotBlank String ldapGroupDN;
   @NotBlank String ldapGroupName;
