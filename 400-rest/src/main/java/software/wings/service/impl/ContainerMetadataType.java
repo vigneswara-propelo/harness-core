645d6822a495e9a855095fbffb9f74702e0930e4
Yogesh Chauhan <yogesh.chauhan@harness.io>
2021-09-04 17:20:25 +0530

    [CDP-17369]: dummy commit to unblock release (#27113)
    * [CDP-17369]: dummy
    
    * [CDP-17369]: auto action
    
    * [CDP-17369]: auto action

--- 400-rest/src/main/java/software/wings/service/impl/ContainerMetadataType.java
+++
@@ -1,9 +0,0 @@
-package software.wings.service.impl;
-
-import static io.harness.annotations.dev.HarnessTeam.CDP;
-
-import io.harness.annotations.dev.HarnessModule;
-import io.harness.annotations.dev.OwnedBy;
-import io.harness.annotations.dev.TargetModule;
-
-@OwnedBy(CDP) @TargetModule(HarnessModule._957_CG_BEANS) public enum ContainerMetadataType { K8S }
---
+++ 957-cg-beans/src/main/java/software/wings/service/impl/ContainerMetadataType.java
@@ -0,0 +1,7 @@
+package software.wings.service.impl;
+
+import static io.harness.annotations.dev.HarnessTeam.CDP;
+
+import io.harness.annotations.dev.OwnedBy;
+
+@OwnedBy(CDP) public enum ContainerMetadataType { K8S }
