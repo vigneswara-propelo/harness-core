a972385c951aa85441b19908074d0fd0589df88a
Utkarsh Gupta <utkarsh.gupta@harness.io>
2021-09-06 15:34:18 +0530

    [BT-322]: Add back access-control aggregator dependencies (#27128)
    * [BT-322]: Add back access-control aggregator dependencies
    
    * [BT-322]: Fix aeriform issue
    
    * [BT-322]: Move the dependencies to runtime deps
    
    * [BT-322]: Move the dependencies to runtime deps

--- 890-sm-core/src/main/java/software/wings/service/intfc/security/ManagerDecryptionService.java
+++ 890-sm-core/src/main/java/software/wings/service/intfc/security/ManagerDecryptionService.java
@@ -2,9 +2,7 @@ package software.wings.service.intfc.security;
 
 import static io.harness.annotations.dev.HarnessTeam.PL;
 
-import io.harness.annotations.dev.HarnessModule;
 import io.harness.annotations.dev.OwnedBy;
-import io.harness.annotations.dev.TargetModule;
 import io.harness.security.encryption.EncryptableSettingWithEncryptionDetails;
 import io.harness.security.encryption.EncryptedDataDetail;
 
@@ -16,7 +14,6 @@ import java.util.List;
  * Created by rsingh on 6/7/18.
  */
 @OwnedBy(PL)
-@TargetModule(HarnessModule._890_SM_CORE)
 public interface ManagerDecryptionService {
   void decrypt(EncryptableSetting object, List<EncryptedDataDetail> encryptedDataDetails);
 
