2c7ce0509d1228156e37b5144ff410aa8ab87ca5
Deepak Patankar <deepak.patankar@harness.io>
2021-09-15 13:31:34 +0530

    [PL-20063]: Move the LdapConnectionConfig (#27347)

--- 400-rest/src/main/java/software/wings/helpers/ext/ldap/LdapConnectionConfig.java
+++ 400-rest/src/main/java/software/wings/helpers/ext/ldap/LdapConnectionConfig.java
@@ -1,5 +1,11 @@
 package software.wings.helpers.ext.ldap;
 
+import static io.harness.annotations.dev.HarnessModule._950_NG_AUTHENTICATION_SERVICE;
+
+import io.harness.annotations.dev.TargetModule;
+
+// Could not move it because this class is used in watcher
+@TargetModule(_950_NG_AUTHENTICATION_SERVICE)
 public interface LdapConnectionConfig {
   int getConnectTimeout();
 
