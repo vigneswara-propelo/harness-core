e30b874df50bcefd2ac3c9db2c974602c44f6337
Guna Chandrasekaran <guna.chandrasekaran@harness.io>
2021-09-21 11:50:05 -0700

    [ONP-260]: OSS build target changes (#27450)

--- cd-oss/cd-oss.sh
+++ cd-oss/cd-oss.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+cd ..
+BAZEL_BIN=`bazel info bazel-bin`
+
 bazel build `bazel query 'attr(tags, "oss", //...:*)'`
 
 bazel run //360-cg-manager/container:manager
@@ -10,10 +13,14 @@ bazel run //120-ng-manager/container:ng_manager
 
 bazel run //800-pipeline-service/container:pipeline_service
 
+export VERSION_FILE=build.properties
+export VERSION=`cat ${VERSION_FILE} | grep 'build.number=' | sed -e 's: *build.number=::g'`
+echo $VERSION
+
 mkdir -p destination/dist/delegate/
 mkdir -p destination/dist/watcher/
-cp -f ${HOME}/.bazel-dirs/bin/260-delegate/module_deploy.jar destination/dist/delegate/delegate-capsule.jar
-cp -f ${HOME}/.bazel-dirs/bin/960-watcher/module_deploy.jar destination/dist/watcher/watcher-capsule.jar
+cp -f ${BAZEL_BIN}/260-delegate/module_deploy.jar destination/dist/delegate/delegate-capsule.jar
+cp -f ${BAZEL_BIN}/960-watcher/module_deploy.jar destination/dist/watcher/watcher-capsule.jar
 chmod 744 dockerization/delegate-proxy/setup.sh
 ./dockerization/delegate-proxy/setup.sh
 bazel run //dockerization/delegate-proxy:delegate_proxy
