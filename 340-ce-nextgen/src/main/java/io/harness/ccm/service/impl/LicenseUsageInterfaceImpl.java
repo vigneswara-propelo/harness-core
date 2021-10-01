800c7bae4cbcd8ea7b8fb692048f1073ae6a615a
K Rohit Reddy <rohit.reddy@harness.io>
2021-10-01 10:56:32 +0530

    [CCM-5084]: Including Cluster Cost in License Utilisation, avoiding cost duplication (#27718)

--- 340-ce-nextgen/src/main/java/io/harness/ccm/service/impl/LicenseUsageInterfaceImpl.java
+++ 340-ce-nextgen/src/main/java/io/harness/ccm/service/impl/LicenseUsageInterfaceImpl.java
@@ -12,14 +12,15 @@ import io.harness.licensing.usage.interfaces.LicenseUsageInterface;
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.google.cloud.bigquery.BigQuery;
-import com.google.cloud.bigquery.Field;
-import com.google.cloud.bigquery.FieldList;
 import com.google.cloud.bigquery.FieldValue;
 import com.google.cloud.bigquery.FieldValueList;
 import com.google.cloud.bigquery.QueryJobConfiguration;
-import com.google.cloud.bigquery.Schema;
 import com.google.cloud.bigquery.TableResult;
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Multimap;
 import com.google.inject.Inject;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import lombok.AllArgsConstructor;
 import lombok.Value;
@@ -30,10 +31,17 @@ public class LicenseUsageInterfaceImpl implements LicenseUsageInterface<CELicens
   @Inject BigQueryService bigQueryService;
   @Inject CENextGenConfiguration configuration;
 
+  public static final String ACCOUNT_ID = "accountId";
+  public static final String COST = "cost";
+  public static final String MONTH = "month";
+  public static final String CLOUD_PROVIDER = "cloudProvider";
+  private static final Set<String> duplicateK8sCloudProviders = ImmutableSet.of("K8S_AWS", "K8S_AZURE", "K8S_GCP");
+
   public static final String DATA_SET_NAME = "CE_INTERNAL";
   public static final String TABLE_NAME = "costAggregated";
   public static final String QUERY_TEMPLATE =
-      "SELECT SUM(cost) FROM `%s` WHERE cloudProvider IN ('AWS','GCP','AZURE') AND day >= TIMESTAMP_MILLIS(%s) AND accountId = %s";
+      "SELECT SUM(cost), TIMESTAMP_TRUNC(day, month) AS month, cloudProvider FROM `%s` "
+      + "WHERE day >= TIMESTAMP_MILLIS(%s) AND accountId = %s GROUP BY month, cloudProvider";
 
   private final Cache<CacheKey, CELicenseUsageDTO> licenseUsageCache =
       Caffeine.newBuilder().expireAfterWrite(8, TimeUnit.HOURS).build();
@@ -80,26 +88,34 @@ public class LicenseUsageInterfaceImpl implements LicenseUsageInterface<CELicens
       return null;
     }
 
-    Schema schema = result.getSchema();
-    FieldList fields = schema.getFields();
     Long cost = 0L;
+    Multimap<String, String> multiMap = ArrayListMultimap.create();
+
+    // Will maintain a multi map with Month - [Cloud Provider] mapping
+    for (FieldValueList row : result.iterateAll()) {
+      multiMap.put(row.get(MONTH).getStringValue(), row.get(CLOUD_PROVIDER).getStringValue());
+    }
 
     for (FieldValueList row : result.iterateAll()) {
-      for (Field field : fields) {
-        switch (field.getType().getStandardType()) {
-          case FLOAT64:
-            cost = getNumericValue(row, field);
-            break;
-          default:
-            break;
+      String cloudProvider = row.get(CLOUD_PROVIDER).getStringValue();
+      if (duplicateK8sCloudProviders.contains(cloudProvider)) {
+        // K8S_AWS, K8S_AWS, K8S_AWS  => cloudProviderSlices[0] -> K8S | cloudProviderSlices[1] -> AWS/GCP/AZURE
+        String cloudProviderSlices[] = cloudProvider.split("_");
+        String month = row.get(MONTH).getStringValue();
+
+        // If Corresponding Cloud Provider Data is present -> We should not add the K8s Cost
+        if (!multiMap.containsEntry(month, cloudProviderSlices[1])) {
+          cost += getNumericValue(row, COST);
         }
+      } else {
+        cost += getNumericValue(row, COST);
       }
     }
-    return cost;
+    return Math.round(cost * 100L) / 100L;
   }
 
-  private long getNumericValue(FieldValueList row, Field field) {
-    FieldValue value = row.get(field.getName());
+  private long getNumericValue(FieldValueList row, String fieldName) {
+    FieldValue value = row.get(fieldName);
     return Math.round(value.getNumericValue().longValue() * 100L) / 100L;
   }
 }
