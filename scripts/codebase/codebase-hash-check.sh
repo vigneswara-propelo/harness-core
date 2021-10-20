ab686edff4d82a4c7cb9c6f202d993a56a44b856
Bojan Micovic <bojan.micovic@harness.io>
2021-10-20 13:10:39 +0200

    [DEL-2659]: Operationalize PR check for delegate backward compatibility. (#28155)
    Adding new logs into script and beautifying output for better understanding.

--- scripts/codebase/codebase-hash-check.sh
+++ scripts/codebase/codebase-hash-check.sh
@@ -1,38 +1,52 @@
 #!/usr/bin/env bash
 
+bold=$(tput bold)
+normal=$(tput sgr0)
+
 ALL_MODULES=`ls -a`
-MODULE=$1
+for MODULE in $@
+do
+      echo -e "${bold}\n##################################################\nChecking dependency hash for module: $MODULE\n${normal}"
 
-if [[ ! $ALL_MODULES[*] =~ $MODULE ]]; then
-    echo "Module not found in the project root."
-    exit 0
-fi
+      if [[ ! $ALL_MODULES[*] =~ $MODULE ]]; then
+          echo "Module not found in the project root."
+          exit 0
+      fi
 
-KRYO_DEPENDENCIES_FILE=`mktemp`
-PROTO_DEPENDENCIES_FILE=`mktemp`
 
-echo "Kryo dependencies file: " $KRYO_DEPENDENCIES_FILE
-echo "Proto dependencies file: " $PROTO_DEPENDENCIES_FILE
+      TEMP_DIR=`mktemp -d`
+      touch $TEMP_DIR/KryoDeps-$MODULE.text
+      touch $TEMP_DIR/ProtoDeps-$MODULE.text
 
-bazel query "deps(//$MODULE:module)" | grep -i "KryoRegistrar" | rev | cut -f 1 -d "/" | rev | cut -f 1 -d "." > $KRYO_DEPENDENCIES_FILE
-sh scripts/interface-hash/module-deps.sh $MODULE:module > $PROTO_DEPENDENCIES_FILE
+      KRYO_DEPENDENCIES_FILE=$TEMP_DIR/KryoDeps-$MODULE.text
+      PROTO_DEPENDENCIES_FILE=$TEMP_DIR/ProtoDeps-$MODULE.text
 
-CODEBASE_HASH_STRING=`bazel run "//001-microservice-intfc-tool:module" -- kryo-file=$KRYO_DEPENDENCIES_FILE proto-file=$PROTO_DEPENDENCIES_FILE ignore-json | grep "Codebase Hash:"`
-echo "Extracted hash string: " $CODEBASE_HASH_STRING
+      echo "Kryo dependencies file: " $KRYO_DEPENDENCIES_FILE
+      echo "Proto dependencies file: " $PROTO_DEPENDENCIES_FILE
 
-HASH=${CODEBASE_HASH_STRING:14:64}
-echo "New hash: " $HASH
+      bazel query "deps(//$MODULE:module)" | grep -i "KryoRegistrar" | rev | cut -f 1 -d "/" | rev | cut -f 1 -d "." > $KRYO_DEPENDENCIES_FILE
+      sh scripts/interface-hash/module-deps.sh $MODULE:module > $PROTO_DEPENDENCIES_FILE
 
-EXISTING_HASH=$(head -n 1 $MODULE/module-dependency.hash)
-echo "Existing hash: " $EXISTING_HASH
+      CODEBASE_HASH_STRING=`bazel run "//001-microservice-intfc-tool:module" -- kryo-file=$KRYO_DEPENDENCIES_FILE proto-file=$PROTO_DEPENDENCIES_FILE ignore-json | grep "Codebase Hash:"`
 
-if [[ $HASH != $EXISTING_HASH ]];
-then
-  echo "You have changed files which are being used for delegate task communication. Your change should be backward compatible."
-  echo "If the change is backward compatible replace the new hash $HASH in file $MODULE/module-dependency.hash"
-  echo "Please contact delegate team for backward compatibility check."
-  exit 1
-fi
+      HASH=${CODEBASE_HASH_STRING:14:64}
+      echo "New hash: " $HASH
 
-echo "Successful"
+      EXISTING_HASH=$(head -n 1 $MODULE/module-dependency.hash)
+      echo "Existing hash: " $EXISTING_HASH
+
+      if [[ $HASH != $EXISTING_HASH ]];
+      then
+        echo "${bold}You have changed files which are being used for delegate task communication. Your change should be backward compatible."
+        echo "If the change is backward compatible replace the new hash $HASH in file $MODULE/module-dependency.hash"
+        echo "Please start script from Portal project in your local environment for modules $@. e.g. ./scripts/codebase/codebase-hash-check.sh $@"
+        echo "Please contact delegate team for backward compatibility check."
+        echo -e "\nFailure.${normal}"
+        exit 1
+      fi
+      echo "There are no changes in dependencies for module: $MODULE"
+      echo -e "\n${bold}Successful${normal}"
+done
+
+echo -e "\n${bold}All checks are successful.${normal}"
 
