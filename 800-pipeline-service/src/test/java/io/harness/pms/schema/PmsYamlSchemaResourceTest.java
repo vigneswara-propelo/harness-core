17f65af1079e9ea930e44791705d07cf20039f64
Naman Verma <naman.verma@harness.io>
2021-11-09 13:46:38 +0530

    [PIE-1712]: Add tests for 800-pipeline-service (#28457)

---
+++ 800-pipeline-service/src/test/java/io/harness/pms/schema/PmsYamlSchemaResourceTest.java
@@ -0,0 +1,75 @@
+package io.harness.pms.schema;
+
+import static io.harness.EntityType.PIPELINES;
+import static io.harness.EntityType.TRIGGERS;
+import static io.harness.rule.OwnerRule.NAMAN;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import io.harness.EntityType;
+import io.harness.PipelineServiceTestBase;
+import io.harness.category.element.UnitTests;
+import io.harness.encryption.Scope;
+import io.harness.ng.core.dto.ResponseDTO;
+import io.harness.ngtriggers.service.NGTriggerYamlSchemaService;
+import io.harness.pms.pipeline.service.PMSYamlSchemaService;
+import io.harness.rule.Owner;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.TextNode;
+import javax.ws.rs.NotSupportedException;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+public class PmsYamlSchemaResourceTest extends PipelineServiceTestBase {
+  @InjectMocks PmsYamlSchemaResource pmsYamlSchemaResource;
+  @Mock PMSYamlSchemaService pmsYamlSchemaService;
+  @Mock NGTriggerYamlSchemaService ngTriggerYamlSchemaService;
+
+  String project = "project";
+  String org = "org";
+  String acc = "acc";
+  String id = "id";
+
+  @Test
+  @Owner(developers = NAMAN)
+  @Category(UnitTests.class)
+  public void testGetYamlSchema() {
+    JsonNode textNode1 = new TextNode("placeholder1");
+    doReturn(textNode1).when(pmsYamlSchemaService).getPipelineYamlSchema(acc, project, org, Scope.PROJECT);
+    ResponseDTO<JsonNode> yamlSchema1 =
+        pmsYamlSchemaResource.getYamlSchema(PIPELINES, project, org, Scope.PROJECT, id, acc);
+    assertThat(yamlSchema1.getData()).isEqualTo(textNode1);
+
+    JsonNode textNode2 = new TextNode("placeholder2");
+    doReturn(textNode2).when(ngTriggerYamlSchemaService).getTriggerYamlSchema(project, org, id, Scope.PROJECT);
+    ResponseDTO<JsonNode> yamlSchema2 =
+        pmsYamlSchemaResource.getYamlSchema(TRIGGERS, project, org, Scope.PROJECT, id, acc);
+    assertThat(yamlSchema2.getData()).isEqualTo(textNode2);
+
+    for (EntityType value : EntityType.values()) {
+      if (value.equals(PIPELINES) || value.equals(TRIGGERS)) {
+        continue;
+      }
+      assertThatThrownBy(() -> pmsYamlSchemaResource.getYamlSchema(value, project, org, Scope.PROJECT, id, acc))
+          .isInstanceOf(NotSupportedException.class);
+    }
+  }
+
+  @Test
+  @Owner(developers = NAMAN)
+  @Category(UnitTests.class)
+  public void testInvalidateYamlSchemaCache() {
+    doNothing().when(pmsYamlSchemaService).invalidateAllCache();
+    ResponseDTO<Boolean> responseDTO = pmsYamlSchemaResource.invalidateYamlSchemaCache();
+    assertThat(responseDTO.getData()).isTrue();
+    verify(pmsYamlSchemaService, times(1)).invalidateAllCache();
+  }
+}
---
+++ 800-pipeline-service/src/test/java/io/harness/pms/variables/HTTPStepVariableCreatorTest.java
@@ -0,0 +1,162 @@
+package io.harness.pms.variables;
+
+import static io.harness.annotations.dev.HarnessTeam.PIPELINE;
+import static io.harness.rule.OwnerRule.NAMAN;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import io.harness.CategoryTest;
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.category.element.UnitTests;
+import io.harness.exception.InvalidRequestException;
+import io.harness.pms.contracts.plan.YamlOutputProperties;
+import io.harness.pms.contracts.plan.YamlProperties;
+import io.harness.pms.yaml.YamlNode;
+import io.harness.pms.yaml.YamlUtils;
+import io.harness.rule.Owner;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@OwnedBy(PIPELINE)
+public class HTTPStepVariableCreatorTest extends CategoryTest {
+  String emptyVariablesAndHeaders = "pipeline:\n"
+      + "  step:\n"
+      + "    name: search\n"
+      + "    identifier: search\n"
+      + "    type: Http\n"
+      + "    timeout: 10m\n"
+      + "    spec:\n"
+      + "      url: uuid1\n"
+      + "      method: uuid2\n"
+      + "      headers: []\n"
+      + "      outputVariables: []\n"
+      + "      requestBody: uuid3\n"
+      + "      assertion: uuid4";
+
+  String bareMinimum = "pipeline:\n"
+      + "  step:\n"
+      + "    name: search\n"
+      + "    identifier: search\n"
+      + "    type: Http\n"
+      + "    timeout: 10m\n"
+      + "    spec:\n"
+      + "      url: uuid1\n"
+      + "      method: uuid2";
+
+  String headers = "pipeline:\n"
+      + "  step:\n"
+      + "    name: search\n"
+      + "    identifier: search\n"
+      + "    type: Http\n"
+      + "    timeout: 10m\n"
+      + "    spec:\n"
+      + "      url: uuid1\n"
+      + "      method: uuid2\n"
+      + "      headers:\n"
+      + "        - key: aq\n"
+      + "          value: uuid3\n"
+      + "        - key: aw\n"
+      + "          value: uuid4";
+
+  String missingHeaderKey = "pipeline:\n"
+      + "  step:\n"
+      + "    name: search\n"
+      + "    identifier: search\n"
+      + "    type: Http\n"
+      + "    timeout: 10m\n"
+      + "    spec:\n"
+      + "      url: uuid1\n"
+      + "      method: uuid2\n"
+      + "      headers:\n"
+      + "        - key: aq\n"
+      + "          value: uuid3\n"
+      + "        - value: uuid4";
+
+  String outputVariables = "pipeline:\n"
+      + "  step:\n"
+      + "    name: search\n"
+      + "    identifier: search\n"
+      + "    type: Http\n"
+      + "    timeout: 10m\n"
+      + "    spec:\n"
+      + "      url: uuid1\n"
+      + "      method: uuid2\n"
+      + "      outputVariables:\n"
+      + "        - name: m\n"
+      + "          value: uuid3\n"
+      + "          type: String\n"
+      + "          __uuid: eh";
+  @Test
+  @Owner(developers = NAMAN)
+  @Category(UnitTests.class)
+  public void testGetSupportedStepTypes() {
+    Set<String> supportedStepTypes = new HTTPStepVariableCreator().getSupportedStepTypes();
+    assertThat(supportedStepTypes).hasSize(1);
+    assertThat(supportedStepTypes).contains("Http");
+  }
+
+  @Test
+  @Owner(developers = NAMAN)
+  @Category(UnitTests.class)
+  public void testAddVariablesInComplexObject() throws IOException {
+    HTTPStepVariableCreator variableCreator = new HTTPStepVariableCreator();
+
+    Map<String, YamlProperties> yamlPropertiesMap = new HashMap<>();
+    Map<String, YamlOutputProperties> yamlOutputPropertiesMap = new HashMap<>();
+
+    YamlNode step1 = getStepNode(emptyVariablesAndHeaders);
+    variableCreator.addVariablesInComplexObject(yamlPropertiesMap, yamlOutputPropertiesMap, step1);
+    assertThat(yamlOutputPropertiesMap).hasSize(0);
+    assertThat(yamlPropertiesMap).hasSize(4);
+    assertThat(yamlPropertiesMap).containsKeys("uuid1", "uuid2", "uuid3", "uuid4");
+
+    yamlPropertiesMap.clear();
+    yamlOutputPropertiesMap.clear();
+    YamlNode step2 = getStepNode(bareMinimum);
+    variableCreator.addVariablesInComplexObject(yamlPropertiesMap, yamlOutputPropertiesMap, step2);
+    assertThat(yamlOutputPropertiesMap).hasSize(0);
+    assertThat(yamlPropertiesMap).hasSize(2);
+    assertThat(yamlPropertiesMap).containsKeys("uuid1", "uuid2");
+
+    yamlPropertiesMap.clear();
+    yamlOutputPropertiesMap.clear();
+    YamlNode step3 = getStepNode(headers);
+    variableCreator.addVariablesInComplexObject(yamlPropertiesMap, yamlOutputPropertiesMap, step3);
+    assertThat(yamlOutputPropertiesMap).hasSize(0);
+    assertThat(yamlPropertiesMap).hasSize(4);
+    assertThat(yamlPropertiesMap).containsKeys("uuid1", "uuid2", "aq", "aw");
+
+    yamlPropertiesMap.clear();
+    yamlOutputPropertiesMap.clear();
+    YamlNode step4 = getStepNode(missingHeaderKey);
+    assertThatThrownBy(
+        () -> variableCreator.addVariablesInComplexObject(yamlPropertiesMap, yamlOutputPropertiesMap, step4))
+        .isInstanceOf(InvalidRequestException.class);
+
+    yamlPropertiesMap.clear();
+    yamlOutputPropertiesMap.clear();
+    YamlNode step5 = getStepNode(outputVariables);
+    variableCreator.addVariablesInComplexObject(yamlPropertiesMap, yamlOutputPropertiesMap, step5);
+    assertThat(yamlOutputPropertiesMap).hasSize(1);
+    assertThat(yamlOutputPropertiesMap).containsKeys("uuid3");
+    assertThat(yamlPropertiesMap).hasSize(2);
+    assertThat(yamlPropertiesMap).containsKeys("uuid1", "uuid2");
+  }
+
+  private YamlNode getStepNode(String yaml) throws IOException {
+    return YamlUtils.readTree(yaml)
+        .getNode()
+        .getField("pipeline")
+        .getNode()
+        .getField("step")
+        .getNode()
+        .getField("spec")
+        .getNode();
+  }
+}
