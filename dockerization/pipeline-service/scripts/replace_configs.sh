7c885dcb4256a1b4c89444e18b79d7abd0eb0066
Harsh Jain <harsh.jain@harness.io>
2021-11-18 19:57:38 +0530

    [CI-2894]: add dockerization (#28693)
    * [CI-2894]: add dockerization
    
    * [CI-2894]: increase webhook processing threads

--- dockerization/pipeline-service/scripts/replace_configs.sh
+++ dockerization/pipeline-service/scripts/replace_configs.sh
@@ -393,5 +393,6 @@ replace_key_value pipelineEventConsumersConfig.partialPlanResponse.threads "$PAR
 replace_key_value pipelineEventConsumersConfig.createPlan.threads "$CREATE_PLAN_EVENT_CONSUMER_THREAD_COUNT"
 replace_key_value pipelineEventConsumersConfig.planNotify.threads "$PLAN_NOTIFY_EVENT_CONSUMER_THREAD_COUNT"
 replace_key_value pipelineEventConsumersConfig.pmsNotify.threads "$PMS_NOTIFY_EVENT_CONSUMER_THREAD_COUNT"
+replace_key_value pipelineEventConsumersConfig.webhookEvent.threads "$PMS_WEBHOOK_EVENT_CONSUMER_THREAD_COUNT"
 
 replace_key_value enforcementClientConfiguration.enforcementCheckEnabled "$ENFORCEMENT_CHECK_ENABLED"
