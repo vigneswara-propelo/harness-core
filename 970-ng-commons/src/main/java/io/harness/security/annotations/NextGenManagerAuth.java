8ea54a09307e0ec46b8714383889233d8f258c03
Karan Siwach <karan.siwach@harness.io>
2021-12-13 20:41:32 +0530

    [PL-21264]: inherited annotations (#29217)

--- 970-ng-commons/src/main/java/io/harness/security/annotations/NextGenManagerAuth.java
+++ 970-ng-commons/src/main/java/io/harness/security/annotations/NextGenManagerAuth.java
@@ -5,11 +5,13 @@ import static io.harness.annotations.dev.HarnessTeam.PL;
 import io.harness.annotations.dev.OwnedBy;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @OwnedBy(PL)
+@Inherited
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.TYPE})
 public @interface NextGenManagerAuth {}
