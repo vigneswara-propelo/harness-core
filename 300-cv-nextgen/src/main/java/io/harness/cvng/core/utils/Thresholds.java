02f1a7c94c3a3e3ef29b8cb168e1b6fa5c8dd34e
Deepak Chhikara <deepak.chhikara@harness.io>
2021-12-24 09:54:45 +0530

    [feat]: [CVNG-3980]:  adding EXHAUSTED state to te Error budget risk (#29542)
    * [CVNG-3980]: adding error budget risk

--- 300-cv-nextgen/src/main/java/io/harness/cvng/core/utils/Thresholds.java
+++ 300-cv-nextgen/src/main/java/io/harness/cvng/core/utils/Thresholds.java
@@ -2,7 +2,8 @@ package io.harness.cvng.core.utils;
 
 public class Thresholds {
   private Thresholds() {}
-  public static double HEALTHY_PERCENTAGE = 75;
-  public static double OBSERVE_PERCENTAGE = 50;
-  public static double NEED_ATTENTION_PERCENTAGE = 25;
+  public static final double HEALTHY_PERCENTAGE = 75;
+  public static final double OBSERVE_PERCENTAGE = 50;
+  public static final double NEED_ATTENTION_PERCENTAGE = 25;
+  public static final double UNHEALTHY_PERCENTAGE = 0;
 }
--- 300-cv-nextgen/src/main/java/io/harness/cvng/servicelevelobjective/beans/ErrorBudgetRisk.java
+++ 300-cv-nextgen/src/main/java/io/harness/cvng/servicelevelobjective/beans/ErrorBudgetRisk.java
@@ -6,17 +6,19 @@ public enum ErrorBudgetRisk {
   HEALTHY,
   OBSERVE,
   NEED_ATTENTION,
-  UNHEALTHY;
-  public static io.harness.cvng.servicelevelobjective.beans.ErrorBudgetRisk getFromPercentage(
-      double errorBudgetRemainingPercentage) {
+  UNHEALTHY,
+  EXHAUSTED;
+  public static ErrorBudgetRisk getFromPercentage(double errorBudgetRemainingPercentage) {
     if (errorBudgetRemainingPercentage >= Thresholds.HEALTHY_PERCENTAGE) {
-      return io.harness.cvng.servicelevelobjective.beans.ErrorBudgetRisk.HEALTHY;
+      return ErrorBudgetRisk.HEALTHY;
     } else if (errorBudgetRemainingPercentage >= Thresholds.NEED_ATTENTION_PERCENTAGE) {
-      return io.harness.cvng.servicelevelobjective.beans.ErrorBudgetRisk.NEED_ATTENTION;
+      return ErrorBudgetRisk.NEED_ATTENTION;
     } else if (errorBudgetRemainingPercentage >= Thresholds.OBSERVE_PERCENTAGE) {
-      return io.harness.cvng.servicelevelobjective.beans.ErrorBudgetRisk.OBSERVE;
+      return ErrorBudgetRisk.OBSERVE;
+    } else if (errorBudgetRemainingPercentage >= Thresholds.UNHEALTHY_PERCENTAGE) {
+      return ErrorBudgetRisk.UNHEALTHY;
     } else {
-      return io.harness.cvng.servicelevelobjective.beans.ErrorBudgetRisk.UNHEALTHY;
+      return ErrorBudgetRisk.EXHAUSTED;
     }
   }
 }
