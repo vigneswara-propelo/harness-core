8f37ca7839af96a3969cc97f4dc0d944048c9804
Mounik V V S S <mounik.velavalivenkata@harness.io>
2021-12-29 19:22:24 +0530

    fix: [CDC-14699]: Resolving output variables (#29498)
    * fix: [CDC-14699]: Resolving output vaiables
    
    * fix: [CDC-14699]: Changed Some files

--- 860-orchestration-steps/src/main/java/io/harness/steps/approval/ApprovalStepVariableCreator.java
+++ 860-orchestration-steps/src/main/java/io/harness/steps/approval/ApprovalStepVariableCreator.java
@@ -56,7 +56,7 @@ public class ApprovalStepVariableCreator extends GenericStepVariableCreator {
       YamlField uuidNode = variableNode.getField(YAMLFieldNameConstants.UUID);
       if (uuidNode != null) {
         String original = YAMLFieldNameConstants.SPEC + "." + YamlTypes.APPROVAL_INPUTS;
-        String replacement = YAMLFieldNameConstants.INPUT + "." + YamlTypes.APPROVAL_INPUTS;
+        String replacement = YAMLFieldNameConstants.OUTPUT + "." + YamlTypes.APPROVAL_INPUTS;
 
         String fqn = YamlUtils.getFullyQualifiedName(uuidNode.getNode()).replace(original, replacement);
         String localName =
