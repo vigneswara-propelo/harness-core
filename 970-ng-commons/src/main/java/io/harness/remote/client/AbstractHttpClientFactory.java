cd3d35e667ea8b3069050d0ceece978782ff841d
Deepak Patankar <deepak.patankar@harness.io>
2022-01-06 18:30:21 +0530

    [PL-21850]: Add the null checks (#29738)

--- 970-ng-commons/src/main/java/io/harness/remote/client/AbstractHttpClientFactory.java
+++ 970-ng-commons/src/main/java/io/harness/remote/client/AbstractHttpClientFactory.java
@@ -165,14 +165,18 @@ public abstract class AbstractHttpClientFactory {
       if (globalContextData != null) {
         final GitEntityInfo gitBranchInfo =
             ((GitSyncBranchContext) Objects.requireNonNull(globalContextData)).getGitBranchInfo();
-        HttpUrl url = request.url()
-                          .newBuilder()
-                          .addQueryParameter("repoIdentifier", gitBranchInfo.getYamlGitConfigId())
-                          .addQueryParameter("branch", gitBranchInfo.getBranch())
-                          .addQueryParameter(
-                              "getDefaultFromOtherRepo", String.valueOf(gitBranchInfo.isFindDefaultFromOtherRepos()))
-                          .build();
-        return chain.proceed(request.newBuilder().url(url).build());
+        if (gitBranchInfo != null && gitBranchInfo.getYamlGitConfigId() != null && gitBranchInfo.getBranch() != null) {
+          HttpUrl url = request.url()
+                            .newBuilder()
+                            .addQueryParameter("repoIdentifier", gitBranchInfo.getYamlGitConfigId())
+                            .addQueryParameter("branch", gitBranchInfo.getBranch())
+                            .addQueryParameter(
+                                "getDefaultFromOtherRepo", String.valueOf(gitBranchInfo.isFindDefaultFromOtherRepos()))
+                            .build();
+          return chain.proceed(request.newBuilder().url(url).build());
+        } else {
+          return chain.proceed(request);
+        }
       } else {
         return chain.proceed(request);
       }
