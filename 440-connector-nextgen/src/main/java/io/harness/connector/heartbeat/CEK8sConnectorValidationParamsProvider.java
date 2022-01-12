076c24e838eb37b2e1e9651a6bfb6f78fc23691b
Utsav Krishnan <utsav.krishnan@harness.io>
2022-01-12 22:09:13 +0530

    fix: [CCM-5380]: change referenced identifier without an explicit scope to Account level (#29898)

--- 440-connector-nextgen/src/main/java/io/harness/connector/heartbeat/CEK8sConnectorValidationParamsProvider.java
+++ 440-connector-nextgen/src/main/java/io/harness/connector/heartbeat/CEK8sConnectorValidationParamsProvider.java
@@ -13,6 +13,7 @@ import io.harness.connector.ConnectorResponseDTO;
 import io.harness.connector.services.ConnectorService;
 import io.harness.delegate.beans.connector.ConnectorValidationParams;
 import io.harness.delegate.beans.connector.cek8s.CEKubernetesClusterConfigDTO;
+import io.harness.encryption.Scope;
 import io.harness.exception.InvalidRequestException;
 import io.harness.utils.IdentifierRefHelper;
 
@@ -22,7 +23,7 @@ import java.util.Optional;
 import javax.validation.constraints.NotNull;
 
 public class CEK8sConnectorValidationParamsProvider extends K8sConnectorValidationParamsProvider {
-  @Inject @Named("defaultConnectorService") ConnectorService connectorService;
+  @Inject @Named("defaultConnectorService") private ConnectorService connectorService;
 
   @Override
   public ConnectorValidationParams getConnectorValidationParams(ConnectorInfoDTO connectorInfoDTO, String connectorName,
@@ -43,10 +44,26 @@ public class CEK8sConnectorValidationParamsProvider extends K8sConnectorValidati
 
   private Optional<ConnectorInfoDTO> getReferencedConnectorConfig(@NotNull String scopedConnectorIdentifier,
       String accountIdentifier, String orgIdentifier, String projectIdentifier) {
+    final String sanitizedScopedConnectorIdentifier = sanitizeK8sConnectorScope(scopedConnectorIdentifier);
+
     IdentifierRef connectorRef = IdentifierRefHelper.getIdentifierRef(
-        scopedConnectorIdentifier, accountIdentifier, orgIdentifier, projectIdentifier);
+        sanitizedScopedConnectorIdentifier, accountIdentifier, orgIdentifier, projectIdentifier);
     Optional<ConnectorResponseDTO> connectorResponseDTO = connectorService.get(connectorRef.getAccountIdentifier(),
         connectorRef.getOrgIdentifier(), connectorRef.getProjectIdentifier(), connectorRef.getIdentifier());
     return connectorResponseDTO.map(ConnectorResponseDTO::getConnector);
+  }
+
+  /**
+   * Should return Account scoped connector identifier when the scope is not explicitly defined,
+   * since by default an identifier without an explicit scope is assumed at Project level.
+   */
+  @NotNull
+  private static String sanitizeK8sConnectorScope(@NotNull String scopedConnectorIdentifier) {
+    String[] scopedIdentifier = scopedConnectorIdentifier.split("\\.");
+    if (scopedIdentifier.length == 1) {
+      return String.format("%s.%s", Scope.ACCOUNT.getYamlRepresentation(), scopedConnectorIdentifier);
+    }
+
+    return scopedConnectorIdentifier;
   }
 }
---
+++ 440-connector-nextgen/src/test/java/io/harness/connector/heartbeat/CEK8sConnectorValidationParamsProviderTest.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright 2022 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
+ */
+
+package io.harness.connector.heartbeat;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.when;
+
+import io.harness.CategoryTest;
+import io.harness.category.element.UnitTests;
+import io.harness.connector.ConnectorInfoDTO;
+import io.harness.connector.ConnectorResponseDTO;
+import io.harness.connector.helper.EncryptionHelper;
+import io.harness.connector.services.ConnectorService;
+import io.harness.delegate.beans.connector.CEFeatures;
+import io.harness.delegate.beans.connector.ConnectorType;
+import io.harness.delegate.beans.connector.ConnectorValidationParams;
+import io.harness.delegate.beans.connector.cek8s.CEKubernetesClusterConfigDTO;
+import io.harness.delegate.beans.connector.k8Connector.KubernetesClusterConfigDTO;
+import io.harness.delegate.beans.connector.k8Connector.KubernetesCredentialDTO;
+import io.harness.delegate.beans.connector.k8Connector.KubernetesCredentialType;
+import io.harness.rule.Owner;
+import io.harness.rule.OwnerRule;
+
+import com.google.common.collect.ImmutableSet;
+import java.util.Collections;
+import java.util.Optional;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CEK8sConnectorValidationParamsProviderTest extends CategoryTest {
+  private static final String ACCOUNT_ID = "accountId";
+  private static final String CONNECTOR_NAME = "connectorName";
+  private static final String KUBERNETES_CONNECTOR_IDENTIFIER = "kubernetesConnectorIdentifier";
+  private static final String SCOPED_KUBERNETES_CONNECTOR_IDENTIFIER = "account." + KUBERNETES_CONNECTOR_IDENTIFIER;
+
+  @Mock private ConnectorService connectorService;
+  @Mock private EncryptionHelper encryptionHelper;
+  @InjectMocks private CEK8sConnectorValidationParamsProvider cek8sConnectorValidationParamsProvider;
+
+  @Test
+  @Owner(developers = OwnerRule.UTSAV)
+  @Category(UnitTests.class)
+  public void testAccountScopedConnectorIdentifier() throws Exception {
+    when(encryptionHelper.getEncryptionDetail(eq(null), eq(ACCOUNT_ID), eq(null), eq(null)))
+        .thenReturn(Collections.emptyList());
+
+    final ConnectorResponseDTO connectorResponseDTO = createCloudProviderK8sConnector();
+    when(connectorService.get(eq(ACCOUNT_ID), eq(null), eq(null), eq(KUBERNETES_CONNECTOR_IDENTIFIER)))
+        .thenReturn(Optional.of(connectorResponseDTO));
+
+    final ConnectorInfoDTO connectorInfoDTO =
+        createCCMK8sConnectorForReferencedConnector(SCOPED_KUBERNETES_CONNECTOR_IDENTIFIER);
+    ConnectorValidationParams connectorValidationParams =
+        cek8sConnectorValidationParamsProvider.getConnectorValidationParams(
+            connectorInfoDTO, CONNECTOR_NAME, ACCOUNT_ID, null, null);
+
+    assertThat(connectorValidationParams).isNotNull();
+    assertThat(connectorValidationParams.getConnectorType()).isEqualTo(ConnectorType.KUBERNETES_CLUSTER);
+    assertThat(connectorValidationParams.getConnectorName()).isEqualTo(CONNECTOR_NAME);
+  }
+
+  @Test
+  @Owner(developers = OwnerRule.UTSAV)
+  @Category(UnitTests.class)
+  public void testNotExplicitlyScopedConnectorIdentifier() throws Exception {
+    when(encryptionHelper.getEncryptionDetail(eq(null), eq(ACCOUNT_ID), eq(null), eq(null)))
+        .thenReturn(Collections.emptyList());
+
+    final ConnectorResponseDTO connectorResponseDTO = createCloudProviderK8sConnector();
+    when(connectorService.get(eq(ACCOUNT_ID), eq(null), eq(null), eq(KUBERNETES_CONNECTOR_IDENTIFIER)))
+        .thenReturn(Optional.of(connectorResponseDTO));
+
+    final ConnectorInfoDTO connectorInfoDTO =
+        createCCMK8sConnectorForReferencedConnector(KUBERNETES_CONNECTOR_IDENTIFIER);
+    ConnectorValidationParams connectorValidationParams =
+        cek8sConnectorValidationParamsProvider.getConnectorValidationParams(
+            connectorInfoDTO, CONNECTOR_NAME, ACCOUNT_ID, null, null);
+
+    assertThat(connectorValidationParams).isNotNull();
+    assertThat(connectorValidationParams.getConnectorType()).isEqualTo(ConnectorType.KUBERNETES_CLUSTER);
+    assertThat(connectorValidationParams.getConnectorName()).isEqualTo(CONNECTOR_NAME);
+  }
+
+  private static ConnectorResponseDTO createCloudProviderK8sConnector() {
+    final KubernetesClusterConfigDTO kubernetesClusterConfigDTO =
+        KubernetesClusterConfigDTO.builder()
+            .delegateSelectors(ImmutableSet.of("ce-dev"))
+            .credential(KubernetesCredentialDTO.builder()
+                            .kubernetesCredentialType(KubernetesCredentialType.INHERIT_FROM_DELEGATE)
+                            .build())
+            .build();
+
+    final ConnectorInfoDTO connectorInfoDTO =
+        ConnectorInfoDTO.builder()
+            .connectorType(ConnectorType.KUBERNETES_CLUSTER)
+            .identifier(CEK8sConnectorValidationParamsProviderTest.KUBERNETES_CONNECTOR_IDENTIFIER)
+            .connectorConfig(kubernetesClusterConfigDTO)
+            .build();
+
+    return ConnectorResponseDTO.builder().connector(connectorInfoDTO).build();
+  }
+
+  private static ConnectorInfoDTO createCCMK8sConnectorForReferencedConnector(String connectorRef) {
+    final CEKubernetesClusterConfigDTO ceKubernetesClusterConfigDTO =
+        CEKubernetesClusterConfigDTO.builder()
+            .connectorRef(connectorRef)
+            .featuresEnabled(Collections.singletonList(CEFeatures.VISIBILITY))
+            .build();
+
+    return ConnectorInfoDTO.builder()
+        .connectorType(ConnectorType.CE_KUBERNETES_CLUSTER)
+        .connectorConfig(ceKubernetesClusterConfigDTO)
+        .identifier("randomString_jhdfv")
+        .build();
+  }
+}
