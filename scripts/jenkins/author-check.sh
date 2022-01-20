cd0e32019b9673040105cebac7e7f0d4d78cd520
sandeepa-kv-jois <64277898+sandeepa-kv-jois@users.noreply.github.com>
2022-01-20 11:21:51 +0530

    fix: [ONP-311]: fix delegate-proxy nginx.conf changes for IPv6 support (#30074)

--- scripts/jenkins/author-check.sh
+++ scripts/jenkins/author-check.sh
@@ -219,6 +219,7 @@ git log -1000 --oneline --format='%aN <%aE>' | sort -u |\
     grep -iv "^Milos Vasic <milos.vasic@harness.io>$" |\
     grep -iv "^David Warren <david.warren@harness.io>$" |\
     grep -iv "^Shivam Negi <shivam.negi@harness.io>$" |\
+    grep -iv "^sandeepa-kv-jois <sandeepa.kv@harness.io>$" |\
     grep -iv "^Alexandru Ciofu <alexandru.ciofu@harness.io>$"` || :
 
 if [ ! -z "$UNKNOWN_USERS" ]
