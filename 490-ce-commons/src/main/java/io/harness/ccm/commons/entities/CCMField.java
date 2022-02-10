dd38b28a027f80e1d48b6408651f339d36a92744
Shubhanshu Verma <shubhanshu.verma@harness.io>
2022-02-10 17:08:58 +0530

    [feat]: [CCM-5510]: Add common query DTOs for customer facing apis (#30672)
    * [CCM-5367]: Added query DTOs
    
    * [CCM-5510]: Added aggreagtion and time filter
    
    * [CCM-5510]: Added sort

---
+++ 490-ce-commons/src/main/java/io/harness/ccm/commons/entities/CCMField.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2021 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Shield 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
+ */
+
+package io.harness.ccm.commons.entities;
+
+public enum CCMField {
+  PERSPECTIVE_ID,
+  WORKLOAD,
+  WORKLOAD_TYPE,
+  CLUSTER_ID,
+  CLUSTER_NAME,
+  NAMESPACE,
+  GCP_PRODUCT,
+  GCP_PROJECT,
+  GCP_SKU_ID,
+  GCP_SKU_DESCRIPTION,
+  AWS_ACCOUNT,
+  AWS_SERVICE,
+  CLOUD_PROVIDER,
+  STATUS,
+  ANOMALY_TIME,
+  ACTUAL_COST,
+  EXPECTED_COST,
+  COST_IMPACT,
+  ALL
+}
---
+++ 490-ce-commons/src/main/java/io/harness/ccm/commons/entities/CCMOperator.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2021 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Shield 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
+ */
+
+package io.harness.ccm.commons.entities;
+
+public enum CCMOperator {
+  NOT_IN,
+  IN,
+  EQUALS,
+  NOT_NULL,
+  NULL,
+  LIKE,
+  GREATER_THAN,
+  LESS_THAN,
+  GREATER_THAN_EQUALS_TO,
+  LESS_THAN_EQUALS_TO,
+  AFTER,
+  BEFORE
+}
