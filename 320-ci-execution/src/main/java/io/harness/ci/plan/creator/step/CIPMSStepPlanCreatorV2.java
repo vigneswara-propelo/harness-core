c4f19cea54620f223a0c3de2b9569af9a5b4c77b
Harsh Jain <harsh.jain@harness.io>
2022-02-15 11:51:12 +0530

    fix: [CI-3485]: fix failure strategy (#30764)

--- 320-ci-execution/src/main/java/io/harness/ci/plan/creator/step/CIPMSStepPlanCreatorV2.java
+++ 320-ci-execution/src/main/java/io/harness/ci/plan/creator/step/CIPMSStepPlanCreatorV2.java
@@ -7,9 +7,17 @@
 
 package io.harness.ci.plan.creator.step;
 
+import static io.harness.pms.yaml.YAMLFieldNameConstants.FAILURE_STRATEGIES;
 import static io.harness.pms.yaml.YAMLFieldNameConstants.ROLLBACK_STEPS;
+import static io.harness.pms.yaml.YAMLFieldNameConstants.STAGE;
+import static io.harness.pms.yaml.YAMLFieldNameConstants.STEP_GROUP;
 
+import io.harness.advisers.manualIntervention.ManualInterventionAdviserRollbackParameters;
+import io.harness.advisers.manualIntervention.ManualInterventionAdviserWithRollback;
 import io.harness.advisers.nextstep.NextStepAdviserParameters;
+import io.harness.advisers.retry.RetryAdviserRollbackParameters;
+import io.harness.advisers.retry.RetryAdviserWithRollback;
+import io.harness.advisers.rollback.OnFailRollbackAdviser;
 import io.harness.advisers.rollback.OnFailRollbackParameters;
 import io.harness.advisers.rollback.OnFailRollbackParameters.OnFailRollbackParametersBuilder;
 import io.harness.advisers.rollback.RollbackStrategy;
@@ -18,8 +26,11 @@ import io.harness.annotations.dev.OwnedBy;
 import io.harness.beans.steps.CIAbstractStepNode;
 import io.harness.beans.steps.CIStepInfo;
 import io.harness.data.structure.EmptyPredicate;
+import io.harness.exception.InvalidRequestException;
+import io.harness.govern.Switch;
 import io.harness.plancreator.PipelineServiceUtilPlanCreationConstants;
 import io.harness.plancreator.steps.AbstractStepPlanCreator;
+import io.harness.plancreator.steps.FailureStrategiesUtils;
 import io.harness.plancreator.steps.GenericPlanCreatorUtils;
 import io.harness.plancreator.steps.common.WithStepElementParameters;
 import io.harness.pms.contracts.advisers.AdviserObtainment;
@@ -29,6 +40,12 @@ import io.harness.pms.contracts.facilitators.FacilitatorObtainment;
 import io.harness.pms.contracts.facilitators.FacilitatorType;
 import io.harness.pms.execution.utils.SkipInfoUtils;
 import io.harness.pms.sdk.core.adviser.OrchestrationAdviserTypes;
+import io.harness.pms.sdk.core.adviser.abort.OnAbortAdviser;
+import io.harness.pms.sdk.core.adviser.abort.OnAbortAdviserParameters;
+import io.harness.pms.sdk.core.adviser.ignore.IgnoreAdviser;
+import io.harness.pms.sdk.core.adviser.ignore.IgnoreAdviserParameters;
+import io.harness.pms.sdk.core.adviser.marksuccess.OnMarkSuccessAdviser;
+import io.harness.pms.sdk.core.adviser.marksuccess.OnMarkSuccessAdviserParameters;
 import io.harness.pms.sdk.core.adviser.success.OnSuccessAdviserParameters;
 import io.harness.pms.sdk.core.plan.PlanNode;
 import io.harness.pms.sdk.core.plan.creation.beans.PlanCreationContext;
@@ -39,19 +56,30 @@ import io.harness.pms.timeout.AbsoluteSdkTimeoutTrackerParameters;
 import io.harness.pms.timeout.SdkTimeoutObtainment;
 import io.harness.pms.yaml.ParameterField;
 import io.harness.pms.yaml.YamlField;
+import io.harness.pms.yaml.YamlNode;
 import io.harness.pms.yaml.YamlUtils;
 import io.harness.timeout.trackers.absolute.AbsoluteTimeoutTrackerFactory;
 import io.harness.utils.TimeoutUtils;
 import io.harness.when.utils.RunInfoUtils;
+import io.harness.yaml.core.failurestrategy.FailureStrategyActionConfig;
+import io.harness.yaml.core.failurestrategy.FailureStrategyConfig;
+import io.harness.yaml.core.failurestrategy.NGFailureActionType;
+import io.harness.yaml.core.failurestrategy.manualintervention.ManualInterventionFailureActionConfig;
+import io.harness.yaml.core.failurestrategy.retry.RetryFailureActionConfig;
 import io.harness.yaml.core.timeout.Timeout;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.google.protobuf.ByteString;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @OwnedBy(HarnessTeam.CI)
 public abstract class CIPMSStepPlanCreatorV2<T extends CIAbstractStepNode> extends AbstractStepPlanCreator<T> {
@@ -103,7 +131,8 @@ public abstract class CIPMSStepPlanCreatorV2<T extends CIAbstractStepNode> exten
       isStepInsideRollback = true;
     }
 
-    List<AdviserObtainment> adviserObtainmentList = new ArrayList<>();
+    List<AdviserObtainment> adviserObtainmentList =
+        new ArrayList<>(getAdviserObtainmentForFailureStrategy(currentField, isStepInsideRollback));
 
     /*
      * Adding OnSuccess adviser if step is inside rollback section else adding NextStep adviser for when condition to
@@ -124,6 +153,112 @@ public abstract class CIPMSStepPlanCreatorV2<T extends CIAbstractStepNode> exten
 
     return adviserObtainmentList;
   }
+
+  protected List<AdviserObtainment> getAdviserObtainmentForFailureStrategy(
+      YamlField currentField, boolean isStepInsideRollback) {
+    List<AdviserObtainment> adviserObtainmentList = new ArrayList<>();
+    List<FailureStrategyConfig> stageFailureStrategies =
+        getFieldFailureStrategies(currentField, STAGE, isStepInsideRollback);
+    List<FailureStrategyConfig> stepGroupFailureStrategies =
+        getFieldFailureStrategies(currentField, STEP_GROUP, isStepInsideRollback);
+    List<FailureStrategyConfig> stepFailureStrategies = getFailureStrategies(currentField.getNode());
+
+    Map<FailureStrategyActionConfig, Collection<FailureType>> actionMap;
+    FailureStrategiesUtils.priorityMergeFailureStrategies(
+        stepFailureStrategies, stepGroupFailureStrategies, stageFailureStrategies);
+
+    actionMap = FailureStrategiesUtils.priorityMergeFailureStrategies(
+        stepFailureStrategies, stepGroupFailureStrategies, stageFailureStrategies);
+
+    for (Map.Entry<FailureStrategyActionConfig, Collection<FailureType>> entry : actionMap.entrySet()) {
+      FailureStrategyActionConfig action = entry.getKey();
+      Set<FailureType> failureTypes = new HashSet<>(entry.getValue());
+      NGFailureActionType actionType = action.getType();
+
+      String nextNodeUuid = null;
+      YamlField siblingField = GenericPlanCreatorUtils.obtainNextSiblingField(currentField);
+      // Check if step is in parallel section then dont have nextNodeUUid set.
+      if (siblingField != null && !GenericPlanCreatorUtils.checkIfStepIsInParallelSection(currentField)) {
+        nextNodeUuid = siblingField.getNode().getUuid();
+      }
+
+      AdviserObtainment.Builder adviserObtainmentBuilder = AdviserObtainment.newBuilder();
+      if (isStepInsideRollback) {
+        if (actionType == NGFailureActionType.STAGE_ROLLBACK || actionType == NGFailureActionType.STEP_GROUP_ROLLBACK) {
+          throw new InvalidRequestException("Step inside rollback section cannot have Rollback as failure strategy.");
+        }
+      }
+
+      switch (actionType) {
+        case IGNORE:
+          adviserObtainmentList.add(
+              adviserObtainmentBuilder.setType(IgnoreAdviser.ADVISER_TYPE)
+                  .setParameters(ByteString.copyFrom(kryoSerializer.asBytes(IgnoreAdviserParameters.builder()
+                                                                                .applicableFailureTypes(failureTypes)
+                                                                                .nextNodeId(nextNodeUuid)
+                                                                                .build())))
+                  .build());
+          break;
+        case RETRY:
+          RetryFailureActionConfig retryAction = (RetryFailureActionConfig) action;
+          FailureStrategiesUtils.validateRetryFailureAction(retryAction);
+          ParameterField<Integer> retryCount = retryAction.getSpecConfig().getRetryCount();
+          FailureStrategyActionConfig actionUnderRetry = retryAction.getSpecConfig().getOnRetryFailure().getAction();
+          adviserObtainmentList.add(getRetryAdviserObtainment(failureTypes, nextNodeUuid, adviserObtainmentBuilder,
+              retryAction, retryCount, actionUnderRetry, currentField));
+          break;
+        case MARK_AS_SUCCESS:
+          adviserObtainmentList.add(
+              adviserObtainmentBuilder.setType(OnMarkSuccessAdviser.ADVISER_TYPE)
+                  .setParameters(ByteString.copyFrom(kryoSerializer.asBytes(OnMarkSuccessAdviserParameters.builder()
+                                                                                .applicableFailureTypes(failureTypes)
+                                                                                .nextNodeId(nextNodeUuid)
+                                                                                .build())))
+                  .build());
+
+          break;
+        case ABORT:
+          adviserObtainmentList.add(
+              adviserObtainmentBuilder.setType(OnAbortAdviser.ADVISER_TYPE)
+                  .setParameters(ByteString.copyFrom(kryoSerializer.asBytes(
+                      OnAbortAdviserParameters.builder().applicableFailureTypes(failureTypes).build())))
+                  .build());
+          break;
+        case STAGE_ROLLBACK:
+          OnFailRollbackParameters rollbackParameters =
+              getRollbackParameters(currentField, failureTypes, RollbackStrategy.STAGE_ROLLBACK);
+          adviserObtainmentList.add(adviserObtainmentBuilder.setType(OnFailRollbackAdviser.ADVISER_TYPE)
+                                        .setParameters(ByteString.copyFrom(kryoSerializer.asBytes(rollbackParameters)))
+                                        .build());
+          break;
+        case MANUAL_INTERVENTION:
+          ManualInterventionFailureActionConfig actionConfig = (ManualInterventionFailureActionConfig) action;
+          FailureStrategiesUtils.validateManualInterventionFailureAction(actionConfig);
+          FailureStrategyActionConfig actionUnderManualIntervention =
+              actionConfig.getSpecConfig().getOnTimeout().getAction();
+          adviserObtainmentList.add(getManualInterventionAdviserObtainment(
+              failureTypes, adviserObtainmentBuilder, actionConfig, actionUnderManualIntervention));
+          break;
+        default:
+          Switch.unhandled(actionType);
+      }
+    }
+    return adviserObtainmentList;
+  }
+
+  protected AdviserObtainment getManualInterventionAdviserObtainment(Set<FailureType> failureTypes,
+      AdviserObtainment.Builder adviserObtainmentBuilder, ManualInterventionFailureActionConfig actionConfig,
+      FailureStrategyActionConfig actionUnderManualIntervention) {
+    return adviserObtainmentBuilder.setType(ManualInterventionAdviserWithRollback.ADVISER_TYPE)
+        .setParameters(ByteString.copyFrom(kryoSerializer.asBytes(
+            ManualInterventionAdviserRollbackParameters.builder()
+                .applicableFailureTypes(failureTypes)
+                .timeoutAction(GenericPlanCreatorUtils.toRepairAction(actionUnderManualIntervention))
+                .timeout((int) TimeoutUtils.getTimeoutInSeconds(actionConfig.getSpecConfig().getTimeout(), 0))
+                .build())))
+        .build();
+  }
+
   private AdviserObtainment getNextStepAdviserObtainment(YamlField currentField) {
     if (currentField != null && currentField.getNode() != null) {
       if (GenericPlanCreatorUtils.checkIfStepIsInParallelSection(currentField)) {
@@ -139,6 +274,27 @@ public abstract class CIPMSStepPlanCreatorV2<T extends CIAbstractStepNode> exten
       }
     }
     return null;
+  }
+
+  protected AdviserObtainment getRetryAdviserObtainment(Set<FailureType> failureTypes, String nextNodeUuid,
+      AdviserObtainment.Builder adviserObtainmentBuilder, RetryFailureActionConfig retryAction,
+      ParameterField<Integer> retryCount, FailureStrategyActionConfig actionUnderRetry, YamlField currentField) {
+    return adviserObtainmentBuilder.setType(RetryAdviserWithRollback.ADVISER_TYPE)
+        .setParameters(ByteString.copyFrom(kryoSerializer.asBytes(
+            RetryAdviserRollbackParameters.builder()
+                .applicableFailureTypes(failureTypes)
+                .nextNodeId(nextNodeUuid)
+                .repairActionCodeAfterRetry(GenericPlanCreatorUtils.toRepairAction(actionUnderRetry))
+                .retryCount(retryCount.getValue())
+                .strategyToUuid(getRollbackStrategyMap(currentField))
+                .waitIntervalList(retryAction.getSpecConfig()
+                                      .getRetryIntervals()
+                                      .getValue()
+                                      .stream()
+                                      .map(s -> (int) TimeoutUtils.getTimeoutInSeconds(s, 0))
+                                      .collect(Collectors.toList()))
+                .build())))
+        .build();
   }
 
   private AdviserObtainment getOnSuccessAdviserObtainment(YamlField currentField) {
@@ -196,6 +352,37 @@ public abstract class CIPMSStepPlanCreatorV2<T extends CIAbstractStepNode> exten
     rollbackParametersBuilder.strategy(rollbackStrategy);
     rollbackParametersBuilder.strategyToUuid(getRollbackStrategyMap(currentField));
     return rollbackParametersBuilder.build();
+  }
+
+  private List<FailureStrategyConfig> getFieldFailureStrategies(
+      YamlField currentField, String fieldName, boolean isStepInsideRollback) {
+    YamlNode fieldNode = YamlUtils.getGivenYamlNodeFromParentPath(currentField.getNode(), fieldName);
+    if (isStepInsideRollback && fieldNode != null) {
+      // Check if found fieldNode is within rollbackSteps section
+      YamlNode rollbackNode = YamlUtils.findParentNode(fieldNode, ROLLBACK_STEPS);
+      if (rollbackNode == null) {
+        return Collections.emptyList();
+      }
+    }
+    if (fieldNode != null) {
+      return getFailureStrategies(fieldNode);
+    }
+    return Collections.emptyList();
+  }
+
+  private List<FailureStrategyConfig> getFailureStrategies(YamlNode node) {
+    YamlField failureStrategy = node.getField(FAILURE_STRATEGIES);
+    List<FailureStrategyConfig> failureStrategyConfigs = null;
+
+    try {
+      if (failureStrategy != null) {
+        failureStrategyConfigs =
+            YamlUtils.read(failureStrategy.getNode().toString(), new TypeReference<List<FailureStrategyConfig>>() {});
+      }
+    } catch (IOException e) {
+      throw new InvalidRequestException("Invalid yaml", e);
+    }
+    return failureStrategyConfigs;
   }
 
   protected Map<RollbackStrategy, String> getRollbackStrategyMap(YamlField currentField) {
--- 330-ci-beans/src/main/java/io/harness/beans/steps/CIAbstractStepNode.java
+++ 330-ci-beans/src/main/java/io/harness/beans/steps/CIAbstractStepNode.java
@@ -10,9 +10,13 @@ package io.harness.beans.steps;
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.plancreator.steps.AbstractStepNode;
+import io.harness.yaml.core.failurestrategy.FailureStrategyConfig;
 
+import java.util.List;
 import lombok.Data;
 
 @OwnedBy(HarnessTeam.CI)
 @Data
-public abstract class CIAbstractStepNode extends AbstractStepNode {}
+public abstract class CIAbstractStepNode extends AbstractStepNode {
+  List<FailureStrategyConfig> failureStrategies;
+}
