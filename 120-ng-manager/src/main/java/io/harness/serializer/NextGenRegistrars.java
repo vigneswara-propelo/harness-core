fd48b88bc50d36a52fa9fe1df30ecfd5c67c16ed
Shivam Negi <shivam.negi@harness.io>
2022-03-09 13:48:35 +0530

    feat: [CDS-34693]: Added Yaml Schema support for Services (#31267)
    * [CDS-34693]: Added Yaml Schema support for Services
    
    * [CDS-34693]: removed the unused imports

--- 120-ng-manager/src/main/java/io/harness/serializer/NextGenRegistrars.java
+++ 120-ng-manager/src/main/java/io/harness/serializer/NextGenRegistrars.java
@@ -12,6 +12,7 @@ import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.morphia.MorphiaRegistrar;
 import io.harness.ng.core.dto.secrets.SecretRequestWrapper;
+import io.harness.ng.core.service.yaml.NGServiceConfig;
 import io.harness.serializer.morphia.FeedbackMorphiaRegistrars;
 import io.harness.serializer.morphia.InvitesMorphiaRegistrar;
 import io.harness.serializer.morphia.MockRoleAssignmentMorphiaRegistrar;
@@ -84,6 +85,13 @@ public class NextGenRegistrars {
                    .availableAtOrgLevel(true)
                    .availableAtAccountLevel(true)
                    .clazz(SecretRequestWrapper.class)
+                   .build())
+          .add(YamlSchemaRootClass.builder()
+                   .entityType(EntityType.SERVICE)
+                   .availableAtProjectLevel(true)
+                   .availableAtAccountLevel(true)
+                   .availableAtOrgLevel(true)
+                   .clazz(NGServiceConfig.class)
                    .build())
           .build();
 
