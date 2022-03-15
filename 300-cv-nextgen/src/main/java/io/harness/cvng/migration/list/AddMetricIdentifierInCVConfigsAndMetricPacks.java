a04064cb7eedaf19e5ec78054b96a0a70fde6ea3
Kanhaiya Rathi <kanhaiya.rathi@harness.io>
2022-03-15 18:44:09 +0530

    fix: [CVS-9515]: migration fix (#31468)
    * [CVS-9515]: migration fix
    
    * [CVS-9515]: disable query validation

--- 300-cv-nextgen/src/main/java/io/harness/cvng/migration/list/AddMetricIdentifierInCVConfigsAndMetricPacks.java
+++ 300-cv-nextgen/src/main/java/io/harness/cvng/migration/list/AddMetricIdentifierInCVConfigsAndMetricPacks.java
@@ -9,45 +9,73 @@ package io.harness.cvng.migration.list;
 
 import static io.harness.data.structure.EmptyPredicate.isEmpty;
 
+import io.harness.cvng.core.entities.CVConfig;
 import io.harness.cvng.core.entities.MetricCVConfig;
 import io.harness.cvng.core.entities.MetricPack;
+import io.harness.cvng.core.entities.MetricPack.MetricPackKeys;
 import io.harness.cvng.migration.CVNGMigration;
 import io.harness.cvng.migration.beans.ChecklistItem;
+import io.harness.persistence.HIterator;
 import io.harness.persistence.HPersistence;
 
 import com.google.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+import org.mongodb.morphia.query.Query;
 
+@Slf4j
 public class AddMetricIdentifierInCVConfigsAndMetricPacks implements CVNGMigration {
   @Inject private HPersistence hPersistence;
 
   @Override
   public void migrate() {
-    hPersistence.createQuery(MetricPack.class).asList().forEach(metricPack -> {
-      metricPack.getMetrics().forEach(metricDefinition -> {
-        if (isEmpty(metricDefinition.getIdentifier())) {
-          String identifier = metricDefinition.getName().replaceAll(" ", "_");
-          identifier = identifier.replaceAll("\\(", "");
-          identifier = identifier.replaceAll("\\)", "");
-          metricDefinition.setIdentifier(identifier);
-        }
-      });
-      hPersistence.save(metricPack);
-    });
+    log.info("Deleting Metric Packs for Old Custom Health data source type");
+    hPersistence.deleteOnServer(
+        hPersistence.createQuery(MetricPack.class).filter(MetricPackKeys.dataSourceType, "CUSTOM_HEALTH"));
+    log.info("Deleting cvCOnfigs for Old Custom Health data source type");
+    hPersistence.deleteOnServer(hPersistence.createQuery(CVConfig.class)
+                                    .disableValidation()
+                                    .filter("className", "io.harness.cvng.core.entities.CustomHealthCVConfig"));
 
-    hPersistence.createQuery(MetricCVConfig.class).asList().forEach(cvConfig -> {
-      if (cvConfig.getMetricPack() != null && cvConfig.getMetricPack().getMetrics() != null) {
-        cvConfig.getMetricPack().getMetrics().forEach(metricDefinition -> {
-          if (metricDefinition != null && isEmpty(metricDefinition.getIdentifier())) {
+    log.info("Adding identifiers for Metric Packs");
+    Query<MetricPack> metricPackQuery = hPersistence.createQuery(MetricPack.class);
+    try (HIterator<MetricPack> iterator = new HIterator<>(metricPackQuery.fetch())) {
+      while (iterator.hasNext()) {
+        MetricPack metricPack = iterator.next();
+
+        metricPack.getMetrics().forEach(metricDefinition -> {
+          if (isEmpty(metricDefinition.getIdentifier())) {
             String identifier = metricDefinition.getName().replaceAll(" ", "_");
             identifier = identifier.replaceAll("\\(", "");
             identifier = identifier.replaceAll("\\)", "");
             metricDefinition.setIdentifier(identifier);
           }
         });
+        hPersistence.save(metricPack);
+        log.info("Identifier updation for metric pack {}, {}", metricPack.getProjectIdentifier(),
+            metricPack.getIdentifier());
       }
+    }
 
-      hPersistence.save(cvConfig);
-    });
+    log.info("Adding identifiers for metric pack in cvconfigs");
+    Query<MetricCVConfig> metricCVConfigQuery = hPersistence.createQuery(MetricCVConfig.class);
+    try (HIterator<MetricCVConfig> iterator = new HIterator<>(metricCVConfigQuery.fetch())) {
+      while (iterator.hasNext()) {
+        MetricCVConfig cvConfig = iterator.next();
+
+        if (cvConfig.getMetricPack() != null && cvConfig.getMetricPack().getMetrics() != null) {
+          cvConfig.getMetricPack().getMetrics().forEach(metricDefinition -> {
+            if (metricDefinition != null && isEmpty(metricDefinition.getIdentifier())) {
+              String identifier = metricDefinition.getName().replaceAll(" ", "_");
+              identifier = identifier.replaceAll("\\(", "");
+              identifier = identifier.replaceAll("\\)", "");
+              metricDefinition.setIdentifier(identifier);
+            }
+          });
+        }
+        hPersistence.save(cvConfig);
+        log.info("Identifier updation for cvConfig {}, {}", cvConfig.getProjectIdentifier(), cvConfig.getUuid());
+      }
+    }
   }
 
   @Override
--- 300-cv-nextgen/src/main/java/io/harness/cvng/migration/list/AddMetricIdentifierToTimeSeriesThreshold.java
+++ 300-cv-nextgen/src/main/java/io/harness/cvng/migration/list/AddMetricIdentifierToTimeSeriesThreshold.java
@@ -10,26 +10,44 @@ package io.harness.cvng.migration.list;
 import static io.harness.data.structure.EmptyPredicate.isEmpty;
 
 import io.harness.cvng.core.entities.TimeSeriesThreshold;
+import io.harness.cvng.core.entities.TimeSeriesThreshold.TimeSeriesThresholdKeys;
 import io.harness.cvng.migration.CVNGMigration;
 import io.harness.cvng.migration.beans.ChecklistItem;
+import io.harness.persistence.HIterator;
 import io.harness.persistence.HPersistence;
 
 import com.google.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+import org.mongodb.morphia.query.Query;
 
+@Slf4j
 public class AddMetricIdentifierToTimeSeriesThreshold implements CVNGMigration {
   @Inject private HPersistence hPersistence;
 
   @Override
   public void migrate() {
-    hPersistence.createQuery(TimeSeriesThreshold.class).asList().forEach(timeSeriesThreshold -> {
-      if (isEmpty(timeSeriesThreshold.getMetricIdentifier())) {
-        String identifier = timeSeriesThreshold.getMetricName().replaceAll(" ", "_");
-        identifier = identifier.replaceAll("\\(", "");
-        identifier = identifier.replaceAll("\\)", "");
-        timeSeriesThreshold.setMetricIdentifier(identifier);
+    log.info("Deleting TimeSeries Thresholds for Old Custom Health data source type");
+    hPersistence.deleteOnServer(hPersistence.createQuery(TimeSeriesThreshold.class)
+                                    .filter(TimeSeriesThresholdKeys.dataSourceType, "CUSTOM_HEALTH"));
+
+    log.info("Adding identifiers for Time Series Thresholds");
+    Query<TimeSeriesThreshold> timeSeriesThresholdQuery = hPersistence.createQuery(TimeSeriesThreshold.class);
+    try (HIterator<TimeSeriesThreshold> iterator = new HIterator<>(timeSeriesThresholdQuery.fetch())) {
+      while (iterator.hasNext()) {
+        TimeSeriesThreshold timeSeriesThreshold = iterator.next();
+
+        if (isEmpty(timeSeriesThreshold.getMetricIdentifier())) {
+          String identifier = timeSeriesThreshold.getMetricName().replaceAll(" ", "_");
+          identifier = identifier.replaceAll("\\(", "");
+          identifier = identifier.replaceAll("\\)", "");
+          timeSeriesThreshold.setMetricIdentifier(identifier);
+        }
+        hPersistence.save(timeSeriesThreshold);
+
+        log.info("Identifier updation for time series thresholds {}, {}", timeSeriesThreshold.getProjectIdentifier(),
+            timeSeriesThreshold.getUuid());
       }
-      hPersistence.save(timeSeriesThreshold);
-    });
+    }
   }
 
   @Override
