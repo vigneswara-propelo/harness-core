ad35534b4b86e63da8fa4eab6402fc9aaa677fe1
Mohit Garg <mohit.garg@harness.io>
2022-03-19 23:23:02 +0530

    feat: [PL-23889]: added feature flag for git sync project cleanup event (#31578)
    * [PL-23889]: added feature flag for git sync project cleanup event
    
    * [PL-23889]: added global try catch block to handle -ve cases

--- 136-git-sync-manager/src/main/java/io/harness/gitsync/common/events/GitSyncProjectCleanup.java
+++ 136-git-sync-manager/src/main/java/io/harness/gitsync/common/events/GitSyncProjectCleanup.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2022 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
+ */
+
 package io.harness.gitsync.common.events;
 
 import static io.harness.eventsframework.EventsFrameworkMetadataConstants.ACTION;
@@ -6,14 +13,17 @@ import static io.harness.eventsframework.EventsFrameworkMetadataConstants.ENTITY
 import static io.harness.eventsframework.EventsFrameworkMetadataConstants.PROJECT_ENTITY;
 
 import io.harness.NgAutoLogContext;
+import io.harness.account.AccountClient;
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
+import io.harness.beans.FeatureName;
 import io.harness.eventsframework.consumer.Message;
 import io.harness.eventsframework.entity_crud.project.ProjectEntityChangeDTO;
 import io.harness.exception.InvalidRequestException;
 import io.harness.gitsync.common.eventhandlers.GitSyncProjectCleanupHandler;
 import io.harness.logging.AutoLogContext;
 import io.harness.ng.core.event.MessageListener;
+import io.harness.remote.client.RestClientUtils;
 
 import com.google.inject.Inject;
 import com.google.protobuf.InvalidProtocolBufferException;
@@ -26,6 +36,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class GitSyncProjectCleanup implements MessageListener {
   private final GitSyncProjectCleanupHandler gitSyncProjectCleanupHandler;
+  private final AccountClient accountClient;
 
   @Override
   public boolean handleMessage(Message message) {
@@ -49,9 +60,20 @@ public class GitSyncProjectCleanup implements MessageListener {
       throw new InvalidRequestException(
           String.format("Exception in unpacking ProjectEntityChangeDTO for key %s", message.getId()), e);
     }
-    String action = message.getMessage().getMetadataMap().get(ACTION);
-    if (DELETE_ACTION.equals(action)) {
-      return processProjectDeleteEvent(projectEntityChangeDTO);
+
+    try {
+      if (!RestClientUtils.getResponse(accountClient.isFeatureFlagEnabled(
+              FeatureName.GIT_SYNC_PROJECT_CLEANUP.name(), projectEntityChangeDTO.getAccountIdentifier()))) {
+        return true;
+      }
+
+      String action = message.getMessage().getMetadataMap().get(ACTION);
+      if (DELETE_ACTION.equals(action)) {
+        return processProjectDeleteEvent(projectEntityChangeDTO);
+      }
+    } catch (Exception ex) {
+      log.error("Faced error while processing GitSyncProjectCleanup event: ", ex);
+      return false;
     }
     return true;
   }
