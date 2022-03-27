3572ab910b1a6ecb85705da9e84227a49f028aad
Meet Rathod <rathod.meetsatish@harness.io>
2022-03-27 22:00:33 +0530

    [fix]: [PL-21549]: Changed description of some fields in GitSyncConfigDTO (#31772)
    * [fix]: [PL-21549]: changed description of fields in GitSyncConfigDTO
    
    * [fix]: [PL-21549]: added space in trim message
    
    * [fix]: [PL-21549]: added space in trim message

--- 953-git-sync-commons/src/main/java/io/harness/gitsync/common/dtos/GitSyncConfigDTO.java
+++ 953-git-sync-commons/src/main/java/io/harness/gitsync/common/dtos/GitSyncConfigDTO.java
@@ -37,16 +37,19 @@ import org.hibernate.validator.constraints.NotEmpty;
 @ApiModel("GitSyncConfig")
 public class GitSyncConfigDTO {
   @Schema(description = GitSyncApiConstants.REPOID_PARAM_MESSAGE) @Trimmed @NotEmpty private String identifier;
-  @Schema(description = GitSyncApiConstants.REPO_NAME_PARAM_MESSAGE + GitSyncApiConstants.TRIM_LEADING_TRAILING_SPACES)
+  @Schema(description =
+              GitSyncApiConstants.REPO_NAME_PARAM_MESSAGE + " " + GitSyncApiConstants.TRIM_LEADING_TRAILING_SPACES)
   @NotEmpty
   private String name;
   @Schema(description = PROJECT_PARAM_MESSAGE) @Trimmed private String projectIdentifier;
   @Schema(description = ORG_PARAM_MESSAGE) @Trimmed private String orgIdentifier;
   @Schema(description = "Id of the Connector referenced in Git") @Trimmed @NotEmpty private String gitConnectorRef;
-  @Schema(description = GitSyncApiConstants.REPO_URL_PARAM_MESSAGE + GitSyncApiConstants.TRIM_LEADING_TRAILING_SPACES)
+  @Schema(
+      description = GitSyncApiConstants.REPO_URL_PARAM_MESSAGE + " " + GitSyncApiConstants.TRIM_LEADING_TRAILING_SPACES)
   @NotEmpty
   private String repo;
-  @Schema(description = GitSyncApiConstants.BRANCH_PARAM_MESSAGE + GitSyncApiConstants.TRIM_LEADING_TRAILING_SPACES)
+  @Schema(
+      description = GitSyncApiConstants.BRANCH_PARAM_MESSAGE + " " + GitSyncApiConstants.TRIM_LEADING_TRAILING_SPACES)
   @NotEmpty
   private String branch;
   @Schema(description = "Connector Type")
