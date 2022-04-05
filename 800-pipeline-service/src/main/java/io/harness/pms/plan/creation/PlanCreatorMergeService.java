cb6469e2887200d04b5564d0fade2b776f95201a
Sahil Hindwani <sahil.hindwani@harness.io>
2022-04-05 22:03:49 +0530

    feat: [PIE-3380]: Add retry and autoLogContext during plan creation (#32029)

--- 800-pipeline-service/src/main/java/io/harness/pms/plan/creation/PlanCreatorMergeService.java
+++ 800-pipeline-service/src/main/java/io/harness/pms/plan/creation/PlanCreatorMergeService.java
@@ -36,6 +36,7 @@ import io.harness.pms.exception.PmsExceptionUtils;
 import io.harness.pms.plan.creation.validator.PlanCreationValidator;
 import io.harness.pms.sdk.PmsSdkHelper;
 import io.harness.pms.utils.CompletableFutures;
+import io.harness.pms.utils.PmsGrpcClientUtils;
 import io.harness.pms.yaml.YamlField;
 import io.harness.pms.yaml.YamlUtils;
 import io.harness.waiter.WaitNotifyEngine;
@@ -297,19 +298,23 @@ public class PlanCreatorMergeService {
   private void executeDependenciesAsync(CompletableFutures<PlanCreationResponse> completableFutures,
       Map.Entry<String, PlanCreatorServiceInfo> serviceInfo, Dependencies batchDependency,
       Map<String, PlanCreationContextValue> contextMap) {
+    PlanCreationContextValue metadata = contextMap.get("metadata");
     completableFutures.supplyAsync(() -> {
-      try {
-        return serviceInfo.getValue().getPlanCreationClient().createPlan(
-            PlanCreationBlobRequest.newBuilder().setDeps(batchDependency).putAllContext(contextMap).build());
-      } catch (StatusRuntimeException ex) {
-        log.error(
-            String.format("Error connecting with service: [%s]. Is this service Running?", serviceInfo.getKey()), ex);
-        return PlanCreationResponse.newBuilder()
-            .setErrorResponse(
-                ErrorResponse.newBuilder()
-                    .addMessages(String.format("Error connecting with service: [%s]", serviceInfo.getKey()))
-                    .build())
-            .build();
+      try (AutoLogContext ignore = PlanCreatorUtils.autoLogContext(metadata.getMetadata(),
+               metadata.getAccountIdentifier(), metadata.getOrgIdentifier(), metadata.getProjectIdentifier())) {
+        try {
+          return PmsGrpcClientUtils.retryAndProcessException(serviceInfo.getValue().getPlanCreationClient()::createPlan,
+              PlanCreationBlobRequest.newBuilder().setDeps(batchDependency).putAllContext(contextMap).build());
+        } catch (StatusRuntimeException ex) {
+          log.error(
+              String.format("Error connecting with service: [%s]. Is this service Running?", serviceInfo.getKey()), ex);
+          return PlanCreationResponse.newBuilder()
+              .setErrorResponse(
+                  ErrorResponse.newBuilder()
+                      .addMessages(String.format("Error connecting with service: [%s]", serviceInfo.getKey()))
+                      .build())
+              .build();
+        }
       }
     });
   }
