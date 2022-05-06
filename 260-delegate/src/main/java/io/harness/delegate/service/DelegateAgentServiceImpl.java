391e08421683833249708288fe6b7bf933ba10e7
Raghvendra Singh <raghu@harness.io>
2022-05-06 11:19:49 -0700

    [fix]: [DEL-3961]: add log in case the rest call fails (#32880)

--- 260-delegate/src/main/java/io/harness/delegate/service/DelegateAgentServiceImpl.java
+++ 260-delegate/src/main/java/io/harness/delegate/service/DelegateAgentServiceImpl.java
@@ -966,6 +966,9 @@ public class DelegateAgentServiceImpl implements DelegateAgentService {
     try {
       response = call.execute();
       return response.body();
+    } catch (Exception e) {
+      log.error("error executing rest call", e);
+      throw e;
     } finally {
       if (response != null && !response.isSuccessful()) {
         String errorResponse = response.errorBody().string();
