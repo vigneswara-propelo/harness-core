4582b63d6ad33a6936a1a89221ca26b83bb792df
Jenny James <jenny.james@harness.io>
2022-05-15 17:24:40 -0700

    fix: [DEL-4076]: When delegate selectors is an empty array, the other selectors are being ignored (#33166)

--- 878-ng-common-utilities/src/main/java/io/harness/steps/StepUtils.java
+++ 878-ng-common-utilities/src/main/java/io/harness/steps/StepUtils.java
@@ -437,25 +437,26 @@ public class StepUtils {
         // Delegate Selector Precedence: 1)Step -> 2)stepGroup -> 3)Stage ->  4)Pipeline
 
         ParameterField<List<TaskSelectorYaml>> delegateSelectors = withDelegateSelector.fetchDelegateSelectors();
-        if (!ParameterField.isNull(withDelegateSelector.fetchDelegateSelectors())) {
+        if (!ParameterField.isNull(withDelegateSelector.fetchDelegateSelectors())
+            && !isEmpty(delegateSelectors.getValue())) {
           setOriginAndDelegateSelectors(delegateSelectors, withDelegateSelector, STEP);
           return;
         }
 
         delegateSelectors = delegateSelectorsFromFqn(ctx, STEP_GROUP);
-        if (!ParameterField.isNull(delegateSelectors)) {
+        if (!ParameterField.isNull(delegateSelectors) && !isEmpty(delegateSelectors.getValue())) {
           setOriginAndDelegateSelectors(delegateSelectors, withDelegateSelector, STEP_GROUP);
           return;
         }
 
         delegateSelectors = delegateSelectorsFromFqn(ctx, STAGE);
-        if (!ParameterField.isNull(delegateSelectors)) {
+        if (!ParameterField.isNull(delegateSelectors) && !isEmpty(delegateSelectors.getValue())) {
           setOriginAndDelegateSelectors(delegateSelectors, withDelegateSelector, STAGE);
           return;
         }
 
         delegateSelectors = delegateSelectorsFromFqn(ctx, YAMLFieldNameConstants.PIPELINE);
-        if (!ParameterField.isNull(delegateSelectors)) {
+        if (!ParameterField.isNull(delegateSelectors) && !isEmpty(delegateSelectors.getValue())) {
           setOriginAndDelegateSelectors(delegateSelectors, withDelegateSelector, YAMLFieldNameConstants.PIPELINE);
         }
       }
