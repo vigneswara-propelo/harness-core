27cadb55f2f303e7a300ae563d4df5336c004cc8
Abhijith Pradeep <abhijith.pradeep@harness.io>
2022-05-27 10:02:18 +0530

    [SRM-10656]: manual intervention with default rollback. (#33541)

--- 300-cv-nextgen/src/main/java/io/harness/cvng/cdng/services/impl/CVNGPlanCreator.java
+++ 300-cv-nextgen/src/main/java/io/harness/cvng/cdng/services/impl/CVNGPlanCreator.java
@@ -7,12 +7,21 @@
 
 package io.harness.cvng.cdng.services.impl;
 
+import io.harness.advisers.manualIntervention.ManualInterventionAdviserRollbackParameters;
+import io.harness.advisers.manualIntervention.ManualInterventionAdviserWithRollback;
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.cvng.cdng.beans.CVNGStepType;
 import io.harness.plancreator.steps.GenericStepPMSPlanCreator;
+import io.harness.pms.contracts.advisers.AdviserObtainment;
+import io.harness.pms.contracts.execution.failure.FailureType;
+import io.harness.pms.yaml.YamlField;
+import io.harness.utils.TimeoutUtils;
+import io.harness.yaml.core.failurestrategy.FailureStrategyActionConfig;
+import io.harness.yaml.core.failurestrategy.manualintervention.ManualInterventionFailureActionConfig;
 
 import com.google.common.collect.Sets;
+import com.google.protobuf.ByteString;
 import java.util.Set;
 @OwnedBy(HarnessTeam.CV)
 public class CVNGPlanCreator extends GenericStepPMSPlanCreator {
@@ -20,5 +29,19 @@ public class CVNGPlanCreator extends GenericStepPMSPlanCreator {
   @Override
   public Set<String> getSupportedStepTypes() {
     return CVNG_SUPPORTED_TYPES;
+  }
+
+  @Override
+  protected AdviserObtainment getManualInterventionAdviserObtainment(Set<FailureType> failureTypes,
+      AdviserObtainment.Builder adviserObtainmentBuilder, ManualInterventionFailureActionConfig actionConfig,
+      FailureStrategyActionConfig actionUnderManualIntervention, YamlField currentField) {
+    return adviserObtainmentBuilder.setType(ManualInterventionAdviserWithRollback.ADVISER_TYPE)
+        .setParameters(ByteString.copyFrom(kryoSerializer.asBytes(
+            ManualInterventionAdviserRollbackParameters.builder()
+                .applicableFailureTypes(failureTypes)
+                .timeoutAction(toRepairAction(actionUnderManualIntervention))
+                .timeout((int) TimeoutUtils.getTimeoutInSeconds(actionConfig.getSpecConfig().getTimeout(), 0))
+                .build())))
+        .build();
   }
 }
