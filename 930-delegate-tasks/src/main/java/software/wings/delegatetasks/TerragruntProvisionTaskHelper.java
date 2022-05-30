ed8c1bb26e30eb9a2b722fa02b3ee64087bbd223
Aleksa Buha <aleksa.buha@harness.io>
2022-05-30 11:17:27 +0200

    [CDS-38028]: Erorr Handling for null stateFile in TerragruntProvsionTask (#33632)

--- 930-delegate-tasks/src/main/java/software/wings/delegatetasks/TerragruntProvisionTaskHelper.java
+++ 930-delegate-tasks/src/main/java/software/wings/delegatetasks/TerragruntProvisionTaskHelper.java
@@ -21,6 +21,7 @@ import static java.lang.String.format;
 import io.harness.annotations.dev.HarnessModule;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.annotations.dev.TargetModule;
+import io.harness.exception.FileReadException;
 import io.harness.filesystem.FileIo;
 import io.harness.git.model.GitRepositoryType;
 import io.harness.logging.CommandExecutionStatus;
@@ -200,8 +201,7 @@ public class TerragruntProvisionTaskHelper {
     return "";
   }
 
-  public void downloadTfStateFile(TerragruntProvisionParameters parameters, String terraformConfigFileDirectory)
-      throws IOException {
+  public void downloadTfStateFile(TerragruntProvisionParameters parameters, String terraformConfigFileDirectory) {
     File tfStateFile = (isEmpty(parameters.getWorkspace()))
         ? Paths.get(terraformConfigFileDirectory, TERRAFORM_STATE_FILE_NAME).toFile()
         : Paths.get(terraformConfigFileDirectory, format(WORKSPACE_STATE_FILE_PATH_FORMAT, parameters.getWorkspace()))
@@ -218,6 +218,9 @@ public class TerragruntProvisionTaskHelper {
           pushbackInputStream.unread(firstByte);
           FileUtils.copyInputStreamToFile(pushbackInputStream, tfStateFile);
         }
+      } catch (IOException ex) {
+        throw new FileReadException(
+            format("Could not download Terraform state file with id: %s", parameters.getCurrentStateFileId()));
       }
     } else {
       FileUtils.deleteQuietly(tfStateFile);
