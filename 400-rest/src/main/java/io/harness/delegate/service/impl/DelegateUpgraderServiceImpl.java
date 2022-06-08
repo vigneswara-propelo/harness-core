b7a3b2a697361e306f3b6d26248cb55d7a9c74d0
Arpit Tiwari <arpit.tiwari@harness.io>
2022-06-08 11:45:23 +0530

    fix: [DEL-4213]: fetch delegateImageTag from versionOverride for upgr… (#33909)
    * fix: [DEL-4213]: fetch delegateImageTag from versionOverride for upgrader
    
    * [DEL-4213]: pr comments

--- 400-rest/src/main/java/io/harness/delegate/service/impl/DelegateUpgraderServiceImpl.java
+++ 400-rest/src/main/java/io/harness/delegate/service/impl/DelegateUpgraderServiceImpl.java
@@ -9,13 +9,10 @@ package io.harness.delegate.service.impl;
 
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
-import io.harness.delegate.beans.DelegateRing;
-import io.harness.delegate.beans.DelegateRing.DelegateRingKeys;
+import io.harness.delegate.beans.DelegateType;
 import io.harness.delegate.beans.UpgradeCheckResult;
+import io.harness.delegate.service.DelegateVersionService;
 import io.harness.delegate.service.intfc.DelegateUpgraderService;
-import io.harness.persistence.HPersistence;
-
-import software.wings.service.intfc.AccountService;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -27,38 +24,24 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @OwnedBy(HarnessTeam.DEL)
 public class DelegateUpgraderServiceImpl implements DelegateUpgraderService {
-  private final HPersistence persistence;
-  private final AccountService accountService;
+  private final DelegateVersionService delegateVersionService;
 
   @Inject
-  public DelegateUpgraderServiceImpl(HPersistence persistence, AccountService accountService) {
-    this.persistence = persistence;
-    this.accountService = accountService;
+  public DelegateUpgraderServiceImpl(DelegateVersionService delegateVersionService) {
+    this.delegateVersionService = delegateVersionService;
   }
 
   @Override
   public UpgradeCheckResult getDelegateImageTag(String accountId, String currentDelegateImageTag) {
-    // TODO: ARPIT implement cache mechanism for getting ImageTag from DelegateRing class
-    DelegateRing delegateRing = persistence.createQuery(DelegateRing.class)
-                                    .filter(DelegateRingKeys.ringName, accountService.get(accountId).getRingName())
-                                    .get();
-    boolean shouldUpgrade = !currentDelegateImageTag.equals(delegateRing.getDelegateImageTag());
-    if (shouldUpgrade) {
-      return new UpgradeCheckResult(delegateRing.getDelegateImageTag(), shouldUpgrade);
-    }
-    return new UpgradeCheckResult(currentDelegateImageTag, shouldUpgrade);
+    String newDelegateImageTag = delegateVersionService.getDelegateImageTag(accountId, DelegateType.KUBERNETES);
+    final boolean shouldUpgrade = !currentDelegateImageTag.equals(newDelegateImageTag);
+    return new UpgradeCheckResult(shouldUpgrade ? newDelegateImageTag : currentDelegateImageTag, shouldUpgrade);
   }
 
   @Override
   public UpgradeCheckResult getUpgraderImageTag(String accountId, String currentUpgraderImageTag) {
-    // TODO: ARPIT implement cache mechanism for getting ImageTag from DelegateRing class
-    DelegateRing delegateRing = persistence.createQuery(DelegateRing.class)
-                                    .filter(DelegateRingKeys.ringName, accountService.get(accountId).getRingName())
-                                    .get();
-    boolean shouldUpgrade = !currentUpgraderImageTag.equals(delegateRing.getUpgraderImageTag());
-    if (shouldUpgrade) {
-      return new UpgradeCheckResult(delegateRing.getUpgraderImageTag(), shouldUpgrade);
-    }
-    return new UpgradeCheckResult(currentUpgraderImageTag, shouldUpgrade);
+    String newUpgraderImageTag = delegateVersionService.getUpgraderImageTag(accountId, DelegateType.KUBERNETES);
+    final boolean shouldUpgrade = !currentUpgraderImageTag.equals(newUpgraderImageTag);
+    return new UpgradeCheckResult(shouldUpgrade ? newUpgraderImageTag : currentUpgraderImageTag, shouldUpgrade);
   }
 }
--- 400-rest/src/test/java/io/harness/delegate/service/DelegateUpgraderServiceTest.java
+++ 400-rest/src/test/java/io/harness/delegate/service/DelegateUpgraderServiceTest.java
@@ -10,93 +10,86 @@ package io.harness.delegate.service;
 import static io.harness.rule.OwnerRule.ARPIT;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.MockitoAnnotations.initMocks;
+import static org.mockito.Mockito.when;
 
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.category.element.UnitTests;
-import io.harness.delegate.beans.DelegateRing;
+import io.harness.delegate.beans.DelegateType;
 import io.harness.delegate.beans.UpgradeCheckResult;
 import io.harness.delegate.service.impl.DelegateUpgraderServiceImpl;
-import io.harness.delegate.utils.DelegateRingConstants;
-import io.harness.persistence.HPersistence;
 import io.harness.rule.Owner;
 
-import software.wings.WingsBaseTest;
-import software.wings.beans.Account;
-
-import com.google.inject.Inject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @OwnedBy(HarnessTeam.DEL)
 @RunWith(MockitoJUnitRunner.class)
-public class DelegateUpgraderServiceTest extends WingsBaseTest {
+public class DelegateUpgraderServiceTest {
   private static final String TEST_ACCOUNT_ID1 = "accountId1";
-  private static final String TEST_ACCOUNT_ID2 = "accountId2";
   private static final String LATEST_DELEGATE_IMAGE_TAG = "harness/delegate:latest";
   private static final String LATEST_UPGRADER_IMAGE_TAG = "harness/upgrader:latest";
   private static final String DELEGATE_IMAGE_TAG_1 = "harness/delegate:1";
   private static final String UPGRADER_IMAGE_TAG_1 = "harness/upgrader:1";
 
-  @InjectMocks @Inject private DelegateUpgraderServiceImpl upgraderService;
-  @Inject private HPersistence persistence;
+  private DelegateUpgraderServiceImpl underTest;
+
+  @Mock private DelegateVersionService delegateVersionService;
 
   @Before
   public void setUp() {
-    initMocks(this);
+    underTest = new DelegateUpgraderServiceImpl(delegateVersionService);
   }
 
   @Test
   @Owner(developers = ARPIT)
   @Category(UnitTests.class)
-  public void shouldGetDelegateImageTag() {
-    setupAccount(TEST_ACCOUNT_ID1, DelegateRingConstants.RING_NAME_1);
-    setupAccount(TEST_ACCOUNT_ID2, DelegateRingConstants.RING_NAME_2);
-    setupDelegateRing();
-    UpgradeCheckResult upgradeCheckResult1 =
-        upgraderService.getDelegateImageTag(TEST_ACCOUNT_ID1, DELEGATE_IMAGE_TAG_1);
-    UpgradeCheckResult upgradeCheckResult2 =
-        upgraderService.getDelegateImageTag(TEST_ACCOUNT_ID2, DELEGATE_IMAGE_TAG_1);
+  public void shouldUpgradeDelegateImageTag() {
+    when(delegateVersionService.getDelegateImageTag(TEST_ACCOUNT_ID1, DelegateType.KUBERNETES))
+        .thenReturn(LATEST_DELEGATE_IMAGE_TAG);
+    UpgradeCheckResult upgradeCheckResult1 = underTest.getDelegateImageTag(TEST_ACCOUNT_ID1, DELEGATE_IMAGE_TAG_1);
 
     assertThat(upgradeCheckResult1.isShouldUpgrade()).isTrue();
     assertThat(upgradeCheckResult1.getImageTag()).isEqualTo(LATEST_DELEGATE_IMAGE_TAG);
-    assertThat(upgradeCheckResult2.isShouldUpgrade()).isFalse();
-    assertThat(upgradeCheckResult2.getImageTag()).isEqualTo(DELEGATE_IMAGE_TAG_1);
   }
 
   @Test
   @Owner(developers = ARPIT)
   @Category(UnitTests.class)
-  public void shouldGetUpgraderImageTag() {
-    setupAccount(TEST_ACCOUNT_ID1, DelegateRingConstants.RING_NAME_1);
-    setupAccount(TEST_ACCOUNT_ID2, DelegateRingConstants.RING_NAME_2);
-    setupDelegateRing();
+  public void shouldNotUpgradeDelegateImageTag() {
+    when(delegateVersionService.getDelegateImageTag(TEST_ACCOUNT_ID1, DelegateType.KUBERNETES))
+        .thenReturn(DELEGATE_IMAGE_TAG_1);
+    UpgradeCheckResult upgradeCheckResult1 = underTest.getDelegateImageTag(TEST_ACCOUNT_ID1, DELEGATE_IMAGE_TAG_1);
 
-    UpgradeCheckResult upgradeCheckResult1 =
-        upgraderService.getUpgraderImageTag(TEST_ACCOUNT_ID1, UPGRADER_IMAGE_TAG_1);
-    UpgradeCheckResult upgradeCheckResult2 =
-        upgraderService.getUpgraderImageTag(TEST_ACCOUNT_ID2, UPGRADER_IMAGE_TAG_1);
+    assertThat(upgradeCheckResult1.isShouldUpgrade()).isFalse();
+    assertThat(upgradeCheckResult1.getImageTag()).isEqualTo(DELEGATE_IMAGE_TAG_1);
+  }
+
+  @Test
+  @Owner(developers = ARPIT)
+  @Category(UnitTests.class)
+  public void shouldUpgradeUpgraderImageTag() {
+    when(delegateVersionService.getUpgraderImageTag(TEST_ACCOUNT_ID1, DelegateType.KUBERNETES))
+        .thenReturn(LATEST_UPGRADER_IMAGE_TAG);
+    UpgradeCheckResult upgradeCheckResult1 = underTest.getUpgraderImageTag(TEST_ACCOUNT_ID1, UPGRADER_IMAGE_TAG_1);
+
+    assertThat(upgradeCheckResult1.isShouldUpgrade()).isTrue();
+    assertThat(upgradeCheckResult1.getImageTag()).isEqualTo(LATEST_UPGRADER_IMAGE_TAG);
+  }
+
+  @Test
+  @Owner(developers = ARPIT)
+  @Category(UnitTests.class)
+  public void shouldNotUpgradeUpgraderImageTag() {
+    when(delegateVersionService.getUpgraderImageTag(TEST_ACCOUNT_ID1, DelegateType.KUBERNETES))
+        .thenReturn(UPGRADER_IMAGE_TAG_1);
+    UpgradeCheckResult upgradeCheckResult1 = underTest.getUpgraderImageTag(TEST_ACCOUNT_ID1, UPGRADER_IMAGE_TAG_1);
 
     assertThat(upgradeCheckResult1.isShouldUpgrade()).isFalse();
     assertThat(upgradeCheckResult1.getImageTag()).isEqualTo(UPGRADER_IMAGE_TAG_1);
-    assertThat(upgradeCheckResult2.isShouldUpgrade()).isTrue();
-    assertThat(upgradeCheckResult2.getImageTag()).isEqualTo(LATEST_UPGRADER_IMAGE_TAG);
-  }
-
-  private void setupAccount(String accountId, String ringName) {
-    Account account = Account.Builder.anAccount().withUuid(accountId).withRingName(ringName).build();
-    persistence.save(account);
-  }
-
-  private void setupDelegateRing() {
-    persistence.save(
-        new DelegateRing(DelegateRingConstants.RING_NAME_1, LATEST_DELEGATE_IMAGE_TAG, UPGRADER_IMAGE_TAG_1));
-    persistence.save(
-        new DelegateRing(DelegateRingConstants.RING_NAME_2, DELEGATE_IMAGE_TAG_1, LATEST_UPGRADER_IMAGE_TAG));
   }
 }
