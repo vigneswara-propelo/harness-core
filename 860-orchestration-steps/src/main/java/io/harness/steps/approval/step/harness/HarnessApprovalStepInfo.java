b41593b0b1fdf816407e8539f9640a99e76bad40
Prabu Rajendran <prabu.rajendran@harness.io>
2022-06-13 15:10:23 +0530

    fix: [CDS-24470]: Added validation for empty approval message (#34095)
    * [CDS-24470]: Added validation for empty approval message
    
    * [CDS-24470]: fixed checkstyle and schema
    merge
    
    * [CDS-24470]: rearranged imports
    for checkstyle
    
    * [CDS-24470]: fixed unit tests

--- 860-orchestration-steps/src/main/java/io/harness/steps/approval/step/harness/HarnessApprovalStepInfo.java
+++ 860-orchestration-steps/src/main/java/io/harness/steps/approval/step/harness/HarnessApprovalStepInfo.java
@@ -32,8 +32,8 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
+import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.data.annotation.TypeAlias;
-
 @OwnedBy(CDC)
 @Data
 @Builder
@@ -42,7 +42,7 @@ import org.springframework.data.annotation.TypeAlias;
 @TypeAlias("harnessApprovalStepInfo")
 @RecasterAlias("io.harness.steps.approval.step.harness.HarnessApprovalStepInfo")
 public class HarnessApprovalStepInfo implements PMSStepInfo {
-  @NotNull @ApiModelProperty(dataType = SwaggerConstants.STRING_CLASSPATH) ParameterField<String> approvalMessage;
+  @NotEmpty @ApiModelProperty(dataType = SwaggerConstants.STRING_CLASSPATH) ParameterField<String> approvalMessage;
 
   @NotNull
   @YamlSchemaTypes(value = {string})
