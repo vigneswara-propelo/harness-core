cc19046272ee194cd2e095141949fbe96c1b439c
Shalini Agrawal <shalini.agarwal@harness.io>
2022-06-15 16:29:05 +0530

    fix: [PIE-3919]: Better error handling when dashboard data gen fails in pipeline execution history (#34114)
    * fix: [PIE-3919]: added try and catch blocks and throw invalid request exception
    
    * removed logger
    
    * Handling connection error
    
    Co-authored-by: Shalini Agrawal <shalini.agrawal@harness.io>

--- 800-pipeline-service/src/main/java/io/harness/pms/pipeline/service/PipelineDashboardQueryService.java
+++ 800-pipeline-service/src/main/java/io/harness/pms/pipeline/service/PipelineDashboardQueryService.java
@@ -7,6 +7,7 @@
 
 package io.harness.pms.pipeline.service;
 
+import io.harness.exception.InvalidRequestException;
 import io.harness.pms.Dashboard.StatusAndTime;
 import io.harness.timescaledb.Tables;
 
@@ -18,6 +19,7 @@ import org.jooq.Condition;
 import org.jooq.DSLContext;
 import org.jooq.SelectField;
 import org.jooq.Table;
+import org.jooq.exception.DataAccessException;
 import org.jooq.impl.DSL;
 
 public class PipelineDashboardQueryService {
@@ -25,102 +27,142 @@ public class PipelineDashboardQueryService {
 
   private String CD_TableName = "pipeline_execution_summary_cd";
 
+  public boolean isConnectionError(DataAccessException ex) {
+    if (ex.getMessage().contains("Error getting connection from data source")) {
+      return true;
+    }
+    return false;
+  }
+
   public List<StatusAndTime> getPipelineExecutionStatusAndTime(String accountId, String orgId, String projectId,
       String pipelineId, long startInterval, long endInterval, String tableName) {
-    List<StatusAndTime> statusAndTime;
-    Table<?> from;
-    List<Condition> conditions = new ArrayList<>();
-    SelectField<?>[] select;
-    if (tableName == CD_TableName) {
-      select =
-          new SelectField[] {Tables.PIPELINE_EXECUTION_SUMMARY_CD.STATUS, Tables.PIPELINE_EXECUTION_SUMMARY_CD.STARTTS};
-      from = Tables.PIPELINE_EXECUTION_SUMMARY_CD;
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.ORGIDENTIFIER.eq(orgId));
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.PIPELINEIDENTIFIER.eq(pipelineId));
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.PROJECTIDENTIFIER.eq(projectId));
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.ACCOUNTID.eq(accountId));
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.STARTTS.ge(startInterval));
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.STARTTS.lt(endInterval));
-    } else {
-      select =
-          new SelectField[] {Tables.PIPELINE_EXECUTION_SUMMARY_CI.STATUS, Tables.PIPELINE_EXECUTION_SUMMARY_CI.STARTTS};
-      from = Tables.PIPELINE_EXECUTION_SUMMARY_CI;
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.ORGIDENTIFIER.eq(orgId));
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.PIPELINEIDENTIFIER.eq(pipelineId));
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.PROJECTIDENTIFIER.eq(projectId));
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.ACCOUNTID.eq(accountId));
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.STARTTS.ge(startInterval));
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.STARTTS.lt(endInterval));
+    try {
+      List<StatusAndTime> statusAndTime;
+      Table<?> from;
+      List<Condition> conditions = new ArrayList<>();
+      SelectField<?>[] select;
+      if (tableName == CD_TableName) {
+        select = new SelectField[] {
+            Tables.PIPELINE_EXECUTION_SUMMARY_CD.STATUS, Tables.PIPELINE_EXECUTION_SUMMARY_CD.STARTTS};
+        from = Tables.PIPELINE_EXECUTION_SUMMARY_CD;
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.ORGIDENTIFIER.eq(orgId));
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.PIPELINEIDENTIFIER.eq(pipelineId));
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.PROJECTIDENTIFIER.eq(projectId));
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.ACCOUNTID.eq(accountId));
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.STARTTS.ge(startInterval));
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.STARTTS.lt(endInterval));
+      } else {
+        select = new SelectField[] {
+            Tables.PIPELINE_EXECUTION_SUMMARY_CI.STATUS, Tables.PIPELINE_EXECUTION_SUMMARY_CI.STARTTS};
+        from = Tables.PIPELINE_EXECUTION_SUMMARY_CI;
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.ORGIDENTIFIER.eq(orgId));
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.PIPELINEIDENTIFIER.eq(pipelineId));
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.PROJECTIDENTIFIER.eq(projectId));
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.ACCOUNTID.eq(accountId));
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.STARTTS.ge(startInterval));
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.STARTTS.lt(endInterval));
+      }
+      statusAndTime = this.dsl.select(select).from(from).where(conditions).fetch().into(StatusAndTime.class);
+      return statusAndTime;
+    } catch (DataAccessException ex) {
+      if (isConnectionError(ex)) {
+        throw new InvalidRequestException(
+            "Unable to fetch Dashboard data for executions, Please ensure timescale is enabled", ex);
+      } else {
+        throw new InvalidRequestException("Unable to fetch Dashboard data for executions", ex);
+      }
+    } catch (Exception ex) {
+      throw new InvalidRequestException("Unable to fetch Dashboard data for executions", ex);
     }
-    statusAndTime = this.dsl.select(select).from(from).where(conditions).fetch().into(StatusAndTime.class);
-    return statusAndTime;
   }
 
   public long getPipelineExecutionMeanDuration(String accountId, String orgId, String projectId, String pipelineId,
       long startInterval, long endInterval, String tableName) {
-    @NotNull List<Long> mean;
-    Table<?> from;
-    List<Condition> conditions = new ArrayList<>();
-    SelectField<?>[] select;
-    if (tableName == CD_TableName) {
-      select = new SelectField[] {
-          DSL.avg(Tables.PIPELINE_EXECUTION_SUMMARY_CD.ENDTS.sub(Tables.PIPELINE_EXECUTION_SUMMARY_CD.STARTTS))
-              .div(1000)};
-      from = Tables.PIPELINE_EXECUTION_SUMMARY_CD;
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.ORGIDENTIFIER.eq(orgId));
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.PIPELINEIDENTIFIER.eq(pipelineId));
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.PROJECTIDENTIFIER.eq(projectId));
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.ACCOUNTID.eq(accountId));
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.STARTTS.ge(startInterval));
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.STARTTS.lt(endInterval));
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.ENDTS.isNotNull());
-    } else {
-      select = new SelectField[] {
-          DSL.avg(Tables.PIPELINE_EXECUTION_SUMMARY_CI.ENDTS.sub(Tables.PIPELINE_EXECUTION_SUMMARY_CI.STARTTS))
-              .div(1000)};
-      from = Tables.PIPELINE_EXECUTION_SUMMARY_CI;
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.ORGIDENTIFIER.eq(orgId));
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.PIPELINEIDENTIFIER.eq(pipelineId));
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.PROJECTIDENTIFIER.eq(projectId));
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.ACCOUNTID.eq(accountId));
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.STARTTS.ge(startInterval));
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.STARTTS.lt(endInterval));
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.ENDTS.isNotNull());
+    try {
+      @NotNull List<Long> mean;
+      Table<?> from;
+      List<Condition> conditions = new ArrayList<>();
+      SelectField<?>[] select;
+      if (tableName == CD_TableName) {
+        select = new SelectField[] {
+            DSL.avg(Tables.PIPELINE_EXECUTION_SUMMARY_CD.ENDTS.sub(Tables.PIPELINE_EXECUTION_SUMMARY_CD.STARTTS))
+                .div(1000)};
+        from = Tables.PIPELINE_EXECUTION_SUMMARY_CD;
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.ORGIDENTIFIER.eq(orgId));
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.PIPELINEIDENTIFIER.eq(pipelineId));
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.PROJECTIDENTIFIER.eq(projectId));
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.ACCOUNTID.eq(accountId));
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.STARTTS.ge(startInterval));
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.STARTTS.lt(endInterval));
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.ENDTS.isNotNull());
+      } else {
+        select = new SelectField[] {
+            DSL.avg(Tables.PIPELINE_EXECUTION_SUMMARY_CI.ENDTS.sub(Tables.PIPELINE_EXECUTION_SUMMARY_CI.STARTTS))
+                .div(1000)};
+        from = Tables.PIPELINE_EXECUTION_SUMMARY_CI;
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.ORGIDENTIFIER.eq(orgId));
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.PIPELINEIDENTIFIER.eq(pipelineId));
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.PROJECTIDENTIFIER.eq(projectId));
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.ACCOUNTID.eq(accountId));
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.STARTTS.ge(startInterval));
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.STARTTS.lt(endInterval));
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.ENDTS.isNotNull());
+      }
+      mean = this.dsl.select(select).from(from).where(conditions).fetch().into(long.class);
+      return mean.get(0);
+    } catch (DataAccessException ex) {
+      if (isConnectionError(ex)) {
+        throw new InvalidRequestException(
+            "Unable to fetch Dashboard data for executions, Please ensure timescale is enabled", ex);
+      } else {
+        throw new InvalidRequestException("Unable to fetch Dashboard data for executions", ex);
+      }
+    } catch (Exception ex) {
+      throw new InvalidRequestException("Unable to fetch Dashboard data for executions", ex);
     }
-    mean = this.dsl.select(select).from(from).where(conditions).fetch().into(long.class);
-    return mean.get(0);
   }
 
   public long getPipelineExecutionMedianDuration(String accountId, String orgId, String projectId, String pipelineId,
       long startInterval, long endInterval, String tableName) {
-    @NotNull List<Long> median;
-    Table<?> from;
-    List<Condition> conditions = new ArrayList<>();
-    SelectField<?>[] select;
-    if (tableName == CD_TableName) {
-      select = new SelectField[] {DSL.percentileDisc(0.5).withinGroupOrderBy(
-          (Tables.PIPELINE_EXECUTION_SUMMARY_CD.ENDTS.sub(Tables.PIPELINE_EXECUTION_SUMMARY_CD.STARTTS)).div(1000))};
-      from = Tables.PIPELINE_EXECUTION_SUMMARY_CD;
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.ORGIDENTIFIER.eq(orgId));
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.PIPELINEIDENTIFIER.eq(pipelineId));
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.PROJECTIDENTIFIER.eq(projectId));
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.ACCOUNTID.eq(accountId));
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.STARTTS.ge(startInterval));
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.STARTTS.lt(endInterval));
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.ENDTS.isNotNull());
-    } else {
-      select = new SelectField[] {DSL.percentileDisc(0.5).withinGroupOrderBy(
-          (Tables.PIPELINE_EXECUTION_SUMMARY_CI.ENDTS.sub(Tables.PIPELINE_EXECUTION_SUMMARY_CI.STARTTS)).div(1000))};
-      from = Tables.PIPELINE_EXECUTION_SUMMARY_CI;
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.ORGIDENTIFIER.eq(orgId));
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.PIPELINEIDENTIFIER.eq(pipelineId));
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.PROJECTIDENTIFIER.eq(projectId));
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.ACCOUNTID.eq(accountId));
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.STARTTS.ge(startInterval));
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.STARTTS.lt(endInterval));
-      conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.ENDTS.isNotNull());
+    try {
+      @NotNull List<Long> median;
+      Table<?> from;
+      List<Condition> conditions = new ArrayList<>();
+      SelectField<?>[] select;
+      if (tableName == CD_TableName) {
+        select = new SelectField[] {DSL.percentileDisc(0.5).withinGroupOrderBy(
+            (Tables.PIPELINE_EXECUTION_SUMMARY_CD.ENDTS.sub(Tables.PIPELINE_EXECUTION_SUMMARY_CD.STARTTS)).div(1000))};
+        from = Tables.PIPELINE_EXECUTION_SUMMARY_CD;
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.ORGIDENTIFIER.eq(orgId));
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.PIPELINEIDENTIFIER.eq(pipelineId));
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.PROJECTIDENTIFIER.eq(projectId));
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.ACCOUNTID.eq(accountId));
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.STARTTS.ge(startInterval));
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.STARTTS.lt(endInterval));
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CD.ENDTS.isNotNull());
+      } else {
+        select = new SelectField[] {DSL.percentileDisc(0.5).withinGroupOrderBy(
+            (Tables.PIPELINE_EXECUTION_SUMMARY_CI.ENDTS.sub(Tables.PIPELINE_EXECUTION_SUMMARY_CI.STARTTS)).div(1000))};
+        from = Tables.PIPELINE_EXECUTION_SUMMARY_CI;
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.ORGIDENTIFIER.eq(orgId));
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.PIPELINEIDENTIFIER.eq(pipelineId));
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.PROJECTIDENTIFIER.eq(projectId));
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.ACCOUNTID.eq(accountId));
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.STARTTS.ge(startInterval));
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.STARTTS.lt(endInterval));
+        conditions.add(Tables.PIPELINE_EXECUTION_SUMMARY_CI.ENDTS.isNotNull());
+      }
+      median = this.dsl.select(select).from(from).where(conditions).fetch().into(long.class);
+      return median.get(0);
+    } catch (DataAccessException ex) {
+      if (isConnectionError(ex)) {
+        throw new InvalidRequestException(
+            "Unable to fetch Dashboard data for executions, Please ensure timescale is enabled", ex);
+      } else {
+        throw new InvalidRequestException("Unable to fetch Dashboard data for executions", ex);
+      }
+    } catch (Exception ex) {
+      throw new InvalidRequestException("Unable to fetch Dashboard data for executions", ex);
     }
-    median = this.dsl.select(select).from(from).where(conditions).fetch().into(long.class);
-    return median.get(0);
   }
 }
