7b6eaf504518f6237ed0c8e2e12cf2812984f18a
Prashant Sharma <prashant.sharma@harness.io>
2022-06-22 19:12:52 +0530

    feat: [CDS-39083]: Setup usages for serviceRef referred in pipeline f… (#34573)
    * feat: [CDS-39083]: Setup usages for serviceRef referred in pipeline for v2 impl
    
    * removed extra function

--- 125-cd-nextgen/src/main/java/io/harness/cdng/visitor/helpers/serviceconfig/ServiceEntityVisitorHelperV2.java
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/visitor/helpers/serviceconfig/ServiceEntityVisitorHelperV2.java
@@ -7,11 +7,28 @@
 
 package io.harness.cdng.visitor.helpers.serviceconfig;
 
+import static io.harness.walktree.visitor.utilities.VisitorParentPathUtils.PATH_CONNECTOR;
+
+import io.harness.beans.IdentifierRef;
 import io.harness.cdng.service.beans.ServiceYamlV2;
+import io.harness.cdng.visitor.YamlTypes;
+import io.harness.eventsframework.schemas.entity.EntityDetailProtoDTO;
+import io.harness.eventsframework.schemas.entity.EntityTypeProtoEnum;
+import io.harness.preflight.PreFlightCheckMetadata;
+import io.harness.utils.IdentifierRefHelper;
+import io.harness.utils.IdentifierRefProtoUtils;
+import io.harness.walktree.visitor.entityreference.EntityReferenceExtractor;
+import io.harness.walktree.visitor.utilities.VisitorParentPathUtils;
 import io.harness.walktree.visitor.validation.ConfigValidator;
 import io.harness.walktree.visitor.validation.ValidationVisitor;
 
-public class ServiceEntityVisitorHelperV2 implements ConfigValidator {
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class ServiceEntityVisitorHelperV2 implements ConfigValidator, EntityReferenceExtractor {
   @Override
   public void validate(Object object, ValidationVisitor visitor) {
     // Nothing to validate.
@@ -20,5 +37,39 @@ public class ServiceEntityVisitorHelperV2 implements ConfigValidator {
   @Override
   public Object createDummyVisitableElement(Object originalElement) {
     return ServiceYamlV2.builder().build();
+  }
+
+  @Override
+  public Set<EntityDetailProtoDTO> addReference(Object object, String accountIdentifier, String orgIdentifier,
+      String projectIdentifier, Map<String, Object> contextMap) {
+    ServiceYamlV2 serviceYamlV2 = (ServiceYamlV2) object;
+    Set<EntityDetailProtoDTO> result = new HashSet<>();
+
+    String fullQualifiedDomainName =
+        VisitorParentPathUtils.getFullQualifiedDomainName(contextMap) + PATH_CONNECTOR + YamlTypes.SERVICE_REF;
+    Map<String, String> metadata =
+        new HashMap<>(Collections.singletonMap(PreFlightCheckMetadata.FQN, fullQualifiedDomainName));
+    if (!serviceYamlV2.getServiceRef().isExpression()) {
+      String serviceRefString = serviceYamlV2.getServiceRef().getValue();
+      IdentifierRef identifierRef = IdentifierRefHelper.getIdentifierRef(
+          serviceRefString, accountIdentifier, orgIdentifier, projectIdentifier, metadata);
+      EntityDetailProtoDTO entityDetail =
+          EntityDetailProtoDTO.newBuilder()
+              .setIdentifierRef(IdentifierRefProtoUtils.createIdentifierRefProtoFromIdentifierRef(identifierRef))
+              .setType(EntityTypeProtoEnum.SERVICE)
+              .build();
+      result.add(entityDetail);
+    } else {
+      metadata.put(PreFlightCheckMetadata.EXPRESSION, serviceYamlV2.getServiceRef().getExpressionValue());
+      IdentifierRef identifierRef = IdentifierRefHelper.createIdentifierRefWithUnknownScope(accountIdentifier,
+          orgIdentifier, projectIdentifier, serviceYamlV2.getServiceRef().getExpressionValue(), metadata);
+      EntityDetailProtoDTO entityDetail =
+          EntityDetailProtoDTO.newBuilder()
+              .setIdentifierRef(IdentifierRefProtoUtils.createIdentifierRefProtoFromIdentifierRef(identifierRef))
+              .setType(EntityTypeProtoEnum.SERVICE)
+              .build();
+      result.add(entityDetail);
+    }
+    return result;
   }
 }
