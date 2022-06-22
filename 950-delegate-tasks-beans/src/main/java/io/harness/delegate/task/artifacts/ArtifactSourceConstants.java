4ab563343dae5be472aa9cbd8bf8590c7117681f
Ved Tambat <ved.atultambat@harness.io>
2022-06-22 17:20:48 +0530

    [fix]: [CDS-38988]: Revert PR for S3 changes (#34574)
    * [CDS-38988]: revert PR
    
    * [CDS-38988]: codebase

--- 950-delegate-tasks-beans/src/main/java/io/harness/delegate/task/artifacts/ArtifactSourceConstants.java
+++ 950-delegate-tasks-beans/src/main/java/io/harness/delegate/task/artifacts/ArtifactSourceConstants.java
@@ -17,5 +17,4 @@ public interface ArtifactSourceConstants {
   String CUSTOM_ARTIFACT_NAME = "CustomArtifact";
   String ARTIFACTORY_GENERIC_REGISTRY_NAME = "ArtifactoryGenericRegistry";
   String JENKINS_NAME = "Jenkins";
-  String AMAZON_S3_NAME = "AmazonS3";
 }
