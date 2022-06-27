a566530777adaecd29e3ac43bf5d506e29c8a00a
Sahil Hindwani <sahil.hindwani@harness.io>
2022-06-27 13:03:44 +0530

    techdebt: [PIE-3591]: Unit test for strategy classes (#34700)

---
+++ 878-ng-common-utilities/src/test/java/io/harness/steps/matrix/ParallelismStrategyConfigServiceTest.java
@@ -0,0 +1,51 @@
+package io.harness.steps.matrix;
+
+import static io.harness.rule.OwnerRule.SAHIL;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.harness.NGCommonUtilitiesTestBase;
+import io.harness.category.element.UnitTests;
+import io.harness.plancreator.strategy.StrategyConfig;
+import io.harness.pms.contracts.execution.ChildrenExecutableResponse;
+import io.harness.pms.yaml.YamlField;
+import io.harness.pms.yaml.YamlNode;
+import io.harness.pms.yaml.YamlUtils;
+import io.harness.rule.Owner;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+public class ParallelismStrategyConfigServiceTest extends NGCommonUtilitiesTestBase {
+  private ParallelismStrategyConfigService parallelStrategyConfigService = new ParallelismStrategyConfigService();
+
+  @Test
+  @Owner(developers = SAHIL)
+  @Category(UnitTests.class)
+  public void testFetchChildren() throws IOException {
+    ClassLoader classLoader = this.getClass().getClassLoader();
+    final URL testFile = classLoader.getResource("pipeline-with-strategy.yaml");
+    assertThat(testFile).isNotNull();
+    String pipelineYaml = Resources.toString(testFile, Charsets.UTF_8);
+    String pipelineYamlWithUuid = YamlUtils.injectUuid(pipelineYaml);
+    YamlField pipelineYamlField = YamlUtils.readTree(pipelineYamlWithUuid).getNode().getField("pipeline");
+    assertThat(pipelineYamlField).isNotNull();
+    YamlField stagesYamlField = pipelineYamlField.getNode().getField("stages");
+    assertThat(stagesYamlField).isNotNull();
+    List<YamlNode> stageYamlNodes = stagesYamlField.getNode().asArray();
+
+    YamlField approvalStageYamlField = stageYamlNodes.get(3).getField("stage");
+
+    YamlField strategyField = approvalStageYamlField.getNode().getField("strategy");
+    StrategyConfig strategyConfig = YamlUtils.read(strategyField.getNode().toString(), StrategyConfig.class);
+
+    List<ChildrenExecutableResponse.Child> children =
+        parallelStrategyConfigService.fetchChildren(strategyConfig, "childNodeId");
+    assertThat(children.size()).isEqualTo(4);
+  }
+}
---
+++ 878-ng-common-utilities/src/test/java/io/harness/steps/matrix/StrategyStepTest.java
@@ -0,0 +1,164 @@
+package io.harness.steps.matrix;
+
+import static io.harness.data.structure.UUIDGenerator.generateUuid;
+import static io.harness.rule.OwnerRule.SAHIL;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import io.harness.NGCommonUtilitiesTestBase;
+import io.harness.category.element.UnitTests;
+import io.harness.plancreator.strategy.StrategyConfig;
+import io.harness.pms.contracts.ambiance.Ambiance;
+import io.harness.pms.contracts.execution.Status;
+import io.harness.pms.sdk.core.steps.io.StepInputPackage;
+import io.harness.pms.sdk.core.steps.io.StepResponse;
+import io.harness.pms.sdk.core.steps.io.StepResponseNotifyData;
+import io.harness.pms.yaml.ParameterField;
+import io.harness.pms.yaml.YamlField;
+import io.harness.pms.yaml.YamlNode;
+import io.harness.pms.yaml.YamlUtils;
+import io.harness.rule.Owner;
+import io.harness.tasks.ResponseData;
+
+import com.google.common.base.Charsets;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.io.Resources;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+public class StrategyStepTest extends NGCommonUtilitiesTestBase {
+  private static final String CHILD_ID = generateUuid();
+
+  @Mock MatrixConfigService matrixConfigService;
+  @Mock ForLoopStrategyConfigService forLoopStrategyConfigService;
+  @Mock ParallelismStrategyConfigService parallelismStrategyConfigService;
+
+  @InjectMocks StrategyStep strategyStep;
+
+  @Test
+  @Owner(developers = SAHIL)
+  @Category(UnitTests.class)
+  public void testObtainChildrenForLoop() throws IOException {
+    ClassLoader classLoader = this.getClass().getClassLoader();
+    final URL testFile = classLoader.getResource("pipeline-with-strategy.yaml");
+    assertThat(testFile).isNotNull();
+    String pipelineYaml = Resources.toString(testFile, Charsets.UTF_8);
+    String pipelineYamlWithUuid = YamlUtils.injectUuid(pipelineYaml);
+    YamlField pipelineYamlField = YamlUtils.readTree(pipelineYamlWithUuid).getNode().getField("pipeline");
+    assertThat(pipelineYamlField).isNotNull();
+    YamlField stagesYamlField = pipelineYamlField.getNode().getField("stages");
+    assertThat(stagesYamlField).isNotNull();
+    List<YamlNode> stageYamlNodes = stagesYamlField.getNode().asArray();
+
+    YamlField approvalStageYamlField = stageYamlNodes.get(2).getField("stage");
+
+    YamlField strategyField = approvalStageYamlField.getNode().getField("strategy");
+    StrategyConfig strategyConfig = YamlUtils.read(strategyField.getNode().toString(), StrategyConfig.class);
+    StrategyStepParameters stepParameters =
+        StrategyStepParameters.builder().childNodeId("childNodeId").strategyConfig(strategyConfig).build();
+
+    when(forLoopStrategyConfigService.fetchChildren(strategyConfig, "childNodeId")).thenReturn(new ArrayList<>());
+
+    strategyStep.obtainChildren(Ambiance.newBuilder().build(), stepParameters, StepInputPackage.builder().build());
+
+    verify(forLoopStrategyConfigService).fetchChildren(strategyConfig, "childNodeId");
+
+    strategyConfig.getForConfig().setMaxConcurrency(ParameterField.ofNull());
+
+    when(forLoopStrategyConfigService.fetchChildren(strategyConfig, "childNodeId")).thenReturn(new ArrayList<>());
+
+    strategyStep.obtainChildren(Ambiance.newBuilder().build(), stepParameters, StepInputPackage.builder().build());
+
+    verify(forLoopStrategyConfigService, times(2)).fetchChildren(strategyConfig, "childNodeId");
+  }
+
+  @Test
+  @Owner(developers = SAHIL)
+  @Category(UnitTests.class)
+  public void testObtainChildrenParallelism() throws IOException {
+    ClassLoader classLoader = this.getClass().getClassLoader();
+    final URL testFile = classLoader.getResource("pipeline-with-strategy.yaml");
+    assertThat(testFile).isNotNull();
+    String pipelineYaml = Resources.toString(testFile, Charsets.UTF_8);
+    String pipelineYamlWithUuid = YamlUtils.injectUuid(pipelineYaml);
+    YamlField pipelineYamlField = YamlUtils.readTree(pipelineYamlWithUuid).getNode().getField("pipeline");
+    assertThat(pipelineYamlField).isNotNull();
+    YamlField stagesYamlField = pipelineYamlField.getNode().getField("stages");
+    assertThat(stagesYamlField).isNotNull();
+    List<YamlNode> stageYamlNodes = stagesYamlField.getNode().asArray();
+
+    YamlField approvalStageYamlField = stageYamlNodes.get(3).getField("stage");
+
+    YamlField strategyField = approvalStageYamlField.getNode().getField("strategy");
+    StrategyConfig strategyConfig = YamlUtils.read(strategyField.getNode().toString(), StrategyConfig.class);
+    StrategyStepParameters stepParameters =
+        StrategyStepParameters.builder().childNodeId("childNodeId").strategyConfig(strategyConfig).build();
+
+    when(parallelismStrategyConfigService.fetchChildren(strategyConfig, "childNodeId")).thenReturn(new ArrayList<>());
+
+    strategyStep.obtainChildren(Ambiance.newBuilder().build(), stepParameters, StepInputPackage.builder().build());
+
+    verify(parallelismStrategyConfigService).fetchChildren(strategyConfig, "childNodeId");
+  }
+
+  @Test
+  @Owner(developers = SAHIL)
+  @Category(UnitTests.class)
+  public void testObtainChildrenMatrix() throws IOException {
+    ClassLoader classLoader = this.getClass().getClassLoader();
+    final URL testFile = classLoader.getResource("pipeline-with-strategy.yaml");
+    assertThat(testFile).isNotNull();
+    String pipelineYaml = Resources.toString(testFile, Charsets.UTF_8);
+    String pipelineYamlWithUuid = YamlUtils.injectUuid(pipelineYaml);
+    YamlField pipelineYamlField = YamlUtils.readTree(pipelineYamlWithUuid).getNode().getField("pipeline");
+    assertThat(pipelineYamlField).isNotNull();
+    YamlField stagesYamlField = pipelineYamlField.getNode().getField("stages");
+    assertThat(stagesYamlField).isNotNull();
+    List<YamlNode> stageYamlNodes = stagesYamlField.getNode().asArray();
+
+    YamlField approvalStageYamlField = stageYamlNodes.get(0).getField("stage");
+
+    YamlField strategyField = approvalStageYamlField.getNode().getField("strategy");
+    StrategyConfig strategyConfig = YamlUtils.read(strategyField.getNode().toString(), StrategyConfig.class);
+    StrategyStepParameters stepParameters =
+        StrategyStepParameters.builder().childNodeId("childNodeId").strategyConfig(strategyConfig).build();
+
+    when(matrixConfigService.fetchChildren(strategyConfig, "childNodeId")).thenReturn(new ArrayList<>());
+
+    strategyStep.obtainChildren(Ambiance.newBuilder().build(), stepParameters, StepInputPackage.builder().build());
+
+    verify(matrixConfigService).fetchChildren(strategyConfig, "childNodeId");
+  }
+
+  @Test
+  @Owner(developers = SAHIL)
+  @Category(UnitTests.class)
+  public void testGetStepParameterClass() throws IOException {
+    assertThat(strategyStep.getStepParametersClass()).isEqualTo(StrategyStepParameters.class);
+  }
+
+  @Test
+  @Owner(developers = SAHIL)
+  @Category(UnitTests.class)
+  public void testHandleChildrenResponse() throws IOException {
+    Ambiance ambiance = Ambiance.newBuilder().build();
+
+    Map<String, ResponseData> responseDataMap =
+        ImmutableMap.<String, ResponseData>builder()
+            .put(CHILD_ID, StepResponseNotifyData.builder().nodeUuid(CHILD_ID).status(Status.FAILED).build())
+            .build();
+    StepResponse stepResponse =
+        strategyStep.handleChildrenResponse(ambiance, StrategyStepParameters.builder().build(), responseDataMap);
+    assertThat(stepResponse.getStatus()).isEqualTo(Status.FAILED);
+  }
+}
