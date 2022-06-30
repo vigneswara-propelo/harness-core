63928883c3e71c84b014e25499aec69071c85f6e
Abhinav Singh <abhinav.singh2@harness.io>
2022-06-30 16:27:55 +0530

    fix: [CDS-39219]: don't publish outcome for STOP_STAGE_SEQUENCE (#34755)

--- 953-yaml-commons/src/main/java/io/harness/pms/yaml/YAMLFieldNameConstants.java
+++ 953-yaml-commons/src/main/java/io/harness/pms/yaml/YAMLFieldNameConstants.java
@@ -56,7 +56,6 @@ public class YAMLFieldNameConstants {
   public final String CODEBASE_CONNECTOR_REF = "ciCodebase.connectorRef";
   public final String USE_ROLLBACK_STRATEGY = "useRollbackStrategy";
   public final String FAILED_CHILDREN_OUTPUT = "failedChildrenOutput";
-  public final String STOP_STAGE_SEQUENCE = "stopStageSequence";
 
   public final String STORE = "store";
   public final String PIPELINE_INFRASTRUCTURE = "infrastructure";
