50862b1afb8ea38b6f2c944625493bd7c4be38b9
Mitisha Pandey <mitisha.pandey@harness.io>
2022-07-01 17:03:50 +0530

    fix: [PL-22567]: get, list graphql tests (#34896)
    * [CDS-38270]: Made changes in WorkflowExecutionServiceHelper.java and its test class
    
    * [PL-22567]: Wrote unit tests for custom secret manager
    
    * [PL-22567]: Wrote unit tests for custom secret manager
    
    * [PL-22567]: Wrote unit tests for custom secret manager
    
    * [PL-22567]: Wrote unit tests for custom secret manager
    
    * [PL-22567]: Wrote unit tests for custom secret manager
    
    * [PL-22567]: Wrote unit tests for custom secret manager
    
    * [PL-22567]: Wrote unit tests for custom secret manager
    
    * [PL-22567]: Wrote unit tests for custom secret manager

--- 220-graphql-test/src/test/java/io/harness/SecretManagerTest.java
+++ 220-graphql-test/src/test/java/io/harness/SecretManagerTest.java
@@ -8,28 +8,32 @@
 package io.harness;
 
 import static io.harness.rule.OwnerRule.ABHINAV;
+import static io.harness.rule.OwnerRule.MITISHA;
 import static io.harness.rule.OwnerRule.VOJIN;
 
 import static com.google.common.collect.Sets.newHashSet;
-import static io.github.benas.randombeans.api.EnhancedRandom.random;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import io.harness.beans.SecretManagerConfig;
 import io.harness.category.element.UnitTests;
 import io.harness.category.layer.GraphQLTests;
 import io.harness.generator.AccountGenerator;
+import io.harness.generator.OwnerManager;
+import io.harness.generator.Randomizer;
+import io.harness.generator.TemplateGenerator;
 import io.harness.multiline.MultilineStringMixin;
 import io.harness.rule.Owner;
 import io.harness.secretmanagers.SecretManagerConfigService;
+import io.harness.security.encryption.EncryptedDataParams;
 import io.harness.security.encryption.EncryptionType;
 import io.harness.serializer.JsonUtils;
 import io.harness.testframework.graphql.QLTestObject;
 
 import software.wings.beans.Account;
-import software.wings.beans.AccountType;
 import software.wings.beans.AwsSecretsManagerConfig;
 import software.wings.beans.KmsConfig;
 import software.wings.beans.VaultConfig;
+import software.wings.beans.template.Template;
 import software.wings.graphql.schema.type.QLEnvFilterType;
 import software.wings.graphql.schema.type.QLGenericFilterType;
 import software.wings.graphql.schema.type.secretManagers.QLSecretManager.QLSecretManagerKeys;
@@ -39,9 +43,13 @@ import software.wings.graphql.schema.type.secrets.QLUsageScope;
 import software.wings.security.EnvFilter;
 import software.wings.security.GenericEntityFilter;
 import software.wings.security.UsageRestrictions;
+import software.wings.security.encryption.secretsmanagerconfigs.CustomSecretsManagerConfig;
 
 import com.google.common.collect.Iterables;
 import com.google.inject.Inject;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Set;
 import org.junit.Before;
@@ -51,17 +59,154 @@ import org.junit.experimental.categories.Category;
 public class SecretManagerTest extends GraphQLTest {
   @Inject private AccountGenerator accountGenerator;
   @Inject SecretManagerConfigService secretManagerConfigService;
+  @Inject private TemplateGenerator templateGenerator;
+  @Inject private OwnerManager ownerManager;
   Account account;
   SecretManagerConfig secretManagerConfig;
+  CustomSecretsManagerConfig customSecretsManagerConfig;
   String SECRET_MANAGER_NAME = "test secret manager";
+  String CUSTOM_SECRET_MANAGER_NAME = "custom test secret manager";
+  Randomizer.Seed seed;
+  OwnerManager.Owners owners;
 
   @Before
   public void setUp() {
-    account = accountGenerator.ensureAccount(random(String.class), random(String.class), AccountType.TRIAL);
+    seed = new Randomizer.Seed(0);
+    owners = ownerManager.create();
+    account = accountGenerator.ensurePredefined(seed, owners, AccountGenerator.Accounts.GENERIC_TEST);
     secretManagerConfig = AwsSecretsManagerConfig.builder().name(SECRET_MANAGER_NAME).build();
     secretManagerConfig.setAccountId(account.getUuid());
     secretManagerConfig.setUsageRestrictions(createUsageRestrictions());
     secretManagerConfigService.save(secretManagerConfig);
+    customSetup();
+  }
+
+  public void customSetup() {
+    Template shellScriptTemplate =
+        templateGenerator.ensurePredefined(seed, owners, TemplateGenerator.Templates.SHELL_SCRIPT);
+    Set<String> delegateSelectors = new HashSet<>();
+    delegateSelectors.add("delegate1");
+    Set<EncryptedDataParams> testVariables = new HashSet<>();
+    testVariables.add(EncryptedDataParams.builder().name("testVariable1").value("testValue1").build());
+    customSecretsManagerConfig = CustomSecretsManagerConfig.builder()
+                                     .name(CUSTOM_SECRET_MANAGER_NAME)
+                                     .templateId(shellScriptTemplate.getUuid())
+                                     .executeOnDelegate(true)
+                                     .isConnectorTemplatized(true)
+                                     .host("host")
+                                     .commandPath("/tmp")
+                                     .connectorId("ConnectorId")
+                                     .isDefault(false)
+                                     .delegateSelectors(delegateSelectors)
+                                     .testVariables(testVariables)
+                                     .build();
+    customSecretsManagerConfig.setAccountId(account.getUuid());
+    customSecretsManagerConfig.setUsageRestrictions(createUsageRestrictions());
+    secretManagerConfigService.save(customSecretsManagerConfig);
+  }
+
+  @Test
+  @Owner(developers = MITISHA)
+  @Category({GraphQLTests.class, UnitTests.class})
+  public void testCustomSecretManagerQuery() {
+    String secretManagerQueryPattern = MultilineStringMixin.$.GQL(/*
+{
+secretManager(secretManagerId: "%s"){
+id
+name
+config{
+... on CustomSecretManagerConfig {
+templateId
+executeOnDelegate
+isConnectorTemplatized
+host
+commandPath
+connectorId
+isDefault
+delegateSelectors
+testVariables {
+  name
+  value
+  }
+}
+}
+}
+}
+*/ SecretManagerTest.class);
+    String query = String.format(secretManagerQueryPattern, customSecretsManagerConfig.getUuid());
+    final QLTestObject qlSecretManagerObject = qlExecute(query, account.getUuid());
+    assertThat(qlSecretManagerObject.get(QLSecretManagerKeys.id)).isEqualTo(customSecretsManagerConfig.getUuid());
+    assertThat(qlSecretManagerObject.get(QLSecretManagerKeys.name)).isEqualTo(customSecretsManagerConfig.getName());
+    LinkedHashMap testConfig = (LinkedHashMap) qlSecretManagerObject.get("config");
+    assertThat(testConfig.get("host")).isEqualTo(customSecretsManagerConfig.getHost());
+    assertThat(testConfig.get("templateId")).isEqualTo(customSecretsManagerConfig.getTemplateId());
+    assertThat(testConfig.get("isDefault")).isEqualTo(customSecretsManagerConfig.isDefault());
+    ArrayList<String> delegateSelectors = (ArrayList<String>) testConfig.get("delegateSelectors");
+    assertThat(delegateSelectors.contains("delegate1"));
+    assertThat(testConfig.get("connectorId")).isEqualTo(customSecretsManagerConfig.getConnectorId());
+    assertThat(testConfig.get("commandPath")).isEqualTo(customSecretsManagerConfig.getCommandPath());
+    assertThat(testConfig.get("isConnectorTemplatized")).isEqualTo(customSecretsManagerConfig.isConnectorTemplatized());
+    assertThat(testConfig.get("executeOnDelegate")).isEqualTo(customSecretsManagerConfig.isExecuteOnDelegate());
+    ArrayList<LinkedHashMap> testVariables = (ArrayList<LinkedHashMap>) testConfig.get("testVariables");
+    LinkedHashMap newObj = testVariables.get(0);
+    assertThat(newObj.get("name")).isEqualTo("testVariable1");
+    assertThat(newObj.get("value")).isEqualTo("testValue1");
+  }
+
+  @Test
+  @Owner(developers = MITISHA)
+  @Category({GraphQLTests.class, UnitTests.class})
+  public void testCustomSecretManagerListQuery() {
+    String query = MultilineStringMixin.$.GQL(/*
+{
+secretManagers(limit:10){
+nodes{
+  id
+  name
+  config{
+... on CustomSecretManagerConfig {
+templateId
+executeOnDelegate
+isConnectorTemplatized
+host
+commandPath
+connectorId
+isDefault
+delegateSelectors
+testVariables {
+  name
+  value
+  }
+}
+}
+}
+}
+}
+*/ SecretManagerTest.class);
+    addAzureAndAwsKmsSecretManagers();
+    saveAwsSecretManagerWithDifferentAccount();
+    final QLTestObject qlSecretManagerObject = qlExecute(query, account.getUuid());
+    List<LinkedHashMap> secretManagerNodes = (List<LinkedHashMap>) qlSecretManagerObject.get("nodes");
+    assertThat(secretManagerNodes.size()).isEqualTo(4);
+    LinkedHashMap testConfig = new LinkedHashMap();
+    for (LinkedHashMap obj : secretManagerNodes) {
+      if (obj.get("config") != null) {
+        testConfig = (LinkedHashMap) obj.get("config");
+      }
+    }
+    assertThat(testConfig.get("host")).isEqualTo(customSecretsManagerConfig.getHost());
+    assertThat(testConfig.get("templateId")).isEqualTo(customSecretsManagerConfig.getTemplateId());
+    assertThat(testConfig.get("isDefault")).isEqualTo(customSecretsManagerConfig.isDefault());
+    ArrayList<String> delegateSelectors = (ArrayList<String>) testConfig.get("delegateSelectors");
+    assertThat(delegateSelectors.contains("delegate1"));
+    assertThat(testConfig.get("connectorId")).isEqualTo(customSecretsManagerConfig.getConnectorId());
+    assertThat(testConfig.get("commandPath")).isEqualTo(customSecretsManagerConfig.getCommandPath());
+    assertThat(testConfig.get("isConnectorTemplatized")).isEqualTo(customSecretsManagerConfig.isConnectorTemplatized());
+    assertThat(testConfig.get("executeOnDelegate")).isEqualTo(customSecretsManagerConfig.isExecuteOnDelegate());
+    ArrayList<LinkedHashMap> testVariables = (ArrayList<LinkedHashMap>) testConfig.get("testVariables");
+    LinkedHashMap newObj = testVariables.get(0);
+    assertThat(newObj.get("name")).isEqualTo("testVariable1");
+    assertThat(newObj.get("value")).isEqualTo("testValue1");
   }
 
   @Test
@@ -70,10 +215,10 @@ public class SecretManagerTest extends GraphQLTest {
   public void testSecretManagerQuery() {
     String secretManagerQueryPattern = MultilineStringMixin.$.GQL(/*
 {
-    secretManager(secretManagerId: "%s"){
-    id
-    name
-  }
+secretManager(secretManagerId: "%s"){
+id
+name
+}
 }
 */ SecretManagerTest.class);
     String query = String.format(secretManagerQueryPattern, secretManagerConfig.getUuid());
@@ -88,10 +233,10 @@ public class SecretManagerTest extends GraphQLTest {
   public void testSecretManagerGetByNameQuery() {
     String secretManagerQueryPattern = MultilineStringMixin.$.GQL(/*
 {
-    secretManagerByName(name: "%s"){
-    id
-    name
-  }
+secretManagerByName(name: "%s"){
+id
+name
+}
 }
 */ SecretManagerTest.class);
     String query = String.format(secretManagerQueryPattern, SECRET_MANAGER_NAME);
@@ -106,19 +251,19 @@ public class SecretManagerTest extends GraphQLTest {
   public void testSecretManagerListQuery() {
     String query = MultilineStringMixin.$.GQL(/*
 {
-  secretManagers(limit:10){
-    nodes{
-      id
-      name
-    }
-  }
+secretManagers(limit:10){
+nodes{
+  id
+  name
+}
+}
 }
 */ SecretManagerTest.class);
     addAzureAndAwsKmsSecretManagers();
     saveAwsSecretManagerWithDifferentAccount();
     final QLTestObject qlSecretManagerObject = qlExecute(query, account.getUuid());
     List secretManagerNodes = (List) qlSecretManagerObject.get("nodes");
-    assertThat(secretManagerNodes.size()).isEqualTo(3);
+    assertThat(secretManagerNodes.size()).isEqualTo(4);
   }
 
   @Test
@@ -135,20 +280,20 @@ public class SecretManagerTest extends GraphQLTest {
   public void testSecretManagerQuery_usageScope() {
     String secretManagerQueryPattern = MultilineStringMixin.$.GQL(/*
 {
-    secretManager(secretManagerId: "%s"){
-    usageScope{
-      appEnvScopes{
-        application {
-          appId
-          filterType
-        }
-        environment {
-          envId
-          filterType
-        }
-      }
+secretManager(secretManagerId: "%s"){
+usageScope{
+  appEnvScopes{
+    application {
+      appId
+      filterType
+    }
+    environment {
+      envId
+      filterType
     }
   }
+}
+}
 }
 */ SecretManagerTest.class);
     String query = String.format(secretManagerQueryPattern, secretManagerConfig.getUuid());
