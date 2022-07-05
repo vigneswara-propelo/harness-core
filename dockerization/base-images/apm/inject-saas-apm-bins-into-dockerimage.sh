f52c3b48418832faf43b1f5c2dce59b9ea5a1c06
Diptiman Adak <diptiman.adak@harness.io>
2022-07-05 12:31:20 +0530

    [feat]: [BT-1254]: Adding open telemetry agent (#34927)

--- dockerization/base-images/apm/inject-saas-apm-bins-into-dockerimage.sh
+++ dockerization/base-images/apm/inject-saas-apm-bins-into-dockerimage.sh
@@ -13,10 +13,14 @@ function download_saas_apm_binaries(){
 	rm /opt/harness/harness-et-agent.tar.gz
 
 	curl ${APPD_AGENT} --output ${APPD_AGENT##*/}; STATUS2=$?
-	echo "INFO: Download Status: ${APPD_AGENT##*/}: $STATUS3"
+	echo "INFO: Download Status: ${APPD_AGENT##*/}: $STATUS2"
 	chmod 711 ${APPD_AGENT##*/}
 
-	if [ "${STATUS1}" -eq 0 ] && [ "${STATUS2}" -eq 0 ] ; then
+	curl ${OT_AGENT} --output ${OT_AGENT##*/}; STATUS3=$?
+	echo "INFO: Download Status: ${OT_AGENT##*/}: $STATUS3"
+	chmod 711 ${OT_AGENT##*/}
+
+	if [ "${STATUS1}" -eq 0 ] && [ "${STATUS2}" -eq 0 ] && [ "${STATUS3}" -eq 0 ] ; then
 		echo "Download Finished..."
 	else
 		echo "Failed to Download Saas APM Binaries. Exiting..."
@@ -26,6 +30,7 @@ function download_saas_apm_binaries(){
 
 export APPD_AGENT='https://harness.jfrog.io/artifactory/BuildsTools/docker/apm/appd/AppServerAgent-1.8-21.11.2.33305.zip'
 export ET_AGENT='https://get.et.harness.io/releases/latest/nix/harness-et-agent.tar.gz'
+export OT_AGENT='https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/latest/download/opentelemetry-javaagent.jar'
 
 echo "STEP 1: INFO: Downloading APM Binaries Locally..."
 download_saas_apm_binaries
