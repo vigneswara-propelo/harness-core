1354b4dbd22a334606460db32b37c2ec3da46d6b
Marko Lukic <marko.lukic@harness.io>
2022-07-06 13:43:23 +0200

    [CDS-39357]: exporting k8sInfraDelegateConfig into sweeping output K8S_INFRA_DELEGATE_CONFIG_OUTPUT_NAME (#35066)

--- 125-cd-nextgen/src/main/java/io/harness/cdng/infra/steps/InfrastructureStep.java
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/infra/steps/InfrastructureStep.java
@@ -23,6 +23,7 @@ import io.harness.cdng.CDStepHelper;
 import io.harness.cdng.infra.InfrastructureMapper;
 import io.harness.cdng.infra.beans.InfraMapping;
 import io.harness.cdng.infra.beans.InfrastructureOutcome;
+import io.harness.cdng.infra.beans.K8sAzureInfrastructureOutcome;
 import io.harness.cdng.infra.beans.K8sDirectInfrastructureOutcome;
 import io.harness.cdng.infra.beans.K8sGcpInfrastructureOutcome;
 import io.harness.cdng.infra.yaml.AzureWebAppInfrastructure;
@@ -193,7 +194,8 @@ public class InfrastructureStep implements SyncExecutableWithRbac<Infrastructure
     }
 
     if (infrastructureOutcome instanceof K8sGcpInfrastructureOutcome
-        || infrastructureOutcome instanceof K8sDirectInfrastructureOutcome) {
+        || infrastructureOutcome instanceof K8sDirectInfrastructureOutcome
+        || infrastructureOutcome instanceof K8sAzureInfrastructureOutcome) {
       K8sInfraDelegateConfig k8sInfraDelegateConfig =
           cdStepHelper.getK8sInfraDelegateConfig(infrastructureOutcome, ambiance);
 
