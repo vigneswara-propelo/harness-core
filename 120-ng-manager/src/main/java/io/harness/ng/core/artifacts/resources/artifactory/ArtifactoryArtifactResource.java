8ebab9e6530e557b63823bf900df77be04b6e220
Nasser Gonzalez Gonzalez <nasser.gonzalez@harness.io>
2022-07-28 12:15:59 +0100

    [CDS-40882]: Refactor Artifactory repositoryDetails endpoint (#35786)

--- 120-ng-manager/src/main/java/io/harness/ng/core/artifacts/resources/artifactory/ArtifactoryArtifactResource.java
+++ 120-ng-manager/src/main/java/io/harness/ng/core/artifacts/resources/artifactory/ArtifactoryArtifactResource.java
@@ -180,11 +180,22 @@ public class ArtifactoryArtifactResource {
   @Path("repositoriesDetails")
   @ApiOperation(value = "Gets repository details", nickname = "getRepositoriesDetailsForArtifactory")
   public ResponseDTO<ArtifactoryRepoDetailsDTO> getRepositoriesDetails(
-      @NotNull @QueryParam("connectorRef") String artifactoryConnectorIdentifier,
+      @QueryParam("connectorRef") String artifactoryConnectorIdentifier,
       @QueryParam("repositoryType") @DefaultValue("any") String repositoryType,
       @NotNull @QueryParam(NGCommonEntityConstants.ACCOUNT_KEY) String accountId,
       @NotNull @QueryParam(NGCommonEntityConstants.ORG_KEY) String orgIdentifier,
-      @NotNull @QueryParam(NGCommonEntityConstants.PROJECT_KEY) String projectIdentifier) {
+      @NotNull @QueryParam(NGCommonEntityConstants.PROJECT_KEY) String projectIdentifier,
+      @QueryParam("fqnPath") String fqnPath, @QueryParam(NGCommonEntityConstants.SERVICE_KEY) String serviceRef) {
+    if (isNotEmpty(serviceRef)) {
+      final ArtifactConfig artifactSpecFromService = artifactResourceUtils.locateArtifactInService(
+          accountId, orgIdentifier, projectIdentifier, serviceRef, fqnPath);
+      ArtifactoryRegistryArtifactConfig artifactoryRegistryArtifactConfig =
+          (ArtifactoryRegistryArtifactConfig) artifactSpecFromService;
+
+      if (isEmpty(artifactoryConnectorIdentifier)) {
+        artifactoryConnectorIdentifier = artifactoryRegistryArtifactConfig.getConnectorRef().getValue();
+      }
+    }
     IdentifierRef connectorRef = IdentifierRefHelper.getIdentifierRef(
         artifactoryConnectorIdentifier, accountId, orgIdentifier, projectIdentifier);
     ArtifactoryRepoDetailsDTO repoDetailsDTO =
