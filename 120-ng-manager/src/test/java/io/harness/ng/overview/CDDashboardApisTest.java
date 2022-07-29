bca4d61c622cbb2408a3fd673868cc24b52af19d
Shalini Agrawal <shalini.agrawal@harness.io>
2022-07-29 19:24:01 +0530

    fix: [PIE-0]: fixed a test (#35862)

--- 120-ng-manager/src/test/java/io/harness/ng/overview/CDDashboardApisTest.java
+++ 120-ng-manager/src/test/java/io/harness/ng/overview/CDDashboardApisTest.java
@@ -22,6 +22,7 @@ import io.harness.CategoryTest;
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.category.element.UnitTests;
+import io.harness.cd.CDDashboardServiceHelper;
 import io.harness.ng.core.dashboard.AuthorInfo;
 import io.harness.ng.core.dashboard.DashboardExecutionStatusInfo;
 import io.harness.ng.core.dashboard.EnvironmentDeploymentsInfo;
@@ -76,8 +77,7 @@ public class CDDashboardApisTest extends CategoryTest {
   @Mock TimeScaleDBService timeScaleDBService;
   @InjectMocks @Spy private CDOverviewDashboardServiceImpl cdOverviewDashboardServiceImpl;
 
-  private List<String> failedStatusList = Arrays.asList(ExecutionStatus.FAILED.name(), ExecutionStatus.ABORTED.name(),
-      ExecutionStatus.EXPIRED.name(), ExecutionStatus.IGNOREFAILED.name(), ExecutionStatus.ERRORED.name());
+  private List<String> failedStatusList = CDDashboardServiceHelper.failedStatusList;
   private List<String> activeStatusList = Arrays.asList(ExecutionStatus.RUNNING.name(),
       ExecutionStatus.ASYNCWAITING.name(), ExecutionStatus.TASKWAITING.name(), ExecutionStatus.TIMEDWAITING.name(),
       ExecutionStatus.PAUSED.name(), ExecutionStatus.PAUSING.name());
