b3581ed72ddeeb99eea06dcd7471b0770998e801
Rutvij Mehta <rutvij.mehta@harness.io>
2022-08-03 23:57:13 -0700

    feat: [CI-5166]: CI independent branch cut and build changes (#36020)
    * [CI-5166]: Release branch and build shell script
    
    * [CI-5166]: Release branch and build shell script

--- 332-ci-manager/build/build_jar.sh
+++ 332-ci-manager/build/build_jar.sh
@@ -22,7 +22,7 @@ if [ "${PLATFORM}" == "jenkins" ]; then
   module=332-ci-manager
   moduleName=ci-manager
 
-  bazel query "deps(//${module}/service:module)" | grep -i "KryoRegistrar" | rev | cut -f 1 -d "/" | rev | cut -f 1 -d "." > /tmp/KryoDeps.text
+  bazel query "deps(//${module}/app:module)" | grep -i "KryoRegistrar" | rev | cut -f 1 -d "/" | rev | cut -f 1 -d "." > /tmp/KryoDeps.text
   cp scripts/interface-hash/module-deps.sh .
   sh module-deps.sh //${module}/service:module > /tmp/ProtoDeps.text
   bazel ${bazelrc} run ${BAZEL_ARGUMENTS}  //001-microservice-intfc-tool:module -- kryo-file=/tmp/KryoDeps.text proto-file=/tmp/ProtoDeps.text ignore-json | grep "Codebase Hash:" > ${moduleName}-protocol.info
--- 332-ci-manager/release/release_build.sh
+++ 332-ci-manager/release/release_build.sh
@@ -1,5 +1,17 @@
+#!/usr/bin/env bash
 # Copyright 2022 Harness Inc. All rights reserved.
 # Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
 # that can be found in the licenses directory at the root of this repository, also available at
 # https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
 
+set -ex
+
+export PLATFORM="jenkins"
+
+export VERSION_FILE=332-ci-manager/build.properties
+export BUILD=`cat ${VERSION_FILE} | grep 'build.number=' | sed -e 's: *build.number=::g'`
+export PATCH=`cat ${VERSION_FILE} | grep 'build.patch=' | sed -e 's: *build.patch=::g'`
+export VERSION=$BUILD-$PATCH
+
+chmod +x 332-ci-manager/build/feature_build.sh
+332-ci-manager/build/feature_build.sh
