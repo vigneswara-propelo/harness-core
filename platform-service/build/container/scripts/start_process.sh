2e6a5add1590dd6a0b48ee893ee8a845add20578
Kshitij Rastogi <kshitij.rastogi@harness.io>
2022-08-04 12:16:57 +0530

    fix: [PL-27049] Adding open telemetry changes for platform and access-control (#35979)
    * fix: [PL-27049] Adding open telemetry changes for platform and access-control
    
    * Correcting service names

--- platform-service/build/container/scripts/start_process.sh
+++ platform-service/build/container/scripts/start_process.sh
@@ -40,6 +40,16 @@ if [[ "${ENABLE_APPDYNAMICS}" == "true" ]]; then
     echo "Using Appdynamics java agent"
 fi
 
+if [[ "${ENABLE_OPENTELEMETRY}" == "true" ]] ; then
+    echo "OpenTelemetry is enabled"
+    JAVA_OPTS=$JAVA_OPTS" -javaagent:/opt/harness/opentelemetry-javaagent.jar -Dotel.service.name=${OTEL_SERVICE_NAME:-platform-service}"
+
+    if [ -n "$OTEL_EXPORTER_OTLP_ENDPOINT" ]; then
+        JAVA_OPTS=$JAVA_OPTS" -Dotel.exporter.otlp.endpoint=$OTEL_EXPORTER_OTLP_ENDPOINT "
+    fi
+    echo "Using OpenTelemetry Java Agent"
+fi
+
 if [[ "${DEPLOY_MODE}" == "KUBERNETES" || "${DEPLOY_MODE}" == "KUBERNETES_ONPREM" || "${DEPLOY_VERSION}" == "COMMUNITY" ]]; then
     java $JAVA_OPTS -jar $CAPSULE_JAR $COMMAND /opt/harness/config.yml
 else
