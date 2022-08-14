a8046b1e14c78995161db05af31c54b1fda460ff
Anupam Jaiswal <102655013+AnupamIO@users.noreply.github.com>
2022-08-15 01:57:19 +0530

    [DEL-4338]: remove delegate build.prop version upgrade from root release-branch.sh (#36276)

--- 260-delegate/release-branch.sh
+++ 260-delegate/release-branch.sh
@@ -86,8 +86,15 @@ export VERSION_FILE=build.properties
 export VERSION=`cat ${VERSION_FILE} | grep 'build.number=' | sed -e 's: *build.number=::g'`
 export VERSION=${VERSION%??}
 export NEW_VERSION=$(( ${VERSION}+1 ))
+export MAJOR_VERSION=`cat ${VERSION_FILE} | grep 'build.majorVersion=' | sed -e 's: *build.majorVersion=::g'`
+export MINOR_VERSION=`cat ${VERSION_FILE} | grep 'build.minorVersion=' | sed -e 's: *build.minorVersion=::g'`
+
+YEAR=$(date +%y)
+MONTH=$(date +%m)
 
 # sed -i "s:build.number=${VERSION}00:build.number=${NEW_VERSION}00:g" ${VERSION_FILE}
+# sed -i "s:build.majorVersion=${MAJOR_VERSION}:build.majorVersion=${YEAR}:g" ${VERSION_FILE}
+# sed -i "s:build.minorVersion=${MINOR_VERSION}:build.minorVersion=${MONTH}:g" ${VERSION_FILE}
 
 # git add ${VERSION_FILE}
 # git commit -m "Branching to release/${PURPOSE}/${VERSION}xx. New version ${NEW_VERSION}xx"
