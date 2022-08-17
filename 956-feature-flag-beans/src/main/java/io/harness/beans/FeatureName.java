b80204ae65640f33022fdec3964e0421f2d9663b
Sridhar <106265061+sribalij@users.noreply.github.com>
2022-08-16 23:05:06 -0700

    feat: [PIE-4792]: Add a feature flag with name TRIGGERS_REFACTOR (#36439)
    * [PIE-4792]: Add a feature flag with name TRIGGERS_REFACTOR
    
    * [PIE-4792]: Add a feature flag with name TRIGGERS_REFACTOR- Updated text

--- 956-feature-flag-beans/src/main/java/io/harness/beans/FeatureName.java
+++ 956-feature-flag-beans/src/main/java/io/harness/beans/FeatureName.java
@@ -413,7 +413,8 @@ public enum FeatureName {
   CUSTOM_SECRET_MANAGER_NG("Enable Custom Secret Manager in NG", HarnessTeam.PL),
   AZURE_ARM_BP_NG("Used to allow customers to access ARM/BP capabilities", HarnessTeam.CDP),
   CV_AWS_PROMETHEUS("Enable AWS Prometheus for CV State", HarnessTeam.CV),
-  GIT_WEBHOOK_POLLING("Used to poll git webhook recent delivery events", HarnessTeam.CDP);
+  GIT_WEBHOOK_POLLING("Used to poll git webhook recent delivery events", HarnessTeam.CDP),
+  TRIGGERS_REFACTOR("Enable NG Triggers UI refactoring", HarnessTeam.CDP);
   @Deprecated
   FeatureName() {
     scope = Scope.PER_ACCOUNT;
