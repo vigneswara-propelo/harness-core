dc20331f13f19713fd2ec6c717ff13ef05945c2b
Varsha Lalwani <varsha.lalwani@harness.io>
2022-08-23 14:44:54 +0530

    [DEL-0]: Update authorsList (#36638)

--- 990-commons-test/src/main/java/io/harness/rule/OwnerRule.java
+++ 990-commons-test/src/main/java/io/harness/rule/OwnerRule.java
@@ -293,6 +293,7 @@ public class OwnerRule implements TestRule {
   public static final String JAMES_RICKS = "james.ricks";
   public static final String GAURAV_NANDA = "gaurav.nanda";
   public static final String ANKIT_TIWARI = "ankit.tiwari";
+  public static final String VARSHA_LALWANI = "varsha.lalwani";
   @Deprecated public static final String UNKNOWN = "unknown";
 
   private static UserInfoBuilder defaultUserInfo(String user) {
@@ -517,6 +518,7 @@ public class OwnerRule implements TestRule {
           .put(JAMES_RICKS, defaultUserInfo(JAMES_RICKS).slack("U03FVTUD5J7").team(CI).build())
           .put(GAURAV_NANDA, defaultUserInfo(GAURAV_NANDA).slack("U03E7JHB70C").team(PL).build())
           .put(ANKIT_TIWARI, defaultUserInfo(ANKIT_TIWARI).slack("U03S7RGTEBS").team(CDC).build())
+          .put(VARSHA_LALWANI, defaultUserInfo(VARSHA_LALWANI).slack("U03U3QQDQL8").team(CV).build())
           .build();
 
   private static String prDeveloperId = findDeveloperId(System.getenv(GHPRB_PULL_AUTHOR_EMAIL));
