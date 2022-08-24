f7ced3e3be3fd9a2682b2b2a12b1e45310883966
Tejas Tiwari <tejas.tiwari@harness.io>
2022-08-24 17:56:45 +0530

    feat: [PL-27559]: Changed permission for list APIs (#36711)
    * [PL-27559]: Changed permission for list APIs
    
    * [PL-27559]: Removed unused import

--- 400-rest/src/main/java/software/wings/resources/secretsmanagement/SecretManagementResource.java
+++ 400-rest/src/main/java/software/wings/resources/secretsmanagement/SecretManagementResource.java
@@ -14,8 +14,8 @@ import static io.harness.eraro.ErrorCode.SECRET_MANAGEMENT_ERROR;
 import static io.harness.exception.WingsException.SRE;
 import static io.harness.logging.AutoLogContext.OverrideBehavior.OVERRIDE_ERROR;
 
+import static software.wings.security.PermissionAttribute.PermissionType.LOGGED_IN;
 import static software.wings.security.PermissionAttribute.PermissionType.MANAGE_SECRETS;
-import static software.wings.security.PermissionAttribute.PermissionType.MANAGE_SECRET_MANAGERS;
 import static software.wings.security.PermissionAttribute.ResourceType.SETTING;
 
 import static javax.ws.rs.core.MediaType.MULTIPART_FORM_DATA;
@@ -134,7 +134,7 @@ public class SecretManagementResource {
 
   @GET
   @Path("/list-configs")
-  @ApiKeyAuthorized(permissionType = MANAGE_SECRET_MANAGERS, action = PermissionAttribute.Action.READ)
+  @ApiKeyAuthorized(permissionType = LOGGED_IN, action = PermissionAttribute.Action.READ)
   public RestResponse<List<SecretManagerConfig>> listEncryptionConfig(@QueryParam("accountId") final String accountId) {
     return new RestResponse<>(secretManager.listSecretManagers(accountId));
   }
@@ -343,7 +343,7 @@ public class SecretManagementResource {
 
   @GET
   @Path("/list-secrets-page")
-  @ApiKeyAuthorized(permissionType = MANAGE_SECRETS, action = PermissionAttribute.Action.READ)
+  @ApiKeyAuthorized(permissionType = LOGGED_IN, action = PermissionAttribute.Action.READ)
   public RestResponse<PageResponse<EncryptedData>> listSecrets(@QueryParam("accountId") final String accountId,
       @QueryParam("type") final SettingVariableTypes type, @QueryParam("currentAppId") String currentAppId,
       @QueryParam("currentEnvId") String currentEnvId, @DefaultValue("true") @QueryParam("details") boolean details,
