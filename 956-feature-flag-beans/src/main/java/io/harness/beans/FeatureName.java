2afad7cff0a1dffdb1f974c15cf90fa777ff4037
Ved Tambat <ved.atultambat@harness.io>
2022-09-05 17:46:28 +0530

    [CDS-25409]: feature flag (#37008)

--- 956-feature-flag-beans/src/main/java/io/harness/beans/FeatureName.java
+++ 956-feature-flag-beans/src/main/java/io/harness/beans/FeatureName.java
@@ -358,6 +358,7 @@ public enum FeatureName {
   TEMPLATE_SCHEMA_VALIDATION,
   YAML_APIS_GRANULAR_PERMISSION,
   JENKINS_BUILD,
+  GITHUB_PACKAGES,
   DO_NOT_RENEW_APPROLE_TOKEN(
       "CAUTION: USE THIS ONLY WHEN THE CUSTOMER DELEGATE IS IN VERSION HIGHER OR EQUAL TO 764xx. Used for disabling appRole token renewal and fetching token on the fly before CRUD",
       HarnessTeam.PL),
