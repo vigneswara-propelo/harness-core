a4c0a02202f5a850f3a419476ba22b9922c8e689
Tudor Macari <tudor.macari@harness.io>
2022-09-07 15:06:22 +0300

    [fix]: [CDS-42898]: fix yaml schema (#37258)

--- 125-cd-nextgen/src/main/java/io/harness/cdng/pipeline/CDStepInfo.java
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/pipeline/CDStepInfo.java
@@ -10,10 +10,10 @@ package io.harness.cdng.pipeline;
 import io.harness.advisers.rollback.OnFailRollbackParameters;
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
-import io.harness.cdng.azure.webapp.AzureWebAppRollbackStep;
-import io.harness.cdng.azure.webapp.AzureWebAppSlotDeploymentStep;
-import io.harness.cdng.azure.webapp.AzureWebAppSwapSlotStep;
-import io.harness.cdng.azure.webapp.AzureWebAppTrafficShiftStep;
+import io.harness.cdng.azure.webapp.AzureWebAppRollbackStepInfo;
+import io.harness.cdng.azure.webapp.AzureWebAppSlotDeploymentStepInfo;
+import io.harness.cdng.azure.webapp.AzureWebAppSwapSlotStepInfo;
+import io.harness.cdng.azure.webapp.AzureWebAppTrafficShiftStepInfo;
 import io.harness.cdng.ecs.EcsCanaryDeleteStepInfo;
 import io.harness.cdng.ecs.EcsCanaryDeployStepInfo;
 import io.harness.cdng.ecs.EcsRollingDeployStepInfo;
@@ -56,18 +56,19 @@ import io.harness.yaml.core.StepSpecType;
 
 import io.swagger.annotations.ApiModel;
 
-@ApiModel(subTypes = {CreatePRStepInfo.class, MergePRStepInfo.class, K8sApplyStepInfo.class, K8sBlueGreenStepInfo.class,
-              K8sCanaryStepInfo.class, K8sRollingStepInfo.class, K8sRollingRollbackStepInfo.class,
-              K8sScaleStepInfo.class, K8sDeleteStepInfo.class, K8sBGSwapServicesStepInfo.class,
-              K8sCanaryDeleteStepInfo.class, TerraformApplyStepInfo.class, TerraformPlanStepInfo.class,
-              TerraformDestroyStepInfo.class, TerraformRollbackStepInfo.class, HelmDeployStepInfo.class,
-              HelmRollbackStepInfo.class, CloudformationCreateStackStepInfo.class,
-              CloudformationDeleteStackStepInfo.class, ServerlessAwsLambdaDeployStepInfo.class,
-              ServerlessAwsLambdaRollbackStepInfo.class, CloudformationRollbackStepInfo.class, CommandStepInfo.class,
-              AzureWebAppSlotDeploymentStep.class, AzureWebAppTrafficShiftStep.class, AzureWebAppSwapSlotStep.class,
-              AzureWebAppRollbackStep.class, JenkinsBuildStepInfo.class, AzureCreateARMResourceStepInfo.class,
-              AzureCreateBPStepInfo.class, AzureARMRollbackStepInfo.class, EcsRollingDeployStepInfo.class,
-              EcsRollingRollbackStepInfo.class, EcsCanaryDeployStepInfo.class, EcsCanaryDeleteStepInfo.class})
+@ApiModel(
+    subTypes = {CreatePRStepInfo.class, MergePRStepInfo.class, K8sApplyStepInfo.class, K8sBlueGreenStepInfo.class,
+        K8sCanaryStepInfo.class, K8sRollingStepInfo.class, K8sRollingRollbackStepInfo.class, K8sScaleStepInfo.class,
+        K8sDeleteStepInfo.class, K8sBGSwapServicesStepInfo.class, K8sCanaryDeleteStepInfo.class,
+        TerraformApplyStepInfo.class, TerraformPlanStepInfo.class, TerraformDestroyStepInfo.class,
+        TerraformRollbackStepInfo.class, HelmDeployStepInfo.class, HelmRollbackStepInfo.class,
+        CloudformationCreateStackStepInfo.class, CloudformationDeleteStackStepInfo.class,
+        ServerlessAwsLambdaDeployStepInfo.class, ServerlessAwsLambdaRollbackStepInfo.class,
+        CloudformationRollbackStepInfo.class, CommandStepInfo.class, AzureWebAppSlotDeploymentStepInfo.class,
+        AzureWebAppTrafficShiftStepInfo.class, AzureWebAppSwapSlotStepInfo.class, AzureWebAppRollbackStepInfo.class,
+        JenkinsBuildStepInfo.class, AzureCreateARMResourceStepInfo.class, AzureCreateBPStepInfo.class,
+        AzureARMRollbackStepInfo.class, EcsRollingDeployStepInfo.class, EcsRollingRollbackStepInfo.class,
+        EcsCanaryDeployStepInfo.class, EcsCanaryDeleteStepInfo.class})
 
 @OwnedBy(HarnessTeam.CDC)
 public interface CDStepInfo extends StepSpecType, WithStepElementParameters, WithDelegateSelector {
