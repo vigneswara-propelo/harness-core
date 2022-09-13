839c1b64591a2b9cd4d01d1ebcc2c6a21070d422
Jimit Gandhi <jimit.gandhi@harness.io>
2022-09-13 11:04:03 +0530

    Fetching all accounts using accounts API. (#37432)

--- 123-ng-core-user/src/main/java/io/harness/ng/core/DefaultUserGroupCreationService.java
+++ 123-ng-core-user/src/main/java/io/harness/ng/core/DefaultUserGroupCreationService.java
@@ -12,24 +12,28 @@ import static io.harness.data.structure.EmptyPredicate.isEmpty;
 
 import static java.lang.Boolean.FALSE;
 
+import io.harness.account.AccountClient;
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.beans.Scope;
 import io.harness.lock.AcquiredLock;
 import io.harness.lock.PersistentLocker;
 import io.harness.ng.core.api.DefaultUserGroupService;
+import io.harness.ng.core.dto.AccountDTO;
 import io.harness.ng.core.entities.Organization;
 import io.harness.ng.core.entities.Organization.OrganizationKeys;
 import io.harness.ng.core.entities.Project;
 import io.harness.ng.core.entities.Project.ProjectKeys;
 import io.harness.ng.core.services.OrganizationService;
 import io.harness.ng.core.services.ProjectService;
+import io.harness.remote.client.RestClientUtils;
 import io.harness.security.SecurityContextBuilder;
 import io.harness.security.dto.ServicePrincipal;
 
 import com.google.inject.Inject;
 import java.time.Duration;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -42,16 +46,19 @@ public class DefaultUserGroupCreationService implements Runnable {
   private final OrganizationService organizationService;
   private final ProjectService projectService;
   private final PersistentLocker persistentLocker;
+  private final AccountClient accountClient;
   private static final String DEBUG_MESSAGE = "DefaultUserGroupCreationService: ";
   private static final String LOCK_NAME = "DefaultUserGroupsCreationJobLock";
 
   @Inject
   public DefaultUserGroupCreationService(DefaultUserGroupService defaultUserGroupService,
-      OrganizationService organizationService, ProjectService projectService, PersistentLocker persistentLocker) {
+      OrganizationService organizationService, ProjectService projectService, PersistentLocker persistentLocker,
+      AccountClient accountClient) {
     this.defaultUserGroupService = defaultUserGroupService;
     this.organizationService = organizationService;
     this.projectService = projectService;
     this.persistentLocker = persistentLocker;
+    this.accountClient = accountClient;
   }
 
   @Override
@@ -84,11 +91,16 @@ public class DefaultUserGroupCreationService implements Runnable {
     log.info(DEBUG_MESSAGE + "User Groups creation started.");
 
     try {
-      List<String> distinctAccountIds = organizationService.getDistinctAccounts();
+      List<AccountDTO> allAccounts = RestClientUtils.getResponse(accountClient.getAllAccounts());
+      List<String> distinctAccountIds = allAccounts.stream()
+                                            .filter(AccountDTO::isNextGenEnabled)
+                                            .map(AccountDTO::getIdentifier)
+                                            .collect(Collectors.toList());
       if (isEmpty(distinctAccountIds)) {
         return;
       }
-      log.info(DEBUG_MESSAGE + String.format("Fetched %s accounts...", distinctAccountIds.size()));
+      log.info(
+          DEBUG_MESSAGE + String.format("Fetched %s accounts having NextGen enabled...", distinctAccountIds.size()));
       createUserGroupForAccounts(distinctAccountIds);
     } catch (Exception ex) {
       log.error(DEBUG_MESSAGE + " Fetching all accounts failed : ", ex);
