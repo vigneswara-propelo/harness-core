2a4c589e0aa1a95c675ba7a3c51c5444c85da9b6
Brijesh Dhakar <brijesh.dhakar@harness.io>
2022-09-21 23:25:46 +0530

    [fix]: [PIE-5410]: Update expressions regex for schema to wider scope (#37756)

--- 960-yaml-sdk/src/main/java/io/harness/yaml/schema/beans/SchemaConstants.java
+++ 960-yaml-sdk/src/main/java/io/harness/yaml/schema/beans/SchemaConstants.java
@@ -58,7 +58,7 @@ public class SchemaConstants {
       "^<\\+input>((?!.*\\.executionInput\\(\\))(\\.)(allowedValues|default|regex)\\(.+?\\))*$";
   public static final String INPUT_SET_PATTERN = "^" + EXPR_START_ESC + "input" + EXPR_END_ESC + ".*";
   public static final String EXPRESSION_PATTERN =
-      "(^" + EXPR_START_ESC + "([a-zA-Z]\\w*\\.?)*([a-zA-Z]\\w*)" + EXPR_END_ESC + "$|" + INPUT_SET_PATTERN + ")";
+      "(.*" + EXPR_START_ESC + ".+" + EXPR_END_ESC + ".*|" + INPUT_SET_PATTERN + ")";
   // This should validate string patterns starting with optional + or - ([+-]?). Then at least one digit ([0-9]+). Then
   // optional `.` amd optional digits.
   public static final String NUMBER_STRING_WITH_EXPRESSION_PATTERN = "("
