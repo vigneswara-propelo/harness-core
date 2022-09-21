0f139b2154db5da426bbe27b8842a959c37b584e
Sahildeep Singh <sahildeep.singh@harness.io>
2022-09-21 16:27:11 +0530

    [fix]: [CCM-8936]: Fixed Null pointer excpetion (#37757)
    * [CCM-8936]: Fixed Null pointer excpetion
    
    * [CCM-8936]: Fixed argument mismatch

--- 450-ce-views/src/main/java/io/harness/ccm/views/service/impl/ViewsBillingServiceImpl.java
+++ 450-ce-views/src/main/java/io/harness/ccm/views/service/impl/ViewsBillingServiceImpl.java
@@ -174,6 +174,7 @@ import com.google.inject.Singleton;
 import com.healthmarketscience.sqlbuilder.SelectQuery;
 import com.healthmarketscience.sqlbuilder.custom.postgresql.PgLimitClause;
 import com.healthmarketscience.sqlbuilder.custom.postgresql.PgOffsetClause;
+import io.fabric8.utils.Lists;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.Instant;
@@ -625,7 +626,7 @@ public class ViewsBillingServiceImpl implements ViewsBillingService {
     List<ViewRule> viewRuleList = new ArrayList<>();
     List<QLCEViewFilter> idFilters = getModifiedIdFilters(getIdFilters(filters), isClusterTableQuery);
     List<QLCEViewTimeFilter> timeFilters = viewsQueryHelper.getTimeFilters(filters);
-    if (groupBy.isEmpty()) {
+    if (Lists.isNullOrEmpty(groupBy)) {
       Optional<QLCEViewFilterWrapper> viewMetadataFilter = getViewMetadataFilter(filters);
       if (viewMetadataFilter.isPresent()) {
         QLCEViewMetadataFilter metadataFilter = viewMetadataFilter.get().getViewMetadataFilter();
