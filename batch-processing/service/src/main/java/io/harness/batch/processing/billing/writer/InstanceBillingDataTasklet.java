7e5c6d6d8305e31a9dd729d4c33578f0fb1b19d4
Hitesh Aringa <hitesh.aringa@harness.io>
2022-09-21 12:52:46 +0530

    [fix]: [CCM-9123]: Handle -ve storage storage usage (#37734)
    * [CCM-9123]: Handle -ve storage storage usage
    
    * [CCM-9123]: Handle -ve storage storage usage

--- batch-processing/service/src/main/java/io/harness/batch/processing/billing/writer/InstanceBillingDataTasklet.java
+++ batch-processing/service/src/main/java/io/harness/batch/processing/billing/writer/InstanceBillingDataTasklet.java
@@ -479,7 +479,8 @@ public class InstanceBillingDataTasklet implements Tasklet {
       BillingData billingData, UtilizationData utilizationData, InstanceData instanceData) {
     if (K8S_PV == instanceData.getInstanceType()) {
       BigDecimal storageUnallocatedFraction = BigDecimal.ZERO;
-      if (instanceData.getStorageResource() != null && instanceData.getStorageResource().getCapacity() > 0) {
+      if (instanceData.getStorageResource() != null && instanceData.getStorageResource().getCapacity() > 0
+          && utilizationData.getAvgStorageRequestValue() > 0) {
         BigDecimal capacityFromInstanceData = BigDecimal.valueOf(instanceData.getStorageResource().getCapacity());
         storageUnallocatedFraction =
             capacityFromInstanceData.subtract(BigDecimal.valueOf(utilizationData.getAvgStorageRequestValue()))
@@ -490,7 +491,12 @@ public class InstanceBillingDataTasklet implements Tasklet {
             utilizationData.getAvgStorageCapacityValue(), instanceData.toString());
         return BigDecimal.ZERO;
       }
-      return billingData.getBillingAmountBreakup().getStorageBillingAmount().multiply(storageUnallocatedFraction);
+      BigDecimal storageUnallocatedCost =
+          billingData.getBillingAmountBreakup().getStorageBillingAmount().multiply(storageUnallocatedFraction);
+      if (storageUnallocatedCost.compareTo(billingData.getBillingAmountBreakup().getStorageBillingAmount()) > 0) {
+        return billingData.getBillingAmountBreakup().getStorageBillingAmount();
+      }
+      return storageUnallocatedCost;
     }
     return BigDecimal.ZERO;
   }
