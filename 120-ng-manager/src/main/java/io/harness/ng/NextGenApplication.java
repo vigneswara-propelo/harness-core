b2236daeb32f091a6343a874441c87b4a5b232c0
Jimit Gandhi <jimit.gandhi@harness.io>
2022-09-25 18:29:04 +0530

    [feat]: [PL-28494] : Remove Default User Group Creation Job  (#37875)
    * Removing reference to DefaultUserGroupsCreationJob as we are done with activity of creating Default User Groups at all scopes.
    
    * Removed unused import.

--- 120-ng-manager/src/main/java/io/harness/ng/NextGenApplication.java
+++ 120-ng-manager/src/main/java/io/harness/ng/NextGenApplication.java
@@ -97,7 +97,6 @@ import io.harness.migration.NGMigrationSdkModule;
 import io.harness.migration.beans.NGMigrationConfiguration;
 import io.harness.migrations.InstanceMigrationProvider;
 import io.harness.ng.core.CorrelationFilter;
-import io.harness.ng.core.DefaultUserGroupsCreationJob;
 import io.harness.ng.core.EtagFilter;
 import io.harness.ng.core.event.NGEventConsumerService;
 import io.harness.ng.core.exceptionmappers.GenericExceptionMapperV2;
@@ -725,7 +724,6 @@ public class NextGenApplication extends Application<NextGenConfiguration> {
     environment.lifecycle().manage(injector.getInstance(QueueListenerController.class));
     environment.lifecycle().manage(injector.getInstance(NotifierScheduledExecutorService.class));
     environment.lifecycle().manage(injector.getInstance(OutboxEventPollService.class));
-    environment.lifecycle().manage(injector.getInstance(DefaultUserGroupsCreationJob.class));
     createConsumerThreadsToListenToEvents(environment, injector);
   }
 
