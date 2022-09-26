be3694768465297c53eca7c870ff5050879bb2be
Vikyath Harekal <vikyath.harekal@harness.io>
2022-09-26 19:48:16 +0530

    [CI-5559]: Fix RunTestStepVariableCreatorTest (#37921)

--- 332-ci-manager/service/src/test/java/io/harness/ci/creator/variables/RunTestStepVariableCreatorTest.java
+++ 332-ci-manager/service/src/test/java/io/harness/ci/creator/variables/RunTestStepVariableCreatorTest.java
@@ -99,7 +99,9 @@ public class RunTestStepVariableCreatorTest extends CategoryTest {
             "pipeline.stages.run_test.spec.execution.steps.ti.spec.resources.limits.cpu",
             "pipeline.stages.run_test.spec.execution.steps.ti.spec.namespaces",
             "pipeline.stages.run_test.spec.execution.steps.ti.spec.frameworkVersion",
-            "pipeline.stages.run_test.spec.execution.steps.ti.spec.buildEnvironment");
+            "pipeline.stages.run_test.spec.execution.steps.ti.spec.buildEnvironment",
+            "pipeline.stages.run_test.spec.execution.steps.ti.spec.testSplitStrategy",
+            "pipeline.stages.run_test.spec.execution.steps.ti.spec.enableTestSplitting");
 
     // yaml extra properties
     List<String> fqnExtraPropertiesList = variablesForParentNodeV2.getYamlExtraProperties()
