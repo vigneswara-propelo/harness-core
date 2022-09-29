689f24bfc5bb982fab8c89807c3e04dba68275a9
Marko Lukic <marko.lukic@harness.io>
2022-09-29 13:27:50 +0200

    [CDS-40288]: removing AZURE_ARM feature flag (#38130)

--- 956-feature-flag-beans/src/main/java/io/harness/beans/FeatureName.java
+++ 956-feature-flag-beans/src/main/java/io/harness/beans/FeatureName.java
@@ -35,7 +35,6 @@ public enum FeatureName {
   AZURE_US_GOV_CLOUD,
   AZURE_VMSS,
   AZURE_WEBAPP,
-  AZURE_ARM,
   BIND_FETCH_FILES_TASK_TO_DELEGATE,
   CCM_SUSTAINABILITY("Sustainability Feature in CCM Module", HarnessTeam.CE),
   CDNG_ENABLED,
