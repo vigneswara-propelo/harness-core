bb0838d0dc03d5360ce506afc374b165aff28a48
Shreyas Singh <shreyas.singh@harness.io>
2022-09-30 17:22:11 +0530

    [PL-28304]: adding config dto mapper and config mapper (#38192)

---
+++ 890-sm-delegate/src/main/java/io/harness/ng/GcpSecretManagerConfigDTOMapper.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2022 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
+ */
+
+package io.harness.ng;
+
+import static io.harness.annotations.dev.HarnessTeam.PL;
+
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.connector.ConnectorDTO;
+import io.harness.connector.ConnectorInfoDTO;
+import io.harness.delegate.beans.connector.gcpsecretmanager.GcpSecretManagerConnectorDTO;
+import io.harness.secretmanagerclient.dto.GcpSecretManagerConfigDTO;
+import io.harness.secretmanagerclient.dto.GcpSecretManagerConfigUpdateDTO;
+import io.harness.security.encryption.EncryptionType;
+
+import lombok.experimental.UtilityClass;
+
+@OwnedBy(PL)
+@UtilityClass
+public class GcpSecretManagerConfigDTOMapper {
+  public static GcpSecretManagerConfigDTO getGcpSecretManagerConfigDTO(String accountIdentifier,
+      ConnectorDTO connectorRequestDTO, GcpSecretManagerConnectorDTO gcpSecretManagerConnectorDTO) {
+    ConnectorInfoDTO connector = connectorRequestDTO.getConnectorInfo();
+
+    GcpSecretManagerConfigDTO gcpSecretManagerConfigDTO =
+        GcpSecretManagerConfigDTO.builder()
+            .delegateSelectors(gcpSecretManagerConnectorDTO.getDelegateSelectors())
+            .isDefault(gcpSecretManagerConnectorDTO.isDefault())
+            .encryptionType(EncryptionType.GCP_SECRETS_MANAGER)
+
+            .name(connector.getName())
+            .accountIdentifier(accountIdentifier)
+            .orgIdentifier(connector.getOrgIdentifier())
+            .projectIdentifier(connector.getProjectIdentifier())
+            .tags(connector.getTags())
+            .identifier(connector.getIdentifier())
+            .description(connector.getDescription())
+            .build();
+
+    if (null != gcpSecretManagerConnectorDTO.getCredentialsRef().getDecryptedValue()) {
+      gcpSecretManagerConfigDTO.setCredentials(gcpSecretManagerConnectorDTO.getCredentialsRef().getDecryptedValue());
+    }
+    return gcpSecretManagerConfigDTO;
+  }
+
+  public static GcpSecretManagerConfigUpdateDTO getGcpSecretManagerConfigUpdateDTO(
+      ConnectorDTO connectorRequestDTO, GcpSecretManagerConnectorDTO gcpSecretManagerConnectorDTO) {
+    ConnectorInfoDTO connector = connectorRequestDTO.getConnectorInfo();
+    GcpSecretManagerConfigUpdateDTO gcpSecretManagerConfigUpdateDTO =
+        GcpSecretManagerConfigUpdateDTO.builder()
+            .delegateSelectors(gcpSecretManagerConnectorDTO.getDelegateSelectors())
+            .isDefault(gcpSecretManagerConnectorDTO.isDefault())
+
+            .name(connector.getName())
+            .encryptionType(EncryptionType.GCP_SECRETS_MANAGER)
+            .tags(connector.getTags())
+            .description(connector.getDescription())
+            .build();
+
+    if (null != gcpSecretManagerConnectorDTO.getCredentialsRef().getDecryptedValue()) {
+      gcpSecretManagerConfigUpdateDTO.setCredentials(
+          gcpSecretManagerConnectorDTO.getCredentialsRef().getDecryptedValue());
+    }
+    return gcpSecretManagerConfigUpdateDTO;
+  }
+}
---
+++ 930-delegate-tasks/src/main/java/io/harness/mappers/GcpSecretManagerConfigMapper.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2022 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
+ */
+
+package io.harness.mappers;
+
+import static io.harness.annotations.dev.HarnessTeam.PL;
+import static io.harness.data.structure.EmptyPredicate.isNotEmpty;
+
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.ng.core.mapper.TagMapper;
+import io.harness.secretmanagerclient.NGSecretManagerMetadata;
+import io.harness.secretmanagerclient.dto.GcpSecretManagerConfigDTO;
+import io.harness.secretmanagerclient.dto.GcpSecretManagerConfigUpdateDTO;
+
+import software.wings.beans.GcpSecretsManagerConfig;
+
+import java.util.Optional;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+@OwnedBy(PL)
+public class GcpSecretManagerConfigMapper {
+  public static GcpSecretsManagerConfig fromDTO(GcpSecretManagerConfigDTO configDTO) {
+    GcpSecretsManagerConfig gcpSecretsManagerConfig = GcpSecretsManagerConfig.builder()
+                                                          .name(configDTO.getName())
+                                                          .delegateSelectors(configDTO.getDelegateSelectors())
+                                                          .credentials(configDTO.getCredentials())
+                                                          .build();
+    gcpSecretsManagerConfig.setNgMetadata(SecretManagerConfigMapper.ngMetaDataFromDto(configDTO));
+    gcpSecretsManagerConfig.setAccountId(configDTO.getAccountIdentifier());
+    gcpSecretsManagerConfig.setEncryptionType(configDTO.getEncryptionType());
+    gcpSecretsManagerConfig.setDefault(configDTO.isDefault());
+    return gcpSecretsManagerConfig;
+  }
+
+  public static GcpSecretsManagerConfig applyUpdate(
+      GcpSecretsManagerConfig gcpSecretsManagerConfig, GcpSecretManagerConfigUpdateDTO configUpdateDTO) {
+    if (isNotEmpty(configUpdateDTO.getCredentials())) {
+      gcpSecretsManagerConfig.setCredentials(configUpdateDTO.getCredentials());
+    }
+    gcpSecretsManagerConfig.setDefault(configUpdateDTO.isDefault());
+    gcpSecretsManagerConfig.setName(configUpdateDTO.getName());
+    if (!Optional.ofNullable(gcpSecretsManagerConfig.getNgMetadata()).isPresent()) {
+      gcpSecretsManagerConfig.setNgMetadata(NGSecretManagerMetadata.builder().build());
+    }
+    gcpSecretsManagerConfig.getNgMetadata().setTags(TagMapper.convertToList(configUpdateDTO.getTags()));
+    gcpSecretsManagerConfig.getNgMetadata().setDescription(configUpdateDTO.getDescription());
+    return gcpSecretsManagerConfig;
+  }
+}
