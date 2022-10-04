b7081854516799333617b75e67b700858d644bff
Yogesh Chauhan <yogesh.chauhan@harness.io>
2022-10-04 13:29:41 +0530

    Revert "feat: [CDS-41366]: GA service as expression FF (#38238)" (#38317)
    This reverts commit f3685de55e06f5791ed1cfdb28c0b9d90183313f.

--- 125-cd-nextgen/src/main/java/io/harness/cdng/creator/plan/stage/DeploymentStagePMSPlanCreatorV2.java
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/creator/plan/stage/DeploymentStagePMSPlanCreatorV2.java
@@ -11,6 +11,7 @@ import static io.harness.annotations.dev.HarnessTeam.CDC;
 import static io.harness.cdng.pipeline.steps.MultiDeploymentSpawnerUtils.SERVICE_REF_EXPRESSION;
 
 import io.harness.annotations.dev.OwnedBy;
+import io.harness.beans.FeatureName;
 import io.harness.cdng.creator.plan.envGroup.EnvGroupPlanCreatorHelper;
 import io.harness.cdng.creator.plan.environment.EnvironmentPlanCreatorHelper;
 import io.harness.cdng.creator.plan.infrastructure.InfrastructurePmsPlanCreator;
@@ -316,7 +317,10 @@ public class DeploymentStagePMSPlanCreatorV2 extends AbstractStagePlanCreator<De
         && deploymentStageConfig.getService().getUseFromStage() != null
         && EmptyPredicate.isNotEmpty(deploymentStageConfig.getService().getUseFromStage().getStage());
     boolean isServices = deploymentStageConfig.getServices() != null;
-    return isServices || isServiceV2 || serviceV2UseFromStage;
+    boolean shouldUseNewFlow = isServices || isServiceV2 || serviceV2UseFromStage;
+    return shouldUseNewFlow
+        && featureFlagHelperService.isEnabled(
+            ctx.getMetadata().getAccountIdentifier(), FeatureName.SERVICE_V2_EXPRESSION);
   }
 
   private OverridesFromEnvironment addEnvAndInfraDependency(PlanCreationContext ctx, DeploymentStageNode stageNode,
