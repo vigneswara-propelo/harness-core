4c76a02dba4c7ed8f2ca4cf3f47a383ce61dc639
Filip Petrovic <filip.petrovic@harness.io>
2022-10-06 09:26:11 +0200

    fix: [CDS-43491]: when Skip instances with the same artifact version already deployed is enabled, deploying same artifact to hosts fail with items list cannot be empty (#37818)
    * [CDS-43491]: Return empty lists when there is no hosts to be deployed to
    
    * [CDS-43491]: Bound sublist params to max

--- clients/pipeline-service/pms-sdk-core/src/main/java/io/harness/steps/matrix/ForLoopStrategyConfigService.java
+++ clients/pipeline-service/pms-sdk-core/src/main/java/io/harness/steps/matrix/ForLoopStrategyConfigService.java
@@ -23,8 +23,10 @@ import io.harness.yaml.utils.JsonPipelineUtils;
 import com.fasterxml.jackson.databind.JsonNode;
 import io.fabric8.utils.Lists;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import org.jetbrains.annotations.NotNull;
 
 public class ForLoopStrategyConfigService implements StrategyConfigService {
   @Override
@@ -86,6 +88,11 @@ public class ForLoopStrategyConfigService implements StrategyConfigService {
 
   private List<List<String>> partitionItemsByPercentage(HarnessForConfig harnessForConfig) {
     List<String> items = harnessForConfig.getItems().getValue();
+
+    if (items.isEmpty()) {
+      return Collections.emptyList();
+    }
+
     int itemsSize = items.size();
     ParameterField<Integer> partitionSizeInPercentage = harnessForConfig.getPartitionSize();
     float partitionSizeInPercentageValue = partitionSizeInPercentage.getValue().floatValue();
@@ -106,6 +113,11 @@ public class ForLoopStrategyConfigService implements StrategyConfigService {
 
   private List<List<String>> partitionItemsByCount(HarnessForConfig harnessForConfig) {
     List<String> items = harnessForConfig.getItems().getValue();
+
+    if (items.isEmpty()) {
+      return Collections.emptyList();
+    }
+
     int itemsSize = items.size();
     int partitionSize = harnessForConfig.getPartitionSize().getValue();
 
@@ -128,6 +140,9 @@ public class ForLoopStrategyConfigService implements StrategyConfigService {
     int start = 0;
     List<String> params = harnessForConfig.getItems().getValue();
     validateItems(params);
+    if (params.isEmpty()) {
+      return params;
+    }
     int itemsSize = params.size();
     int end = itemsSize - 1;
     if (!ParameterField.isBlank(harnessForConfig.getStart())) {
@@ -143,28 +158,42 @@ public class ForLoopStrategyConfigService implements StrategyConfigService {
     }
 
     validateStartEnd(start, end, itemsSize);
-    return params.subList(start, end);
+    return boundedSubList(params, start, end);
   }
 
   private List<String> handleSplitByCount(HarnessForConfig harnessForConfig) {
     List<String> params = harnessForConfig.getItems().getValue();
     validateItems(params);
+    if (params.isEmpty()) {
+      return params;
+    }
     if (!ParameterField.isBlank(harnessForConfig.getStart())) {
       int start = harnessForConfig.getStart().getValue();
       if (!ParameterField.isBlank(harnessForConfig.getEnd())) {
         int end = harnessForConfig.getEnd().getValue();
         validateStartEnd(start, end, params.size());
-        params = params.subList(start, end);
+        params = boundedSubList(params, start, end);
       } else {
         validateStartEnd(start, params.size() - 1, params.size());
-        params = params.subList(start, params.size() - 1);
+        params = boundedSubList(params, start, params.size() - 1);
       }
     } else if (!ParameterField.isBlank(harnessForConfig.getEnd())) {
       int end = harnessForConfig.getEnd().getValue();
       validateStartEnd(0, end, params.size());
-      params = params.subList(0, end);
+      params = boundedSubList(params, 0, end);
     }
     return params;
+  }
+
+  @NotNull
+  private List<String> boundedSubList(List<String> params, int start, int end) {
+    int boundedEnd = limitIndex(end, params.size());
+    int boundedStart = limitIndex(start, boundedEnd);
+    return params.subList(boundedStart, boundedEnd);
+  }
+
+  private int limitIndex(int index, int size) {
+    return Math.min(index, size);
   }
 
   private void validateItems(List<String> params) {
@@ -176,12 +205,6 @@ public class ForLoopStrategyConfigService implements StrategyConfigService {
   private void validateStartEnd(int start, int end, int size) {
     if (start < 0 || end < 0) {
       throw new InvalidRequestException("start or end cannot be less that 0");
-    }
-    if (start >= size || end > size) {
-      throw new InvalidRequestException("start or end cannot be greater than the size of list");
-    }
-    if (start > end) {
-      throw new InvalidRequestException("start cannot be greater than end");
     }
   }
 
