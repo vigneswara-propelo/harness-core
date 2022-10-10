24d0f12231c290e48e3e3df1332b0d9aeb854cd5
Nasser Gonzalez Gonzalez <nasser.gonzalez@harness.io>
2022-10-10 09:28:47 +0100

    [feat]:[CDS-41441] Increase CV for Azure NG (#38488)
    * [CDS-43983]: Fix small issues found in bugsquash

--- 125-cd-nextgen/src/test/java/io/harness/cdng/provision/azure/AzureCommonHelperTest.java
+++ 125-cd-nextgen/src/test/java/io/harness/cdng/provision/azure/AzureCommonHelperTest.java
@@ -19,6 +19,7 @@ import static org.mockito.Mockito.when;
 
 import io.harness.CategoryTest;
 import io.harness.annotations.dev.OwnedBy;
+import io.harness.azure.AzureEnvironmentType;
 import io.harness.azure.model.ARMScopeType;
 import io.harness.azure.model.AzureDeploymentMode;
 import io.harness.category.element.UnitTests;
@@ -30,6 +31,13 @@ import io.harness.cdng.provision.azure.beans.AzureCreateARMResourcePassThroughDa
 import io.harness.connector.ConnectorInfoDTO;
 import io.harness.connector.validator.scmValidators.GitConfigAuthenticationInfoHelper;
 import io.harness.delegate.beans.connector.ConnectorType;
+import io.harness.delegate.beans.connector.azureconnector.AzureAuthDTO;
+import io.harness.delegate.beans.connector.azureconnector.AzureClientSecretKeyDTO;
+import io.harness.delegate.beans.connector.azureconnector.AzureConnectorDTO;
+import io.harness.delegate.beans.connector.azureconnector.AzureCredentialDTO;
+import io.harness.delegate.beans.connector.azureconnector.AzureCredentialType;
+import io.harness.delegate.beans.connector.azureconnector.AzureManualDetailsDTO;
+import io.harness.delegate.beans.connector.azureconnector.AzureSecretType;
 import io.harness.delegate.beans.connector.scm.GitAuthType;
 import io.harness.delegate.beans.connector.scm.GitConnectionType;
 import io.harness.delegate.beans.connector.scm.genericgitconnector.GitConfigDTO;
@@ -41,6 +49,7 @@ import io.harness.delegate.beans.connector.scm.github.GithubHttpCredentialsDTO;
 import io.harness.delegate.beans.connector.scm.github.GithubUsernamePasswordDTO;
 import io.harness.delegate.beans.storeconfig.GitStoreDelegateConfig;
 import io.harness.delegate.task.git.GitFetchFilesConfig;
+import io.harness.encryption.SecretRefData;
 import io.harness.ng.core.dto.secrets.SSHKeySpecDTO;
 import io.harness.plancreator.steps.TaskSelectorYaml;
 import io.harness.plancreator.steps.common.StepElementParameters;
@@ -125,6 +134,52 @@ public class AzureCommonHelperTest extends CategoryTest {
                                             .build();
     doNothing().when(cdStepHelper).validateGitStoreConfig(store);
     doReturn(connectorInfoDTO).when(cdStepHelper).getConnector(any(), any());
+    ArrayList<String> paths = new ArrayList<>(Arrays.asList("Foo"));
+    doReturn(SSHKeySpecDTO.builder().build())
+        .when(gitConfigAuthenticationInfoHelper)
+        .getSSHKey(any(), any(), any(), any());
+    List<EncryptedDataDetail> encryptedDataDetails = mock(List.class);
+    doReturn(encryptedDataDetails).when(gitConfigAuthenticationInfoHelper).getEncryptedDataDetails(any(), any(), any());
+    GitStoreDelegateConfig response =
+        azureCommonHelper.getGitStoreDelegateConfig(store, azureHelperTest.getAmbiance(), paths);
+    assertThat(response.getGitConfigDTO().getConnectorType()).isEqualTo(ConnectorType.GIT);
+    assertThat(response.getPaths()).isEqualTo(paths);
+    assertThat(response.getConnectorName()).isEqualTo("connectorName");
+  }
+
+  @Test
+  @Owner(developers = NGONZALEZ)
+  @Category(UnitTests.class)
+  public void testGetGitStoreDelegateConfigWithAccountType() {
+    GithubConnectorDTO githubConnectorDTO =
+        GithubConnectorDTO.builder()
+            .connectionType(GitConnectionType.ACCOUNT)
+            .authentication(GithubAuthenticationDTO.builder()
+                                .authType(GitAuthType.HTTP)
+                                .credentials(GithubHttpCredentialsDTO.builder()
+                                                 .type(GithubHttpAuthenticationType.USERNAME_AND_PASSWORD)
+                                                 .httpCredentialsSpec(GithubUsernamePasswordDTO.builder()
+                                                                          .username("foobar")
+                                                                          .passwordRef(null)
+                                                                          .usernameRef(null)
+                                                                          .build())
+                                                 .build())
+                                .build())
+            .apiAccess(GithubApiAccessDTO.builder().build())
+            .url("url")
+            .build();
+    GithubStore store = GithubStore.builder()
+                            .connectorRef(ParameterField.createValueField("template-connector-ref"))
+                            .folderPath(ParameterField.createValueField("folderPath"))
+                            .build();
+    ConnectorInfoDTO connectorInfoDTO = ConnectorInfoDTO.builder()
+                                            .name("connectorName")
+                                            .connectorType(ConnectorType.GIT)
+                                            .connectorConfig(githubConnectorDTO)
+                                            .build();
+    doNothing().when(cdStepHelper).validateGitStoreConfig(store);
+    doReturn(connectorInfoDTO).when(cdStepHelper).getConnector(any(), any());
+    doReturn("url").when(cdStepHelper).getGitRepoUrl(any(), any());
     ArrayList<String> paths = new ArrayList<>(Arrays.asList("Foo"));
     doReturn(SSHKeySpecDTO.builder().build())
         .when(gitConfigAuthenticationInfoHelper)
@@ -235,5 +290,102 @@ public class AzureCommonHelperTest extends CategoryTest {
     assertThat(azureDeploymentMode2).isEqualTo(AzureDeploymentMode.INCREMENTAL);
     AzureDeploymentMode azureDeploymentMode3 = azureCommonHelper.retrieveDeploymentMode(scopeB, IncrementalMode);
     assertThat(azureDeploymentMode3).isEqualTo(AzureDeploymentMode.INCREMENTAL);
+  }
+
+  @Test
+  @Owner(developers = NGONZALEZ)
+  @Category(UnitTests.class)
+  public void testAzureEncryptionsDetails() {
+    AzureConnectorDTO connectorDTO =
+        AzureConnectorDTO.builder()
+            .azureEnvironmentType(AzureEnvironmentType.AZURE_US_GOVERNMENT)
+            .credential(
+                AzureCredentialDTO.builder()
+                    .azureCredentialType(AzureCredentialType.MANUAL_CREDENTIALS)
+                    .config(AzureManualDetailsDTO.builder()
+                                .clientId("client-id")
+                                .tenantId("tenant-id")
+                                .authDTO(AzureAuthDTO.builder()
+                                             .azureSecretType(AzureSecretType.SECRET_KEY)
+                                             .credentials(AzureClientSecretKeyDTO.builder()
+                                                              .secretKey(SecretRefData.builder()
+                                                                             .decryptedValue("secret-key".toCharArray())
+                                                                             .build())
+                                                              .build())
+                                             .build())
+                                .build())
+                    .build())
+            .build();
+    when(secretManagerClientService.getEncryptionDetails(any(), any()))
+        .thenReturn(Arrays.asList(EncryptedDataDetail.builder().build()));
+    List<EncryptedDataDetail> encryptedDataDetails =
+        azureCommonHelper.getAzureEncryptionDetails(azureHelperTest.getAmbiance(), connectorDTO);
+    assertThat(encryptedDataDetails.size()).isGreaterThan(0);
+  }
+
+  @Test
+  @Owner(developers = NGONZALEZ)
+  @Category(UnitTests.class)
+  public void testGetAzureCreatePassThroughDataWithGitFiles() {
+    AzureCreateARMResourceParameterFile parameterFile = new AzureCreateARMResourceParameterFile();
+    StoreConfigWrapper store = StoreConfigWrapper.builder()
+                                   .spec(GithubStore.builder()
+                                             .connectorRef(ParameterField.createValueField("template-connector-ref"))
+                                             .folderPath(ParameterField.createValueField("folderPath"))
+                                             .build())
+                                   .build();
+    parameterFile.setStore(store);
+    AzureCreateARMResourceStepConfigurationParameters parameters =
+        AzureCreateARMResourceStepConfigurationParameters.builder().parameters(parameterFile).build();
+    AzureCreateARMResourcePassThroughData azureCreateARMResourcePassThroughData =
+        azureCommonHelper.getAzureCreatePassThroughData(parameters);
+    assertThat(azureCreateARMResourcePassThroughData.hasGitFiles()).isTrue();
+  }
+
+  @Test
+  @Owner(developers = NGONZALEZ)
+  @Category(UnitTests.class)
+  public void testGetParametersGitFetchFileConfigs() {
+    AzureCreateARMResourceParameterFile parameterFile = new AzureCreateARMResourceParameterFile();
+    StoreConfigWrapper store =
+        StoreConfigWrapper.builder()
+            .spec(GithubStore.builder()
+                      .connectorRef(ParameterField.createValueField("template-connector-ref"))
+                      .paths(ParameterField.<List<String>>builder().value(Arrays.asList("thisIsAPath")).build())
+                      .build())
+            .build();
+    parameterFile.setStore(store);
+    AzureCreateARMResourceStepConfigurationParameters parameters =
+        AzureCreateARMResourceStepConfigurationParameters.builder().parameters(parameterFile).build();
+    doReturn(SSHKeySpecDTO.builder().build())
+        .when(gitConfigAuthenticationInfoHelper)
+        .getSSHKey(any(), any(), any(), any());
+    GithubConnectorDTO githubConnectorDTO =
+        GithubConnectorDTO.builder()
+            .connectionType(GitConnectionType.ACCOUNT)
+            .authentication(GithubAuthenticationDTO.builder()
+                                .authType(GitAuthType.HTTP)
+                                .credentials(GithubHttpCredentialsDTO.builder()
+                                                 .type(GithubHttpAuthenticationType.USERNAME_AND_PASSWORD)
+                                                 .httpCredentialsSpec(GithubUsernamePasswordDTO.builder()
+                                                                          .username("foobar")
+                                                                          .passwordRef(null)
+                                                                          .usernameRef(null)
+                                                                          .build())
+                                                 .build())
+                                .build())
+            .apiAccess(GithubApiAccessDTO.builder().build())
+            .url("url")
+            .build();
+    ConnectorInfoDTO connectorInfoDTO = ConnectorInfoDTO.builder()
+                                            .name("connectorName")
+                                            .connectorType(ConnectorType.GIT)
+                                            .connectorConfig(githubConnectorDTO)
+                                            .build();
+    doReturn(connectorInfoDTO).when(cdStepHelper).getConnector(any(), any());
+    List<GitFetchFilesConfig> fetchFilesConfigs =
+        azureCommonHelper.getParametersGitFetchFileConfigs(azureHelperTest.getAmbiance(), parameters);
+    assertThat(fetchFilesConfigs.size()).isEqualTo(1);
+    assertThat(fetchFilesConfigs.get(0).getManifestType()).isEqualTo("Azure Parameter");
   }
 }
