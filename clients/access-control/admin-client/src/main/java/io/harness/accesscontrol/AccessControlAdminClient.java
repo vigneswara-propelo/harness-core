a8fa41fc84264031acefaa9f678e86c4d32d023f
Jimit Gandhi <jimit.gandhi@harness.io>
2022-10-10 14:07:43 +0530

    [feat]: [PL-28394]: Adding Get and update roles api in AccessControlAdminClient.  (#38548)
    * Adding Get and update roles api in AccessControlAdminClient.
    
    * fixed updateRole client API definition.

--- clients/access-control/admin-client/src/main/java/io/harness/accesscontrol/AccessControlAdminClient.java
+++ clients/access-control/admin-client/src/main/java/io/harness/accesscontrol/AccessControlAdminClient.java
@@ -13,6 +13,8 @@ import io.harness.accesscontrol.roleassignments.api.RoleAssignmentAggregateRespo
 import io.harness.accesscontrol.roleassignments.api.RoleAssignmentCreateRequestDTO;
 import io.harness.accesscontrol.roleassignments.api.RoleAssignmentFilterDTO;
 import io.harness.accesscontrol.roleassignments.api.RoleAssignmentResponseDTO;
+import io.harness.accesscontrol.roles.api.RoleDTO;
+import io.harness.accesscontrol.roles.api.RoleResponseDTO;
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.ng.beans.PageResponse;
@@ -21,14 +23,17 @@ import io.harness.ng.core.dto.ResponseDTO;
 import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 @OwnedBy(HarnessTeam.PL)
 public interface AccessControlAdminClient {
   String ROLE_ASSIGNMENTS_API = "roleassignments";
   String ACL_PREFERENCES_API = "aclPreferences";
+  String ROLES_API = "roles";
 
   @POST(ROLE_ASSIGNMENTS_API + "/filter")
   Call<ResponseDTO<PageResponse<RoleAssignmentResponseDTO>>> getFilteredRoleAssignments(
@@ -62,4 +67,18 @@ public interface AccessControlAdminClient {
   @PUT(ACL_PREFERENCES_API)
   Call<ResponseDTO<Boolean>> updateAccessControlPreference(
       @Query(NGCommonEntityConstants.ACCOUNT_KEY) String accountIdentifier, @Query("enabled") boolean enabled);
+
+  @GET(ROLES_API)
+  Call<ResponseDTO<PageResponse<RoleResponseDTO>>> getRoles(@Query(NGResourceFilterConstants.PAGE_KEY) int page,
+      @Query(NGResourceFilterConstants.SIZE_KEY) int size,
+      @Query(value = NGCommonEntityConstants.ACCOUNT_KEY) String accountIdentifier,
+      @Query(NGCommonEntityConstants.ORG_KEY) String orgIdentifier,
+      @Query(NGCommonEntityConstants.PROJECT_KEY) String projectIdentifier,
+      @Query(NGResourceFilterConstants.SEARCH_TERM_KEY) String searchTerm);
+
+  @PUT(ROLES_API + "/{identifier}")
+  Call<ResponseDTO<RoleResponseDTO>> updateRole(@Path(NGCommonEntityConstants.IDENTIFIER_KEY) String identifier,
+      @Query(value = NGCommonEntityConstants.ACCOUNT_KEY) String accountIdentifier,
+      @Query(NGCommonEntityConstants.ORG_KEY) String orgIdentifier,
+      @Query(NGCommonEntityConstants.PROJECT_KEY) String projectIdentifier, @Body RoleDTO roleDTO);
 }
