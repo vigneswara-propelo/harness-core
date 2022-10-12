63a03e3a9fc141f54e7717715d17414b17c05c29
Tudor Macari <tudor.macari@harness.io>
2022-10-12 13:34:32 +0300

    [fix]: [CDS-44735]: support secrets expressions in ARM template and params file (#38711)

--- 950-delegate-tasks-beans/src/main/java/io/harness/delegate/task/azure/arm/AzureARMTaskNGParameters.java
+++ 950-delegate-tasks-beans/src/main/java/io/harness/delegate/task/azure/arm/AzureARMTaskNGParameters.java
@@ -8,6 +8,7 @@
 package io.harness.delegate.task.azure.arm;
 
 import static io.harness.annotations.dev.HarnessTeam.CDP;
+import static io.harness.expression.Expression.ALLOW_SECRETS;
 
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.azure.model.ARMScopeType;
@@ -16,6 +17,7 @@ import io.harness.beans.DecryptableEntity;
 import io.harness.delegate.beans.connector.azureconnector.AzureConnectorDTO;
 import io.harness.delegate.beans.logstreaming.CommandUnitsProgress;
 import io.harness.delegate.task.azure.appservice.settings.AppSettingsFile;
+import io.harness.expression.Expression;
 import io.harness.security.encryption.EncryptedDataDetail;
 
 import java.util.ArrayList;
@@ -34,8 +36,8 @@ import org.apache.commons.lang3.tuple.Pair;
 @EqualsAndHashCode(callSuper = true)
 @OwnedBy(CDP)
 public class AzureARMTaskNGParameters extends AzureResourceCreationTaskNGParameters {
-  AppSettingsFile templateBody;
-  AppSettingsFile parametersBody;
+  @Expression(ALLOW_SECRETS) AppSettingsFile templateBody;
+  @Expression(ALLOW_SECRETS) AppSettingsFile parametersBody;
   ARMScopeType deploymentScope;
   AzureDeploymentMode deploymentMode;
   String managementGroupId;
