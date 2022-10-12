93117e97b20d7334acfcbcc55808a61f27d8cea7
Anil Chowdhury <anil.chowdhury@harness.io>
2022-10-12 11:10:51 +0530

    fix: [CDS-44687]: added junit tests for Deployment Template in NG (#38686)

---
+++ 125-cd-nextgen/src/test/java/io/harness/cdng/customDeployment/eventlistener/CustomDeploymentEntityCRUDStreamEventListenerTest.java
@@ -0,0 +1,148 @@
+package io.harness.cdng.customDeployment.eventlistener;
+
+import static io.harness.annotations.dev.HarnessTeam.CDP;
+import static io.harness.eventsframework.EventsFrameworkMetadataConstants.ACTION;
+import static io.harness.eventsframework.EventsFrameworkMetadataConstants.CREATE_ACTION;
+import static io.harness.eventsframework.EventsFrameworkMetadataConstants.DELETE_ACTION;
+import static io.harness.eventsframework.EventsFrameworkMetadataConstants.ENTITY_TYPE;
+import static io.harness.eventsframework.EventsFrameworkMetadataConstants.UPDATE_ACTION;
+import static io.harness.rule.OwnerRule.ANIL;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import io.harness.CategoryTest;
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.category.element.UnitTests;
+import io.harness.cdng.customDeployment.EventListener.CustomDeploymentEntityCRUDEventHandler;
+import io.harness.cdng.customDeployment.EventListener.CustomDeploymentEntityCRUDStreamEventListener;
+import io.harness.eventsframework.consumer.Message;
+import io.harness.eventsframework.entity_crud.EntityChangeDTO;
+import io.harness.ng.core.template.TemplateEntityType;
+import io.harness.rule.Owner;
+
+import com.google.protobuf.ByteString;
+import com.google.protobuf.StringValue;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+@OwnedBy(CDP)
+public class CustomDeploymentEntityCRUDStreamEventListenerTest extends CategoryTest {
+  @Mock CustomDeploymentEntityCRUDEventHandler deploymentTemplateEntityCRUDEventHandler;
+  @InjectMocks CustomDeploymentEntityCRUDStreamEventListener deploymentEntityEventListener;
+
+  @Before
+  public void setUp() {
+    MockitoAnnotations.initMocks(this);
+  }
+
+  @Test
+  @Owner(developers = ANIL)
+  @Category(UnitTests.class)
+  public void testHandleMessageDelete() {
+    ByteString bytes = EntityChangeDTO.newBuilder()
+                           .setIdentifier(StringValue.newBuilder().setValue("deploymentTemplateId"))
+                           .setAccountIdentifier(StringValue.newBuilder().setValue("accountIdentifier"))
+                           .build()
+                           .toByteString();
+
+    Message message = Message.newBuilder()
+                          .setMessage(io.harness.eventsframework.producer.Message.newBuilder()
+                                          .putMetadata(ENTITY_TYPE, "TEMPLATE")
+                                          .putMetadata(ACTION, DELETE_ACTION)
+                                          .setData(bytes)
+                                          .build())
+                          .build();
+    assertTrue(deploymentEntityEventListener.handleMessage(message));
+    verify(deploymentTemplateEntityCRUDEventHandler, times(0)).updateInfraAsObsolete(any(), any(), any(), any(), any());
+  }
+
+  @Test
+  @Owner(developers = ANIL)
+  @Category(UnitTests.class)
+  public void testHandleMessageCreate() {
+    ByteString bytes = EntityChangeDTO.newBuilder()
+                           .setIdentifier(StringValue.newBuilder().setValue("deploymentTemplateId"))
+                           .setAccountIdentifier(StringValue.newBuilder().setValue("accountIdentifier"))
+                           .build()
+                           .toByteString();
+
+    Message message = Message.newBuilder()
+                          .setMessage(io.harness.eventsframework.producer.Message.newBuilder()
+                                          .putMetadata(ENTITY_TYPE, "TEMPLATE")
+                                          .putMetadata(ACTION, CREATE_ACTION)
+                                          .setData(bytes)
+                                          .build())
+                          .build();
+    assertTrue(deploymentEntityEventListener.handleMessage(message));
+    verify(deploymentTemplateEntityCRUDEventHandler, times(0)).updateInfraAsObsolete(any(), any(), any(), any(), any());
+  }
+
+  @Test
+  @Owner(developers = ANIL)
+  @Category(UnitTests.class)
+  public void testHandleMessageUpdate() {
+    ByteString bytes = EntityChangeDTO.newBuilder()
+                           .setIdentifier(StringValue.newBuilder().setValue("deploymentTemplateId"))
+                           .setProjectIdentifier(StringValue.newBuilder().setValue("projectId"))
+                           .setOrgIdentifier(StringValue.newBuilder().setValue("orgId"))
+                           .setAccountIdentifier(StringValue.newBuilder().setValue("accountIdentifier"))
+                           .putMetadata("versionLabel", "1")
+                           .putMetadata("templateType", TemplateEntityType.CUSTOM_DEPLOYMENT_TEMPLATE.toString())
+                           .build()
+                           .toByteString();
+
+    Message message = Message.newBuilder()
+                          .setMessage(io.harness.eventsframework.producer.Message.newBuilder()
+                                          .putMetadata(ENTITY_TYPE, "TEMPLATE")
+                                          .putMetadata(ACTION, UPDATE_ACTION)
+                                          .setData(bytes)
+                                          .build())
+                          .build();
+
+    assertTrue(deploymentEntityEventListener.handleMessage(message));
+    verify(deploymentTemplateEntityCRUDEventHandler, times(1))
+        .updateInfraAsObsolete(
+            eq("accountIdentifier"), eq("orgId"), eq("projectId"), eq("deploymentTemplateId"), eq("1"));
+  }
+
+  @Test
+  @Owner(developers = ANIL)
+  @Category(UnitTests.class)
+  public void testHandleMessageUpdateStableVersion() {
+    ByteString bytes = EntityChangeDTO.newBuilder()
+                           .setIdentifier(StringValue.newBuilder().setValue("deploymentTemplateId"))
+                           .setProjectIdentifier(StringValue.newBuilder().setValue("projectId"))
+                           .setOrgIdentifier(StringValue.newBuilder().setValue("orgId"))
+                           .setAccountIdentifier(StringValue.newBuilder().setValue("accountIdentifier"))
+                           .putMetadata("versionLabel", "1")
+                           .putMetadata("isStable", "true")
+                           .putMetadata("templateType", TemplateEntityType.CUSTOM_DEPLOYMENT_TEMPLATE.toString())
+                           .build()
+                           .toByteString();
+
+    Message message = Message.newBuilder()
+                          .setMessage(io.harness.eventsframework.producer.Message.newBuilder()
+                                          .putMetadata(ENTITY_TYPE, "TEMPLATE")
+                                          .putMetadata(ACTION, UPDATE_ACTION)
+                                          .setData(bytes)
+                                          .build())
+                          .build();
+
+    assertTrue(deploymentEntityEventListener.handleMessage(message));
+    verify(deploymentTemplateEntityCRUDEventHandler, times(1))
+        .updateInfraAsObsolete(
+            eq("accountIdentifier"), eq("orgId"), eq("projectId"), eq("deploymentTemplateId"), eq("1"));
+
+    verify(deploymentTemplateEntityCRUDEventHandler, times(1))
+        .updateInfraAsObsolete(
+            eq("accountIdentifier"), eq("orgId"), eq("projectId"), eq("deploymentTemplateId"), eq(null));
+  }
+}
---
+++ 260-delegate/src/test/java/io/harness/perpetualtask/CustomDeploymentInstanceSyncPerpetualTaskExecuterTest.java
@@ -0,0 +1,225 @@
+package io.harness.perpetualtask;
+
+import static io.harness.perpetualtask.CustomDeploymentInstanceSyncPerpetualTaskExecuter.OUTPUT_PATH_KEY;
+import static io.harness.rule.OwnerRule.ANIL;
+
+import static javax.servlet.http.HttpServletResponse.SC_OK;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import io.harness.DelegateTestBase;
+import io.harness.category.element.UnitTests;
+import io.harness.delegate.beans.instancesync.ServerInstanceInfo;
+import io.harness.delegate.beans.instancesync.info.CustomDeploymentServerInstanceInfo;
+import io.harness.delegate.beans.instancesync.mapper.CustomDeploymentInstanceSyncPerpetualTaskResponse;
+import io.harness.delegate.task.shell.ShellExecutorFactoryNG;
+import io.harness.logging.CommandExecutionStatus;
+import io.harness.managerclient.DelegateAgentManagerClient;
+import io.harness.perpetualtask.instancesync.CustomDeploymentNGInstanceSyncPerpetualTaskParams;
+import io.harness.rest.RestResponse;
+import io.harness.rule.Owner;
+import io.harness.shell.ExecuteCommandResponse;
+import io.harness.shell.ScriptProcessExecutor;
+import io.harness.shell.ShellExecutionData;
+
+import software.wings.sm.states.customdeploymentng.InstanceMapperUtils;
+
+import com.google.protobuf.Any;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import retrofit2.Call;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CustomDeploymentInstanceSyncPerpetualTaskExecuterTest extends DelegateTestBase {
+  @Mock private Call<RestResponse<Boolean>> mockCall;
+  @Mock private DelegateAgentManagerClient mockDelegateAgentManagerClient;
+  @Mock private ShellExecutorFactoryNG shellExecutorFactory;
+  @InjectMocks private CustomDeploymentInstanceSyncPerpetualTaskExecuter executor;
+
+  @Captor private ArgumentCaptor<CustomDeploymentInstanceSyncPerpetualTaskResponse> perpetualTaskResponseCaptor;
+
+  private final String taskId = "task-id";
+  private final String accountId = "accountId";
+  private final String output = "{\n"
+      + "  \"items\": [\n"
+      + "    {\n"
+      + "      \"identity\": [\n"
+      + "        \"servers\",\n"
+      + "        \"myserver\"\n"
+      + "      ],\n"
+      + "      \"stagingMode\": \"nostage\",\n"
+      + "      \"listenAddress\": \"4.4\"\n"
+      + "    },\n"
+      + "    {\n"
+      + "      \"identity\": [\n"
+      + "        \"servers\",\n"
+      + "        \"server-1\"\n"
+      + "      ],\n"
+      + "      \"stagingMode\": \"external_stage\",\n"
+      + "      \"listenAddress\": \"2.2\"\n"
+      + "    },\n"
+      + "    {\n"
+      + "      \"identity\": [\n"
+      + "        \"servers\",\n"
+      + "        \"cluster-2-server1\"\n"
+      + "      ],\n"
+      + "      \"stagingMode\": \"stage\",\n"
+      + "      \"listenAddress\": \"3.3\"\n"
+      + "    },\n"
+      + "    {\n"
+      + "      \"identity\": [\n"
+      + "        \"servers\",\n"
+      + "        \"cluster-2-server2\"\n"
+      + "      ],\n"
+      + "      \"stagingMode\": \"stage\",\n"
+      + "      \"listenAddress\": \"1.1\"\n"
+      + "    }\n"
+      + "  ]\n"
+      + "}";
+
+  @Before
+  public void setUp() throws IOException {
+    when(mockDelegateAgentManagerClient.processInstanceSyncNGResult(
+             anyString(), anyString(), perpetualTaskResponseCaptor.capture()))
+        .thenReturn(mockCall);
+    doReturn(retrofit2.Response.success("success")).when(mockCall).execute();
+  }
+
+  @Test
+  @Owner(developers = ANIL)
+  @Category(UnitTests.class)
+  public void testRunOnceSuccess() {
+    Map<String, String> outPutEnvVariables = new HashMap<>();
+    outPutEnvVariables.put(OUTPUT_PATH_KEY, output);
+    ShellExecutionData shellExecutionData =
+        ShellExecutionData.builder().sweepingOutputEnvVariables(outPutEnvVariables).build();
+    ExecuteCommandResponse commandResponse = ExecuteCommandResponse.builder()
+                                                 .commandExecutionData(shellExecutionData)
+                                                 .status(CommandExecutionStatus.SUCCESS)
+                                                 .build();
+
+    ScriptProcessExecutor scriptProcessExecutor = mock(ScriptProcessExecutor.class);
+    doReturn(scriptProcessExecutor).when(shellExecutorFactory).getExecutor(any(), any(), any());
+    doReturn(commandResponse).when(scriptProcessExecutor).executeCommandString(any(), any(), any(), any());
+
+    ArgumentCaptor<CustomDeploymentInstanceSyncPerpetualTaskResponse> argumentCaptor =
+        ArgumentCaptor.forClass(CustomDeploymentInstanceSyncPerpetualTaskResponse.class);
+
+    PerpetualTaskId perpetualTaskId = PerpetualTaskId.newBuilder().setId(taskId).build();
+    PerpetualTaskExecutionParams perpetualTaskParams = getPerpetualTaskParams();
+    PerpetualTaskResponse perpetualTaskResponse = executor.runOnce(perpetualTaskId, perpetualTaskParams, Instant.now());
+
+    assertThat(perpetualTaskResponse.getResponseCode()).isEqualTo(SC_OK);
+    assertThat(perpetualTaskResponse.getResponseMessage()).isEqualTo("success");
+    assertThat(executor.cleanup(perpetualTaskId, perpetualTaskParams)).isEqualTo(false);
+
+    verify(mockDelegateAgentManagerClient)
+        .processInstanceSyncNGResult(eq(taskId), eq(accountId), argumentCaptor.capture());
+    CustomDeploymentInstanceSyncPerpetualTaskResponse argumentCaptorValue = argumentCaptor.getValue();
+    assertThat(argumentCaptorValue).isInstanceOf(CustomDeploymentInstanceSyncPerpetualTaskResponse.class);
+    assertThat(argumentCaptorValue.getCommandExecutionStatus()).isEqualTo(CommandExecutionStatus.SUCCESS);
+    List<ServerInstanceInfo> serverInstanceDetails = argumentCaptorValue.getServerInstanceDetails();
+    assertThat(serverInstanceDetails.size()).isEqualTo(4);
+    List<String> instancesHostNames = serverInstanceDetails.stream()
+                                          .map(server -> ((CustomDeploymentServerInstanceInfo) server).getHostName())
+                                          .collect(Collectors.toList());
+    assertThat(instancesHostNames).contains("1.1", "2.2", "3.3", "4.4");
+  }
+
+  @Test
+  @Owner(developers = ANIL)
+  @Category(UnitTests.class)
+  public void processInstanceSyncResultFailure() {
+    Map<String, String> outPutEnvVariables = new HashMap<>();
+    outPutEnvVariables.put(OUTPUT_PATH_KEY, output);
+    ShellExecutionData shellExecutionData =
+        ShellExecutionData.builder().sweepingOutputEnvVariables(outPutEnvVariables).build();
+    ExecuteCommandResponse commandResponse = ExecuteCommandResponse.builder()
+                                                 .commandExecutionData(shellExecutionData)
+                                                 .status(CommandExecutionStatus.SUCCESS)
+                                                 .build();
+
+    ScriptProcessExecutor scriptProcessExecutor = mock(ScriptProcessExecutor.class);
+    doReturn(scriptProcessExecutor).when(shellExecutorFactory).getExecutor(any(), any(), any());
+    doReturn(commandResponse).when(scriptProcessExecutor).executeCommandString(any(), any(), any(), any());
+
+    ArgumentCaptor<CustomDeploymentInstanceSyncPerpetualTaskResponse> argumentCaptor =
+        ArgumentCaptor.forClass(CustomDeploymentInstanceSyncPerpetualTaskResponse.class);
+
+    doThrow(new RuntimeException("Fail"))
+        .when(mockDelegateAgentManagerClient)
+        .processInstanceSyncNGResult(eq(taskId), eq(accountId), any());
+    PerpetualTaskResponse perpetualTaskResponse =
+        executor.runOnce(PerpetualTaskId.newBuilder().setId(taskId).build(), getPerpetualTaskParams(), Instant.now());
+
+    verify(mockDelegateAgentManagerClient)
+        .processInstanceSyncNGResult(eq(taskId), eq(accountId), argumentCaptor.capture());
+    CustomDeploymentInstanceSyncPerpetualTaskResponse argumentCaptorValue = argumentCaptor.getValue();
+    assertThat(argumentCaptorValue).isInstanceOf(CustomDeploymentInstanceSyncPerpetualTaskResponse.class);
+    assertThat(perpetualTaskResponse.getResponseCode()).isEqualTo(SC_OK);
+    assertThat(perpetualTaskResponse.getResponseMessage())
+        .isEqualTo(
+            "Failed to publish CustomDeployment instance sync result PerpetualTaskId [task-id], accountId [accountId]");
+  }
+
+  private PerpetualTaskExecutionParams getPerpetualTaskParams() {
+    String script = "INSTANCE_OUTPUT_PATH=$(echo '{\n"
+        + "  \"items\" : [\n"
+        + "    {\n"
+        + "      \"identity\" : [ \"servers\", \"myserver\" ],\n"
+        + "      \"stagingMode\" : \"nostage\",\n"
+        + "      \"listenAddress\" : \"3.3\"\n"
+        + "    },\n"
+        + "    {\n"
+        + "      \"identity\" : [ \"servers\", \"server-1\" ],\n"
+        + "      \"stagingMode\" : \"external_stage\",\n"
+        + "      \"listenAddress\" : \"2.2\"\n"
+        + "    },\n"
+        + "    {\n"
+        + "      \"identity\" : [ \"servers\", \"cluster-2-server1\" ],\n"
+        + "      \"stagingMode\" : \"stage\",\n"
+        + "      \"listenAddress\" : \"4.4\n"
+        + "    },\n"
+        + "    {\n"
+        + "      \"identity\" : [ \"servers\", \"cluster-2-server2\" ],\n"
+        + "      \"stagingMode\" : \"stage\",\n"
+        + "      \"listenAddress\" : \"1.1\"\n"
+        + "    }\n"
+        + "  ]\n"
+        + "}')";
+
+    String instancesListPath = "items";
+    Map<String, String> attributes = new HashMap<>();
+    attributes.put(InstanceMapperUtils.hostname, "listenAddress");
+    CustomDeploymentNGInstanceSyncPerpetualTaskParams taskParams =
+        CustomDeploymentNGInstanceSyncPerpetualTaskParams.newBuilder()
+            .setAccountId("accountId")
+            .setScript(script)
+            .setOutputPathKey("")
+            .setInstancesListPath(instancesListPath)
+            .setInfrastructureKey("infra1")
+            .putAllInstanceAttributes(attributes)
+            .build();
+    return PerpetualTaskExecutionParams.newBuilder().setCustomizedParams(Any.pack(taskParams)).build();
+  }
+}
