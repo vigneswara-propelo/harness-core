f437b34f0db543d63280628ee571e8c0eb828c31
Vladimir Peric <vladimir.peric@harness.io>
2022-10-21 18:44:44 +0200

    [feat]: [CDS-43129]: Validate copy artifact for WinRm deployment (#39042)
    * [feat]: [CDS-43129]: winrm copy command validation
    
    * [feat]: [CDS-43129]: winrm copy command validation part2
    
    * [feat]: [CDS-43129]: winrm copy command validation added
    
    * [feat]: [CDS-43129]: winrm copy command validation pr check fix
    
    * [feat]: [CDS-43129]: winrm copy command validation pr review enhancements

--- 125-cd-nextgen/src/main/java/io/harness/cdng/creator/plan/steps/CDPMSCommandStepFilterJsonCreator.java
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/creator/plan/steps/CDPMSCommandStepFilterJsonCreator.java
@@ -10,14 +10,24 @@ package io.harness.cdng.creator.plan.steps;
 import static io.harness.data.structure.EmptyPredicate.isEmpty;
 
 import io.harness.cdng.ssh.CommandStepNode;
+import io.harness.cdng.ssh.CommandUnitSourceType;
+import io.harness.cdng.ssh.CommandUnitSpecType;
+import io.harness.cdng.ssh.CommandUnitWrapper;
+import io.harness.cdng.ssh.CopyCommandUnitSpec;
+import io.harness.cdng.visitor.YamlTypes;
 import io.harness.exception.InvalidYamlException;
 import io.harness.executions.steps.StepSpecTypeConstants;
 import io.harness.plancreator.steps.AbstractStepNode;
 import io.harness.plancreator.strategy.StrategyConfig;
 import io.harness.pms.filter.creation.FilterCreationResponse;
 import io.harness.pms.sdk.core.filter.creation.beans.FilterCreationContext;
+import io.harness.pms.yaml.YamlNode;
 
+import software.wings.api.DeploymentType;
+
+import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.collect.Sets;
+import java.util.Optional;
 import java.util.Set;
 
 public class CDPMSCommandStepFilterJsonCreator extends CDPMSStepFilterJsonCreatorV2 {
@@ -31,6 +41,7 @@ public class CDPMSCommandStepFilterJsonCreator extends CDPMSStepFilterJsonCreato
     FilterCreationResponse response = super.handleNode(filterCreationContext, yamlField);
     validateStrategy(yamlField.getStrategy());
     validateCommandUnitsExist(yamlField);
+    validateCommandUnits(filterCreationContext, yamlField);
     return response;
   }
 
@@ -49,6 +60,68 @@ public class CDPMSCommandStepFilterJsonCreator extends CDPMSStepFilterJsonCreato
         && (((CommandStepNode) stepNode).getCommandStepInfo() == null
             || isEmpty(((CommandStepNode) stepNode).getCommandStepInfo().getCommandUnits()))) {
       throw new InvalidYamlException("Command step needs at least one command unit defined.");
+    }
+  }
+
+  private void validateCommandUnits(FilterCreationContext filterCreationContext, AbstractStepNode stepNode) {
+    Optional<CommandUnitWrapper> copyCommandUnit = findCopyCommandUnit(stepNode);
+    if (copyCommandUnit.isPresent() && isArtifactSourceType(copyCommandUnit.get())
+        && isWinRmDeploymentType(filterCreationContext)) {
+      throw new InvalidYamlException(
+          "Copy command unit is not supported for WinRm deployment type. Please use download command unit instead.");
+    }
+  }
+
+  private boolean isWinRmDeploymentType(FilterCreationContext filterCreationContext) {
+    JsonNode deploymentType = findDeploymentType(filterCreationContext);
+    return deploymentType != null && DeploymentType.WINRM.name().equalsIgnoreCase(deploymentType.textValue());
+  }
+
+  private boolean isArtifactSourceType(CommandUnitWrapper commandUnitWrapper) {
+    if (commandUnitWrapper.getSpec() instanceof CopyCommandUnitSpec) {
+      return CommandUnitSourceType.Artifact.equals(
+          ((CopyCommandUnitSpec) commandUnitWrapper.getSpec()).getSourceType());
+    } else {
+      return false;
+    }
+  }
+
+  private Optional<CommandUnitWrapper> findCopyCommandUnit(AbstractStepNode stepNode) {
+    if (stepNode == null || !stepNode.getClass().isAssignableFrom(CommandStepNode.class)) {
+      return Optional.empty();
+    }
+    CommandStepNode commandStepNode = (CommandStepNode) stepNode;
+    if (commandStepNode.getCommandStepInfo() == null
+        || commandStepNode.getCommandStepInfo().getCommandUnits() == null) {
+      return Optional.empty();
+    }
+    return commandStepNode.getCommandStepInfo()
+        .getCommandUnits()
+        .stream()
+        .filter(i -> CommandUnitSpecType.COPY.equals(i.getType()))
+        .findFirst();
+  }
+
+  private JsonNode findDeploymentType(FilterCreationContext filterCreationContext) {
+    if (filterCreationContext == null || filterCreationContext.getCurrentField() == null) {
+      return null;
+    }
+    YamlNode currentNode = filterCreationContext.getCurrentField().getNode();
+    return findDeploymentTypeInHierarchy(currentNode);
+  }
+
+  private JsonNode findDeploymentTypeInHierarchy(YamlNode node) {
+    if (node == null) {
+      return null;
+    }
+    if (node.getCurrJsonNode() == null) {
+      return node.getParentNode() != null ? findDeploymentTypeInHierarchy(node.getParentNode()) : null;
+    }
+    JsonNode deploymentType = node.getCurrJsonNode().get(YamlTypes.DEPLOYMENT_TYPE);
+    if (deploymentType != null) {
+      return deploymentType;
+    } else {
+      return findDeploymentTypeInHierarchy(node.getParentNode());
     }
   }
 }
