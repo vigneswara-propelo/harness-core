128ed87e0cde8d166d7fb98cb6f8ba7402d566e5
Shalini Agrawal <shalini.agrawal@harness.io>
2022-10-25 16:09:36 +0530

    fixed release-branch and release-build scripts (#39178)

--- debezium-service/release/release_build.sh
+++ debezium-service/release/release_build.sh
@@ -6,10 +6,13 @@
 
 set -ex
 
+export PLATFORM="jenkins"
+
 export VERSION_FILE=debezium-service/build.properties
-export BUILD=`cat ${VERSION_FILE} | grep 'build.number=' | sed -e 's: *build.number=::g'`
-export PATCH=`cat ${VERSION_FILE} | grep 'build.patch=' | sed -e 's: *build.patch=::g'`
-export VERSION=$BUILD-$PATCH
+export MAJOR=`cat ${VERSION_FILE} | grep 'build.majorVersion=' | sed -e 's: *build.majorVersion=::g'`
+export MINOR=`cat ${VERSION_FILE} | grep 'build.minorVersion=' | sed -e 's: *build.minorVersion=::g'`
+export PATCHVERSION=`cat ${VERSION_FILE} | grep 'build.patchVersion=' | sed -e 's: *build.patchVersion=::g'`
+export VERSION=${MAJOR}.${MINOR}.${PATCHVERSION}
 
 chmod +x debezium-service/build/feature_build.sh
 debezium-service/build/feature_build.sh
