e584081a3b911d29357da7d3d0e4b3c0921becec
Tejas Tiwari <tejas.tiwari@harness.io>
2022-10-27 11:56:07 +0530

    feat: [PL-27160]: added disable harness sm migration (#39231)
    * feat: [PL-27160]: added disable harness sm migration

--- 120-ng-manager/src/main/java/io/harness/ng/core/migration/DisableBuiltInHarnessSMSettingsMigration.java
+++ 120-ng-manager/src/main/java/io/harness/ng/core/migration/DisableBuiltInHarnessSMSettingsMigration.java
@@ -6,13 +6,13 @@ import static io.harness.ngsettings.SettingValueType.BOOLEAN;
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.beans.FeatureName;
-import io.harness.ff.FeatureFlagService;
 import io.harness.migration.NGMigration;
 import io.harness.ng.core.accountsetting.dto.AccountSettingType;
 import io.harness.ng.core.accountsetting.services.NGAccountSettingService;
 import io.harness.ngsettings.SettingIdentifiers;
 import io.harness.ngsettings.entities.Setting;
 import io.harness.repositories.ngsettings.spring.SettingRepository;
+import io.harness.utils.featureflaghelper.NGFeatureFlagHelperService;
 
 import com.google.inject.Inject;
 import java.util.Set;
@@ -21,7 +21,7 @@ import lombok.extern.slf4j.Slf4j;
 @OwnedBy(HarnessTeam.PL)
 @Slf4j
 public class DisableBuiltInHarnessSMSettingsMigration implements NGMigration {
-  @Inject FeatureFlagService featureFlagService;
+  @Inject NGFeatureFlagHelperService featureFlagService;
   @Inject NGAccountSettingService accountSettingService;
   @Inject SettingRepository settingRepository;
 
@@ -29,25 +29,25 @@ public class DisableBuiltInHarnessSMSettingsMigration implements NGMigration {
   public void migrate() {
     String settingIdentifier = SettingIdentifiers.DISABLE_HARNESS_BUILT_IN_SECRET_MANAGER;
     String settingTrueValue = "true";
-    Set<String> accountIds = featureFlagService.getAccountIds(FeatureName.DISABLE_HARNESS_SM);
-    accountIds.forEach(accountId -> {
-      if (accountSettingService.getIsBuiltInSMDisabled(accountId, null, null, AccountSettingType.CONNECTOR)) {
-        Setting setting = Setting.builder()
-                              .accountIdentifier(accountId)
-                              .identifier(settingIdentifier)
-                              .allowOverrides(false)
-                              .category(CORE)
-                              .value(settingTrueValue)
-                              .valueType(BOOLEAN)
-                              .build();
-        try {
+    try {
+      Set<String> accountIds =
+          featureFlagService.getFeatureFlagEnabledAccountIds(FeatureName.DISABLE_HARNESS_SM.name());
+      accountIds.forEach(accountId -> {
+        if (accountSettingService.getIsBuiltInSMDisabled(accountId, null, null, AccountSettingType.CONNECTOR)) {
+          Setting setting = Setting.builder()
+                                .accountIdentifier(accountId)
+                                .identifier(settingIdentifier)
+                                .allowOverrides(false)
+                                .category(CORE)
+                                .value(settingTrueValue)
+                                .valueType(BOOLEAN)
+                                .build();
+
           settingRepository.upsert(setting);
-        } catch (Exception e) {
-          log.error(String.format(
-                        "Error occurred while migrating the setting- %s for account- %s", settingIdentifier, accountId),
-              e);
         }
-      }
-    });
+      });
+    } catch (Exception e) {
+      log.error(String.format("Error occurred while migrating the setting- %s", settingIdentifier), e);
+    }
   }
 }
