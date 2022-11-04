75120deb2377119b250eb97a86afa2a8319d74a0
Soumyajit Das <soumyajit.das@harness.io>
2022-11-04 10:23:48 +0530

    fix: [CI-5792]: Fix openapi failure (#39426)

--- 953-yaml-commons/src/main/java/io/harness/plancreator/strategy/MatrixConfigInterface.java
+++ 953-yaml-commons/src/main/java/io/harness/plancreator/strategy/MatrixConfigInterface.java
@@ -11,7 +11,6 @@ import static io.harness.annotations.dev.HarnessTeam.CDC;
 
 import io.harness.annotations.dev.OwnedBy;
 
-import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import io.swagger.annotations.ApiModel;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -19,7 +18,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @OwnedBy(CDC)
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type", include = JsonTypeInfo.As.EXISTING_PROPERTY,
     defaultImpl = MatrixConfig.class)
-@JsonSubTypes({ @JsonSubTypes.Type(value = MatrixConfig.class, name = "matrix") })
 @ApiModel("MatrixConfigInterface")
 @Schema(name = "MatrixConfigInterface", description = "Interface for Matrix Config")
 public interface MatrixConfigInterface {}
