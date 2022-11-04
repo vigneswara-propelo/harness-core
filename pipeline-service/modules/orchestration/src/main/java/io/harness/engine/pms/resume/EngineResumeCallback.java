697a6cd13aa0c063c19eb7eb1fdda389ebbd72e7
Tathagat Chaurasiya <tathagat.chaurasiya@harness.io>
2022-11-04 11:02:04 +0530

    [fix]: [CDS-44875]: adding debug logs for EngineResumeCallback (#39558)

--- pipeline-service/modules/orchestration/src/main/java/io/harness/engine/pms/resume/EngineResumeCallback.java
+++ pipeline-service/modules/orchestration/src/main/java/io/harness/engine/pms/resume/EngineResumeCallback.java
@@ -12,6 +12,7 @@ import static io.harness.annotations.dev.HarnessTeam.CDC;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.engine.OrchestrationEngine;
 import io.harness.pms.contracts.ambiance.Ambiance;
+import io.harness.pms.execution.utils.AmbianceUtils;
 import io.harness.pms.sdk.core.steps.io.ResponseDataMapper;
 import io.harness.tasks.ResponseData;
 import io.harness.waiter.OldNotifyCallback;
@@ -21,10 +22,12 @@ import com.google.protobuf.ByteString;
 import java.util.Map;
 import lombok.Builder;
 import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
 
 @OwnedBy(CDC)
 @Value
 @Builder
+@Slf4j
 public class EngineResumeCallback implements OldNotifyCallback {
   @Inject OrchestrationEngine orchestrationEngine;
   @Inject ResponseDataMapper responseDataMapper;
@@ -42,7 +45,10 @@ public class EngineResumeCallback implements OldNotifyCallback {
   }
 
   private void notifyWithError(Map<String, ResponseData> response, boolean asyncError) {
+    log.info("EngineResumeCallback notify is called for ambiance with nodeExecutionId {}",
+        AmbianceUtils.obtainCurrentRuntimeId(ambiance));
     Map<String, ByteString> byteStringMap = responseDataMapper.toResponseDataProto(response);
+
     orchestrationEngine.resumeNodeExecution(ambiance, byteStringMap, asyncError);
   }
 }
