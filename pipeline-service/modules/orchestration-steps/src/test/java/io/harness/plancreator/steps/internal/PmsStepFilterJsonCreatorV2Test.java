c3de060fd4fc86eb35302ac03d24beb761a97849
Vivek Dixit <vivek.dixit@harness.io>
2022-11-07 12:35:39 +0530

    techdebt: [PIE-6170]: Added Uts For PmsStepFilterJsonCreatorV2 (#39627)
    * Added UTs for Class PmsStepFilterJsonCreatorV2
    
    * Checkstyle

---
+++ pipeline-service/modules/orchestration-steps/src/test/java/io/harness/plancreator/steps/internal/PmsStepFilterJsonCreatorV2Test.java
@@ -0,0 +1,47 @@
+package io.harness.plancreator.steps.internal;
+
+import static io.harness.rule.OwnerRule.VIVEK_DIXIT;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.harness.CategoryTest;
+import io.harness.annotations.dev.HarnessTeam;
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.category.element.UnitTests;
+import io.harness.rule.Owner;
+import io.harness.steps.StepSpecTypeConstants;
+
+import com.google.common.collect.Sets;
+import java.io.IOException;
+import java.util.Set;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+
+@OwnedBy(HarnessTeam.PIPELINE)
+public class PmsStepFilterJsonCreatorV2Test extends CategoryTest {
+  @InjectMocks PmsStepFilterJsonCreatorV2 pmsStepFilterJsonCreatorV2;
+
+  @Before
+  public void setup() throws IOException {
+    MockitoAnnotations.initMocks(this);
+  }
+
+  @Test
+  @Owner(developers = VIVEK_DIXIT)
+  @Category(UnitTests.class)
+  public void testGetSupportedStepTypes() {
+    Set<String> strings = pmsStepFilterJsonCreatorV2.getSupportedStepTypes();
+    Set<String> strings2 = Sets.newHashSet(StepSpecTypeConstants.HTTP, StepSpecTypeConstants.JIRA_CREATE,
+        StepSpecTypeConstants.CUSTOM_APPROVAL, StepSpecTypeConstants.JIRA_UPDATE, StepSpecTypeConstants.JIRA_APPROVAL,
+        StepSpecTypeConstants.HARNESS_APPROVAL, StepSpecTypeConstants.SHELL_SCRIPT,
+        StepSpecTypeConstants.SERVICENOW_APPROVAL, StepSpecTypeConstants.BARRIER, StepSpecTypeConstants.POLICY_STEP,
+        StepSpecTypeConstants.SERVICENOW_CREATE, StepSpecTypeConstants.SERVICENOW_UPDATE,
+        StepSpecTypeConstants.SERVICENOW_IMPORT_SET, StepSpecTypeConstants.QUEUE, StepSpecTypeConstants.EMAIL,
+        StepSpecTypeConstants.WAIT_STEP);
+    assertThat(strings).isEqualTo(strings2);
+    assertThat(strings).contains(StepSpecTypeConstants.JIRA_CREATE);
+  }
+}
