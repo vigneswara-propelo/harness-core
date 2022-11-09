bca123687e7f942782a565266384a4815a122f08
Udham Singh <udham.singh@harness.io>
2022-11-09 13:42:32 +0530

    fix: [BT-2213]: Computing sources from git diff (#39780)
    * fix: [BT-2213]: Computing sources from git diff
    
    * feat: [BT-2213]: adding node js and python in cie agent
    
    * fix: [BT-2213]: Adding java libs path in the sonar properties.

--- scripts/sonar/sonar-pr-scanner.sh
+++ scripts/sonar/sonar-pr-scanner.sh
@@ -35,13 +35,15 @@ function clean_temp_files() {
 JAVA_CLASSES_PATH="/tmp/execroot/harness_monorepo/bazel-out/k8-fastbuild/bin"
 JAVA_SRCS="src"
 JAVA_TEST_SRCS='src/test/**/*.java'
-JAVA_SRC_CLASS="_javac"
+JAVA_LIBS="*.jar"
+JAVA_SRC_CLASS="_javac/**/*.class"
 PR_MODULES_JAVAC_FILE="pr_javac_list.txt"
+PR_MODULES_LIB_FILE="pr_lib_list.txt"
 PR_SRCS_FILE="pr_srcs.txt"
 PR_TEST_INCLUSION_FILE="pr_test_inclusions.txt"
 SONAR_CONFIG_FILE='sonar-project.properties'
 
-create_empty_files "$PR_MODULES_JAVAC_FILE $PR_SRCS_FILE $PR_TEST_INCLUSION_FILE"
+create_empty_files "$PR_MODULES_JAVAC_FILE $PR_SRCS_FILE $PR_TEST_INCLUSION_FILE $PR_MODULES_LIB_FILE"
 
 while getopts ":hb:c:" opt; do
   case $opt in
@@ -66,12 +68,17 @@ PR_MODULES=$($GIT_DIFF | awk -F/ '{print $1}' | sort -u | tr '\r\n' ' ')
 check_cmd_status "$?" "Failed to get modules from commits."
 #echo "PR_MODULES: $PR_MODULES"
 
+for file in $($GIT_DIFF | tr '\r\n' ' ')
+  do
+    grep -w 'src' <<< $file | sed 's|src|:|' | awk -F: '{print $1}' | sed 's|$|src|' >> $PR_SRCS_FILE
+  done
+
 for module in $PR_MODULES
   do
      [ -d ${module} ] && [[ "${HARNESS_CORE_MODULES}" =~ "${module}" ]] \
      && echo "$module is present in the bazel modules list." \
      && echo "${JAVA_CLASSES_PATH}/${module}/${JAVA_SRC_CLASS}" >> $PR_MODULES_JAVAC_FILE \
-     && echo "${module}/${JAVA_SRCS}" >> $PR_SRCS_FILE \
+     && echo "${JAVA_CLASSES_PATH}/${module}/${JAVA_LIBS}" >> $PR_MODULES_LIB_FILE \
      && echo "${module}/${JAVA_TEST_SRCS}" >> $PR_TEST_INCLUSION_FILE \
      || echo "$module is not present in the bazel modules list"
   done
@@ -81,6 +88,7 @@ if [ ! -s $PR_MODULES_JAVAC_FILE ]; then
 fi
 
 export SONAR_JAVAC_FILES=$(cat $PR_MODULES_JAVAC_FILE | tr '\r\n' ',' | rev | cut -c2- | rev)
+export SONAR_LIBS_FILES=$(cat $PR_MODULES_LIB_FILE | tr '\r\n' ',' | rev | cut -c2- | rev)
 export SONAR_SRCS=$(cat $PR_SRCS_FILE | tr '\r\n' ',' | rev | cut -c2- | rev)
 export SONAR_TEST_INCLUSIONS=$(cat $PR_TEST_INCLUSION_FILE | tr '\r\n' ',' | rev | cut -c2- | rev)
 
@@ -89,13 +97,13 @@ echo "sonar.tests=$SONAR_SRCS" >> ${SONAR_CONFIG_FILE}
 echo "sonar.test.inclusions=$SONAR_TEST_INCLUSIONS" >> ${SONAR_CONFIG_FILE}
 echo "sonar.inclusions=$PR_FILES" >> ${SONAR_CONFIG_FILE}
 echo "sonar.java.binaries=$SONAR_JAVAC_FILES" >> ${SONAR_CONFIG_FILE}
+echo "sonar.java.libraries=$SONAR_LIBS_FILES" >> ${SONAR_CONFIG_FILE}
 echo "sonar.pullrequest.key=$PR_NUMBER" >> ${SONAR_CONFIG_FILE}
 echo "sonar.pullrequest.branch=$PR_BRANCH" >> ${SONAR_CONFIG_FILE}
 echo "sonar.pullrequest.base=$BASE_BRANCH" >> ${SONAR_CONFIG_FILE}
-echo "sonar.pullrequest.provider=GitHub" >> ${SONAR_CONFIG_FILE}
 echo "sonar.pullrequest.github.repository=$REPO_NAME" >> ${SONAR_CONFIG_FILE}
 
 echo "INFO: Sonar Properties"
 cat ${SONAR_CONFIG_FILE}
 
-clean_temp_files "$PR_MODULES_JAVAC_FILE $PR_SRCS_FILE $PR_TEST_INCLUSION_FILE"
+clean_temp_files "$PR_MODULES_JAVAC_FILE $PR_SRCS_FILE $PR_TEST_INCLUSION_FILE $PR_MODULES_LIB_FILE"
