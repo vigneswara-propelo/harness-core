4797e6450a925cdd4f834bc7cf682d0b8a7afd5d
Jenny James <jenny.james@harness.io>
2022-11-11 13:09:02 -0800

    reverting change add to throw exception of no owner mismatch (#40041)

--- 400-rest/src/main/java/software/wings/service/impl/AssignDelegateServiceImpl.java
+++ 400-rest/src/main/java/software/wings/service/impl/AssignDelegateServiceImpl.java
@@ -47,7 +47,6 @@ import io.harness.delegate.beans.DelegateProfile;
 import io.harness.delegate.beans.DelegateProfileScopingRule;
 import io.harness.delegate.beans.DelegateScope;
 import io.harness.delegate.beans.DelegateSelectionLogParams;
-import io.harness.delegate.beans.DelegateTaskInvalidRequestException;
 import io.harness.delegate.beans.NgSetupFields;
 import io.harness.delegate.beans.NoAvailableDelegatesException;
 import io.harness.delegate.beans.TaskGroup;
@@ -889,7 +888,7 @@ public class AssignDelegateServiceImpl implements AssignDelegateService, Delegat
       if (isEmpty(delegates)) {
         task.getNonAssignableDelegates().put(CAN_NOT_ASSIGN_OWNER, Collections.emptyList());
         delegateTaskServiceClassic.addToTaskActivityLog(task, CAN_NOT_ASSIGN_OWNER);
-        throw new DelegateTaskInvalidRequestException(task.getUuid());
+        return eligibleDelegateIds;
       }
 
       eligibleDelegateIds =
