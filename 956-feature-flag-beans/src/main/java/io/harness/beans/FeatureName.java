8063cb5adc8f9a8d789255e04b996b43afecd5bc
Meenakshi Raikwar <meenakshi.raikwar@harness.io>
2022-11-11 12:11:47 +0530

    feat: [PL-29698]: Added FF PL_FORCE_DELETE_CONNECTOR_SECRET (#39938)

--- 956-feature-flag-beans/src/main/java/io/harness/beans/FeatureName.java
+++ 956-feature-flag-beans/src/main/java/io/harness/beans/FeatureName.java
@@ -444,6 +444,8 @@ public enum FeatureName {
   SRM_HOST_SAMPLING_ENABLE("Enables Host Sampling feature for Learning Engine.", HarnessTeam.CV),
   CDS_SHOW_CREATE_PR("Start showing CreatePR step on the plan creator if enabled", HarnessTeam.GITOPS),
   SPG_PIPELINE_ROLLBACK("Enables pipeline rollback on failure option", HarnessTeam.SPG),
+  PL_FORCE_DELETE_CONNECTOR_SECRET(
+      "Enables force delete of connectors and secrets irrespective of existing references.", HarnessTeam.PL),
   PL_CONNECTOR_ENCRYPTION_PRIVILEGED_CALL("make the encryption/decryption call as pirvileged call", HarnessTeam.PL),
   SPG_DASHBOARD_STATS_OPTIMIZE_DEPLOYMENTS(
       "Dashboard stats slow api call optimization. Also solves MOM issue in CG.", HarnessTeam.SPG),
