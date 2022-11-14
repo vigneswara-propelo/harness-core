7743a0dbf4b93ff3e9938f948ff164d825e8b551
Abhinav Singh <abhinav.singh2@harness.io>
2022-11-14 22:51:43 +0530

    feat: [CDS-45915]: common connection pool for all k8s api clients (#40025)

--- 960-api-services/src/main/java/io/harness/k8s/apiclient/ApiClientFactoryImpl.java
+++ 960-api-services/src/main/java/io/harness/k8s/apiclient/ApiClientFactoryImpl.java
@@ -27,13 +27,19 @@ import io.kubernetes.client.util.credentials.ClientCertificateAuthentication;
 import io.kubernetes.client.util.credentials.UsernamePasswordAuthentication;
 import java.util.Base64;
 import java.util.concurrent.TimeUnit;
+import okhttp3.ConnectionPool;
 import okhttp3.OkHttpClient;
 
 @Singleton
 public class ApiClientFactoryImpl implements ApiClientFactory {
+  private static final ConnectionPool connectionPool;
   @Inject OidcTokenRetriever oidcTokenRetriever;
   private static final long READ_TIMEOUT_IN_SECONDS = 120;
   private static final long CONNECTION_TIMEOUT_IN_SECONDS = 60;
+
+  static {
+    connectionPool = new ConnectionPool(32, 5L, TimeUnit.MINUTES);
+  }
 
   @Override
   public ApiClient getClient(KubernetesConfig kubernetesConfig) {
@@ -101,6 +107,7 @@ public class ApiClientFactoryImpl implements ApiClientFactory {
             .newBuilder()
             .readTimeout(useNewReadTimeoutForValidation ? READ_TIMEOUT_IN_SECONDS : 0, TimeUnit.SECONDS)
             .connectTimeout(CONNECTION_TIMEOUT_IN_SECONDS, TimeUnit.SECONDS)
+            .connectionPool(connectionPool)
             .build();
     apiClient.setHttpClient(httpClient);
     return apiClient;
