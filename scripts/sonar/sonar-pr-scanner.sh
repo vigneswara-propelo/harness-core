25656b461c750509469ac23d42716013cb26867a
Udham Singh <udham.singh@harness.io>
2022-11-16 18:13:52 +0530

    fix: [BT-2351]: Disabling cache and compiling PR modules only. (#40151)
    * fix: [BT-2351]: Disabling cache and compiling PR modules only.
    
    * fix: [BT-2351]: Disabling cache and compiling PR modules only.
    
    * fix: [BT-2351]: Disabling cache and compiling PR modules only.
    
    * fix: [BT-2351]: modifying array assingment to bazel build
    
    * fix: [BT-2351]: adding echo statements.
    
    * fix: [BT-2351]: Handling different javac and jar paths
    
    * fix: [BT-2351]: removing ls command

--- scripts/sonar/sonar-pr-scanner.sh
+++ scripts/sonar/sonar-pr-scanner.sh
@@ -4,12 +4,15 @@
 # that can be found in the licenses directory at the root of this repository, also available at
 # https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
 
-#Copyright 2021 Harness Inc. All rights reserved.
-#Use of this source code is governed by the PolyForm Shield 1.0.0 license
-#that can be found in the licenses directory at the root of this repository, also available at
-#https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
-
 set -e
+
+trap 'report_error' ERR
+
+function report_error(){
+    echo 'please re-trigger this stage after resolving the issue.'
+    echo 'to trigger this specific stage comment "trigger ss" in your github PR'
+    exit 1
+}
 
 function usage() {
   echo "This scripts runs sonar scan to generate reports for vulnerabilities \
@@ -36,16 +39,34 @@ function clean_temp_files() {
    done
 }
 
+function get_info_from_file(){
+  local_filename=$1
+  cat $local_filename | sort -u | tr '\r\n' ',' | rev | cut -c2- | rev
+  check_cmd_status "$?" "Unable to find file to extract info for sonar file."
+}
+
+function get_javac_path(){
+  local_modulename=$1
+  find $local_modulename -type d -name "_javac" | sort -u | tr '\r\n' ',' | rev | cut -c2- | rev
+  check_cmd_status "$?" "Unable to find the javac path."
+}
+
 JAVA_CLASSES_PATH="/tmp/execroot/harness_monorepo/bazel-out/k8-fastbuild/bin"
 JAVA_SRCS="src"
 JAVA_TEST_SRCS='src/test/**/*.java'
-JAVA_LIBS="*.jar"
+JAVA_LIBS="**/*.jar"
 JAVA_SRC_CLASS="_javac"
 PR_MODULES_JAVAC_FILE="pr_javac_list.txt"
 PR_MODULES_LIB_FILE="pr_lib_list.txt"
 PR_SRCS_FILE="pr_srcs.txt"
 PR_TEST_INCLUSION_FILE="pr_test_inclusions.txt"
 SONAR_CONFIG_FILE='sonar-project.properties'
+BAZEL_COMPILE_MODULES=()
+
+BAZEL_ARGS="--announce_rc --keep_going --show_timestamps --verbose_failures --remote_max_connections=1000 --remote_retries=1"
+
+# This script is required to generate the test util bzl file in root directory.
+scripts/bazel/generate_credentials.sh
 
 create_empty_files "$PR_MODULES_JAVAC_FILE $PR_SRCS_FILE $PR_TEST_INCLUSION_FILE $PR_MODULES_LIB_FILE"
 
@@ -77,24 +98,41 @@ for file in $($GIT_DIFF | tr '\r\n' ' ')
     grep -w 'src' <<< $file | sed 's|src|:|' | awk -F: '{print $1}' | sed 's|$|src|' >> $PR_SRCS_FILE
   done
 
+# Running Bazel Build
+for module in $PR_MODULES
+  do
+     [ -d ${module} ] && [[ "${HARNESS_CORE_MODULES}" =~ "${module}" ]] \
+     && BAZEL_COMPILE_MODULES+=("//${module}/...") \
+     || echo "$module is not present in the bazel modules list"
+  done
+
+echo "INFO: BAZEL COMMAND: bazel build ${BAZEL_ARGS} -- ${BAZEL_COMPILE_MODULES[@]} -//product/... -//commons/..."
+bazel build ${BAZEL_ARGS} -- "${BAZEL_COMPILE_MODULES[@]}" -//product/... -//commons/...
+check_cmd_status "$?" "Failed to build harness core modules."
+# Running Bazel Build
+
 for module in $PR_MODULES
   do
      [ -d ${module} ] && [[ "${HARNESS_CORE_MODULES}" =~ "${module}" ]] \
      && echo "$module is present in the bazel modules list." \
-     && echo "${JAVA_CLASSES_PATH}/${module}/${JAVA_SRC_CLASS}" >> $PR_MODULES_JAVAC_FILE \
+     && get_javac_path ${JAVA_CLASSES_PATH}/${module} >> $PR_MODULES_JAVAC_FILE \
      && echo "${JAVA_CLASSES_PATH}/${module}/${JAVA_LIBS}" >> $PR_MODULES_LIB_FILE \
      && echo "${module}/${JAVA_TEST_SRCS}" >> $PR_TEST_INCLUSION_FILE \
      || echo "$module is not present in the bazel modules list"
   done
 
+echo "----------PR_MODULES_JAVAC_FILE-----------"
+cat $PR_MODULES_JAVAC_FILE
+echo "---------------------"
+
 if [ ! -s $PR_MODULES_JAVAC_FILE ]; then
   echo "INFO: No need to run Sonar Scan."; exit 0
 fi
 
-export SONAR_JAVAC_FILES=$(cat $PR_MODULES_JAVAC_FILE | tr '\r\n' ',' | rev | cut -c2- | rev)
-export SONAR_LIBS_FILES=$(cat $PR_MODULES_LIB_FILE | tr '\r\n' ',' | rev | cut -c2- | rev)
-export SONAR_SRCS=$(cat $PR_SRCS_FILE | sort -u | tr '\r\n' ',' | rev | cut -c2- | rev)
-export SONAR_TEST_INCLUSIONS=$(cat $PR_TEST_INCLUSION_FILE | tr '\r\n' ',' | rev | cut -c2- | rev)
+export SONAR_JAVAC_FILES=$(get_info_from_file $PR_MODULES_JAVAC_FILE)
+export SONAR_LIBS_FILES=$(get_info_from_file $PR_MODULES_LIB_FILE)
+export SONAR_SRCS=$(get_info_from_file $PR_SRCS_FILE)
+export SONAR_TEST_INCLUSIONS=$(get_info_from_file $PR_TEST_INCLUSION_FILE)
 
 echo "sonar.sources=$SONAR_SRCS" >> ${SONAR_CONFIG_FILE}
 echo "sonar.tests=$SONAR_SRCS" >> ${SONAR_CONFIG_FILE}
