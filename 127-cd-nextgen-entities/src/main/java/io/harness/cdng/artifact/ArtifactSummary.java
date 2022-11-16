31f1fe8b65ee05c373455ec98f95bead3bade212
vivekkarnatiharness <105721928+vivekkarnatiharness@users.noreply.github.com>
2022-11-16 16:00:54 +0530

    [CDS-46708]: artifact summary fix (#40224)

--- 127-cd-nextgen-entities/src/main/java/io/harness/cdng/artifact/ArtifactSummary.java
+++ 127-cd-nextgen-entities/src/main/java/io/harness/cdng/artifact/ArtifactSummary.java
@@ -17,7 +17,8 @@ import io.swagger.annotations.ApiModel;
 @ApiModel(subTypes = {NexusArtifactSummary.class, ArtifactoryArtifactSummary.class, DockerArtifactSummary.class,
               CustomArtifactSummary.class, AcrArtifactSummary.class, ArtifactoryGenericArtifactSummary.class,
               EcrArtifactSummary.class, GcrArtifactSummary.class, S3ArtifactSummary.class, JenkinsArtifactSummary.class,
-              GithubPackagesArtifactSummary.class, AzureArtifactsSummary.class, AMIArtifactSummary.class})
+              GithubPackagesArtifactSummary.class, AzureArtifactsSummary.class, AMIArtifactSummary.class,
+              GarArtifactSummary.class})
 public interface ArtifactSummary {
   String getType();
   String getDisplayName();
