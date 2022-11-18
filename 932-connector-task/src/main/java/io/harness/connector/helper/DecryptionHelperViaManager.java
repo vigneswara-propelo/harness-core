3719c3cb86be6da109a81a1b7bc9a18b3fdd7695
Dev Mittal <devki.mittal@harness.io>
2022-11-18 13:11:18 +0530

    fix: [CI-6003]: fixed error message in connection test (#40229)

--- 932-connector-task/src/main/java/io/harness/connector/helper/DecryptionHelperViaManager.java
+++ 932-connector-task/src/main/java/io/harness/connector/helper/DecryptionHelperViaManager.java
@@ -9,12 +9,16 @@ package io.harness.connector.helper;
 
 import io.harness.beans.DecryptableEntity;
 import io.harness.connector.DecryptableEntityHelper;
+import io.harness.exception.InvalidRequestException;
 import io.harness.ng.core.DecryptableEntityWithEncryptionConsumers;
 import io.harness.remote.client.NGRestClientExecutor;
 import io.harness.secrets.remote.SecretNGManagerClient;
 import io.harness.security.encryption.EncryptedDataDetail;
+import io.harness.security.encryption.EncryptedRecordData;
+import io.harness.security.encryption.EncryptionType;
 
 import com.google.inject.Inject;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DecryptionHelperViaManager implements DecryptionHelper {
@@ -25,6 +29,28 @@ public class DecryptionHelperViaManager implements DecryptionHelper {
   public DecryptableEntity decrypt(DecryptableEntity decryptableEntity, List<EncryptedDataDetail> encryptionDetails) {
     final DecryptableEntityWithEncryptionConsumers build =
         decryptableEntityHelper.buildDecryptableEntityWithEncryptionConsumers(decryptableEntity, encryptionDetails);
+    // While using connection via harness platform, custom secret managers are not allowed,
+    // https://harness.atlassian.net/browse/CI-6003
+    validateSecretManager(encryptionDetails);
     return restClientExecutor.getResponse(ngSecretDecryptionClient.decryptEncryptedDetails(build, "random"));
+  }
+
+  private void validateSecretManager(List<EncryptedDataDetail> encryptionDetails) {
+    List<String> customManagedSecrets = new ArrayList<>();
+    for (EncryptedDataDetail encryptedDataDetail : encryptionDetails) {
+      EncryptedRecordData encryptedRecordData = encryptedDataDetail.getEncryptedData();
+
+      if (encryptedRecordData.getEncryptionType() != EncryptionType.LOCAL) {
+        if (!customManagedSecrets.contains(encryptedRecordData.getName())) {
+          customManagedSecrets.add(encryptedRecordData.getName());
+        }
+      }
+    }
+
+    if (!customManagedSecrets.isEmpty()) {
+      throw new InvalidRequestException(
+          "Connection via Harness Platform is not allowed if secrets are saved using Custom Secret Manager. Review the following secrets to use Connection via Harness Platform : "
+          + customManagedSecrets);
+    }
   }
 }
