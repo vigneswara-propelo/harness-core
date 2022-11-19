62f8f2afd38a87bcfad3e35744ef35092e2da94c
Tathagat Chaurasiya <tathagat.chaurasiya@harness.io>
2022-11-19 20:48:19 +0530

    [feat]: [CDS-46820]: populate yaml for envV2 when used in envV1 supported pipelines (#40374)
    * [feat]: [CDS-46820]: populate yaml for envV2 when used in envV1 supported pipelines
    
    * [feat]: [CDS-46820]: refactor and null checks
    
    * [fix]: [CDS-46820]: pr comments
    
    * [fix]: [CDS-46820]: handle environmenYaml nonNull && envRef null case

--- 125-cd-nextgen/src/main/java/io/harness/cdng/infra/InfraStepUtils.java
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/infra/InfraStepUtils.java
@@ -9,6 +9,8 @@ package io.harness.cdng.infra;
 
 import static io.harness.ng.core.mapper.TagMapper.convertToList;
 
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+
 import io.harness.accesscontrol.acl.api.Principal;
 import io.harness.accesscontrol.acl.api.Resource;
 import io.harness.accesscontrol.acl.api.ResourceScope;
@@ -23,6 +25,7 @@ import io.harness.common.ParameterFieldHelper;
 import io.harness.data.structure.EmptyPredicate;
 import io.harness.exception.InvalidRequestException;
 import io.harness.ng.core.environment.beans.Environment;
+import io.harness.ng.core.environment.beans.Environment.EnvironmentKeys;
 import io.harness.ng.core.environment.services.EnvironmentService;
 import io.harness.ng.core.mapper.TagMapper;
 import io.harness.pms.contracts.ambiance.Ambiance;
@@ -31,16 +34,28 @@ import io.harness.pms.execution.utils.AmbianceUtils;
 import io.harness.pms.rbac.PrincipalTypeProtoToPrincipalTypeMapper;
 import io.harness.pms.tags.TagUtils;
 import io.harness.pms.yaml.ParameterField;
+import io.harness.pms.yaml.YamlField;
+import io.harness.pms.yaml.YamlNode;
+import io.harness.pms.yaml.YamlUtils;
 import io.harness.rbac.CDNGRbacPermissions;
 import io.harness.repositories.UpsertOptions;
 import io.harness.steps.environment.EnvironmentOutcome;
+import io.harness.yaml.utils.JsonPipelineUtils;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import lombok.NonNull;
 import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 
 @OwnedBy(HarnessTeam.CDC)
 @UtilityClass
+@Slf4j
 public class InfraStepUtils {
+  public static List<String> envFieldToBeUpdated = Arrays.asList("type", "name");
   public void validateResources(
       AccessControlClient accessControlClient, Ambiance ambiance, InfraSectionStepParameters stepParameters) {
     String accountIdentifier = AmbianceUtils.getAccountId(ambiance);
@@ -71,15 +86,66 @@ public class InfraStepUtils {
 
   public EnvironmentOutcome processEnvironment(EnvironmentService environmentService, Ambiance ambiance,
       EnvironmentYaml environmentYaml, ParameterField<String> environmentRef) {
+    // environmentRef and environmentYaml are annotated with @OneOfField in PipelineInfrastructure.java
+    Optional<Environment> environmentInDb = Optional.empty();
+    if (environmentRef != null && isNotBlank(environmentRef.getValue())) {
+      environmentInDb = getEnvironment(environmentService, ambiance, environmentRef.getValue());
+    } else if (environmentYaml != null) {
+      environmentInDb = getEnvironment(environmentService, ambiance, environmentYaml.getIdentifier());
+    }
+
+    if (environmentInDb.isEmpty() && environmentYaml == null) {
+      throw new InvalidRequestException("Env with identifier " + environmentRef.getValue() + " does not exist");
+    }
+
     if (environmentYaml == null) {
-      environmentYaml = createEnvYamlFromEnvRef(environmentService, ambiance, environmentRef);
+      environmentYaml = createEnvYamlFromEnvRef(environmentInDb.get());
     }
     if (EmptyPredicate.isEmpty(environmentYaml.getName())) {
       environmentYaml.setName(environmentYaml.getIdentifier());
     }
+
     Environment environment = getEnvironmentObject(environmentYaml, ambiance);
+
+    // To support EnvironmentV2 entities in older EnvironmentV1 supported pipeline
+    if (environmentInDb.isPresent() && isNotBlank(environmentInDb.get().getYaml())) {
+      String updatedYaml = getUpdatedYaml(environmentInDb.get().getYaml(), environmentYaml);
+      if (isNotBlank(updatedYaml)) {
+        environment.setYaml(updatedYaml);
+      }
+    }
+
     environmentService.upsert(environment, UpsertOptions.DEFAULT.withNoOutbox());
     return EnvironmentMapper.toOutcome(environmentYaml);
+  }
+
+  private static String getUpdatedYaml(@NonNull String environmentYamlInDb, @NonNull EnvironmentYaml environmentYaml) {
+    try {
+      YamlField yamlField = YamlUtils.readTree(environmentYamlInDb);
+      YamlNode yamlNode = yamlField.getNode();
+      if (yamlNode != null && yamlNode.isObject() && yamlNode.getCurrJsonNode().get("environment") != null
+          && yamlNode.getCurrJsonNode().get("environment").isObject()) {
+        ObjectNode objectNode = (ObjectNode) yamlNode.getCurrJsonNode().get("environment");
+        if (objectNode != null) {
+          objectNode.put(EnvironmentKeys.name, environmentYaml.getName());
+          objectNode.put(EnvironmentKeys.type, environmentYaml.getType().toString());
+          objectNode.replace(EnvironmentKeys.tags, JsonPipelineUtils.asTree(environmentYaml.getTags()));
+          objectNode.put(EnvironmentKeys.description, environmentYaml.getDescription().getValue());
+        }
+      }
+      return YamlUtils.writeYamlString(yamlField).replaceFirst("---\n", "");
+    } catch (Exception e) {
+      log.warn("updating environment yaml operation failed", e);
+      return StringUtils.EMPTY;
+    }
+  }
+
+  private Optional<Environment> getEnvironment(
+      EnvironmentService environmentService, Ambiance ambiance, String envIdentifier) {
+    String accountId = AmbianceUtils.getAccountId(ambiance);
+    String projectIdentifier = AmbianceUtils.getProjectIdentifier(ambiance);
+    String orgIdentifier = AmbianceUtils.getOrgIdentifier(ambiance);
+    return environmentService.get(accountId, orgIdentifier, projectIdentifier, envIdentifier, false);
   }
 
   private Environment getEnvironmentObject(EnvironmentYaml environmentYaml, Ambiance ambiance) {
@@ -101,26 +167,14 @@ public class InfraStepUtils {
         .build();
   }
 
-  private EnvironmentYaml createEnvYamlFromEnvRef(
-      EnvironmentService environmentService, Ambiance ambiance, ParameterField<String> environmentRef) {
-    String accountId = AmbianceUtils.getAccountId(ambiance);
-    String projectIdentifier = AmbianceUtils.getProjectIdentifier(ambiance);
-    String orgIdentifier = AmbianceUtils.getOrgIdentifier(ambiance);
-    String envIdentifier = environmentRef.getValue();
-
-    Optional<Environment> optionalEnvironment =
-        environmentService.get(accountId, orgIdentifier, projectIdentifier, envIdentifier, false);
-    if (optionalEnvironment.isPresent()) {
-      Environment env = optionalEnvironment.get();
-      return EnvironmentYaml.builder()
-          .identifier(envIdentifier)
-          .name(env.getName())
-          .description(env.getDescription() == null ? ParameterField.createValueField("")
-                                                    : ParameterField.createValueField(env.getDescription()))
-          .type(env.getType())
-          .tags(TagMapper.convertToMap(env.getTags()))
-          .build();
-    }
-    throw new InvalidRequestException("Env with identifier " + envIdentifier + " does not exist");
+  private EnvironmentYaml createEnvYamlFromEnvRef(Environment env) {
+    return EnvironmentYaml.builder()
+        .identifier(env.getIdentifier())
+        .name(env.getName())
+        .description(env.getDescription() == null ? ParameterField.createValueField("")
+                                                  : ParameterField.createValueField(env.getDescription()))
+        .type(env.getType())
+        .tags(TagMapper.convertToMap(env.getTags()))
+        .build();
   }
 }
