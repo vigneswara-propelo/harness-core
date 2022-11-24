7c6fb25399c4dcf4e17b8a429fd6384b7f3b446f
Ved Tambat <ved.atultambat@harness.io>
2022-11-24 16:13:45 +0530

    [feat]: [CDS-47130]: S3ResourceServiceImplTest coverage Improvement (#40579)
    * [feat]: [CDS-47130]: S3ResourceServiceImplTest coverage Improvement
    
    * [feat]: [CDS-47130]: Pr check issue

--- 125-cd-nextgen/src/main/java/io/harness/cdng/buckets/resources/s3/S3ResourceServiceImpl.java
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/buckets/resources/s3/S3ResourceServiceImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Harness Inc. All rights reserved.
+ * Copyright 2022 Harness Inc. All rights reserved.
  * Use of this source code is governed by the PolyForm Shield 1.0.0 license
  * that can be found in the licenses directory at the root of this repository, also available at
  * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
@@ -32,6 +32,7 @@ import software.wings.helpers.ext.jenkins.BuildDetails;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -72,7 +73,7 @@ public class S3ResourceServiceImpl implements S3ResourceService {
   public List<BuildDetails> getFilePaths(IdentifierRef awsConnectorRef, String region, String bucketName,
       String filePathRegex, String orgIdentifier, String projectIdentifier) {
     if (EmptyPredicate.isEmpty(filePathRegex)) {
-      return null;
+      return new ArrayList<>();
     }
 
     if (EmptyPredicate.isEmpty(region)) {
--- 125-cd-nextgen/src/test/java/io/harness/cdng/buckets/resources/s3/S3ResourceServiceImplTest.java
+++ 125-cd-nextgen/src/test/java/io/harness/cdng/buckets/resources/s3/S3ResourceServiceImplTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Harness Inc. All rights reserved.
+ * Copyright 2022 Harness Inc. All rights reserved.
  * Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
  * that can be found in the licenses directory at the root of this repository, also available at
  * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
@@ -9,9 +9,11 @@ package io.harness.cdng.buckets.resources.s3;
 
 import static io.harness.annotations.dev.HarnessTeam.CDP;
 import static io.harness.rule.OwnerRule.ACASIAN;
+import static io.harness.rule.OwnerRule.VED;
 
 import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.nullable;
 import static org.mockito.Matchers.any;
@@ -32,6 +34,7 @@ import io.harness.delegate.beans.connector.awsconnector.AwsCredentialType;
 import io.harness.delegate.beans.connector.awsconnector.AwsS3BucketResponse;
 import io.harness.delegate.beans.connector.awsconnector.AwsTaskParams;
 import io.harness.delegate.beans.connector.awsconnector.AwsTaskType;
+import io.harness.delegate.beans.connector.awsconnector.S3BuildsResponse;
 import io.harness.exception.BucketServerException;
 import io.harness.exception.InvalidRequestException;
 import io.harness.logging.CommandExecutionStatus;
@@ -40,9 +43,11 @@ import io.harness.rule.Owner;
 import io.harness.security.encryption.EncryptedDataDetail;
 
 import software.wings.beans.TaskType;
+import software.wings.helpers.ext.jenkins.BuildDetails;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Sets;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import org.junit.Before;
@@ -58,10 +63,23 @@ public class S3ResourceServiceImplTest extends CategoryTest {
   @Mock private AwsResourceServiceHelper serviceHelper;
 
   @InjectMocks private S3ResourceServiceImpl s3ResourceServiceImpl;
+
   private final IdentifierRef awsConnectorRef = IdentifierRef.builder().identifier("aws").build();
+
   private final List<EncryptedDataDetail> encryptionDetails = singletonList(EncryptedDataDetail.builder().build());
+
   private String region = "us-east-1";
+
+  private String filePathRegex = "*";
+
+  private String orgIdentifier = "org";
+
+  private String projectIdentifier = "project";
+
+  private String bucket = "bucket";
+
   private BaseNGAccess baseNGAccess = BaseNGAccess.builder().build();
+
   @Before
   public void setup() {
     MockitoAnnotations.initMocks(this);
@@ -108,9 +126,25 @@ public class S3ResourceServiceImplTest extends CategoryTest {
   }
 
   @Test
-  @Owner(developers = ACASIAN)
+  @Owner(developers = VED)
   @Category(UnitTests.class)
   public void testShouldGetBucketsFailure() {
+    AwsS3BucketResponse response =
+        AwsS3BucketResponse.builder().commandExecutionStatus(CommandExecutionStatus.FAILURE).build();
+
+    doReturn(response)
+        .when(serviceHelper)
+        .getResponseData(eq(baseNGAccess), nullable(AwsTaskParams.class), eq(TaskType.NG_AWS_TASK.name()));
+
+    assertThatThrownBy(() -> s3ResourceServiceImpl.getBuckets(awsConnectorRef, region, "test-org", "test-proj"))
+        .isInstanceOf(BucketServerException.class)
+        .hasMessage("AWS S3 Get Buckets task failure due to error");
+  }
+
+  @Test
+  @Owner(developers = ACASIAN)
+  @Category(UnitTests.class)
+  public void testShouldGetBucketsError() {
     ErrorNotifyResponseData response = ErrorNotifyResponseData.builder()
                                            .errorMessage("Failed to list buckets")
                                            .exception(new InvalidRequestException("Failed to list buckets"))
@@ -125,6 +159,264 @@ public class S3ResourceServiceImplTest extends CategoryTest {
     } catch (BucketServerException bucketServerException) {
       assertThat(bucketServerException.getMessage())
           .isEqualTo("AWS S3 Get Buckets task failure due to error - Failed to list buckets");
+    }
+  }
+
+  @Test
+  @Owner(developers = VED)
+  @Category(UnitTests.class)
+  public void testShouldGetBucketsWithDefaultRegion() {
+    Map<String, String> actualBuckets =
+        ImmutableMap.of("test-bucket-1", "test-bucket-1", "test-bucket-2", "test-bucket-2");
+    AwsS3BucketResponse response = AwsS3BucketResponse.builder()
+                                       .buckets(actualBuckets)
+                                       .commandExecutionStatus(CommandExecutionStatus.SUCCESS)
+                                       .build();
+    doReturn(response)
+        .when(serviceHelper)
+        .getResponseData(eq(baseNGAccess), nullable(AwsTaskParams.class), eq(TaskType.NG_AWS_TASK.name()));
+
+    Map<String, String> result = s3ResourceServiceImpl.getBuckets(awsConnectorRef, null, "test-org", "test-proj");
+    assertThat(result).isNotNull();
+    assertThat(result).isNotEmpty();
+    assertThat(result).containsOnlyKeys("test-bucket-1", "test-bucket-2");
+    assertThat(result).containsValues("test-bucket-1", "test-bucket-2");
+
+    ArgumentCaptor<AwsTaskParams> awsTaskParamsCaptor = ArgumentCaptor.forClass(AwsTaskParams.class);
+    verify(serviceHelper, times(1))
+        .getResponseData(eq(baseNGAccess), awsTaskParamsCaptor.capture(), eq(TaskType.NG_AWS_TASK.name()));
+    AwsTaskParams awsTaskParams = awsTaskParamsCaptor.getValue();
+    assertThat(awsTaskParams).isNotNull();
+    assertThat(awsTaskParams.getAwsConnector()).isNotNull();
+    assertThat(awsTaskParams.getRegion()).isEqualTo(region);
+    assertThat(awsTaskParams.getAwsTaskType()).isEqualTo(AwsTaskType.LIST_S3_BUCKETS);
+    assertThat(awsTaskParams.getAwsConnector().getDelegateSelectors()).contains("proj-delegate");
+  }
+
+  @Test
+  @Owner(developers = VED)
+  @Category(UnitTests.class)
+  public void testGetFilePaths() {
+    List<BuildDetails> builds = new ArrayList<>();
+
+    BuildDetails build1 = new BuildDetails();
+    build1.setNumber("b1");
+    build1.setUiDisplayName("Version# b1");
+
+    BuildDetails build2 = new BuildDetails();
+    build2.setNumber("b2");
+    build2.setUiDisplayName("Version# b2");
+
+    BuildDetails build3 = new BuildDetails();
+    build3.setNumber("b3");
+    build3.setUiDisplayName("Version# b3");
+
+    BuildDetails build4 = new BuildDetails();
+    build4.setNumber("b4");
+    build4.setUiDisplayName("Version# b4");
+
+    BuildDetails build5 = new BuildDetails();
+    build5.setNumber("b5");
+    build5.setUiDisplayName("Version# b5");
+
+    builds.add(build1);
+    builds.add(build2);
+    builds.add(build3);
+    builds.add(build4);
+    builds.add(build5);
+
+    S3BuildsResponse expectedBuilds = S3BuildsResponse.builder()
+                                          .builds(builds)
+                                          .commandExecutionStatus(CommandExecutionStatus.SUCCESS)
+                                          .delegateMetaInfo(null)
+                                          .build();
+
+    doReturn(expectedBuilds)
+        .when(serviceHelper)
+        .getResponseData(eq(baseNGAccess), nullable(AwsTaskParams.class), eq(TaskType.NG_AWS_TASK.name()));
+
+    List<BuildDetails> buildResponse = s3ResourceServiceImpl.getFilePaths(
+        awsConnectorRef, region, bucket, filePathRegex, orgIdentifier, projectIdentifier);
+
+    assertThat(buildResponse).isNotNull();
+    assertThat(buildResponse).isNotEmpty();
+    assertThat(buildResponse.size()).isEqualTo(5);
+    assertThat(buildResponse.get(0).getNumber()).isEqualTo("b1");
+    assertThat(buildResponse.get(1).getUiDisplayName()).isEqualTo("Version# b2");
+    assertThat(buildResponse.get(2).getUiDisplayName()).isEqualTo("Version# b3");
+    assertThat(buildResponse.get(3).getUiDisplayName()).isEqualTo("Version# b4");
+    assertThat(buildResponse.get(4).getNumber()).isEqualTo("b5");
+
+    ArgumentCaptor<AwsTaskParams> awsTaskParamsCaptor = ArgumentCaptor.forClass(AwsTaskParams.class);
+
+    verify(serviceHelper, times(1))
+        .getResponseData(eq(baseNGAccess), awsTaskParamsCaptor.capture(), eq(TaskType.NG_AWS_TASK.name()));
+
+    AwsTaskParams awsTaskParams = awsTaskParamsCaptor.getValue();
+
+    assertThat(awsTaskParams).isNotNull();
+    assertThat(awsTaskParams.getAwsConnector()).isNotNull();
+    assertThat(awsTaskParams.getRegion()).isEqualTo(region);
+    assertThat(awsTaskParams.getAwsTaskType()).isEqualTo(AwsTaskType.GET_BUILDS);
+    assertThat(awsTaskParams.getAwsConnector().getDelegateSelectors()).contains("proj-delegate");
+  }
+
+  @Test
+  @Owner(developers = VED)
+  @Category(UnitTests.class)
+  public void testGetFilePathsWithEmptyRegion() {
+    List<BuildDetails> builds = new ArrayList<>();
+
+    BuildDetails build1 = new BuildDetails();
+    build1.setNumber("b1");
+    build1.setUiDisplayName("Version# b1");
+
+    BuildDetails build2 = new BuildDetails();
+    build2.setNumber("b2");
+    build2.setUiDisplayName("Version# b2");
+
+    BuildDetails build3 = new BuildDetails();
+    build3.setNumber("b3");
+    build3.setUiDisplayName("Version# b3");
+
+    BuildDetails build4 = new BuildDetails();
+    build4.setNumber("b4");
+    build4.setUiDisplayName("Version# b4");
+
+    BuildDetails build5 = new BuildDetails();
+    build5.setNumber("b5");
+    build5.setUiDisplayName("Version# b5");
+
+    builds.add(build1);
+    builds.add(build2);
+    builds.add(build3);
+    builds.add(build4);
+    builds.add(build5);
+
+    S3BuildsResponse expectedBuilds = S3BuildsResponse.builder()
+                                          .builds(builds)
+                                          .commandExecutionStatus(CommandExecutionStatus.SUCCESS)
+                                          .delegateMetaInfo(null)
+                                          .build();
+
+    doReturn(expectedBuilds)
+        .when(serviceHelper)
+        .getResponseData(eq(baseNGAccess), nullable(AwsTaskParams.class), eq(TaskType.NG_AWS_TASK.name()));
+
+    List<BuildDetails> buildResponse = s3ResourceServiceImpl.getFilePaths(
+        awsConnectorRef, null, bucket, filePathRegex, orgIdentifier, projectIdentifier);
+
+    assertThat(buildResponse).isNotNull();
+    assertThat(buildResponse).isNotEmpty();
+    assertThat(buildResponse.size()).isEqualTo(5);
+    assertThat(buildResponse.get(0).getNumber()).isEqualTo("b1");
+    assertThat(buildResponse.get(1).getUiDisplayName()).isEqualTo("Version# b2");
+    assertThat(buildResponse.get(2).getUiDisplayName()).isEqualTo("Version# b3");
+    assertThat(buildResponse.get(3).getUiDisplayName()).isEqualTo("Version# b4");
+    assertThat(buildResponse.get(4).getNumber()).isEqualTo("b5");
+
+    ArgumentCaptor<AwsTaskParams> awsTaskParamsCaptor = ArgumentCaptor.forClass(AwsTaskParams.class);
+
+    verify(serviceHelper, times(1))
+        .getResponseData(eq(baseNGAccess), awsTaskParamsCaptor.capture(), eq(TaskType.NG_AWS_TASK.name()));
+
+    AwsTaskParams awsTaskParams = awsTaskParamsCaptor.getValue();
+
+    assertThat(awsTaskParams).isNotNull();
+    assertThat(awsTaskParams.getAwsConnector()).isNotNull();
+    assertThat(awsTaskParams.getRegion()).isEqualTo(region);
+    assertThat(awsTaskParams.getAwsTaskType()).isEqualTo(AwsTaskType.GET_BUILDS);
+    assertThat(awsTaskParams.getAwsConnector().getDelegateSelectors()).contains("proj-delegate");
+  }
+
+  @Test
+  @Owner(developers = VED)
+  @Category(UnitTests.class)
+  public void testGetFilePathsWithEmptyFilePathRegex() {
+    List<BuildDetails> builds = new ArrayList<>();
+
+    BuildDetails build1 = new BuildDetails();
+    build1.setNumber("b1");
+    build1.setUiDisplayName("Version# b1");
+
+    BuildDetails build2 = new BuildDetails();
+    build2.setNumber("b2");
+    build2.setUiDisplayName("Version# b2");
+
+    BuildDetails build3 = new BuildDetails();
+    build3.setNumber("b3");
+    build3.setUiDisplayName("Version# b3");
+
+    BuildDetails build4 = new BuildDetails();
+    build4.setNumber("b4");
+    build4.setUiDisplayName("Version# b4");
+
+    BuildDetails build5 = new BuildDetails();
+    build5.setNumber("b5");
+    build5.setUiDisplayName("Version# b5");
+
+    builds.add(build1);
+    builds.add(build2);
+    builds.add(build3);
+    builds.add(build4);
+    builds.add(build5);
+
+    S3BuildsResponse expectedBuilds = S3BuildsResponse.builder()
+                                          .builds(new ArrayList<>())
+                                          .commandExecutionStatus(CommandExecutionStatus.SUCCESS)
+                                          .delegateMetaInfo(null)
+                                          .build();
+
+    doReturn(expectedBuilds)
+        .when(serviceHelper)
+        .getResponseData(eq(baseNGAccess), nullable(AwsTaskParams.class), eq(TaskType.NG_AWS_TASK.name()));
+
+    List<BuildDetails> buildResponse =
+        s3ResourceServiceImpl.getFilePaths(awsConnectorRef, region, bucket, null, orgIdentifier, projectIdentifier);
+
+    assertThat(buildResponse).isNotNull();
+    assertThat(buildResponse).isEmpty();
+  }
+
+  @Test
+  @Owner(developers = VED)
+  @Category(UnitTests.class)
+  public void testGetFilePathsFailure() {
+    S3BuildsResponse response =
+        S3BuildsResponse.builder().commandExecutionStatus(CommandExecutionStatus.FAILURE).build();
+
+    doReturn(response)
+        .when(serviceHelper)
+        .getResponseData(eq(baseNGAccess), nullable(AwsTaskParams.class), eq(TaskType.NG_AWS_TASK.name()));
+
+    assertThatThrownBy(()
+                           -> s3ResourceServiceImpl.getFilePaths(
+                               awsConnectorRef, region, bucket, filePathRegex, orgIdentifier, projectIdentifier))
+        .isInstanceOf(BucketServerException.class)
+        .hasMessage("AWS S3 Get File Paths task failure due to error");
+  }
+
+  @Test
+  @Owner(developers = VED)
+  @Category(UnitTests.class)
+  public void testFilePathsError() {
+    ErrorNotifyResponseData response = ErrorNotifyResponseData.builder()
+                                           .errorMessage("Failed to list file paths")
+                                           .exception(new InvalidRequestException("Failed to list file paths"))
+                                           .build();
+
+    doReturn(response)
+        .when(serviceHelper)
+        .getResponseData(eq(baseNGAccess), nullable(AwsTaskParams.class), eq(TaskType.NG_AWS_TASK.name()));
+
+    try {
+      s3ResourceServiceImpl.getFilePaths(
+          awsConnectorRef, region, bucket, filePathRegex, orgIdentifier, projectIdentifier);
+
+      fail("Should throw invalid request exception");
+    } catch (BucketServerException bucketServerException) {
+      assertThat(bucketServerException.getMessage())
+          .isEqualTo("AWS S3 Get File Paths task failure due to error - Failed to list file paths");
     }
   }
 }
