abf52f6ddc4694c6f8296e2bf043de03d0734e10
Prashant Sharma <prashant.sharma@harness.io>
2022-11-24 22:14:43 +0530

    fix: [PIE-6078]:  be given in identifier but ideally it shouldn't be … (#40502)
    * fix: [PIE-6078]:  be given in identifier but ideally it shouldn't be allowed similar to name.
    
    * schema chagne
    
    * fix test
    
    * fix test

--- 970-ng-commons/src/main/java/io/harness/validator/NGRegexValidatorConstants.java
+++ 970-ng-commons/src/main/java/io/harness/validator/NGRegexValidatorConstants.java
@@ -13,7 +13,7 @@ import io.harness.annotations.dev.OwnedBy;
 
 @OwnedBy(PIPELINE)
 public interface NGRegexValidatorConstants {
-  String IDENTIFIER_PATTERN = "^[a-zA-Z_][0-9a-zA-Z_$]{0,63}$";
+  String IDENTIFIER_PATTERN = "^[a-zA-Z_][0-9a-zA-Z_]{0,63}$";
   String VARIABLE_NAME_PATTERN = "^[a-zA-Z_][0-9a-zA-Z_\\.$]{0,63}$";
   String NAME_PATTERN = "^[a-zA-Z_][-0-9a-zA-Z_\\s]{0,63}$";
   String TIMEOUT_PATTERN =
