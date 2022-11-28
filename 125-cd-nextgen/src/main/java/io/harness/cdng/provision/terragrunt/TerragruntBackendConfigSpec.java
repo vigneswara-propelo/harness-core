86626743f83ce847c43ef17b74c8adfca31a6503
Vasile Lica <vasile.lica@harness.io>
2022-11-28 13:46:31 +0200

    feat: [CDS-47382]: adjust terragrunt plan creator for backendConfig a… (#40729)
    * feat: [CDS-47382]: adjust terragrunt plan creator for backendConfig and varFiles
    
    * feat: [CDS-47382]: update deployment stage and steps jsons

--- 125-cd-nextgen/src/main/java/io/harness/cdng/provision/terragrunt/TerragruntBackendConfigSpec.java
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/provision/terragrunt/TerragruntBackendConfigSpec.java
@@ -11,9 +11,20 @@ package io.harness.cdng.provision.terragrunt;
 
 import static io.harness.annotations.dev.HarnessTeam.CDP;
 
+import static com.fasterxml.jackson.annotation.JsonTypeInfo.As.EXTERNAL_PROPERTY;
+import static com.fasterxml.jackson.annotation.JsonTypeInfo.Id.NAME;
+
 import io.harness.annotations.dev.OwnedBy;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
 @OwnedBy(CDP)
+@JsonTypeInfo(use = NAME, property = "type", include = EXTERNAL_PROPERTY, visible = true)
+@JsonSubTypes({
+  @JsonSubTypes.Type(value = InlineTerragruntBackendConfigSpec.class, name = TerragruntBackendFileTypes.Inline)
+  , @JsonSubTypes.Type(value = RemoteTerragruntBackendConfigSpec.class, name = TerragruntBackendFileTypes.Remote),
+})
 public interface TerragruntBackendConfigSpec {
   String getType();
 }
--- 125-cd-nextgen/src/main/java/io/harness/cdng/provision/terragrunt/TerragruntVarFileSpec.java
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/provision/terragrunt/TerragruntVarFileSpec.java
@@ -11,9 +11,20 @@ package io.harness.cdng.provision.terragrunt;
 
 import static io.harness.annotations.dev.HarnessTeam.CDP;
 
+import static com.fasterxml.jackson.annotation.JsonTypeInfo.As.EXTERNAL_PROPERTY;
+import static com.fasterxml.jackson.annotation.JsonTypeInfo.Id.NAME;
+
 import io.harness.annotations.dev.OwnedBy;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
 @OwnedBy(CDP)
+@JsonTypeInfo(use = NAME, property = "type", include = EXTERNAL_PROPERTY, visible = true)
+@JsonSubTypes({
+  @JsonSubTypes.Type(value = InlineTerragruntVarFileSpec.class, name = TerragruntVarFileTypes.Inline)
+  , @JsonSubTypes.Type(value = RemoteTerragruntVarFileSpec.class, name = TerragruntVarFileTypes.Remote),
+})
 public interface TerragruntVarFileSpec {
   String getType();
 }
