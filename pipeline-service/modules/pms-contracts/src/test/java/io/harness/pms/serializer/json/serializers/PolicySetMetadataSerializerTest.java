fa705d60d7a594252c92125a5a0a740ed13f44b7
Vivek Dixit <vivek.dixit@harness.io>
2022-11-30 11:47:00 +0530

    techdebt: [PIE-6527] : Added UTs for Coverage (#40657)
    * Added UTs for Coverage
    
    * Added @OwnedBy(PIPELINE)
    
    * Added Copyright statements
    
    * Added Copyright statements

---
+++ pipeline-service/modules/pms-contracts/src/test/java/io/harness/pms/serializer/json/serializers/PolicySetMetadataSerializerTest.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2022 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
+ */
+
+package io.harness.pms.serializer.json.serializers;
+
+import static io.harness.annotations.dev.HarnessTeam.PIPELINE;
+import static io.harness.rule.OwnerRule.VIVEK_DIXIT;
+
+import io.harness.CategoryTest;
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.category.element.UnitTests;
+import io.harness.rule.Owner;
+
+import org.assertj.core.api.AssertionsForClassTypes;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@OwnedBy(PIPELINE)
+public class PolicySetMetadataSerializerTest extends CategoryTest {
+  @Test
+  @Owner(developers = VIVEK_DIXIT)
+  @Category(UnitTests.class)
+  public void testConstructor() {
+    PolicySetMetadataSerializer policySetMetadataSerializer = new PolicySetMetadataSerializer();
+    AssertionsForClassTypes.assertThat(policySetMetadataSerializer).isNotNull();
+  }
+}
