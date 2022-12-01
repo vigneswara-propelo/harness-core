7cc2916b036b59d663ec3765fc3bc240f9eea930
Vaibhav Singhal <vaibhav.si@harness.io>
2022-12-01 18:43:25 +0530

    feat: [CDS-47536]: added test for FetchLinkeApps Step (#40864)

---
+++ 125-cd-nextgen/src/test/java/io/harness/cdng/gitops/steps/FetchLinkedAppsStepTest.java
@@ -0,0 +1,237 @@
+/*
+ * Copyright 2022 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
+ */
+
+package io.harness.cdng.gitops.steps;
+
+import static io.harness.rule.OwnerRule.VAIBHAV_SI;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+
+import io.harness.CategoryTest;
+import io.harness.annotations.dev.HarnessTeam;
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.beans.EnvironmentType;
+import io.harness.category.element.UnitTests;
+import io.harness.cdng.CDStepHelper;
+import io.harness.cdng.gitops.beans.FetchLinkedAppsStepParams;
+import io.harness.cdng.gitops.beans.GitOpsLinkedAppsOutcome;
+import io.harness.cdng.manifest.yaml.DeploymentRepoManifestOutcome;
+import io.harness.cdng.manifest.yaml.GithubStore;
+import io.harness.connector.ConnectorInfoDTO;
+import io.harness.delegate.beans.TaskData;
+import io.harness.delegate.beans.storeconfig.GitStoreDelegateConfig;
+import io.harness.delegate.task.git.TaskStatus;
+import io.harness.delegate.task.gitops.GitOpsFetchAppTaskResponse;
+import io.harness.exception.InvalidRequestException;
+import io.harness.gitops.models.Application;
+import io.harness.gitops.remote.GitopsResourceClient;
+import io.harness.logstreaming.ILogStreamingStepClient;
+import io.harness.logstreaming.LogStreamingStepClientFactory;
+import io.harness.ng.beans.PageResponse;
+import io.harness.plancreator.steps.common.StepElementParameters;
+import io.harness.pms.contracts.ambiance.Ambiance;
+import io.harness.pms.contracts.execution.Status;
+import io.harness.pms.contracts.execution.tasks.TaskRequest;
+import io.harness.pms.sdk.core.data.OptionalSweepingOutput;
+import io.harness.pms.sdk.core.resolver.RefObjectUtils;
+import io.harness.pms.sdk.core.resolver.outputs.ExecutionSweepingOutputService;
+import io.harness.pms.sdk.core.steps.io.StepResponse;
+import io.harness.pms.yaml.ParameterField;
+import io.harness.rule.Owner;
+import io.harness.steps.StepHelper;
+import io.harness.steps.StepUtils;
+import io.harness.supplier.ThrowingSupplier;
+
+import software.wings.beans.TaskType;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.MockitoRule;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import retrofit2.Call;
+import retrofit2.Response;
+
+@RunWith(MockitoJUnitRunner.class)
+@PrepareForTest({StepUtils.class})
+@OwnedBy(HarnessTeam.GITOPS)
+public class FetchLinkedAppsStepTest extends CategoryTest {
+  @Mock LogStreamingStepClientFactory logStreamingStepClientFactory;
+  @Mock ILogStreamingStepClient logStreamingStepClient;
+  @Mock GitOpsStepHelper gitOpsStepHelper;
+  @Mock CDStepHelper cdStepHelper;
+  @Mock StepHelper stepHelper;
+  @Mock ExecutionSweepingOutputService executionSweepingOutputService;
+  @Mock GitopsResourceClient gitopsResourceClient;
+  @InjectMocks FetchLinkedAppsStep fetchLinkedAppsStep;
+  @Rule public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+  @Test
+  @Owner(developers = VAIBHAV_SI)
+  @Category(UnitTests.class)
+  public void shouldObtainTask() {
+    Ambiance ambiance = Ambiance.newBuilder().build();
+    FetchLinkedAppsStepParams stepParams = FetchLinkedAppsStepParams.infoBuilder().build();
+    StepElementParameters stepElementParameters = StepElementParameters.builder().spec(stepParams).build();
+    doReturn(logStreamingStepClient).when(logStreamingStepClientFactory).getLogStreamingStepClient(ambiance);
+    GithubStore githubStore = GithubStore.builder()
+                                  .connectorRef(ParameterField.createValueField("connectorRef"))
+                                  .branch(ParameterField.createValueField("branch"))
+                                  .paths(ParameterField.createValueField(Collections.singletonList("path1")))
+                                  .build();
+    DeploymentRepoManifestOutcome manifestOutcome =
+        DeploymentRepoManifestOutcome.builder().identifier("id").store(githubStore).build();
+    doReturn(manifestOutcome).when(gitOpsStepHelper).getDeploymentRepoOutcome(ambiance);
+    ConnectorInfoDTO connectorInfoDTO = ConnectorInfoDTO.builder().build();
+    doReturn(connectorInfoDTO).when(cdStepHelper).getConnector("connectorRef", ambiance);
+    GitStoreDelegateConfig gitStoreDelegateConfig = GitStoreDelegateConfig.builder().build();
+    doReturn(gitStoreDelegateConfig)
+        .when(cdStepHelper)
+        .getGitStoreDelegateConfig(
+            githubStore, connectorInfoDTO, manifestOutcome, Collections.singletonList("path"), ambiance);
+    doReturn(EnvironmentType.PROD).when(stepHelper).getEnvironmentType(ambiance);
+    ArgumentCaptor<TaskData> taskDataArgumentCaptor = ArgumentCaptor.forClass(TaskData.class);
+    Mockito.mockStatic(StepUtils.class);
+    PowerMockito
+        .when(StepUtils.prepareCDTaskRequest(
+            eq(ambiance), taskDataArgumentCaptor.capture(), any(), any(), any(), any(), any(), any()))
+        .thenReturn(TaskRequest.newBuilder().build());
+
+    TaskRequest taskRequest = fetchLinkedAppsStep.obtainTaskAfterRbac(ambiance, stepElementParameters, null);
+    assertThat(taskRequest).isNotNull();
+    assertThat(taskDataArgumentCaptor.getValue().getTaskType()).isEqualTo(TaskType.GITOPS_FETCH_APP_TASK.name());
+  }
+
+  @Test
+  @Owner(developers = VAIBHAV_SI)
+  @Category(UnitTests.class)
+  public void shouldThrowErrorIfDeploymentRepoOutcomeNotFound() {
+    Ambiance ambiance = Ambiance.newBuilder().build();
+    FetchLinkedAppsStepParams stepParams = FetchLinkedAppsStepParams.infoBuilder().build();
+    StepElementParameters stepElementParameters = StepElementParameters.builder().spec(stepParams).build();
+    doReturn(logStreamingStepClient).when(logStreamingStepClientFactory).getLogStreamingStepClient(ambiance);
+    doThrow(new InvalidRequestException("Not found")).when(gitOpsStepHelper).getDeploymentRepoOutcome(ambiance);
+    Assertions.assertThatThrownBy(() -> fetchLinkedAppsStep.obtainTaskAfterRbac(ambiance, stepElementParameters, null))
+        .hasMessageContaining("Not found");
+  }
+
+  @Test
+  @Owner(developers = VAIBHAV_SI)
+  @Category(UnitTests.class)
+  public void shouldThrowErrorWhenTaskResponseStatusIsFail() throws Exception {
+    Ambiance ambiance = Ambiance.newBuilder().build();
+    FetchLinkedAppsStepParams stepParams = FetchLinkedAppsStepParams.infoBuilder().build();
+    StepElementParameters stepElementParameters = StepElementParameters.builder().spec(stepParams).build();
+    doReturn(logStreamingStepClient).when(logStreamingStepClientFactory).getLogStreamingStepClient(ambiance);
+    GitOpsFetchAppTaskResponse taskResponse =
+        GitOpsFetchAppTaskResponse.builder().taskStatus(TaskStatus.FAILURE).errorMessage("Task Failed").build();
+    ThrowingSupplier<GitOpsFetchAppTaskResponse> throwingSupplier = () -> taskResponse;
+
+    StepResponse stepResponse =
+        fetchLinkedAppsStep.handleTaskResultWithSecurityContext(ambiance, stepElementParameters, throwingSupplier);
+    assertThat(stepResponse.getStatus()).isEqualTo(Status.FAILED);
+    assertThat(stepResponse.getFailureInfo()).isNotNull();
+    assertThat(stepResponse.getFailureInfo().getFailureData(0)).isNotNull();
+    assertThat(stepResponse.getFailureInfo().getFailureData(0).getMessage()).isEqualTo("Task Failed");
+  }
+
+  @Test
+  @Owner(developers = VAIBHAV_SI)
+  @Category(UnitTests.class)
+  public void shouldThrowErrorWhenClusterOutcomeNotFound() {
+    Ambiance ambiance = Ambiance.newBuilder().build();
+    FetchLinkedAppsStepParams stepParams = FetchLinkedAppsStepParams.infoBuilder().build();
+    StepElementParameters stepElementParameters = StepElementParameters.builder().spec(stepParams).build();
+    doReturn(logStreamingStepClient).when(logStreamingStepClientFactory).getLogStreamingStepClient(ambiance);
+    GitOpsFetchAppTaskResponse taskResponse =
+        GitOpsFetchAppTaskResponse.builder().taskStatus(TaskStatus.SUCCESS).build();
+    ThrowingSupplier<GitOpsFetchAppTaskResponse> throwingSupplier = () -> taskResponse;
+    doReturn(null)
+        .when(executionSweepingOutputService)
+        .resolveOptional(ambiance, RefObjectUtils.getOutcomeRefObject(GitopsClustersStep.GITOPS_SWEEPING_OUTPUT));
+
+    Assertions
+        .assertThatThrownBy(()
+                                -> fetchLinkedAppsStep.handleTaskResultWithSecurityContext(
+                                    ambiance, stepElementParameters, throwingSupplier))
+        .hasMessageContaining("GitOps Clusters Outcome Not Found.");
+  }
+
+  @Test
+  @Owner(developers = VAIBHAV_SI)
+  @Category(UnitTests.class)
+  public void shouldThrowErrorWhenGitOpsServiceThrowsError() throws IOException {
+    Ambiance ambiance = Ambiance.newBuilder().build();
+    FetchLinkedAppsStepParams stepParams = FetchLinkedAppsStepParams.infoBuilder().build();
+    StepElementParameters stepElementParameters = StepElementParameters.builder().spec(stepParams).build();
+    doReturn(logStreamingStepClient).when(logStreamingStepClientFactory).getLogStreamingStepClient(ambiance);
+    GitOpsFetchAppTaskResponse taskResponse =
+        GitOpsFetchAppTaskResponse.builder().taskStatus(TaskStatus.SUCCESS).build();
+    ThrowingSupplier<GitOpsFetchAppTaskResponse> throwingSupplier = () -> taskResponse;
+    GitopsClustersOutcome gitopsClustersOutcome =
+        new GitopsClustersOutcome(Collections.singletonList(GitopsClustersOutcome.ClusterData.builder().build()));
+    doReturn(OptionalSweepingOutput.builder().found(true).output(gitopsClustersOutcome).build())
+        .when(executionSweepingOutputService)
+        .resolveOptional(any(), any());
+    Call call = mock(Call.class);
+    doReturn(call).when(gitopsResourceClient).listApps(any());
+    doThrow(new IOException("Not Available")).when(call).execute();
+
+    Assertions
+        .assertThatThrownBy(()
+                                -> fetchLinkedAppsStep.handleTaskResultWithSecurityContext(
+                                    ambiance, stepElementParameters, throwingSupplier))
+        .hasMessageContaining("Failed to retrieve Linked Apps from Gitops Service");
+  }
+
+  @Test
+  @Owner(developers = VAIBHAV_SI)
+  @Category(UnitTests.class)
+  public void shouldSetOutcome() throws Exception {
+    Ambiance ambiance = Ambiance.newBuilder().build();
+    FetchLinkedAppsStepParams stepParams = FetchLinkedAppsStepParams.infoBuilder().build();
+    StepElementParameters stepElementParameters = StepElementParameters.builder().spec(stepParams).build();
+    doReturn(logStreamingStepClient).when(logStreamingStepClientFactory).getLogStreamingStepClient(ambiance);
+    GitOpsFetchAppTaskResponse taskResponse =
+        GitOpsFetchAppTaskResponse.builder().taskStatus(TaskStatus.SUCCESS).build();
+    ThrowingSupplier<GitOpsFetchAppTaskResponse> throwingSupplier = () -> taskResponse;
+    GitopsClustersOutcome gitopsClustersOutcome =
+        new GitopsClustersOutcome(Collections.singletonList(GitopsClustersOutcome.ClusterData.builder().build()));
+    doReturn(OptionalSweepingOutput.builder().found(true).output(gitopsClustersOutcome).build())
+        .when(executionSweepingOutputService)
+        .resolveOptional(any(), any());
+    Call call = mock(Call.class);
+    PageResponse pageResponse = PageResponse.builder()
+                                    .content(Collections.singletonList(Application.builder().identifier("id").build()))
+                                    .build();
+    doReturn(call).when(gitopsResourceClient).listApps(any());
+    doReturn(Response.success(pageResponse)).when(call).execute();
+
+    StepResponse stepResponse =
+        fetchLinkedAppsStep.handleTaskResultWithSecurityContext(ambiance, stepElementParameters, throwingSupplier);
+    assertThat(stepResponse.getStatus()).isEqualTo(Status.SUCCEEDED);
+    StepResponse.StepOutcome stepOutcome = (StepResponse.StepOutcome) ((List) stepResponse.getStepOutcomes()).get(0);
+    assertThat(((GitOpsLinkedAppsOutcome) stepOutcome.getOutcome()).getApps()).hasSize(1);
+  }
+}
