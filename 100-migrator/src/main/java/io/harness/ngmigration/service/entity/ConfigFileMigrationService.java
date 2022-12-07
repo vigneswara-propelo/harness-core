bf5fefb43df8393f4b1dbb260aaac4af34726208
Deepak Puthraya <deepak.puthraya@harness.io>
2022-12-07 07:25:52 +0000

    fix: [CDS-47373]: Fix empty issue with config files (#41198)

--- 100-migrator/src/main/java/io/harness/ngmigration/service/entity/ConfigFileMigrationService.java
+++ 100-migrator/src/main/java/io/harness/ngmigration/service/entity/ConfigFileMigrationService.java
@@ -119,8 +119,14 @@ public class ConfigFileMigrationService extends NgMigrationService {
 
   private NGYamlFile getYamlFileForConfigFile(
       ConfigFile configFile, MigrationInputDTO inputDTO, Map<CgEntityId, CgEntityNode> entities) {
-    byte[] fileContent = configService.getFileContent(configFile.getAppId(), configFile);
-    if (isEmpty(fileContent)) {
+    byte[] fileContent;
+    try {
+      fileContent = configService.getFileContent(configFile.getAppId(), configFile);
+      if (isEmpty(fileContent)) {
+        return null;
+      }
+    } catch (Exception e) {
+      log.error(String.format("There was an error with reading contents of config file %s", configFile.getUuid()), e);
       return null;
     }
     CgEntityNode serviceNode = null;
@@ -142,7 +148,7 @@ public class ConfigFileMigrationService extends NgMigrationService {
       serviceName = service.getName();
     }
     if (environmentNode != null && environmentNode.getEntity() != null) {
-      Environment environment = (Environment) serviceNode.getEntity();
+      Environment environment = (Environment) environmentNode.getEntity();
       envName = environment.getName();
     }
     return getYamlFile(inputDTO, configFile, fileContent, envName, serviceName);
