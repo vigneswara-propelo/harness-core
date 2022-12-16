17cb8ffa8e102842ef0a61d0c6a5514bea6e0866
Anmol Maheshwari <anmol.maheshwari@harness.io>
2022-12-16 21:34:21 +0530

    [fix]: [CCM-10361]: AutoStopping Rule connector yaml is pointing to http end point instead of https (#41667)
    * [fix]: [CCM-10361]: AutoStopping Rule connector yaml is pointing to http end point instead of https

--- ce-nextgen/service/src/main/java/io/harness/ccm/remote/resources/CEYamlResource.java
+++ ce-nextgen/service/src/main/java/io/harness/ccm/remote/resources/CEYamlResource.java
@@ -118,7 +118,7 @@ public class CEYamlResource {
       }
 
       final String serverName = getServerName(request);
-      final String harnessHost = request.getScheme() + "://" + serverName;
+      final String harnessHost = request.getHeader("x-forwarded-proto") + "://" + serverName;
 
       try {
         final String yamlFileContent =
