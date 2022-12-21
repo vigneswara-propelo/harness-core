4fd0e7a7e610fa8025ae91e54987600a827fbd54
Abhinav Singh <abhinav.singh2@harness.io>
2022-12-21 22:11:39 +0530

    feat: [CDS-46287]: create FF for v2 env dynamic provisioning (#41883)
    * feat: [CDS-46287]: create FF for UI
    * feat: [CDS-46287]: FF renamed

--- 956-feature-flag-beans/src/main/java/io/harness/beans/FeatureName.java
+++ 956-feature-flag-beans/src/main/java/io/harness/beans/FeatureName.java
@@ -537,6 +537,8 @@ public enum FeatureName {
   ASG_NG("Supports Amazon ASG in NG", HarnessTeam.CDP),
   CDP_UPDATE_INSTANCE_DETAILS_WITH_IMAGE_SUFFIX("Update instance details if image suffix matches", HarnessTeam.CDP),
   SRM_INTERNAL_CHANGE_SOURCE_FF("Control Feature Flag Internal Change Source On UI", HarnessTeam.CV),
+  CD_NG_DYNAMIC_PROVISIONING_ENV_V2(
+      "Enable dynamic provisioning support in v2 environment. Epic: CDS-39606", HarnessTeam.CDC),
   CDS_ARTIFACTORY_REPOSITORY_URL_MANDATORY(
       "FF to make the Artifactory Repository Url as mandatory in case of docker repositoryFormat", HarnessTeam.CDC);
 
