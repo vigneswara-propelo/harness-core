326335553e4739de3ad585e4951b53065aea0efe
Anders Hokinson <1762048+ahokinson@users.noreply.github.com>
2023-01-03 14:19:22 -0500

    feat: [STO-4901]: Remove security feature flags (#38467)
    * [STO-4901]: Remove security feature flags
    
    * [STO-4901]: Remove security feature flags usages
    
    * [STO-4901]: Add feature restriction back
    
    * [STO-4901]: Fix builder order
    
    * feat: [STO-4901]: add sto_features.yml
    
    * feat: [STO-4901]: add SECURITY feature restriction
    
    * feat: [STO-4901]: remove sto_features.yml

--- 332-ci-manager/service/src/main/java/io/harness/ci/execution/plan/creator/CIPipelineServiceInfoProvider.java
+++ 332-ci-manager/service/src/main/java/io/harness/ci/execution/plan/creator/CIPipelineServiceInfoProvider.java
@@ -13,7 +13,6 @@ import static io.harness.pms.yaml.YAMLFieldNameConstants.STRATEGY;
 
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
-import io.harness.beans.FeatureName;
 import io.harness.beans.steps.StepSpecTypeConstants;
 import io.harness.ci.creator.variables.ActionStepVariableCreator;
 import io.harness.ci.creator.variables.ArtifactoryUploadStepVariableCreator;
@@ -244,7 +243,6 @@ public class CIPipelineServiceInfoProvider implements PipelineServiceInfoProvide
     StepInfo securityStepInfo = StepInfo.newBuilder()
                                     .setName("Security")
                                     .setType(StepSpecTypeConstants.SECURITY)
-                                    .setFeatureFlag(FeatureName.SECURITY.name())
                                     .setFeatureRestrictionName(FeatureRestrictionName.SECURITY.name())
                                     .setStepMetaData(StepMetaData.newBuilder().addFolderPaths("Security").build())
                                     .build();
