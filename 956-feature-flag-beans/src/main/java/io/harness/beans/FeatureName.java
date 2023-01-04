3992ec5c6b1f6593abab460603fb852e0392d6bd
satyam-harness <96036463+satyam-harness@users.noreply.github.com>
2023-01-04 11:55:34 +0530

    fix: [CDS-45912]: adding feature flags for multiple triggers (#41518)
    * fix: [CDS-45912
    ]: adding feature flags for multiple triggers
    
    * fix: team name changed
    
    * fix: FF name changed
    
    * fix: FF name changed
    
    Co-authored-by: vivekkarnatiharness <105721928+vivekkarnatiharness@users.noreply.github.com>

--- 956-feature-flag-beans/src/main/java/io/harness/beans/FeatureName.java
+++ 956-feature-flag-beans/src/main/java/io/harness/beans/FeatureName.java
@@ -387,6 +387,7 @@ public enum FeatureName {
       HarnessTeam.CI),
   CHANGE_INSTANCE_QUERY_OPERATOR_TO_NE("Change instance service query operator from $exists to $ne", HarnessTeam.SPG),
   NEXUS3_RAW_REPOSITORY("Enable support for Nexus3 raw repository format on CG", HarnessTeam.SPG),
+  CD_TRIGGER_V2("Enable support for nexus3, nexus2, azure, ami trigger", HarnessTeam.CDC),
   NG_ARTIFACT_SOURCES("Flag to support multi artifact sources for service V2", HarnessTeam.CDC),
   UPDATE_EMAILS_VIA_SCIM("Will enable updating emails in Harness via SCIM", HarnessTeam.PL),
   ELK_HEALTH_SOURCE("Will enable ELK health source in SRM", HarnessTeam.CV),
