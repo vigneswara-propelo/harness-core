562b057c7278f06060cdabb73160bafbd8e2d1cf
Soumyajit Das <soumyajit.das@harness.io>
2023-01-09 10:16:58 +0300

    fix: [CI-6647]: Fix npe for null build type (#42354)

--- 332-ci-manager/service/src/main/java/io/harness/ci/execution/execution/QueueExecutionUtils.java
+++ 332-ci-manager/service/src/main/java/io/harness/ci/execution/execution/QueueExecutionUtils.java
@@ -9,6 +9,7 @@ package io.harness.ci.execution;
 
 import static io.harness.pms.contracts.execution.Status.RUNNING;
 
+import io.harness.beans.serializer.RunTimeInputHandler;
 import io.harness.beans.stages.IntegrationStageStepParametersPMS;
 import io.harness.beans.yaml.extended.infrastrucutre.DockerInfraYaml;
 import io.harness.beans.yaml.extended.infrastrucutre.HostedVmInfraYaml;
@@ -18,6 +19,7 @@ import io.harness.beans.yaml.extended.infrastrucutre.OSType;
 import io.harness.beans.yaml.extended.infrastrucutre.VmInfraYaml;
 import io.harness.beans.yaml.extended.infrastrucutre.VmPoolYaml;
 import io.harness.ci.config.CIExecutionServiceConfig;
+import io.harness.exception.ngexception.CIStageExecutionException;
 import io.harness.plancreator.steps.common.StageElementParameters;
 import io.harness.pms.sdk.core.events.OrchestrationEvent;
 import io.harness.repositories.CIExecutionRepository;
@@ -27,6 +29,7 @@ import com.google.inject.Inject;
 public class QueueExecutionUtils {
   @Inject private CIExecutionRepository ciExecutionRepository;
   @Inject private CIExecutionServiceConfig ciExecutionServiceConfig;
+  @Inject private RunTimeInputHandler runTimeInputHandler;
 
   public void addActiveExecutionBuild(OrchestrationEvent event, String accountID, String runtimeID) {
     int count = 0;
@@ -52,23 +55,19 @@ public class QueueExecutionUtils {
     String os = "Linux";
     if (specConfig.getInfrastructure() instanceof VmInfraYaml) {
       VmInfraYaml infrastructure = (VmInfraYaml) specConfig.getInfrastructure();
-      os = ((VmPoolYaml) infrastructure.getSpec()).getSpec().getOs().fetchFinalValue().toString();
+      return RunTimeInputHandler.resolveOSType(((VmPoolYaml) infrastructure.getSpec()).getSpec().getOs());
     } else if (specConfig.getInfrastructure() instanceof DockerInfraYaml) {
       DockerInfraYaml infrastructure = (DockerInfraYaml) specConfig.getInfrastructure();
-      os = ((DockerInfraYaml.DockerInfraSpec) infrastructure.getSpec())
-               .getPlatform()
-               .getValue()
-               .getOs()
-               .fetchFinalValue()
-               .toString();
+      return RunTimeInputHandler.resolveOSType(infrastructure.getSpec().getPlatform().getValue().getOs());
     } else if (specConfig.getInfrastructure() instanceof K8sDirectInfraYaml) {
       K8sDirectInfraYaml infrastructure = (K8sDirectInfraYaml) specConfig.getInfrastructure();
-      os = (infrastructure.getSpec()).getOs().fetchFinalValue().toString();
+      return RunTimeInputHandler.resolveOSType(infrastructure.getSpec().getOs());
     } else if (specConfig.getInfrastructure() instanceof HostedVmInfraYaml) {
       HostedVmInfraYaml infrastructure = (HostedVmInfraYaml) specConfig.getInfrastructure();
-      os = (infrastructure.getSpec()).getPlatform().getValue().getOs().fetchFinalValue().toString();
+      return RunTimeInputHandler.resolveOSType(infrastructure.getSpec().getPlatform().getValue().getOs());
+    } else {
+      throw new CIStageExecutionException("unexptected type of infra received");
     }
-    return OSType.fromString(os);
   }
 
   public long getActiveExecutionsCount(String accountID) {
