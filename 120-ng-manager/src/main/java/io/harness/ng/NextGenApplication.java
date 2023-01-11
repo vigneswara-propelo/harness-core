529e7c04f7185c827ddef6685ced7fb2dfb50b8c
Yuvraj Singhal <yuvraj.singhal@harness.io>
2023-01-11 19:44:29 +0530

    [CDS-50507]: Unregistering the NgDeploymentFreezeActivation Handler (#42479)

--- 120-ng-manager/src/main/java/io/harness/ng/NextGenApplication.java
+++ 120-ng-manager/src/main/java/io/harness/ng/NextGenApplication.java
@@ -115,7 +115,6 @@ import io.harness.ng.core.exceptionmappers.OptimisticLockingFailureExceptionMapp
 import io.harness.ng.core.exceptionmappers.WingsExceptionMapperV2;
 import io.harness.ng.core.filter.ApiResponseFilter;
 import io.harness.ng.core.handler.NGVaultSecretManagerRenewalHandler;
-import io.harness.ng.core.handler.freezeHandlers.NgDeploymentFreezeActivationHandler;
 import io.harness.ng.core.migration.NGBeanMigrationProvider;
 import io.harness.ng.core.migration.ProjectMigrationProvider;
 import io.harness.ng.core.migration.UserGroupMigrationProvider;
@@ -606,7 +605,7 @@ public class NextGenApplication extends Application<NextGenConfiguration> {
     injector.getInstance(InstanceStatsIteratorHandler.class).registerIterators();
     injector.getInstance(GitFullSyncEntityIterator.class)
         .registerIterators(ngIteratorsConfig.getGitFullSyncEntityIteratorConfig().getThreadPoolSize());
-    injector.getInstance(NgDeploymentFreezeActivationHandler.class).registerIterators();
+    //  injector.getInstance(NgDeploymentFreezeActivationHandler.class).registerIterators();
     injector.getInstance(OAuthTokenRefresher.class)
         .registerIterators(ngIteratorsConfig.getOauthTokenRefreshIteratorConfig().getThreadPoolSize());
   }
