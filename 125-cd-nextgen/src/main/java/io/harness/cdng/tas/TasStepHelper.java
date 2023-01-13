2238585e75113e13d8c9cfbd377ada9e9ca38e06
Rishabh Gupta <rishabh.gupta@harness.io>
2023-01-13 17:09:55 +0530

    feat:[CDS-50481]: fixed folder fetch from local store (#42468)

--- 125-cd-nextgen/src/main/java/io/harness/cdng/tas/TasStepHelper.java
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/tas/TasStepHelper.java
@@ -32,6 +32,7 @@ import static io.harness.delegate.task.artifacts.ArtifactSourceType.JENKINS;
 import static io.harness.delegate.task.artifacts.ArtifactSourceType.NEXUS2_REGISTRY;
 import static io.harness.delegate.task.artifacts.ArtifactSourceType.NEXUS3_REGISTRY;
 import static io.harness.exception.WingsException.USER;
+import static io.harness.filestore.utils.FileStoreNodeUtils.mapFileNodes;
 import static io.harness.logging.CommandExecutionStatus.FAILURE;
 import static io.harness.logging.CommandExecutionStatus.SUCCESS;
 import static io.harness.logging.LogLevel.ERROR;
@@ -481,8 +482,19 @@ public class TasStepHelper {
                                      .filePath(scopedFilePath.replaceFirst("^account:", ""))
                                      .build());
             logCallback.saveExecutionLog(color(format("- %s", scopedFilePath), LogColor.White));
+          } else if (NGFileType.FOLDER.equals(fileStoreNodeDTO.getType())) {
+            Integer folderPathLength = fileStoreNodeDTO.getPath().length();
+            manifestContents.addAll(mapFileNodes(fileStoreNodeDTO,
+                fileNode
+                -> TasManifestFileContents.builder()
+                       .manifestType(manifestType)
+                       .fileContent(fileNode.getContent())
+                       .filePath(fileNode.getPath().substring(folderPathLength))
+                       .build()));
+            logCallback.saveExecutionLog(color(format("- %s", scopedFilePath), LogColor.White));
           } else {
-            throw new UnsupportedOperationException("Only File type is supported. Please enter the correct file path");
+            throw new UnsupportedOperationException(
+                "The File/Folder type is not supported. Please enter the correct file path");
           }
         }
       }
