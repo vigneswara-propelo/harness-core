4039acaad4a8766b8f59981bb6d9937a74cc42e1
Shubhanshu Verma <shubhanshu.verma@harness.io>
2023-01-13 12:46:29 +0530

    [fix]: [CCM-10573]: Fixed aws account name in cost details api response (#42575)

--- 450-ce-views/src/main/java/io/harness/ccm/views/helper/ViewParametersHelper.java
+++ 450-ce-views/src/main/java/io/harness/ccm/views/helper/ViewParametersHelper.java
@@ -8,6 +8,7 @@
 package io.harness.ccm.views.helper;
 
 import static io.harness.ccm.commons.constants.ViewFieldConstants.AWS_ACCOUNT_FIELD;
+import static io.harness.ccm.commons.constants.ViewFieldConstants.AWS_ACCOUNT_FIELD_ID;
 import static io.harness.ccm.views.entities.ViewFieldIdentifier.CLUSTER;
 import static io.harness.ccm.views.entities.ViewFieldIdentifier.COMMON;
 import static io.harness.ccm.views.entities.ViewFieldIdentifier.LABEL;
@@ -528,12 +529,14 @@ public class ViewParametersHelper {
         if (ceView.getViewVisualization() != null) {
           ViewVisualization viewVisualization = ceView.getViewVisualization();
           ViewField defaultGroupByField = viewVisualization.getGroupBy();
-          defaultFieldCheck = defaultGroupByField.getFieldName().equals(AWS_ACCOUNT_FIELD);
+          defaultFieldCheck = defaultGroupByField.getFieldName().equals(AWS_ACCOUNT_FIELD)
+              || defaultGroupByField.getFieldId().equals(AWS_ACCOUNT_FIELD_ID);
         }
       }
     }
     return (isGroupByEntityEmpty && defaultFieldCheck)
-        || viewsQueryHelper.isGroupByFieldPresent(groupBy, AWS_ACCOUNT_FIELD);
+        || viewsQueryHelper.isGroupByFieldPresent(groupBy, AWS_ACCOUNT_FIELD)
+        || viewsQueryHelper.isGroupByFieldIdPresent(groupBy, AWS_ACCOUNT_FIELD_ID);
   }
 
   // ----------------------------------------------------------------------------------------------------------------
