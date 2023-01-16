34dc89b9593166544145ecc6fbf963581ddf1bac
Anmol Maheshwari <anmol.maheshwari@harness.io>
2023-01-16 18:05:03 +0530

    [feat]: [CCM-10628]: Save perspective folder events to ENTITY_CRUD redis stream (#42532)
    * [feat]: [CCM-10628]: Save perspective events to ENTITY_CRUD redis stream
    
    * [feat]: [CCM-10628]: Save perspective folder events to ENTITY_CRUD redis stream

--- ce-nextgen/service/src/main/java/io/harness/ccm/EventsFrameworkModule.java
+++ ce-nextgen/service/src/main/java/io/harness/ccm/EventsFrameworkModule.java
@@ -14,14 +14,18 @@ import io.harness.annotations.dev.OwnedBy;
 import io.harness.eventsframework.EventsFrameworkConfiguration;
 import io.harness.eventsframework.EventsFrameworkConstants;
 import io.harness.eventsframework.api.Consumer;
+import io.harness.eventsframework.api.Producer;
 import io.harness.eventsframework.impl.noop.NoOpConsumer;
+import io.harness.eventsframework.impl.noop.NoOpProducer;
 import io.harness.eventsframework.impl.redis.RedisConsumer;
+import io.harness.eventsframework.impl.redis.RedisProducer;
 import io.harness.redis.RedisConfig;
 import io.harness.redis.RedissonClientFactory;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.name.Names;
 import lombok.AllArgsConstructor;
+import org.redisson.api.RedissonClient;
 
 @AllArgsConstructor
 @OwnedBy(CE)
@@ -32,11 +36,20 @@ public class EventsFrameworkModule extends AbstractModule {
   protected void configure() {
     RedisConfig redisConfig = this.eventsFrameworkConfiguration.getRedisConfig();
     if (redisConfig.getRedisUrl().equals("dummyRedisUrl")) {
+      bind(Producer.class)
+          .annotatedWith(Names.named(EventsFrameworkConstants.ENTITY_CRUD))
+          .toInstance(NoOpProducer.of(EventsFrameworkConstants.DUMMY_TOPIC_NAME));
       bind(Consumer.class)
           .annotatedWith(Names.named(EventsFrameworkConstants.ENTITY_CRUD))
           .toInstance(
               NoOpConsumer.of(EventsFrameworkConstants.DUMMY_TOPIC_NAME, EventsFrameworkConstants.DUMMY_GROUP_NAME));
     } else {
+      RedissonClient redissonClient = RedissonClientFactory.getClient(redisConfig);
+      bind(Producer.class)
+          .annotatedWith(Names.named(EventsFrameworkConstants.ENTITY_CRUD))
+          .toInstance(RedisProducer.of(EventsFrameworkConstants.ENTITY_CRUD, redissonClient,
+              EventsFrameworkConstants.ENTITY_CRUD_MAX_TOPIC_SIZE, CE_NEXT_GEN.getServiceId(),
+              redisConfig.getEnvNamespace()));
       bind(Consumer.class)
           .annotatedWith(Names.named(EventsFrameworkConstants.ENTITY_CRUD))
           .toInstance(RedisConsumer.of(EventsFrameworkConstants.ENTITY_CRUD, CE_NEXT_GEN.getServiceId(),
--- ce-nextgen/service/src/main/java/io/harness/ccm/audittrails/eventhandler/PerspectiveFolderEventHandler.java
+++ ce-nextgen/service/src/main/java/io/harness/ccm/audittrails/eventhandler/PerspectiveFolderEventHandler.java
@@ -24,24 +24,38 @@ import io.harness.ccm.audittrails.events.PerspectiveFolderCreateEvent;
 import io.harness.ccm.audittrails.events.PerspectiveFolderDeleteEvent;
 import io.harness.ccm.audittrails.events.PerspectiveFolderUpdateEvent;
 import io.harness.ccm.audittrails.yamlDTOs.PerspectiveFolderDTO;
+import io.harness.ccm.views.entities.CEViewFolder;
 import io.harness.context.GlobalContext;
+import io.harness.eventsframework.EventsFrameworkConstants;
+import io.harness.eventsframework.EventsFrameworkMetadataConstants;
+import io.harness.eventsframework.api.EventsFrameworkDownException;
+import io.harness.eventsframework.api.Producer;
+import io.harness.eventsframework.entity_crud.EntityChangeDTO;
+import io.harness.eventsframework.producer.Message;
 import io.harness.exception.InvalidArgumentsException;
 import io.harness.outbox.OutboxEvent;
 import io.harness.outbox.api.OutboxEventHandler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableMap;
 import com.google.inject.Inject;
+import com.google.inject.name.Named;
+import com.google.protobuf.ByteString;
+import com.google.protobuf.StringValue;
 import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class PerspectiveFolderEventHandler implements OutboxEventHandler {
   private final ObjectMapper objectMapper;
+  private final Producer eventProducer;
   private final AuditClientService auditClientService;
 
   @Inject
-  public PerspectiveFolderEventHandler(AuditClientService auditClientService) {
+  public PerspectiveFolderEventHandler(
+      @Named(EventsFrameworkConstants.ENTITY_CRUD) Producer eventProducer, AuditClientService auditClientService) {
     this.objectMapper = NG_DEFAULT_OBJECT_MAPPER;
+    this.eventProducer = eventProducer;
     this.auditClientService = auditClientService;
   }
 
@@ -68,6 +82,8 @@ public class PerspectiveFolderEventHandler implements OutboxEventHandler {
     GlobalContext globalContext = outboxEvent.getGlobalContext();
     PerspectiveFolderCreateEvent perspectiveFolderCreateEvent =
         objectMapper.readValue(outboxEvent.getEventData(), PerspectiveFolderCreateEvent.class);
+    boolean publishedToRedis = publishEvent(
+        perspectiveFolderCreateEvent.getPerspectiveFolderDTO(), EventsFrameworkMetadataConstants.CREATE_ACTION);
     AuditEntry auditEntry =
         AuditEntry.builder()
             .action(Action.CREATE)
@@ -80,13 +96,15 @@ public class PerspectiveFolderEventHandler implements OutboxEventHandler {
             .resourceScope(ResourceScopeDTO.fromResourceScope(outboxEvent.getResourceScope()))
             .insertId(outboxEvent.getId())
             .build();
-    return auditClientService.publishAudit(auditEntry, globalContext);
+    return publishedToRedis && auditClientService.publishAudit(auditEntry, globalContext);
   }
 
   private boolean handlePerspectiveFolderUpdateEvent(OutboxEvent outboxEvent) throws IOException {
     GlobalContext globalContext = outboxEvent.getGlobalContext();
     PerspectiveFolderUpdateEvent perspectiveFolderUpdateEvent =
         objectMapper.readValue(outboxEvent.getEventData(), PerspectiveFolderUpdateEvent.class);
+    boolean publishedToRedis = publishEvent(
+        perspectiveFolderUpdateEvent.getPerspectiveFolderDTO(), EventsFrameworkMetadataConstants.UPDATE_ACTION);
     AuditEntry auditEntry =
         AuditEntry.builder()
             .action(Action.UPDATE)
@@ -103,13 +121,15 @@ public class PerspectiveFolderEventHandler implements OutboxEventHandler {
             .insertId(outboxEvent.getId())
             .build();
 
-    return auditClientService.publishAudit(auditEntry, globalContext);
+    return publishedToRedis && auditClientService.publishAudit(auditEntry, globalContext);
   }
 
   private boolean handlePerspectiveFolderDeleteEvent(OutboxEvent outboxEvent) throws IOException {
     GlobalContext globalContext = outboxEvent.getGlobalContext();
     PerspectiveFolderDeleteEvent perspectiveFolderDeleteEvent =
         objectMapper.readValue(outboxEvent.getEventData(), PerspectiveFolderDeleteEvent.class);
+    boolean publishedToRedis = publishEvent(
+        perspectiveFolderDeleteEvent.getPerspectiveFolderDTO(), EventsFrameworkMetadataConstants.DELETE_ACTION);
     AuditEntry auditEntry =
         AuditEntry.builder()
             .action(Action.DELETE)
@@ -123,6 +143,31 @@ public class PerspectiveFolderEventHandler implements OutboxEventHandler {
             .insertId(outboxEvent.getId())
             .build();
 
-    return auditClientService.publishAudit(auditEntry, globalContext);
+    return publishedToRedis && auditClientService.publishAudit(auditEntry, globalContext);
+  }
+
+  private boolean publishEvent(CEViewFolder perspectiveFolder, String action) {
+    try {
+      String eventId = eventProducer.send(
+          Message.newBuilder()
+              .putAllMetadata(ImmutableMap.of("accountId", perspectiveFolder.getAccountId(),
+                  EventsFrameworkMetadataConstants.ENTITY_TYPE, EventsFrameworkMetadataConstants.CCM_FOLDER,
+                  EventsFrameworkMetadataConstants.ACTION, action))
+              .setData(getPerspectiveFolderPayload(perspectiveFolder))
+              .build());
+      log.info("Produced event id:[{}] for perspective folder:[{}], action:[{}]", eventId, perspectiveFolder, action);
+      return true;
+    } catch (EventsFrameworkDownException e) {
+      log.error("Failed to send event to events framework for perspective folder identifier {}",
+          perspectiveFolder.getUuid(), e);
+      return false;
+    }
+  }
+
+  private ByteString getPerspectiveFolderPayload(CEViewFolder perspectiveFolder) {
+    EntityChangeDTO.Builder builder = EntityChangeDTO.newBuilder()
+                                          .setAccountIdentifier(StringValue.of(perspectiveFolder.getAccountId()))
+                                          .setIdentifier(StringValue.of(perspectiveFolder.getUuid()));
+    return builder.build().toByteString();
   }
 }
