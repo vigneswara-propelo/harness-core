1c575637c7606dc0561a795cdee1a5eaa7218c83
Rishabh Gupta <rishabh.gupta@harness.io>
2023-01-16 14:30:56 +0530

    feat:[CDS-46753]: added junit to compare num of fields (#42629)

--- 954-connector-beans/src/test/java/io/harness/delegate/beans/connector/ConnectorOutcomeTest.java
+++ 954-connector-beans/src/test/java/io/harness/delegate/beans/connector/ConnectorOutcomeTest.java
@@ -12,14 +12,88 @@ import static io.harness.data.structure.EmptyPredicate.isEmpty;
 import static io.harness.rule.OwnerRule.RISHABH;
 
 import static java.util.Objects.isNull;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.category.element.UnitTests;
+import io.harness.connector.ConnectorInfoDTO;
 import io.harness.connector.ConnectorInfoOutcomeDTO;
 import io.harness.data.structure.EmptyPredicate;
+import io.harness.delegate.beans.connector.awsconnector.AwsConnectorDTO;
+import io.harness.delegate.beans.connector.awsconnector.AwsCredentialDTO;
+import io.harness.delegate.beans.connector.awsconnector.outcome.AwsConnectorOutcomeDTO;
+import io.harness.delegate.beans.connector.awsconnector.outcome.AwsCredentialOutcomeDTO;
+import io.harness.delegate.beans.connector.azureconnector.AzureAuthCredentialDTO;
+import io.harness.delegate.beans.connector.azureconnector.AzureAuthDTO;
+import io.harness.delegate.beans.connector.azureconnector.AzureClientKeyCertDTO;
+import io.harness.delegate.beans.connector.azureconnector.AzureClientSecretKeyDTO;
+import io.harness.delegate.beans.connector.azureconnector.AzureConnectorDTO;
+import io.harness.delegate.beans.connector.azureconnector.AzureCredentialDTO;
+import io.harness.delegate.beans.connector.azureconnector.AzureCredentialSpecDTO;
+import io.harness.delegate.beans.connector.azureconnector.AzureInheritFromDelegateDetailsDTO;
+import io.harness.delegate.beans.connector.azureconnector.AzureMSIAuthDTO;
+import io.harness.delegate.beans.connector.azureconnector.AzureMSIAuthSADTO;
+import io.harness.delegate.beans.connector.azureconnector.AzureMSIAuthUADTO;
+import io.harness.delegate.beans.connector.azureconnector.AzureManualDetailsDTO;
+import io.harness.delegate.beans.connector.azureconnector.AzureSystemAssignedMSIAuthDTO;
+import io.harness.delegate.beans.connector.azureconnector.AzureUserAssignedMSIAuthDTO;
+import io.harness.delegate.beans.connector.azureconnector.outcome.AzureAuthCredentialOutcomeDTO;
+import io.harness.delegate.beans.connector.azureconnector.outcome.AzureAuthOutcomeDTO;
+import io.harness.delegate.beans.connector.azureconnector.outcome.AzureClientKeyCertOutcomeDTO;
+import io.harness.delegate.beans.connector.azureconnector.outcome.AzureClientSecretKeyOutcomeDTO;
+import io.harness.delegate.beans.connector.azureconnector.outcome.AzureConnectorOutcomeDTO;
+import io.harness.delegate.beans.connector.azureconnector.outcome.AzureCredentialOutcomeDTO;
+import io.harness.delegate.beans.connector.azureconnector.outcome.AzureCredentialSpecOutcomeDTO;
+import io.harness.delegate.beans.connector.azureconnector.outcome.AzureInheritFromDelegateDetailsOutcomeDTO;
+import io.harness.delegate.beans.connector.azureconnector.outcome.AzureMSIAuthOutcomeDTO;
 import io.harness.delegate.beans.connector.azureconnector.outcome.AzureMSIAuthSAOutcomeDTO;
 import io.harness.delegate.beans.connector.azureconnector.outcome.AzureMSIAuthUAOutcomeDTO;
+import io.harness.delegate.beans.connector.azureconnector.outcome.AzureManualDetailsOutcomeDTO;
+import io.harness.delegate.beans.connector.azureconnector.outcome.AzureSystemAssignedMSIAuthOutcomeDTO;
+import io.harness.delegate.beans.connector.azureconnector.outcome.AzureUserAssignedMSIAuthOutcomeDTO;
+import io.harness.delegate.beans.connector.gcpconnector.GcpConnectorCredentialDTO;
+import io.harness.delegate.beans.connector.gcpconnector.GcpConnectorDTO;
 import io.harness.delegate.beans.connector.gcpconnector.GcpDelegateDetailsDTO;
+import io.harness.delegate.beans.connector.gcpconnector.outcome.GcpConnectorCredentialOutcomeDTO;
+import io.harness.delegate.beans.connector.gcpconnector.outcome.GcpConnectorOutcomeDTO;
+import io.harness.delegate.beans.connector.k8Connector.KubernetesAuthDTO;
+import io.harness.delegate.beans.connector.k8Connector.KubernetesClusterConfigDTO;
+import io.harness.delegate.beans.connector.k8Connector.KubernetesClusterDetailsDTO;
+import io.harness.delegate.beans.connector.k8Connector.KubernetesCredentialDTO;
+import io.harness.delegate.beans.connector.k8Connector.KubernetesDelegateDetailsDTO;
+import io.harness.delegate.beans.connector.k8Connector.outcome.KubernetesAuthOutcomeDTO;
+import io.harness.delegate.beans.connector.k8Connector.outcome.KubernetesClusterConfigOutcomeDTO;
+import io.harness.delegate.beans.connector.k8Connector.outcome.KubernetesClusterDetailsOutcomeDTO;
+import io.harness.delegate.beans.connector.k8Connector.outcome.KubernetesCredentialOutcomeDTO;
+import io.harness.delegate.beans.connector.k8Connector.outcome.KubernetesDelegateDetailsOutcomeDTO;
+import io.harness.delegate.beans.connector.pdcconnector.HostDTO;
+import io.harness.delegate.beans.connector.pdcconnector.PhysicalDataCenterConnectorDTO;
+import io.harness.delegate.beans.connector.pdcconnector.outcome.HostOutcomeDTO;
+import io.harness.delegate.beans.connector.pdcconnector.outcome.PhysicalDataCenterConnectorOutcomeDTO;
+import io.harness.delegate.beans.connector.scm.genericgitconnector.GitAuthenticationDTO;
+import io.harness.delegate.beans.connector.scm.genericgitconnector.GitConfigDTO;
+import io.harness.delegate.beans.connector.scm.genericgitconnector.GitHTTPAuthenticationDTO;
+import io.harness.delegate.beans.connector.scm.genericgitconnector.GitSSHAuthenticationDTO;
+import io.harness.delegate.beans.connector.scm.genericgitconnector.outcome.GitAuthenticationOutcomeDTO;
+import io.harness.delegate.beans.connector.scm.genericgitconnector.outcome.GitConfigOutcomeDTO;
+import io.harness.delegate.beans.connector.scm.genericgitconnector.outcome.GitHTTPAuthenticationOutcomeDTO;
+import io.harness.delegate.beans.connector.scm.genericgitconnector.outcome.GitSSHAuthenticationOutcomeDTO;
+import io.harness.delegate.beans.connector.scm.github.GithubAuthenticationDTO;
+import io.harness.delegate.beans.connector.scm.github.GithubHttpCredentialsDTO;
+import io.harness.delegate.beans.connector.scm.github.GithubSshCredentialsDTO;
+import io.harness.delegate.beans.connector.scm.github.outcome.GithubAuthenticationOutcomeDTO;
+import io.harness.delegate.beans.connector.scm.github.outcome.GithubHttpCredentialsOutcomeDTO;
+import io.harness.delegate.beans.connector.scm.github.outcome.GithubSshCredentialsOutcomeDTO;
+import io.harness.delegate.beans.connector.scm.gitlab.GitlabAuthenticationDTO;
+import io.harness.delegate.beans.connector.scm.gitlab.GitlabHttpCredentialsDTO;
+import io.harness.delegate.beans.connector.scm.gitlab.GitlabSshCredentialsDTO;
+import io.harness.delegate.beans.connector.scm.gitlab.outcome.GitlabAuthenticationOutcomeDTO;
+import io.harness.delegate.beans.connector.scm.gitlab.outcome.GitlabHttpCredentialsOutcomeDTO;
+import io.harness.delegate.beans.connector.scm.gitlab.outcome.GitlabSshCredentialsOutcomeDTO;
+import io.harness.delegate.beans.connector.tasconnector.TasConnectorDTO;
+import io.harness.delegate.beans.connector.tasconnector.TasCredentialDTO;
+import io.harness.delegate.beans.connector.tasconnector.outcome.TasConnectorOutcomeDTO;
+import io.harness.delegate.beans.connector.tasconnector.outcome.TasCredentialOutcomeDTO;
 import io.harness.exception.InvalidRequestException;
 import io.harness.exception.InvalidYamlException;
 import io.harness.reflection.ReflectionUtils;
@@ -33,6 +107,7 @@ import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -50,6 +125,55 @@ public class ConnectorOutcomeTest {
         AzureMSIAuthUAOutcomeDTO.class, GcpDelegateDetailsDTO.class);
     for (Class<?> classToTest : classesToTest) {
       getExpressionsInObject(classToTest, "");
+    }
+  }
+
+  @Test
+  @Owner(developers = RISHABH)
+  @Category(UnitTests.class)
+  public void testCompareNumberOfFields() {
+    Map<Class<?>, Class<?>> classToOutcomeClass = new HashMap<>();
+    classToOutcomeClass.put(ConnectorConfigDTO.class, ConnectorConfigOutcomeDTO.class);
+    classToOutcomeClass.put(ConnectorInfoDTO.class, ConnectorInfoOutcomeDTO.class);
+    classToOutcomeClass.put(AwsConnectorDTO.class, AwsConnectorOutcomeDTO.class);
+    classToOutcomeClass.put(AwsCredentialDTO.class, AwsCredentialOutcomeDTO.class);
+    classToOutcomeClass.put(AzureAuthCredentialDTO.class, AzureAuthCredentialOutcomeDTO.class);
+    classToOutcomeClass.put(AzureAuthDTO.class, AzureAuthOutcomeDTO.class);
+    classToOutcomeClass.put(AzureClientKeyCertDTO.class, AzureClientKeyCertOutcomeDTO.class);
+    classToOutcomeClass.put(AzureClientSecretKeyDTO.class, AzureClientSecretKeyOutcomeDTO.class);
+    classToOutcomeClass.put(AzureConnectorDTO.class, AzureConnectorOutcomeDTO.class);
+    classToOutcomeClass.put(AzureCredentialDTO.class, AzureCredentialOutcomeDTO.class);
+    classToOutcomeClass.put(AzureCredentialSpecDTO.class, AzureCredentialSpecOutcomeDTO.class);
+    classToOutcomeClass.put(AzureInheritFromDelegateDetailsDTO.class, AzureInheritFromDelegateDetailsOutcomeDTO.class);
+    classToOutcomeClass.put(AzureMSIAuthDTO.class, AzureMSIAuthOutcomeDTO.class);
+    classToOutcomeClass.put(AzureMSIAuthUADTO.class, AzureMSIAuthUAOutcomeDTO.class);
+    classToOutcomeClass.put(AzureMSIAuthSADTO.class, AzureMSIAuthSAOutcomeDTO.class);
+    classToOutcomeClass.put(AzureManualDetailsDTO.class, AzureManualDetailsOutcomeDTO.class);
+    classToOutcomeClass.put(AzureSystemAssignedMSIAuthDTO.class, AzureSystemAssignedMSIAuthOutcomeDTO.class);
+    classToOutcomeClass.put(AzureUserAssignedMSIAuthDTO.class, AzureUserAssignedMSIAuthOutcomeDTO.class);
+    classToOutcomeClass.put(GcpConnectorCredentialDTO.class, GcpConnectorCredentialOutcomeDTO.class);
+    classToOutcomeClass.put(GcpConnectorDTO.class, GcpConnectorOutcomeDTO.class);
+    classToOutcomeClass.put(KubernetesAuthDTO.class, KubernetesAuthOutcomeDTO.class);
+    classToOutcomeClass.put(KubernetesClusterConfigDTO.class, KubernetesClusterConfigOutcomeDTO.class);
+    classToOutcomeClass.put(KubernetesClusterDetailsDTO.class, KubernetesClusterDetailsOutcomeDTO.class);
+    classToOutcomeClass.put(KubernetesCredentialDTO.class, KubernetesCredentialOutcomeDTO.class);
+    classToOutcomeClass.put(KubernetesDelegateDetailsDTO.class, KubernetesDelegateDetailsOutcomeDTO.class);
+    classToOutcomeClass.put(HostDTO.class, HostOutcomeDTO.class);
+    classToOutcomeClass.put(PhysicalDataCenterConnectorDTO.class, PhysicalDataCenterConnectorOutcomeDTO.class);
+    classToOutcomeClass.put(GitAuthenticationDTO.class, GitAuthenticationOutcomeDTO.class);
+    classToOutcomeClass.put(GitConfigDTO.class, GitConfigOutcomeDTO.class);
+    classToOutcomeClass.put(GitHTTPAuthenticationDTO.class, GitHTTPAuthenticationOutcomeDTO.class);
+    classToOutcomeClass.put(GitSSHAuthenticationDTO.class, GitSSHAuthenticationOutcomeDTO.class);
+    classToOutcomeClass.put(GithubAuthenticationDTO.class, GithubAuthenticationOutcomeDTO.class);
+    classToOutcomeClass.put(GithubHttpCredentialsDTO.class, GithubHttpCredentialsOutcomeDTO.class);
+    classToOutcomeClass.put(GithubSshCredentialsDTO.class, GithubSshCredentialsOutcomeDTO.class);
+    classToOutcomeClass.put(GitlabAuthenticationDTO.class, GitlabAuthenticationOutcomeDTO.class);
+    classToOutcomeClass.put(GitlabHttpCredentialsDTO.class, GitlabHttpCredentialsOutcomeDTO.class);
+    classToOutcomeClass.put(GitlabSshCredentialsDTO.class, GitlabSshCredentialsOutcomeDTO.class);
+    classToOutcomeClass.put(TasConnectorDTO.class, TasConnectorOutcomeDTO.class);
+    classToOutcomeClass.put(TasCredentialDTO.class, TasCredentialOutcomeDTO.class);
+    for (Map.Entry<Class<?>, Class<?>> entry : classToOutcomeClass.entrySet()) {
+      compareNumOfFields(entry.getKey(), entry.getValue());
     }
   }
 
@@ -72,6 +196,16 @@ public class ConnectorOutcomeTest {
     }
     return annotation;
   }
+
+  public void compareNumOfFields(Class<?> mainClass, Class<?> outcomeClass) {
+    List<Field> mainClassFields = ReflectionUtils.getAllDeclaredAndInheritedFields(mainClass);
+    List<Field> outcomeClassFields = ReflectionUtils.getAllDeclaredAndInheritedFields(outcomeClass);
+    assertThat(mainClassFields)
+        .hasSize(outcomeClassFields.size())
+        .withFailMessage(
+            "Number of fields doesn't match for class: %s and %s", mainClass.getName(), outcomeClass.getName());
+  }
+
   // Traverses the object checking values inside the object and return their expressions
   public List<String> getExpressionsInObject(Class<?> c, String prefix) {
     List<Field> fields = ReflectionUtils.getAllDeclaredAndInheritedFields(c);
