d6733a8b9d8e91827e3f46be65e413dfa2222b67
Abhinav Mittal <abhinav.mittal@harness.io>
2023-01-20 17:28:24 +0530

    fix: [CDS-51579]: Add limit to number of calls made in a single instance stats collector job (#42873)

--- 400-rest/src/main/java/software/wings/service/impl/instance/stats/collector/StatsCollectorImpl.java
+++ 400-rest/src/main/java/software/wings/service/impl/instance/stats/collector/StatsCollectorImpl.java
@@ -38,6 +38,8 @@ public class StatsCollectorImpl implements StatsCollector {
   private static final int SYNC_INTERVAL_MINUTES = 10;
   private static final long SYNC_INTERVAL = TimeUnit.MINUTES.toMinutes(SYNC_INTERVAL_MINUTES);
 
+  private static final int MAX_CALLS_PER_ACCOUNT = 60 / SYNC_INTERVAL_MINUTES * 24;
+
   private InstanceStatService statService;
   private ServerlessInstanceStatService serverlessInstanceStatService;
   private DashboardStatisticsService dashboardStatisticsService;
@@ -65,13 +67,20 @@ public class StatsCollectorImpl implements StatsCollector {
 
     SnapshotTimeProvider snapshotTimeProvider = new SnapshotTimeProvider(lastSnapshot, SYNC_INTERVAL);
     boolean ranAtLeastOnce = false;
+    int instanceStatsCallsMade = 0;
     while (snapshotTimeProvider.hasNext()) {
+      if (instanceStatsCallsMade >= MAX_CALLS_PER_ACCOUNT) {
+        log.warn("Tried publishing {} stats for accountId {}. Pending backlog will be published in the next iteration",
+            MAX_CALLS_PER_ACCOUNT, accountId);
+        break;
+      }
       Instant nextTs = snapshotTimeProvider.next();
       if (nextTs == null) {
         throw new IllegalStateException("nextTs is null even though hasNext() returned true. Shouldn't be possible");
       }
       boolean success = createStats(accountId, nextTs);
       ranAtLeastOnce = ranAtLeastOnce || success;
+      instanceStatsCallsMade++;
     }
 
     return ranAtLeastOnce;
@@ -86,13 +95,21 @@ public class StatsCollectorImpl implements StatsCollector {
 
     SnapshotTimeProvider snapshotTimeProvider = new SnapshotTimeProvider(lastSnapshot, SYNC_INTERVAL);
     boolean ranAtLeastOnce = false;
+    int instanceStatsCallsMade = 0;
     while (snapshotTimeProvider.hasNext()) {
+      if (instanceStatsCallsMade >= MAX_CALLS_PER_ACCOUNT) {
+        log.warn(
+            "Tried publishing {} serverless stats for accountId {}. Pending backlog will be published in the next iteration",
+            MAX_CALLS_PER_ACCOUNT, accountId);
+        break;
+      }
       Instant nextTs = snapshotTimeProvider.next();
       if (nextTs == null) {
         throw new IllegalStateException("nextTs is null even though hasNext() returned true. Shouldn't be possible");
       }
       boolean success = createServerlessStats(accountId, nextTs);
       ranAtLeastOnce = ranAtLeastOnce || success;
+      instanceStatsCallsMade++;
     }
 
     return ranAtLeastOnce;
