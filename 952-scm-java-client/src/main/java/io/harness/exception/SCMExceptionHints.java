0a72692aae43fbd4cdd18dea7ca34bb3b95bdbb5
Soumyajit Das <soumyajit.das@harness.io>
2023-01-23 17:34:20 +0530

    fix: [CI-6458]: Refine error message (#42920)

--- 952-scm-java-client/src/main/java/io/harness/exception/SCMExceptionHints.java
+++ 952-scm-java-client/src/main/java/io/harness/exception/SCMExceptionHints.java
@@ -11,5 +11,5 @@ import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class SCMExceptionHints {
-  public static final String INVALID_CREDENTIALS = "Please check your credentials.";
+  public static final String INVALID_CREDENTIALS = "Invalid Repository or wrong credentials.";
 }
