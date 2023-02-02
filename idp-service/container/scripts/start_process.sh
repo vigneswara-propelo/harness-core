5a98415e1f5370d7fba3e9d821d396accfa874db
devesh-harness <devesh.mishra@harness.io>
2023-02-02 20:06:30 +0530

    feat: [IDP-135]: Feature build pipeline changes (#43344)
    * IDP-0: Initial build
    
    * IDP-0: Initial build
    
    * IDP-0: Initial build
    
    * IDP-0: Initial build
    
    * IDP-0: Initial build

--- idp-service/container/scripts/start_process.sh
+++ idp-service/container/scripts/start_process.sh
@@ -4,6 +4,7 @@
 # that can be found in the licenses directory at the root of this repository, also available at
 # https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
 
+set -x
 if [[ -v "{hostname}" ]]; then
    export HOSTNAME=$(hostname)
 fi
@@ -19,22 +20,12 @@ fi
 echo "Using memory " "$MEMORY"
 
 if [[ -z "$CAPSULE_JAR" ]]; then
-   export CAPSULE_JAR=/opt/harness/ng-manager-capsule.jar
+   export CAPSULE_JAR=/opt/harness/idp-service-capsule.jar
 fi
 
-if [[ "${ENABLE_SERIALGC}" == "true" ]]; then
-    export GC_PARAMS=" -XX:+UseSerialGC -Dfile.encoding=UTF-8"
-else
-    export GC_PARAMS=" -XX:+UseG1GC -XX:InitiatingHeapOccupancyPercent=40 -XX:MaxGCPauseMillis=1000 -Dfile.encoding=UTF-8"
-fi
+export GC_PARAMS=" -XX:+UseG1GC -XX:InitiatingHeapOccupancyPercent=40 -XX:MaxGCPauseMillis=1000 -Dfile.encoding=UTF-8"
 
-export JAVA_OPTS="-Xmx${MEMORY} -XX:+HeapDumpOnOutOfMemoryError -Xloggc:mygclogfilename.gc $GC_PARAMS $JAVA_ADVANCED_FLAGS"
-
-if [[ "${ENABLE_REMOTE_DEBUG}" == "true" ]]; then
-  export REMOTE_DEBUG="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=localhost:5005"
-  export JAVA_OPTS="$REMOTE_DEBUG $JAVA_OPTS"
-  echo "Enabled remote debug"
-fi
+export JAVA_OPTS="-Xmx${MEMORY} -XX:+HeapDumpOnOutOfMemoryError -Xloggc:mygclogfilename.gc $GC_PARAMS -XX:+UseStringDeduplication -XX:StringDeduplicationAgeThreshold=2 -XX:NativeMemoryTracking=summary -XX:-UseCompressedOops -XX:+AlwaysPreTouch"
 
 if [[ "${ENABLE_APPDYNAMICS}" == "true" ]]; then
     mkdir /opt/harness/AppServerAgent-1.8-21.11.2.33305 && unzip AppServerAgent-1.8-21.11.2.33305.zip -d /opt/harness/AppServerAgent-1.8-21.11.2.33305
@@ -44,14 +35,21 @@ if [[ "${ENABLE_APPDYNAMICS}" == "true" ]]; then
     echo "Using Appdynamics java agent"
 fi
 
-if [[ "${ENABLE_OVEROPS}" == "true" ]] ; then
-    echo "OverOps is enabled"
-    JAVA_OPTS=$JAVA_OPTS" -Xshare:off -XX:-UseTypeSpeculation -XX:ReservedCodeCacheSize=512m -agentpath:/opt/harness/harness/lib/libETAgent.so"
-    echo "Using Overops Java Agent"
+if [[ "${ENABLE_OPENTELEMETRY}" == "true" ]] ; then
+    echo "OpenTelemetry is enabled"
+    JAVA_OPTS=$JAVA_OPTS" -javaagent:/opt/harness/opentelemetry-javaagent.jar -Dotel.service.name=${OTEL_SERVICE_NAME:-idp-service}"
+
+    if [ -n "$OTEL_EXPORTER_OTLP_ENDPOINT" ]; then
+        JAVA_OPTS=$JAVA_OPTS" -Dotel.exporter.otlp.endpoint=$OTEL_EXPORTER_OTLP_ENDPOINT "
+    else
+        echo "OpenTelemetry export is disabled"
+        JAVA_OPTS=$JAVA_OPTS" -Dotel.traces.exporter=none -Dotel.metrics.exporter=none "
+    fi
+    echo "Using OpenTelemetry Java Agent"
 fi
 
 if [[ "${DEPLOY_MODE}" == "KUBERNETES" || "${DEPLOY_MODE}" == "KUBERNETES_ONPREM" || "${DEPLOY_VERSION}" == "COMMUNITY" ]]; then
     java $JAVA_OPTS -jar $CAPSULE_JAR $COMMAND /opt/harness/config.yml
 else
-    java $JAVA_OPTS -jar $CAPSULE_JAR $COMMAND /opt/harness/config.yml > /opt/harness/logs/ng-manager.log 2>&1
+    java $JAVA_OPTS -jar $CAPSULE_JAR $COMMAND /opt/harness/config.yml > /opt/harness/logs/idp-service.log 2>&1
 fi
