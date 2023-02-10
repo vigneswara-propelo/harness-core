6f38c25bc001ef65feeddb96dcec0f0212a4999f
Pratyush Goel <goel.pratyush@harness.io>
2023-02-10 16:23:08 +0530

    fix: [CDS-53114]: fixing Class Cast exception during runtime values of valuesPaths field. (#43793)

--- 127-cd-nextgen-entities/src/main/java/io/harness/cdng/visitor/helpers/store/HarnessStoreVisitorHelper.java
+++ 127-cd-nextgen-entities/src/main/java/io/harness/cdng/visitor/helpers/store/HarnessStoreVisitorHelper.java
@@ -8,6 +8,7 @@
 package io.harness.cdng.visitor.helpers.store;
 
 import static io.harness.cdng.visitor.YamlTypes.PATH_CONNECTOR;
+import static io.harness.common.ParameterFieldHelper.getParameterFieldValue;
 
 import io.harness.beans.FileReference;
 import io.harness.cdng.manifest.yaml.harness.HarnessStore;
@@ -24,6 +25,7 @@ import io.harness.walktree.visitor.entityreference.EntityReferenceExtractor;
 import io.harness.walktree.visitor.utilities.VisitorParentPathUtils;
 
 import com.google.inject.Inject;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -68,7 +70,9 @@ public class HarnessStoreVisitorHelper implements EntityReferenceExtractor {
   public Set<EntityDetailProtoDTO> getEntityDetailsProtoDTO(ParameterField<List<String>> valuesFiles,
       String accountIdentifier, String orgIdentifier, String projectIdentifier, Map<String, Object> contextMap,
       String overridePathFieldName) {
-    List<String> files = ParameterFieldHelper.getParameterFieldListValue(valuesFiles, false);
+    List<String> files = getParameterFieldValue(valuesFiles) instanceof List
+        ? ParameterFieldHelper.getParameterFieldListValue(valuesFiles, false)
+        : Collections.emptyList();
     Set<EntityDetailProtoDTO> result = new HashSet<>(files.size());
     files.forEach(scopedFilePath -> {
       FileReference fileReference =
