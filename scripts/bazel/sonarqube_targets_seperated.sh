bf2d001e1f299e4dfcf2b06ee8639b8dc0f866e9
Udham Singh <udham.singh@harness.io>
2023-02-10 17:09:14 +0530

    fix: [BT-2923]: Correcting sonar target names (#43471)
    * fix: [BT-2923]: reducing xmx and test size in java test
    
    * fix: [BT-2923]: Reducing test size and defining timeout
    
    * fix: [BT-2923]: Splitting targets for individual
    
    * fix: [BT-2923]: Splitting targets for individual
    
    * fix: [BT-2923]: reverting back gentestrule
    
    * fix: [BT-2923]: reverting back sonar target script
    
    * fix: [BT-2923]: Dividing modules below 400
    
    * fix: [BT-2923]: Splitting from 960 expression service
    
    * fix: [BT-2923]: splitting from 955-cg-yaml
    
    * fix: [BT-2923]: splitting from 815-cg-triggers
    
    * fix: [BT-2923]: splitting from 800 onwards
    
    * fix: [BT-2923]: splitting from 900
    
    * fix: [BT-2923]: Correcting sonar target names
    
    * fix: [BT-2923]: Correcting sonar target names
    
    * Update sonarqube_targets_seperated.sh

--- scripts/bazel/sonarqube_targets_seperated.sh
+++ scripts/bazel/sonarqube_targets_seperated.sh
@@ -11,15 +11,32 @@ MODULES="$1"
 echo "def get_sonarqube_targets_seperated():"
 echo "    return {"
 
-if [[ "$MODULES" == "below400" ]]; then
+if [[ "$MODULES" == "401to899" ]]; then
+
   bazel query 'attr(tags, "sonarqube", //...:*)' | cut -c 3- | rev | cut -c 11- | rev |\
-    awk ' { print "        \"//"$1":sonarqube\": \""$1"\"," } ' | sed '1,/400-rest/d'
+    awk ' { print "        \"//"$1":sonarqube\": \""$1"\"," } ' | sed '1,/400-rest/d' | grep "\/\/[4-8][0-9][0-9]-*"
+
 elif [[ "$MODULES" == "only400" ]]; then
+
   bazel query 'attr(tags, "sonarqube", //...:*)' | cut -c 3- | rev | cut -c 11- | rev |\
     awk ' { print "        \"//"$1":sonarqube\": \""$1"\"," } ' | grep 400-rest
-elif [[ "$MODULES" == "above400" ]]; then
+
+elif [[ "$MODULES" == "000to399" ]]; then
+
   bazel query 'attr(tags, "sonarqube", //...:*)' | cut -c 3- | rev | cut -c 11- | rev |\
     awk ' { print "        \"//"$1":sonarqube\": \""$1"\"," } ' | sed -n '/400-rest/q;p'
+
+elif [[ "$MODULES" == "900to999toIndivi" ]]; then
+
+  bazel query 'attr(tags, "sonarqube", //...:*)' | cut -c 3- | rev | cut -c 11- | rev |\
+      awk ' { print "        \"//"$1":sonarqube\": \""$1"\"," } ' | sed '1,/400-rest/d' | grep "\/\/9[0-9][0-9]-*" > 900-onwards.log
+
+  bazel query 'attr(tags, "sonarqube", //...:*)' | cut -c 3- | rev | cut -c 11- | rev |\
+        awk ' { print "        \"//"$1":sonarqube\": \""$1"\"," } ' | sed '1,/400-rest/d' | grep -v "\/\/[0-9][0-9][0-9]-*" >> 900-onwards.log
+
+  modules=$(cat 900-onwards.log)
+  echo "$modules"
+
 else
   echo "No Module Range Provided...Exiting"; exit 1
 fi
