421afc81d448e6b2316b2ebd52108ef559664e43
Richa Jajoo <richa.jajoo@harness.io>
2023-02-13 21:48:50 +0530

    fix: [PL-31145]: Remove GOOGLE_APPLICATION_CREDENTIALS (#43891)

--- 400-rest/src/main/java/software/wings/service/impl/GoogleDataStoreServiceImpl.java
+++ 400-rest/src/main/java/software/wings/service/impl/GoogleDataStoreServiceImpl.java
@@ -63,7 +63,7 @@ public class GoogleDataStoreServiceImpl implements DataStoreService {
   public GoogleDataStoreServiceImpl(WingsPersistence wingsPersistence) {
     String googleCredentialsPath = System.getenv(GOOGLE_APPLICATION_CREDENTIALS_PATH);
     boolean usingWorkloadIdentity = Boolean.parseBoolean(System.getenv(ENV_VARIABLE_WORKLOAD_IDENTITY));
-    if (!usingWorkloadIdentity && isEmpty(googleCredentialsPath) || !new File(googleCredentialsPath).exists()) {
+    if (!usingWorkloadIdentity && (isEmpty(googleCredentialsPath) || !new File(googleCredentialsPath).exists())) {
       throw new WingsException("Invalid credentials found at " + googleCredentialsPath);
     }
     mongoDataStoreService = new MongoDataStoreServiceImpl(wingsPersistence);
