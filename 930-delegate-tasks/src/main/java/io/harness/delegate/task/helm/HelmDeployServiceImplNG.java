4943d80a3e95c8681fa27dcdb1ce5ca3db0bab61
Achyuth Prakash <achyuth.prakash@harness.io>
2023-02-23 12:15:30 +0530

    fix: [CDS-53884]: extract only manifests from o/p of helm template cmd in case of dependencies (#44186)

--- 930-delegate-tasks/src/main/java/io/harness/delegate/task/helm/HelmDeployServiceImplNG.java
+++ 930-delegate-tasks/src/main/java/io/harness/delegate/task/helm/HelmDeployServiceImplNG.java
@@ -1052,11 +1052,8 @@ public class HelmDeployServiceImplNG implements HelmDeployServiceNG {
     HelmChartManifestDelegateConfig helmManifest =
         (HelmChartManifestDelegateConfig) commandRequest.getManifestDelegateConfig();
 
-    // sub-chart name will be non-empty only if FF is on and some value has been set
-    int index = isEmpty(helmManifest.getSubChartName())
-        ? -1
-        : helmTaskHelperBase.checkForDependencyUpdateFlag(
-            helmManifest.getHelmCommandFlag().getValueMap(), cliResponse.getOutput());
+    int index = helmTaskHelperBase.checkForDependencyUpdateFlag(
+        helmManifest.getHelmCommandFlag().getValueMap(), cliResponse.getOutput());
 
     return new HelmCommandResponseNG(cliResponse.getCommandExecutionStatus(),
         index == -1 ? cliResponse.getOutput() : cliResponse.getOutput().substring(index));
