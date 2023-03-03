eb1d60d9c167dc0f271527d3fda8ff8894f61c94
Yuvraj Singhal <yuvraj.singhal@harness.io>
2023-03-03 15:07:55 +0530

    [CDS-54523]: Resolving connector in jenkinsArtifact ArtifactPath api (#44639)

--- 120-ng-manager/src/main/java/io/harness/ng/core/artifacts/resources/jenkins/JenkinsArtifactResource.java
+++ 120-ng-manager/src/main/java/io/harness/ng/core/artifacts/resources/jenkins/JenkinsArtifactResource.java
@@ -178,13 +178,16 @@ public class JenkinsArtifactResource {
           accountId, orgIdentifier, projectIdentifier, serviceRef, fqnPath);
       JenkinsArtifactConfig jenkinsArtifactConfig = (JenkinsArtifactConfig) artifactSpecFromService;
       if (isEmpty(jenkinsConnectorIdentifier)) {
-        jenkinsConnectorIdentifier = jenkinsArtifactConfig.getConnectorRef().getValue();
+        jenkinsConnectorIdentifier = (String) jenkinsArtifactConfig.getConnectorRef().fetchFinalValue();
       }
 
       if (isEmpty(jobName)) {
-        jobName = jenkinsArtifactConfig.getJobName().getValue();
+        jobName = (String) jenkinsArtifactConfig.getJobName().fetchFinalValue();
       }
     }
+    jenkinsConnectorIdentifier =
+        artifactResourceUtils.getResolvedFieldValue(accountId, orgIdentifier, projectIdentifier, pipelineIdentifier,
+            runtimeInputYaml, jenkinsConnectorIdentifier, fqnPath, gitEntityBasicInfo, serviceRef);
     jobName = artifactResourceUtils.getResolvedFieldValue(accountId, orgIdentifier, projectIdentifier,
         pipelineIdentifier, runtimeInputYaml, jobName, fqnPath, gitEntityBasicInfo, serviceRef);
     IdentifierRef connectorRef =
