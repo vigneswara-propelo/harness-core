01672e97889a7da46e75ba323e83d38f25166326
Dev Mittal <devki.mittal@harness.io>
2023-03-06 11:20:00 +0530

    fix: [CI-6209]: added CI run step validation (#44614)

--- 332-ci-manager/service/src/main/java/io/harness/ci/execution/plan/creator/step/CIStepFilterJsonCreatorV2.java
+++ 332-ci-manager/service/src/main/java/io/harness/ci/execution/plan/creator/step/CIStepFilterJsonCreatorV2.java
@@ -9,15 +9,92 @@ package io.harness.ci.plan.creator.step;
 
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
+import io.harness.beans.steps.CIStepInfoType;
+import io.harness.beans.steps.nodes.RunStepNode;
+import io.harness.beans.steps.nodes.RunTestStepNode;
+import io.harness.beans.steps.stepinfo.RunStepInfo;
+import io.harness.beans.steps.stepinfo.RunTestsStepInfo;
+import io.harness.beans.yaml.extended.infrastrucutre.Infrastructure;
 import io.harness.ci.plan.creator.CICreatorUtils;
+import io.harness.exception.InvalidYamlException;
 import io.harness.filters.GenericStepPMSFilterJsonCreatorV2;
+import io.harness.plancreator.steps.AbstractStepNode;
+import io.harness.pms.filter.creation.FilterCreationResponse;
+import io.harness.pms.sdk.core.filter.creation.beans.FilterCreationContext;
+import io.harness.pms.yaml.YamlNode;
 
+import java.util.Objects;
 import java.util.Set;
+import org.apache.logging.log4j.util.Strings;
 
 @OwnedBy(HarnessTeam.CI)
 public class CIStepFilterJsonCreatorV2 extends GenericStepPMSFilterJsonCreatorV2 {
   @Override
   public Set<String> getSupportedStepTypes() {
     return CICreatorUtils.getSupportedStepsV2();
+  }
+
+  @Override
+  public FilterCreationResponse handleNode(FilterCreationContext filterCreationContext, AbstractStepNode yamlField) {
+    validateStep(filterCreationContext, yamlField);
+    return super.handleNode(filterCreationContext, yamlField);
+  }
+
+  public void validateStep(FilterCreationContext filterCreationContext, AbstractStepNode yamlField) {
+    String infra = getStageInfra(filterCreationContext);
+    String k = CIStepInfoType.RUN.getDisplayName();
+    String stepType = yamlField.getType();
+    switch (stepType) {
+      case "Run":
+        validateRunStep(infra, (RunStepNode) yamlField);
+        break;
+      case "RunTests":
+        validateRunTestsStep(infra, (RunTestStepNode) yamlField);
+        break;
+      default:
+        break;
+    }
+  }
+
+  private void validateRunStep(String infra, RunStepNode runStepNode) {
+    RunStepInfo runStep = runStepNode.getRunStepInfo();
+    if (Infrastructure.Type.KUBERNETES_DIRECT.getYamlName().equals(infra)) {
+      String connectorRef = runStep.getConnectorRef().getValue();
+      String image = runStep.getImage().getValue();
+      if (Strings.isBlank(connectorRef)) {
+        throw new InvalidYamlException("Run step with Kubernetes infra can't have empty connector field");
+      }
+      if (Strings.isBlank(image)) {
+        throw new InvalidYamlException("Run step with Kubernetes infra can't have empty image field");
+      }
+    }
+  }
+
+  private void validateRunTestsStep(String infra, RunTestStepNode runStepNode) {
+    RunTestsStepInfo runTestsStep = runStepNode.getRunTestsStepInfo();
+    if (Infrastructure.Type.KUBERNETES_DIRECT.getYamlName().equals(infra)) {
+      String connectorRef = runTestsStep.getConnectorRef().getValue();
+      String image = runTestsStep.getImage().getValue();
+      if (Strings.isBlank(connectorRef)) {
+        throw new InvalidYamlException("RunTests step with Kubernetes infra can't have empty connector field");
+      }
+      if (Strings.isBlank(image)) {
+        throw new InvalidYamlException("RunTests step with Kubernetes infra can't have empty image field");
+      }
+    }
+  }
+
+  private String getStageInfra(FilterCreationContext filterCreationContext) {
+    YamlNode currNode = filterCreationContext.getCurrentField().getNode();
+    while (!Objects.isNull(currNode)) {
+      if (!Objects.isNull(currNode.getField("infrastructure"))) {
+        break;
+      }
+      currNode = currNode.getParentNode();
+    }
+    if (!Objects.isNull(currNode)) {
+      return currNode.getField("infrastructure").getType();
+    }
+    return null;
   }
 }
---
+++ 332-ci-manager/service/src/test/java/io/harness/ci/plan/creator/step/CIStepFilterJsonCreatorV2Test.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2023 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
+ */
+
+package io.harness.ci.plan.creator.step;
+
+import static io.harness.rule.OwnerRule.DEV_MITTAL;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import io.harness.beans.steps.nodes.RunStepNode;
+import io.harness.beans.steps.stepinfo.RunStepInfo;
+import io.harness.category.element.UnitTests;
+import io.harness.exception.InvalidYamlException;
+import io.harness.pms.sdk.core.filter.creation.beans.FilterCreationContext;
+import io.harness.pms.yaml.ParameterField;
+import io.harness.pms.yaml.YamlField;
+import io.harness.pms.yaml.YamlNode;
+import io.harness.rule.Owner;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CIStepFilterJsonCreatorV2Test {
+  CIStepFilterJsonCreatorV2 ciStepFilterJsonCreatorV2 = new CIStepFilterJsonCreatorV2();
+
+  @Test
+  @Owner(developers = DEV_MITTAL)
+  @Category(UnitTests.class)
+  public void testValidateRunStepK8() {
+    YamlNode yamlNode = new YamlNode(getRunStepElementConfigAsJsonNode("KubernetesDirect"));
+    YamlField yamlField = new YamlField("Command", yamlNode);
+    FilterCreationContext context = FilterCreationContext.builder().currentField(yamlField).build();
+    RunStepNode runStepNode =
+        RunStepNode.builder()
+            .runStepInfo(RunStepInfo.builder()
+                             .connectorRef(ParameterField.<String>builder().value("connector").build())
+                             .image(ParameterField.<String>builder().value("image").build())
+                             .build())
+            .build();
+
+    ciStepFilterJsonCreatorV2.validateStep(context, runStepNode);
+  }
+
+  @Test
+  @Owner(developers = DEV_MITTAL)
+  @Category(UnitTests.class)
+  public void testValidateRunStepK8Exception() {
+    YamlNode yamlNode = new YamlNode(getRunStepElementConfigAsJsonNode("KubernetesDirect"));
+    YamlField yamlField = new YamlField("Command", yamlNode);
+    FilterCreationContext context = FilterCreationContext.builder().currentField(yamlField).build();
+    RunStepNode runStepNode =
+        RunStepNode.builder()
+            .runStepInfo(RunStepInfo.builder()
+                             .connectorRef(ParameterField.<String>builder().value("connector").build())
+                             .image(ParameterField.<String>builder().build())
+                             .build())
+            .build();
+
+    assertThatThrownBy(() -> ciStepFilterJsonCreatorV2.validateStep(context, runStepNode))
+        .isInstanceOf(InvalidYamlException.class);
+
+    RunStepNode runStepNode1 = RunStepNode.builder()
+                                   .runStepInfo(RunStepInfo.builder()
+                                                    .connectorRef(ParameterField.<String>builder().build())
+                                                    .image(ParameterField.<String>builder().value("image").build())
+                                                    .build())
+                                   .build();
+
+    assertThatThrownBy(() -> ciStepFilterJsonCreatorV2.validateStep(context, runStepNode1))
+        .isInstanceOf(InvalidYamlException.class);
+  }
+
+  @Test
+  @Owner(developers = DEV_MITTAL)
+  @Category(UnitTests.class)
+  public void testValidateRunStepVMImageNull() {
+    YamlNode yamlNode = new YamlNode(getRunStepElementConfigAsJsonNode("VM"));
+    YamlField yamlField = new YamlField("Command", yamlNode);
+    FilterCreationContext context = FilterCreationContext.builder().currentField(yamlField).build();
+    RunStepNode runStepNode = RunStepNode.builder()
+                                  .runStepInfo(RunStepInfo.builder()
+                                                   .connectorRef(ParameterField.<String>builder().build())
+                                                   .image(ParameterField.<String>builder().build())
+                                                   .build())
+                                  .build();
+
+    ciStepFilterJsonCreatorV2.validateStep(context, runStepNode);
+  }
+
+  private static JsonNode getRunStepElementConfigAsJsonNode(String infra) {
+    ObjectMapper mapper = new ObjectMapper();
+    ObjectNode stepElementConfig = mapper.createObjectNode();
+    stepElementConfig.put("identifier", "identifier");
+    stepElementConfig.put("name", "name");
+
+    ObjectNode infraNode = mapper.createObjectNode();
+    infraNode.put("type", infra);
+    stepElementConfig.set("infrastructure", infraNode);
+
+    return stepElementConfig;
+  }
+}
