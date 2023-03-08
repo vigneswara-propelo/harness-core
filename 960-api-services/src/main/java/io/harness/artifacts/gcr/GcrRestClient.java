83cbc1b75a0a8bf4fb8a6258efd731c304ad6ed1
Deepak Puthraya <deepak.puthraya@harness.io>
2023-03-09 00:05:11 +0530

    fix: [CDS-54925]: Add OCI headers to accept for GCR (#44887)

--- 960-api-services/src/main/java/io/harness/artifacts/gcr/GcrRestClient.java
+++ 960-api-services/src/main/java/io/harness/artifacts/gcr/GcrRestClient.java
@@ -32,7 +32,7 @@ public interface GcrRestClient {
       @Header("Authorization") String bearerAuthHeader, @Path(value = "imageName", encoded = true) String imageName);
 
   @Headers(
-      "Accept: application/vnd.docker.distribution.manifest.v1+json, application/vnd.docker.distribution.manifest.v1+prettyjws")
+      "Accept: application/vnd.docker.distribution.manifest.v1+json, application/vnd.docker.distribution.manifest.v1+prettyjws, application/vnd.oci.image.index.v1+json, application/vnd.oci.image.manifest.v1+json")
   @GET("/v2/{imageName}/manifests/{tag}")
   Call<DockerImageManifestResponse>
   getImageManifest(@Header("Authorization") String bearerAuthHeader,
