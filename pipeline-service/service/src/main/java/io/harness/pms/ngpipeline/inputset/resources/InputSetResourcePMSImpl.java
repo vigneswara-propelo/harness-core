6e4c0c056aa7912ecc27810b74e5cf85d583d0d9
Meet Rathod <rathod.meetsatish@harness.io>
2023-03-08 22:10:39 +0530

    [fix]: [SPG-2260]: fix trigger is loading from master branch (#44890)

--- pipeline-service/service/src/main/java/io/harness/pms/ngpipeline/inputset/resources/InputSetResourcePMSImpl.java
+++ pipeline-service/service/src/main/java/io/harness/pms/ngpipeline/inputset/resources/InputSetResourcePMSImpl.java
@@ -262,6 +262,9 @@ public class InputSetResourcePMSImpl implements InputSetResourcePMS {
       @NotNull @ProjectIdentifier String projectIdentifier, @NotNull @ResourceIdentifier String pipelineIdentifier,
       String pipelineBranch, String pipelineRepoID, GitEntityFindInfoDTO gitEntityBasicInfo,
       @NotNull @Valid MergeInputSetRequestDTOPMS mergeInputSetRequestDTO, String loadFromCache) {
+    if (pipelineBranch == null && gitEntityBasicInfo != null) {
+      pipelineBranch = gitEntityBasicInfo.getBranch();
+    }
     List<String> inputSetReferences = mergeInputSetRequestDTO.getInputSetReferences();
     String mergedYaml;
     try {
