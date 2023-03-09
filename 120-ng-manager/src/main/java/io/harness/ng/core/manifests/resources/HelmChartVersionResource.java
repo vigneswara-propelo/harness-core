2bde401014e9b7fcd481d86cb7c55f611b851e42
Marko Lukic <marko.lukic@harness.io>
2023-03-09 09:49:51 +0100

    [fix]: [CDS-54147]: adding rest api resource for helm manifest to support ex… (#44751)
    * [CDS-54147]: adding rest api resource for helm manifest to support expression resolution for connector and chart name when chart version is runtime input
    
    * [CDS-54147]: changing call from GET to POST as it a recommended practice in case when having a body payload
    
    * [CDS-54147]: added expression resolution for specific S3 Helm chart fields

--- 120-ng-manager/src/main/java/io/harness/ng/core/manifests/resources/HelmChartVersionResource.java
+++ 120-ng-manager/src/main/java/io/harness/ng/core/manifests/resources/HelmChartVersionResource.java
@@ -8,11 +8,17 @@
 package io.harness.ng.core.manifests.resources;
 
 import static io.harness.annotations.dev.HarnessTeam.CDP;
+import static io.harness.data.structure.EmptyPredicate.isEmpty;
+import static io.harness.data.structure.EmptyPredicate.isNotEmpty;
 
 import io.harness.NGCommonEntityConstants;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.cdng.manifest.resources.HelmChartService;
 import io.harness.cdng.manifest.resources.dtos.HelmChartResponseDTO;
+import io.harness.cdng.manifest.yaml.S3StoreConfig;
+import io.harness.cdng.manifest.yaml.kinds.HelmChartManifest;
+import io.harness.cdng.manifest.yaml.storeConfig.StoreConfig;
+import io.harness.gitsync.interceptor.GitEntityFindInfoDTO;
 import io.harness.ng.core.artifacts.resources.util.ArtifactResourceUtils;
 import io.harness.ng.core.dto.ErrorDTO;
 import io.harness.ng.core.dto.FailureDTO;
@@ -24,8 +30,10 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import javax.validation.constraints.NotNull;
+import javax.ws.rs.BeanParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -48,6 +56,7 @@ import lombok.extern.slf4j.Slf4j;
 public class HelmChartVersionResource {
   private final HelmChartService helmChartService;
   private final ArtifactResourceUtils artifactResourceUtils;
+
   @GET
   @Path("chart/version")
   @ApiOperation(value = "Gets helm chart version details", nickname = "getHelmChartVersionDetails")
@@ -59,6 +68,70 @@ public class HelmChartVersionResource {
       @NotNull @QueryParam("fqnPath") String fqnPath, @QueryParam("connectorRef") String connectorIdentifier,
       @QueryParam("chartName") String chartName, @QueryParam("region") String region,
       @QueryParam("bucketName") String bucketName, @QueryParam("folderPath") String folderPath) {
+    HelmChartResponseDTO helmChartResponseDTO = helmChartService.getHelmChartVersionDetails(accountId, orgIdentifier,
+        projectIdentifier, serviceRef, fqnPath, connectorIdentifier, chartName, region, bucketName, folderPath);
+    return ResponseDTO.newResponse(helmChartResponseDTO);
+  }
+
+  @POST
+  @Path("v2/chart/version")
+  @ApiOperation(value = "Gets helm chart version details with yaml input for expression resolution",
+      nickname = "getHelmChartVersionDetailsWithYaml")
+  public ResponseDTO<HelmChartResponseDTO>
+  getHelmChartVersionDetailsWithYaml(@NotNull @QueryParam(NGCommonEntityConstants.ACCOUNT_KEY) String accountId,
+      @NotNull @QueryParam(NGCommonEntityConstants.ORG_KEY) String orgIdentifier,
+      @NotNull @QueryParam(NGCommonEntityConstants.PROJECT_KEY) String projectIdentifier,
+      @NotNull @QueryParam(NGCommonEntityConstants.PIPELINE_KEY) String pipelineIdentifier,
+      @QueryParam(NGCommonEntityConstants.SERVICE_KEY) String serviceRef,
+      @NotNull @QueryParam("fqnPath") String fqnPath, @QueryParam("connectorRef") String connectorIdentifier,
+      @QueryParam("chartName") String chartName, @QueryParam("region") String region,
+      @QueryParam("bucketName") String bucketName, @QueryParam("folderPath") String folderPath,
+      @BeanParam GitEntityFindInfoDTO gitEntityBasicInfo, @NotNull String runtimeInputYaml) {
+    if (isNotEmpty(serviceRef)) {
+      HelmChartManifest helmChartManifest =
+          (HelmChartManifest) helmChartService
+              .locateManifestInService(accountId, orgIdentifier, projectIdentifier, serviceRef, fqnPath)
+              .getSpec();
+      StoreConfig storeConfig = helmChartManifest.getStoreConfig();
+
+      if (isEmpty(connectorIdentifier)) {
+        connectorIdentifier = (String) storeConfig.getConnectorReference().fetchFinalValue();
+      }
+
+      if (isEmpty(chartName)) {
+        chartName = (String) helmChartManifest.getChartName().fetchFinalValue();
+      }
+
+      if (storeConfig instanceof S3StoreConfig) {
+        S3StoreConfig s3StoreConfig = (S3StoreConfig) storeConfig;
+        if (isEmpty(region)) {
+          region = (String) s3StoreConfig.getRegion().fetchFinalValue();
+        }
+
+        if (isEmpty(bucketName)) {
+          bucketName = (String) s3StoreConfig.getBucketName().fetchFinalValue();
+        }
+
+        if (isEmpty(folderPath)) {
+          folderPath = (String) s3StoreConfig.getFolderPath().fetchFinalValue();
+        }
+
+        region = artifactResourceUtils.getResolvedFieldValue(accountId, orgIdentifier, projectIdentifier,
+            pipelineIdentifier, runtimeInputYaml, region, fqnPath, gitEntityBasicInfo, serviceRef);
+
+        bucketName = artifactResourceUtils.getResolvedFieldValue(accountId, orgIdentifier, projectIdentifier,
+            pipelineIdentifier, runtimeInputYaml, bucketName, fqnPath, gitEntityBasicInfo, serviceRef);
+
+        folderPath = artifactResourceUtils.getResolvedFieldValue(accountId, orgIdentifier, projectIdentifier,
+            pipelineIdentifier, runtimeInputYaml, folderPath, fqnPath, gitEntityBasicInfo, serviceRef);
+      }
+
+      connectorIdentifier = artifactResourceUtils.getResolvedFieldValue(accountId, orgIdentifier, projectIdentifier,
+          pipelineIdentifier, runtimeInputYaml, connectorIdentifier, fqnPath, gitEntityBasicInfo, serviceRef);
+
+      chartName = artifactResourceUtils.getResolvedFieldValue(accountId, orgIdentifier, projectIdentifier,
+          pipelineIdentifier, runtimeInputYaml, chartName, fqnPath, gitEntityBasicInfo, serviceRef);
+    }
     HelmChartResponseDTO helmChartResponseDTO = helmChartService.getHelmChartVersionDetails(accountId, orgIdentifier,
         projectIdentifier, serviceRef, fqnPath, connectorIdentifier, chartName, region, bucketName, folderPath);
     return ResponseDTO.newResponse(helmChartResponseDTO);
