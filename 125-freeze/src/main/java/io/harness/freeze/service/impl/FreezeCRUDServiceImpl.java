49e56fae43abd70ba6c422f286843cc480e87d3c
Abhishek <abhishek.abhishek@harness.io>
2023-03-10 15:43:06 +0530

    added break to switch statement (#44968)

--- 125-freeze/src/main/java/io/harness/freeze/service/impl/FreezeCRUDServiceImpl.java
+++ 125-freeze/src/main/java/io/harness/freeze/service/impl/FreezeCRUDServiceImpl.java
@@ -298,6 +298,7 @@ public class FreezeCRUDServiceImpl implements FreezeCRUDService {
           break;
         case MANUAL:
           url = notificationHelper.getManualFreezeUrl(baseUrl, accountId, orgId, projectId, freeze.getIdentifier());
+          break;
         default:
           break;
       }
