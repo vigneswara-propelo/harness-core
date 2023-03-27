4d3a1db45043adaeca1a1fd5ef527b58f0d2f301
Boopesh Shanmugam <boopesh.shanmugam@harness.io>
2023-03-27 21:07:16 +0530

    fix: [CDS-54510]: fixing NPE on stateInspection. (#45690)

--- 870-cg-orchestration/src/main/java/io/harness/state/inspection/StateInspectionServiceImpl.java
+++ 870-cg-orchestration/src/main/java/io/harness/state/inspection/StateInspectionServiceImpl.java
@@ -43,7 +43,13 @@ public class StateInspectionServiceImpl implements StateInspectionService {
   private final String EXPRESSION_VARIABLE_USAGE = "expressionVariableUsage";
 
   public void transformToYaml(StateInspection stateInspection) {
-    ExpressionVariableUsage v = (ExpressionVariableUsage) stateInspection.getData().get(EXPRESSION_VARIABLE_USAGE);
+    ExpressionVariableUsage v = null;
+    try {
+      v = (ExpressionVariableUsage) stateInspection.getData().get(EXPRESSION_VARIABLE_USAGE);
+    } catch (Exception e) {
+      log.info("There is no variable expression usage for stateExecutionInstance {}",
+          stateInspection.getStateExecutionInstanceId());
+    }
     if (v != null) {
       List<ExpressionVariableUsage.Item> vars = v.getVariables();
 
