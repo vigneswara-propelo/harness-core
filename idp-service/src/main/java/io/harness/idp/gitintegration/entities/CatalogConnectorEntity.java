876c87086c79c547abeff8a9879fb63366562674
Vigneswara <119295389+vigneswara-propelo@users.noreply.github.com>
2023-03-28 15:04:09 +0530

    feat: [IDP-283]: Add @Document for CatalogConnectorEntity (#45737)

--- idp-service/src/main/java/io/harness/idp/gitintegration/entities/CatalogConnectorEntity.java
+++ idp-service/src/main/java/io/harness/idp/gitintegration/entities/CatalogConnectorEntity.java
@@ -42,6 +42,7 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Builder
@@ -52,6 +53,7 @@ import org.springframework.data.annotation.LastModifiedDate;
 @AllArgsConstructor
 @StoreIn(DbAliases.IDP)
 @Entity(value = "catalogConnector", noClassnameStored = true)
+@Document("catalogConnector")
 @HarnessEntity(exportable = true)
 @OwnedBy(HarnessTeam.IDP)
 public class CatalogConnectorEntity
