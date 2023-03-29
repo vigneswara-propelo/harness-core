9ab056193f4d82d882aeb65e407ad58fc8a4c457
Abhinav Singh <abhinav.singh@harness.io>
2023-03-29 02:05:24 -0700

    feat: [CDS-57804]: container step group basic pojo (#45454)

---
+++ clients/pipeline-service/pms-sdk-core/src/main/java/io/harness/steps/common/steps/stepgroup/StepGroupInfra.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2023 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
+ */
+
+package io.harness.steps.common.steps.stepgroup;
+
+import io.harness.annotations.dev.HarnessTeam;
+import io.harness.annotations.dev.OwnedBy;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonValue;
+import io.swagger.annotations.ApiModelProperty;
+import org.springframework.data.annotation.TypeAlias;
+
+@OwnedBy(HarnessTeam.PIPELINE)
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type", visible = true,
+    include = JsonTypeInfo.As.EXISTING_PROPERTY, defaultImpl = DelegateInfra.class)
+public interface StepGroupInfra {
+  @TypeAlias("infrastructure_type")
+  enum Type {
+    @JsonProperty("KubernetesDirect") KUBERNETES_DIRECT("KubernetesDirect"),
+    @JsonProperty("Delegate") DELEGATE("Delegate");
+    private final String yamlName;
+
+    Type(String yamlName) {
+      this.yamlName = yamlName;
+    }
+
+    @JsonValue
+    public String getYamlName() {
+      return yamlName;
+    }
+  }
+
+  @ApiModelProperty(allowableValues = "KubernetesDirect, Delegate") Type getType();
+}
---
+++ pipeline-service/modules/orchestration-steps/contracts/src/main/java/io/harness/steps/plugin/infrastructure/K8sDirectInfra.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2023 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
+ */
+
+package io.harness.steps.plugin.infrastructure;
+
+import io.harness.annotations.dev.HarnessTeam;
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.pms.yaml.YamlNode;
+import io.harness.steps.common.steps.stepgroup.StepGroupInfra;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import io.swagger.annotations.ApiModelProperty;
+import javax.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+
+@JsonTypeName("KubernetesDirect")
+@OwnedBy(HarnessTeam.PIPELINE)
+public class K8sDirectInfra implements StepGroupInfra {
+  @Builder.Default @NotNull @Getter private Type type = Type.KUBERNETES_DIRECT;
+  @NotNull private ContainerInfraYamlSpec spec;
+  @JsonProperty(YamlNode.UUID_FIELD_NAME)
+  @Getter(onMethod_ = { @ApiModelProperty(hidden = true) })
+  @ApiModelProperty(hidden = true)
+  String uuid;
+}
