e95a555745c17c448840c727b328b1d5627e2150
Meenakshi Raikwar <meenakshi.raikwar@harness.io>
2023-04-03 22:07:24 +0530

    feat: [PL-32076]: Removed GCP KMS Migration needed for prod-3 (#46002)

--- 120-ng-manager/src/main/java/io/harness/ng/migration/NGCoreBackgroundMigrationDetails.java
+++ 120-ng-manager/src/main/java/io/harness/ng/migration/NGCoreBackgroundMigrationDetails.java
@@ -10,8 +10,8 @@ package io.harness.ng.migration;
 import io.harness.migration.MigrationDetails;
 import io.harness.migration.NGMigration;
 import io.harness.migration.beans.MigrationType;
+import io.harness.migration.utils.NoopMigration;
 import io.harness.ng.core.migration.CopyTemplatesPermissionRoleUpdate;
-import io.harness.ng.core.migration.CreateDefaultGcpKmsSMInNGMigration;
 import io.harness.ng.core.migration.NGWebhookMendateSettingsCategoryUpdateMigration;
 import io.harness.ng.core.migration.PopulateYamlFieldInNGEnvironmentMigration;
 import io.harness.ng.core.migration.background.AddDeploymentTypeToInfrastructureEntityMigration;
@@ -47,7 +47,7 @@ public class NGCoreBackgroundMigrationDetails implements MigrationDetails {
         .add(Pair.of(6, NGWebhookMendateSettingsCategoryUpdateMigration.class))
         .add(Pair.of(7, DeleteSoftDeletedConnectorsMigration.class))
         .add(Pair.of(8, PopulateYamlAuthFieldInNGJiraConnectorMigration.class))
-        .add(Pair.of(9, CreateDefaultGcpKmsSMInNGMigration.class))
+        .add(Pair.of(9, NoopMigration.class))
         .add(Pair.of(10, UserMetadataTwoFactorAuthenticationMigration.class))
         .build();
   }
