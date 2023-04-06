90a4a2b28a2c49e63f21d90e31a899aaec6202cb
Ankit Tiwari <ankit.tiwari@harness.io>
2023-04-06 20:01:35 +0530

    feat: [PIE-9477]: Adding redis timeout and subscriptionPerConnection Details for redis configs (#46170)

--- pipeline-service/build/container/scripts/replace_configs.sh
+++ pipeline-service/build/container/scripts/replace_configs.sh
@@ -378,6 +378,10 @@ if [[ "" != "$REDIS_TIMEOUT" ]]; then
   export REDIS_TIMEOUT; yq -i '.singleServerConfig.timeout=env(REDIS_TIMEOUT)' $REDISSON_CACHE_FILE
 fi
 
+if [[ "" != "$REDIS_SUBSCRIPTIONS_PER_CONNECTION" ]]; then
+  export REDIS_SUBSCRIPTIONS_PER_CONNECTION; yq -i '.singleServerConfig.subscriptionsPerConnection=env(REDIS_SUBSCRIPTIONS_PER_CONNECTION)' $REDISSON_CACHE_FILE
+fi
+
 yq -i 'del(.codec)' $ENTERPRISE_REDISSON_CACHE_FILE
 
 if [[ "$REDIS_SCRIPT_CACHE" == "false" ]]; then
