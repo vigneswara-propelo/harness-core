defca9cc15f46a46956a7de70c9773e5eb2e3504
Abhinav Hinger <abhinav.hinger@harness.io>
2023-04-07 19:10:05 +0530

    fix: [CDS-58621]: do not partially render service variables (#46228)

--- 125-cd-nextgen/modules/common/core/src/main/java/io/harness/cdng/service/steps/ServiceStepV3.java
+++ 125-cd-nextgen/modules/common/core/src/main/java/io/harness/cdng/service/steps/ServiceStepV3.java
@@ -363,8 +363,6 @@ public class ServiceStepV3 implements ChildrenExecutable<ServiceStepV3Parameters
 
     serviceStepsHelper.checkForAccessOrThrow(ambiance, secretNGVariables);
 
-    resolve(ambiance, envToEnvVariables, envToSvcVariables);
-
     GitOpsEnvOutCome gitOpsEnvOutCome = new GitOpsEnvOutCome(envToEnvVariables, envToSvcVariables);
 
     sweepingOutputService.consume(ambiance, GITOPS_ENV_OUTCOME, gitOpsEnvOutCome, StepCategory.STAGE.name());
@@ -476,7 +474,6 @@ public class ServiceStepV3 implements ChildrenExecutable<ServiceStepV3Parameters
             mergeSvcOverrideInputs(ngServiceOverridesEntity.get().getYaml(), parameters.getServiceOverrideInputs());
       }
 
-      resolve(ambiance, ngEnvironmentConfig, ngServiceOverrides);
       List<NGVariable> secretNGVariables = new ArrayList<>();
       if (ngEnvironmentConfig != null && ngEnvironmentConfig.getNgEnvironmentInfoConfig() != null
           && ngEnvironmentConfig.getNgEnvironmentInfoConfig().getVariables() != null) {
