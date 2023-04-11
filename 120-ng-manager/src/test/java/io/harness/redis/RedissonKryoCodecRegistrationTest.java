b76828a9971f9700c0b15201763b27095e3e8d72
Bhavya Agrawal <bhavya.agrawal@harness.io>
2023-04-11 06:55:32 +0530

    feat: [PL-32077]: Add UT for registration of kryo classes in manager and ng-manager (#46280)
    * [PL-32077]: add UT for kryo instantion in manager and ng-manager
    
    * [PL-32077]: fix UT
    
    * [PL-32077]: changed method name

---
+++ 120-ng-manager/src/test/java/io/harness/redis/RedissonKryoCodecRegistrationTest.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2023 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
+ */
+package io.harness.redis;
+
+import static io.harness.annotations.dev.HarnessTeam.PL;
+import static io.harness.rule.OwnerRule.BHAVYA;
+
+import io.harness.NgManagerTestBase;
+import io.harness.agent.sdk.HarnessAlwaysRun;
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.category.element.UnitTests;
+import io.harness.exception.GeneralException;
+import io.harness.reflection.CodeUtils;
+import io.harness.reflection.HarnessReflections;
+import io.harness.rule.Owner;
+import io.harness.serializer.ClassResolver;
+import io.harness.serializer.HKryo;
+import io.harness.serializer.KryoRegistrar;
+
+import com.esotericsoftware.kryo.serializers.FieldSerializer;
+import java.lang.reflect.Constructor;
+import java.util.Set;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@OwnedBy(PL)
+public class RedissonKryoCodecRegistrationTest extends NgManagerTestBase {
+  @Test
+  @Owner(developers = BHAVYA)
+  @Category(UnitTests.class)
+  @HarnessAlwaysRun
+  public void testKryoRegistration() {
+    final Set<Class<? extends KryoRegistrar>> kryoRegistrars =
+        HarnessReflections.get().getSubTypesOf(KryoRegistrar.class);
+    final ClassResolver classResolver = new ClassResolver();
+    HKryo kryo = new HKryo(classResolver);
+    kryo.setDefaultSerializer(FieldSerializer.class);
+    try {
+      for (Class<? extends KryoRegistrar> clazz : kryoRegistrars) {
+        Constructor<?> constructor = clazz.getConstructor();
+        final KryoRegistrar kryoRegistrar = (KryoRegistrar) constructor.newInstance();
+
+        kryo.setCurrentLocation(CodeUtils.location(kryoRegistrar.getClass()));
+        kryoRegistrar.register(kryo);
+      }
+    } catch (Exception ex) {
+      throw new GeneralException("Failed initializing kryo", ex);
+    }
+  }
+}
