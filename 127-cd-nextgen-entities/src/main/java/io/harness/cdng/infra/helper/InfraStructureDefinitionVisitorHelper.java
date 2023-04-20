961a39a1d9c13898bfdbbcbe992dfb090b60723b
Abhinav Hinger <abhinav.hinger@harness.io>
2023-04-20 11:46:03 +0530

    fix: [CDS-54016]: add references for infra inputs (#46480)
    * fix: [CDS-54016]: add references for infra inputs
    
    * add UT

--- 127-cd-nextgen-entities/src/main/java/io/harness/cdng/infra/helper/InfraStructureDefinitionVisitorHelper.java
+++ 127-cd-nextgen-entities/src/main/java/io/harness/cdng/infra/helper/InfraStructureDefinitionVisitorHelper.java
@@ -8,6 +8,7 @@
 package io.harness.cdng.infra.helper;
 
 import static io.harness.annotations.dev.HarnessTeam.CDC;
+import static io.harness.data.structure.EmptyPredicate.isNotEmpty;
 import static io.harness.walktree.visitor.utilities.VisitorParentPathUtils.PATH_CONNECTOR;
 
 import static org.apache.commons.lang3.StringUtils.defaultIfBlank;
@@ -20,25 +21,42 @@ import io.harness.common.NGExpressionUtils;
 import io.harness.data.structure.EmptyPredicate;
 import io.harness.eventsframework.schemas.entity.EntityDetailProtoDTO;
 import io.harness.eventsframework.schemas.entity.EntityTypeProtoEnum;
+import io.harness.eventsframework.schemas.entity.IdentifierRefProtoDTO;
 import io.harness.eventsframework.schemas.entity.InfraDefinitionReferenceProtoDTO;
+import io.harness.ng.core.infrastructure.entity.InfrastructureEntity;
+import io.harness.ng.core.infrastructure.services.InfrastructureEntityService;
+import io.harness.ng.core.yaml.CDYamlUtils;
+import io.harness.pms.merger.fqn.FQN;
+import io.harness.pms.merger.helpers.FQNMapGenerator;
+import io.harness.pms.yaml.ParameterField;
 import io.harness.preflight.PreFlightCheckMetadata;
+import io.harness.setupusage.InfrastructureEntitySetupUsageHelper;
 import io.harness.utils.IdentifierRefHelper;
+import io.harness.utils.IdentifierRefProtoUtils;
 import io.harness.walktree.visitor.entityreference.EntityReferenceExtractor;
 import io.harness.walktree.visitor.utilities.VisitorParentPathUtils;
 import io.harness.walktree.visitor.validation.ConfigValidator;
 import io.harness.walktree.visitor.validation.ValidationVisitor;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.inject.Inject;
 import com.google.protobuf.StringValue;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
 
 @OwnedBy(CDC)
 @Slf4j
 public class InfraStructureDefinitionVisitorHelper implements ConfigValidator, EntityReferenceExtractor {
+  @Inject InfrastructureEntityService infrastructureEntityService;
+
+  @Inject InfrastructureEntitySetupUsageHelper infrastructureEntitySetupUsageHelper;
+
   @Override
   public void validate(Object object, ValidationVisitor visitor) {
     // Nothing to validate.
@@ -78,12 +96,56 @@ public class InfraStructureDefinitionVisitorHelper implements ConfigValidator, E
             IdentifierRefHelper.getIdentifierRef(envRef, accountIdentifier, orgIdentifier, projectIdentifier, metadata);
         EntityDetailProtoDTO detailProto = getEntityDetailProtoDTO(infraRef, envIdentifierRef);
         result.add(detailProto);
+
+        if (ParameterField.isNotNull(infraYaml.getInputs()) && !infraYaml.getInputs().isExpression()) {
+          if (EmptyPredicate.isEmpty(infraRef) || NGExpressionUtils.isRuntimeOrExpressionField(infraRef)) {
+            return result;
+          }
+
+          Optional<InfrastructureEntity> infraEntity = infrastructureEntityService.get(
+              envIdentifierRef.getAccountIdentifier(), envIdentifierRef.getOrgIdentifier(),
+              envIdentifierRef.getProjectIdentifier(), envIdentifierRef.getIdentifier(), infraRef);
+
+          if (infraEntity.isEmpty()) {
+            return result;
+          }
+
+          Set<EntityDetailProtoDTO> entityDetailProtoDTOS =
+              infrastructureEntitySetupUsageHelper.getAllReferredEntities(infraEntity.get());
+
+          Map<String, Object> map = new LinkedHashMap<>();
+          map.put("infrastructureDefinition", infraYaml.getInputs().getValue());
+          Map<FQN, Object> fqnToValueMap = FQNMapGenerator.generateFQNMap(CDYamlUtils.getMapper().valueToTree(map));
+          Map<String, Object> fqnStringToValueMap = new HashMap<>();
+          fqnToValueMap.forEach((fqn, value) -> fqnStringToValueMap.put(fqn.getExpressionFqn(), value));
+
+          for (EntityDetailProtoDTO entityDetailProtoDTO : entityDetailProtoDTOS) {
+            if (isReferredEntityForRuntimeInput(entityDetailProtoDTO.getIdentifierRef())) {
+              JsonNode obj = (JsonNode) fqnStringToValueMap.get(
+                  entityDetailProtoDTO.getIdentifierRef().getMetadataMap().get("fqn"));
+              if (obj != null) {
+                EntityDetailProtoDTO entityDetailProtoDTOFinal =
+                    convertToEntityDetailProtoDTO(accountIdentifier, orgIdentifier, projectIdentifier,
+                        entityDetailProtoDTO.getIdentifierRef().getMetadataMap().get("fqn"), obj.textValue(),
+                        entityDetailProtoDTO.getType(), true);
+                result.add(entityDetailProtoDTOFinal);
+              }
+            }
+          }
+        }
       }
       return result;
     } catch (Exception ex) {
       log.error("failed to add setup usage reference for infrastructure" + infraRef);
       return new HashSet<>();
     }
+  }
+
+  private boolean isReferredEntityForRuntimeInput(IdentifierRefProtoDTO identifierRefOfReferredEntity) {
+    return identifierRefOfReferredEntity.getMetadataMap() != null
+        && isNotEmpty(identifierRefOfReferredEntity.getMetadataMap().get(PreFlightCheckMetadata.FQN))
+        && isNotEmpty(identifierRefOfReferredEntity.getMetadataMap().get(PreFlightCheckMetadata.EXPRESSION))
+        && NGExpressionUtils.matchesInputSetPattern(identifierRefOfReferredEntity.getIdentifier().getValue());
   }
 
   private EntityDetailProtoDTO getEntityDetailProtoDTO(String infraRef, IdentifierRef envIdentifierRef) {
@@ -99,5 +161,30 @@ public class InfraStructureDefinitionVisitorHelper implements ConfigValidator, E
         .setInfraDefRef(infraDefinitionReferenceProtoDTO)
         .setType(EntityTypeProtoEnum.INFRASTRUCTURE)
         .build();
+  }
+
+  private EntityDetailProtoDTO convertToEntityDetailProtoDTO(String accountId, String orgId, String projectId,
+      String fullQualifiedDomainName, String entityRefValue, EntityTypeProtoEnum entityTypeProtoEnum,
+      boolean shouldModifyFqn) {
+    Map<String, String> metadata = new HashMap<>();
+
+    metadata.put(PreFlightCheckMetadata.FQN, fullQualifiedDomainName);
+
+    if (NGExpressionUtils.isRuntimeOrExpressionField(entityRefValue)) {
+      metadata.put(PreFlightCheckMetadata.EXPRESSION, entityRefValue);
+      IdentifierRef identifierRef = IdentifierRefHelper.createIdentifierRefWithUnknownScope(
+          accountId, orgId, projectId, entityRefValue, metadata);
+      return EntityDetailProtoDTO.newBuilder()
+          .setIdentifierRef(IdentifierRefProtoUtils.createIdentifierRefProtoFromIdentifierRef(identifierRef))
+          .setType(entityTypeProtoEnum)
+          .build();
+    } else {
+      IdentifierRef identifierRef =
+          IdentifierRefHelper.getIdentifierRef(entityRefValue, accountId, orgId, projectId, metadata);
+      return EntityDetailProtoDTO.newBuilder()
+          .setIdentifierRef(IdentifierRefProtoUtils.createIdentifierRefProtoFromIdentifierRef(identifierRef))
+          .setType(entityTypeProtoEnum)
+          .build();
+    }
   }
 }
--- 127-cd-nextgen-entities/src/test/java/io/harness/cdng/infra/helper/InfraStructureDefinitionVisitorHelperTest.java
+++ 127-cd-nextgen-entities/src/test/java/io/harness/cdng/infra/helper/InfraStructureDefinitionVisitorHelperTest.java
@@ -8,28 +8,51 @@
 package io.harness.cdng.infra.helper;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doReturn;
 
 import io.harness.CategoryTest;
 import io.harness.category.element.UnitTests;
 import io.harness.cdng.infra.yaml.InfraStructureDefinitionYaml;
 import io.harness.eventsframework.schemas.entity.EntityDetailProtoDTO;
 import io.harness.eventsframework.schemas.entity.EntityTypeProtoEnum;
+import io.harness.eventsframework.schemas.entity.IdentifierRefProtoDTO;
 import io.harness.eventsframework.schemas.entity.InfraDefinitionReferenceProtoDTO;
+import io.harness.ng.core.infrastructure.entity.InfrastructureEntity;
+import io.harness.ng.core.infrastructure.services.InfrastructureEntityService;
 import io.harness.pms.yaml.ParameterField;
 import io.harness.rule.Owner;
 import io.harness.rule.OwnerRule;
+import io.harness.setupusage.InfrastructureEntitySetupUsageHelper;
 
+import com.google.protobuf.StringValue;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
 public class InfraStructureDefinitionVisitorHelperTest extends CategoryTest {
+  @Rule public MockitoRule mockitoRule = MockitoJUnit.rule();
   String accountId = "acc";
   String orgId = "org";
   String projectId = "proj";
 
-  InfraStructureDefinitionVisitorHelper visitorHelper = new InfraStructureDefinitionVisitorHelper();
+  @Mock InfrastructureEntityService infrastructureEntityService;
+
+  @Mock InfrastructureEntitySetupUsageHelper infrastructureEntitySetupUsageHelper;
+
+  @InjectMocks InfraStructureDefinitionVisitorHelper visitorHelper;
+
   @Test
   @Owner(developers = OwnerRule.YOGESH)
   @Category(UnitTests.class)
@@ -122,6 +145,89 @@ public class InfraStructureDefinitionVisitorHelperTest extends CategoryTest {
     assertThat(infraDefRef.getOrgIdentifier().getValue()).isEmpty();
     assertThat(infraDefRef.getProjectIdentifier().getValue()).isEmpty();
     assertThat(infraDefRef.getEnvIdentifier().getValue()).isEqualTo("my_env");
+  }
+
+  @Test
+  @Owner(developers = OwnerRule.HINGER)
+  @Category(UnitTests.class)
+  public void testAddReference_WithInfraInputs0() {
+    Map<String, Object> inputs = new LinkedHashMap<>();
+    inputs.put("identifier", "UAT_INFRA");
+    inputs.put("type", "KubernetesDirect");
+    inputs.put("__uuid", "UUID");
+
+    Map<String, String> kubernetesSpec = new LinkedHashMap<>();
+    kubernetesSpec.put("connectorRef", "testConn");
+    kubernetesSpec.put("namespace", "<+input>");
+
+    inputs.put("spec", kubernetesSpec);
+
+    ParameterField<Map<String, Object>> inputsParam = ParameterField.createValueField(inputs);
+
+    InfrastructureEntity infrastructureEntity = InfrastructureEntity.builder()
+                                                    .accountId(accountId)
+                                                    .orgIdentifier(orgId)
+                                                    .projectIdentifier(projectId)
+                                                    .envIdentifier("envRef")
+                                                    .identifier("infra1")
+                                                    .yaml("infrastructureDefinition:\n"
+                                                        + "  name: infra1\n"
+                                                        + "  identifier: infra1\n"
+                                                        + "  description: \"\"\n"
+                                                        + "  tags: {}\n"
+                                                        + "  orgIdentifier: org\n"
+                                                        + "  projectIdentifier: proj\n"
+                                                        + "  environmentRef: my_env\n"
+                                                        + "  deploymentType: Kubernetes\n"
+                                                        + "  type: KubernetesDirect\n"
+                                                        + "  spec:\n"
+                                                        + "    connectorRef: <+input>\n"
+                                                        + "    namespace: <+input>\n"
+                                                        + "    releaseName: release-<+INFRA_KEY>\n"
+                                                        + "  allowSimultaneousDeployments: false\n")
+                                                    .build();
+
+    doReturn(Optional.of(infrastructureEntity))
+        .when(infrastructureEntityService)
+        .get(eq(accountId), eq(orgId), eq(projectId), eq("my_env"), eq("infra1"));
+
+    Set<EntityDetailProtoDTO> setEntityDetail = new HashSet<>();
+    Map<String, String> metadataMap = new HashMap<>();
+    metadataMap.put("fqn", "infrastructureDefinition.spec.connectorRef");
+    metadataMap.put("expression", "<+input>");
+
+    EntityDetailProtoDTO entityDetailProtoDTO = EntityDetailProtoDTO.newBuilder()
+                                                    .setType(EntityTypeProtoEnum.CONNECTORS)
+                                                    .setIdentifierRef(IdentifierRefProtoDTO.newBuilder()
+                                                                          .setIdentifier(StringValue.of("<+input>"))
+                                                                          .putAllMetadata(metadataMap))
+                                                    .build();
+    setEntityDetail.add(entityDetailProtoDTO);
+
+    doReturn(setEntityDetail)
+        .when(infrastructureEntitySetupUsageHelper)
+        .getAllReferredEntities(eq(infrastructureEntity));
+
+    Set<EntityDetailProtoDTO> result =
+        visitorHelper.addReference(InfraStructureDefinitionYaml.builder()
+                                       .identifier(ParameterField.createValueField("infra1"))
+                                       .inputs(inputsParam)
+                                       .build(),
+            accountId, orgId, projectId, Map.of("envRef", "my_env"));
+
+    // Infra and Connector
+    assertThat(result).hasSize(2);
+
+    Iterator<EntityDetailProtoDTO> resultIterator = result.iterator();
+    EntityDetailProtoDTO infraEntityDetail = resultIterator.next();
+    assertThat(infraEntityDetail.getType()).isEqualTo(EntityTypeProtoEnum.INFRASTRUCTURE);
+    verifyInfraDefRef(infraEntityDetail.getInfraDefRef());
+    assertThat(infraEntityDetail.getInfraDefRef().getEnvIdentifier().getValue()).isEqualTo("my_env");
+
+    EntityDetailProtoDTO connectorEntityDetail = resultIterator.next();
+    assertThat(connectorEntityDetail.getType()).isEqualTo(EntityTypeProtoEnum.CONNECTORS);
+    // connector ref provided in infra inputs
+    assertThat(connectorEntityDetail.getIdentifierRef().getIdentifier().getValue()).isEqualTo("testConn");
   }
 
   private void verifyInfraDefRef(InfraDefinitionReferenceProtoDTO infraDefRef) {
