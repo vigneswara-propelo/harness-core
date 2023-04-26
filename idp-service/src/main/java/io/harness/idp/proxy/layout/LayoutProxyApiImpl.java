4a30ae4eef9782b93b078dbf24a5078ffb15e017
Sathish <sathish.soundarapandian@harness.io>
2023-04-26 17:18:28 +0530

    feat: [IDP-426]: Error handling for proxied calls, Config change for onboardingGenerateYaml description (#47005)

--- idp-service/src/main/java/io/harness/idp/proxy/layout/LayoutProxyApiImpl.java
+++ idp-service/src/main/java/io/harness/idp/proxy/layout/LayoutProxyApiImpl.java
@@ -16,6 +16,7 @@ import io.harness.accesscontrol.NGAccessControlCheck;
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.clients.BackstageResourceClient;
+import io.harness.eraro.ResponseMessage;
 import io.harness.security.annotations.NextGenManagerAuth;
 import io.harness.spec.server.idp.v1.LayoutProxyApi;
 import io.harness.spec.server.idp.v1.model.LayoutIngestRequest;
@@ -27,50 +28,94 @@ import javax.ws.rs.core.Response;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @OwnedBy(HarnessTeam.IDP)
 @NextGenManagerAuth
 @AllArgsConstructor(onConstructor = @__({ @Inject }))
+@Slf4j
 public class LayoutProxyApiImpl implements LayoutProxyApi {
   BackstageResourceClient backstageResourceClient;
 
   @Override
   @NGAccessControlCheck(resourceType = IDP_RESOURCE_TYPE, permission = IDP_PERMISSION)
   public Response createLayout(@Valid LayoutRequest body, @AccountIdentifier String harnessAccount) {
-    Object entity = getGeneralResponse(backstageResourceClient.createLayout(body, harnessAccount));
-    return Response.ok(entity).build();
+    try {
+      Object entity = getGeneralResponse(backstageResourceClient.createLayout(body, harnessAccount));
+      return Response.ok(entity).build();
+    } catch (Exception ex) {
+      log.error("Error in createLayout - account = {}, error = {}", harnessAccount, ex.getMessage(), ex);
+      return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+          .entity(ResponseMessage.builder().message(ex.getMessage()).build())
+          .build();
+    }
   }
 
   @Override
   @NGAccessControlCheck(resourceType = IDP_RESOURCE_TYPE, permission = IDP_PERMISSION)
   public Response deleteLayout(@Valid LayoutRequest body, @AccountIdentifier String harnessAccount) {
-    Object entity = getGeneralResponse(backstageResourceClient.deleteLayout(body, harnessAccount));
-    return Response.ok(entity).build();
+    try {
+      Object entity = getGeneralResponse(backstageResourceClient.deleteLayout(body, harnessAccount));
+      return Response.ok(entity).build();
+    } catch (Exception ex) {
+      log.error("Error in deleteLayout - account = {}, error = {}", harnessAccount, ex.getMessage(), ex);
+      return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+          .entity(ResponseMessage.builder().message(ex.getMessage()).build())
+          .build();
+    }
   }
 
   @Override
   public Response getAllLayouts(String harnessAccount) {
-    Object entity = getGeneralResponse(backstageResourceClient.getAllLayouts(harnessAccount));
-    return Response.ok(entity).build();
+    try {
+      Object entity = getGeneralResponse(backstageResourceClient.getAllLayouts(harnessAccount));
+      return Response.ok(entity).build();
+    } catch (Exception ex) {
+      log.error("Error in getAllLayouts - account = {}, error = {}", harnessAccount, ex.getMessage(), ex);
+      return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+          .entity(ResponseMessage.builder().message(ex.getMessage()).build())
+          .build();
+    }
   }
 
   @Override
   public Response getLayout(String layoutIdentifier, String harnessAccount) {
-    Object entity = getGeneralResponse(backstageResourceClient.getLayout(harnessAccount, layoutIdentifier));
-    return Response.ok(entity).build();
+    try {
+      Object entity = getGeneralResponse(backstageResourceClient.getLayout(harnessAccount, layoutIdentifier));
+      return Response.ok(entity).build();
+    } catch (Exception ex) {
+      log.error("Error in getLayout - account = {}, error = {}", harnessAccount, ex.getMessage(), ex);
+      return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+          .entity(ResponseMessage.builder().message(ex.getMessage()).build())
+          .build();
+    }
   }
 
   @Override
   public Response getLayoutHealth(String harnessAccount) {
-    Object entity = getGeneralResponse(backstageResourceClient.getHealth(harnessAccount));
-    return Response.ok(entity).build();
+    try {
+      Object entity = getGeneralResponse(backstageResourceClient.getHealth(harnessAccount));
+      return Response.ok(entity).build();
+    } catch (Exception ex) {
+      log.error("Error in getLayoutHealth - account = {}, error = {}", harnessAccount, ex.getMessage(), ex);
+      return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+          .entity(ResponseMessage.builder().message(ex.getMessage()).build())
+          .build();
+    }
   }
 
   @Override
   @NGAccessControlCheck(resourceType = IDP_RESOURCE_TYPE, permission = IDP_PERMISSION)
   public Response layoutIngest(@Valid LayoutIngestRequest body, @AccountIdentifier String harnessAccount) {
-    Object entity = getGeneralResponse(backstageResourceClient.ingestLayout(body, harnessAccount));
-    return Response.ok(entity).build();
+    try {
+      Object entity = getGeneralResponse(backstageResourceClient.ingestLayout(body, harnessAccount));
+      return Response.ok(entity).build();
+    } catch (Exception ex) {
+      log.error("Error in layoutIngest - account = {}, error = {}", harnessAccount, ex.getMessage(), ex);
+      return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+          .entity(ResponseMessage.builder().message(ex.getMessage()).build())
+          .build();
+    }
   }
 }
