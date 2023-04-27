1330027e8e36fe0409a21e48697edc68747747cd
Udham Singh <udham.singh@harness.io>
2023-04-27 15:30:52 +0530

    feat: [BT-3728]: Skipping Sonar Scan for no java files. (#47050)
    * feat: [BT-3728]: Skipping Sonar Scan for no java files.
    
    * feat: [BT-3728]: Skipping Sonar Scan for no java files.
    
    * feat: [BT-3728]: Skipping Sonar Scan for no java files.

--- scripts/jenkins/check-file.sh
+++ scripts/jenkins/check-file.sh
@@ -17,6 +17,7 @@ export SourceBranch=$(echo "${ghprbSourceBranch}")
 
 PR_Name=("SmartPRChecks-Functional_tests" "SmartPRChecks-Functional_tests1" "SmartPRChecks-PMD" "SmartPRChecks-codebasehashcheck")
 PR_TI=("TIAll-ut0" "TIAll-ut1" "TIAll-ut2" "TIAll-ut3" "TIAll-ut4" "TIAll-JavaUnitTests5" "TIAll-JavaUnitTests6" "TIAll-JavaUnitTests7" "TIAll-JavaUnitTests8" "TIAll-JavaUnitTests9")
+SONAR_SCAN="SonarQube Code Analysis"
 
 merge_summary=""
 bazelignore_array=($(cat bazelignore))
@@ -52,11 +53,12 @@ function print_log() {
   echo "Compilation is Not Required as files/folders are added in bazelignore"
   echo "Also marking the heavy checks NotRequired!"
   for checks in "${PR_Name[@]}"; do
-    printf "$checks\t NotRequired \n"
+    printf "${checks}\t NotRequired \n"
   done
   for checks in "${PR_TI[@]}"; do
-    printf "$checks\t NotRequired \n"
+    printf "${checks}\t NotRequired \n"
   done
+  printf "${SONAR_SCAN}\t NotRequired \n"
   echo "Merge Summary:" "${merge_summary[@]}"
   printf "\n\n\n##################################\n\n"
   export COMPILE="false"
@@ -87,31 +89,40 @@ function send_webhook() {
 # function call to check if codeformat check is required or not
   CodeformatRequired
 
-  for i in "${PR_Name[@]}"; do
+  for check in "${PR_Name[@]}"; do
     curl --silent --output /dev/null --location --request POST 'https://api.github.com/repos/harness/harness-core/statuses/'"$COMMIT_SHA"'' \
       --header 'Accept: application/vnd.github+json' \
       --header 'Authorization: Bearer '"$BOT_PWD"'' \
       --header 'Content-Type: application/json' \
       --data-raw '{
-"state": "success",
-"target_url": "https://app.harness.io/ng/#/account/VRuJ8-dqQH6QZgAtoBr66g/ci/orgs/default/projects/PRCHECKS/pipelines/SmartPRChecks/executions/'"$Execution_Id"'/pipeline",
-"description": "Skipped the check as compilation is not required!",
-"context": "'"$i"'"
-}'
+        "state": "success",
+        "target_url": "https://app.harness.io/ng/#/account/VRuJ8-dqQH6QZgAtoBr66g/ci/orgs/default/projects/PRCHECKS/pipelines/SmartPRChecks/executions/'"$Execution_Id"'/pipeline",
+        "description": "Skipped the check as compilation is not required!",
+        "context": "'"${check}"'"
+        }'
   done
-  for i in "${PR_TI[@]}"; do
+  for check in "${PR_TI[@]}"; do
       curl --silent --output /dev/null --location --request POST 'https://api.github.com/repos/harness/harness-core/statuses/'"$COMMIT_SHA"'' \
         --header 'Accept: application/vnd.github+json' \
         --header 'Authorization: Bearer '"$BOT_PWD"'' \
         --header 'Content-Type: application/json' \
         --data-raw '{
-  "state": "success",
-  "target_url": "https://app.harness.io/ng/#/account/VRuJ8-dqQH6QZgAtoBr66g/ci/orgs/default/projects/PRCHECKS/pipelines/TIAll/executions/'"$Execution_Id"'/pipeline",
-  "description": "Skipped the check as compilation is not required!",
-  "context": "'"$i"'"
-  }'
+          "state": "success",
+          "target_url": "https://app.harness.io/ng/#/account/VRuJ8-dqQH6QZgAtoBr66g/ci/orgs/default/projects/PRCHECKS/pipelines/TIAll/executions/'"$Execution_Id"'/pipeline",
+          "description": "Skipped the check as compilation is not required!",
+          "context": "'"${check}"'"
+          }'
     done
-
+  curl --silent --output /dev/null --location --request POST 'https://api.github.com/repos/harness/harness-core/statuses/'"$COMMIT_SHA"'' \
+      --header 'Accept: application/vnd.github+json' \
+      --header 'Authorization: Bearer '"$BOT_PWD"'' \
+      --header 'Content-Type: application/json' \
+      --data-raw '{
+        "state": "success",
+        "target_url": "https://app.harness.io/ng/#/account/VRuJ8-dqQH6QZgAtoBr66g/ci/orgs/default/projects/PRCHECKS/pipelines/SonarPR/executions/'"$Execution_Id"'/pipeline",
+        "description": "Skipped the check as compilation is not required!",
+        "context": "'"${SONAR_SCAN}"'"
+        }'
 }
 
 if [ -z "${merge_summary}" ]; then
