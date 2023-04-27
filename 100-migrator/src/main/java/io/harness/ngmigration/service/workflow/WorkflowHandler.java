9d622aa2402ca786eebf8d2e6774273ab4883a30
Vaibhav Singhal <vaibhav.si@harness.io>
2023-04-27 20:57:36 +0530

    fix: [CDS-59459]: multi stage templatized phases same svc infra (#47064)
    * fix: [CDS-59459]: creating stage template when multi-service workflows are templatized with same service and infra
    
    * fix: [CDS-59459]: checkstyle fix

--- 100-migrator/src/main/java/io/harness/ngmigration/service/workflow/WorkflowHandler.java
+++ 100-migrator/src/main/java/io/harness/ngmigration/service/workflow/WorkflowHandler.java
@@ -7,6 +7,7 @@
 
 package io.harness.ngmigration.service.workflow;
 
+import static io.harness.data.structure.EmptyPredicate.isEmpty;
 import static io.harness.data.structure.EmptyPredicate.isNotEmpty;
 import static io.harness.ngmigration.utils.MigratorUtility.RUNTIME_INPUT;
 import static io.harness.ngmigration.utils.MigratorUtility.getRollbackPhases;
@@ -72,6 +73,7 @@ import software.wings.beans.CanaryOrchestrationWorkflow;
 import software.wings.beans.FailureStrategy;
 import software.wings.beans.GraphNode;
 import software.wings.beans.PhaseStep;
+import software.wings.beans.TemplateExpression;
 import software.wings.beans.Variable;
 import software.wings.beans.VariableType;
 import software.wings.beans.Workflow;
@@ -972,23 +974,53 @@ public abstract class WorkflowHandler {
     }
     List<WorkflowPhase> phases = getPhases(workflow);
 
-    // Case where all the phases uses same service and infra and are not templatized
-    // Later need to handle templatization case as well.
-    if (isNotEmpty(phases)) {
-      Set<String> serviceIds = new HashSet<>();
-      Set<String> infraDefIds = new HashSet<>();
+    if (isEmpty(phases)) {
+      return true;
+    }
 
-      for (WorkflowPhase workflowPhase : phases) {
-        if (workflowPhase.checkServiceTemplatized() || workflowPhase.checkInfraDefinitionTemplatized()) {
-          return false;
+    return areAllPhasesNonTemplatizedAndHaveSameSvcInfra(phases) || areAllPhasesTemplatizedAndHaveSameSvcInfra(phases);
+  }
+
+  private boolean areAllPhasesNonTemplatizedAndHaveSameSvcInfra(List<WorkflowPhase> phases) {
+    Set<String> serviceIds = new HashSet<>();
+    Set<String> infraDefIds = new HashSet<>();
+
+    for (WorkflowPhase workflowPhase : phases) {
+      if (workflowPhase.checkServiceTemplatized() || workflowPhase.checkInfraDefinitionTemplatized()) {
+        return false;
+      }
+      String serviceId = workflowPhase.getServiceId();
+      String infraDefinitionId = workflowPhase.getInfraDefinitionId();
+      serviceIds.add(serviceId);
+      infraDefIds.add(infraDefinitionId);
+      if (serviceIds.size() > 1 || infraDefIds.size() > 1) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private boolean areAllPhasesTemplatizedAndHaveSameSvcInfra(List<WorkflowPhase> phases) {
+    Set<String> serviceExpressions = new HashSet<>();
+    Set<String> infraDefExpressions = new HashSet<>();
+
+    for (WorkflowPhase workflowPhase : phases) {
+      if (!workflowPhase.checkServiceTemplatized() || !workflowPhase.checkInfraDefinitionTemplatized()) {
+        return false;
+      }
+      List<TemplateExpression> templateExpressions =
+          CollectionUtils.emptyIfNull(workflowPhase.getTemplateExpressions());
+
+      for (TemplateExpression templateExpression : templateExpressions) {
+        if ("serviceId".equals(templateExpression.getFieldName())) {
+          serviceExpressions.add(templateExpression.getExpression());
         }
-        String serviceId = workflowPhase.getServiceId();
-        String infraDefinitionId = workflowPhase.getInfraDefinitionId();
-        serviceIds.add(serviceId);
-        infraDefIds.add(infraDefinitionId);
-        if (serviceIds.size() > 1 || infraDefIds.size() > 1) {
-          return false;
+        if ("infraDefinitionId".equals(templateExpression.getFieldName())) {
+          infraDefExpressions.add(templateExpression.getExpression());
         }
+      }
+      if (serviceExpressions.size() > 1 || infraDefExpressions.size() > 1) {
+        return false;
       }
     }
     return true;
