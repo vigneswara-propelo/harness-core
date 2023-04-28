43e45b121064cc2f4da484cac4d1c579438b6296
Diptiman Adak <diptiman.adak@harness.io>
2023-04-28 14:56:56 +0530

    [feat]: [BT-3615]: Removing unused files for manager (#46858)

--- 360-cg-manager/container/scripts/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Copyright 2021 Harness Inc. All rights reserved.
-# Use of this source code is governed by the PolyForm Shield 1.0.0 license
-# that can be found in the licenses directory at the root of this repository, also available at
-# https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
-
-source set_default_variables.sh
-bash ./replace_configs.sh
-bash ./start_process.sh
