df3c9c613cbf4886fb859c3c6ebeb450f04094dc
Abhay Ganvir <abhay.ganvir@harness.io>
2023-05-04 11:34:42 +0530

    [feat]: [BT-3674]: remove build number upgrade for pre_qa branch (#47215)
    * [feat]: [BT-3674]: remove build number upgrade for pre_qa branch
    
    * [feat]: [BT-3674]: skip message for pre_qa branch check
    
    * [feat]: [BT-3674]: skip message for pre_qa branch check
    
    * [feat]: [BT-3674]: skip message for pre_qa branch check
    
    * [feat]: [BT-3674]: skip message for pre_qa branch check
    
    * [feat]: [BT-3674]: skip message for pre_qa branch check

--- scripts/jenkins/buildMetadataPRCheck.sh
+++ scripts/jenkins/buildMetadataPRCheck.sh
@@ -6,13 +6,30 @@
 
 set -xe
 
-export BRANCH_PREFIX=`echo ${ghprbTargetBranch} | sed 's/\(........\).*/\1/g'`
+export BRANCH_PREFIX=`echo ${ghprbTargetBranch} | sed 's/\(.*\/\).*/\1/'`
 echo "INFO: BRANCH_PREFIX=$BRANCH_PREFIX"
 
 service_folders=("pipeline-service" "access-control" "platform-service" "batch-processing" "ce-nextgen" "audit-event-streaming")
 
 #Need confirmation for below services reference path of build.properties
 #"260-delegate" "315-sto-manager" "debezium-service"
+
+# exit if the target branch is pre_qa and build.propeties file is updated
+if [ "${BRANCH_PREFIX}" = "pre_qa/" ]; then
+  BASE_SHA="$(git merge-base $COMMIT_SHA $BASE_COMMIT_SHA)"
+  merge_summary=( $(git diff --name-only $COMMIT_SHA..$BASE_SHA) )
+
+  for file_name in "${merge_summary[@]}"
+  do
+    if [[ "${file_name}" = "build.properties" ]]; then
+      echo -e "\033[0;31mFOR PRE_QA BRANCH, build.properties FILE SHOULD NOT BE UPDATED";
+      exit 1
+    fi
+  done
+else
+  echo "BRANCH IS NOT EQUAL TO PRE_QA/*"
+  echo "SKIPPING THE CHECK FOR PRE_QA HOTFIX"
+fi
 
 #Get path of build.properties file for dedicated services
 if [[ "${BRANCH_PREFIX}" != "release/" ]]; then
