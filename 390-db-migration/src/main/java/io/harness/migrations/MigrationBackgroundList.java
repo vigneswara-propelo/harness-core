c784779fe6c63b0b8e52523bc972fcb78aa5c011
Prateek Barapatre <Prateek.Barapatre@harness.io>
2023-05-07 10:14:03 +0530

    [feat]: [PL-32535]: add one-time migration for existing saml settings to add values for authenticationEnabled, friendlySAMLName, configuredFromNG (#47452)

--- 390-db-migration/src/main/java/io/harness/migrations/MigrationBackgroundList.java
+++ 390-db-migration/src/main/java/io/harness/migrations/MigrationBackgroundList.java
@@ -135,6 +135,7 @@ import io.harness.migrations.all.RemoveDeprecatedFieldsFromHarnessUserGroup;
 import io.harness.migrations.all.RemoveDuplicateUserGroupNameMigration;
 import io.harness.migrations.all.RemoveSupportEmailFromSalesContacts;
 import io.harness.migrations.all.RemoveUsageRestrictionForApplicationDefaultsMigration;
+import io.harness.migrations.all.SamlSSOSettingsConfiguredFromNGAuthenticationEnabledMigration;
 import io.harness.migrations.all.ScheduleSegmentPublishJob;
 import io.harness.migrations.all.SendInviteUrlForAllUserInvites;
 import io.harness.migrations.all.SetAccountIdProvisioners;
@@ -426,6 +427,7 @@ public class MigrationBackgroundList {
         .add(Pair.of(247, BackFillingKeywordsOnWorkflowExecutionCollection.class))
         .add(Pair.of(248, UpdateK8sWatchInvalidStatePerpetualTaskMigration.class))
         .add(Pair.of(249, EncryptDelegateTokenMigration.class))
+        .add(Pair.of(250, SamlSSOSettingsConfiguredFromNGAuthenticationEnabledMigration.class))
         .build();
   }
 }
