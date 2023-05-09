2762978bfc08021be0803d8465a57da2949d3480
Inderpreet Chera <inderpreet.chera@harness.io>
2023-05-09 14:28:50 +0530

    feat: [SSCA-169]: modify attestation section in orchestration step (#47483)
    * feat: [SSCA-169]: modify attestation section in orchestration step
    
    * feat: [SSCA-169]: fix UTs

--- 332-ssca/commons/src/main/java/io/harness/ssca/beans/Attestation.java
+++ 332-ssca/commons/src/main/java/io/harness/ssca/beans/Attestation.java
@@ -7,13 +7,24 @@
 
 package io.harness.ssca.beans;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import static com.fasterxml.jackson.annotation.JsonTypeInfo.As.EXTERNAL_PROPERTY;
+import static com.fasterxml.jackson.annotation.JsonTypeInfo.Id.NAME;
+
+import io.harness.ssca.beans.attestation.AttestationSpec;
+import io.harness.ssca.beans.attestation.AttestationType;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class Attestation {
-  @NotNull @NotBlank private String privateKey;
+  @Deprecated private String privateKey;
+  private AttestationType type;
+
+  @JsonProperty("spec")
+  @JsonTypeInfo(use = NAME, property = "type", include = EXTERNAL_PROPERTY, visible = true)
+  private AttestationSpec attestationSpec;
 }
