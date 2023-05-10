14ab6d942bd1cd4e8ab7608417270e8968d67128
Raghav-Murali <raghavendra.murali@harness.io>
2023-05-10 13:14:49 -0700

    feat: [PL-31752]: Enhance the HTTP error handling for NGRestUtils (#45390)

--- 970-ng-commons/src/main/java/io/harness/remote/client/NGRestUtils.java
+++ 970-ng-commons/src/main/java/io/harness/remote/client/NGRestUtils.java
@@ -9,8 +9,10 @@ package io.harness.remote.client;
 
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
+import io.harness.eraro.ErrorCode;
 import io.harness.exception.InvalidRequestException;
 import io.harness.exception.UnexpectedException;
+import io.harness.exception.ngexception.ErrorMetadataDTO;
 import io.harness.ng.core.dto.ErrorDTO;
 import io.harness.ng.core.dto.GenericErrorMessage;
 import io.harness.ng.core.dto.ResponseDTO;
@@ -27,6 +29,7 @@ import net.jodah.failsafe.FailsafeException;
 import net.jodah.failsafe.RetryPolicy;
 import net.jodah.failsafe.event.ExecutionAttemptedEvent;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.http.HttpStatus;
 import retrofit2.Call;
 import retrofit2.Response;
 
@@ -104,8 +107,11 @@ public class NGRestUtils {
     try {
       GenericErrorMessage restResponse =
           JsonUtils.asObject(response.errorBody().string(), new TypeReference<GenericErrorMessage>() {});
-      errorMessage = restResponse.getErrorMessage();
-      throw new InvalidRequestException(StringUtils.isEmpty(errorMessage) ? defaultErrorMessage : errorMessage);
+      int httpStatusCode = response.code();
+      errorMessage =
+          StringUtils.isEmpty(restResponse.getErrorMessage()) ? defaultErrorMessage : restResponse.getErrorMessage();
+      InvalidRequestException invalidRequestException = buildErrorResponse(null, httpStatusCode, errorMessage);
+      throw invalidRequestException;
     } catch (InvalidRequestException e) {
       throw e;
     } catch (Exception e) {
@@ -127,9 +133,11 @@ public class NGRestUtils {
     String errorMessage = "";
     try {
       ErrorDTO restResponse = JsonUtils.asObject(response.errorBody().string(), new TypeReference<ErrorDTO>() {});
-      errorMessage = restResponse.getMessage();
-      throw new InvalidRequestException(
-          StringUtils.isEmpty(errorMessage) ? defaultErrorMessage : errorMessage, restResponse.getMetadata());
+      int httpStatusCode = response.code();
+      errorMessage = StringUtils.isEmpty(restResponse.getMessage()) ? defaultErrorMessage : restResponse.getMessage();
+      InvalidRequestException invalidRequestException =
+          buildErrorResponse(restResponse.getMetadata(), httpStatusCode, errorMessage);
+      throw invalidRequestException;
     } catch (InvalidRequestException e) {
       throw e;
     } catch (Exception e) {
@@ -185,5 +193,56 @@ public class NGRestUtils {
   private static boolean isRetryableHttpCode(int httpCode) {
     // https://stackoverflow.com/questions/51770071/what-are-the-http-codes-to-automatically-retry-the-request
     return httpCode == 408 || httpCode == 502 || httpCode == 503 || httpCode == 504;
+  }
+
+  private static InvalidRequestException buildErrorResponse(ErrorMetadataDTO metadataDTO, int code, String message) {
+    ErrorCode errorCode = ErrorCode.INVALID_REQUEST;
+    String errorMessage = "";
+    switch (code) {
+      case HttpStatus.SC_BAD_REQUEST:
+        errorCode = ErrorCode.INVALID_FORMAT;
+        errorMessage = "HTTP Error Status (400 - Invalid Format) received";
+        break;
+      case HttpStatus.SC_UNAUTHORIZED:
+        errorCode = ErrorCode.INVALID_CREDENTIAL;
+        errorMessage = "HTTP Error Status (401 - Invalid Credential) received";
+        break;
+      case HttpStatus.SC_FORBIDDEN:
+        errorCode = ErrorCode.ACCESS_DENIED;
+        errorMessage = "HTTP Error Status (403 - Access Denied) received";
+        break;
+      case HttpStatus.SC_NOT_FOUND:
+        errorCode = ErrorCode.RESOURCE_NOT_FOUND;
+        errorMessage = "HTTP Error Status (404 - Resource Not Found) received";
+        break;
+      case HttpStatus.SC_INTERNAL_SERVER_ERROR:
+        errorCode = ErrorCode.HTTP_INTERNAL_SERVER_ERROR;
+        errorMessage = "HTTP Error Status (500 - Internal Server Error) received.";
+        break;
+      case HttpStatus.SC_BAD_GATEWAY:
+        errorCode = ErrorCode.HTTP_BAD_GATEWAY;
+        errorMessage = "HTTP Error Status (502 - Bad Gateway) received.";
+        break;
+      case HttpStatus.SC_SERVICE_UNAVAILABLE:
+        errorCode = ErrorCode.HTTP_SERVICE_UNAVAILABLE;
+        errorMessage = "HTTP Error Status (503 - Service Unavailable) received.";
+        break;
+      case HttpStatus.SC_GATEWAY_TIMEOUT:
+        errorCode = ErrorCode.HTTP_GATEWAY_TIMEOUT;
+        errorMessage = "HTTP Error Status (504 - Gateway Timeout) received.";
+        break;
+      default:
+        if (code >= 400 && code < 500) {
+          errorCode = ErrorCode.HTTP_CLIENT_ERROR_RESPONSE;
+          errorMessage = "HTTP Error Status (" + code + " - Client Error Response) received.";
+        } else if (code >= 500) {
+          errorCode = ErrorCode.HTTP_SERVER_ERROR_RESPONSE;
+          errorMessage = "HTTP Error Status (" + code + " - Server Error Response) received.";
+        }
+        break;
+    }
+    InvalidRequestException invalidRequestException =
+        new InvalidRequestException(errorMessage + " " + message, metadataDTO, errorCode);
+    return invalidRequestException;
   }
 }
