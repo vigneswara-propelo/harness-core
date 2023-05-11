fe9b28d93b4625f910814c25e9ec7d9186c304bc
utkarshchoubey-harness <utkarsh.choubey@harness.io>
2023-05-11 10:24:04 +0530

    feat: [CDS-59323]: Remove CDS_PROPAGATE_STAGE_TEMPLATE FF (#47597)

--- 956-feature-flag-beans/src/main/java/io/harness/beans/FeatureName.java
+++ 956-feature-flag-beans/src/main/java/io/harness/beans/FeatureName.java
@@ -663,7 +663,6 @@ public enum FeatureName {
   PLG_ENABLE_CROSS_GENERATION_ACCESS("Enables cross generation access", GTM),
   CDS_SERVICE_OVERRIDES_2_0("Revamped experience service and environment overrides in NG", HarnessTeam.CDC),
   CDS_USE_OLD_SERVICE_V1("Feature flag to use service v1. NG_SVC_ENV_REDESIGN will be deprecated", HarnessTeam.CDC),
-  CDS_PROPAGATE_STAGE_TEMPLATE("Allow user to propagate service in a templatized stage in pipeline", HarnessTeam.CDC),
   CDS_NG_TRIGGER_EXECUTION_REFACTOR(
       "Refactor trigger execution to use same logic used in manual execution", HarnessTeam.SPG),
   CDS_K8S_SOCKET_CAPABILITY_CHECK_NG(
