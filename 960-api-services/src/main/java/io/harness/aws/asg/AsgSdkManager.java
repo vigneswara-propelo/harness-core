877bcedeb3bb17d36198d42a7cfe9ee20387bbc1
Vitalie Safronovici <vitalie.safronovici@harness.io>
2023-05-16 13:27:23 +0300

    fix: [CDS-69311]: add support for multiple target groups (#47837)

--- 960-api-services/src/main/java/io/harness/aws/asg/AsgSdkManager.java
+++ 960-api-services/src/main/java/io/harness/aws/asg/AsgSdkManager.java
@@ -104,7 +104,6 @@ import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -813,7 +812,9 @@ public class AsgSdkManager {
           .filter(rule -> rule.ruleArn().equalsIgnoreCase(listenerRuleArn))
           .map(Rule::actions)
           .flatMap(Collection::stream)
-          .map(Action::targetGroupArn)
+          .map(action -> action.forwardConfig().targetGroups())
+          .flatMap(Collection::stream)
+          .map(TargetGroupTuple::targetGroupArn)
           .collect(Collectors.toList());
     }
     throw new InvalidRequestException(
@@ -826,7 +827,7 @@ public class AsgSdkManager {
       return true;
     }
 
-    Set<String> instanceIdsRegistered = new HashSet<>();
+    boolean allTargetsRegistered = true;
 
     for (String targetGroupARN : targetGroupARNList) {
       DescribeTargetHealthRequest describeTargetHealthRequest =
@@ -836,19 +837,32 @@ public class AsgSdkManager {
           elbV2Client.describeTargetHealth(awsInternalConfig, describeTargetHealthRequest, region);
 
       List<TargetHealthDescription> healthDescriptions = response.targetHealthDescriptions();
-      if (isNotEmpty(healthDescriptions)) {
-        instanceIdsRegistered.addAll(
-            healthDescriptions.stream()
-                .filter(description -> description.targetHealth().stateAsString().equalsIgnoreCase("Healthy"))
-                .map(description -> description.target().id())
-                .filter(targetIds::contains)
-                .collect(toSet()));
-      }
+      Set<String> healthyTargetIdsRegistered = getHealthyTargetIdsRegistered(healthDescriptions, targetIds, true);
+      Set<String> unhealthyTargetIdsRegistered = getHealthyTargetIdsRegistered(healthDescriptions, targetIds, false);
+      int totalNrOfTargetIds = healthyTargetIdsRegistered.size() + unhealthyTargetIdsRegistered.size();
+
+      info("For targetGroup %s registered and in healthy state are [%d] out of [%d] targets", targetGroupARN,
+          healthyTargetIdsRegistered.size(), totalNrOfTargetIds);
+
+      allTargetsRegistered = allTargetsRegistered && !healthyTargetIdsRegistered.isEmpty()
+          && healthyTargetIdsRegistered.size() == totalNrOfTargetIds;
     }
 
-    info("[%d] out of [%d] targets registered and in healthy state", instanceIdsRegistered.size(), targetIds.size());
-    return instanceIdsRegistered.containsAll(targetIds);
+    return allTargetsRegistered;
   }
+
+  private Set<String> getHealthyTargetIdsRegistered(
+      List<TargetHealthDescription> healthDescriptions, List<String> targetIds, boolean isHealthy) {
+    Predicate<TargetHealthDescription> healthyPredicate =
+        decr -> decr.targetHealth().stateAsString().equalsIgnoreCase("Healthy");
+
+    return healthDescriptions.stream()
+        .filter(isHealthy ? healthyPredicate : Predicate.not(healthyPredicate))
+        .map(description -> description.target().id())
+        .filter(targetIds::contains)
+        .collect(toSet());
+  }
+
   public String describeBGTags(String asgName) {
     Filter filter1 = new Filter().withName("auto-scaling-group").withValues(asgName);
     Filter filter2 = new Filter().withName("key").withValues(BG_VERSION);
