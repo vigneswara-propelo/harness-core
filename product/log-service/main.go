ae0eb30e82d16322497331b3e684f2a12f1de0a9
Soumyajit Das <soumyajit.das@harness.io>
2023-05-17 17:08:01 +0530

    [feat]: [CI-7622]: Add thread dump on log service pod restart (#46122)

--- product/log-service/main.go
+++ product/log-service/main.go
@@ -7,7 +7,12 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"os/signal"
 	"runtime"
+	"runtime/pprof"
+	"syscall"
 
 	"github.com/harness/harness-core/product/log-service/cli"
 
@@ -29,5 +34,43 @@ func main() {
 		}
 	}()
 
+	go threadDump()
+
 	cli.Command()
+}
+
+func threadDump() {
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, os.Kill, syscall.SIGUSR1, syscall.SIGKILL, syscall.SIGTERM)
+
+	// Wait for a signal
+	<-sig
+
+	// Get a stack trace for each goroutine
+	buf := make([]byte, 1<<16)
+	length := runtime.Stack(buf, true)
+
+	podName := os.Getenv("POD_NAME")
+	// Write the stack trace to a file or log it
+	f, err := os.Create(podName + "thread_dump.txt")
+	if err != nil {
+		log.Println(err)
+	}
+	defer f.Close()
+
+	if _, err := f.Write(buf); err != nil {
+		log.Println(err)
+	}
+	log.Println("thread dump before exit = %s\n", buf[:length])
+
+	log.Println("generating heap dump for ", podName)
+
+	heap, err := os.Create(podName + "heap.dump")
+	if err != nil {
+		log.Println("Failed to create heap dump file: %v", err)
+	}
+	if err := pprof.WriteHeapProfile(heap); err != nil {
+		log.Println("Failed to write heap profile: %v", err)
+	}
+	log.Println("Heap dump generated for", podName)
 }
