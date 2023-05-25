ed3df6107bb2b0f7ee1034612749c9ea6d551734
Sahildeep Singh <sahildeep.singh@harness.io>
2023-05-26 04:35:10 +0530

    [fix]: [CCM-12607]: Bind CFServices in event service (#48386)

--- 350-event-server/src/main/java/io/harness/event/app/EventServiceModule.java
+++ 350-event-server/src/main/java/io/harness/event/app/EventServiceModule.java
@@ -9,7 +9,9 @@ package io.harness.event.app;
 
 import static io.harness.annotations.dev.HarnessTeam.PL;
 
+import io.harness.account.AccountClient;
 import io.harness.annotations.dev.OwnedBy;
+import io.harness.cf.CfClientModule;
 import io.harness.delegate.authenticator.DelegateTokenAuthenticatorImpl;
 import io.harness.event.MessageProcessorType;
 import io.harness.event.grpc.EventPublisherServerImpl;
@@ -21,11 +23,15 @@ import io.harness.event.service.impl.LastReceivedPublishedMessageRepositoryImpl;
 import io.harness.event.service.intfc.EventDataBulkWriteService;
 import io.harness.event.service.intfc.EventPublisherService;
 import io.harness.event.service.intfc.LastReceivedPublishedMessageRepository;
+import io.harness.ff.FeatureFlagService;
+import io.harness.ff.FeatureFlagServiceImpl;
 import io.harness.grpc.auth.DelegateAuthServerInterceptor;
 import io.harness.grpc.exception.GrpcExceptionMapper;
 import io.harness.grpc.exception.WingsExceptionGrpcMapper;
 import io.harness.grpc.server.GrpcServerExceptionHandler;
 import io.harness.grpc.server.GrpcServerModule;
+import io.harness.lock.PersistentLocker;
+import io.harness.lock.noop.PersistentNoopLocker;
 import io.harness.metrics.impl.DelegateMetricsServiceImpl;
 import io.harness.metrics.intfc.DelegateMetricsService;
 import io.harness.metrics.modules.MetricsModule;
@@ -35,6 +41,7 @@ import io.harness.security.DelegateTokenAuthenticator;
 import io.harness.service.impl.agent.mtls.AgentMtlsEndpointServiceReadOnlyImpl;
 import io.harness.service.intfc.AgentMtlsEndpointService;
 import io.harness.threading.ExecutorModule;
+import io.harness.time.TimeModule;
 
 import software.wings.dl.WingsMongoPersistence;
 import software.wings.dl.WingsPersistence;
@@ -53,11 +60,13 @@ import com.google.inject.Singleton;
 import com.google.inject.TypeLiteral;
 import com.google.inject.multibindings.MapBinder;
 import com.google.inject.multibindings.Multibinder;
+import com.google.inject.multibindings.OptionalBinder;
 import com.google.inject.name.Named;
 import io.grpc.BindableService;
 import io.grpc.ServerInterceptor;
 import java.util.Collections;
 import java.util.Set;
+import java.util.concurrent.Executors;
 
 @OwnedBy(PL)
 public class EventServiceModule extends AbstractModule {
@@ -118,6 +127,8 @@ public class EventServiceModule extends AbstractModule {
     install(new MetricsModule(OPEN_CENSUS_EXPORT_INTERVAL_MINUTES));
     install(ExecutorModule.getInstance());
     bind(MetricsPublisher.class).to(EventServiceMetricsPublisher.class).in(Scopes.SINGLETON);
+
+    bindCFServices();
   }
 
   @Provides
@@ -131,5 +142,21 @@ public class EventServiceModule extends AbstractModule {
   @Singleton
   public ServiceManager serviceManager(Set<Service> services) {
     return new ServiceManager(services);
+  }
+
+  /**
+   * This dependency only exists for the CFMigrationService which EventService will never use. However,
+   * since it is sharing the same module, we have to provide an implementation for the same. Hence, we are using
+   * NOOP over here.
+   */
+  private void bindCFServices() {
+    ExecutorModule.getInstance().setExecutorService(Executors.newCachedThreadPool());
+    install(ExecutorModule.getInstance());
+    install(TimeModule.getInstance());
+    install(CfClientModule.getInstance());
+
+    bind(PersistentLocker.class).to(PersistentNoopLocker.class).in(Scopes.SINGLETON);
+    OptionalBinder.newOptionalBinder(binder(), AccountClient.class);
+    bind(FeatureFlagService.class).to(FeatureFlagServiceImpl.class);
   }
 }
