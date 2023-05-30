f2627c5de8d03fefbee3341e7c240ff04abf1aa6
AnshuH <anshu.singla@harness.io>
2023-05-30 11:23:32 +0530

    fix: [BT-4133]: Updated echo message (#48453)
    * fix: [BT-4133]: Updated echo message
    
    * Update install.sh
    
    * Update install.sh
    
    * fix: [BT-4133]: Update message

--- toolset/git-hooks/install.sh
+++ toolset/git-hooks/install.sh
@@ -6,10 +6,10 @@
 
 pushd `dirname $0` > /dev/null && BASEDIR=$(pwd -L) && popd > /dev/null
 
-echo This script will install hooks that run scripts that could be updated without notice.
+echo "This script will install hooks that run scripts that could be updated without notice\n"
 
 while true; do
-    read -p "Do you wish to install these hooks?" yn
+    read -p "Do you wish to install these hooks, reply (yY/nN)? " yn
     case $yn in
         [Yy]* ) break;;
         [Nn]* ) exit;;
@@ -19,3 +19,4 @@ done
 
 cd $BASEDIR
 find . -type f -not -name "*.sh" -exec cp "{}" ../../.git/hooks \;
+echo "GitLeaks installed successfully!"
