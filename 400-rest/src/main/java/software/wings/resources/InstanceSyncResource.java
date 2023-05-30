82259011d7296a1f97e16924545222a4c57ee8bf
Aleksa Buha <aleksa.buha@harness.io>
2023-05-30 09:03:15 +0200

    fix: [CDS-70595]: change error logging to warning (#48499)

--- 400-rest/src/main/java/software/wings/resources/InstanceSyncResource.java
+++ 400-rest/src/main/java/software/wings/resources/InstanceSyncResource.java
@@ -72,7 +72,7 @@ public class InstanceSyncResource {
           instanceSyncResponsePublisher.fetchTaskDetails(perpetualTask, page, size, accountId);
       return Response.ok(details).build();
     } catch (Exception e) {
-      log.error(LOG_ERROR_TEMPLATE, perpetualTask, e);
+      log.warn(LOG_ERROR_TEMPLATE, perpetualTask, e);
     }
     return Response.status(Response.Status.EXPECTATION_FAILED).build();
   }
@@ -88,7 +88,7 @@ public class InstanceSyncResource {
          AutoLogContext ignore2 = new PerpetualTaskLogContext(perpetualTaskId, OVERRIDE_ERROR)) {
       instanceSyncResponsePublisher.publishInstanceSyncResponseToNG(accountId, perpetualTask, response);
     } catch (Exception e) {
-      log.error(LOG_ERROR_TEMPLATE, perpetualTask, e);
+      log.warn(LOG_ERROR_TEMPLATE, perpetualTask, e);
     }
     return new RestResponse<>(true);
   }
@@ -105,7 +105,7 @@ public class InstanceSyncResource {
          AutoLogContext ignore2 = new PerpetualTaskLogContext(perpetualTaskId, OVERRIDE_ERROR)) {
       instanceSyncServiceV2.processInstanceSyncResult(perpetualTask, response);
     } catch (Exception e) {
-      log.error(LOG_ERROR_TEMPLATE, perpetualTask, e);
+      log.warn(LOG_ERROR_TEMPLATE, perpetualTask, e);
     }
     return new RestResponse<>(true);
   }
@@ -120,7 +120,7 @@ public class InstanceSyncResource {
          AutoLogContext ignore2 = new PerpetualTaskLogContext(perpetualTaskId, OVERRIDE_ERROR)) {
       instanceHelper.processInstanceSyncResponseFromPerpetualTask(perpetualTask, response);
     } catch (Exception e) {
-      log.error(LOG_ERROR_TEMPLATE, perpetualTask, e);
+      log.warn(LOG_ERROR_TEMPLATE, perpetualTask, e);
     }
     return new RestResponse<>(true);
   }
@@ -136,7 +136,7 @@ public class InstanceSyncResource {
          AutoLogContext ignore2 = new PerpetualTaskLogContext(perpetualTaskId, OVERRIDE_ERROR)) {
       instanceSyncResponsePublisher.publishInstanceSyncResponseToNG(accountId, perpetualTask, response);
     } catch (Exception e) {
-      log.error(LOG_ERROR_TEMPLATE, perpetualTask, e);
+      log.warn(LOG_ERROR_TEMPLATE, perpetualTask, e);
     }
     return new RestResponse<>(true);
   }
@@ -153,7 +153,7 @@ public class InstanceSyncResource {
          AutoLogContext ignore2 = new PerpetualTaskLogContext(perpetualTaskId, OVERRIDE_ERROR)) {
       instanceSyncResponsePublisher.publishInstanceSyncResponseV2ToNG(accountId, perpetualTask, instanceSyncResponseV2);
     } catch (Exception e) {
-      log.error("Failed to process results for v2 perpetual task: [{}]", perpetualTask, e);
+      log.warn("Failed to process results for v2 perpetual task: [{}]", perpetualTask, e);
     }
     return new RestResponse<>(true);
   }
@@ -170,7 +170,7 @@ public class InstanceSyncResource {
          AutoLogContext ignore2 = new PerpetualTaskLogContext(perpetualTaskId, OVERRIDE_ERROR)) {
       instanceSyncServiceV2.processInstanceSyncResult(perpetualTask, response);
     } catch (Exception e) {
-      log.error(LOG_ERROR_TEMPLATE, perpetualTask, e);
+      log.warn(LOG_ERROR_TEMPLATE, perpetualTask, e);
     }
     return new RestResponse<>(true);
   }
@@ -187,7 +187,7 @@ public class InstanceSyncResource {
       InstanceSyncTrackedDeploymentDetails details = instanceSyncServiceV2.fetchTaskDetails(perpetualTask, accountId);
       return Response.ok(details).build();
     } catch (Exception e) {
-      log.error(LOG_ERROR_TEMPLATE, perpetualTask, e);
+      log.warn(LOG_ERROR_TEMPLATE, perpetualTask, e);
     }
     return Response.noContent().build();
   }
