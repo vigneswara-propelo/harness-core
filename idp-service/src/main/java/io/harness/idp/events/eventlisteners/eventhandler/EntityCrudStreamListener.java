ca2681e7fbbdc996cde2b39bb309360e88e17047
devesh-harness <devesh.mishra@harness.io>
2023-06-02 19:02:26 +0530

    feat: [IDP-469]: Multiple replica support for events in IDP service (#48660)
    * feat: [IDP-469]: Multiple replica support for events in IDP service
    
    * feat: [IDP-469]: Multiple replica support for events in IDP service
    
    * feat: [IDP-469]: Multiple replica support for events in IDP service
    
    * feat: [IDP-469]: Multiple replica support for events in IDP service
    
    * feat: [IDP-469]: Multiple replica support for events in IDP service
    
    * feat: [IDP-469]: Multiple replica support for events in IDP service
    
    * feat: [IDP-469]: Multiple replica support for events in IDP service
    
    * feat: [IDP-469]: Multiple replica support for events in IDP service
    
    * feat: [IDP-469]: Refactoring
    
    * feat: [IDP-469]: Refactoring
    
    * feat: [IDP-469]: Refactoring
    
    * feat: [IDP-469]: Review comments
    
    * feat: [IDP-469]: Review comments

--- idp-service/src/main/java/io/harness/idp/events/eventlisteners/eventhandler/EntityCrudStreamListener.java
+++ idp-service/src/main/java/io/harness/idp/events/eventlisteners/eventhandler/EntityCrudStreamListener.java
@@ -19,11 +19,14 @@ import io.harness.eventsframework.entity_crud.EntityChangeDTO;
 import io.harness.exception.InvalidRequestException;
 import io.harness.idp.events.eventlisteners.factory.EventMessageHandlerFactory;
 import io.harness.idp.events.eventlisteners.messagehandler.EventMessageHandler;
+import io.harness.lock.AcquiredLock;
+import io.harness.lock.redis.RedisPersistentLocker;
 import io.harness.logging.AutoLogContext;
 import io.harness.ng.core.event.MessageListener;
 
 import com.google.inject.Inject;
 import com.google.protobuf.InvalidProtocolBufferException;
+import java.time.Duration;
 import java.util.Map;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -33,6 +36,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class EntityCrudStreamListener implements MessageListener {
   EventMessageHandlerFactory eventMessageHandlerFactory;
+  RedisPersistentLocker redisLocker;
 
   @Override
   public boolean handleMessage(Message message) {
@@ -58,6 +62,7 @@ public class EntityCrudStreamListener implements MessageListener {
       }
       EntityChangeDTO entityChangeDTO;
       EventMessageHandler eventMessageHandler = eventMessageHandlerFactory.getEventMessageHandler(entityType);
+
       if (eventMessageHandler != null) {
         try {
           entityChangeDTO = EntityChangeDTO.parseFrom(message.getMessage().getData());
@@ -65,13 +70,44 @@ public class EntityCrudStreamListener implements MessageListener {
           throw new InvalidRequestException(
               String.format("Exception in unpacking EntityChangeDTO for id %s", messageId), e);
         }
+
         if (entityChangeDTO != null) {
-          eventMessageHandler.handleMessage(message, entityChangeDTO, action);
-          log.info("Completed processing the crud event with the id {}", messageId);
+          String accountIdentifier = entityChangeDTO.getAccountIdentifier().getValue();
+          String resourceIdentifier = entityChangeDTO.getIdentifier().getValue();
+
+          AcquiredLock lock;
+          while (true) {
+            lock =
+                this.redisLocker.tryToAcquireLock(accountIdentifier + "_" + resourceIdentifier, Duration.ofMinutes(1));
+            if (lock == null) {
+              log.info(
+                  "Lock not acquired for crud event for resource of type {} with identifier {} for account {} with message id - {}, waiting to acquire lock",
+                  entityType, resourceIdentifier, accountIdentifier, messageId);
+              Thread.sleep(1000);
+              continue;
+            }
+
+            log.info(
+                "Lock acquired for processing crud event for resource of type {} with identifier {} for account {} with message id - {}",
+                entityType, resourceIdentifier, accountIdentifier, messageId);
+
+            try {
+              eventMessageHandler.handleMessage(message, entityChangeDTO, action);
+              log.info("Completed processing the crud event with the id {}", messageId);
+            } catch (Exception e) {
+              log.error("Error in handling the crud event with the id {} for entity type {}", messageId, entityType, e);
+            } finally {
+              redisLocker.destroy(lock);
+              Thread.sleep(200);
+              log.debug(
+                  "Lock released for processing crud event for resource of type {} with identifier {} for account {} with message id - {}",
+                  entityType, resourceIdentifier, accountIdentifier, messageId);
+            }
+            return true;
+          }
         }
       }
 
-      return true;
     } catch (Exception e) {
       log.error("Error processing the crud event with the id {} for entity type {}", messageId,
           message.getMessage().getMetadataMap().get(ENTITY_TYPE), e);
