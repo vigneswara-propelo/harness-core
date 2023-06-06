9100f7278f70385f7abcd72fb244aa277ddfc551
Sathish <sathish.soundarapandian@harness.io>
2023-06-06 10:29:00 +0530

    feat: [IDP-624]: Harness CI/CD plugin multi environment support inside IDP for internal use-case in onboarding flow (#48820)

--- idp-service/src/test/java/io/harness/idp/onboarding/service/OnboardingServiceImplTest.java
+++ idp-service/src/test/java/io/harness/idp/onboarding/service/OnboardingServiceImplTest.java
@@ -115,7 +115,7 @@ public class OnboardingServiceImplTest extends CategoryTest {
         onboardingServiceImpl, "harnessProjectToBackstageSystem", harnessProjectToBackstageSystem, true);
 
     HarnessServiceToBackstageComponent harnessServiceToBackstageComponent =
-        new HarnessServiceToBackstageComponent(onboardingModuleConfig);
+        new HarnessServiceToBackstageComponent(onboardingModuleConfig, "local");
     FieldUtils.writeField(
         onboardingServiceImpl, "harnessServiceToBackstageComponent", harnessServiceToBackstageComponent, true);
 
