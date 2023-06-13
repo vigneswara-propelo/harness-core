da21eaa8effc3ecfc85e9f7324766b8e4bd222d4
Soumyajit Das <soumyajit.das@harness.io>
2023-06-13 11:20:44 +0530

    [fix]: [CI-8168]: Bump scm version in delegate (#48804)

--- 960-api-services/src/main/java/io/harness/delegate/clienttools/ScmVersion.java
+++ 960-api-services/src/main/java/io/harness/delegate/clienttools/ScmVersion.java
@@ -16,7 +16,7 @@ import org.apache.commons.lang3.StringUtils;
 @Getter
 @Slf4j
 public enum ScmVersion implements ClientToolVersion {
-  DEFAULT("9b34bea7");
+  DEFAULT("f1024c6b");
 
   private final String version;
 
