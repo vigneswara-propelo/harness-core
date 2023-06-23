2d5ece15ecf967be386b276ec4f3ca28c9392473
Vikyath Harekal <vikyath.harekal@harness.io>
2023-06-23 17:17:20 +0530

    feat: [IDP-725]: Add account id to log message of delegate proxy (#49666)

--- idp-service/src/main/java/io/harness/idp/proxy/delegate/DelegateProxyApiImpl.java
+++ idp-service/src/main/java/io/harness/idp/proxy/delegate/DelegateProxyApiImpl.java
@@ -18,6 +18,8 @@ import io.harness.http.HttpHeaderConfig;
 import io.harness.idp.annotations.IdpServiceAuthIfHasApiKey;
 import io.harness.idp.common.delegateselectors.cache.DelegateSelectorsCache;
 import io.harness.idp.proxy.delegate.beans.BackstageProxyRequest;
+import io.harness.logging.AccountLogContext;
+import io.harness.logging.AutoLogContext;
 import io.harness.security.annotations.NextGenManagerAuth;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -27,7 +29,6 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.ExecutionException;
 import javax.ws.rs.POST;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Context;
@@ -49,49 +50,52 @@ public class DelegateProxyApiImpl implements DelegateProxyApi {
   @Override
   @POST
   public Response forwardProxy(@Context UriInfo info, @Context javax.ws.rs.core.HttpHeaders headers,
-      @PathParam("url") String urlString, String body) throws JsonProcessingException, ExecutionException {
+      @PathParam("url") String urlString, String body) throws JsonProcessingException {
     var accountIdentifier = headers.getHeaderString("Harness-Account");
-    BackstageProxyRequest backstageProxyRequest;
-    try {
-      ObjectMapper mapper = new ObjectMapper();
-      backstageProxyRequest = mapper.readValue(body, BackstageProxyRequest.class);
-    } catch (Exception err) {
-      log.info("Error parsing backstageProxyRequest. Request: {}", body, err);
-      throw err;
-    }
-    log.info("Parsed request body url: {}", backstageProxyRequest.getUrl());
-    log.info("Parsed request body method: {}", backstageProxyRequest.getMethod());
-    StringBuilder headerString = new StringBuilder();
-    backstageProxyRequest.getHeaders().forEach((key, value) -> {
-      if (!key.equals(AUTHORIZATION)) {
-        headerString.append(String.format(HEADER_STRING_PATTERN, key, value));
-      } else {
-        log.debug("Skipped logging {} header", AUTHORIZATION);
+    try (AutoLogContext ignore1 =
+             new AccountLogContext(accountIdentifier, AutoLogContext.OverrideBehavior.OVERRIDE_ERROR)) {
+      BackstageProxyRequest backstageProxyRequest;
+      try {
+        ObjectMapper mapper = new ObjectMapper();
+        backstageProxyRequest = mapper.readValue(body, BackstageProxyRequest.class);
+      } catch (Exception err) {
+        log.info("Error parsing backstageProxyRequest. Request: {}", body, err);
+        throw err;
       }
-    });
-    log.info("Parsed request body headers: {}", headerString);
+      log.info("Parsed request body url: {}", backstageProxyRequest.getUrl());
+      log.info("Parsed request body method: {}", backstageProxyRequest.getMethod());
+      StringBuilder headerString = new StringBuilder();
+      backstageProxyRequest.getHeaders().forEach((key, value) -> {
+        if (!key.equals(AUTHORIZATION)) {
+          headerString.append(String.format(HEADER_STRING_PATTERN, key, value));
+        } else {
+          log.debug("Skipped logging {} header", AUTHORIZATION);
+        }
+      });
+      log.info("Parsed request body headers: {}", headerString);
 
-    Set<String> delegateSelectors = getDelegateSelectors(backstageProxyRequest.getUrl(), accountIdentifier);
-    List<HttpHeaderConfig> headerList =
-        delegateProxyRequestForwarder.createHeaderConfig(backstageProxyRequest.getHeaders());
+      Set<String> delegateSelectors = getDelegateSelectors(backstageProxyRequest.getUrl(), accountIdentifier);
+      List<HttpHeaderConfig> headerList =
+          delegateProxyRequestForwarder.createHeaderConfig(backstageProxyRequest.getHeaders());
 
-    HttpStepResponse httpResponse =
-        delegateProxyRequestForwarder.forwardRequestToDelegate(accountIdentifier, backstageProxyRequest.getUrl(),
-            headerList, backstageProxyRequest.getBody(), backstageProxyRequest.getMethod(), delegateSelectors);
+      HttpStepResponse httpResponse =
+          delegateProxyRequestForwarder.forwardRequestToDelegate(accountIdentifier, backstageProxyRequest.getUrl(),
+              headerList, backstageProxyRequest.getBody(), backstageProxyRequest.getMethod(), delegateSelectors);
 
-    if (httpResponse == null) {
-      return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-          .entity(ResponseMessage.builder()
-                      .message("Did not receive response from Delegate")
-                      .code(ErrorCode.INTERNAL_SERVER_ERROR)
-                      .build())
-          .build();
+      if (httpResponse == null) {
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+            .entity(ResponseMessage.builder()
+                        .message("Did not receive response from Delegate")
+                        .code(ErrorCode.INTERNAL_SERVER_ERROR)
+                        .build())
+            .build();
+      }
+
+      return Response.status(httpResponse.getHttpResponseCode()).entity(httpResponse.getHttpResponseBody()).build();
     }
-
-    return Response.status(httpResponse.getHttpResponseCode()).entity(httpResponse.getHttpResponseBody()).build();
   }
 
-  private Set<String> getDelegateSelectors(String urlString, String accountIdentifier) throws ExecutionException {
+  private Set<String> getDelegateSelectors(String urlString, String accountIdentifier) {
     URL url;
     try {
       url = new URL(urlString);
