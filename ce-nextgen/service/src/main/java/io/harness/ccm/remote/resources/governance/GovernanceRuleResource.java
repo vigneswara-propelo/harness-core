87bafee1b1dd7b8a2a7420da8749891d620b5973
Nikunj Badjatya <nikunj.badjatya@harness.io>
2023-06-26 15:21:27 -0700

    feat: [CCM-12984]: Remove deprecated api (#49499)

--- ce-nextgen/service/src/main/java/io/harness/ccm/remote/resources/governance/GovernanceRuleResource.java
+++ ce-nextgen/service/src/main/java/io/harness/ccm/remote/resources/governance/GovernanceRuleResource.java
@@ -20,7 +20,6 @@ import static io.harness.outbox.TransactionOutboxModule.OUTBOX_TRANSACTION_TEMPL
 import static io.harness.springdata.PersistenceUtils.DEFAULT_RETRY_POLICY;
 import static io.harness.telemetry.Destination.AMPLITUDE;
 
-import io.harness.EntityType;
 import io.harness.NGCommonEntityConstants;
 import io.harness.accesscontrol.AccountIdentifier;
 import io.harness.annotations.dev.OwnedBy;
@@ -61,7 +60,6 @@ import io.harness.connector.ConnectorResponseDTO;
 import io.harness.delegate.beans.connector.CEFeatures;
 import io.harness.delegate.beans.connector.ConnectorType;
 import io.harness.delegate.beans.connector.ceawsconnector.CEAwsConnectorDTO;
-import io.harness.encryption.Scope;
 import io.harness.exception.InvalidRequestException;
 import io.harness.faktory.FaktoryProducer;
 import io.harness.ng.core.dto.ErrorDTO;
@@ -79,7 +77,6 @@ import io.harness.yaml.validator.YamlSchemaValidator;
 
 import com.codahale.metrics.annotation.ExceptionMetered;
 import com.codahale.metrics.annotation.Timed;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.inject.Inject;
@@ -190,6 +187,7 @@ public class GovernanceRuleResource {
   @POST
   @Path("enqueue")
   @Timed
+  @Hidden
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
   @ApiOperation(value = "Enqueues job for execution", nickname = "enqueueGovernanceJob", hidden = true)
@@ -697,28 +695,6 @@ public class GovernanceRuleResource {
     }
 
     return ResponseDTO.newResponse(GovernanceEnqueueResponseDTO.builder().ruleExecutionId(ruleExecutionId).build());
-  }
-
-  @NextGenManagerAuth
-  @GET
-  @Path("entitySchema")
-  @Consumes(MediaType.APPLICATION_JSON)
-  @Produces(MediaType.APPLICATION_JSON)
-  @ApiOperation(value = "Get Schema for entity", nickname = "getSchemaForEntity")
-  @Operation(operationId = "getSchemaForEntity", description = "Get Schema for entity",
-      summary = "Get Schema for entity",
-      responses =
-      {
-        @io.swagger.v3.oas.annotations.responses.
-        ApiResponse(description = "Schema", content = { @Content(mediaType = MediaType.APPLICATION_JSON) })
-      })
-  public ResponseDTO<JsonNode>
-  getEntityYamlSchema(@NotNull @QueryParam(NGCommonEntityConstants.ACCOUNT_KEY) String accountIdentifier,
-      @QueryParam(NGCommonEntityConstants.PROJECT_KEY) String projectIdentifier,
-      @QueryParam(NGCommonEntityConstants.ORG_KEY) String orgIdentifier,
-      @QueryParam(NGCommonEntityConstants.ENTITY_TYPE) EntityType entityType, Scope scope) {
-    return ResponseDTO.newResponse(
-        yamlSchemaProvider.getYamlSchema(entityType, orgIdentifier, projectIdentifier, scope));
   }
 
   @NextGenManagerAuth
