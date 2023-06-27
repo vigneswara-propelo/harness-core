e60374c09b65aa6b23fbd3a01a445e08871bff74
Vinícius Calasans <vinicius.calasans@harness.io>
2023-06-27 17:24:29 -0300

    techdebt: [CDS-71417]: Add UTs for triggers yaml schema validation (#49627)
    * [CDS-71417]: Add UTs for triggers yaml schema validation
    
    * [CDS-71417]: codeformat

--- pipeline-service/modules/ng-triggers/src/test/java/io/harness/ngtriggers/service/NGTriggerYamlSchemaServiceTest.java
+++ pipeline-service/modules/ng-triggers/src/test/java/io/harness/ngtriggers/service/NGTriggerYamlSchemaServiceTest.java
@@ -8,6 +8,7 @@
 package io.harness.ngtriggers.service;
 
 import static io.harness.rule.OwnerRule.MEET;
+import static io.harness.rule.OwnerRule.VINICIUS;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
@@ -17,26 +18,44 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import io.harness.CategoryTest;
+import io.harness.EntityType;
 import io.harness.category.element.UnitTests;
+import io.harness.encryption.Scope;
 import io.harness.exception.InvalidRequestException;
 import io.harness.exception.ngexception.beans.yamlschema.YamlSchemaErrorWrapperDTO;
+import io.harness.ngtriggers.beans.config.NGTriggerConfigV2;
 import io.harness.ngtriggers.service.impl.NGTriggerYamlSchemaServiceImpl;
 import io.harness.rule.Owner;
 import io.harness.utils.YamlPipelineUtils;
+import io.harness.yaml.schema.JacksonClassHelper;
+import io.harness.yaml.schema.SwaggerGenerator;
+import io.harness.yaml.schema.YamlSchemaException;
+import io.harness.yaml.schema.YamlSchemaGenerator;
+import io.harness.yaml.schema.YamlSchemaHelper;
 import io.harness.yaml.schema.YamlSchemaProvider;
+import io.harness.yaml.schema.beans.YamlSchemaRootClass;
+import io.harness.yaml.validator.EnumCodeSchemaHandler;
 import io.harness.yaml.validator.InvalidYamlException;
+import io.harness.yaml.validator.RequiredCodeSchemaHandler;
 import io.harness.yaml.validator.YamlSchemaValidator;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.io.Resources;
 import com.networknt.schema.JsonSchema;
 import com.networknt.schema.ValidationMessage;
 import com.networknt.schema.ValidatorTypeCode;
+import io.dropwizard.jackson.Jackson;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -46,23 +65,49 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
+@Slf4j
 public class NGTriggerYamlSchemaServiceTest extends CategoryTest {
+  ClassLoader classLoader;
   @InjectMocks NGTriggerYamlSchemaServiceImpl ngTriggerYamlSchemaServiceImpl;
+  NGTriggerYamlSchemaService ngTriggerYamlSchemaService;
   @Mock YamlSchemaProvider yamlSchemaProvider;
   @Mock YamlSchemaValidator yamlSchemaValidator;
   @Rule public MockitoRule mockitoRule = MockitoJUnit.rule();
   String projectId = "projectId";
   String orgIdentifier = "orgIdentifier";
   String identifier = "identifier";
+
   @Before
-  public void setup() throws Exception {}
+  public void setup() throws Exception {
+    classLoader = getClass().getClassLoader();
+    ObjectMapper objectMapper = Jackson.newObjectMapper();
+    JacksonClassHelper jacksonClassHelper = new JacksonClassHelper(objectMapper);
+    SwaggerGenerator swaggerGenerator = new SwaggerGenerator(objectMapper);
+    List<YamlSchemaRootClass> rootClasses = List.of(YamlSchemaRootClass.builder()
+                                                        .entityType(EntityType.TRIGGERS)
+                                                        .availableAtProjectLevel(true)
+                                                        .availableAtOrgLevel(false)
+                                                        .availableAtAccountLevel(false)
+                                                        .clazz(NGTriggerConfigV2.class)
+                                                        .build());
+    YamlSchemaGenerator yamlSchemaGenerator =
+        new YamlSchemaGenerator(jacksonClassHelper, swaggerGenerator, rootClasses);
+    Map<EntityType, JsonNode> schemas = yamlSchemaGenerator.generateYamlSchema();
+    YamlSchemaValidator realYamlSchemaValidator =
+        new YamlSchemaValidator(rootClasses, new EnumCodeSchemaHandler(), new RequiredCodeSchemaHandler());
+    YamlSchemaHelper yamlSchemaHelper = new YamlSchemaHelper(rootClasses);
+    realYamlSchemaValidator.initializeValidatorWithSchema(schemas);
+    yamlSchemaHelper.initializeSchemaMaps(schemas);
+    YamlSchemaProvider realYamlSchemaProvider = new YamlSchemaProvider(yamlSchemaHelper);
+    ngTriggerYamlSchemaService =
+        new NGTriggerYamlSchemaServiceImpl(realYamlSchemaProvider, realYamlSchemaValidator, objectMapper);
+  }
 
   @Test
   @Owner(developers = MEET)
   @Category(UnitTests.class)
   public void testValidateTriggerYaml() throws IOException {
     String filename = "ng-custom-trigger-v0.yaml";
-    ClassLoader classLoader = getClass().getClassLoader();
     ObjectNode triggerSchema = mock(ObjectNode.class);
     String yaml = Resources.toString(Objects.requireNonNull(classLoader.getResource(filename)), StandardCharsets.UTF_8);
     Set validationMessages = new HashSet<>();
@@ -84,5 +129,33 @@ public class NGTriggerYamlSchemaServiceTest extends CategoryTest {
     assertThatThrownBy(() -> ngTriggerYamlSchemaServiceImpl.validateTriggerYaml(yaml, projectId, orgIdentifier, ""))
         .isInstanceOf(InvalidRequestException.class)
         .hasMessage("Error while validating trigger yaml");
+  }
+
+  @Test
+  @Owner(developers = VINICIUS)
+  @Category(UnitTests.class)
+  public void testGetTriggerYamlSchema() throws IOException {
+    String generatedSchema =
+        ngTriggerYamlSchemaService.getTriggerYamlSchema(projectId, orgIdentifier, identifier, Scope.PROJECT)
+            .toPrettyString();
+    String storedSchema = Resources.toString(
+        Objects.requireNonNull(classLoader.getResource("triggerYamlSchema.json")), StandardCharsets.UTF_8);
+    if (!generatedSchema.equals(storedSchema)) {
+      log.info("Difference in trigger's yaml schema :\n" + StringUtils.difference(generatedSchema, storedSchema));
+      throw new YamlSchemaException(
+          "Yaml schema not updated for NG Triggers. Please update the triggerYamlSchema.json accordingly.");
+    }
+  }
+
+  @Test
+  @Owner(developers = VINICIUS)
+  @Category(UnitTests.class)
+  public void testValidateTriggerYamlWithInvalidTriggerIdentifier() throws IOException {
+    String yaml =
+        Resources.toString(Objects.requireNonNull(classLoader.getResource("ng-trigger-v0-invalid-identifier.yaml")),
+            StandardCharsets.UTF_8);
+    assertThatThrownBy(() -> ngTriggerYamlSchemaService.validateTriggerYaml(yaml, projectId, orgIdentifier, identifier))
+        .isInstanceOf(InvalidYamlException.class)
+        .hasMessage("$.identifier: does not match the regex pattern ^[a-zA-Z_][0-9a-zA-Z_]{0,127}$");
   }
 }
