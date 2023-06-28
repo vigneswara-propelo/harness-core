5142bac35c64271a3de32a9c74bd594d5d8ff69b
Aditya Kashyap <122971747+adiyaar24@users.noreply.github.com>
2023-06-28 10:56:08 +0530

    fix: [PL-29458]: Removed Query Param Account Identifier From the API (#49743)
    PL-29458 : Removed Query Param Account Identifier From the API

--- 945-account-mgmt/src/main/java/io/harness/account/resource/AccountResource.java
+++ 945-account-mgmt/src/main/java/io/harness/account/resource/AccountResource.java
@@ -14,7 +14,6 @@ import static io.harness.configuration.DeployVariant.DEPLOY_VERSION;
 
 import static software.wings.security.PermissionAttribute.PermissionType.LOGGED_IN;
 
-import io.harness.NGCommonEntityConstants;
 import io.harness.accesscontrol.AccountIdentifier;
 import io.harness.accesscontrol.NGAccessControlCheck;
 import io.harness.account.AccountClient;
@@ -45,14 +44,12 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import javax.validation.constraints.NotNull;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 @OwnedBy(HarnessTeam.GTM)
@@ -103,11 +100,8 @@ public class AccountResource {
   @NGAccessControlCheck(resourceType = ResourceTypes.ACCOUNT, permission = VIEW_ACCOUNT_PERMISSION)
   public ResponseDTO<AccountDTO>
   get(@Parameter(required = true, description = ACCOUNT_PARAM_MESSAGE) @PathParam(
-          "accountIdentifier") String accountIdentifier,
-      @Parameter(required = true, description = ACCOUNT_PARAM_MESSAGE) @QueryParam(
-          NGCommonEntityConstants.ACCOUNT_KEY) @NotNull @AccountIdentifier String accountId) {
+      "accountIdentifier") @AccountIdentifier String accountIdentifier) {
     AccountDTO accountDTO = CGRestUtils.getResponse(accountClient.getAccountDTO(accountIdentifier));
-
     accountDTO.setCluster(accountConfig.getDeploymentClusterName());
 
     return ResponseDTO.newResponse(accountDTO);
