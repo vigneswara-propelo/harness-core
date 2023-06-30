15edce6b2a82b0b6205724c2360ff8a8c6bc67cf
Brijesh Dhakar <brijesh.dhakar@harness.io>
2023-06-30 16:07:27 +0530

    [feat]: [CDS-69780]: Extending the StrategyNodeFunctor to include all strategy expressions for any level (#49972)
    * [feat]: [CDS-69780]: Extending the StrategyNodeFunctor to include all strategy expressions for any level
    
    * adding unit-tests

--- pipeline-service/modules/orchestration/src/main/java/io/harness/engine/expressions/functors/StrategyNodeFunctor.java
+++ pipeline-service/modules/orchestration/src/main/java/io/harness/engine/expressions/functors/StrategyNodeFunctor.java
@@ -14,12 +14,16 @@ import io.harness.data.structure.EmptyPredicate;
 import io.harness.engine.expressions.NodeExecutionsCache;
 import io.harness.engine.expressions.OrchestrationConstants;
 import io.harness.expression.LateBindingMap;
+import io.harness.plancreator.strategy.StrategyUtils;
 import io.harness.pms.contracts.ambiance.Ambiance;
 import io.harness.pms.contracts.ambiance.Level;
 import io.harness.pms.contracts.execution.Status;
 import io.harness.pms.contracts.steps.StepCategory;
+import io.harness.pms.execution.utils.AmbianceUtils;
 import io.harness.pms.execution.utils.StatusUtils;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import lombok.Builder;
@@ -34,14 +38,19 @@ public class StrategyNodeFunctor extends LateBindingMap {
 
   @Override
   public synchronized Object get(Object key) {
+    Map<String, Object> strategyDataMap = new HashMap<>();
     String status = getCurrentStatus(key.toString(), false);
-    return Map.of(OrchestrationConstants.CURRENT_STATUS, status);
+    strategyDataMap.put(OrchestrationConstants.CURRENT_STATUS, status);
+    strategyDataMap.putAll(getStrategyChildDataForGivenIdentifier((String) key));
+    return strategyDataMap;
   }
 
   public String getCurrentStatus() {
     return getCurrentStatus("", true);
   }
   private String getCurrentStatus(String key, boolean useDeepestStrategyNodeIfIdentifierNotMatched) {
+    // CurrentStatus is calculated for the strategy node. So we are calculating the Strategy level(Not the child of
+    // strategy)
     Level level = getStrategyLevelForGivenIdentifier(key, useDeepestStrategyNodeIfIdentifierNotMatched);
     if (level == null) {
       return "null";
@@ -73,5 +82,29 @@ public class StrategyNodeFunctor extends LateBindingMap {
       }
     }
     return strategyLevel;
+  }
+
+  // Strategy expressions(Except currentStatus) are evaluated on the child of strategy node. So we are getting the
+  // level that is just below the strategy node.
+  private Map<String, Object> getStrategyChildDataForGivenIdentifier(String identifier) {
+    Level childLevel = null;
+    for (int index = ambiance.getLevelsCount() - 2; index >= 0; index--) {
+      Level level = ambiance.getLevels(index);
+      if (level.getStepType().getStepCategory() == StepCategory.STRATEGY) {
+        // If identifier matches then its the level is the correct strategy level. So the child will be
+        // getLevel(index+1).
+        if (level.getIdentifier().equals(identifier)) {
+          Level tempLevel = ambiance.getLevels(index + 1);
+          childLevel = tempLevel.hasStrategyMetadata() ? tempLevel : null;
+          break;
+        }
+      }
+    }
+
+    if (childLevel == null) {
+      return Collections.emptyMap();
+    }
+    return StrategyUtils.fetchStrategyObjectMap(
+        Collections.singletonList(childLevel), AmbianceUtils.shouldUseMatrixFieldName(ambiance));
   }
 }
--- pipeline-service/service/src/test/java/io/harness/engine/expressions/functors/StrategyNodeFunctorTest.java
+++ pipeline-service/service/src/test/java/io/harness/engine/expressions/functors/StrategyNodeFunctorTest.java
@@ -19,7 +19,9 @@ import io.harness.engine.expressions.NodeExecutionsCache;
 import io.harness.engine.expressions.OrchestrationConstants;
 import io.harness.pms.contracts.ambiance.Ambiance;
 import io.harness.pms.contracts.ambiance.Level;
+import io.harness.pms.contracts.execution.MatrixMetadata;
 import io.harness.pms.contracts.execution.Status;
+import io.harness.pms.contracts.execution.StrategyMetadata;
 import io.harness.pms.contracts.steps.StepCategory;
 import io.harness.pms.contracts.steps.StepType;
 import io.harness.rule.Owner;
@@ -43,8 +45,24 @@ public class StrategyNodeFunctorTest extends CategoryTest {
   private static final String APP_ID = generateUuid();
   private static final String PLAN_EXECUTION_ID = generateUuid();
   private static final String PLAN_ID = generateUuid();
-  private static final String SECTION_RUNTIME_ID = generateUuid();
-  private static final String SECTION_SETUP_ID = generateUuid();
+  private static final String STAGE_RUNTIME_ID = generateUuid();
+  private static final String STAGE_SETUP_ID = generateUuid();
+
+  private static final String STRATEGY_RUNTIME_ID_2 = generateUuid();
+  private static final String STRATEGY_SETUP_ID_2 = generateUuid();
+  private static final String STRATEGY_RUNTIME_ID_3 = generateUuid();
+  private static final String STRATEGY_SETUP_ID_3 = generateUuid();
+  private static final String STRATEGY_RUNTIME_ID_4 = generateUuid();
+  private static final String STRATEGY_SETUP_ID_4 = generateUuid();
+
+  private static final String SG1_RUNTIME_ID_1 = generateUuid();
+  private static final String SG1_SETUP_ID_1 = generateUuid();
+  private static final String SG1_RUNTIME_ID_2 = generateUuid();
+  private static final String SG1_SETUP_ID_2 = generateUuid();
+
+  private static final String STEP_RUNTIME_ID = generateUuid();
+  private static final String STEP_SETUP_ID = generateUuid();
+
   @Mock NodeExecutionsCache nodeExecutionsCache;
 
   @Before
@@ -80,6 +98,58 @@ public class StrategyNodeFunctorTest extends CategoryTest {
         .isEqualTo("null");
   }
 
+  @Test
+  @Owner(developers = BRIJESH)
+  @Category(UnitTests.class)
+  public void testGetForMultiStrategyLevels() {
+    Ambiance ambiance = buildMultiLevelStrategyAmbiance();
+    StrategyNodeFunctor strategyNodeFunctor =
+        StrategyNodeFunctor.builder().nodeExecutionsCache(nodeExecutionsCache).ambiance(ambiance).build();
+    Map<String, Object> strategyDataMap = (Map<String, Object>) strategyNodeFunctor.get("strategyNodeId_4");
+
+    assertThat(strategyDataMap).isNotNull();
+    assertThat(strategyDataMap.get("currentStatus")).isEqualTo("RUNNING");
+    assertThat(strategyDataMap.get("identifierPostFix")).isEqualTo("_0");
+    assertThat(strategyDataMap.get("iteration")).isEqualTo(0);
+    assertThat(strategyDataMap.get("iterations")).isEqualTo(2);
+    assertThat(strategyDataMap.get("totalIterations")).isEqualTo(2);
+    assertThat(((Map) strategyDataMap.get("matrix")).get("key1")).isEqualTo("val1_step1");
+    assertThat(((Map) strategyDataMap.get("matrix")).get("key2")).isEqualTo("val2_step1");
+    assertThat(((Map) strategyDataMap.get("matrix")).get("step1_key")).isEqualTo("step1_val");
+
+    strategyDataMap = (Map<String, Object>) strategyNodeFunctor.get("strategyNodeId_3");
+    assertThat(strategyDataMap).isNotNull();
+    assertThat(strategyDataMap.get("currentStatus")).isEqualTo("RUNNING");
+    assertThat(strategyDataMap.get("identifierPostFix")).isEqualTo("_1");
+    assertThat(strategyDataMap.get("iteration")).isEqualTo(1);
+    assertThat(strategyDataMap.get("iterations")).isEqualTo(4);
+    assertThat(strategyDataMap.get("totalIterations")).isEqualTo(4);
+    assertThat(((Map) strategyDataMap.get("matrix")).get("key1")).isEqualTo("val1_sg2");
+    assertThat(((Map) strategyDataMap.get("matrix")).get("key2")).isEqualTo("val2_sg2");
+    assertThat(((Map) strategyDataMap.get("matrix")).get("sg2_key")).isEqualTo("sg2_val");
+
+    strategyDataMap = (Map<String, Object>) strategyNodeFunctor.get("strategyNodeId_2");
+    assertThat(strategyDataMap).isNotNull();
+    assertThat(strategyDataMap.get("currentStatus")).isEqualTo("RUNNING");
+    assertThat(strategyDataMap.get("identifierPostFix")).isEqualTo("_0");
+    assertThat(strategyDataMap.get("iteration")).isEqualTo(0);
+    assertThat(strategyDataMap.get("iterations")).isEqualTo(3);
+    assertThat(strategyDataMap.get("totalIterations")).isEqualTo(3);
+    assertThat(((Map) strategyDataMap.get("matrix")).get("key1")).isEqualTo("val1_sg1");
+    assertThat(((Map) strategyDataMap.get("matrix")).get("key2")).isEqualTo("val2_sg1");
+    assertThat(((Map) strategyDataMap.get("matrix")).get("sg1_key")).isEqualTo("sg1_val");
+
+    strategyDataMap = (Map<String, Object>) strategyNodeFunctor.get("strategyNodeId");
+    assertThat(strategyDataMap).isNotNull();
+    assertThat(strategyDataMap.get("currentStatus")).isEqualTo("RUNNING");
+    assertThat(strategyDataMap.get("identifierPostFix")).isEqualTo("_1");
+    assertThat(strategyDataMap.get("iteration")).isEqualTo(1);
+    assertThat(strategyDataMap.get("iterations")).isEqualTo(2);
+    assertThat(strategyDataMap.get("totalIterations")).isEqualTo(2);
+    assertThat(((Map) strategyDataMap.get("matrix")).get("key1")).isEqualTo("val1_stage1");
+    assertThat(((Map) strategyDataMap.get("matrix")).get("key2")).isEqualTo("val2_stage1");
+    assertThat(((Map) strategyDataMap.get("matrix")).get("stage1_key")).isEqualTo("stage1_val");
+  }
   private Ambiance buildAmbiance() {
     Level strategyLevel =
         Level.newBuilder()
@@ -92,8 +162,8 @@ public class StrategyNodeFunctorTest extends CategoryTest {
             .build();
     Level stageLevel =
         Level.newBuilder()
-            .setRuntimeId(SECTION_RUNTIME_ID)
-            .setSetupId(SECTION_SETUP_ID)
+            .setRuntimeId(STAGE_RUNTIME_ID)
+            .setSetupId(STAGE_SETUP_ID)
             .setGroup("STAGE")
             .setStartTs(3)
             .setIdentifier("i3")
@@ -102,6 +172,134 @@ public class StrategyNodeFunctorTest extends CategoryTest {
     List<Level> levels = new ArrayList<>();
     levels.add(strategyLevel);
     levels.add(stageLevel);
+    return Ambiance.newBuilder()
+        .setPlanExecutionId(PLAN_EXECUTION_ID)
+        .setPlanId(PLAN_ID)
+        .putAllSetupAbstractions(ImmutableMap.of(
+            "accountId", ACCOUNT_ID, "orgIdentifier", ORG_ID, "projectIdentifier", PROJECT_ID, "appId", APP_ID))
+        .addAllLevels(levels)
+        .build();
+  }
+
+  private Ambiance buildMultiLevelStrategyAmbiance() {
+    Level strategyLevel =
+        Level.newBuilder()
+            .setRuntimeId("STRATEGY_RUNTIME_ID")
+            .setSetupId("STRATEGY_SETUP_ID")
+            .setGroup("STRATEGY")
+            .setStartTs(2)
+            .setIdentifier("strategyNodeId")
+            .setStepType(StepType.newBuilder().setType("STRATEGY").setStepCategory(StepCategory.STRATEGY).build())
+            .build();
+    Level stageLevel =
+        Level.newBuilder()
+            .setRuntimeId(STAGE_RUNTIME_ID)
+            .setSetupId(STAGE_SETUP_ID)
+            .setGroup("STAGE")
+            .setStartTs(3)
+            .setIdentifier("i3")
+            .setStrategyMetadata(StrategyMetadata.newBuilder()
+                                     .setMatrixMetadata(MatrixMetadata.newBuilder()
+                                                            .putMatrixValues("key1", "val1_stage1")
+                                                            .putMatrixValues("key2", "val2_stage1")
+                                                            .putMatrixValues("stage1_key", "stage1_val")
+                                                            .build())
+                                     .setTotalIterations(2)
+                                     .setCurrentIteration(1)
+                                     .build())
+            .setStepType(StepType.newBuilder().setType("SECTION").setStepCategory(StepCategory.STAGE).build())
+            .build();
+    Level strategyLevel2 =
+        Level.newBuilder()
+            .setRuntimeId(STRATEGY_RUNTIME_ID_2)
+            .setSetupId(STRATEGY_SETUP_ID_2)
+            .setGroup("STRATEGY")
+            .setStartTs(4)
+            .setIdentifier("strategyNodeId_2")
+            .setStepType(StepType.newBuilder().setType("STRATEGY").setStepCategory(StepCategory.STRATEGY).build())
+            .build();
+    Level stepGroupLevel1 =
+        Level.newBuilder()
+            .setRuntimeId(SG1_RUNTIME_ID_1)
+            .setSetupId(SG1_SETUP_ID_1)
+            .setGroup("STEP_GROUP")
+            .setStartTs(5)
+            .setIdentifier("sg1")
+            .setStrategyMetadata(StrategyMetadata.newBuilder()
+                                     .setMatrixMetadata(MatrixMetadata.newBuilder()
+                                                            .putMatrixValues("key1", "val1_sg1")
+                                                            .putMatrixValues("key2", "val2_sg1")
+                                                            .putMatrixValues("sg1_key", "sg1_val")
+                                                            .build())
+                                     .setTotalIterations(3)
+                                     .setCurrentIteration(0)
+                                     .build())
+            .setStepType(StepType.newBuilder().setType("SECTION").setStepCategory(StepCategory.STEP_GROUP).build())
+            .build();
+    Level strategyLevel3 =
+        Level.newBuilder()
+            .setRuntimeId(STRATEGY_RUNTIME_ID_3)
+            .setSetupId(STRATEGY_SETUP_ID_3)
+            .setGroup("STRATEGY")
+            .setStartTs(6)
+            .setIdentifier("strategyNodeId_3")
+            .setStepType(StepType.newBuilder().setType("STRATEGY").setStepCategory(StepCategory.STRATEGY).build())
+            .build();
+    Level stepGroupLevel2 =
+        Level.newBuilder()
+            .setRuntimeId(SG1_RUNTIME_ID_2)
+            .setSetupId(SG1_SETUP_ID_2)
+            .setGroup("STEP_GROUP")
+            .setStartTs(7)
+            .setIdentifier("sg2")
+            .setStrategyMetadata(StrategyMetadata.newBuilder()
+                                     .setMatrixMetadata(MatrixMetadata.newBuilder()
+                                                            .putMatrixValues("key1", "val1_sg2")
+                                                            .putMatrixValues("key2", "val2_sg2")
+                                                            .putMatrixValues("sg2_key", "sg2_val")
+                                                            .build())
+                                     .setTotalIterations(4)
+                                     .setCurrentIteration(1)
+                                     .build())
+            .setStepType(StepType.newBuilder().setType("SECTION").setStepCategory(StepCategory.STEP_GROUP).build())
+            .build();
+    Level strategyLevel4 =
+        Level.newBuilder()
+            .setRuntimeId(STRATEGY_RUNTIME_ID_4)
+            .setSetupId(STRATEGY_SETUP_ID_4)
+            .setGroup("STRATEGY")
+            .setStartTs(6)
+            .setIdentifier("strategyNodeId_4")
+            .setStepType(StepType.newBuilder().setType("STRATEGY").setStepCategory(StepCategory.STRATEGY).build())
+            .build();
+    Level stepLevel =
+        Level.newBuilder()
+            .setRuntimeId(STEP_RUNTIME_ID)
+            .setSetupId(STEP_SETUP_ID)
+            .setGroup("STEP")
+            .setStartTs(7)
+            .setIdentifier("step1")
+            .setStrategyMetadata(StrategyMetadata.newBuilder()
+                                     .setMatrixMetadata(MatrixMetadata.newBuilder()
+                                                            .putMatrixValues("key1", "val1_step1")
+                                                            .putMatrixValues("key2", "val2_step1")
+                                                            .putMatrixValues("step1_key", "step1_val")
+                                                            .build())
+                                     .setTotalIterations(2)
+                                     .setCurrentIteration(0)
+                                     .build())
+            .setStepType(StepType.newBuilder().setType("SECTION").setStepCategory(StepCategory.STEP_GROUP).build())
+            .build();
+
+    List<Level> levels = new ArrayList<>();
+    levels.add(strategyLevel);
+    levels.add(stageLevel);
+    levels.add(strategyLevel2);
+    levels.add(stepGroupLevel1);
+    levels.add(strategyLevel3);
+    levels.add(stepGroupLevel2);
+    levels.add(strategyLevel4);
+    levels.add(stepLevel);
     return Ambiance.newBuilder()
         .setPlanExecutionId(PLAN_EXECUTION_ID)
         .setPlanId(PLAN_ID)
