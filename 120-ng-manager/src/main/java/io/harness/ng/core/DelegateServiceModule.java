286d12ddafd3fd44de236a039b0ddf8c4ede6b2d
Anupam Jaiswal <102655013+AnupamIO@users.noreply.github.com>
2023-07-05 00:40:37 +0530

    fix: [PL-39824]: Fixing filters api (#50127)

--- 120-ng-manager/src/main/java/io/harness/ng/core/DelegateServiceModule.java
+++ 120-ng-manager/src/main/java/io/harness/ng/core/DelegateServiceModule.java
@@ -10,12 +10,16 @@ package io.harness.ng.core;
 import static io.harness.annotations.dev.HarnessTeam.DEL;
 
 import io.harness.annotations.dev.OwnedBy;
+import io.harness.delegate.filter.DelegateFilterPropertiesMapper;
+import io.harness.filter.FilterType;
+import io.harness.filter.mapper.FilterPropertiesMapper;
 import io.harness.ng.core.api.DelegateDetailsService;
 import io.harness.ng.core.api.DelegateProfileManagerNgService;
 import io.harness.ng.core.api.impl.DelegateDetailsServiceImpl;
 import io.harness.ng.core.api.impl.DelegateProfileManagerNgServiceImpl;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.multibindings.MapBinder;
 
 @OwnedBy(DEL)
 public class DelegateServiceModule extends AbstractModule {
@@ -23,5 +27,8 @@ public class DelegateServiceModule extends AbstractModule {
   protected void configure() {
     bind(DelegateDetailsService.class).to(DelegateDetailsServiceImpl.class);
     bind(DelegateProfileManagerNgService.class).to(DelegateProfileManagerNgServiceImpl.class);
+    MapBinder<String, FilterPropertiesMapper> filterPropertiesMapper =
+        MapBinder.newMapBinder(binder(), String.class, FilterPropertiesMapper.class);
+    filterPropertiesMapper.addBinding(FilterType.DELEGATE.toString()).to(DelegateFilterPropertiesMapper.class);
   }
 }
