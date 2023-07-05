f4013e8885442cffc4997805b845294aec2bea2d
Fernando Dourado <fernando.dourado@harness.io>
2023-07-05 11:22:45 -0300

    fix:[CDS-72451]: Remove flaky unit tests (#50065)
    * fix:[CDS-72451]: Remove flaky unit tests
    
    * fix:[CDS-72451]: Add ignore to flaky unit tests
    
    The choice of ignore strategy is better to don't lose the test concept.
    
    * fix:[CDS-72451]: Re-order annotations

--- 400-rest/src/test/java/software/wings/service/impl/WorkflowExecutionServiceRollbackTest.java
+++ 400-rest/src/test/java/software/wings/service/impl/WorkflowExecutionServiceRollbackTest.java
@@ -93,6 +93,7 @@ import com.google.common.collect.Sets;
 import com.google.inject.Inject;
 import java.util.Collections;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.mockito.InjectMocks;
@@ -116,6 +117,7 @@ public class WorkflowExecutionServiceRollbackTest extends WingsBaseTest {
 
   private Workflow workflow;
   private Application app;
+
   @Before
   public void setup() {
     workflow = aWorkflow()
@@ -144,6 +146,7 @@ public class WorkflowExecutionServiceRollbackTest extends WingsBaseTest {
     when(workflowService.readWorkflow(APP_ID, WORKFLOW_ID)).thenReturn(workflow);
     when(appService.get(APP_ID)).thenReturn(anApplication().build());
   }
+
   @Test
   @Owner(developers = POOJA)
   @Category(UnitTests.class)
@@ -288,6 +291,7 @@ public class WorkflowExecutionServiceRollbackTest extends WingsBaseTest {
   @Test
   @Owner(developers = POOJA)
   @Category(UnitTests.class)
+  @Ignore("CDS-72451: Flaky unit test")
   public void testOnDemandRollbackConfirmationSuccess() {
     WorkflowExecution previousWE = createNewWorkflowExecution(false);
     previousWE.setArtifacts(Collections.emptyList());
@@ -425,6 +429,7 @@ public class WorkflowExecutionServiceRollbackTest extends WingsBaseTest {
   @Test
   @Owner(developers = DEEPAK_PUTHRAYA)
   @Category(UnitTests.class)
+  @Ignore("CDS-72451: Flaky unit test")
   public void testTriggerRollbackExecutionWorkflowWithNoPreviousArtifacts() {
     WorkflowExecution previousWE = createNewWorkflowExecution(false);
     previousWE.setArtifacts(Collections.emptyList());
