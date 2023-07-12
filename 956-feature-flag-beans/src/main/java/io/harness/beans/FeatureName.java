928b5d8bb42459f80aa0931c2474d194ee8cc154
Ved Tambat <ved.atultambat@harness.io>
2023-07-12 18:39:52 +0530

    [fix]: [CDS-74152]: FF addition CDS_ENABLE_LOAD_FROM_CACHE_FOR_RETRY_FORM (#50442)

--- 956-feature-flag-beans/src/main/java/io/harness/beans/FeatureName.java
+++ 956-feature-flag-beans/src/main/java/io/harness/beans/FeatureName.java
@@ -814,6 +814,7 @@ public enum FeatureName {
   CDS_STEP_EXECUTION_DATA_SYNC(
       "This flag controls if you want Step execution data saved in mongodb and eventually synced to timescale",
       HarnessTeam.CDP),
+  CDS_ENABLE_LOAD_FROM_CACHE_FOR_RETRY_FORM("Used for enabling load from cache on retry form", PIPELINE),
   CDS_GITHUB_PACKAGES("Used for SSH/WinRm copy and download Github packages artifacts", HarnessTeam.CDP);
   // keep-sorted end
 
