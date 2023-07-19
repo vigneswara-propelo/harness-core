d4e758b0dfe4092e38c09b842473413d217ae250
Soumyajit Das <soumyajit.das@harness.io>
2023-07-19 12:11:58 +0530

    fix: [CI-8511]: Bump scm version (#50718)

--- 960-api-services/src/main/java/io/harness/delegate/clienttools/ScmVersion.java
+++ 960-api-services/src/main/java/io/harness/delegate/clienttools/ScmVersion.java
@@ -16,7 +16,7 @@ import org.apache.commons.lang3.StringUtils;
 @Getter
 @Slf4j
 public enum ScmVersion implements ClientToolVersion {
-  DEFAULT("0c5acd02");
+  DEFAULT("b4486350");
 
   private final String version;
 
--- dockerization/delegate-proxy/setup.sh
+++ dockerization/delegate-proxy/setup.sh
@@ -199,7 +199,7 @@ for tfConfigInspectVersion in v1.0 v1.1 v1.2; do
 
 done
 
-for scmVersion in 0c5acd02; do
+for scmVersion in b4486350; do
   echo "Adding scm" $scmVersion
 
   SCM_LINUX_DIR="${IMAGES_DIR}/scm/linux/$scmVersion/"
@@ -295,7 +295,7 @@ function setupClientUtils() {
       cp images/oc/${platform}/$ocversion/oc ${STORAGE_DIR_LOCATION}/harness-download/harness-oc/release/$ocversion/bin/${platform}/amd64/
     done
 
-    for scmVersion in 0c5acd02; do
+    for scmVersion in b4486350; do
       mkdir -p ${STORAGE_DIR_LOCATION}/harness-download/harness-scm/release/$scmVersion/bin/${platform}/amd64/
       cp images/scm/${platform}/$scmVersion/scm ${STORAGE_DIR_LOCATION}/harness-download/harness-scm/release/$scmVersion/bin/${platform}/amd64/
     done
