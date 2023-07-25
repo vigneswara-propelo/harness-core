332f515dbe85946d29c9b032c93d927a6fbae275
Anurag Madnawat <anurag.madnawat@harness.io>
2023-07-24 21:05:28 -0700

    [feat]: [CI-8965]: Fix VM Runner builds (#50934)

--- 950-delegate-tasks-beans/src/main/java/io/harness/delegate/beans/ci/vm/runner/SetupVmResponse.java
+++ 950-delegate-tasks-beans/src/main/java/io/harness/delegate/beans/ci/vm/runner/SetupVmResponse.java
@@ -7,12 +7,14 @@
 
 package io.harness.delegate.beans.ci.vm.runner;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Value;
 
 @Value
 @Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class SetupVmResponse {
   @JsonProperty("instance_id") String instanceID;
   @JsonProperty("ip_address") String ipAddress;
