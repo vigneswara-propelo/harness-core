9069769f6430f000cde74b11c5adb6d741f78e5a
Xingchi Jin <109999795+xingchi-jin@users.noreply.github.com>
2023-07-25 09:58:50 -0700

    fix: [PL-39355]: disable the script by default; do not print curl command as it will contain proxy username,password (#50791)

--- dockerization/delegate/immutable-scripts/connectivity_check.sh
+++ dockerization/delegate/immutable-scripts/connectivity_check.sh
@@ -4,15 +4,17 @@
 # that can be found in the licenses directory at the root of this repository, also available at
 # https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
 
-if [[ $PRECHECK_CONN == "true" || $PRECHECK_CONN == "" ]]; then
+if [[ $PRECHECK_CONN == "true" ]]; then
   set +e
+  set -v
   command="curl --connect-timeout 10 --write-out %{http_code} $MANAGER_PROXY_CURL -s $MANAGER_HOST_AND_PORT/api/account/$ACCOUNT_ID/status  --output /dev/null"
   echo "Checking connection to Harness manager..."
-  echo $command
   status=$(eval "$command")
   echo "HTTP Status Code: $status, Curl exit code  $?"
   [[ $status == 000 ]] && echo "Connectivity test failed. Please check network connection from your delegate to Harness manager." && exit 1
   [[ $status == 401 ]] && echo "Account ID: $ACCOUNT_ID not found. Stop launching delegate." && exit 2
   # other errors
   [[ $status -ge 400 ]] && echo "Connection check to Harness manager failed. Stop launching delegate." && exit 3
+  set -e
+  set +v
 fi
