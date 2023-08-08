9972bb9b5a64aff14fb29fda1c0c380de481e53c
Sahildeep Singh <sahildeep.singh@harness.io>
2023-08-09 00:40:27 +0530

    [fix]: [CCM-12833]: Catching ce-ng and batch graphQL error logs (#51465)
    * [fix]: [CCM-12833]: Removed CG classes and added alert for NG graphQL classes
    
    * [fix]: [CCM-12833]: Catching ce-ng and batch error logs

--- scripts/terraform/stackdriver/ce-graphql/main.tf
+++ scripts/terraform/stackdriver/ce-graphql/main.tf
@@ -16,60 +16,41 @@ variable "projectId" {
 locals {
   qa_filter_prefix = join("\n", [
     "resource.type=\"k8s_container\"",
+    "resource.labels.project_id=\"qa-setup\""
+    "resource.labels.location=\"us-west1\""
     "resource.labels.cluster_name=\"qa-private\"",
-    "resource.labels.container_name=\"manager-qa\"",
-    "resource.labels.namespace_name:\"manager-qa\""
-  ])
-
-  qa_free_filter_prefix = join("\n", [
-    "resource.type=\"k8s_container\"",
-    "resource.labels.cluster_name=\"qa-private\"",
-    "resource.labels.container_name=\"manager-qa-free\"",
-    "resource.labels.namespace_name:\"manager-free\""
+    "labels.k8s-pod/app=(\"ce-nextgen\" OR \"batch-processing\")"
   ])
 
   stress_filter_prefix = join("\n", [
     "resource.type=\"k8s_container\"",
+    "resource.labels.project_id=\"qa-setup\""
+    "resource.labels.location=\"us-west1\""
     "resource.labels.cluster_name=\"qa-stress\"",
-    "resource.labels.container_name=\"manager-stress\"",
+    "labels.k8s-pod/app=(\"ce-nextgen\" OR \"batch-processing\")"
   ])
 
   prod_filter_prefix = join("\n", [
     "resource.type=\"k8s_container\"",
+    "resource.labels.project_id=\"prod-setup-205416\""
+    "resource.labels.location=\"us-west1\""
     "resource.labels.cluster_name=\"prod-private-uswest1-primary\"",
-    "resource.labels.container_name=\"manager\"",
-    "resource.labels.namespace_name:\"manager-prod-\""
-  ])
-
-  prod_failover_filter_prefix = join("\n", [
-    "resource.type=\"k8s_container\"",
-    "resource.labels.cluster_name=\"prod-private-uswest2-failover\"",
-    "resource.labels.container_name=\"manager\"",
-    "resource.labels.namespace_name:\"manager-prod-\""
-  ])
-
-  prod_freemium_filter_prefix = join("\n", [
-    "resource.type=\"k8s_container\"",
-    "resource.labels.cluster_name=\"prod-private-uswest1-primary\"",
-    "resource.labels.container_name=\"manager\"",
-    "resource.labels.namespace_name=\"harness-free\""
+    "labels.k8s-pod/app=(\"ce-nextgen\" OR \"batch-processing\")"
   ])
 
   dev_filter_prefix = join("\n", [
     "resource.type=\"k8s_container\"",
+    "resource.labels.project_id=\"ccm-play\""
+    "resource.labels.location=\"us-central1-c\""
     "resource.labels.cluster_name=\"ce-dev\"",
-    "resource.labels.container_name=\"manager-dev\"",
-    "resource.labels.namespace_name:\"manager-\""
+    "labels.k8s-pod/app=(\"ce-nextgen\" OR \"batch-processing\")"
   ])
 
   filter_prefix = (var.deployment == "qa" ? local.qa_filter_prefix :
-    (var.deployment == "qa-free" ? local.qa_free_filter_prefix :
       (var.deployment == "dev" ? local.dev_filter_prefix :
         (var.deployment == "stress" ? local.stress_filter_prefix :
           (var.deployment == "prod" ? local.prod_filter_prefix :
-            (var.deployment == "freemium" ? local.prod_freemium_filter_prefix :
-              (var.deployment == "prod-failover" ? local.prod_failover_filter_prefix :
-  local.qa_filter_prefix)))))))
+  local.prod_filter_prefix))))
 
   name_prefix = join("_", ["x", var.deployment])
 
