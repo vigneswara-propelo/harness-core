18ef45a00c7e188b05058728c73bc71dbd13ded3
Vinícius Calasans <vinicius.calasans@harness.io>
2023-08-16 12:18:34 -0300

    [CDS-77101]: Fix artifactory triggers in migration tool (#51681)

--- 100-migrator/src/main/java/io/harness/ngmigration/service/artifactstream/ArtifactoryArtifactStreamMapper.java
+++ 100-migrator/src/main/java/io/harness/ngmigration/service/artifactstream/ArtifactoryArtifactStreamMapper.java
@@ -8,7 +8,7 @@
 package io.harness.ngmigration.service.artifactstream;
 
 import static io.harness.ngmigration.utils.NGMigrationConstants.PLEASE_FIX_ME;
-import static io.harness.ngmigration.utils.NGMigrationConstants.TRIGGER_TAG_VALUE_DEFAULT;
+import static io.harness.ngtriggers.beans.source.artifact.ArtifactoryRegistrySpec.ArtifactoryRegistrySpecBuilder;
 
 import static software.wings.ngmigration.NGMigrationEntityType.CONNECTOR;
 import static software.wings.utils.RepositoryType.docker;
@@ -73,23 +73,27 @@ public class ArtifactoryArtifactStreamMapper implements ArtifactStreamMapper {
     String repository = PLEASE_FIX_ME;
     String format = docker.name();
     String repositoryUrl = "";
+    boolean isDockerFormat = true;
     if (artifactStream != null) {
       ArtifactoryArtifactStream stream = (ArtifactoryArtifactStream) artifactStream;
       imagePath = stream.getImageName();
       repository = stream.getJobname();
-      format = docker.name().equals(stream.getRepositoryType()) ? docker.name() : "generic";
+      isDockerFormat = docker.name().equals(stream.getRepositoryType());
+      format = isDockerFormat ? docker.name() : "generic";
       repositoryUrl = stream.getDockerRepositoryServer();
     }
-
-    return ArtifactoryRegistrySpec.builder()
-        .connectorRef(connectorRef)
-        .eventConditions(eventConditions)
-        .repositoryFormat(format)
-        .repository(repository)
-        .repositoryUrl(repositoryUrl)
-        .artifactDirectory(imagePath)
-        .artifactPath(TRIGGER_TAG_VALUE_DEFAULT)
-        .build();
+    ArtifactoryRegistrySpecBuilder artifactoryRegistrySpecBuilder = ArtifactoryRegistrySpec.builder()
+                                                                        .connectorRef(connectorRef)
+                                                                        .eventConditions(eventConditions)
+                                                                        .repositoryFormat(format)
+                                                                        .repository(repository)
+                                                                        .repositoryUrl(repositoryUrl);
+    if (isDockerFormat) {
+      artifactoryRegistrySpecBuilder.artifactPath(imagePath);
+    } else {
+      artifactoryRegistrySpecBuilder.artifactDirectory(imagePath);
+    }
+    return artifactoryRegistrySpecBuilder.build();
   }
 
   private ArtifactoryRegistryArtifactConfig generateDockerConfig(
