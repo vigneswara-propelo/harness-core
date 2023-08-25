3fbe66e8b8005a43760c2624e4266ee6d4b979c4
Adithya Viswanathan <adithya.viswanathan@harness.io>
2023-08-25 17:25:15 +0530

    [feat]: [CDS-77390]: Exposed CRUD operations for gitx webhooks (#51970)
    * [feat]: [CDS-77390]: Exposed CRUD operations for gitx webhooks on open api spec
    
    * [feat]: [CDS-77390]: added list api
    
    * [feat]: [CDS-77390]: added list api
    
    * [feat]: [CDS-77390]: added list api
    
    * [feat]: [CDS-77390]: added list api
    
    * [feat]: [CDS-77390]: added list api
    
    * [feat]: [CDS-77390]: Added response bodies for get api
    
    * [feat]: [CDS-77390]: review comments incorporated
    
    * [feat]: [CDS-77390]: review comments incorporated

---
+++ 120-ng-manager/src/main/java/io/harness/ng/gitxwebhook/GitXWebhooksApiImpl.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2023 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
+ */
+
+package io.harness.ng.gitxwebhook;
+
+import io.harness.spec.server.ng.v1.GitXWebhooksApi;
+import io.harness.spec.server.ng.v1.model.CreateGitXWebhookRequest;
+import io.harness.spec.server.ng.v1.model.ListGitXWebhookRequest;
+import io.harness.spec.server.ng.v1.model.UpdateGitXWebhookRequest;
+
+import com.google.inject.Inject;
+import javax.validation.Valid;
+import javax.ws.rs.core.Response;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@AllArgsConstructor(onConstructor = @__({ @Inject }))
+@Slf4j
+public class GitXWebhooksApiImpl implements GitXWebhooksApi {
+  @Override
+  public Response createGitxWebhook(@Valid CreateGitXWebhookRequest body, String harnessAccount) {
+    return null;
+  }
+
+  @Override
+  public Response getGitxWebhook(String gitXWebhookIdentifier, String harnessAccount) {
+    return Response.ok().build();
+  }
+
+  @Override
+  public Response updateGitxWebhook(
+      String gitXWebhookIdentifier, @Valid UpdateGitXWebhookRequest body, String harnessAccount) {
+    return Response.ok().build();
+  }
+
+  @Override
+  public Response deleteGitxWebhook(String gitXWebhookIdentifier, String harnessAccount) {
+    return Response.ok().build();
+  }
+
+  @Override
+  public Response listGitxWebhooks(@Valid ListGitXWebhookRequest body, String harnessAccount) {
+    return Response.ok().build();
+  }
+}
