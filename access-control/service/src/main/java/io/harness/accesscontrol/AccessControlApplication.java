de7eb2aa4c1de64f80c7032c9b333844bdfc2ce2
Ashish Sanodia <ashish.sanodia@harness.io>
2023-08-25 14:48:55 +0530

    [fix]: [PL-38841]: Removes default viewer role creation job (#49941)

--- access-control/service/src/main/java/io/harness/accesscontrol/AccessControlApplication.java
+++ access-control/service/src/main/java/io/harness/accesscontrol/AccessControlApplication.java
@@ -38,7 +38,6 @@ import io.harness.accesscontrol.principals.serviceaccounts.iterators.ServiceAcco
 import io.harness.accesscontrol.principals.usergroups.iterators.UserGroupReconciliationIterator;
 import io.harness.accesscontrol.principals.users.iterators.UserReconciliationIterator;
 import io.harness.accesscontrol.resources.resourcegroups.iterators.ResourceGroupReconciliationIterator;
-import io.harness.accesscontrol.roleassignments.worker.DefaultViewerRoleACLCreationService;
 import io.harness.accesscontrol.roleassignments.worker.ProjectOrgBasicRoleCreationService;
 import io.harness.accesscontrol.roleassignments.worker.UserRoleAssignmentRemovalService;
 import io.harness.accesscontrol.scopes.harness.iterators.ScopeReconciliationIterator;
@@ -291,7 +290,6 @@ public class AccessControlApplication extends Application<AccessControlConfigura
     environment.lifecycle().manage(injector.getInstance(SupportRoleAssignmentsReconciliationService.class));
     environment.lifecycle().manage(injector.getInstance(UserRoleAssignmentRemovalService.class));
     environment.lifecycle().manage(injector.getInstance(ProjectOrgBasicRoleCreationService.class));
-    environment.lifecycle().manage(injector.getInstance(DefaultViewerRoleACLCreationService.class));
     if (configuration.isDisableRedundantACLs()) {
       environment.lifecycle().manage(injector.getInstance(DisableRedundantACLService.class));
     }
