ea1f866a15863c64c2cf7d0b342285b4846acc32
Vikyath Harekal <vikyath.harekal@harness.io>
2023-08-31 17:26:14 +0530

    feat: [IDP-1085]: Add files to compute test coverage (#52298)

---
+++ idp-service/src/test/java/io/harness/idp/scorecard/datasourcelocations/service/DataSourceLocationServiceImplTest.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2023 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
+ */
+
+package io.harness.idp.scorecard.datasourcelocations.service;
+
+import static io.harness.rule.OwnerRule.VIKYATH_HAREKAL;
+
+import io.harness.CategoryTest;
+import io.harness.annotations.dev.HarnessTeam;
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.category.element.UnitTests;
+import io.harness.rule.Owner;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@OwnedBy(HarnessTeam.IDP)
+public class DataSourceLocationServiceImplTest extends CategoryTest {
+  @Test
+  @Owner(developers = VIKYATH_HAREKAL)
+  @Category(UnitTests.class)
+  public void test() {}
+}
