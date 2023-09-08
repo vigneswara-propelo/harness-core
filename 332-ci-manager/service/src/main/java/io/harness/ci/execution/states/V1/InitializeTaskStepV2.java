938ed8c676f25a30c6c0a901f17ebed2e26cea57
Harsh Jain <harsh.jain@harness.io>
2023-09-08 17:16:57 +0530

    feat: [CI-9534]: check build count before queuing (#52636)

--- 332-ci-manager/service/src/main/java/io/harness/ci/execution/states/V1/InitializeTaskStepV2.java
+++ 332-ci-manager/service/src/main/java/io/harness/ci/execution/states/V1/InitializeTaskStepV2.java
@@ -56,6 +56,7 @@ import io.harness.beans.yaml.extended.infrastrucutre.Infrastructure;
 import io.harness.beans.yaml.extended.infrastrucutre.K8sDirectInfraYaml;
 import io.harness.callback.DelegateCallbackToken;
 import io.harness.ci.config.CIExecutionServiceConfig;
+import io.harness.ci.enforcement.CIBuildEnforcer;
 import io.harness.ci.executable.CiAsyncExecutable;
 import io.harness.ci.execution.buildstate.BuildSetupUtils;
 import io.harness.ci.execution.buildstate.ConnectorUtils;
@@ -166,7 +167,7 @@ public class InitializeTaskStepV2 extends CiAsyncExecutable {
   @Inject private ExceptionManager exceptionManager;
   @Inject private AccountClient accountClient;
   @Inject private CIDelegateTaskExecutor ciDelegateTaskExecutor;
-
+  @Inject CIBuildEnforcer buildEnforcer;
   @Inject private ConnectorUtils connectorUtils;
   @Inject private CIFeatureFlagService ciFeatureFlagService;
   @Inject private BuildSetupUtils buildSetupUtils;
@@ -221,9 +222,12 @@ public class InitializeTaskStepV2 extends CiAsyncExecutable {
                                                .stageRunTimeId(stageRuntimeId)
                                                .stepParameters(RecastOrchestrationUtils.toJson(stepParameters))
                                                .build());
+
+    boolean availableCapacity = buildEnforcer.checkBuildEnforcement(AmbianceUtils.getAccountId(ambiance));
+
     boolean queueConcurrencyEnabled =
         ciFeatureFlagService.isEnabled(QUEUE_CI_EXECUTIONS_CONCURRENCY, AmbianceUtils.getAccountId(ambiance));
-    if (queueConcurrencyEnabled) {
+    if (queueConcurrencyEnabled && !availableCapacity) {
       String topic = ciExecutionServiceConfig.getQueueServiceClientConfig().getTopic();
       log.info("start executeAsyncAfterRbac for initialize step with queue. Topic: {}", topic);
       taskId = generateUuid();
@@ -244,6 +248,8 @@ public class InitializeTaskStepV2 extends CiAsyncExecutable {
             AmbianceUtils.getStageRuntimeIdAmbiance(ambiance)));
       }
     } else {
+      ciExecutionRepository.updateExecutionStatus(
+          AmbianceUtils.getAccountId(ambiance), ambiance.getStageExecutionId(), Status.RUNNING.toString());
       taskId = executeBuild(ambiance, stepParameters);
     }
 
@@ -252,7 +258,7 @@ public class InitializeTaskStepV2 extends CiAsyncExecutable {
             CollectionUtils.emptyIfNull(singletonList(logKey)));
 
     // Sending the status if feature flag is enabled
-    if (queueConcurrencyEnabled) {
+    if (queueConcurrencyEnabled && !availableCapacity) {
       return responseBuilder.setStatus(Status.QUEUED_LICENSE_LIMIT_REACHED).build();
     } else {
       InitStepV2DelegateTaskInfo initStepV2DelegateTaskInfo =
