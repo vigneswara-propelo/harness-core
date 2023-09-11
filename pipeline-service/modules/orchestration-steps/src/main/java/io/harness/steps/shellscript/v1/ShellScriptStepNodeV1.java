c69fec8f90e1ca0870934d3b780ad2b846694151
Shalini Agrawal <shalini.agrawal@harness.io>
2023-09-11 11:46:58 +0530

    feat: [CDS-78848]: Add support for when and failure strategies for Shellscript step (#52650)

---
+++ pipeline-service/modules/orchestration-steps/src/main/java/io/harness/steps/shellscript/v1/ShellScriptStepNodeV1.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2023 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Shield 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
+ */
+
+package io.harness.steps.shellscript.v1;
+
+import static io.harness.annotations.dev.HarnessTeam.PIPELINE;
+
+import static com.fasterxml.jackson.annotation.JsonTypeInfo.As.EXTERNAL_PROPERTY;
+import static com.fasterxml.jackson.annotation.JsonTypeInfo.Id.NAME;
+
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.plancreator.steps.common.SpecParameters;
+import io.harness.plancreator.steps.common.v1.StepElementParametersV1;
+import io.harness.plancreator.steps.common.v1.StepElementParametersV1.StepElementParametersV1Builder;
+import io.harness.plancreator.steps.common.v1.StepParametersUtilsV1;
+import io.harness.plancreator.steps.internal.v1.PmsAbstractStepNodeV1;
+import io.harness.pms.sdk.core.plan.creation.beans.PlanCreationContext;
+import io.harness.steps.StepSpecTypeConstants;
+import io.harness.steps.StepUtils;
+import io.harness.steps.shellscript.ShellScriptStepInfo;
+import io.harness.steps.shellscript.ShellScriptStepParameters;
+import io.harness.yaml.utils.NGVariablesUtils;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import lombok.Data;
+
+@Data
+@JsonTypeName(StepSpecTypeConstants.SHELL_SCRIPT)
+@OwnedBy(PIPELINE)
+public class ShellScriptStepNodeV1 extends PmsAbstractStepNodeV1 {
+  String type = StepSpecTypeConstants.SHELL_SCRIPT;
+
+  @JsonTypeInfo(use = NAME, property = "type", include = EXTERNAL_PROPERTY, visible = true) ShellScriptStepInfo spec;
+
+  // TODO: set rollback parameters
+  public StepElementParametersV1 getStepParameters(PlanCreationContext ctx) {
+    StepElementParametersV1Builder stepBuilder = StepParametersUtilsV1.getStepParameters(this);
+    stepBuilder.spec(getSpecParameters());
+    stepBuilder.type(StepSpecTypeConstants.SHELL_SCRIPT);
+    StepUtils.appendDelegateSelectorsToSpecParameters(spec, ctx);
+    return stepBuilder.build();
+  }
+
+  public SpecParameters getSpecParameters() {
+    return ShellScriptStepParameters.infoBuilder()
+        .executionTarget(spec.getExecutionTarget())
+        .onDelegate(spec.getOnDelegate())
+        .outputVariables(NGVariablesUtils.getMapOfVariablesWithoutSecretExpression(spec.getOutputVariables()))
+        .environmentVariables(NGVariablesUtils.getMapOfVariables(spec.getEnvironmentVariables(), 0L))
+        .secretOutputVariables(NGVariablesUtils.getSetOfSecretVars(spec.getOutputVariables()))
+        .shellType(spec.getShell())
+        .source(spec.getSource())
+        .delegateSelectors(spec.getDelegateSelectors())
+        .includeInfraSelectors(spec.getIncludeInfraSelectors())
+        .build();
+  }
+}
--- pipeline-service/modules/orchestration-steps/src/main/java/io/harness/steps/shellscript/v1/ShellScriptStepPlanCreatorV1.java
+++ pipeline-service/modules/orchestration-steps/src/main/java/io/harness/steps/shellscript/v1/ShellScriptStepPlanCreatorV1.java
@@ -11,19 +11,20 @@ import static io.harness.pms.yaml.YAMLFieldNameConstants.STEP;
 
 import io.harness.plancreator.DependencyMetadata;
 import io.harness.plancreator.PlanCreatorUtilsV1;
-import io.harness.plancreator.steps.internal.PMSStepInfo;
 import io.harness.plancreator.strategy.StrategyUtilsV1;
+import io.harness.pms.contracts.advisers.AdviserObtainment;
 import io.harness.pms.contracts.facilitators.FacilitatorObtainment;
 import io.harness.pms.contracts.facilitators.FacilitatorType;
 import io.harness.pms.contracts.plan.Dependency;
+import io.harness.pms.contracts.plan.ExpressionMode;
 import io.harness.pms.contracts.plan.HarnessStruct;
+import io.harness.pms.execution.OrchestrationFacilitatorType;
 import io.harness.pms.sdk.core.plan.PlanNode;
 import io.harness.pms.sdk.core.plan.PlanNode.PlanNodeBuilder;
 import io.harness.pms.sdk.core.plan.creation.beans.PlanCreationContext;
 import io.harness.pms.sdk.core.plan.creation.beans.PlanCreationResponse;
 import io.harness.pms.sdk.core.plan.creation.creators.PartialPlanCreator;
 import io.harness.pms.sdk.core.plan.creation.yaml.StepOutcomeGroup;
-import io.harness.pms.sdk.core.steps.io.StepParameters;
 import io.harness.pms.timeout.AbsoluteSdkTimeoutTrackerParameters;
 import io.harness.pms.timeout.SdkTimeoutObtainment;
 import io.harness.pms.yaml.DependenciesUtils;
@@ -33,15 +34,15 @@ import io.harness.pms.yaml.YamlField;
 import io.harness.pms.yaml.YamlUtils;
 import io.harness.serializer.KryoSerializer;
 import io.harness.steps.StepSpecTypeConstants;
-import io.harness.steps.shellscript.ShellScriptStepNode;
 import io.harness.timeout.trackers.absolute.AbsoluteTimeoutTrackerFactory;
 import io.harness.utils.TimeoutUtils;
-import io.harness.when.utils.RunInfoUtils;
+import io.harness.when.utils.v1.RunInfoUtilsV1;
 
 import com.google.common.collect.Sets;
 import com.google.inject.Inject;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import lombok.SneakyThrows;
@@ -62,23 +63,23 @@ public class ShellScriptStepPlanCreatorV1 implements PartialPlanCreator<YamlFiel
   @SneakyThrows
   @Override
   public PlanCreationResponse createPlanForField(PlanCreationContext ctx, YamlField field) {
-    ShellScriptStepNode stepNode = YamlUtils.read(field.getNode().toString(), ShellScriptStepNode.class);
+    final boolean isStepInsideRollback = PlanCreatorUtilsV1.isStepInsideRollback(ctx.getDependency());
+    ShellScriptStepNodeV1 stepNode = YamlUtils.read(field.getNode().toString(), ShellScriptStepNodeV1.class);
     Map<String, YamlField> dependenciesNodeMap = new HashMap<>();
-    StepParameters stepParameters = ((PMSStepInfo) stepNode.getStepSpecType()).getStepParameters(stepNode, null, ctx);
     PlanNodeBuilder builder =
         PlanNode.builder()
             .uuid(StrategyUtilsV1.getSwappedPlanNodeId(ctx, stepNode.getUuid()))
             .name(StrategyUtilsV1.getIdentifierWithExpression(ctx, field.getNodeName()))
             .identifier(StrategyUtilsV1.getIdentifierWithExpression(ctx, field.getId()))
-            .stepType(stepNode.getStepSpecType().getStepType())
+            .stepType(StepSpecTypeConstants.SHELL_SCRIPT_STEP_TYPE)
             .group(StepOutcomeGroup.STEP.name())
-            .stepParameters(stepParameters)
+            // TODO: send rollback parameters to this method which can be extracted from dependency
+            .stepParameters(stepNode.getStepParameters(ctx))
             .facilitatorObtainment(
                 FacilitatorObtainment.newBuilder()
-                    .setType(
-                        FacilitatorType.newBuilder().setType(stepNode.getStepSpecType().getFacilitatorType()).build())
+                    .setType(FacilitatorType.newBuilder().setType(OrchestrationFacilitatorType.TASK).build())
                     .build())
-            .whenCondition(RunInfoUtils.getRunConditionForStep(stepNode.getWhen()))
+            .whenCondition(RunInfoUtilsV1.getStepWhenCondition(stepNode.getWhen(), isStepInsideRollback))
             .timeoutObtainment(
                 SdkTimeoutObtainment.builder()
                     .dimension(AbsoluteTimeoutTrackerFactory.DIMENSION)
@@ -86,16 +87,16 @@ public class ShellScriptStepPlanCreatorV1 implements PartialPlanCreator<YamlFiel
                                     .timeout(TimeoutUtils.getTimeoutParameterFieldString(stepNode.getTimeout()))
                                     .build())
                     .build())
-            .skipUnresolvedExpressionsCheck(stepNode.getStepSpecType().skipUnresolvedExpressionsCheck())
-            .expressionMode(stepNode.getStepSpecType().getExpressionMode());
+            .skipUnresolvedExpressionsCheck(true)
+            .expressionMode(ExpressionMode.RETURN_NULL_IF_UNRESOLVED);
 
+    List<AdviserObtainment> adviserObtainmentList =
+        PlanCreatorUtilsV1.getAdviserObtainmentsForStep(kryoSerializer, ctx.getDependency(), field.getNode());
     if (field.getNode().getField(YAMLFieldNameConstants.SPEC).getNode().getField("strategy") == null) {
-      builder.adviserObtainments(PlanCreatorUtilsV1.getAdviserObtainmentsForStage(kryoSerializer, ctx.getDependency()));
+      builder.adviserObtainments(adviserObtainmentList);
     }
-    DependencyMetadata dependencyMetadata =
-        StrategyUtilsV1.getStrategyFieldDependencyMetadataIfPresent(kryoSerializer, ctx, field.getUuid(),
-            dependenciesNodeMap, PlanCreatorUtilsV1.getAdviserObtainmentsForStage(kryoSerializer, ctx.getDependency()));
-
+    DependencyMetadata dependencyMetadata = StrategyUtilsV1.getStrategyFieldDependencyMetadataIfPresent(
+        kryoSerializer, ctx, field.getUuid(), dependenciesNodeMap, adviserObtainmentList);
     // Both metadata and nodeMetadata contain the same metadata, the first one's value will be kryo serialized bytes
     // while second one can have values in their primitive form like strings, int, etc. and will have kryo serialized
     // bytes for complex objects. We will deprecate the first one in v1
