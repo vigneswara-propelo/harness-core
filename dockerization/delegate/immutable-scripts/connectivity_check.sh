76ebf722ccdead18a94414116b8154653aa87747
Xingchi Jin <109999795+xingchi-jin@users.noreply.github.com>
2023-09-14 10:44:44 -0700

    feat: [PL-39269]: load certificate from a common source of truth (#51054)
    * load certificate from a common source of truth
    
    * use default java truststore file
    
    * 1. add split pem block support 2. do load certs before conn test
    
    * revert an accidental change
    
    * change  CUSTOM_CERTS_PATH to CUSTOM_CERTS_DIR
    
    * rename env to CA_CERTS_DIR
    
    * 1. rename ADDITIONAL_CA_PATH to SHARED_CA_CERTS_PATH to avoid regression from current CI certs solution 2. fix some log lines
    
    * rename to  SHARED_CA_CERTS_PATH
    
    * add mkdir

--- dockerization/delegate/immutable-scripts/connectivity_check.sh
+++ dockerization/delegate/immutable-scripts/connectivity_check.sh
@@ -4,17 +4,38 @@
 # that can be found in the licenses directory at the root of this repository, also available at
 # https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
 
+function check_connection_with_ca_bundle() {
+  if [ -z $1 ]; then
+    echo "Checking connection to Harness manager using default ca bundle file"
+  else
+    echo "Checking connection to Harness manager using ca bundle from $1"
+  fi
+  command="CURL_CA_BUNDLE=$1 curl --connect-timeout 10 --write-out %{http_code} $MANAGER_PROXY_CURL -s $MANAGER_HOST_AND_PORT/api/account/$ACCOUNT_ID/status  --output /dev/null"
+  status=$(eval "$command")
+  echo -e "\tHTTP Status Code: $status, Curl exit code  $?"
+  [[ $status == 000 ]] && echo -e "\tConnectivity test failed. Please check network connection from your delegate to Harness manager." && return 1
+  [[ $status == 401 ]] && echo -e "\tAccount ID: $ACCOUNT_ID not found. Stop launching delegate." && exit 2
+  # other errors
+  [[ $status -ge 400 ]] && echo -e "\tConnection check to Harness manager failed." && return 3
+  return 0
+}
+
+# By default, curl takes /etc/pki/tls/certs/ca-bundle.crt as it's ca bundle in RHEL
+# curl will take ca bundle provided by CURL_CA_BUNDLE or --cacert argument
+RHEL_CA_TRUSTED_BUNDLE_FILE="/etc/pki/tls/certs/ca-bundle.trust.crt"
+
 if [[ $PRECHECK_CONN == "true" ]]; then
   set +e
   set -v
-  command="curl --connect-timeout 10 --write-out %{http_code} $MANAGER_PROXY_CURL -s $MANAGER_HOST_AND_PORT/api/account/$ACCOUNT_ID/status  --output /dev/null"
-  echo "Checking connection to Harness manager..."
-  status=$(eval "$command")
-  echo "HTTP Status Code: $status, Curl exit code  $?"
-  [[ $status == 000 ]] && echo "Connectivity test failed. Please check network connection from your delegate to Harness manager." && exit 1
-  [[ $status == 401 ]] && echo "Account ID: $ACCOUNT_ID not found. Stop launching delegate." && exit 2
-  # other errors
-  [[ $status -ge 400 ]] && echo "Connection check to Harness manager failed. Stop launching delegate." && exit 3
+  check_connection_with_ca_bundle
+  if [ $? != 0 ]; then
+    check_connection_with_ca_bundle "$RHEL_CA_TRUSTED_BUNDLE_FILE"
+    if [ $? != 0 ]; then
+      echo "Connection check to Harness manager failed. Stop launching delegate."
+      exit 4
+    fi
+  fi
+  echo "Connection test is successful."
   set -e
   set +v
 fi
