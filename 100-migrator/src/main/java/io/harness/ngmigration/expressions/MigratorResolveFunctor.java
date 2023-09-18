821e6d34c752afeefd92f3c636bf8484f07f2bd3
Lucas Sales <lucas.sales@harness.io>
2023-09-18 16:02:12 -0300

    feat: [CDS-79440]: adding support for replace expression (#53027)
    * feat: [CDS-79440]: adding support for replace expression
    
    * adding other cases
    
    * adding other cases

--- 100-migrator/src/main/java/io/harness/ngmigration/expressions/MigratorResolveFunctor.java
+++ 100-migrator/src/main/java/io/harness/ngmigration/expressions/MigratorResolveFunctor.java
@@ -11,11 +11,20 @@ import io.harness.expression.ExpressionEvaluator;
 import io.harness.expression.ExpressionResolveFunctor;
 
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MigratorResolveFunctor implements ExpressionResolveFunctor {
   private final Map<String, Object> context;
 
   private final ExpressionEvaluator expressionEvaluator = new ExpressionEvaluator();
+  private static final String REPLACE_EXPRESSION = "\\.replace\\(.*?\\)";
+  private static final String SPLIT_EXPRESSION = "\\.split\\(.*?\\)";
+  private static final String LOWER_EXPRESSION = ".toLowerCase()";
+  private static final String UPPER_EXPRESSION = ".toUpperCase()";
+
+  private static final Pattern REPLACE_PATTERN = Pattern.compile(REPLACE_EXPRESSION);
+  private static final Pattern SPLIT_PATTERN = Pattern.compile(SPLIT_EXPRESSION);
 
   public MigratorResolveFunctor(Map<String, Object> context) {
     this.context = context;
@@ -23,9 +32,25 @@ public class MigratorResolveFunctor implements ExpressionResolveFunctor {
 
   @Override
   public String processString(String expression) {
-    if (expression.contains(".toLowerCase()")) {
-      String treatedExpression = expression.replace(".toLowerCase()", "");
-      return expressionEvaluator.substitute(treatedExpression, context) + ".toLowerCase()";
+    Matcher replaceMatcher = REPLACE_PATTERN.matcher(expression);
+    Matcher splitMatcher = SPLIT_PATTERN.matcher(expression);
+    if (expression.contains(LOWER_EXPRESSION)) {
+      String treatedExpression = expression.replace(LOWER_EXPRESSION, "");
+      return expressionEvaluator.substitute(treatedExpression, context) + LOWER_EXPRESSION;
+    }
+    if (expression.contains(UPPER_EXPRESSION)) {
+      String treatedExpression = expression.replace(UPPER_EXPRESSION, "");
+      return expressionEvaluator.substitute(treatedExpression, context) + UPPER_EXPRESSION;
+    }
+    if (replaceMatcher.find()) {
+      String replaceContent = replaceMatcher.group();
+      String treatedExpression = expression.replaceAll(REPLACE_EXPRESSION, "");
+      return expressionEvaluator.substitute(treatedExpression, context) + replaceContent;
+    }
+    if (splitMatcher.find()) {
+      String splitContent = splitMatcher.group();
+      String treatedExpression = expression.replaceAll(SPLIT_EXPRESSION, "");
+      return expressionEvaluator.substitute(treatedExpression, context) + splitContent;
     }
     return expressionEvaluator.substitute(expression, context);
   }
