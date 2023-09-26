a50d2f5203b87d002e4f576f977ad7cb1eb99c50
Abhinav Singh <abhinav.singh2@harness.io>
2023-09-26 11:34:47 +0530

    feat: [CDS-74011]: remove Rancher FF usages (#53283)
    * feat: [CDS-74011]: remove Rancher FF
    
    * feat: [CDS-74011]: remove Rancher FF from enum
    
    * Revert "feat: [CDS-74011]: remove Rancher FF from enum"
    
    This reverts commit ae0a428ff7255106bb3f8529139a249c2e214513.

--- 440-connector-nextgen/src/main/java/io/harness/connector/featureflagfilter/ConnectorEnumFilter.java
+++ 440-connector-nextgen/src/main/java/io/harness/connector/featureflagfilter/ConnectorEnumFilter.java
@@ -7,7 +7,6 @@
 
 package io.harness.connector.featureflagfilter;
 import static io.harness.annotations.dev.HarnessTeam.CDP;
-import static io.harness.beans.FeatureName.CDS_RANCHER_SUPPORT_NG;
 import static io.harness.beans.FeatureName.NG_SVC_ENV_REDESIGN;
 import static io.harness.remote.client.CGRestUtils.getResponse;
 
@@ -33,7 +32,6 @@ public class ConnectorEnumFilter extends EnumFeatureFlagFilter {
 
   public ConnectorEnumFilter() {
     put(NG_SVC_ENV_REDESIGN, Sets.newHashSet(ConnectorType.TAS));
-    put(CDS_RANCHER_SUPPORT_NG, Sets.newHashSet(ConnectorType.RANCHER));
   }
 
   @Override
