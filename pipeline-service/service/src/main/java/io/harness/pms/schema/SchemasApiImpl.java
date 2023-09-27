399800f8b2784dece1e30d14d753d280d2a5ec40
Mohit Garg <mohit.garg@harness.io>
2023-09-27 16:18:31 +0530

    feat: [CDS-80193]: integrate inputs schema api for given yaml input (#53409)

--- pipeline-service/service/src/main/java/io/harness/pms/schema/SchemasApiImpl.java
+++ pipeline-service/service/src/main/java/io/harness/pms/schema/SchemasApiImpl.java
@@ -13,13 +13,17 @@ import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.annotations.dev.ProductModule;
 import io.harness.pms.annotations.PipelineServiceAuth;
-import io.harness.pms.pipeline.service.PMSYamlSchemaServiceImpl;
+import io.harness.pms.pipeline.api.PipelinesApiUtils;
+import io.harness.pms.pipeline.service.PMSYamlSchemaService;
 import io.harness.spec.server.pipeline.v1.SchemasApi;
 import io.harness.spec.server.pipeline.v1.model.IndividualSchemaResponseBody;
+import io.harness.spec.server.pipeline.v1.model.PipelineInputSchemaDetailsResponseBody;
 import io.harness.spec.server.pipeline.v1.model.PipelineInputsSchemaRequestBody;
+import io.harness.yaml.schema.inputs.beans.YamlInputDetails;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.inject.Inject;
+import java.util.List;
 import javax.validation.Valid;
 import javax.ws.rs.core.Response;
 import lombok.AccessLevel;
@@ -32,7 +36,7 @@ import lombok.extern.slf4j.Slf4j;
 @PipelineServiceAuth
 @Slf4j
 public class SchemasApiImpl implements SchemasApi {
-  private final PMSYamlSchemaServiceImpl pmsYamlSchemaService;
+  private final PMSYamlSchemaService pmsYamlSchemaService;
 
   @Override
   public Response getIndividualStaticSchema(
@@ -46,6 +50,9 @@ public class SchemasApiImpl implements SchemasApi {
 
   @Override
   public Response getInputsSchema(@Valid PipelineInputsSchemaRequestBody body, String harnessAccount) {
-    return null;
+    List<YamlInputDetails> yamlInputDetails = pmsYamlSchemaService.getInputSchemaDetails(body.getPipelineYaml());
+    PipelineInputSchemaDetailsResponseBody responseBody =
+        PipelinesApiUtils.getPipelineInputSchemaDetailsResponseBody(yamlInputDetails);
+    return Response.ok().entity(responseBody).build();
   }
 }
