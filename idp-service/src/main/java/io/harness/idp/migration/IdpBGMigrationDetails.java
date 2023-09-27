b3db7fdeae8143586a5e0ae41148af58d7bf85e3
sandeepa-kv-jois <64277898+sandeepa-kv-jois@users.noreply.github.com>
2023-09-27 17:38:07 +0530

    fix: [IDP-915]: Fix URL config for Grafana plugin (#53419)

--- idp-service/src/main/java/io/harness/idp/migration/IdpBGMigrationDetails.java
+++ idp-service/src/main/java/io/harness/idp/migration/IdpBGMigrationDetails.java
@@ -57,6 +57,7 @@ public class IdpBGMigrationDetails implements MigrationDetails {
         .add(Pair.of(23, ScorecardMigration.class))
         .add(Pair.of(24, ScorecardMigration.class))
         .add(Pair.of(25, PluginInfoMigration.class))
+        .add(Pair.of(26, PluginInfoMigration.class))
         .build();
   }
 }
