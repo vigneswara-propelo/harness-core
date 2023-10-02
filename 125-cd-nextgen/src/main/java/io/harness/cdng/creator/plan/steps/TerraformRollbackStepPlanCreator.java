fa1745eef469cf1aef56324599f3e4f6324bf1d6
Vasile Lica <vasile.lica@harness.io>
2023-10-02 15:26:52 +0300

    fix: [CDS-80582]: bring back feature to support terraform steps v1 and v2 (#53584)

--- 125-cd-nextgen/src/main/java/io/harness/cdng/creator/plan/steps/TerraformRollbackStepPlanCreator.java
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/creator/plan/steps/TerraformRollbackStepPlanCreator.java
@@ -10,8 +10,10 @@ package io.harness.cdng.creator.plan.steps;
 import static io.harness.annotations.dev.HarnessTeam.CDP;
 
 import io.harness.annotations.dev.OwnedBy;
+import io.harness.beans.FeatureName;
 import io.harness.cdng.featureFlag.CDFeatureFlagHelper;
 import io.harness.cdng.provision.terraform.TerraformRollbackStepNode;
+import io.harness.cdng.provision.terraform.steps.rolllback.TerraformRollbackStep;
 import io.harness.cdng.provision.terraform.steps.rolllback.TerraformRollbackStepV2;
 import io.harness.executions.steps.StepSpecTypeConstants;
 import io.harness.pms.contracts.steps.StepType;
@@ -44,11 +46,21 @@ public class TerraformRollbackStepPlanCreator extends CDPMSStepPlanCreatorV2<Ter
 
   @Override
   public StepType getStepSpecType(PlanCreationContext ctx, TerraformRollbackStepNode stepElement) {
-    return TerraformRollbackStepV2.STEP_TYPE;
+    if (featureFlagService.isEnabled(ctx.getMetadata().getAccountIdentifier(),
+            FeatureName.CDS_SUPPORT_EXPRESSION_REMOTE_TERRAFORM_VAR_FILES_NG)) {
+      return TerraformRollbackStepV2.STEP_TYPE;
+    } else {
+      return TerraformRollbackStep.STEP_TYPE;
+    }
   }
 
   @Override
   public String getFacilitatorType(PlanCreationContext ctx, TerraformRollbackStepNode stepElement) {
-    return OrchestrationFacilitatorType.TASK_CHAIN;
+    if (featureFlagService.isEnabled(ctx.getMetadata().getAccountIdentifier(),
+            FeatureName.CDS_SUPPORT_EXPRESSION_REMOTE_TERRAFORM_VAR_FILES_NG)) {
+      return OrchestrationFacilitatorType.TASK_CHAIN;
+    } else {
+      return OrchestrationFacilitatorType.TASK;
+    }
   }
 }
