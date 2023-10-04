e51b1d6461658263d58e7c190e1b13d9a4ff1633
Meenakshi Raikwar <meenakshi.raikwar@harness.io>
2023-10-04 15:55:36 +0530

    Revert "feat: [PL-39825]: Added support for project and organization as a resource (#52935)" (#53700)
    This reverts commit 4d853e69fdc66d98acd2373fc2a077fdc9495d9d.

--- platform-service/modules/resource-group-service/src/main/java/io/harness/resourcegroup/resourceclient/organization/OrganizationResourceImpl.java
+++ platform-service/modules/resource-group-service/src/main/java/io/harness/resourcegroup/resourceclient/organization/OrganizationResourceImpl.java
@@ -10,8 +10,6 @@ package io.harness.resourcegroup.resourceclient.organization;
 import static io.harness.annotations.dev.HarnessTeam.PL;
 import static io.harness.data.structure.EmptyPredicate.isEmpty;
 import static io.harness.remote.client.NGRestUtils.getResponse;
-import static io.harness.resourcegroup.beans.ValidatorType.BY_RESOURCE_TYPE;
-import static io.harness.resourcegroup.beans.ValidatorType.BY_RESOURCE_TYPE_INCLUDING_CHILD_SCOPES;
 
 import static java.util.stream.Collectors.toList;
 import static org.apache.commons.lang3.StringUtils.stripToNull;
@@ -30,7 +28,6 @@ import io.harness.resourcegroup.framework.v1.service.Resource;
 import io.harness.resourcegroup.framework.v1.service.ResourceInfo;
 import io.harness.resourcegroup.v2.model.AttributeFilter;
 
-import com.google.common.collect.ImmutableMap;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
 import com.google.protobuf.InvalidProtocolBufferException;
@@ -73,7 +70,7 @@ public class OrganizationResourceImpl implements Resource {
 
   @Override
   public Map<ScopeLevel, EnumSet<ValidatorType>> getSelectorKind() {
-    return ImmutableMap.of(ScopeLevel.ACCOUNT, EnumSet.of(BY_RESOURCE_TYPE, BY_RESOURCE_TYPE_INCLUDING_CHILD_SCOPES));
+    return Collections.emptyMap();
   }
 
   @Override
@@ -83,7 +80,7 @@ public class OrganizationResourceImpl implements Resource {
 
   @Override
   public Set<ScopeLevel> getValidScopeLevels() {
-    return EnumSet.of(ScopeLevel.ACCOUNT);
+    return Collections.emptySet();
   }
 
   @Override
--- platform-service/modules/resource-group-service/src/main/java/io/harness/resourcegroup/resourceclient/project/ProjectResourceImpl.java
+++ platform-service/modules/resource-group-service/src/main/java/io/harness/resourcegroup/resourceclient/project/ProjectResourceImpl.java
@@ -10,8 +10,6 @@ package io.harness.resourcegroup.resourceclient.project;
 import static io.harness.annotations.dev.HarnessTeam.PL;
 import static io.harness.data.structure.EmptyPredicate.isEmpty;
 import static io.harness.remote.client.NGRestUtils.getResponse;
-import static io.harness.resourcegroup.beans.ValidatorType.BY_RESOURCE_TYPE;
-import static io.harness.resourcegroup.beans.ValidatorType.BY_RESOURCE_TYPE_INCLUDING_CHILD_SCOPES;
 
 import static java.util.stream.Collectors.toList;
 import static org.apache.commons.lang3.StringUtils.stripToNull;
@@ -31,7 +29,6 @@ import io.harness.resourcegroup.framework.v1.service.Resource;
 import io.harness.resourcegroup.framework.v1.service.ResourceInfo;
 import io.harness.resourcegroup.v2.model.AttributeFilter;
 
-import com.google.common.collect.ImmutableMap;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
 import com.google.protobuf.InvalidProtocolBufferException;
@@ -74,8 +71,7 @@ public class ProjectResourceImpl implements Resource {
 
   @Override
   public Map<ScopeLevel, EnumSet<ValidatorType>> getSelectorKind() {
-    return ImmutableMap.of(ScopeLevel.ACCOUNT, EnumSet.of(BY_RESOURCE_TYPE, BY_RESOURCE_TYPE_INCLUDING_CHILD_SCOPES),
-        ScopeLevel.ORGANIZATION, EnumSet.of(BY_RESOURCE_TYPE, BY_RESOURCE_TYPE_INCLUDING_CHILD_SCOPES));
+    return Collections.emptyMap();
   }
 
   @Override
@@ -85,7 +81,7 @@ public class ProjectResourceImpl implements Resource {
 
   @Override
   public Set<ScopeLevel> getValidScopeLevels() {
-    return EnumSet.of(ScopeLevel.ACCOUNT, ScopeLevel.ORGANIZATION);
+    return Collections.emptySet();
   }
 
   @Override
