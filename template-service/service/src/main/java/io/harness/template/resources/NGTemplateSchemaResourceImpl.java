5153304f478cfeddfacc42a8217a322051e265f0
Shalini Agrawal <shalini.agrawal@harness.io>
2023-10-18 10:52:49 +0530

    feat: [CDS-79207]: remove static schema API for templates (#53326)
    * feat: [CDS-79206]: remove /static api from PmsYamlSchemaResource
    
    * feat: [CDS-79206]: remove dynamic schema api for pipeline service
    
    * fix checkstyle
    
    * add back dynamic schema API with individual schema impl
    
    * add UT
    
    * add deprecated annotation
    
    * feat: [CDS-79207]: remove static schema API for templates
    
    * fix test
    
    * add back dynamic schema individual-schema api in resource
    
    * fix test
    
    * fix

--- template-service/service/src/main/java/io/harness/template/resources/NGTemplateSchemaResourceImpl.java
+++ template-service/service/src/main/java/io/harness/template/resources/NGTemplateSchemaResourceImpl.java
@@ -17,7 +17,6 @@ import io.harness.ng.core.dto.ResponseDTO;
 import io.harness.ng.core.template.TemplateEntityType;
 import io.harness.security.annotations.NextGenManagerAuth;
 import io.harness.template.services.NGTemplateSchemaService;
-import io.harness.template.utils.TemplateSchemaFetcher;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.google.inject.Inject;
@@ -34,7 +33,6 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class NGTemplateSchemaResourceImpl implements NGTemplateSchemaResource {
   private final NGTemplateSchemaService ngTemplateSchemaService;
-  private TemplateSchemaFetcher templateSchemaFetcher;
 
   @Override
   public ResponseDTO<JsonNode> getTemplateSchema(@NotNull TemplateEntityType templateEntityType,
@@ -43,17 +41,5 @@ public class NGTemplateSchemaResourceImpl implements NGTemplateSchemaResource {
     JsonNode schema =
         ngTemplateSchemaService.getIndividualStaticSchema(templateEntityType.getNodeGroup(), templateChildType, "v0");
     return ResponseDTO.newResponse(schema);
-  }
-
-  @Override
-  public ResponseDTO<JsonNode> getStaticYamlSchema(String accountIdentifier, String orgIdentifier,
-      String projectIdentifier, String templateChildType, TemplateEntityType templateEntityType, Scope scope,
-      String version) {
-    JsonNode staticJson = templateSchemaFetcher.getStaticYamlSchema(version);
-
-    // return static json if not empty or return the Pojo Schema
-    return staticJson != null ? ResponseDTO.newResponse(staticJson)
-                              : getTemplateSchema(templateEntityType, projectIdentifier, orgIdentifier, scope,
-                                  templateChildType, accountIdentifier);
   }
 }
