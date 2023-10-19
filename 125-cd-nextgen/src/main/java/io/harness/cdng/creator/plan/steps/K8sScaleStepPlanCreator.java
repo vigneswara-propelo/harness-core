e5c84e9ff0b09e31f3eda66be518cbd0edbfc07d
Abhinav Singh <abhinav.singh2@harness.io>
2023-10-19 13:10:02 +0530

    feat: [CDS-81982]: create async K8s scale step (#54364)

--- 125-cd-nextgen/src/main/java/io/harness/cdng/creator/plan/steps/K8sScaleStepPlanCreator.java
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/creator/plan/steps/K8sScaleStepPlanCreator.java
@@ -10,16 +10,42 @@ package io.harness.cdng.creator.plan.steps;
 import static io.harness.annotations.dev.HarnessTeam.CDP;
 
 import io.harness.annotations.dev.OwnedBy;
+import io.harness.beans.FeatureName;
+import io.harness.cdng.featureFlag.CDFeatureFlagHelper;
+import io.harness.cdng.k8s.K8sScaleStep;
 import io.harness.cdng.k8s.K8sScaleStepNode;
+import io.harness.cdng.k8s.asyncsteps.K8sScaleStepV2;
 import io.harness.executions.steps.StepSpecTypeConstants;
+import io.harness.pms.contracts.steps.StepType;
+import io.harness.pms.execution.OrchestrationFacilitatorType;
 import io.harness.pms.sdk.core.plan.creation.beans.PlanCreationContext;
 import io.harness.pms.sdk.core.plan.creation.beans.PlanCreationResponse;
 
 import com.google.common.collect.Sets;
+import com.google.inject.Inject;
 import java.util.Set;
 
 @OwnedBy(CDP)
 public class K8sScaleStepPlanCreator extends CDPMSStepPlanCreatorV2<K8sScaleStepNode> {
+  @Inject private CDFeatureFlagHelper featureFlagService;
+
+  @Override
+  protected String getFacilitatorType(PlanCreationContext ctx, K8sScaleStepNode stepElement) {
+    if (featureFlagService.isEnabled(
+            ctx.getMetadata().getAccountIdentifier(), FeatureName.CDS_K8S_ASYNC_STEP_STRATEGY)) {
+      return OrchestrationFacilitatorType.ASYNC;
+    }
+    return OrchestrationFacilitatorType.TASK;
+  }
+
+  @Override
+  protected StepType getStepSpecType(PlanCreationContext ctx, K8sScaleStepNode stepElement) {
+    if (featureFlagService.isEnabled(ctx.getAccountIdentifier(), FeatureName.CDS_K8S_ASYNC_STEP_STRATEGY)) {
+      return K8sScaleStepV2.STEP_TYPE;
+    }
+    return K8sScaleStep.STEP_TYPE;
+  }
+
   @Override
   public Set<String> getSupportedStepTypes() {
     return Sets.newHashSet(StepSpecTypeConstants.K8S_SCALE);
---
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/k8s/asyncsteps/K8sScaleStepV2.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2023 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
+ */
+
+package io.harness.cdng.k8s.asyncsteps;
+
+import io.harness.annotations.dev.CodePulse;
+import io.harness.annotations.dev.HarnessModuleComponent;
+import io.harness.annotations.dev.HarnessTeam;
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.annotations.dev.ProductModule;
+import io.harness.cdng.executables.CdAsyncExecutable;
+import io.harness.cdng.k8s.K8sScaleStep;
+import io.harness.delegate.task.k8s.K8sDeployResponse;
+import io.harness.executions.steps.ExecutionNodeType;
+import io.harness.pms.contracts.steps.StepCategory;
+import io.harness.pms.contracts.steps.StepType;
+
+@CodePulse(module = ProductModule.CDS, unitCoverageRequired = false, components = {HarnessModuleComponent.CDS_K8S})
+@OwnedBy(HarnessTeam.CDP)
+public class K8sScaleStepV2 extends CdAsyncExecutable<K8sDeployResponse, K8sScaleStep> {
+  public static final StepType STEP_TYPE = StepType.newBuilder()
+                                               .setType(ExecutionNodeType.K8S_SCALE_V2.getName())
+                                               .setStepCategory(StepCategory.STEP)
+                                               .build();
+}
