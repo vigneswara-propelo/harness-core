578420f11893356b83a477f261164e293f756566
Aleksa Buha <aleksa.buha@harness.io>
2023-10-31 16:13:12 +0100

    feat: [CDS-82520] Move K8s Dry run step to V2 (Async) (#54837)

--- 125-cd-nextgen/src/main/java/io/harness/cdng/creator/plan/steps/K8sDryRunManifestStepPlanCreator.java
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/creator/plan/steps/K8sDryRunManifestStepPlanCreator.java
@@ -11,19 +11,28 @@ import static io.harness.annotations.dev.HarnessTeam.CDP;
 import static io.harness.cdng.visitor.YamlTypes.K8S_CANARY_DEPLOY;
 
 import io.harness.annotations.dev.OwnedBy;
+import io.harness.beans.FeatureName;
+import io.harness.cdng.featureFlag.CDFeatureFlagHelper;
+import io.harness.cdng.k8s.K8sDryRunManifestStep;
 import io.harness.cdng.k8s.K8sDryRunManifestStepNode;
 import io.harness.cdng.k8s.K8sDryRunManifestStepParameters;
+import io.harness.cdng.k8s.asyncsteps.K8sDryRunManifestStepV2;
 import io.harness.executions.steps.StepSpecTypeConstants;
 import io.harness.plancreator.steps.common.StepElementParameters;
+import io.harness.pms.contracts.steps.StepType;
+import io.harness.pms.execution.OrchestrationFacilitatorType;
 import io.harness.pms.sdk.core.plan.creation.beans.PlanCreationContext;
 import io.harness.pms.sdk.core.plan.creation.beans.PlanCreationResponse;
 import io.harness.pms.sdk.core.steps.io.StepParameters;
 
 import com.google.common.collect.Sets;
+import com.google.inject.Inject;
 import java.util.Set;
 
 @OwnedBy(CDP)
 public class K8sDryRunManifestStepPlanCreator extends CDPMSStepPlanCreatorV2<K8sDryRunManifestStepNode> {
+  @Inject private CDFeatureFlagHelper featureFlagService;
+
   @Override
   public Set<String> getSupportedStepTypes() {
     return Sets.newHashSet(StepSpecTypeConstants.K8S_DRY_RUN_MANIFEST);
@@ -48,5 +57,23 @@ public class K8sDryRunManifestStepPlanCreator extends CDPMSStepPlanCreatorV2<K8s
         .setCanaryStepFqn(canaryStepFqn);
 
     return stepParameters;
+  }
+
+  @Override
+  public StepType getStepSpecType(PlanCreationContext ctx, K8sDryRunManifestStepNode stepElement) {
+    if (featureFlagService.isEnabled(
+            ctx.getMetadata().getAccountIdentifier(), FeatureName.CDS_K8S_ASYNC_STEP_STRATEGY)) {
+      return K8sDryRunManifestStepV2.STEP_TYPE;
+    }
+    return K8sDryRunManifestStep.STEP_TYPE;
+  }
+
+  @Override
+  public String getFacilitatorType(PlanCreationContext ctx, K8sDryRunManifestStepNode stepElement) {
+    if (featureFlagService.isEnabled(
+            ctx.getMetadata().getAccountIdentifier(), FeatureName.CDS_K8S_ASYNC_STEP_STRATEGY)) {
+      return OrchestrationFacilitatorType.ASYNC_CHAIN;
+    }
+    return OrchestrationFacilitatorType.TASK_CHAIN;
   }
 }
