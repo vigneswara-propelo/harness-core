4168a6b924576b40113416e9469b8c15c757ba11
Vinícius Calasans <vinicius.calasans@harness.io>
2023-11-17 10:31:33 -0300

    Remove Feature Flag CDS_NG_TRIGGER_EXECUTION_REFACTOR (#55497)

--- 956-feature-flag-beans/src/main/java/io/harness/beans/FeatureName.java
+++ 956-feature-flag-beans/src/main/java/io/harness/beans/FeatureName.java
@@ -134,8 +134,6 @@ public enum FeatureName {
   CDS_K8S_SOCKET_CAPABILITY_CHECK_NG(
       "Replace HTTP capability check for Kubernetes connector with Socket Capability", HarnessTeam.CDP),
   CDS_MULTI_SERVICE_PROPAGATION("FF for enabling multi service propagation for CD", HarnessTeam.CDP),
-  CDS_NG_TRIGGER_EXECUTION_REFACTOR(
-      "Refactor trigger execution to use same logic used in manual execution", HarnessTeam.SPG),
   CDS_NG_SECRET_RUNTIME_USAGE_EVENT_GENERATION(
       "FF to enable secret runtime usage event generation for ng", HarnessTeam.CDC),
   CDS_NG_SERVICE_PRINCIPAL_FOR_CUSTOM_WEBHOOK(
