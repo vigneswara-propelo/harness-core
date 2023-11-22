37320bdf17af0dd5519c927e7dc0b82b24df70fa
Shubhanshu Verma <shubhanshu.verma@harness.io>
2023-11-22 13:01:56 +0530

    [fix]: [CCM-15042]: Fixed label values filtering for a label key in clickhouse (#55745)
    * [fix]: [CCM-15042]: Fixed the query to get values for a particular label key
    
    * [fix]: [CCM-15042]: Removed redundant else statement

--- 450-ce-views/src/main/java/io/harness/ccm/views/graphql/ViewsQueryBuilder.java
+++ 450-ce-views/src/main/java/io/harness/ccm/views/graphql/ViewsQueryBuilder.java
@@ -163,7 +163,7 @@ public class ViewsQueryBuilder {
   private static final String MULTI_IF_STATEMENT_OPENING = "multiIf(";
   private static final String CLICKHOUSE_DISTINCT = "distinct %s";
   private static final String CLICKHOUSE_LABEL_KEYS = "arrayJoin(labels.keys)";
-  private static final String CLICKHOUSE_LABEL_VALUES = "arrayJoin(labels.values)";
+  private static final String CLICKHOUSE_LABEL_VALUES = "labels['%s']";
   private static final Double DEFAULT_MARKUP = 1.0;
   private static final String[] EMPTY_ARRAY = new String[0];
 
@@ -991,10 +991,9 @@ public class ViewsQueryBuilder {
                   LABEL_VALUE_UN_NESTED.getAlias());
             } else {
               if (isClickHouseQuery()) {
-                query.addCondition(
-                    getCondition(getLabelKeyFilter(new String[] {viewFieldInput.getFieldName()}, CLICKHOUSE_LABEL_KEYS),
-                        tableIdentifier, viewLabelsFlattened));
-                query.addAliasedColumn(new CustomSql(String.format(CLICKHOUSE_DISTINCT, CLICKHOUSE_LABEL_VALUES)),
+                String labelKey = viewFieldInput.getFieldName();
+                query.addAliasedColumn(
+                    new CustomSql(String.format(CLICKHOUSE_DISTINCT, String.format(CLICKHOUSE_LABEL_VALUES, labelKey))),
                     LABEL_VALUE_UN_NESTED.getAlias());
                 query.addCondition(
                     BinaryCondition.notEqualTo(new CustomSql(LABEL_VALUE_UN_NESTED.getAlias()), EMPTY_STRING));
