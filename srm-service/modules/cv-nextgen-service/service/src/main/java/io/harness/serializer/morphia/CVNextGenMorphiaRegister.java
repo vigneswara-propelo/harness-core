c09bce9456b3df8fc34a0a90c66a5110b36d2066
Deepak Chhikara <deepak.chhikara@harness.io>
2023-11-23 10:51:48 +0000

    feat: [SRM-16256]: adding error budget brun down to register class (#55878)

--- srm-service/modules/cv-nextgen-service/service/src/main/java/io/harness/serializer/morphia/CVNextGenMorphiaRegister.java
+++ srm-service/modules/cv-nextgen-service/service/src/main/java/io/harness/serializer/morphia/CVNextGenMorphiaRegister.java
@@ -107,6 +107,7 @@ import io.harness.cvng.servicelevelobjective.entities.Annotation;
 import io.harness.cvng.servicelevelobjective.entities.CompositeSLORecord;
 import io.harness.cvng.servicelevelobjective.entities.CompositeSLORecordBucket;
 import io.harness.cvng.servicelevelobjective.entities.CompositeServiceLevelObjective;
+import io.harness.cvng.servicelevelobjective.entities.ErrorBudgetBurnDown;
 import io.harness.cvng.servicelevelobjective.entities.MetricLessServiceLevelIndicator;
 import io.harness.cvng.servicelevelobjective.entities.RatioServiceLevelIndicator;
 import io.harness.cvng.servicelevelobjective.entities.RequestServiceLevelIndicator;
@@ -266,6 +267,7 @@ public class CVNextGenMorphiaRegister implements MorphiaRegistrar {
     set.add(AsyncAutoDiscoveryReImport.class);
     set.add(MetricLessServiceLevelIndicator.class);
     set.add(CompositeSLORecordBucket.class);
+    set.add(ErrorBudgetBurnDown.class);
   }
 
   @Override
