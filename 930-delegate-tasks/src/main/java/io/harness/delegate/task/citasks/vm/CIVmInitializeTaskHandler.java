bb23876397a3d24a050d1ac6ca5829cc0c6c35e3
sahithibanda01 <139854962+sahithibanda01@users.noreply.github.com>
2023-11-23 13:41:32 +0530

    fix: [CI-10296]: improved error message (#55800)
    * fix: [CI-10296]: improved error message
    
    * fix: [CI-10296]: removed unsed import
    
    * fix: [CI-10296]: .
    
    * fix: [CI-10296]: added else condition

--- 930-delegate-tasks/src/main/java/io/harness/delegate/task/citasks/vm/CIVmInitializeTaskHandler.java
+++ 930-delegate-tasks/src/main/java/io/harness/delegate/task/citasks/vm/CIVmInitializeTaskHandler.java
@@ -93,7 +93,11 @@ public class CIVmInitializeTaskHandler implements CIInitializeTaskHandler {
             .commandExecutionStatus(CommandExecutionStatus.SUCCESS)
             .build();
       } else {
-        errMessage = format("failed with code: %d, message: %s", response.code(), response.errorBody());
+        if (response.errorBody() != null) {
+          errMessage = format("failed with code: %d, message: %s", response.code(), response.errorBody().string());
+        } else {
+          errMessage = format("failed with code: %d, message: %s", response.code(), response.errorBody());
+        }
       }
     } catch (Exception e) {
       log.error("Failed to setup VM in runner", e);
