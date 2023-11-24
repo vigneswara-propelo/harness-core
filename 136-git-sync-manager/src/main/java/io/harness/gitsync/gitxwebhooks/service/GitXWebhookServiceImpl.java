9bd9574fbf18234dc91ad98723d62734d1728a6c
Adithya Viswanathan <adithya.viswanathan@harness.io>
2023-11-24 18:44:57 +0530

    [fix]: [CDS-85299]: Fix for not clearing the cache while updating the webhook trigger time (#55936)
    * [fix]: [CDS-85299]: Fix for not clearing the cache while updating the webhook trigger time
    
    * [fix]: [CDS-85299]: Fix for not clearing the cache while updating the webhook trigger time

--- 136-git-sync-manager/src/main/java/io/harness/gitsync/gitxwebhooks/service/GitXWebhookServiceImpl.java
+++ 136-git-sync-manager/src/main/java/io/harness/gitsync/gitxwebhooks/service/GitXWebhookServiceImpl.java
@@ -177,8 +177,10 @@ public class GitXWebhookServiceImpl implements GitXWebhookService {
         log.info(String.format("Updating Webhook with identifier %s in account %s",
             updateGitXWebhookCriteriaDTO.getWebhookIdentifier(),
             updateGitXWebhookCriteriaDTO.getScope().getAccountIdentifier()));
-        clearCache(
-            updateGitXWebhookCriteriaDTO.getScope().getAccountIdentifier(), updateGitXWebhookRequestDTO.getRepoName());
+        if (shouldClearGitXFileCache(updateGitXWebhookRequestDTO)) {
+          clearCache(updateGitXWebhookCriteriaDTO.getScope().getAccountIdentifier(),
+              updateGitXWebhookRequestDTO.getRepoName());
+        }
         Criteria criteria =
             buildCriteria(updateGitXWebhookCriteriaDTO.getScope(), updateGitXWebhookCriteriaDTO.getWebhookIdentifier());
         Query query = new Query(criteria);
@@ -402,5 +404,10 @@ public class GitXWebhookServiceImpl implements GitXWebhookService {
       throw new InternalServerErrorException(
           String.format("Error occurred while clearing the cache files in repo %s", repoName), ex);
     }
+  }
+
+  private boolean shouldClearGitXFileCache(UpdateGitXWebhookRequestDTO updateGitXWebhookRequestDTO) {
+    return isNotEmpty(updateGitXWebhookRequestDTO.getRepoName())
+        || isNotEmpty(updateGitXWebhookRequestDTO.getConnectorRef());
   }
 }
