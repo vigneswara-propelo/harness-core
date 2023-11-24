0c511c366141c01ec20a468fa5df968ee6d551e0
Meenakshi Raikwar <meenakshi.raikwar@harness.io>
2023-11-24 14:39:23 +0530

    feat: [PL-39825]: revert - Added support for project and organization (#54103)
    * Revert "Revert "feat: [PL-39825]: Added support for project and organization as a resource (#52935)" (#53700)"
    
    This reverts commit e51b1d6461658263d58e7c190e1b13d9a4ff1633.
    
    * feat: [PL-39825]: fix selector issue
    
    * feat: fix-2
    
    * feat: [PL-39825]: format fix
    
    * feat: [PL-39825]: format fix
    
    ---------
    
    Co-authored-by: Jimit Gandhi <jimit.gandhi@harness.io>

--- platform-service/modules/resource-group-service/src/main/java/io/harness/resourcegroup/resourceclient/project/ProjectResourceImpl.java
+++ platform-service/modules/resource-group-service/src/main/java/io/harness/resourcegroup/resourceclient/project/ProjectResourceImpl.java
@@ -10,6 +10,9 @@ package io.harness.resourcegroup.resourceclient.project;
 import static io.harness.annotations.dev.HarnessTeam.PL;
 import static io.harness.data.structure.EmptyPredicate.isEmpty;
 import static io.harness.remote.client.NGRestUtils.getResponse;
+import static io.harness.resourcegroup.beans.ValidatorType.BY_RESOURCE_IDENTIFIER;
+import static io.harness.resourcegroup.beans.ValidatorType.BY_RESOURCE_TYPE;
+import static io.harness.resourcegroup.beans.ValidatorType.BY_RESOURCE_TYPE_INCLUDING_CHILD_SCOPES;
 
 import static java.util.stream.Collectors.toList;
 import static org.apache.commons.lang3.StringUtils.stripToNull;
@@ -29,6 +32,7 @@ import io.harness.resourcegroup.framework.v1.service.Resource;
 import io.harness.resourcegroup.framework.v1.service.ResourceInfo;
 import io.harness.resourcegroup.v2.model.AttributeFilter;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
 import com.google.protobuf.InvalidProtocolBufferException;
@@ -64,14 +68,15 @@ public class ProjectResourceImpl implements Resource {
         getResponse(projectClient.listProjects(scope.getAccountIdentifier(), scope.getOrgIdentifier(), resourceIds));
     Set<String> validResourceIds =
         projects.getContent().stream().map(e -> e.getProject().getIdentifier()).collect(Collectors.toSet());
-    return resourceIds.stream()
-        .map(resourceId -> validResourceIds.contains(resourceId) && scope.getProjectIdentifier().equals(resourceId))
-        .collect(toList());
+    return resourceIds.stream().map(validResourceIds::contains).collect(toList());
   }
 
   @Override
   public Map<ScopeLevel, EnumSet<ValidatorType>> getSelectorKind() {
-    return Collections.emptyMap();
+    return ImmutableMap.of(ScopeLevel.ACCOUNT,
+        EnumSet.of(BY_RESOURCE_IDENTIFIER, BY_RESOURCE_TYPE, BY_RESOURCE_TYPE_INCLUDING_CHILD_SCOPES),
+        ScopeLevel.ORGANIZATION,
+        EnumSet.of(BY_RESOURCE_IDENTIFIER, BY_RESOURCE_TYPE, BY_RESOURCE_TYPE_INCLUDING_CHILD_SCOPES));
   }
 
   @Override
@@ -81,7 +86,7 @@ public class ProjectResourceImpl implements Resource {
 
   @Override
   public Set<ScopeLevel> getValidScopeLevels() {
-    return Collections.emptySet();
+    return EnumSet.of(ScopeLevel.ACCOUNT, ScopeLevel.ORGANIZATION);
   }
 
   @Override
