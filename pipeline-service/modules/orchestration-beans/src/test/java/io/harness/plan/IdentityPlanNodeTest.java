11fa8b812af81d3e08d3c01032af19e12a0e3dd7
ayushitiwari024 <142220748+ayushitiwari024@users.noreply.github.com>
2023-11-27 11:53:10 +0530

    feat: [CDS-79655]: analysis of pipeline-service : IdentityPlanNode.java (#55801)
    * Added UTs for IdentityPlanNode

--- pipeline-service/modules/orchestration-beans/src/test/java/io/harness/plan/IdentityPlanNodeTest.java
+++ pipeline-service/modules/orchestration-beans/src/test/java/io/harness/plan/IdentityPlanNodeTest.java
@@ -7,9 +7,11 @@
 
 package io.harness.plan;
 
+import static io.harness.rule.OwnerRule.AYUSHI_TIWARI;
 import static io.harness.rule.OwnerRule.PRASHANTSHARMA;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 import io.harness.advisers.nextstep.NextStepAdviser;
 import io.harness.annotations.dev.HarnessTeam;
@@ -21,9 +23,11 @@ import io.harness.pms.contracts.plan.ExecutionMode;
 import io.harness.pms.contracts.steps.SkipType;
 import io.harness.pms.contracts.steps.StepCategory;
 import io.harness.pms.contracts.steps.StepType;
+import io.harness.pms.data.stepparameters.PmsStepParameters;
 import io.harness.rule.Owner;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -32,6 +36,123 @@ import org.junit.experimental.categories.Category;
 public class IdentityPlanNodeTest {
   StepType TEST_STEP_TYPE = StepType.newBuilder().setType("TEST_STEP_PLAN").setStepCategory(StepCategory.STEP).build();
   StepType PMS_IDENTITY = StepType.newBuilder().setType("PMS_IDENTITY").build();
+
+  private static final String UUID = "uuid";
+  private static final String NAME = "name";
+  private static final String IDENTIFIER = "identifier";
+  private static final String GROUP = "group";
+  private static final String STEP_TYPE = "stepType";
+  private static final String SERVICE_NAME = "serviceName";
+  private static final String STAGE_FQN = "stageFqn";
+  private static final String WHEN_CONDITION = "whenCondition";
+  private static final String ORIGINAL_NODE_EXECUTIONID = "originalNodeExecutionID";
+  NodeType nodeType = NodeType.IDENTITY_PLAN_NODE;
+
+  @Test
+  @Owner(developers = AYUSHI_TIWARI)
+  @Category(UnitTests.class)
+  public void testMapPlanNodeToIdentityNodeFromNodeWithoutUseAdviserObtainmentsFlag() {
+    StepType stepType = StepType.newBuilder().setType(STEP_TYPE).build();
+    ExecutionMode executionMode = mock(ExecutionMode.class);
+    AdviserObtainment adviserObtainment = AdviserObtainment.getDefaultInstance();
+    List<AdviserObtainment> adviserObtainmentList = Collections.singletonList(adviserObtainment);
+    Map<ExecutionMode, List<AdviserObtainment>> advisorObtainmentsForExecutionMode =
+        Collections.singletonMap(executionMode, adviserObtainmentList);
+    IdentityPlanNode identityPlanNode = IdentityPlanNode.builder()
+                                            .uuid(UUID)
+                                            .name(NAME)
+                                            .identifier(IDENTIFIER)
+                                            .group(GROUP)
+                                            .stepType(stepType)
+                                            .advisorObtainmentsForExecutionMode(advisorObtainmentsForExecutionMode)
+                                            .adviserObtainments(adviserObtainmentList)
+                                            .useAdviserObtainments(null)
+                                            .isSkipExpressionChain(false)
+                                            .serviceName(SERVICE_NAME)
+                                            .stageFqn(STAGE_FQN)
+                                            .advisorObtainmentsForExecutionMode(advisorObtainmentsForExecutionMode)
+                                            .adviserObtainments(adviserObtainmentList)
+                                            .whenCondition(WHEN_CONDITION)
+                                            .originalNodeExecutionId(ORIGINAL_NODE_EXECUTIONID)
+                                            .build();
+
+    IdentityPlanNode expectedResult = IdentityPlanNode.builder()
+                                          .uuid(UUID)
+                                          .name(NAME)
+                                          .identifier(IDENTIFIER)
+                                          .group(GROUP)
+                                          .stepType(stepType)
+                                          .advisorObtainmentsForExecutionMode(advisorObtainmentsForExecutionMode)
+                                          .adviserObtainments(adviserObtainmentList)
+                                          .useAdviserObtainments(false)
+                                          .isSkipExpressionChain(false)
+                                          .serviceName(SERVICE_NAME)
+                                          .stageFqn(STAGE_FQN)
+                                          .advisorObtainmentsForExecutionMode(advisorObtainmentsForExecutionMode)
+                                          .adviserObtainments(adviserObtainmentList)
+                                          .whenCondition(WHEN_CONDITION)
+                                          .originalNodeExecutionId(ORIGINAL_NODE_EXECUTIONID)
+                                          .skipGraphType(SkipType.SKIP_NODE)
+                                          .build();
+
+    IdentityPlanNode actualResult =
+        IdentityPlanNode.mapPlanNodeToIdentityNode(identityPlanNode, stepType, ORIGINAL_NODE_EXECUTIONID, true);
+
+    assertThat(actualResult).isEqualTo(expectedResult);
+  }
+
+  @Test
+  @Owner(developers = AYUSHI_TIWARI)
+  @Category(UnitTests.class)
+  public void testMapPlanNodeToIdentityNodeFromNodeWithUseAdviserObtainmentsFlag() {
+    StepType stepType = StepType.newBuilder().setType(STEP_TYPE).build();
+    ExecutionMode executionMode = mock(ExecutionMode.class);
+    AdviserObtainment adviserObtainment = AdviserObtainment.getDefaultInstance();
+    List<AdviserObtainment> adviserObtainmentList = Collections.singletonList(adviserObtainment);
+    Map<ExecutionMode, List<AdviserObtainment>> advisorObtainmentsForExecutionMode =
+        Collections.singletonMap(executionMode, adviserObtainmentList);
+    IdentityPlanNode identityPlanNode = IdentityPlanNode.builder()
+                                            .uuid(UUID)
+                                            .name(NAME)
+                                            .identifier(IDENTIFIER)
+                                            .group(GROUP)
+                                            .stepType(stepType)
+                                            .advisorObtainmentsForExecutionMode(advisorObtainmentsForExecutionMode)
+                                            .adviserObtainments(adviserObtainmentList)
+                                            .useAdviserObtainments(false)
+                                            .isSkipExpressionChain(false)
+                                            .serviceName(SERVICE_NAME)
+                                            .stageFqn(STAGE_FQN)
+                                            .advisorObtainmentsForExecutionMode(advisorObtainmentsForExecutionMode)
+                                            .adviserObtainments(adviserObtainmentList)
+                                            .whenCondition(WHEN_CONDITION)
+                                            .originalNodeExecutionId(ORIGINAL_NODE_EXECUTIONID)
+                                            .build();
+
+    IdentityPlanNode expectedResult = IdentityPlanNode.builder()
+                                          .uuid(UUID)
+                                          .name(NAME)
+                                          .identifier(IDENTIFIER)
+                                          .group(GROUP)
+                                          .stepType(stepType)
+                                          .advisorObtainmentsForExecutionMode(advisorObtainmentsForExecutionMode)
+                                          .adviserObtainments(adviserObtainmentList)
+                                          .useAdviserObtainments(false)
+                                          .isSkipExpressionChain(false)
+                                          .serviceName(SERVICE_NAME)
+                                          .stageFqn(STAGE_FQN)
+                                          .advisorObtainmentsForExecutionMode(advisorObtainmentsForExecutionMode)
+                                          .adviserObtainments(adviserObtainmentList)
+                                          .whenCondition(WHEN_CONDITION)
+                                          .originalNodeExecutionId(ORIGINAL_NODE_EXECUTIONID)
+                                          .skipGraphType(SkipType.SKIP_NODE)
+                                          .build();
+
+    IdentityPlanNode actualResult =
+        IdentityPlanNode.mapPlanNodeToIdentityNode(identityPlanNode, stepType, ORIGINAL_NODE_EXECUTIONID, true);
+
+    assertThat(actualResult).isEqualTo(expectedResult);
+  }
 
   @Test
   @Owner(developers = PRASHANTSHARMA)
@@ -82,5 +203,57 @@ public class IdentityPlanNodeTest {
                                    .skipGraphType(SkipType.SKIP_NODE)
                                    .build();
     assertThat(identityPlanNodeExpected).isEqualTo(identityPlanNodeWithAlwaysSkipGraph);
+  }
+
+  @Test
+  @Owner(developers = AYUSHI_TIWARI)
+  @Category(UnitTests.class)
+  public void testConvertToListOfOGNodeExecIds() {
+    IdentityPlanNode identityPlanNode = IdentityPlanNode.builder().build();
+
+    identityPlanNode.convertToListOfOGNodeExecIds("123");
+
+    assertThat(identityPlanNode.getAllOriginalNodeExecutionIds()).hasSize(2);
+  }
+
+  @Test
+  @Owner(developers = AYUSHI_TIWARI)
+  @Category(UnitTests.class)
+  public void testMapPlanNodeToIdentityNodeWithSkipAsTrue() {
+    IdentityPlanNode identityPlanNode = IdentityPlanNode.builder().build();
+    PlanNode node = PlanNode.builder().build();
+    StepType stepType = StepType.newBuilder().build();
+    IdentityPlanNode identityPlanNode1 = identityPlanNode.mapPlanNodeToIdentityNodeWithSkipAsTrue(
+        "newUuid", node, "nodeIdentifier", "nodeName", stepType, "originalNodeExecutionId");
+    assertThat(identityPlanNode1.getSkipGraphType()).isEqualTo(SkipType.SKIP_NODE);
+  }
+
+  @Test
+  @Owner(developers = AYUSHI_TIWARI)
+  @Category(UnitTests.class)
+  public void testGetNodeType() {
+    IdentityPlanNode identityPlanNode = IdentityPlanNode.builder().build();
+    NodeType nodeType = identityPlanNode.getNodeType();
+    assertThat(nodeType).isEqualTo(NodeType.IDENTITY_PLAN_NODE);
+  }
+
+  @Test
+  @Owner(developers = AYUSHI_TIWARI)
+  @Category(UnitTests.class)
+  public void testIsSkipUnresolvedExpressionsCheck() {
+    IdentityPlanNode identityPlanNode = IdentityPlanNode.builder().build();
+    boolean result = identityPlanNode.isSkipUnresolvedExpressionsCheck();
+    assertThat(result).isEqualTo(true);
+  }
+
+  @Test
+  @Owner(developers = AYUSHI_TIWARI)
+  @Category(UnitTests.class)
+  public void testGetStepParameters() {
+    IdentityPlanNode identityPlanNode = IdentityPlanNode.builder().build();
+
+    PmsStepParameters result = identityPlanNode.getStepParameters();
+    assertThat(result).containsKey("originalNodeExecutionId");
+    assertThat(result.size()).isEqualTo(1);
   }
 }
