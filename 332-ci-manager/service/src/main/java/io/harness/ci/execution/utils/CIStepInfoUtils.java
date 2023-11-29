1606d571d6aedb731a2103753c4fbaa9b369449e
Scott Walker <scott.walker@harness.io>
2023-11-29 16:15:33 +0000

    [Fix]: [IAC-1269]: IACM approvals in kube (#56073)
    Fixing a class cast exception when using approvals with kube delegate.

--- 332-ci-manager/service/src/main/java/io/harness/ci/execution/utils/CIStepInfoUtils.java
+++ 332-ci-manager/service/src/main/java/io/harness/ci/execution/utils/CIStepInfoUtils.java
@@ -19,6 +19,7 @@ import io.harness.beans.FeatureName;
 import io.harness.beans.plugin.compatible.PluginCompatibleStep;
 import io.harness.beans.steps.CIRegistry;
 import io.harness.beans.steps.CIStepInfoType;
+import io.harness.beans.steps.stepinfo.IACMApprovalInfo;
 import io.harness.beans.steps.stepinfo.IACMTerraformPluginInfo;
 import io.harness.beans.steps.stepinfo.SecurityStepInfo;
 import io.harness.beans.steps.stepinfo.security.shared.STOGenericStepInfo;
@@ -207,11 +208,20 @@ public class CIStepInfoUtils {
     StepImageConfig defaultImageConfig = ciExecutionConfigService.getPluginVersionForK8(stepInfoType, accountId);
     if (stepInfoType == CIStepInfoType.SECURITY) {
       return getSecurityStepImageConfig(step, ciExecutionConfigService, defaultImageConfig);
-    } else if (stepInfoType == CIStepInfoType.IACM_TERRAFORM_PLUGIN || stepInfoType == CIStepInfoType.IACM_APPROVAL) {
+    } else if (stepInfoType == CIStepInfoType.IACM_TERRAFORM_PLUGIN) {
       if (((IACMTerraformPluginInfo) step).getImage().getValue() != null
           && isNotEmpty(((IACMTerraformPluginInfo) step).getImage().getValue())) {
         return StepImageConfig.builder()
             .image(((IACMTerraformPluginInfo) step).getImage().getValue())
+            .entrypoint(defaultImageConfig.getEntrypoint())
+            .windowsEntrypoint(defaultImageConfig.getWindowsEntrypoint())
+            .build();
+      }
+    } else if (stepInfoType == CIStepInfoType.IACM_APPROVAL) {
+      if (((IACMApprovalInfo) step).getImage().getValue() != null
+          && isNotEmpty(((IACMApprovalInfo) step).getImage().getValue())) {
+        return StepImageConfig.builder()
+            .image(((IACMApprovalInfo) step).getImage().getValue())
             .entrypoint(defaultImageConfig.getEntrypoint())
             .windowsEntrypoint(defaultImageConfig.getWindowsEntrypoint())
             .build();
