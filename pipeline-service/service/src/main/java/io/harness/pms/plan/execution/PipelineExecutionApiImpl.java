2396c3c0c8a6632d1947e1209c123e6644a1d239
utkarshchoubey-harness <utkarsh.choubey@harness.io>
2023-11-30 11:13:18 +0530

    feat: [CDS-85044]: Create Open API for Re-run and Retry pipeline for v1 (#55882)
    * feat: [CDS-85044]: Create Open API for Re-run and Retry pipeline for v1
    
    feat: [CDS-85044]: use existing params
    
    feat: [CDS-85044]: review comment
    
    * feat: [CDS-85044]: review comment
     Please enter the commit message for your changes. Lines starting

--- pipeline-service/service/src/main/java/io/harness/pms/plan/execution/PipelineExecutionApiImpl.java
+++ pipeline-service/service/src/main/java/io/harness/pms/plan/execution/PipelineExecutionApiImpl.java
@@ -7,21 +7,32 @@
 
 package io.harness.pms.plan.execution;
 
+import static io.harness.beans.FeatureName.PIE_GET_FILE_CONTENT_ONLY;
+
 import io.harness.annotations.dev.CodePulse;
 import io.harness.annotations.dev.HarnessModuleComponent;
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.annotations.dev.ProductModule;
+import io.harness.engine.executions.retry.RetryInfo;
 import io.harness.eraro.ErrorCode;
+import io.harness.exception.InvalidRequestException;
 import io.harness.exception.ngexception.NGTemplateException;
 import io.harness.exception.ngexception.PipelineException;
+import io.harness.gitaware.helper.GitAwareContextHelper;
+import io.harness.gitsync.interceptor.GitEntityInfo;
+import io.harness.gitx.USER_FLOW;
 import io.harness.pms.annotations.PipelineServiceAuth;
 import io.harness.spec.server.pipeline.v1.PipelineExecutionApi;
 import io.harness.spec.server.pipeline.v1.model.ExecutionDetails;
 import io.harness.spec.server.pipeline.v1.model.PipelineExecuteBody;
 import io.harness.spec.server.pipeline.v1.model.PipelineExecuteResponseBody;
+import io.harness.spec.server.pipeline.v1.model.RerunPipelineRequestBody;
+import io.harness.utils.PmsFeatureFlagService;
+import io.harness.utils.ThreadOperationContextHelper;
 
 import com.google.inject.Inject;
+import java.util.List;
 import javax.validation.Valid;
 import javax.ws.rs.core.Response;
 import lombok.AccessLevel;
@@ -35,6 +46,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class PipelineExecutionApiImpl implements PipelineExecutionApi {
   private final PipelineExecutor pipelineExecutor;
+  private final PmsFeatureFlagService pmsFeatureFlagService;
+  private final RetryExecutionHelper retryExecutionHelper;
 
   @Override
   public Response executePipeline(String org, String project, String pipeline, @Valid PipelineExecuteBody body,
@@ -46,15 +59,69 @@ public class PipelineExecutionApiImpl implements PipelineExecutionApi {
       }
       PlanExecutionResponseDto planExecutionResponseDto = pipelineExecutor.runPipelineWithInputSetPipelineYaml(
           harnessAccount, org, project, pipeline, module, inputSetPipelineYaml, false, notifyOnlyUser, notes);
-      PipelineExecuteResponseBody pipelineExecuteResponseBody = new PipelineExecuteResponseBody();
-      ExecutionDetails executionDetails = new ExecutionDetails();
-      executionDetails.setExecutionId(planExecutionResponseDto.getPlanExecution().getUuid());
-      executionDetails.setStatus(planExecutionResponseDto.getPlanExecution().getStatus().toString());
-      pipelineExecuteResponseBody.setExecutionDetails(executionDetails);
+      PipelineExecuteResponseBody pipelineExecuteResponseBody =
+          getPipelineExecutionResponseFromPlanExecutionResponse(planExecutionResponseDto);
       return Response.ok().entity(pipelineExecuteResponseBody).build();
     } catch (NGTemplateException ex) {
       throw new PipelineException(
           PipelineException.PIPELINE_Execution_MESSAGE, ex, ErrorCode.NG_PIPELINE_EXECUTION_EXCEPTION);
     }
+  }
+
+  @Override
+  public Response rerunPipeline(String org, String project, String pipeline, String executionId,
+      @Valid RerunPipelineRequestBody body, String harnessAccount, String module, Boolean useFqnIfError, String notes,
+      String branchName, String connectorRef, String repoName) {
+    GitAwareContextHelper.populateGitDetails(
+        GitEntityInfo.builder().branch(branchName).connectorRef(connectorRef).repoName(repoName).build());
+    if (pmsFeatureFlagService.isEnabled(harnessAccount, PIE_GET_FILE_CONTENT_ONLY)) {
+      ThreadOperationContextHelper.setUserFlow(USER_FLOW.EXECUTION);
+    }
+    String inputSetPipelineYaml = null;
+    if (body != null) {
+      inputSetPipelineYaml = body.getInputsYaml();
+    }
+    PlanExecutionResponseDto planExecutionResponseDto = pipelineExecutor.runPipelineWithInputSetPipelineYaml(
+        harnessAccount, org, project, pipeline, module, inputSetPipelineYaml, false, false, notes);
+    PipelineExecuteResponseBody pipelineExecuteResponseBody =
+        getPipelineExecutionResponseFromPlanExecutionResponse(planExecutionResponseDto);
+    return Response.ok().entity(pipelineExecuteResponseBody).build();
+  }
+
+  @Override
+  public Response retryPipelineWithInputsetPipelineYaml(String org, String project, String pipeline, String executionId,
+      @Valid RerunPipelineRequestBody body, String harnessAccount, String module, List retryStages,
+      Boolean runAllStages, String notes) {
+    if (pmsFeatureFlagService.isEnabled(harnessAccount, PIE_GET_FILE_CONTENT_ONLY)) {
+      ThreadOperationContextHelper.setUserFlow(USER_FLOW.EXECUTION);
+    }
+    if (retryStages.size() == 0) {
+      throw new InvalidRequestException("You need to select the stage to retry!!");
+    }
+
+    RetryInfo retryInfo = retryExecutionHelper.validateRetry(harnessAccount, org, project, pipeline, executionId, null);
+    if (!retryInfo.isResumable()) {
+      throw new InvalidRequestException(retryInfo.getErrorMessage());
+    }
+    String inputSetPipelineYaml = null;
+    if (body != null) {
+      inputSetPipelineYaml = body.getInputsYaml();
+    }
+    PlanExecutionResponseDto planExecutionResponseDto =
+        pipelineExecutor.retryPipelineWithInputSetPipelineYaml(harnessAccount, org, project, pipeline, module,
+            inputSetPipelineYaml, executionId, retryStages, runAllStages, false, false, notes);
+    PipelineExecuteResponseBody pipelineExecuteResponseBody =
+        getPipelineExecutionResponseFromPlanExecutionResponse(planExecutionResponseDto);
+    return Response.ok().entity(pipelineExecuteResponseBody).build();
+  }
+
+  PipelineExecuteResponseBody getPipelineExecutionResponseFromPlanExecutionResponse(
+      PlanExecutionResponseDto planExecutionResponseDto) {
+    PipelineExecuteResponseBody pipelineExecuteResponseBody = new PipelineExecuteResponseBody();
+    ExecutionDetails executionDetails = new ExecutionDetails();
+    executionDetails.setExecutionId(planExecutionResponseDto.getPlanExecution().getUuid());
+    executionDetails.setStatus(planExecutionResponseDto.getPlanExecution().getStatus().toString());
+    pipelineExecuteResponseBody.setExecutionDetails(executionDetails);
+    return pipelineExecuteResponseBody;
   }
 }
