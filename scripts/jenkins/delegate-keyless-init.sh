7c7e8c69d7d2fe55f7b022975e7e10b2677a7f1d
Marc Batchelor <marc.batchelor@harness.io>
2023-12-06 13:45:15 +0000

    feat: [BT-6065]: Install gcloud a different way (#13)

--- scripts/jenkins/delegate-keyless-init.sh
+++ scripts/jenkins/delegate-keyless-init.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-sudo curl -s -L -o /etc/yum.repos.d/google-cloud-sdk.repo https://harness.jfrog.io/artifactory/BuildsTools/yum-repos/google-cloud-sdk.repo
-sudo microdnf install -y yum
-sudo yum update -y
-sudo yum install -y python3
-sudo yum install -y python3-pip
-sudo yum install -y python3-requests
-sudo yum install -y google-cloud-cli --nodocs --skip-broken
-sudo yum install -y mongodb-enterprise-4.2.18 mongodb-enterprise-server-4.2.18 mongodb-enterprise-mongos-4.2.18 mongodb-enterprise-tools-4.2.18 --nodocs --skip-broken
 DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC
-sudo yum install -y tzdata wget sudo openssl jq gnupg unzip --nodocs --skip-broken
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-455.0.0-linux-x86_64.tar.gz
+tar -xf google-cloud-cli-455.0.0-linux-x86_64.tar.gz
+rm google-cloud-cli-455.0.0-linux-x86_64.tar.gz
+./google-cloud-sdk/install.sh --quiet
+export PATH=$PATH:/opt/harness-delegate/google-cloud-sdk/bin:/opt/harness-delegate/google-cloud-sdk/platform/bundledpythonunix/bin
+
