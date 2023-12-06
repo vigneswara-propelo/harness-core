f08271b222c71ed1dbb541653bb749d8b99535a0
Marc Batchelor <marc.batchelor@harness.io>
2023-12-06 14:43:32 +0000

    feat: [BT-6065]: Create .bashrc (#14)

--- scripts/jenkins/delegate-keyless-init.sh
+++ scripts/jenkins/delegate-keyless-init.sh
@@ -4,5 +4,12 @@ curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-
 tar -xf google-cloud-cli-455.0.0-linux-x86_64.tar.gz
 rm google-cloud-cli-455.0.0-linux-x86_64.tar.gz
 ./google-cloud-sdk/install.sh --quiet
-export PATH=$PATH:/opt/harness-delegate/google-cloud-sdk/bin:/opt/harness-delegate/google-cloud-sdk/platform/bundledpythonunix/bin
+# Fix Bash
+echo "alias ls='ls --color=auto'">>.bashrc
+echo "alias grep='grep --color=auto'">>.bashrc
+echo "alias ll='ls -alF'">>.bashrc
+echo "alias la='ls -A'">>.bashrc
+echo "alias l='ls -CF'">>.bashrc
+echo "export PYTHON_BIN_PATH=/opt/harness-delegate/google-cloud-sdk/platform/bundledpythonunix/bin">>.bashrc
+echo 'export PATH=$PATH:/opt/harness-delegate/google-cloud-sdk/bin:/opt/harness-delegate/google-cloud-sdk/platform/bundledpythonunix/bin'>>.bashrc
 
