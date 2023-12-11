da455464169155767b9ab5d1d9dc0fcdef80b8fd
Varsha Lalwani <varsha.lalwani@harness.io>
2023-12-11 14:38:55 +0530

    feat: Add FF for remediation tracker (#56599)

--- 956-feature-flag-beans/src/main/java/io/harness/beans/FeatureName.java
+++ 956-feature-flag-beans/src/main/java/io/harness/beans/FeatureName.java
@@ -924,7 +924,9 @@ public enum FeatureName {
   CDS_CONTAINER_STEP_GROUP_AWS_S3_DOWNLOAD(
       "Enable to dowmload Aws S3 buckets, folder or object inside container step group", HarnessTeam.CDP),
   CDS_USER_CD_LICENSE_VIEW("Enable to show Users CD License usage on UI", HarnessTeam.CDP),
-  SSCA_USE_ELK("Enables ELK search for Artifact listing instead of Mongo", HarnessTeam.SSCA);
+  SSCA_USE_ELK("Enables ELK search for Artifact listing instead of Mongo", HarnessTeam.SSCA),
+  SSCA_REMEDIATION_TRACKER("FF for Remediation Tracker flow.", HarnessTeam.SSCA);
+
   // keep-sorted end
 
   @Deprecated
