781fba73697ac3664d3027d9ff23cc724f5daaf8
Dev Mittal <devki.mittal@harness.io>
2023-12-13 17:35:15 +0530

    fix: [CI-8872]: upgrade scm version (#56714)

--- 960-api-services/src/main/java/io/harness/delegate/clienttools/ScmVersion.java
+++ 960-api-services/src/main/java/io/harness/delegate/clienttools/ScmVersion.java
@@ -16,7 +16,7 @@ import org.apache.commons.lang3.StringUtils;
 @Getter
 @Slf4j
 public enum ScmVersion implements ClientToolVersion {
-  DEFAULT("f5203793");
+  DEFAULT("80f7e024");
 
   private final String version;
 
--- dockerization/delegate/scripts/client_tools.sh
+++ dockerization/delegate/scripts/client_tools.sh
@@ -38,11 +38,11 @@ if [[ ( -z "$DISABLE_CLIENT_TOOLS") || ("$DISABLE_CLIENT_TOOLS" = "false") ]]; t
   && curl -s -L -o client-tools/kustomize/v3.5.4/kustomize https://app.harness.io/public/shared/tools/kustomize/release/v3.5.4/bin/linux/$ARCH/kustomize \
   && mkdir -m 777 -p client-tools/kustomize/v4.0.0 \
   && curl -s -L -o client-tools/kustomize/v4.0.0/kustomize https://app.harness.io/public/shared/tools/kustomize/release/v4.0.0/bin/linux/$ARCH/kustomize \
-  && mkdir -m 777 -p client-tools/scm/f5203793 \
-  && curl -s -L -o client-tools/scm/f5203793/scm https://app.harness.io/public/shared/tools/scm/release/f5203793/bin/linux/$ARCH/scm
+  && mkdir -m 777 -p client-tools/scm/80f7e024 \
+  && curl -s -L -o client-tools/scm/80f7e024/scm https://app.harness.io/public/shared/tools/scm/release/80f7e024/bin/linux/$ARCH/scm
 else
   echo "Client tools are disabled"
   echo "Installing scm binary"
-  mkdir -m 777 -p client-tools/scm/f5203793 \
-  && curl -s -L -o client-tools/scm/f5203793/scm https://app.harness.io/public/shared/tools/scm/release/f5203793/bin/linux/$ARCH/scm
+  mkdir -m 777 -p client-tools/scm/80f7e024 \
+  && curl -s -L -o client-tools/scm/80f7e024/scm https://app.harness.io/public/shared/tools/scm/release/80f7e024/bin/linux/$ARCH/scm
 fi
