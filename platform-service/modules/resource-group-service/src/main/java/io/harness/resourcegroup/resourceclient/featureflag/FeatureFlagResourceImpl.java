650d052956745b05ea4878b3226c4a454a94f271
danmack-harness <111863704+danmack-harness@users.noreply.github.com>
2023-12-14 09:08:31 +0000

    [feat]: [FFM-10200] Restrict Access Control Scope of Feature Flag resource group (#56289)
    * restrict scope of resource group
    
    * restrict scope of resource group
    
    * code format
    
    * adjust test for scope change
    
    * updates to restrict scope
    
    * format fix
    
    * update from review

--- platform-service/modules/resource-group-service/src/main/java/io/harness/resourcegroup/resourceclient/featureflag/FeatureFlagResourceImpl.java
+++ platform-service/modules/resource-group-service/src/main/java/io/harness/resourcegroup/resourceclient/featureflag/FeatureFlagResourceImpl.java
@@ -10,7 +10,6 @@ package io.harness.resourcegroup.resourceclient.featureflag;
 import static io.harness.annotations.dev.HarnessTeam.CF;
 import static io.harness.data.structure.EmptyPredicate.isEmpty;
 import static io.harness.resourcegroup.beans.ValidatorType.BY_ATTRIBUTE;
-import static io.harness.resourcegroup.beans.ValidatorType.BY_ATTRIBUTE_INCLUDING_CHILD_SCOPES;
 import static io.harness.resourcegroup.beans.ValidatorType.BY_RESOURCE_IDENTIFIER;
 import static io.harness.resourcegroup.beans.ValidatorType.BY_RESOURCE_TYPE;
 import static io.harness.resourcegroup.beans.ValidatorType.BY_RESOURCE_TYPE_INCLUDING_CHILD_SCOPES;
@@ -107,11 +106,9 @@ public class FeatureFlagResourceImpl implements Resource {
   @Override
   public Map<ScopeLevel, EnumSet<ValidatorType>> getSelectorKind() {
     return ImmutableMap.of(ScopeLevel.ACCOUNT,
-        EnumSet.of(BY_RESOURCE_IDENTIFIER, BY_RESOURCE_TYPE, BY_RESOURCE_TYPE_INCLUDING_CHILD_SCOPES,
-            BY_ATTRIBUTE_INCLUDING_CHILD_SCOPES),
+        EnumSet.of(BY_RESOURCE_IDENTIFIER, BY_RESOURCE_TYPE, BY_RESOURCE_TYPE_INCLUDING_CHILD_SCOPES),
         ScopeLevel.ORGANIZATION,
-        EnumSet.of(BY_RESOURCE_IDENTIFIER, BY_RESOURCE_TYPE, BY_RESOURCE_TYPE_INCLUDING_CHILD_SCOPES,
-            BY_ATTRIBUTE_INCLUDING_CHILD_SCOPES),
+        EnumSet.of(BY_RESOURCE_IDENTIFIER, BY_RESOURCE_TYPE, BY_RESOURCE_TYPE_INCLUDING_CHILD_SCOPES),
         ScopeLevel.PROJECT, EnumSet.of(BY_RESOURCE_IDENTIFIER, BY_RESOURCE_TYPE, BY_ATTRIBUTE));
   }
 
