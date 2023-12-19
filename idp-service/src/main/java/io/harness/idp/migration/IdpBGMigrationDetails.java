3e8b8f29e516b687f61c51ce7acc8415f21d70f5
Vikyath Harekal <vikyath.harekal@harness.io>
2023-12-19 16:40:49 +0530

    fix: [IDP-1807]: Fix migration ordering (#56945)

--- idp-service/src/main/java/io/harness/idp/migration/IdpBGMigrationDetails.java
+++ idp-service/src/main/java/io/harness/idp/migration/IdpBGMigrationDetails.java
@@ -84,8 +84,8 @@ public class IdpBGMigrationDetails implements MigrationDetails {
         .add(Pair.of(50, PluginInfoMigration.class))
         .add(Pair.of(51, PluginsInfoCreatedByMigration.class))
         .add(Pair.of(52, PluginInfoMigration.class))
-        .add(Pair.of(53, PluginsInfoClassMigration.class))
-        .add(Pair.of(54, PluginInfoMigration.class))
+        .add(Pair.of(53, PluginInfoMigration.class))
+        .add(Pair.of(54, PluginsInfoClassMigration.class))
         .add(Pair.of(55, IdpCatalogEntitiesAsHarnessEntitiesMigration.class))
         .build();
   }
