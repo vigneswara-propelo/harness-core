6f19bb51725b6c450a18c45b0353922f88619574
abhinav.hinger@harness.io <abhinav.hinger@harness.io>
2023-12-20 08:16:06 +0000

    feat: [CDS-87064]: add FF for onboarding overrides to gitX (#98)

--- 956-feature-flag-beans/src/main/java/io/harness/beans/FeatureName.java
+++ 956-feature-flag-beans/src/main/java/io/harness/beans/FeatureName.java
@@ -912,7 +912,8 @@ public enum FeatureName {
   CDS_USER_CD_LICENSE_VIEW("Enable to show Users CD License usage on UI", HarnessTeam.CDP),
   SSCA_USE_ELK("Enables ELK search for Artifact listing instead of Mongo", HarnessTeam.SSCA),
   SSCA_REMEDIATION_TRACKER("FF for Remediation Tracker flow.", HarnessTeam.SSCA),
-  CDS_VARIABLES_MERGE_V2_OPTIMIZED_FLOW("FF to switch the v2/variables flow to a new optimized one", CDC, Scope.GLOBAL);
+  CDS_VARIABLES_MERGE_V2_OPTIMIZED_FLOW("FF to switch the v2/variables flow to a new optimized one", CDC, Scope.GLOBAL),
+  CDS_OVERRIDES_GITX("Onboard Overrides entity to GitX", HarnessTeam.CDC);
 
   // keep-sorted end
 
